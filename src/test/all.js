03f92bb15573e380cdf7261e3f952b8792e4cd17
Ben Newman <bn@cs.stanford.edu>
2013-05-28 17:29:57 -0400

    Polyfill Function.prototype.bind during tests.

--- src/test/all.js
+++ src/test/all.js
@@ -2,6 +2,27 @@
 // modules in src/test and to specify an ordering on those modules, since
 // some still have implicit dependencies on others.
 
+var Ap = Array.prototype;
+var slice = Ap.slice;
+var Fp = Function.prototype;
+
+if (!Fp.bind) {
+  // PhantomJS doesn't support Function.prototype.bind natively, so
+  // polyfill it whenever this module is required.
+  Fp.bind = function(context) {
+    var func = this;
+    var args = slice.call(arguments, 1);
+    return args.length > 0 ? function() {
+      return func.apply(
+        context || this,
+        args.concat(slice.call(arguments))
+      );
+    } : function() {
+      return func.apply(context || this, arguments);
+    };
+  };
+}
+
 require("ReactTestUtils");
 require("reactComponentExpect");
 require("mocks");
