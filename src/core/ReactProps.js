54d3134da2c87fe96eae0df1ee1b05a4f0bbf117
CommitSyncScript <jeffmo@fb.com>
2013-06-04 12:15:29 -0700

    Add `ReactProps.func`
    This adds ReactProps.func so people don't need to write the
    slightly-more-cryptic ReactProps.instanceOf(Function). We should have
    had this all along.

--- src/core/ReactProps.js
+++ src/core/ReactProps.js
@@ -73,6 +73,7 @@ var Props = {
 
   array: createPrimitiveTypeChecker('array'),
   bool: createPrimitiveTypeChecker('boolean'),
+  func: createPrimitiveTypeChecker('function'),
   number: createPrimitiveTypeChecker('number'),
   object: createPrimitiveTypeChecker('object'),
   string: createPrimitiveTypeChecker('string'),
