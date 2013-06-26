9ceaff73183837387835f9e96c80e58fc4aea34c
CommitSyncScript <jeffmo@fb.com>
2013-06-26 14:29:31 -0700

    Refactor of ChangeEventPlugin
    some refactoring and also handle if `blur` doesn't fire on the form
    input in IE8 (by always cleaning up on focus). per discussions with
    @balpert

--- src/eventPlugins/ChangeEventPlugin.js
+++ src/eventPlugins/ChangeEventPlugin.js
@@ -50,12 +50,12 @@ var activeElementValueProp = null;
 /**
  * SECTION: handle `change` event
  */
-var shouldUseChangeEvent = function(elem) {
+function shouldUseChangeEvent(elem) {
   return (
     elem.nodeName === 'SELECT' ||
     (elem.nodeName === 'INPUT' && elem.type === 'file')
   );
-};
+}
 
 var doesChangeEventBubble = false;
 if (ExecutionEnvironment.canUseDOM) {
@@ -65,11 +65,7 @@ if (ExecutionEnvironment.canUseDOM) {
   );
 }
 
-/**
- * (For IE8). The `change` event does not bubble in IE8 so we add a listener
- * when the element is focused (and remove when blurred).
- */
-var handleChange = function(nativeEvent) {
+function manualDispatchChangeEvent(nativeEvent) {
   var event = SyntheticEvent.getPooled(
     eventTypes.change,
     activeElementID,
@@ -83,31 +79,43 @@ var handleChange = function(nativeEvent) {
   // process the abstract event manually.
   EventPluginHub.enqueueEvents(event);
   EventPluginHub.processEventQueue();
-};
+}
 
-var getTargetIDForChangeEvent;
-if (doesChangeEventBubble) {
-  getTargetIDForChangeEvent = function(
-      topLevelType,
-      topLevelTarget,
-      topLevelTargetID) {
-    if (topLevelType === topLevelTypes.topChange) {
-      return topLevelTargetID;
-    }
-  };
-} else {
-  getTargetIDForChangeEvent = function(
-      topLevelType,
-      topLevelTarget,
-      topLevelTargetID) {
-    if (topLevelType === topLevelTypes.topFocus) {
-      activeElementID = topLevelTargetID;
-      topLevelTarget.attachEvent('onchange', handleChange);
-    } else if (topLevelType === topLevelTypes.topBlur) {
-      topLevelTarget.detachEvent('onchange', handleChange);
-      activeElementID = null;
-    }
-  };
+function startWatchingForChangeEventIE8(target, targetID) {
+  activeElement = target;
+  activeElementID = targetID;
+  activeElement.attachEvent('onchange', manualDispatchChangeEvent);
+}
+
+function stopWatchingForChangeEventIE8() {
+  if (!activeElement) {
+    return;
+  }
+  activeElement.detachEvent('onchange', manualDispatchChangeEvent);
+  activeElement = null;
+  activeElementID = null;
+}
+
+function getTargetIDForChangeEvent(
+    topLevelType,
+    topLevelTarget,
+    topLevelTargetID) {
+  if (topLevelType === topLevelTypes.topChange) {
+    return topLevelTargetID;
+  }
+}
+function handleEventsForChangeEventIE8(
+    topLevelType,
+    topLevelTarget,
+    topLevelTargetID) {
+  if (topLevelType === topLevelTypes.topFocus) {
+    // stopWatching() should be a noop here but we call it just in case we
+    // missed a blur event somehow.
+    stopWatchingForChangeEventIE8();
+    startWatchingForChangeEventIE8(topLevelTarget, topLevelTargetID);
+  } else if (topLevelType === topLevelTypes.topBlur) {
+    stopWatchingForChangeEventIE8();
+  }
 }
 
 
@@ -145,12 +153,12 @@ var supportedInputTypes = {
   'week': true
 };
 
-var shouldUseInputEvent = function(elem) {
+function shouldUseInputEvent(elem) {
   return (
     (elem.nodeName === 'INPUT' && supportedInputTypes[elem.type]) ||
     elem.nodeName === 'TEXTAREA'
   );
-};
+}
 
 /**
  * (For old IE.) Replacement getter/setter for the `value` property that gets
@@ -171,7 +179,7 @@ var newValueProp =  {
  * and override the value property so that we can distinguish user events from
  * value changes in JS.
  */
-var startWatchingForValueChange = function(target, targetID) {
+function startWatchingForValueChange(target, targetID) {
   activeElement = target;
   activeElementID = targetID;
   activeElementValue = target.value;
@@ -182,13 +190,13 @@ var startWatchingForValueChange = function(target, targetID) {
 
   Object.defineProperty(activeElement, 'value', newValueProp);
   activeElement.attachEvent('onpropertychange', handlePropertyChange);
-};
+}
 
 /**
  * (For old IE.) Removes the event listeners from the currently-tracked element,
  * if any exists.
  */
-var stopWatchingForValueChange = function() {
+function stopWatchingForValueChange() {
   if (!activeElement) {
     return;
   }
@@ -201,13 +209,13 @@ var stopWatchingForValueChange = function() {
   activeElementID = null;
   activeElementValue = null;
   activeElementValueProp = null;
-};
+}
 
 /**
  * (For old IE.) Handles a propertychange event, sending a `change` event if
  * the value of the active element has changed.
  */
-var handlePropertyChange = function(nativeEvent) {
+function handlePropertyChange(nativeEvent) {
   if (nativeEvent.propertyName !== 'value') {
     return;
   }
@@ -217,83 +225,72 @@ var handlePropertyChange = function(nativeEvent) {
   }
   activeElementValue = value;
 
-  var event = SyntheticEvent.getPooled(
-    eventTypes.change,
-    activeElementID,
-    nativeEvent
-  );
-  EventPropagators.accumulateTwoPhaseDispatches(event);
-
-  // See comment above in handleChange
-  EventPluginHub.enqueueEvents(event);
-  EventPluginHub.processEventQueue();
-};
+  manualDispatchChangeEvent(nativeEvent);
+}
 
 /**
  * If a `change` event should be fired, returns the target's ID.
  */
-var getTargetIDForValueChangeEvent;
-if (isInputEventSupported) {
-  getTargetIDForValueChangeEvent = function(
-      topLevelType,
-      topLevelTarget,
-      topLevelTargetID) {
-    if (topLevelType === topLevelTypes.topInput) {
-      // In modern browsers (i.e., not IE8 or IE9), the input event is exactly
-      // what we want so fall through here and trigger an abstract event
-      return topLevelTargetID;
+function  getTargetIDForInputEvent(
+    topLevelType,
+    topLevelTarget,
+    topLevelTargetID) {
+  if (topLevelType === topLevelTypes.topInput) {
+    // In modern browsers (i.e., not IE8 or IE9), the input event is exactly
+    // what we want so fall through here and trigger an abstract event
+    return topLevelTargetID;
+  }
+}
+
+// For IE8 and IE9
+function handleEventsForInputEventIE(
+    topLevelType,
+    topLevelTarget,
+    topLevelTargetID) {
+  if (topLevelType === topLevelTypes.topFocus) {
+    // In IE8, we can capture almost all .value changes by adding a
+    // propertychange handler and looking for events with propertyName
+    // equal to 'value'
+    // In IE9, propertychange fires for most input events but is buggy and
+    // doesn't fire when text is deleted, but conveniently, selectionchange
+    // appears to fire in all of the remaining cases so we catch those and
+    // forward the event if the value has changed
+    // In either case, we don't want to call the event handler if the value
+    // is changed from JS so we redefine a setter for `.value` that updates
+    // our activeElementValue variable, allowing us to ignore those changes
+    //
+    // stopWatching() should be a noop here but we call it just in case we
+    // missed a blur event somehow.
+    stopWatchingForValueChange();
+    startWatchingForValueChange(topLevelTarget, topLevelTargetID);
+  } else if (topLevelType === topLevelTypes.topBlur) {
+    stopWatchingForValueChange();
+  } else if (
+      topLevelType === topLevelTypes.topSelectionChange ||
+      topLevelType === topLevelTypes.topKeyUp ||
+      topLevelType === topLevelTypes.topKeyDown) {
+    // On the selectionchange event, the target is just document which isn't
+    // helpful for us so just check activeElement instead.
+    //
+    // 99% of the time, keydown and keyup aren't necessary. IE8 fails to fire
+    // propertychange on the first input event after setting `value` from a
+    // script and fires only keydown, keypress, keyup. Catching keyup usually
+    // gets it and catching keydown lets us fire an event for the first
+    // keystroke if user does a key repeat (it'll be a little delayed: right
+    // before the second keystroke). Other input methods (e.g., paste) seem to
+    // fire selectionchange normally.
+    if (activeElement && activeElement.value !== activeElementValue) {
+      activeElementValue = activeElement.value;
+      return activeElementID;
     }
-  };
-} else {
-  getTargetIDForValueChangeEvent = function(
-      topLevelType,
-      topLevelTarget,
-      topLevelTargetID) {
-    if (topLevelType === topLevelTypes.topFocus) {
-      // In IE8, we can capture almost all .value changes by adding a
-      // propertychange handler and looking for events with propertyName
-      // equal to 'value'
-      // In IE9, propertychange fires for most input events but is buggy and
-      // doesn't fire when text is deleted, but conveniently, selectionchange
-      // appears to fire in all of the remaining cases so we catch those and
-      // forward the event if the value has changed
-      // In either case, we don't want to call the event handler if the value
-      // is changed from JS so we redefine a setter for `.value` that updates
-      // our activeElementValue variable, allowing us to ignore those changes
-      //
-      // stopWatching() should be a noop here but we call it just in case we
-      // missed a blur event somehow.
-      stopWatchingForValueChange();
-      startWatchingForValueChange(topLevelTarget, topLevelTargetID);
-    } else if (topLevelType === topLevelTypes.topBlur) {
-      stopWatchingForValueChange();
-    } else if (
-        topLevelType === topLevelTypes.topSelectionChange ||
-        topLevelType === topLevelTypes.topKeyUp ||
-        topLevelType === topLevelTypes.topKeyDown) {
-      // On the selectionchange event, the target is just document which isn't
-      // helpful for us so just check activeElement instead.
-      //
-      // 99% of the time, keydown and keyup aren't necessary. IE8 fails to fire
-      // propertychange on the first input event after setting `value` from a
-      // script and fires only keydown, keypress, keyup. Catching keyup usually
-      // gets it and catching keydown lets us fire an event for the first
-      // keystroke if user does a key repeat (it'll be a little delayed: right
-      // before the second keystroke). Other input methods (e.g., paste) seem to
-      // fire selectionchange normally.
-      if (activeElement && activeElement.value !== activeElementValue) {
-        activeElementValue = activeElement.value;
-        return activeElementID;
-      }
-    }
-  };
+  }
 }
 
 
 /**
  * SECTION: handle `click` event
  */
-var shouldUseClickEvent = function(elem) {
+function shouldUseClickEvent(elem) {
   // Use the `click` event to detect changes to checkbox and radio inputs.
   // This approach works across all browsers, whereas `change` does not fire
   // until `blur` in IE8.
@@ -301,16 +298,16 @@ var shouldUseClickEvent = function(elem) {
     elem.nodeName === 'INPUT' &&
     (elem.type === 'checkbox' || elem.type === 'radio')
   );
-};
+}
 
-var getTargetIDForClickEvent = function(
+function getTargetIDForClickEvent(
     topLevelType,
     topLevelTarget,
     topLevelTargetID) {
   if (topLevelType === topLevelTypes.topClick) {
     return topLevelTargetID;
   }
-};
+}
 
 /**
  * This plugin creates an `onChange` event that normalizes change events
@@ -340,30 +337,44 @@ var ChangeEventPlugin = {
       topLevelTargetID,
       nativeEvent) {
 
-    var getTargetIDFunc, targetID;
+    var getTargetIDFunc, handleEventFunc;
     if (shouldUseChangeEvent(topLevelTarget)) {
-      getTargetIDFunc = getTargetIDForChangeEvent;
+      if (doesChangeEventBubble) {
+        getTargetIDFunc = getTargetIDForChangeEvent;
+      } else {
+        handleEventFunc = handleEventsForChangeEventIE8;
+      }
     } else if (shouldUseInputEvent(topLevelTarget)) {
-      getTargetIDFunc = getTargetIDForValueChangeEvent;
+      if (isInputEventSupported) {
+        getTargetIDFunc = getTargetIDForInputEvent;
+      } else {
+        handleEventFunc = handleEventsForInputEventIE;
+      }
     } else if (shouldUseClickEvent(topLevelTarget)) {
       getTargetIDFunc = getTargetIDForClickEvent;
     }
+
     if (getTargetIDFunc) {
-      targetID = getTargetIDFunc(
+      var targetID = getTargetIDFunc(
         topLevelType,
         topLevelTarget,
         topLevelTargetID
       );
-    }
-
-    if (targetID) {
-      var event = SyntheticEvent.getPooled(
-        eventTypes.change,
-        targetID,
-        nativeEvent
+      if (targetID) {
+        var event = SyntheticEvent.getPooled(
+          eventTypes.change,
+          targetID,
+          nativeEvent
+        );
+        EventPropagators.accumulateTwoPhaseDispatches(event);
+        return event;
+      }
+    } else if (handleEventFunc) {
+      handleEventFunc(
+        topLevelType,
+        topLevelTarget,
+        topLevelTargetID
       );
-      EventPropagators.accumulateTwoPhaseDispatches(event);
-      return event;
     }
   }
 
