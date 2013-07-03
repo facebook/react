5e5064b256f4bfe7e4f83a93eed78067a189f904
Ben Newman <bn@cs.stanford.edu>
2013-07-03 12:22:51 -0400

    Enable module.exports mocking in react-test.js.
    We don't currently attempt to mock modules automatically, but we do
    respect require("mock-modules").mock, .dontMock, and .dumpCache.
    
    I'm going to keep investigating auto-mocking, since that would move us
    much closer to the behavior used within Facebook.
    
    Closes #154.
    Closes #155.

--- src/test/mock-modules.js
+++ src/test/mock-modules.js
@@ -16,23 +16,86 @@
  * @providesModule mock-modules
  */
 
+var mocks = require("mocks");
+var exportsRegistry = {};
+
+function getMock(exports) {
+    try {
+        return mocks.generateFromMetadata(
+            mocks.getMetadata(exports));
+    } catch (err) {
+        console.warn(err);
+        return exports;
+    }
+}
+
+// This function should be called at the bottom of any module that might
+// need to be mocked, after the final value of module.exports is known.
 exports.register = function(id, module) {
-    // TODO
+    var directive = exportsRegistry[id];
+
+    exportsRegistry[id] = {
+        module: module,
+        actual: module.exports,
+        mocked: null // Filled in lazily later.
+    };
+
+    // If doMock or doNotMock was called earlier, before the module was
+    // registered, then we ought to have recorded the choice as a string
+    // literal. Now that the module is registered, we can finally fulfill
+    // the request.
+    if (directive === "doNotMock") {
+        doNotMock(id);
+    } else if (directive === "doMock") {
+        doMock(id);
+    }
+
+    return exports;
 };
 
 exports.dumpCache = function() {
+    // This .clear() call intelligently resets all mocked properties that
+    // have been modified, so we don't have to recreate every mock exports
+    // object or invalidate the exportsRegistry.
     require("mocks").clear();
     global.require.dumpCache();
     return exports;
 };
 
-exports.dontMock = function() {
-    return exports;
-};
+// Call this function to ensure that require(id) returns the actual
+// exports object created by the module.
+function doNotMock(id) {
+    var entry = exportsRegistry[id];
+    if (entry && entry.module && entry.actual) {
+        entry.module.exports = entry.actual;
+    } else {
+        // When this module is first registered, make sure NOT to mock it.
+        exportsRegistry[id] = "doNotMock";
+    }
 
-exports.mock = function() {
     return exports;
-};
+}
+
+// Call this function to ensure that require(id) returns a mock exports
+// object based on the actual exports object created by the module.
+function doMock(id) {
+    var entry = exportsRegistry[id];
+    if (entry && entry.module && entry.actual) {
+        entry.module.exports = entry.mocked || (
+            // Because mocking can be expensive, create the mock exports
+            // object on demand, the first time doMock is called.
+            entry.mocked = getMock(entry.actual));
+    } else {
+        // When this module is first registered, make sure TO mock it.
+        exportsRegistry[id] = "doMock";
+    }
+
+    return exports;
+}
 
 var global = Function("return this")();
 require('test/mock-timers').installMockTimers(global);
+
+// Exported names are different for backwards compatibility.
+exports.dontMock = doNotMock;
+exports.mock = doMock;
