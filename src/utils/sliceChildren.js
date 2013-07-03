d50148591b9a8bf7a49f873e1eaf5f8af364cb70
Sebastian Markbage <sema@fb.com>
2013-07-02 18:26:54 -0700

    Introduce a supported way to slice children
    Introduces a counterpart to mapChildren. It excludes empty children just as
    mapChildren for compatibility. With might introduce something like
    sliceChildrenIncludingEmptyValues at some point.

---
+++ src/utils/sliceChildren.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2013 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @providesModule sliceChildren
+ */
+
+"use strict";
+
+var flattenChildren = require('flattenChildren');
+
+/**
+ * Slice children that are typically specified as `props.children`. This version
+ * of slice children ignores empty child components.
+ *
+ * @param {*} children The children set to filter.
+ * @param {number} start The first zero-based index to include in the subset.
+ * @param {?number} end The non-inclusive last index of the subset.
+ * @return {object} mirrored array with mapped children
+ */
+function sliceChildren(children, start, end) {
+  if (children == null) {
+    return children;
+  }
+
+  var slicedChildren = {};
+  var flattenedMap = flattenChildren(children);
+  var ii = 0;
+  for (var key in flattenedMap) {
+    if (!flattenedMap.hasOwnProperty(key)) {
+      continue;
+    }
+    var child = flattenedMap[key];
+    // In this version of slice children we ignore empty children.
+    if (child !== null) {
+      if (ii >= start) {
+        slicedChildren[key] = child;
+      }
+      ii++;
+      if (end != null && ii >= end) {
+        break;
+      }
+    }
+  }
+  return slicedChildren;
+}
+
+module.exports = sliceChildren;
