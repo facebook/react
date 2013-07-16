f4321f862485032f1f41cf2011475e57d53834f7
Jordan Walke <jordanjcw@fb.com>
2013-07-16 11:29:00 -0700

    [React Children] Step One: Refactor flattenChildren
    Refactoring `flattenChildren` so that the traversal code is reusable
    for other purposes.

--- src/utils/flattenChildren.js
+++ src/utils/flattenChildren.js
@@ -18,85 +18,36 @@
 
 "use strict";
 
-var ReactComponent = require('ReactComponent');
-var ReactTextComponent = require('ReactTextComponent');
-
 var throwIf = require('throwIf');
+var traverseAllChildren = require('traverseAllChildren');
 
 /**
- * @polyFill Array.isArray
+ * @param {function} traverseContext Context passed through traversal.
+ * @param {?ReactComponent} child React child component.
+ * @param {!string} name String name of key path to child.
  */
-
-
-var INVALID_CHILD = 'INVALID_CHILD';
-if (__DEV__) {
-  INVALID_CHILD =
-    'You may not pass a child of that type to a React component. It ' +
-    'is a common mistake to try to pass a standard browser DOM element ' +
-    'as a child of a React component.';
-}
-
-var DUPLICATE_KEY_ERROR =
-    'You have two children with identical keys. Make sure that you set the ' +
-    '"key" property to a unique value such as a row ID.';
-
-var flattenChildrenImpl = function(res, children, nameSoFar) {
-  if (Array.isArray(children)) {
-    for (var i = 0; i < children.length; i++) {
-      var child = children[i];
-      flattenChildrenImpl(
-        res,
-        child,
-        nameSoFar + '[' + ReactComponent.getKey(child, i) + ']'
-      );
-    }
-  } else {
-    var type = typeof children;
-    var isOnlyChild = nameSoFar === '';
-    // If it's the only child, treat the name as if it was wrapped in an array
-    // so that it's consistent if the number of children grows
-    var storageName = isOnlyChild ?
-                      '[' + ReactComponent.getKey(children, 0) + ']' :
-                      nameSoFar;
-    if (children === null || children === undefined || type === 'boolean') {
-      res[storageName] = null;
-    } else if (children.mountComponentIntoNode) {
-      /* We found a component instance */
-      if (__DEV__) {
-        throwIf(res.hasOwnProperty(storageName), DUPLICATE_KEY_ERROR);
-      }
-      res[storageName] = children;
-    } else {
-      if (type === 'object') {
-        throwIf(children && children.nodeType === 1, INVALID_CHILD);
-        for (var key in children) {
-          if (children.hasOwnProperty(key)) {
-            flattenChildrenImpl(
-              res,
-              children[key],
-              nameSoFar + '{' + key + '}'
-            );
-          }
-        }
-      } else if (type === 'string') {
-        res[storageName] = new ReactTextComponent(children);
-      } else if (type === 'number') {
-        res[storageName] = new ReactTextComponent('' + children);
-      }
-    }
+function flattenSingleChildIntoContext(traverseContext, child, name) {
+  // We found a component instance
+  var result = traverseContext;
+  if (__DEV__) {
+    throwIf(
+      result.hasOwnProperty(name),
+      traverseAllChildren.DUPLICATE_KEY_ERROR
+    );
   }
-};
+  result[name] = child;
+}
 
 /**
  * Flattens children that are typically specified as `props.children`.
- * @return {!Object} flattened children keyed by name.
+ * @return {!object} flattened children keyed by name.
  */
 function flattenChildren(children) {
   if (children === null || children === undefined) {
     return children;
   }
   var result = {};
-  flattenChildrenImpl(result, children, '');
+  traverseAllChildren(children, flattenSingleChildIntoContext, result);
   return result;
 }
 
