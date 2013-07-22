add809be2151a828f6a02fff393ad43b01486b28
Ben Newman <benjamn@fb.com>
2013-07-22 10:24:56 -0700

    Add comment explaining internalGetID

--- src/core/ReactID.js
+++ src/core/ReactID.js
@@ -57,6 +57,9 @@ function getID(node) {
 }
 
 function internalGetID(node) {
+  // If node is something like a window, document, or text node, none of
+  // which support attributes or a .getAttribute method, gracefully return
+  // the empty string, as if the attribute were missing.
   return node && node.getAttribute && node.getAttribute(ATTR_NAME) || '';
 }
 
