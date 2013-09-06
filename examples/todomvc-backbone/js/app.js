78d305eb167af7f5446c5d4812a160946f493234
Cheng Lou <chenglou92@gmail.com>
2013-09-06 16:40:35 -0400

    Fix backbone todo example bugs.
    Fixed:
    - New todo not submitting correctly (page refreshes. `preventDefault`
    wasn't there.
    - Old checked todo being removed will leave the checkmark on the next
    todo replacing its position.
    - Cannot change todo (`value`'s now a controlled field).
    - `autofocus` (should be `autoFocus`, how ironic given the current
    situation) on new todo input isn't working. Switched to manual
    `focus()` in `componentDidMount` for now.
    - More consistent breathing space between lines.
    - Gutter at 80.
    
    Added:
    - Use todomvc-common base.css. The old one had to change ids to
    classes. No longer necessary.
    - Give `cx` a better name and move it in `Utils`.
    - Trim input upon finishing edit.
    - Remove todo if the new edited value is empty.
    - Submit edited todo value on input blur.
    - README to explain the existence of this example. Being able to
    maintain a non-compilant version allows nice deviations from the
    todomvc specs, such as animations, in the future.

--- examples/todomvc-backbone/js/app.js
+++ examples/todomvc-backbone/js/app.js
@@ -1,20 +1,6 @@
 /** @jsx React.DOM */
 
-function cx(obj) {
-  var s = '';
-  for (var key in obj) {
-    if (!obj.hasOwnProperty(key)) {
-      continue;
-    }
-    if (obj[key]) {
-      s += key + ' ';
-    }
-  }
-  return s;
-}
-
 var Todo = Backbone.Model.extend({
-
   // Default attributes for the todo
   // and ensure that each todo created has `title` and `completed` keys.
   defaults: {
@@ -28,7 +14,6 @@ var Todo = Backbone.Model.extend({
       completed: !this.get('completed')
     });
   }
-
 });
 
 var TodoList = Backbone.Collection.extend({
@@ -68,6 +53,19 @@ var TodoList = Backbone.Collection.extend({
 var Utils = {
   pluralize: function( count, word ) {
     return count === 1 ? word : word + 's';
+  },
+
+  stringifyObjKeys: function(obj) {
+    var s = '';
+    for (var key in obj) {
+      if (!obj.hasOwnProperty(key)) {
+        continue;
+      }
+      if (obj[key]) {
+        s += key + ' ';
+      }
+    }
+    return s;
   }
 };
 
@@ -75,31 +73,47 @@ var Utils = {
 
 var TodoItem = React.createClass({
   handleSubmit: function(event) {
-    var val = this.refs.editField.getDOMNode().value;
+    var val = this.refs.editField.getDOMNode().value.trim();
     if (val) {
       this.props.onSave(val);
+    } else {
+      this.props.onDestroy();
     }
     return false;
   },
+
   onEdit: function() {
     this.props.onEdit();
     this.refs.editField.getDOMNode().focus();
   },
+
   render: function() {
+    var classes = Utils.stringifyObjKeys({
+      completed: this.props.todo.get('completed'), editing: this.props.editing
+    });
     return (
-      <li class={cx({completed: this.props.todo.get('completed'), editing: this.props.editing})}>
+      <li class={classes}>
         <div class="view">
           <input
             class="toggle"
             type="checkbox"
-            checked={this.props.todo.get('completed') ? 'checked' : null}
+            checked={this.props.todo.get('completed')}
             onChange={this.props.onToggle}
+            key={this.props.key}
           />
-          <label onDoubleClick={this.onEdit}>{this.props.todo.get('title')}</label>
+          <label onDoubleClick={this.onEdit}>
+            {this.props.todo.get('title')}
+          </label>
           <button class="destroy" onClick={this.props.onDestroy} />
         </div>
         <form onSubmit={this.handleSubmit}>
-          <input ref="editField" class="edit" value={this.props.todo.get('title')} />
+          <input
+            ref="editField"
+            class="edit"
+            defaultValue={this.props.todo.get('title')}
+            onBlur={this.handleSubmit}
+            autoFocus="autofocus"
+          />
         </form>
       </li>
     );
@@ -113,37 +127,46 @@ var TodoFooter = React.createClass({
 
     if (this.props.completedCount > 0) {
       clearButton = (
-        <button class="clear-completed" onClick={this.props.onClearCompleted}>Clear completed ({this.props.completedCount})</button>
+        <button id="clear-completed" onClick={this.props.onClearCompleted}>
+          Clear completed ({this.props.completedCount})
+        </button>
       );
     }
 
     return (
-      <footer class="footer">
-        <span class="todo-count"><strong>{this.props.count}</strong>{' '}{activeTodoWord}{' '}left</span>
+      <footer id="footer">
+        <span id="todo-count">
+          <strong>{this.props.count}</strong>{' '}
+          {activeTodoWord}{' '}left
+        </span>
         {clearButton}
       </footer>
     );
   }
 });
 
-// An example generic Mixin that you can add to any component that should react to changes in a Backbone component.
-// The use cases we've identified thus far are for Collections -- since they trigger a change event whenever
-// any of their constituent items are changed there's no need to reconcile for regular models.
-// One caveat: this relies on getBackboneModels() to always return the same model instances throughout the
-// lifecycle of the component. If you're using this mixin correctly (it should be near the top of your
-// component hierarchy) this should not be an issue.
+// An example generic Mixin that you can add to any component that should react
+// to changes in a Backbone component. The use cases we've identified thus far
+// are for Collections -- since they trigger a change event whenever any of
+// their constituent items are changed there's no need to reconcile for regular
+// models. One caveat: this relies on getBackboneModels() to always return the
+// same model instances throughout the lifecycle of the component. If you're
+// using this mixin correctly (it should be near the top of your component
+// hierarchy) this should not be an issue.
 var BackboneMixin = {
   componentDidMount: function() {
     // Whenever there may be a change in the Backbone data, trigger a reconcile.
-    this.getBackboneModels().map(function(model) {
-      model.on('add change remove', this.forceUpdate, this);
-    }.bind(this));
+    this.getBackboneModels().forEach(function(model) {
+      model.on('add change remove', this.forceUpdate.bind(this, null), this);
+    }, this);
   },
+
   componentWillUnmount: function() {
-    // Ensure that we clean up any dangling references when the component is destroyed.
-    this.getBackboneModels().map(function(model) {
+    // Ensure that we clean up any dangling references when the component is
+    // destroyed.
+    this.getBackboneModels().forEach(function(model) {
       model.off(null, null, this);
-    }.bind(this));
+    }, this);
   }
 };
 
@@ -152,21 +175,28 @@ var TodoApp = React.createClass({
   getInitialState: function() {
     return {editing: null};
   },
+
   componentDidMount: function() {
     // Additional functionality for todomvc: fetch() the collection on init
     this.props.todos.fetch();
+    this.refs.newField.getDOMNode().focus();
   },
+
   componentDidUpdate: function() {
-    // If saving were expensive we'd listen for mutation events on Backbone and do this manually.
-    // however, since saving isn't expensive this is an elegant way to keep it reactively up-to-date.
-    this.props.todos.map(function(todo) {
+    // If saving were expensive we'd listen for mutation events on Backbone and
+    // do this manually. however, since saving isn't expensive this is an
+    // elegant way to keep it reactively up-to-date.
+    this.props.todos.forEach(function(todo) {
       todo.save();
     });
   },
+
   getBackboneModels: function() {
     return [this.props.todos];
   },
-  handleSubmit: function() {
+
+  handleSubmit: function(event) {
+    event.preventDefault();
     var val = this.refs.newField.getDOMNode().value.trim();
     if (val) {
       this.props.todos.create({
@@ -176,48 +206,63 @@ var TodoApp = React.createClass({
       });
       this.refs.newField.getDOMNode().value = '';
     }
-    return false;
   },
+
   toggleAll: function(event) {
     var checked = event.nativeEvent.target.checked;
-    this.props.todos.map(function(todo) {
+    this.props.todos.forEach(function(todo) {
       todo.set('completed', checked);
     });
   },
-  destroy: function(todo) {
-    this.props.todos.remove(todo);
-  },
+
   edit: function(todo) {
     this.setState({editing: todo.get('id')});
   },
+
   save: function(todo, text) {
     todo.set('title', text);
     this.setState({editing: null});
   },
+
   clearCompleted: function() {
-    this.props.todos.completed().map(function(todo) {
+    this.props.todos.completed().forEach(function(todo) {
       todo.destroy();
     });
   },
+
   render: function() {
     var footer = null;
     var main = null;
     var todoItems = this.props.todos.map(function(todo) {
-      return <TodoItem todo={todo} onToggle={todo.toggle.bind(todo)} onDestroy={this.destroy.bind(this, todo)} onEdit={this.edit.bind(this, todo)} editing={this.state.editing === todo.get('id')} onSave={this.save.bind(this, todo)} />;
-    }.bind(this));
+      return (
+        <TodoItem
+          key={Math.random()}
+          todo={todo}
+          onToggle={todo.toggle.bind(todo)}
+          onDestroy={todo.destroy.bind(todo)}
+          onEdit={this.edit.bind(this, todo)}
+          editing={this.state.editing === todo.get('id')}
+          onSave={this.save.bind(this, todo)}
+        />
+      );
+    }, this);
 
     var activeTodoCount = this.props.todos.remaining().length;
     var completedCount = todoItems.length - activeTodoCount;
-	  if (activeTodoCount || completedCount) {
-      footer = <TodoFooter count={activeTodoCount} completedCount={completedCount} onClearCompleted={this.clearCompleted} />;
+    if (activeTodoCount || completedCount) {
+      footer =
+        <TodoFooter
+          count={activeTodoCount}
+          completedCount={completedCount}
+          onClearCompleted={this.clearCompleted}
+        />;
     }
 
     if (todoItems.length) {
       main = (
-        <section class="main">
-          <input class="toggle-all" type="checkbox" onChange={this.toggleAll} />
-          <label class="toggle-all-label">Mark all as complete</label>
-          <ul class="todo-list">
+        <section id="main">
+          <input id="toggle-all" type="checkbox" onChange={this.toggleAll} />
+          <ul id="todo-list">
             {todoItems}
           </ul>
         </section>
@@ -226,23 +271,33 @@ var TodoApp = React.createClass({
 
     return (
       <div>
-        <section class="todoapp">
-          <header class="header">
+        <section id="todoapp">
+          <header id="header">
             <h1>todos</h1>
             <form onSubmit={this.handleSubmit}>
-              <input ref="newField" class="new-todo" placeholder="What needs to be done?" autofocus="autofocus" />
+              <input
+                ref="newField"
+                id="new-todo"
+                placeholder="What needs to be done?"
+              />
             </form>
           </header>
           {main}
           {footer}
         </section>
-        <footer class="info">
+        <footer id="info">
           <p>Double-click to edit a todo</p>
-          <p>Created by{' '}<a href="http://github.com/petehunt/">petehunt</a></p>
+          <p>
+            Created by{' '}
+            <a href="http://github.com/petehunt/">petehunt</a>
+          </p>
           <p>Part of{' '}<a href="http://todomvc.com">TodoMVC</a></p>
         </footer>
       </div>
     );
   }
 });
-React.renderComponent(<TodoApp todos={new TodoList()} />, document.getElementById('todoapp'));
+
+React.renderComponent(
+  <TodoApp todos={new TodoList()} />, document.getElementById('container')
+);
