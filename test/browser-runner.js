fc572832b10792ce3aca764648e6f49d29dabadb
Thomas Aylott <aylott@fb.com>
2013-10-29 13:21:04 -0400

    Absolute urls for less flakiness

--- test/browser-runner.js
+++ test/browser-runner.js
@@ -5,7 +5,7 @@
     var scripts = document.getElementsByTagName('script');
     var a = document.createElement('a');
     a.href = scripts[scripts.length-1].src;
-    return a.pathname;
+    return a.protocol + '//' + a.hostname + ':' + a.port + a.pathname.replace(/^\/?/,'/');
   }());
 
   var __dirname = __filename.split('/').reverse().slice(1).reverse().join('/');
@@ -21,11 +21,17 @@
 
   window.ReactWebWorker_URL = __dirname + '/../src/test/worker.js' + cacheBust;
 
-  document.write('<script src="' + __dirname + '/../build/jasmine.js' + cacheBust + '"><\/script>');
-  document.write('<script src="' + __dirname + '/../build/react.js' + cacheBust + '"><\/script>');
-  document.write('<script src="' + __dirname + '/../build/react-test.js' + cacheBust + '"><\/script>');
-  document.write('<script src="' + __dirname + '/../node_modules/jasmine-tapreporter/src/tapreporter.js' + cacheBust + '"><\/script>');
-  document.write('<script src="' + __dirname + '/../test/the-files-to-test.generated.js' + cacheBust + '"><\/script>');
-  document.write('<script src="' + __dirname + '/../test/jasmine-execute.js' + cacheBust + '"><\/script>');
+  var urls = [
+    __dirname + '/../build/jasmine.js',
+    __dirname + '/../build/react.js',
+    __dirname + '/../build/react-test.js',
+    __dirname + '/../node_modules/jasmine-tapreporter/src/tapreporter.js',
+    __dirname + '/../test/the-files-to-test.generated.js',
+    __dirname + '/../test/jasmine-execute.js',
+  ];
+  
+  urls.forEach(function(url){
+    document.write('<script src="' + url + cacheBust + '"><\/script>');
+  })
 
 }());
