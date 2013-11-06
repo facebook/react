f289e9862a1819c3a1c6dcdec5b654b3ee75ea04
Thomas Aylott <aylott@fb.com>
2013-11-06 17:29:00 -0500

    rename sauce-harness to index

--- grunt/config/webdriver-jasmine.js
+++ grunt/config/webdriver-jasmine.js
@@ -5,7 +5,7 @@ exports.local = {
   webdriver: {
     remote: { protocol: 'http:', hostname: '127.0.0.1', port: 9515, path: '/' }
   },
-  url: "http://127.0.0.1:9999/test/sauce-harness.html",
+  url: "http://127.0.0.1:9999/test/index.html",
   onComplete: function(report){
     var browser = this;
     if (!report.passed){
