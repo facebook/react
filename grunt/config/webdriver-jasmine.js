3e2d3e48375af253fa85243f13bcb5492fd74875
Thomas Aylott <aylott@fb.com>
2013-11-06 15:19:17 -0500

    add logging to webdriver config

--- grunt/config/webdriver-jasmine.js
+++ grunt/config/webdriver-jasmine.js
@@ -1,16 +1,18 @@
+var grunt = require('grunt');
+
+
 exports.local = {
   webdriver: {
-    remote: {
-      protocol: 'http:',
-      hostname: '127.0.0.1',
-      port: '9515',
-      path: '/'
-    }
+    remote: { protocol: 'http:', hostname: '127.0.0.1', port: 9515, path: '/' }
   },
-  browser:{browserName:'chrome'},
   url: "http://127.0.0.1:9999/test/sauce-harness.html",
   onComplete: function(report){
     var browser = this;
-    console.log('report.passed', report.passed)
+    if (!report.passed){
+      grunt.fatal("tests failed");
+    }
+  },
+  onError: function(error){
+    grunt.fatal(error);
   }
 }
