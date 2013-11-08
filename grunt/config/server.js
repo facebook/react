66a0f2e7bde5b8e38457aab0513b609473be9c83
Thomas Aylott <aylott@fb.com>
2013-11-08 16:30:27 -0500

    Suppress encoding warning in Firefox

--- grunt/config/server.js
+++ grunt/config/server.js
@@ -16,6 +16,7 @@ module.exports = function(grunt){
   function testResultLoggerMiddleware(req, res, next) {
     if (!(req.body && req.body.data)) return next();
     grunt.log.writeln('[%s][%s]', req.headers['user-agent'], Date.now(), req.body.data);
+    res.write('<!doctype html><meta charset=utf-8>');
     res.end('Got it, thanks!');
   }
   
