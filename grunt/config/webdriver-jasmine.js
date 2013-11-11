d1fd4058daa79535f45ced936b33f891876ed95b
Thomas Aylott <aylott@fb.com>
2013-11-11 16:23:50 -0500

    sauce labs browser configs for running manually

--- grunt/config/webdriver-jasmine.js
+++ grunt/config/webdriver-jasmine.js
@@ -44,3 +44,45 @@ exports.saucelabs = {
   onComplete: exports.local.onComplete,
   onError: exports.local.onError
 }
+
+/* https://saucelabs.com/docs/platforms */
+exports.saucelabs_ios =
+exports.saucelabs_ios6_1 = sauceItUp({ browserName: 'iphone', version: '6.1', platform:'OS X 10.8' });
+exports.saucelabs_ios6 = sauceItUp({ browserName: 'iphone', version: '6', platform:'OS X 10.8' });
+exports.saucelabs_ios5_1 = sauceItUp({ browserName: 'iphone', version: '5.1', platform:'OS X 10.8' });
+exports.saucelabs_ios5 = sauceItUp({ browserName: 'iphone', version: '5', platform:'OS X 10.6' });
+exports.saucelabs_ios4 = sauceItUp({ browserName: 'iphone', version: '4', platform:'OS X 10.6' });
+
+exports.saucelabs_ipad =
+exports.saucelabs_ipad6_1 = sauceItUp({ browserName: 'ipad', version: '6.1', platform:'OS X 10.8' });
+exports.saucelabs_ipad6 = sauceItUp({ browserName: 'ipad', version: '6', platform:'OS X 10.8' });
+exports.saucelabs_ipad5_1 = sauceItUp({ browserName: 'ipad', version: '5.1', platform:'OS X 10.8' });
+exports.saucelabs_ipad5 = sauceItUp({ browserName: 'ipad', version: '5', platform:'OS X 10.6' });
+exports.saucelabs_ipad4 = sauceItUp({ browserName: 'ipad', version: '4', platform:'OS X 10.6' });
+
+exports.saucelabs_android = sauceItUp({ browserName: 'android', version: '4.0', platform:'Linux' });
+exports.saucelabs_android_tablet = sauceItUp({ browserName: 'android', version: '4.0', platform:'Linux', 'device-type':'tablet' });
+
+exports.saucelabs_safari = sauceItUp({ browserName: 'safari' });
+exports.saucelabs_chrome = sauceItUp({ browserName: 'chrome' });
+exports.saucelabs_firefox = sauceItUp({ browserName: 'firefox' });
+
+exports.saucelabs_ie =
+exports.saucelabs_ie8 = sauceItUp({ browserName: 'internet explorer', version: 8 });
+exports.saucelabs_ie9 = sauceItUp({ browserName: 'internet explorer', version: 9 });
+exports.saucelabs_ie10 = sauceItUp({ browserName: 'internet explorer', version: 10 });
+exports.saucelabs_ie11 = sauceItUp({ browserName: 'internet explorer', version: 11, platform:'Windows 8.1' });
+
+
+function sauceItUp(desiredCapabilities){
+  desiredCapabilities["build"] = exports.saucelabs.desiredCapabilities["build"];
+  desiredCapabilities["tunnel-identifier"] = exports.saucelabs.desiredCapabilities["tunnel-identifier"];
+  return {
+    webdriver: exports.saucelabs.webdriver,
+    url: exports.saucelabs.url,
+    onStart: exports.saucelabs.onStart,
+    onComplete: exports.saucelabs.onComplete,
+    onError: exports.saucelabs.onError,
+    desiredCapabilities: desiredCapabilities,
+  };
+}
