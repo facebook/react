9e9b3f7348fcb325250a82952be1298529b3c38a
Simon Højberg <r.hackr@gmail.com>
2013-11-13 23:02:06 -0500

    Transitions: Handle undefined input to mergeKeySet
    Gracefully handle undefined input to mergeKeySet.

--- src/addons/transitions/ReactTransitionGroup.js
+++ src/addons/transitions/ReactTransitionGroup.js
@@ -44,13 +44,9 @@ var ReactTransitionGroupMixin = {
       'getTransitionConfig() method.'
     );
 
-    // don't animate undefined children
-    if (typeof sourceChildren === 'undefined') {
-      return;
-    }
-
     var children = {};
     var childMapping = ReactTransitionKeySet.getChildMapping(sourceChildren);
+
     var transitionConfig = this.getTransitionConfig();
     var currentKeys = ReactTransitionKeySet.mergeKeySets(
       this._transitionGroupCurrentKeys,
