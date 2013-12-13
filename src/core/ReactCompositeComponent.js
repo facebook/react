toptaldev92/react_project

Rename receiveProps to receiveComponent
This renames receiveProps and changes it to take the next component to copy props from instead of just the props. That is,

  component.receiveComponent(nextComponent, transaction)

instead of

  component.receiveProps(nextComponent.props, transaction)

This is a precursor to adding contexts, which will also need to get propagated just like props. This change allows ReactCompositeComponent to override `receiveProps` and do something like

  this._pendingContext = nextComponent.context;

src/core/ReactCompositeComponent.js
@@ -794,7 +794,7 @@ var ReactCompositeComponentMixin = {
       var prevComponent = this._renderedComponent;
       var nextComponent = this._renderValidatedComponent();
       if (shouldUpdateReactComponent(prevComponent, nextComponent)) {
-        prevComponent.receiveProps(nextComponent.props, transaction);
+        prevComponent.receiveComponent(nextComponent, transaction);
       } else {
         // These two IDs are actually the same! But nothing should rely on that.
         var thisID = this._rootNodeID;

facebook/react

Conflicts:
	src/core/ReactCompositeComponent.js

src/core/ReactCompositeComponent.js
@@ -793,7 +793,7 @@ var ReactCompositeComponentMixin = {
       var currentComponent = this._renderedComponent;
       var nextComponent = this._renderValidatedComponent();
       if (currentComponent.constructor === nextComponent.constructor) {
-        currentComponent.receiveProps(nextComponent.props, transaction);
+        currentComponent.receiveComponent(nextComponent, transaction);
       } else {
         // These two IDs are actually the same! But nothing should rely on that.
         var thisID = this._rootNodeID;