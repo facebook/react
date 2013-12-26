e35f4c29bb378d58c283630f54da590c020310e9
Jignesh Kakadiya <jigneshhk1992@gmail.com>
2013-12-26 12:29:01 +0530

    fix params

--- src/core/ReactOwner.js
+++ src/core/ReactOwner.js
@@ -81,7 +81,7 @@ var ReactOwner = {
       'usually means that you\'re trying to add a ref to a component that ' +
       'doesn\'t have an owner (that is, was not created inside of another ' +
       'component\'s `render` method). Try rendering this component inside of ' +
-      'a new top-level component which will hold the ref.)'
+      'a new top-level component which will hold the ref.'
     );
     owner.attachRef(ref, component);
   },
