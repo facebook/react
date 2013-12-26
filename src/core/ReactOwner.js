ecf9f8ef6d1b075bb3cbbc6896b1b8f2766670d1
Jignesh Kakadiya <jigneshhk1992@gmail.com>
2013-12-26 12:17:36 +0530

    Adjust params

--- src/core/ReactOwner.js
+++ src/core/ReactOwner.js
@@ -80,8 +80,8 @@ var ReactOwner = {
       'addComponentAsRefTo(...): Only a ReactOwner can have refs. This ' +
       'usually means that you\'re trying to add a ref to a component that ' +
       'doesn\'t have an owner (that is, was not created inside of another ' +
-      'component\'s `render` method. Try rendering this component inside of ' +
-      'a new top-level component which will hold the ref.'
+      'component\'s `render` method). Try rendering this component inside of ' +
+      'a new top-level component which will hold the ref.)'
     );
     owner.attachRef(ref, component);
   },
@@ -101,7 +101,7 @@ var ReactOwner = {
       'removeComponentAsRefFrom(...): Only a ReactOwner can have refs. This ' +
       'usually means that you\'re trying to remove a ref to a component that ' +
       'doesn\'t have an owner (that is, was not created inside of another ' +
-      'component\'s `render` method. Try rendering this component inside of ' +
+      'component\'s `render` method). Try rendering this component inside of ' +
       'a new top-level component which will hold the ref.'
     );
     // Check that `component` is still the current ref because we do not want to
