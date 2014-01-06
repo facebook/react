3c40fb2e010a6b7ec8eaa510ad87d4838eea7609
Cheng Lou <chenglou92@gmail.com>
2014-01-05 21:54:24 -0800

    better error message for mergeHelpers and setState

--- src/core/ReactCompositeComponent.js
+++ src/core/ReactCompositeComponent.js
@@ -772,6 +772,14 @@ var ReactCompositeComponentMixin = {
       typeof partialState === 'object' || partialState == null,
       'setState(...): takes an object of state variables to update.'
     );
+    if (__DEV__){
+      if (partialState == null) {
+        console.warn(
+          'setState(...): You passed an undefined or null state object; ' +
+          'instead, use forceUpdate().'
+        );
+      }
+    }
     // Merge with `_pendingState` if it exists, otherwise with existing state.
     this.replaceState(
       merge(this._pendingState || this.state, partialState),
