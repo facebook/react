182a237fa77185ebb823e0f97ae3fa13abe4421a
Tim Yung <yungsters@fb.com>
2014-01-06 09:44:04 -0800

    Separate Module for Key Normalization
    This moves key normalization into its own module so that it can be re-used elsewhere.

--- src/event/synthetic/SyntheticKeyboardEvent.js
+++ src/event/synthetic/SyntheticKeyboardEvent.js
@@ -21,71 +21,14 @@
 
 var SyntheticUIEvent = require('SyntheticUIEvent');
 
-/**
- * Normalization of deprecated HTML5 "key" values
- * @see https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent#Key_names
- */
-
-var normalizeKey = {
-  'Esc': 'Escape',
-  'Spacebar': ' ',
-  'Left': 'ArrowLeft',
-  'Up': 'ArrowUp',
-  'Right': 'ArrowRight',
-  'Down': 'ArrowDown',
-  'Del': 'Delete',
-  'Win': 'OS',
-  'Menu': 'ContextMenu',
-  'Apps': 'ContextMenu',
-  'Scroll': 'ScrollLock',
-  'MozPrintableKey': 'Unidentified'
-};
-
-/**
- * Translation from legacy "which/keyCode" to HTML5 "key"
- * Only special keys supported, all others depend on keyboard layout or browser
- * @see https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent#Key_names
- */
-
-var translateToKey = {
-  8: 'Backspace',
-  9: 'Tab',
-  12: 'Clear',
-  13: 'Enter',
-  16: 'Shift',
-  17: 'Control',
-  18: 'Alt',
-  19: 'Pause',
-  20: 'CapsLock',
-  27: 'Escape',
-  32: ' ',
-  33: 'PageUp',
-  34: 'PageDown',
-  35: 'End',
-  36: 'Home',
-  37: 'ArrowLeft',
-  38: 'ArrowUp',
-  39: 'ArrowRight',
-  40: 'ArrowDown',
-  45: 'Insert',
-  46: 'Delete',
-  112: 'F1', 113: 'F2', 114: 'F3', 115: 'F4', 116: 'F5', 117: 'F6',
-  118: 'F7', 119: 'F8', 120: 'F9', 121: 'F10', 122: 'F11', 123: 'F12',
-  144: 'NumLock',
-  145: 'ScrollLock',
-  224: 'Meta'
-};
+var getEventKey = require('getEventKey');
 
 /**
  * @interface KeyboardEvent
  * @see http://www.w3.org/TR/DOM-Level-3-Events/
  */
 var KeyboardEventInterface = {
-  key: function(event) {
-    return 'key' in event ?
-      normalizeKey[event.key] || event.key :
-      translateToKey[event.which || event.keyCode] || 'Unidentified';
-  },
+  key: getEventKey,
   location: null,
   ctrlKey: null,
   shiftKey: null,
