4c4446d283552f9280305eaeda26be97680ff454
Paul O’Shannessy <paul@oshannessy.com>
2014-03-03 15:34:14 -0800

    Sync more vendored modules

---
+++ src/vendor/core/requestAnimationFrame.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @providesModule requestAnimationFrame
+ */
+
+var emptyFunction = require('emptyFunction');
+var nativeRequestAnimationFrame = require('nativeRequestAnimationFrame');
+
+var lastTime = 0;
+
+var requestAnimationFrame =
+  nativeRequestAnimationFrame ||
+  function(callback) {
+    var currTime = Date.now();
+    var timeDelay = Math.max(0, 16 - (currTime - lastTime));
+    lastTime = currTime + timeDelay;
+    return global.setTimeout(function() {
+      callback(Date.now());
+    }, timeDelay);
+  };
+
+// Works around a rare bug in Safari 6 where the first request is never invoked.
+requestAnimationFrame(emptyFunction);
+
+module.exports = requestAnimationFrame;
