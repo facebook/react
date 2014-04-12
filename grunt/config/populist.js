2220f0478cf5ba520e06bc12e6bce74dd6b5b3dd
petehunt <floydophone@gmail.com>
2014-04-12 21:05:30 +1000

    Drastically improve test performance

--- grunt/config/populist.js
+++ grunt/config/populist.js
@@ -1,24 +1,32 @@
 'use strict';
 
-var jasmine = {
-  rootDirectory: "build/jasmine",
-  // This syntax means to require and expose the "jasmine" module
-  // (build/jasmine/jasmine.js) as global.jasmine, and to require the
-  // "all" module (build/jasmine/all.js) but not expose it globally.
-  args: ["jasmine:jasmine", "all:"],
-  outfile: "./build/jasmine.js"
-};
+module.exports = function(grunt) {
+  var jasmine = {
+    rootDirectory: "build/jasmine",
+    // This syntax means to require and expose the "jasmine" module
+    // (build/jasmine/jasmine.js) as global.jasmine, and to require the
+    // "all" module (build/jasmine/all.js) but not expose it globally.
+    args: ["jasmine:jasmine", "all:"],
+    outfile: "./build/jasmine.js"
+  };
 
-var test = {
-  rootDirectory: "build/modules",
-  args: ["test/all:harness"],
-  requires: [
-    "**/__tests__/*-test.js"
-  ],
-  outfile: './build/react-test.js'
-};
+  var filterExpr = grunt.option('filter');
 
-module.exports = {
-  jasmine: jasmine,
-  test: test
+  if (filterExpr) {
+    filterExpr = '**/__tests__/' + filterExpr + '-test.js';
+  } else {
+    filterExpr = '**/__tests__/*-test.js';
+  }
+
+  var test = {
+    rootDirectory: "build/modules",
+    args: ["test/all:harness"],
+    requires: [filterExpr],
+    outfile: './build/react-test.js'
+  };
+
+  return {
+    jasmine: jasmine,
+    test: test
+  };
 };
