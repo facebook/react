c12b1f0289b2898716f54b931a14353065544122
Raynos <raynos2@gmail.com>
2014-04-18 20:38:16 -0700

    dom-property-config: move constants to own file
    This prepares the DefaultDOMPropertyConfig.js file to be broken
      out by breaing it's tight coupling to DOMProperty.js
    
    This will reduce complexity and size of `dom-property-config`
      module

---
+++ src/browser/ui/dom/DOMPropertyInjectionConstants.js
@@ -0,0 +1,11 @@
+var DOMPropertyInjectionConstants = {
+  MUST_USE_ATTRIBUTE: 0x1,
+  MUST_USE_PROPERTY: 0x2,
+  HAS_SIDE_EFFECTS: 0x4,
+  HAS_BOOLEAN_VALUE: 0x8,
+  HAS_NUMERIC_VALUE: 0x10,
+  HAS_POSITIVE_NUMERIC_VALUE: 0x20 | 0x10,
+  HAS_OVERLOADED_BOOLEAN_VALUE: 0x40,
+};
+
+module.exports = DOMPropertyInjectionConstants;
