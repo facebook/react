4ed9e22a4405de5b5dd5e275f9f3f1015c3de64e
Paul O’Shannessy <paul@oshannessy.com>
2014-04-23 16:35:17 -0700

    Upgrade jstransform to 4.0
    Also enables the object-concise-method transform for --harmony
    
    closes #1438

--- vendor/fbtransform/visitors.js
+++ vendor/fbtransform/visitors.js
@@ -1,6 +1,7 @@
 /*global exports:true*/
 var es6ArrowFunctions = require('jstransform/visitors/es6-arrow-function-visitors');
 var es6Classes = require('jstransform/visitors/es6-class-visitors');
+var es6ObjectConciseMethod = require('jstransform/visitors/es6-object-concise-method-visitors');
 var es6ObjectShortNotation = require('jstransform/visitors/es6-object-short-notation-visitors');
 var es6RestParameters = require('jstransform/visitors/es6-rest-param-visitors');
 var es6Templates = require('jstransform/visitors/es6-template-visitors');
@@ -13,6 +14,7 @@ var reactDisplayName = require('./transforms/reactDisplayName');
 var transformVisitors = {
   'es6-arrow-functions': es6ArrowFunctions.visitorList,
   'es6-classes': es6Classes.visitorList,
+  'es6-object-concise-method': es6ObjectConciseMethod.visitorList,
   'es6-object-short-notation': es6ObjectShortNotation.visitorList,
   'es6-rest-params': es6RestParameters.visitorList,
   'es6-templates': es6Templates.visitorList,
@@ -24,6 +26,7 @@ var transformVisitors = {
  */
 var transformRunOrder = [
   'es6-arrow-functions',
+  'es6-object-concise-method',
   'es6-object-short-notation',
   'es6-classes',
   'es6-rest-params',
