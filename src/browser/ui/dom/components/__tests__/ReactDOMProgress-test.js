edbaa9ed408eaaabbc98a497ab69dc873e856d49
Steven Luscher <github@steveluscher.com>
2014-04-24 00:07:03 -0700

    Skip the ReactDOMProgress tests if the <progress> element isn't supported by the test host.

--- src/browser/ui/dom/components/__tests__/ReactDOMProgress-test.js
+++ src/browser/ui/dom/components/__tests__/ReactDOMProgress-test.js
@@ -21,39 +21,44 @@
 
 /*jshint evil:true */
 
-var expectProgressToBeIndeterminate = function(progressInstance) {
-  var progressDOMNode = progressInstance.getDOMNode();
-  expect(progressDOMNode.position).toBe(-1); // For the "position" property to be -1 means that the <progress> element is in an indeterminate (barber-pole) state
-};
+var progressElement = document.createElement('progress');
+var progressElementSupported = typeof progressElement.max !== 'undefined';
 
-describe('ReactDOMProgress', function() {
-  var React;
-  var ReactTestUtils;
-  
-  beforeEach(function() {
-    React = require('React');
-    ReactTestUtils = require('ReactTestUtils');
-  });
+if(progressElementSupported) {
+  var expectProgressToBeIndeterminate = function(progressInstance) {
+    var progressDOMNode = progressInstance.getDOMNode();
+    expect(progressDOMNode.position).toBe(-1); // For the "position" property to be -1 means that the <progress> element is in an indeterminate (barber-pole) state
+  };
 
-  describe('when value transitions from being set to being null', function() {
+  describe('ReactDOMProgress', function() {
+    var React;
+    var ReactTestUtils;
+
     beforeEach(function() {
-      this.progressInstance = ReactTestUtils.renderIntoDocument(<progress value="3.14" max="100" />);
-      this.progressInstance.setProps({value: null});
+      React = require('React');
+      ReactTestUtils = require('ReactTestUtils');
     });
 
-    it('should return to the indeterminate state', function() {
-      expectProgressToBeIndeterminate(this.progressInstance);
+    describe('when value transitions from being set to being null', function() {
+      beforeEach(function() {
+        this.progressInstance = ReactTestUtils.renderIntoDocument(<progress value="3.14" max="100" />);
+        this.progressInstance.setProps({value: null});
+      });
+
+      it('should return to the indeterminate state', function() {
+        expectProgressToBeIndeterminate(this.progressInstance);
+      });
+    });
+
+    describe('when value transitions from being set to being undefined', function() {
+      beforeEach(function() {
+        this.progressInstance = ReactTestUtils.renderIntoDocument(<progress value="42" max="100" />);
+        this.progressInstance.setProps({value: (void 0)});
+      });
+
+      it('should return to the indeterminate state', function() {
+        expectProgressToBeIndeterminate(this.progressInstance);
+      });
     });
   });
-
-  describe('when value transitions from being set to being undefined', function() {
-    beforeEach(function() {
-      this.progressInstance = ReactTestUtils.renderIntoDocument(<progress value="42" max="100" />);
-      this.progressInstance.setProps({value: (void 0)});
-    });
-
-    it('should return to the indeterminate state', function() {
-      expectProgressToBeIndeterminate(this.progressInstance);
-    });
-  });
-});
+}
