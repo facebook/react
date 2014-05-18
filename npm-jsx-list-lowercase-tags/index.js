4444630016d7cb24ef8375c0324a891740e6b0f0
Ben Alpert <spicyjalapeno@gmail.com>
2014-05-17 18:22:53 -0700

    Remove JSX tag whitelist; use case instead
    With this, tags that start with a lowercase letter are assumed to be built-in and have React.DOM prepended; uppercase tags are assumed to be user-defined and are left as-is.

---
+++ npm-jsx-list-lowercase-tags/index.js
@@ -0,0 +1,177 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+
+var types = require('ast-types');
+var esprima = require('esprima-fb');
+
+var namedTypes = types.namedTypes;
+
+var knownTags = {
+  a: true,
+  abbr: true,
+  address: true,
+  applet: true,
+  area: true,
+  article: true,
+  aside: true,
+  audio: true,
+  b: true,
+  base: true,
+  bdi: true,
+  bdo: true,
+  big: true,
+  blockquote: true,
+  body: true,
+  br: true,
+  button: true,
+  canvas: true,
+  caption: true,
+  circle: true,
+  cite: true,
+  code: true,
+  col: true,
+  colgroup: true,
+  command: true,
+  data: true,
+  datalist: true,
+  dd: true,
+  defs: true,
+  del: true,
+  details: true,
+  dfn: true,
+  dialog: true,
+  div: true,
+  dl: true,
+  dt: true,
+  ellipse: true,
+  em: true,
+  embed: true,
+  fieldset: true,
+  figcaption: true,
+  figure: true,
+  footer: true,
+  form: true,
+  g: true,
+  h1: true,
+  h2: true,
+  h3: true,
+  h4: true,
+  h5: true,
+  h6: true,
+  head: true,
+  header: true,
+  hgroup: true,
+  hr: true,
+  html: true,
+  i: true,
+  iframe: true,
+  img: true,
+  input: true,
+  ins: true,
+  kbd: true,
+  keygen: true,
+  label: true,
+  legend: true,
+  li: true,
+  line: true,
+  linearGradient: true,
+  link: true,
+  main: true,
+  map: true,
+  mark: true,
+  marquee: true,
+  mask: false,
+  menu: true,
+  menuitem: true,
+  meta: true,
+  meter: true,
+  nav: true,
+  noscript: true,
+  object: true,
+  ol: true,
+  optgroup: true,
+  option: true,
+  output: true,
+  p: true,
+  param: true,
+  path: true,
+  pattern: false,
+  polygon: true,
+  polyline: true,
+  pre: true,
+  progress: true,
+  q: true,
+  radialGradient: true,
+  rect: true,
+  rp: true,
+  rt: true,
+  ruby: true,
+  s: true,
+  samp: true,
+  script: true,
+  section: true,
+  select: true,
+  small: true,
+  source: true,
+  span: true,
+  stop: true,
+  strong: true,
+  style: true,
+  sub: true,
+  summary: true,
+  sup: true,
+  svg: true,
+  table: true,
+  tbody: true,
+  td: true,
+  text: true,
+  textarea: true,
+  tfoot: true,
+  th: true,
+  thead: true,
+  time: true,
+  title: true,
+  tr: true,
+  track: true,
+  tspan: true,
+  u: true,
+  ul: true,
+  'var': true,
+  video: true,
+  wbr: true
+};
+
+process.argv.slice(2).forEach(function(filename) {
+  fs.readFile(filename, 'utf8', function(err, code) {
+    if (err) {
+      console.log(err.message);
+      process.exit(1);
+    }
+
+    if (code.indexOf('@jsx') === -1) {
+      return;
+    }
+
+    try {
+      var ast = esprima.parse(code, {loc: true});
+    } catch (e) {
+      process.stderr.write(
+        '\x1B[31mError parsing ' + filename + ': ' + e + '\x1B[0m\n'
+      );
+      return;
+    }
+
+    types.traverse(ast, function(node) {
+      if (namedTypes.XJSOpeningElement.check(node) &&
+          /^[a-z]/.test(node.name.name) &&
+          !knownTags.hasOwnProperty(node.name.name)) {
+        console.log(
+          filename + ':' +
+          node.loc.start.line + ':' +
+          code.split('\n')[node.loc.start.line - 1]
+        );
+      }
+    });
+  });
+});
