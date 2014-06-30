08656fec3f7f42501de81730d615c9c8158abef1
Ben Alpert <spicyjalapeno@gmail.com>
2014-06-30 01:08:56 -0700

    Rebuild docs

--- js/jsx-compiler.js
+++ js/jsx-compiler.js
@@ -13,38 +13,15 @@ var HelloMessage = React.createClass({\n\
 React.renderComponent(<HelloMessage name=\"John\" />, mountNode);\
 ";
 
-function transformer(harmony, code) {
-  return JSXTransformer.transform(code, {harmony: harmony}).code;
+var transformer = function(code) {
+  return JSXTransformer.transform(code).code;
 }
-
-var CompilerPlayground = React.createClass({displayName: 'CompilerPlayground',
-  getInitialState: function() {
-    return {harmony: false};
-  },
-  handleHarmonyChange: function(e) {
-    this.setState({harmony: e.target.checked});
-  },
-  render: function() {
-    return (
-      React.DOM.div(null, 
-        ReactPlayground(
-          {codeText:HELLO_COMPONENT,
-          renderCode:true,
-          transformer:transformer.bind(null, this.state.harmony),
-          showCompiledJSTab:false}
-        ),
-        React.DOM.label( {className:"compiler-option"}, 
-          React.DOM.input(
-            {type:"checkbox",
-            onChange:this.handleHarmonyChange,
-            checked:this.state.harmony} ),' ',
-          "Enable ES6 transforms (",React.DOM.code(null, "--harmony"),")"
-        )
-      )
-    );
-  },
-});
 React.renderComponent(
-  CompilerPlayground(null ),
+  ReactPlayground(
+    {codeText:HELLO_COMPONENT,
+    renderCode:true,
+    transformer:transformer,
+    showCompiledJSTab:false}
+  ),
   document.getElementById('jsxCompiler')
 );
