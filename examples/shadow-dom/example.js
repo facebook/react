3ead6bc55ef82085c8c79a94bdffab3483a090a6
petehunt <floydophone@gmail.com>
2014-07-18 20:18:30 -0700

    add WebCOmponent

--- examples/shadow-dom/example.js
+++ examples/shadow-dom/example.js
@@ -34,27 +34,35 @@ var ShadowRoot = React.createClass({
   }
 });
 
-var PaperButton = React.createClass({
+var WebComponent = React.createClass({
   render: function() {
-    return (
-      <ShadowRoot component={createCustomElementClass('paper-button')}>
-        <style>{'h3 { color: blue; }'}</style>
-        <h3>Hello world</h3>
+    return this.transferPropsTo(
+      <ShadowRoot component={createCustomElementClass(this.props.tagName)}>
+        {this.props.children}
       </ShadowRoot>
     );
   }
 });
 
-var Hello = React.createClass({
-  getInitialState: function() {
-    return {n: 0};
-  },
-  handleClick: function() {
-    this.setState({n: this.state.n + 1});
-  },
+var PaperButton = React.createClass({
   render: function() {
-    return <div><PaperButton /><h3>yolo</h3></div>;
+    return (
+      <WebComponent tagName="paper-button">
+        <style>{css}</style>
+        <h3>Hello world</h3>
+      </WebComponent>
+    );
+  }
+});
+
+var Hello = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <PaperButton />
+      </div>
+    );
   }
 });
  
-React.renderComponent(<Hello name="World" />, document.body);
+React.renderComponent(<Hello />, document.body);
