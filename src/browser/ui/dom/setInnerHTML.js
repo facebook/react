9f33b6b86eef7f9a2ed31fc5036e64404ce33089
唐鳳 <audreyt@audreyt.org>
2014-09-08 10:53:59 +0800

    Typo

--- src/browser/ui/dom/setInnerHTML.js
+++ src/browser/ui/dom/setInnerHTML.js
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0d
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
