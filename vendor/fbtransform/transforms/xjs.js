bf0a857a9296046552c771ed983fd7e4761f894f
Andres Suarez <zertosh@gmail.com>
2014-09-21 13:12:19 -0400

    Remove esprima-fb and use Syntax from jstransform

--- vendor/fbtransform/transforms/xjs.js
+++ vendor/fbtransform/transforms/xjs.js
@@ -8,7 +8,7 @@
  */
 /*global exports:true*/
 "use strict";
-var Syntax = require('esprima-fb').Syntax;
+var Syntax = require('jstransform').Syntax;
 var utils = require('jstransform/src/utils');
 
 var knownTags = {
