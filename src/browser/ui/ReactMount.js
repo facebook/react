fdffec73c36a73443182b206595b239924d294cb
Árni Hermann Reynisson <arnihr@gmail.com>
2014-12-03 14:49:55 +0000

    Use String.charAt for character by index lookup
    Using object property accessor for string character lookup breaks in
    non-modern browsers.

--- src/browser/ui/ReactMount.js
+++ src/browser/ui/ReactMount.js
@@ -63,7 +63,7 @@ var findComponentRootReusableArray = [];
 function firstDifferenceIndex(string1, string2) {
   var minLen = Math.min(string1.length, string2.length);
   for (var i = 0; i < minLen; i++) {
-    if (string1[i] !== string2[i]) {
+    if (string1.charAt(i) !== string2.charAt(i)) {
       return i;
     }
   }
