bbe2e886abbfc5f9c94cf859418edd33b7446d45
Paul O’Shannessy <paul@oshannessy.com>
2014-12-04 19:02:34 -0800

    Merge pull request #2658 from ianobermiller/margin-attrs
    Support marginHeight and marginWidth attributes

--- src/browser/ui/dom/HTMLDOMPropertyConfig.js
+++ src/browser/ui/dom/HTMLDOMPropertyConfig.js
@@ -107,6 +107,8 @@ var HTMLDOMPropertyConfig = {
     list: MUST_USE_ATTRIBUTE,
     loop: MUST_USE_PROPERTY | HAS_BOOLEAN_VALUE,
     manifest: MUST_USE_ATTRIBUTE,
+    marginHeight: null,
+    marginWidth: null,
     max: null,
     maxLength: MUST_USE_ATTRIBUTE,
     media: MUST_USE_ATTRIBUTE,
