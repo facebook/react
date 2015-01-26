5f150822e658b03660c4930bcf959814e47fcea6
Sebastian Markbage <sema@fb.com>
2015-01-26 02:19:35 -0800

    Add CoffeeScript Class Test
    As part of the new class effort it is now possible to define React
    Components using any type of generic JavaScript class syntax.
    
    This includes CoffeeScript. This test ensures that we don't regress that
    support, and also serves as an example for using React in CoffeeScript.
    
    This test fail trails the ReactES6Class-test file. Some manual tweaking is
    required when converting tests.

--- jest/preprocessor.js
+++ jest/preprocessor.js
@@ -2,8 +2,13 @@
 
 var ReactTools = require('../main.js');
 
+var coffee = require('coffee-script');
+
 module.exports = {
-  process: function(src) {
+  process: function(src, path) {
+    if (path.match(/\.coffee$/)) {
+      return coffee.compile(src, {'bare': true});
+    }
     return ReactTools.transform(src, {harmony: true});
   }
 };
