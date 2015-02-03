7b92d717441b1dac39159e446345df32e487cde9
Brian Holt <btholt@gmail.com>
2015-02-02 21:58:00 -0800

    Changed itemId to itemID and added clarity to the comments of where it is specified.

--- src/browser/ui/dom/HTMLDOMPropertyConfig.js
+++ src/browser/ui/dom/HTMLDOMPropertyConfig.js
@@ -169,7 +169,10 @@ var HTMLDOMPropertyConfig = {
     itemProp: MUST_USE_ATTRIBUTE,
     itemScope: MUST_USE_ATTRIBUTE | HAS_BOOLEAN_VALUE,
     itemType: MUST_USE_ATTRIBUTE,
-    itemId: MUST_USE_ATTRIBUTE,
+    // itemID and itemRef are for Microdata support as well but
+    // only specified in the the WHATWG spec document. See
+    // https://html.spec.whatwg.org/multipage/microdata.html#microdata-dom-api
+    itemID: MUST_USE_ATTRIBUTE,
     itemRef: MUST_USE_ATTRIBUTE,
     // property is supported for OpenGraph in meta tags.
     property: null
