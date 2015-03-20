63aa7259b9f48886af545afcc06c29acf225b05f
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2015-03-20 20:50:11 +0200

    move getReactRootElementInContainer to ReactMount.js
    getReactRootElementInContainer.js file contains one function used only
    in ReactMount.js.

--- src/browser/ui/ReactMount.js
+++ src/browser/ui/ReactMount.js
@@ -27,7 +27,6 @@ var ReactUpdates = require('ReactUpdates');
 
 var emptyObject = require('emptyObject');
 var containsNode = require('containsNode');
-var getReactRootElementInContainer = require('getReactRootElementInContainer');
 var instantiateReactComponent = require('instantiateReactComponent');
 var invariant = require('invariant');
 var setInnerHTML = require('setInnerHTML');
@@ -71,6 +70,23 @@ function firstDifferenceIndex(string1, string2) {
     }
   }
   return string1.length === string2.length ? -1 : minLen;
+}
+
+/**
+ * @param {DOMElement|DOMDocument} container DOM element that may contain
+ * a React component
+ * @return {?*} DOM element that may have the reactRoot ID, or null.
+ */
+function getReactRootElementInContainer(container) {
+  if (!container) {
+    return null;
+  }
+
+  if (container.nodeType === DOC_NODE_TYPE) {
+    return container.documentElement;
+  } else {
+    return container.firstChild;
+  }
 }
 
 /**
