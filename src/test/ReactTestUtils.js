f2bf71f33fc693b670d5c08574c9a623621dffb5
axross <kou@axross.org>
2015-04-02 18:09:31 +0900

    Add callback into addons/TestUtils.renderIntoDocument()

--- src/test/ReactTestUtils.js
+++ src/test/ReactTestUtils.js
@@ -42,14 +42,14 @@ function Event(suffix) {}
  * @lends ReactTestUtils
  */
 var ReactTestUtils = {
-  renderIntoDocument: function(instance) {
+  renderIntoDocument: function(instance, callback) {
     var div = document.createElement('div');
     // None of our tests actually require attaching the container to the
     // DOM, and doing so creates a mess that we rely on test isolation to
     // clean up, so we're going to stop honoring the name of this method
     // (and probably rename it eventually) if no problems arise.
     // document.documentElement.appendChild(div);
-    return React.render(instance, div);
+    return React.render(instance, div, callback);
   },
 
   isElement: function(element) {
