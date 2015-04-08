245f0d0afac73ddad60716dd741122d298a81e7e
cpojer <christoph.pojer@gmail.com>
2015-04-08 10:24:52 -0700

    Add copyright headers to npm-react-codemod.

--- npm-react-codemod/transforms/utils/ReactUtils.js
+++ npm-react-codemod/transforms/utils/ReactUtils.js
@@ -1,4 +1,14 @@
-/*eslint-disable no-comma-dangle*/
+/**
+ * Copyright 2013-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+ /*eslint-disable no-comma-dangle*/
 
 'use strict';
 
