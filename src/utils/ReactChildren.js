5a431a12ace4a705d50ba087caaf78a6b01ce546
Michael Ridgway <mridgway@yahoo-inc.com>
2015-04-13 14:17:16 -0700

    Replace hasOwnProperty in child processing with typeof undefined check

--- src/utils/ReactChildren.js
+++ src/utils/ReactChildren.js
@@ -81,7 +81,7 @@ function mapSingleChildIntoContext(traverseContext, child, name, i) {
   var mapBookKeeping = traverseContext;
   var mapResult = mapBookKeeping.mapResult;
 
-  var keyUnique = !mapResult.hasOwnProperty(name);
+  var keyUnique = ('undefined' === typeof mapResult[name]);
   if (__DEV__) {
     warning(
       keyUnique,
