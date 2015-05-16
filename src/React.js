0b063f8a09dae4b6b1de400ff2d9aba9108647de
Sebastian Markbage <sema@fb.com>
2015-05-15 18:35:22 -0700

    Reorganize Src Files for Isomorphic React Package
    The new folder structure is organized around major packages that are expected to ship separately in some form.
    
    `/isomorphic`
    
    I moved classic/modern and children utils into a directory called "isomorphic" with the main export being ReactIsomorphic. This will eventually become the "react" package.
    
    This includes all the dependencies that you might need to create a component without dependencies on the renderer/reconciler.
    
    The rest moves into decoupled renderers.
    
    `/renderers/dom/client` - This is the main renderer for DOM.
    
    `/renderers/dom/server` - This is the server-side renderer for HTML strings.
    
    `/addons` and `/test` - Same as before for now.
    
    You're not supposed to take on a dependency inside another package.
    
    Shared code is organized into a "shared" directory which is intended to support all the packages in that subdirectory. Meaning that once we swap to CommonJS modules, the only time you should use `..` is to target `../shared/` or `../../shared`.
    
    E.g. `/shared/` is common utils that are used by everything.
    
    `/renderers/shared/` is code that is shared by all renderers, such as the main reconciliation algorithm.
    
    Shared code will likely be copied into each package rather than referenced. This allow us to have separate state and allow inlining and deadcode elimination.

---
+++ src/React.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2013-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule React
+ */
+
+'use strict';
+
+var ReactDOMClient = require('ReactDOMClient');
+var ReactDOMServer = require('ReactDOMServer');
+var ReactIsomorphic = require('ReactIsomorphic');
+
+var assign = require('Object.assign');
+
+var React = {};
+
+assign(React, ReactIsomorphic);
+assign(React, ReactDOMClient);
+assign(React, ReactDOMServer);
+
+React.version = '0.14.0-alpha1';
+
+module.exports = React;
