b687a229530dc304fbb74018027156968258c295
Paul O’Shannessy <paul@oshannessy.com>
2015-05-20 13:37:42 -0700

    0.14.0-alpha2

--- src/React.js
+++ src/React.js
@@ -23,6 +23,6 @@ assign(React, ReactIsomorphic);
 assign(React, ReactDOMClient);
 assign(React, ReactDOMServer);
 
-React.version = '0.14.0-alpha1';
+React.version = '0.14.0-alpha2';
 
 module.exports = React;
