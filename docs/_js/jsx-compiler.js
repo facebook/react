7c9ed152542fc6f94c8d98149dfb90e6bad54731
Vipul A M <vipulnsward@gmail.com>
2015-06-15 17:26:17 +0530

    #4129 - Added import of babel browser plugin to transform JSX code - Removed occurrences of JSXTransformer from all places - Fixed jsx-compiler and live_editor to start using babel transform - Added rake task to start copying latest babel plugin for compilation

--- docs/_js/jsx-compiler.js
+++ docs/_js/jsx-compiler.js
@@ -8,34 +8,21 @@ var HelloMessage = React.createClass({\n\
 React.render(<HelloMessage name=\"John\" />, mountNode);\
 ";
 
-function transformer(harmony, code) {
-  return JSXTransformer.transform(code, {harmony: harmony}).code;
+function transformer(code) {
+  return babel.transform(code).code;
 }
 
 var CompilerPlayground = React.createClass({
-  getInitialState: function() {
-    return {harmony: false};
-  },
-  handleHarmonyChange: function(e) {
-    this.setState({harmony: e.target.checked});
-  },
   render: function() {
     return (
       <div>
         <ReactPlayground
           codeText={HELLO_COMPONENT}
           renderCode={true}
-          transformer={transformer.bind(null, this.state.harmony)}
+          transformer={transformer.bind(null)}
           showCompiledJSTab={false}
           showLineNumbers={true}
         />
-        <label className="compiler-option">
-          <input
-            type="checkbox"
-            onChange={this.handleHarmonyChange}
-            checked={this.state.harmony} />{' '}
-          Enable ES6 transforms (<code>--harmony</code>)
-        </label>
       </div>
     );
   }
