0eaa0a6ff7eed83bda99619bbacf6ed21d834afa
Ben Alpert <balpert@fb.com>
2015-07-02 10:36:33 +0200

    Fix jest tests

--- src/shared/utils/__tests__/traverseAllChildren-test.js
+++ src/shared/utils/__tests__/traverseAllChildren-test.js
@@ -16,6 +16,7 @@ describe('traverseAllChildren', function() {
   var React;
   var ReactFragment;
   beforeEach(function() {
+    require('mock-modules').dumpCache();
     traverseAllChildren = require('traverseAllChildren');
     React = require('React');
     ReactFragment = require('ReactFragment');
