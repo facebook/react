fa4063455b9d362229d3745b70f189be682bf4c5
Murray M. Moss <mmoss@users.noreply.github.com>
2015-07-07 15:22:49 -0400

    Concatenate warning message, due to parsing issues
    The addons module warning is currently causing issues with babel/JSPM due to the warning message getting parsed as a require statement. Adding a break using string concatenation appears to prevent any issues.
    
    Example error using JSPM:
    
    ```
    Error loading "react-addons-{addon}" from...
    ```

--- packages/react/addons.js
+++ packages/react/addons.js
@@ -1,8 +1,8 @@
 var warning = require('./lib/warning');
 warning(
   false,
-  "require('react/addons') is deprecated. " +
-  "Access using require('react-addons-{addon}') instead."
+  'require' + "('react/addons') is deprecated. " +
+  'Access using require' + "('react-addons-{addon}') instead."
 );
 
 module.exports = require('./lib/ReactWithAddons');
