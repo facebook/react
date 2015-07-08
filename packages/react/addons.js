334372758c27df3a6ef98a4decccef86550fa55f
Murray M. Moss <MMoss@cainc.com>
2015-07-08 13:26:36 -0400

    Add comment to explain string concatenation

--- packages/react/addons.js
+++ packages/react/addons.js
@@ -1,6 +1,9 @@
 var warning = require('./lib/warning');
 warning(
   false,
+  // Require examples in this string must be split to prevent React's
+  // build tools from mistaking them for real requires.
+  // Otherwise the build tools will attempt to build a 'react-addons-{addon}' module.
   'require' + "('react/addons') is deprecated. " +
   'Access using require' + "('react-addons-{addon}') instead."
 );
