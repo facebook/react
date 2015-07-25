f6aa1679795cb78179853d8aec77020a8b45f62f
Niole Nelson <niolenelson@gmail.com>
2015-07-25 02:19:12 -0400

    fixed error message

--- src/renderers/dom/shared/ReactDOMComponent.js
+++ src/renderers/dom/shared/ReactDOMComponent.js
@@ -263,7 +263,7 @@ function assertValidProps(component, props) {
   }
   invariant(
     props.style == null || typeof props.style === 'object',
-    'The `style` prop of '+component._currentElement._owner._instance.__proto__.constructor.displayName + ' expects a mapping from style properties to values, ' +
+    'Please check the render method of '+ component._currentElement._owner._instance.__proto__.constructor.displayName + '. The `style` prop expects a mapping from style properties to values, ' +
     'not a string. For example, style={{marginRight: spacing + \'em\'}} when ' +
     'using JSX.'
   );
