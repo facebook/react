cc02c22cb4f0d8d057846945b9255fca3909845f
Domenico Matteo <matteo.domenico@gmail.com>
2015-07-27 00:32:59 +0200

    ensure tags are upperCase before comparing. #4388

--- src/test/ReactTestUtils.js
+++ src/test/ReactTestUtils.js
@@ -213,7 +213,7 @@ var ReactTestUtils = {
   scryRenderedDOMComponentsWithTag: function(root, tagName) {
     return ReactTestUtils.findAllInRenderedTree(root, function(inst) {
       return ReactTestUtils.isDOMComponent(inst) &&
-            inst.tagName === tagName.toUpperCase();
+            inst.tagName.toUpperCase() === tagName.toUpperCase();
     });
   },
 
