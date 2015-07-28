fca37e180e2190f202860347ccab53fd12b9c176
George Fogle <fogle.george@gmail.com>
2015-07-27 23:58:15 -0400

    seed default per test file

---
+++ src/test/__tests__/ReactDefaultPerf-test.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2013-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @emails react-core
+ */
+
+'use strict';
+
+var React;
+var ReactDefaultPerf;
+
+var mocks;
+
+describe('ReactDefaultPerf', function() {
+
+  beforeEach(function () {
+    mocks = require('mocks');
+
+    React = require('React');
+    ReactDefaultPerf = require('ReactDefaultPerf');
+  });
+});
