acfdec410d7de5727db0ff646b89866c281dc6ae
Niole Nelson <niolenelson@gmail.com>
2015-07-27 23:21:16 -0400

    use getDeclarationErrorAddendum() to identify the react class with incorrect inline styling

--- src/renderers/dom/shared/ReactDOMComponent.js
+++ src/renderers/dom/shared/ReactDOMComponent.js
@@ -263,8 +263,7 @@ function assertValidProps(component, props) {
   }
   invariant(
     props.style == null || typeof props.style === 'object',
-    'Please check the render method of ' +
-     component._currentElement._owner._instance.__proto__.constructor.displayName +
+      getDeclarationErrorAddendum(component) +
     '. The `style` prop expects a mapping from style properties to values, ' +
     'not a string. For example, style={{marginRight: spacing + \'em\'}} when ' +
     'using JSX.'
