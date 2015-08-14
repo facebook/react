ebc1a24f6085cb926bd4403754fd6a5b59da2e5b
Paul O’Shannessy <paul@oshannessy.com>
2015-08-14 15:18:18 -0700

    Initial checkpoint.
    - side nav picks up the right translation
      - does not link to the right place
    - ditto for next/prev links

--- docs/_plugins/sidebar_item.rb
+++ docs/_plugins/sidebar_item.rb
@@ -1,6 +1,26 @@
 module Jekyll
+  class SidebarItemBlock < Liquid::Block
+    def initialize(tag_name, item, tokens)
+      super
+      @item = item
+    end
+
+    def render(context)
+      pageID = context["page"]["id"]
+      item = context[@item]
+      itemID = item["id"]
+      # TODO: link to correct locale
+      href = item["href"] || "/react/docs/#{itemID}.html"
+      className = pageID == itemID ? ' class="active"' : ''
+      
+      return "<a data-id=\"#{pageID}\" data-other=\"#{itemID}\" href=\"#{href}\"#{className}>#{super}</a>"
+    end
+  end
+
+  # DECPRECATED
   module SidebarItemFilter
     def sidebar_item_link(item)
+      # item, label = split_params(params)
       pageID = @context.registers[:page]["id"]
       itemID = item["id"]
       href = item["href"] || "/react/docs/#{itemID}.html"
@@ -12,3 +32,4 @@ module Jekyll
 end
 
 Liquid::Template.register_filter(Jekyll::SidebarItemFilter)
+Liquid::Template.register_tag('sidebaritem', Jekyll::SidebarItemBlock)
