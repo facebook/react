241533782c7a2f5459a0a0e384b238122a67753f
Glen Mailer <glenjamin@gmail.com>
2015-09-20 15:16:51 +0100

    Expose component instance in shallow rendering
    Fixes #4056

--- src/test/ReactTestUtils.js
+++ src/test/ReactTestUtils.js
@@ -356,6 +356,10 @@ ReactShallowRenderer.prototype.getRenderOutput = function() {
   );
 };
 
+ReactShallowRenderer.prototype.getMountedInstance = function() {
+  return this._instance ? this._instance._instance : null;
+};
+
 var NoopInternalComponent = function(element) {
   this._renderedOutput = element;
   this._currentElement = element;
