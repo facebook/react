f558b955843b0b1057709ddc4f844df45c00a5e9
Ben Alpert <balpert@fb.com>
2015-10-26 14:52:23 -0700

    Fix cycle in AutoFocusUtils
    In 5c5d2ec182e4f517b7f3b865d0277ed0dc4f0abd I accidentally introduced a dependency cycle:
    
    AutoFocusUtils -> findDOMNode -> ReactDOMComponent -> AutoFocusUtils
    
    This breaks some tools internally. We're not actually using findDOMNode in AutoFocusUtils any more so we can just delete it.

--- src/renderers/dom/client/wrappers/AutoFocusUtils.js
+++ src/renderers/dom/client/wrappers/AutoFocusUtils.js
@@ -14,20 +14,9 @@
 
 var ReactMount = require('ReactMount');
 
-var findDOMNode = require('findDOMNode');
 var focusNode = require('focusNode');
 
-var Mixin = {
-  componentDidMount: function() {
-    if (this.props.autoFocus) {
-      focusNode(findDOMNode(this));
-    }
-  },
-};
-
 var AutoFocusUtils = {
-  Mixin: Mixin,
-
   focusDOMComponent: function() {
     focusNode(ReactMount.getNode(this._rootNodeID));
   },
