73b496db95fed805ac64c7a395d8e098272436ff
jj <jj.weber@gmail.com>
2015-11-04 14:30:33 -0800

    Clear transition timeouts when component unmounts. Fixes #4876

--- src/addons/transitions/ReactCSSTransitionGroupChild.js
+++ src/addons/transitions/ReactCSSTransitionGroupChild.js
@@ -100,6 +100,7 @@ var ReactCSSTransitionGroupChild = React.createClass({
     if (userSpecifiedDelay) {
       // Clean-up the animation after the specified delay
       timeout = setTimeout(endListener, userSpecifiedDelay);
+      this.transitionTimeouts.push(timeout);
     } else {
       // DEPRECATED: this listener will be removed in a future version of react
       ReactTransitionEvents.addEndEventListener(node, endListener);
@@ -126,12 +127,16 @@ var ReactCSSTransitionGroupChild = React.createClass({
 
   componentWillMount: function() {
     this.classNameQueue = [];
+    this.transitionTimeouts = [];
   },
 
   componentWillUnmount: function() {
     if (this.timeout) {
       clearTimeout(this.timeout);
     }
+    this.transitionTimeouts.forEach(function(timeout) {
+      clearTimeout(timeout);
+    });
   },
 
   componentWillAppear: function(done) {
