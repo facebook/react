2fdaba49c72d6c95046b05d0ef80de1e1bef69cc
David Ed Mellum <david@edmellum.com>
2015-12-18 17:10:58 +0100

    Fix #4963 - SVG <use> swallows click events

--- src/renderers/dom/client/utils/getEventTarget.js
+++ src/renderers/dom/client/utils/getEventTarget.js
@@ -20,6 +20,12 @@
  */
 function getEventTarget(nativeEvent) {
   var target = nativeEvent.target || nativeEvent.srcElement || window;
+
+  // Normalize SVG <use> element events #4963
+  if (target.correspondingUseElement) {
+    target = target.correspondingUseElement;
+  }
+
   // Safari may fire events on text nodes (Node.TEXT_NODE is 3).
   // @see http://www.quirksmode.org/js/events_properties.html
   return target.nodeType === 3 ? target.parentNode : target;
