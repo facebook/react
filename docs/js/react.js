fb6b3b05f41d02734c055681130503fd60c5c6d7
Ben Alpert <balpert@fb.com>
2015-12-29 14:39:05 -0800

    Website for 0.14.5

--- docs/js/react.js
+++ docs/js/react.js
@@ -1,5 +1,5 @@
  /**
-  * React v0.14.4
+  * React v0.14.5
   */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.React = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 /**
@@ -13840,7 +13840,7 @@ module.exports = ReactUpdates;
 
 'use strict';
 
-module.exports = '0.14.4';
+module.exports = '0.14.5';
 },{}],85:[function(_dereq_,module,exports){
 /**
  * Copyright 2013-2015, Facebook, Inc.
