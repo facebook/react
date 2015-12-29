bef45b0b1a98ea9b472ba664d955a039cf2f8068
Rick Beerendonk <rick@beerendonk.com>
2015-12-29 20:20:32 +0100

    Year-agnostic copyright message, like React Native uses, to prevent the need for yearly changes.

--- src/renderers/dom/shared/ReactDOMComponentFlags.js
+++ src/renderers/dom/shared/ReactDOMComponentFlags.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015, Facebook, Inc.
+ * Copyright 2015-present, Facebook, Inc.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
