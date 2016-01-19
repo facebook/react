b797075c63044e51dd3a0045e4c4929d1c1e81ad
Ben Alpert <ben@benalpert.com>
2016-01-19 15:12:09 -0800

    Merge pull request #5886 from goatslacker/cant-dangerouslysetinnerhtml-for-option
    Only add children in ReactDOMOption when there are children

--- src/renderers/dom/client/wrappers/ReactDOMOption.js
+++ src/renderers/dom/client/wrappers/ReactDOMOption.js
@@ -84,7 +84,10 @@ var ReactDOMOption = {
       }
     });
 
-    nativeProps.children = content;
+    if (content) {
+      nativeProps.children = content;
+    }
+
     return nativeProps;
   },
 
