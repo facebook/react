c569b329e9334b59baf64e0c706af56521099383
Scott Feeney <scott@oceanbase.org>
2016-01-28 11:46:27 -0800

    Update website for 0.14.7
    (cherry picked from commit e48343bd038bff190d2566a4f9c6f70babbfa02b)

--- docs/js/react-dom.js
+++ docs/js/react-dom.js
@@ -1,5 +1,5 @@
 /**
- * ReactDOM v0.14.6
+ * ReactDOM v0.14.7
  *
  * Copyright 2013-2015, Facebook, Inc.
  * All rights reserved.
@@ -21,7 +21,7 @@
 
   // <script>
   } else {
-    var g
+    var g;
     if (typeof window !== "undefined") {
       g = window;
     } else if (typeof global !== "undefined") {
