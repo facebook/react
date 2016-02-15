ff7d9f1b9275612e101cbd62b90f833a51adf232
Dustan Kasten <dustan.kasten@gmail.com>
2016-02-15 07:26:20 -0500

    Add react-current-owner to module-map

--- packages/react-current-owner/index.js
+++ packages/react-current-owner/index.js
@@ -5,6 +5,8 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule ReactCurrentOwner
  */
 
 'use strict';
