383e2f938d21fe3d402a931abd3057641d3e7961
cpojer <christoph.pojer@gmail.com>
2016-02-17 13:01:14 +0900

    Update to jest-cli 0.9.

--- src/test/__tests__/ReactDefaultPerf-test.js
+++ src/test/__tests__/ReactDefaultPerf-test.js
@@ -24,7 +24,7 @@ describe('ReactDefaultPerf', function() {
 
   beforeEach(function() {
     var now = 0;
-    jest.setMock('performanceNow', function() {
+    jest.setMock('fbjs/lib/performanceNow', function() {
       return now++;
     });
 
