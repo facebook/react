a1aa2f987e2add87c6516dab8213c7720d0cb1c6
Travis CI <travis@reactjs.org>
2016-03-23 15:59:42 -0700

    actually build docs without failing

--- scripts/circleci/build_gh_pages.sh
+++ scripts/circleci/build_gh_pages.sh
@@ -10,25 +10,27 @@ if [ -z $CI_PULL_REQUEST ] && [ "$CIRCLE_BRANCH" = "$REACT_WEBSITE_BRANCH" ]; th
   git config --global user.email "travis@reactjs.org"
 
   # TODO: check if directory exists (restored from cache)
-  # if [ -d $GH_PAGES_DIR ]
-  #   pushd $GH_PAGES_DIR
-  #   git pull origin gh-pages
-  #   popd
-  # else
-  #   clone
-  # fi
-  git clone --branch gh-pages --depth=1 \
-    https://zpao@github.com/zpao/react.git \
-    $GH_PAGES_DIR
+  if [ -d $GH_PAGES_DIR ]; then
+    pushd $GH_PAGES_DIR
+    git pull origin gh-pages
+    popd
+  else
+    git clone --branch gh-pages --depth=1 \
+      https://zpao@github.com/zpao/react.git \
+      $GH_PAGES_DIR
+  fi
+
   pushd docs
   bundle exec rake release
   cd $GH_PAGES_DIR
   git status
-  git diff
+  git --no-pager diff
   if ! git diff-index --quiet HEAD --; then
     git add -A .
     git commit -m "Rebuild website"
     git push origin gh-pages
   fi
   popd
+else
+  echo "Not building website"
 fi
