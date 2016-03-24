d17091ddebf317173eb30e3ab4a8e28e124c8f50
Paul O’Shannessy <paul@oshannessy.com>
2016-03-24 11:00:38 -0700

    Make Browserify-specific modules to remove DOM dependencies from npm package

--- src/addons/ReactWithAddons.js
+++ src/addons/ReactWithAddons.js
@@ -9,13 +9,6 @@
  * @providesModule ReactWithAddons
  */
 
-/**
- * This module exists purely in the open source project, and is meant as a way
- * to create a separate standalone build of React. This build has "addons", or
- * functionality we've built and think might be useful but doesn't have a good
- * place to live inside React core.
- */
-
 'use strict';
 
 var LinkedStateMixin = require('LinkedStateMixin');
