b61367e7d322c61a8a8a6006aed3da35160d4686
Nate Hunzaker <nate.hunzaker@gmail.com>
2016-03-26 22:34:40 -0400

    Use Node.contains in lowestCommonAncestor
    This commit takes advantage of prior work to utilize a change to
    isAncestor that makes it fast enough to use within
    lowestCommonAncestor. The result is much faster, and with less code.

--- src/renderers/dom/client/ReactDOMTreeTraversal.js
+++ src/renderers/dom/client/ReactDOMTreeTraversal.js
@@ -21,36 +21,14 @@ function getLowestCommonAncestor(instA, instB) {
   invariant('_nativeNode' in instA, 'getNodeFromInstance: Invalid argument.');
   invariant('_nativeNode' in instB, 'getNodeFromInstance: Invalid argument.');
 
-  var depthA = 0;
-  for (var tempA = instA; tempA; tempA = tempA._nativeParent) {
-    depthA++;
-  }
-  var depthB = 0;
-  for (var tempB = instB; tempB; tempB = tempB._nativeParent) {
-    depthB++;
-  }
-
-  // If A is deeper, crawl up.
-  while (depthA - depthB > 0) {
-    instA = instA._nativeParent;
-    depthA--;
-  }
-
-  // If B is deeper, crawl up.
-  while (depthB - depthA > 0) {
-    instB = instB._nativeParent;
-    depthB--;
-  }
-
-  // Walk in lockstep until we find a match.
-  var depth = depthA;
-  while (depth--) {
-    if (instA === instB) {
-      return instA;
+  while (instA) {
+    if (isAncestor(instA, instB)) {
+      return instA
     }
-    instA = instA._nativeParent;
-    instB = instB._nativeParent;
+
+    instA = instA._nativeParent
   }
+
   return null;
 }
 
