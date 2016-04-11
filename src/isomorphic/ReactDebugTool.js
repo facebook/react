4dae72fea58a21dffe49438008083769e61a33df
omerts <omerts3@gmail.com>
2016-04-11 12:23:49 +0300

    add component instance onSetState event

--- src/isomorphic/ReactDebugTool.js
+++ src/isomorphic/ReactDebugTool.js
@@ -55,8 +55,8 @@ var ReactDebugTool = {
   onEndProcessingChildContext() {
     emitEvent('onEndProcessingChildContext');
   },
-  onSetState() {
-    emitEvent('onSetState');
+  onSetState(componentInstance = null) {
+    emitEvent('onSetState', componentInstance);
   },
   onMountRootComponent(internalInstance) {
     emitEvent('onMountRootComponent', internalInstance);
