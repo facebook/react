cf157886e9f8f05c645c6c13cf0be2c41a8fafee
Sebastian Markbåge <sebastian@calyptus.eu>
2016-05-10 18:24:57 -0700

    React Fiber Reconciler (#6690)
    This is an outline for the new reconciler infrastructure.
    
    I created a noop renderer to have something to get started from.
    
    I split the reconciler folder into old and new, as well as shared.
    I put shouldUpdateReactComponent in shared as an example of a
    utility that can easily be shared between both. I plan on breaking
    out more utilities like these.

---
+++ src/renderers/shared/fiber/ReactFiberReconciler.js
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule ReactFiberReconciler
+ * @flow
+ */
+
+'use strict';
+
+import type { Fiber } from 'ReactFiber';
+var ReactFiberFunctionalComponent = require('ReactFiberFunctionalComponent');
+
+var ReactTypesOfWork = require('ReactTypesOfWork');
+var {
+  FunctionalComponent,
+  ClassComponent,
+  NativeComponent,
+} = ReactTypesOfWork;
+
+type ReactHostElement<T, P> = {
+  type: T,
+  props: P
+};
+
+type Deadline = {
+  timeRemaining : () => number
+};
+
+var timeHeuristicForUnitOfWork = 1;
+
+export type HostConfig<T, P, I> = {
+
+  createHostInstance(element : ReactHostElement<T, P>) : I,
+  scheduleHighPriCallback(callback : () => void) : void,
+  scheduleLowPriCallback(callback : (deadline : Deadline) => void) : void
+
+};
+
+type OpaqueID = {};
+
+export type Reconciler = {
+  mountNewRoot(element : ReactElement) : OpaqueID;
+};
+
+module.exports = function<T, P, I>(config : HostConfig<T, P, I>) : Reconciler {
+
+  // const scheduleHighPriCallback = config.scheduleHighPriCallback;
+  const scheduleLowPriCallback = config.scheduleLowPriCallback;
+
+  let nextUnitOfWork : ?Fiber = null;
+
+  function performUnitOfWork(unit : Fiber) : ?Fiber {
+    switch (unit.tag) {
+      case FunctionalComponent:
+        return ReactFiberFunctionalComponent.performWork(unit);
+      case ClassComponent:
+        break;
+      case NativeComponent:
+        break;
+    }
+    return null;
+  }
+
+  function performLowPriWork(deadline : Deadline) {
+    while (nextUnitOfWork) {
+      if (deadline.timeRemaining() > timeHeuristicForUnitOfWork) {
+        nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+      } else {
+        scheduleLowPriCallback(performLowPriWork);
+        break;
+      }
+    }
+  }
+
+  function ensureLowPriIsScheduled() {
+    if (nextUnitOfWork) {
+      return;
+    }
+    scheduleLowPriCallback(performLowPriWork);
+  }
+
+  /*
+  function performHighPriWork() {
+    // There is no such thing as high pri work yet.
+  }
+
+  function ensureHighPriIsScheduled() {
+    scheduleHighPriCallback(performHighPriWork);
+  }
+  */
+
+  return {
+
+    mountNewRoot(element : ReactElement) : OpaqueID {
+
+      ensureLowPriIsScheduled();
+
+      nextUnitOfWork = ReactFiberFunctionalComponent.createFiber(element);
+
+      return {};
+    },
+
+  };
+};
---
+++ src/renderers/shared/fiber/ReactTypesOfWork.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule ReactTypesOfWork
+ * @flow
+ */
+
+'use strict';
+
+var TypesOfWork = {
+  FunctionalComponent: 1,
+  ClassComponent: 2,
+  NativeComponent: 3,
+};
+
+module.exports = TypesOfWork;
