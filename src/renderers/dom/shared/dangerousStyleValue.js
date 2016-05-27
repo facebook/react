e8a8d005f0bfdafa3317d537ec15435ce1dd856f
Paul O’Shannessy <paul@oshannessy.com>
2016-05-26 20:05:32 -0700

    Merge pull request #6677 from zpao/dont-warn-css-0-string
    Don't warn when style value is '0'
    (cherry picked from commit 5c6f9d31bdec63021f6ae7b651dc5af71281a487)

--- src/renderers/dom/shared/dangerousStyleValue.js
+++ src/renderers/dom/shared/dangerousStyleValue.js
@@ -51,7 +51,9 @@ function dangerousStyleValue(name, value, component) {
 
   if (typeof value === 'string') {
     if (__DEV__) {
-      if (component) {
+      // Allow '0' to pass through without warning. 0 is already special and
+      // doesn't require units, so we don't need to warn about it.
+      if (component && value !== '0') {
         var owner = component._currentElement._owner;
         var ownerName = owner ? owner.getName() : null;
         if (ownerName && !styleWarnings[ownerName]) {
