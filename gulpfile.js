6aacfdb9ba2ff26ff53de5059f5f7cdac85087a5
Keyan Zhang <root@keyanzhang.com>
2016-06-01 11:21:26 -0700

    Add a gulp script for extracting error codes (#6882) (cherry picked from commit bfd1531eca38e4e25a9517a00d5be1283bda6696)

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var flatten = require('gulp-flatten');
 var del = require('del');
 
 var babelPluginModules = require('fbjs-scripts/babel-6/rewrite-modules');
+var extractErrors = require('./scripts/error-codes/gulp-extract-errors');
 
 var paths = {
   react: {
@@ -51,9 +52,13 @@ whiteListNames.forEach(function(name) {
 
 moduleMap['object-assign'] = 'object-assign';
 
+var errorCodeOpts = {
+  errorMapFilePath: 'scripts/error-codes/codes.json',
+};
+
 var babelOpts = {
   plugins: [
-    [babelPluginModules, { map: moduleMap }],
+    [babelPluginModules, {map: moduleMap}],
   ],
 };
 
@@ -67,6 +72,12 @@ gulp.task('react:modules', function() {
     .pipe(babel(babelOpts))
     .pipe(flatten())
     .pipe(gulp.dest(paths.react.lib));
+});
+
+gulp.task('react:extract-errors', function() {
+  return gulp
+    .src(paths.react.src)
+    .pipe(extractErrors(errorCodeOpts));
 });
 
 gulp.task('default', ['react:modules']);
