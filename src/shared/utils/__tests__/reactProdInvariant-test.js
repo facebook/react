6bfaa6d24fb09c975895cce9ebf85a43c20494e0
Keyan Zhang <keyanz@fb.com>
2016-06-02 14:29:04 -0700

    fixed failing reactProdInvariant stack-related tests

--- src/shared/utils/__tests__/reactProdInvariant-test.js
+++ src/shared/utils/__tests__/reactProdInvariant-test.js
@@ -23,25 +23,19 @@ describe('reactProdInvariant', function() {
     expect(function() {
       reactProdInvariant(124, 'foo', 'bar');
     }).toThrowError(
-      'React: production error #124. Visit ' +
-      'http://facebook.github.io/react/docs/error-codes.html?invariant=124&args=%22foo%22&args=%22bar%22 ' +
-      'for more details.'
+      /React: production error #124\. Visit http:\/\/facebook\.github\.io\/react\/docs\/error-codes\.html\?invariant=124&args=%22foo%22&args=%22bar%22&stack=%22Error.*reactProdInvariant.*%22 for more details\./
     );
 
     expect(function() {
       reactProdInvariant(20);
     }).toThrowError(
-      'React: production error #20. Visit ' +
-      'http://facebook.github.io/react/docs/error-codes.html?invariant=20 ' +
-      'for more details.'
+      /React: production error #20\. Visit http:\/\/facebook\.github\.io\/react\/docs\/error-codes\.html\?invariant=20&stack=%22Error.*reactProdInvariant.*%22 for more details\./
     );
 
     expect(function() {
       reactProdInvariant(77, 'foo', 'bar', 'and', 'what', 'else');
     }).toThrowError(
-      'React: production error #77. Visit ' +
-      'http://facebook.github.io/react/docs/error-codes.html?invariant=77&args=%22foo%22&args=%22bar%22&args=%22and%22&args=%22what%22&args=%22else%22 ' +
-      'for more details.'
+      /React: production error #77\. Visit http:\/\/facebook\.github\.io\/react\/docs\/error-codes\.html\?invariant=77&args=%22foo%22&args=%22bar%22&args=%22and%22&args=%22what%22&args=%22else%22&stack=%22Error.*reactProdInvariant.*%22 for more details\./
     );
   });
 });
