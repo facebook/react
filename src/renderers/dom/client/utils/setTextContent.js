40f6d3eacad563c67bea0aa1c2220de6239ece43
Dominic Gannaway <trueadm@users.noreply.github.com>
2016-06-10 17:57:31 +0100

    Performance: setTextContent should attempt to set TextNode nodeValue where possible (#7005)

--- src/renderers/dom/client/utils/setTextContent.js
+++ src/renderers/dom/client/utils/setTextContent.js
@@ -26,6 +26,14 @@ var setInnerHTML = require('setInnerHTML');
  * @internal
  */
 var setTextContent = function(node, text) {
+  if (text) {
+    var firstChild = node.firstChild;
+    
+    if (firstChild && firstChild === node.lastChild && firstChild.nodeType === 3) {
+      firstChild.nodeValue = text;
+      return;
+    }
+  }
   node.textContent = text;
 };
 
