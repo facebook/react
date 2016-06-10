c938ee5ff3b4824b7cd52161d969552de3472f3f
Dominic Gannaway <dominic.gannaway@caplin.com>
2016-06-10 14:40:40 +0100

    Validates that the nodeType of the firstChild is a TextNode before setting nodeValue

--- src/renderers/dom/client/utils/setTextContent.js
+++ src/renderers/dom/client/utils/setTextContent.js
@@ -29,7 +29,7 @@ var setTextContent = function(node, text) {
   if (text) {
     var firstChild = node.firstChild;
     
-    if (firstChild && firstChild === node.lastChild) {
+    if (firstChild && firstChild === node.lastChild && firstChild.nodeType === 3) {
       firstChild.nodeValue = text;
       return;
     }
