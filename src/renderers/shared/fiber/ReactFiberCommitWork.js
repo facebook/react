a4b8bebe1805c59271cd4dd7e8151a32ba2dff33
Sebastian Markbage <sema@fb.com>
2016-06-29 17:24:18 -0700

    Dependency injection in the begin/complete/commit phases
    This just makes them instantiable so that we can get access to the
    host config in these.

--- src/renderers/shared/fiber/ReactFiberCommitWork.js
+++ src/renderers/shared/fiber/ReactFiberCommitWork.js
@@ -13,6 +13,7 @@
 'use strict';
 
 import type { Fiber } from 'ReactFiber';
+import type { HostConfig } from 'ReactFiberReconciler';
 
 var ReactTypeOfWork = require('ReactTypeOfWork');
 var {
@@ -21,18 +22,26 @@ var {
   HostComponent,
 } = ReactTypeOfWork;
 
-exports.commitWork = function(finishedWork : Fiber) : void {
-  switch (finishedWork.tag) {
-    case ClassComponent:
-      // TODO: Fire componentDidMount/componentDidUpdate, update refs
-      return;
-    case HostContainer:
-      // TODO: Attach children to root container.
-      return;
-    case HostComponent:
-      console.log('commit updates to host component', finishedWork.type);
-      return;
-    default:
-      throw new Error('This unit of work tag should not have side-effects.');
+module.exports = function<T, P, I>(config : HostConfig<T, P, I>) {
+
+  function commitWork(finishedWork : Fiber) : void {
+    switch (finishedWork.tag) {
+      case ClassComponent:
+        // TODO: Fire componentDidMount/componentDidUpdate, update refs
+        return;
+      case HostContainer:
+        // TODO: Attach children to root container.
+        return;
+      case HostComponent:
+        console.log('commit updates to host component', finishedWork.type);
+        return;
+      default:
+        throw new Error('This unit of work tag should not have side-effects.');
+    }
   }
+
+  return {
+    commitWork,
+  };
+
 };
