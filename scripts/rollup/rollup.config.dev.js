10b1079e3e11559d20cada459e8ef694fb6e8e65
Keyan Zhang <keyanz@fb.com>
2016-07-02 22:47:01 -0700

    added rollup config

---
+++ scripts/rollup/rollup.config.dev.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+'use strict';
+
+var babel = require('rollup-plugin-babel');
+var replace = require('rollup-plugin-replace');
+var resolve = require('rollup-plugin-node-resolve');
+
+var babelEs6ModulifyPlugin = require('../babel/transform-es6-modulify');
+
+module.exports = {
+  dest: 'build/react.rollup.dev.js',
+  entry: 'build/modules/ReactUMDEntry.js',
+  moduleName: 'React',
+  format: 'umd',
+  plugins: [
+    babel({
+      babelrc: false,
+      plugins: [babelEs6ModulifyPlugin],
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    resolve({
+      main: true,
+    }),
+  ],
+  sourceMap: false,
+};
