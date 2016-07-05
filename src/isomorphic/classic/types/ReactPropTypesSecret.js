2c93a41580030ae44105626cdd19c97704128771
Brandon Dail <Aweary@users.noreply.github.com>
2016-07-05 18:22:01 -0500

    Use hardcoded value for PropType secret (#7194)
    Rename secret!

--- src/isomorphic/classic/types/ReactPropTypesSecret.js
+++ src/isomorphic/classic/types/ReactPropTypesSecret.js
@@ -13,6 +13,6 @@
 'use strict';
 
 
-const ReactPropTypesSecret = '__REACT_PROP_TYPES_SECRET__' + Math.random().toString();
+const ReactPropTypesSecret = 'SECRET_DO_NOT_PASS_THIS_OR_YOU_WILL_BE_FIRED';
 
 module.exports = ReactPropTypesSecret;
