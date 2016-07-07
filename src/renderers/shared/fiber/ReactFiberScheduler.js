5c094fb5d1467e7b1fde823363fa4d1b9d8bdd11
Sebastian Markbage <sema@fb.com>
2016-07-06 21:08:43 -0700

    Don't reset next work pointer for lower priority work
    If the current work is higher priority than the new work, then
    don't bother resetting the unit of work pointer since it won't
    affect the execution order.

--- src/renderers/shared/fiber/ReactFiberScheduler.js
+++ src/renderers/shared/fiber/ReactFiberScheduler.js
@@ -15,6 +15,7 @@
 import type { Fiber } from 'ReactFiber';
 import type { FiberRoot } from 'ReactFiberRoot';
 import type { HostConfig } from 'ReactFiberReconciler';
+import type { PriorityLevel } from 'ReactPriorityLevel';
 
 var ReactFiberBeginWork = require('ReactFiberBeginWork');
 var ReactFiberCompleteWork = require('ReactFiberCompleteWork');
@@ -43,6 +44,7 @@ module.exports = function<T, P, I, C>(config : HostConfig<T, P, I, C>) {
 
   // The next work in progress fiber that we're currently working on.
   let nextUnitOfWork : ?Fiber = null;
+  let nextPriorityLevel : PriorityLevel = NoWork;
 
   // Linked list of roots with scheduled work on them.
   let nextScheduledRoot : ?FiberRoot = null;
@@ -55,6 +57,7 @@ module.exports = function<T, P, I, C>(config : HostConfig<T, P, I, C>) {
       if (nextScheduledRoot === lastScheduledRoot) {
         nextScheduledRoot = null;
         lastScheduledRoot = null;
+        nextPriorityLevel = NoWork;
         return null;
       }
       nextScheduledRoot = nextScheduledRoot.nextScheduledRoot;
@@ -68,19 +71,23 @@ module.exports = function<T, P, I, C>(config : HostConfig<T, P, I, C>) {
       // too many just means flushing changes too often.
       let work = findNextUnitOfWorkAtPriority(root.current, HighPriority);
       if (work) {
+        nextPriorityLevel = HighPriority;
         return work;
       }
       work = findNextUnitOfWorkAtPriority(root.current, LowPriority);
       if (work) {
+        nextPriorityLevel = LowPriority;
         return work;
       }
       work = findNextUnitOfWorkAtPriority(root.current, OffscreenPriority);
       if (work) {
+        nextPriorityLevel = OffscreenPriority;
         return work;
       }
       // We didn't find anything to do in this root, so let's try the next one.
       root = root.nextScheduledRoot;
     }
+    nextPriorityLevel = NoWork;
     return null;
   }
 
@@ -208,11 +215,13 @@ module.exports = function<T, P, I, C>(config : HostConfig<T, P, I, C>) {
     }
   }
 
-  function scheduleLowPriWork(root : FiberRoot) {
+  function scheduleLowPriWork(root : FiberRoot, priority : PriorityLevel) {
     // We must reset the current unit of work pointer so that we restart the
     // search from the root during the next tick, in case there is now higher
     // priority work somewhere earlier than before.
-    nextUnitOfWork = null;
+    if (priority <= nextPriorityLevel) {
+      nextUnitOfWork = null;
+    }
 
     if (root.isScheduled) {
       // If we're already scheduled, we can bail out.
