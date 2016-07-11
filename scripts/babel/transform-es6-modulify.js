f35bc47a5d61507233857797089e07abd60228b9
Keyan Zhang <keyanz@fb.com>
2016-07-11 01:00:49 -0700

    updated babel plugin (better error message)

--- scripts/babel/transform-es6-modulify.js
+++ scripts/babel/transform-es6-modulify.js
@@ -8,25 +8,63 @@
  */
 'use strict';
 
+var basename = require('path').basename;
+
+function getFileLoc(path, file) {
+  if (file.file.opts.filename === 'unknown') {
+    return '';
+  }
+  var startLinePos = path.node.loc.start.line;
+  var fileName = file.file.opts.filename;
+  var moduleName = basename(fileName);
+  return (
+    'Check line ' + startLinePos + ' at `' + fileName +
+    '` and the original `' + moduleName + '` module under `/src`.'
+  );
+}
+
 module.exports = function(babel) {
-  const t = babel.types;
+  var t = babel.types;
 
   return {
     visitor: {
       CallExpression: function(path, file) {
-        // TODO: support RHS member expression and LHS destructuring
-        if (
-          !t.isIdentifier(path.node.callee, {name: 'require'}) ||
-          !t.isVariableDeclarator(path.parent) || // not an assignment
-          path.scope.path.type !== 'Program' // is top-level
-        ) {
+        if (!t.isIdentifier(path.node.callee, {name: 'require'})) {
           return;
+        }
+
+        var sourceLocText = getFileLoc(path, file);
+        if (t.isMemberExpression(path.parent)) {
+          // `var createElement = require('react').createElement;`
+          throw new Error(
+            'Invalid require: `require()` must be in a form of `var ... = require(...);`. ' + sourceLocText
+          );
+        } else if (!t.isVariableDeclarator(path.parent)) {
+          // is not directly in a VariableDeclarator
+          throw new Error(
+            'Invalid require: `require()` must be directly in a variable declarator. ' + sourceLocText
+          );
+        } else if (!t.isProgram(path.scope.block)) {
+          // is not on the top-level
+          throw new Error(
+            'Invalid require: `require()` must be on the top-level. ' + sourceLocText
+          );
+        } else if (!t.isStringLiteral(path.node.arguments[0])) {
+          // the argument is not a StringLiteral
+          throw new Error(
+            'Invalid require: `require()` must take a literal string as argument. ' + sourceLocText
+          );
+        } else if (!t.isIdentifier(path.parent.id)) {
+          // LHS is not an Identifier; presumably an ObjectPattern (destructuring)
+          throw new Error(
+            'Invalid require: left hand side of `require()` must be an identifier. ' + sourceLocText
+          );
         }
 
         // instead of calling `replaceWith`, this handles cases like
         // `var m0 = require('m0'), m1 = require('m1');`
         // we don't have this pattern in the code base but
-        // it happens when Babel inserts requires
+        // it happens when Babel inserts `require`s
         path.parentPath.parentPath.insertBefore(
           t.importDeclaration(
             [t.importDefaultSpecifier(path.parent.id)],
@@ -36,22 +74,39 @@ module.exports = function(babel) {
         path.parentPath.remove();
       },
 
-      AssignmentExpression: function(path, file) {
-        if (
-          !path.get('left').matchesPattern('module.exports') ||
-          path.scope.path.type !== 'Program' // is top-level
-        ) {
+      MemberExpression: function(path, file) {
+        if (!path.matchesPattern('module.exports')) {
           return;
         }
 
-        // TODO: use FunctionDeclaration and ClassDeclaration
-        // and don't make everything an Expression
-        let rhs = path.node.right;
+        var sourceLocText = getFileLoc(path, file);
+        if (
+          !t.isAssignmentExpression(path.parent) ||
+          path.node !== path.parent.left
+        ) {
+          // `module.exports` is on the RHS, or the LHS looks like `module.exports.foo`
+          throw new Error(
+            'Invalid exports: `module.exports` must be in a form of `module.exports = ...;`. ' + sourceLocText
+          );
+        } else if (!t.isProgram(path.scope.block)) {
+          // is not on the top-level
+          throw new Error(
+            'Invalid exports: `module.exports = ...` must be on the top-level. ' + sourceLocText
+          );
+        }
 
-        path.parentPath.replaceWith(
-          t.exportDefaultDeclaration(
-            rhs
-          )
+        this.numberOfExports = this.numberOfExports ? this.numberOfExports + 1 : 1;
+        if (this.numberOfExports > 1) {
+          throw new Error(
+            'Invalid exports: `module.exports = ...` can only happen once in a module. ' + sourceLocText
+          );
+        }
+
+        path.parentPath.parentPath.replaceWith(
+          // for now, it's not necessary to convert the RHS to
+          // a FunctionDeclaration or a ClassDeclaration since we only export default
+          // (i.e., only named exports resolve the `id` field for naming)
+          t.exportDefaultDeclaration(path.parent.right)
         );
       },
     },
