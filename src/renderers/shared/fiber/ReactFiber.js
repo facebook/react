a2a189bf65b882c026425a0d6a43488f92a569c8
Sebastian Markbage <sema@fb.com>
2016-08-08 19:22:45 -0700

    Move priority reset to the end and search pending work in work tree
    The priority level gets reset at the wrong time because I rely
    on mutating it at the wrong point. This moves it to the end of
    completed work as a second pass over the children to see what
    the highest priority is. This is inefficient for large sets but
    we can try to find a smarter way to do this later. E.g. passing
    it down the stack.
    
    This bug fix revealed another bug that I had flagged before that
    we're finding work to do in the "current" tree instead of the
    working tree. For trees that were paused, e.g. childInProgress
    trees, this won't work since don't have a current tree to search.
    
    Therefore I fixed findNextUnitOfWorkAtPriority to use
    workInProgress instead of current.

--- src/renderers/shared/fiber/ReactFiber.js
+++ src/renderers/shared/fiber/ReactFiber.js
@@ -168,6 +168,9 @@ exports.cloneFiber = function(fiber : Fiber, priorityLevel : PriorityLevel) : Fi
     alt.pendingProps = fiber.pendingProps;
     alt.pendingWorkPriority = priorityLevel;
 
+    alt.memoizedProps = fiber.memoizedProps;
+    alt.output = fiber.output;
+
     // Whenever we clone, we do so to get a new work in progress.
     // This ensures that we've reset these in the new tree.
     alt.nextEffect = null;
@@ -188,6 +191,9 @@ exports.cloneFiber = function(fiber : Fiber, priorityLevel : PriorityLevel) : Fi
   // pendingProps is here for symmetry but is unnecessary in practice for now.
   alt.pendingProps = fiber.pendingProps;
   alt.pendingWorkPriority = priorityLevel;
+
+  alt.memoizedProps = fiber.memoizedProps;
+  alt.output = fiber.output;
 
   alt.alternate = fiber;
   fiber.alternate = alt;
