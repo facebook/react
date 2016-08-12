cba2b19b84b339e937b4bbc258cd09b2b8303391
Dan Abramov <dan.abramov@gmail.com>
2016-08-12 20:09:27 +0100

    Remove ReactDOMInstrumentation (#7481)
    Its events are not being used anywhere.

--- src/renderers/dom/ReactDOM.js
+++ src/renderers/dom/ReactDOM.js
@@ -135,4 +135,13 @@ if (__DEV__) {
   }
 }
 
+if (__DEV__) {
+  var ReactInstrumentation = require('ReactInstrumentation');
+  var ReactDOMUnknownPropertyHook = require('ReactDOMUnknownPropertyHook');
+  var ReactDOMNullInputValuePropHook = require('ReactDOMNullInputValuePropHook');
+
+  ReactInstrumentation.debugTool.addHook(ReactDOMUnknownPropertyHook);
+  ReactInstrumentation.debugTool.addHook(ReactDOMNullInputValuePropHook);
+}
+
 module.exports = ReactDOM;
