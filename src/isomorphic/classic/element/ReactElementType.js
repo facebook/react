774a94b6c27746d7f806979701b1393bfb69b689
Christopher Chedeau <vjeuxx@gmail.com>
2016-08-25 15:27:03 -0700

    Type ReactComponentTreeHook (#7504)
    For this one, I wanted to type a non-trivial piece of the codebase and ran into the fact that we do not have types for ReactElement nor ReactInstance, so I had to create them.
    
    I'll add comments inline
    (cherry picked from commit ea494a2c1082c705ef79d0b73516aa6923a4c75a)

---
+++ src/isomorphic/classic/element/ReactElementType.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @providesModule ReactElementType
+ */
+
+'use strict';
+
+import type { ReactInstance } from 'ReactInstanceType';
+
+export type Source = {
+  fileName: string,
+  lineNumber: number,
+};
+
+export type ReactElement = {
+  $$typeof: any,
+  type: any,
+  key: any,
+  ref: any,
+  props: any,
+  _owner: ReactInstance,
+
+  // __DEV__
+  _store: {
+    validated: bool,
+  },
+  _self: ReactElement,
+  _shadowChildren: any,
+  _source: Source,
+};
