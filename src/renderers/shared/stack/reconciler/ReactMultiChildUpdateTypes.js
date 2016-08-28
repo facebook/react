c9e03f0a859180a150d14f61f5e57148a013259e
Christopher Chedeau <vjeuxx@gmail.com>
2016-08-28 10:31:30 -0700

    Type ReactMultiChildUpdateTypes (#7589)
    As mentioned in https://www.facebook.com/groups/2003630259862046/permalink/2097088000516271/ I'm going to kill this instance of keyMirror and use a plain string instead with a type union.
    
    The current type union is unused right now but when I type ReactMultiChild I will use it.

--- src/renderers/shared/stack/reconciler/ReactMultiChildUpdateTypes.js
+++ src/renderers/shared/stack/reconciler/ReactMultiChildUpdateTypes.js
@@ -11,22 +11,15 @@
 
 'use strict';
 
-var keyMirror = require('keyMirror');
-
 /**
  * When a component's children are updated, a series of update configuration
  * objects are created in order to batch and serialize the required changes.
  *
  * Enumerates all the possible types of update configurations.
- *
- * @internal
  */
-var ReactMultiChildUpdateTypes = keyMirror({
-  INSERT_MARKUP: null,
-  MOVE_EXISTING: null,
-  REMOVE_NODE: null,
-  SET_MARKUP: null,
-  TEXT_CONTENT: null,
-});
-
-module.exports = ReactMultiChildUpdateTypes;
+export type ReactMultiChildUpdateTypes =
+  'INSERT_MARKUP' |
+  'MOVE_EXISTING' |
+  'REMOVE_NODE' |
+  'SET_MARKUP' |
+  'TEXT_CONTENT';
