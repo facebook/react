839697f60c6117e3a562c91142bda4c12acc4146
Christopher Chedeau <vjeuxx@gmail.com>
2016-09-02 14:23:58 -0700

    Restore coverage in Travis (#7628)
    We disabled coverage in Travis because the implementation was crashing ( https://github.com/facebook/react/issues/6290 ). Since we upgraded to Jest 15, the entire coverage implementation is brand new so we should give it another try.

--- grunt/tasks/jest.js
+++ grunt/tasks/jest.js
@@ -1,84 +1,16 @@
 'use strict';
 
-var async = require('async');
-var fs = require('fs');
-var glob = require('glob');
 var grunt = require('grunt');
 var path = require('path');
 
-var rootPath = path.resolve('.');
-var buildPath = path.join(rootPath, 'build');
-var tempConfigPath = path.join(buildPath, 'jest-config.json');
-
-var config = require(path.join(rootPath, 'package.json')).jest;
-
-var collectCoverageOnlyFrom = {
-  'src/**/*.js': {
-    ignore: [
-      'src/**/__tests__/*.js',
-      'src/shared/vendor/third_party/*.js',
-      'src/test/*.js',
-    ],
-  },
-};
-
-function getCollectCoverageOnlyFrom(callback) {
-  var patterns = Object.keys(collectCoverageOnlyFrom);
-  var result = {};
-
-  async.each(patterns, function(pattern) {
-    var options = Object.assign({ nodir: true }, collectCoverageOnlyFrom[pattern]);
-    glob(pattern, options, function(err, files) {
-      (files || []).reduce(function(object, key) {
-        object[key] = true;
-        return object;
-      }, result);
-
-      callback(err);
-    });
-  }, function(err) {
-    callback(err, result);
-  });
-}
-
-function getJestConfig(callback) {
-  var rootDir = path.resolve(buildPath, path.resolve(config.rootDir));
-  getCollectCoverageOnlyFrom(function(err, data) {
-    callback(err, Object.assign({}, config, {
-      rootDir: rootDir,
-      name: 'react',
-      collectCoverage: true,
-      collectCoverageOnlyFrom: data,
-    }));
-  });
-}
-
-function onError(err) {
-  grunt.log.error('jest failed');
-  grunt.log.error(err);
-}
-
-function writeTempConfig(callback) {
-  getJestConfig(function(err, data) {
-    if (err) {
-      callback(err);
-    } else {
-      grunt.file.mkdir(buildPath);
-      fs.writeFile(tempConfigPath, JSON.stringify(data, null, '  '), 'utf8', callback);
-    }
-  });
-}
-
-function run(done, configPath) {
+function run(done, coverage) {
   grunt.log.writeln('running jest');
 
   var args = [
     path.join('node_modules', 'jest', 'bin', 'jest'),
-    '--runInBand',
-    '--no-watchman',
   ];
-  if (configPath) {
-    args.push('--config', configPath);
+  if (coverage) {
+    args.push('--coverage');
   }
   grunt.util.spawn({
     cmd: 'node',
@@ -91,7 +23,8 @@ function run(done, configPath) {
     },
   }, function(spawnErr, result, code) {
     if (spawnErr) {
-      onError(spawnErr);
+      grunt.log.error('jest failed');
+      grunt.log.error(spawnErr);
     } else {
       grunt.log.ok('jest passed');
     }
@@ -108,15 +41,7 @@ function runJestNormally() {
 
 function runJestWithCoverage() {
   var done = this.async();
-
-  writeTempConfig(function(writeErr) {
-    if (writeErr) {
-      onError(writeErr);
-      return;
-    }
-
-    run(done, tempConfigPath);
-  });
+  run(done, /* coverage */ true);
 }
 
 module.exports = {
