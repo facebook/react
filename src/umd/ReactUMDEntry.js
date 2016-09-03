facebook/react

commit 8ef00dbb7de944da0e7b14438baffd37df01bf37

Bundle DOM renderers into their individual UMD bundles (#7164)
* Cut out isomorphic dependencies from the renderers

These files reaches into isomorphic files.

The ReactElement functions are exposed on the React object anyway
so I can just use those instead.

I also found some files that are not shared that should be in
renderers shared.

* Found a few more shared dependencies

renderSubtreeIntoContainer is only used by the DOM renderer.
It's not an addon.

ReactClass isn't needed as a dependency since injection doesn't
happen anymore.

* Use a shim file to load addons' dependencies on DOM

By replacing this intermediate file we can do the lazy loading
without needing any lazy requires. This set up works with ES
modules.

We could also replace the globalShim thing with aliased files
instead for consistency.

* Bundle DOM renderers into their individual UMD bundles

Instead of exposing the entire DOM renderer on the react.js
package, I only expose CurrentOwner and ComponentTreeDevtool which
are currently the only two modules that share __state__ with the
renderers.

Then I package each renderer in its own package. That could allow
us to drop more server dependencies from the client package. It
will also allow us to ship fiber as a separate renderer.

Unminified DEV            after     before
react.js                  123kb     696kb
react-with-addons.js      227kb     774kb
react-dom.js              668kb     1kb
react-dom-server.js       638kb     1kb

Minified PROD             after     before
react.min.js               24kb     154kb
react-with-addons.min.js   37kb     166kb
react-dom.min.js          149kb     1kb
react-dom-server.min.js   144kb     1kb

The total size for react.min.js + react-dom.min.js is +19kb larger
because of the overlap between them right now. I'd like to see
what an optimizing compiler can do to this. Some of that is fbjs
stuff. There shouldn't need to be that much overlap so that's

src/umd/ReactUMDEntry.js
@@ -11,15 +11,14 @@
 
 'use strict';
 
-var ReactDOM = require('ReactDOM');
-var ReactDOMServer = require('ReactDOMServer');
 var React = require('React');
 
-
-// `version` will be added here by ReactIsomorphic.
+// `version` will be added here by the React module.
 var ReactUMDEntry = Object.assign({
-  __SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: ReactDOM,
-  __SECRET_DOM_SERVER_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: ReactDOMServer,
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: {
+    ReactCurrentOwner: require('ReactCurrentOwner'),
+    ReactComponentTreeHook: require('ReactComponentTreeHook'),
+  },
 }, React);
 
 module.exports = ReactUMDEntry;

src/umd/ReactWithAddonsUMDEntry.js
@@ -11,15 +11,14 @@
 
 'use strict';
 
-var ReactDOM = require('ReactDOM');
-var ReactDOMServer = require('ReactDOMServer');
 var ReactWithAddons = require('ReactWithAddons');
 
-
-// `version` will be added here by ReactIsomorphic.
+// `version` will be added here by the React module.
 var ReactWithAddonsUMDEntry = Object.assign({
-  __SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: ReactDOM,
-  __SECRET_DOM_SERVER_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: ReactDOMServer,
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: {
+    ReactCurrentOwner: require('ReactCurrentOwner'),
+    ReactComponentTreeHook: require('ReactComponentTreeHook'),
+  },
 }, ReactWithAddons);
 
 module.exports = ReactWithAddonsUMDEntry;

Gruntfile.js
@@ -117,12 +117,31 @@ module.exports = function(grunt) {
     'build-modules',
     'browserify:addonsMin',
   ]);
+  grunt.registerTask('build:dom', [
+    'build-modules',
+    'version-check',
+    'browserify:dom',
+  ]);
+  grunt.registerTask('build:dom-min', [
+    'build-modules',
+    'version-check',
+    'browserify:domMin',
+  ]);
+  grunt.registerTask('build:dom-server', [
+    'build-modules',
+    'version-check',
+    'browserify:domServer',
+  ]);
+  grunt.registerTask('build:dom-server-min', [
+    'build-modules',
+    'version-check',
+    'browserify:domServerMin',
+  ]);
   grunt.registerTask('build:npm-react', [
     'version-check',
     'build-modules',
     'npm-react:release',
   ]);
-  grunt.registerTask('build:react-dom', require('./grunt/tasks/react-dom'));
 
   var jestTasks = require('./grunt/tasks/jest');
   grunt.registerTask('jest:normal', jestTasks.normal);
@@ -141,7 +160,10 @@ module.exports = function(grunt) {
     'browserify:addons',
     'browserify:min',
     'browserify:addonsMin',
-    'build:react-dom',
+    'browserify:dom',
+    'browserify:domMin',
+    'browserify:domServer',
+    'browserify:domServerMin',
     'npm-react:release',
     'npm-react:pack',
     'npm-react-dom:release',

grunt/config/browserify.js
@@ -7,10 +7,29 @@ var grunt = require('grunt');
 var UglifyJS = require('uglify-js');
 var uglifyify = require('uglifyify');
 var derequire = require('derequire');
+var aliasify = require('aliasify');
+var globalShim = require('browserify-global-shim');
 var collapser = require('bundle-collapser/plugin');
 
 var envifyDev = envify({NODE_ENV: process.env.NODE_ENV || 'development'});
 var envifyProd = envify({NODE_ENV: process.env.NODE_ENV || 'production'});
+
+var SECRET_INTERNALS_NAME = 'React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED';
+
+var shimSharedModules = globalShim.configure({
+  './ReactCurrentOwner': SECRET_INTERNALS_NAME + '.ReactCurrentOwner',
+  './ReactComponentTreeHook': SECRET_INTERNALS_NAME + '.ReactComponentTreeHook',
+  // All these methods are shared are exposed.
+  './ReactElement': 'React',
+  './ReactPropTypes': 'React.PropTypes',
+  './ReactChildren': 'React.Children',
+});
+
+var shimDOMModules = aliasify.configure({
+  'aliases': {
+    './ReactAddonsDOMDependencies': './build/modules/ReactAddonsDOMDependenciesUMDShim.js',
+  },
+});
 
 var SIMPLE_TEMPLATE =
   grunt.file.read('./grunt/data/header-template-short.txt');
@@ -87,6 +106,7 @@ var addons = {
   debug: false,
   standalone: 'React',
   packageName: 'React (with addons)',
+  transforms: [shimDOMModules],
   globalTransforms: [envifyDev],
   plugins: [collapser],
   after: [derequire, simpleBannerify],
@@ -100,7 +120,72 @@ var addonsMin = {
   debug: false,
   standalone: 'React',
   packageName: 'React (with addons)',
-  transforms: [envifyProd, uglifyify],
+  transforms: [shimDOMModules, envifyProd, uglifyify],
+  globalTransforms: [envifyProd],
+  plugins: [collapser],
+  // No need to derequire because the minifier will mangle
+  // the "require" calls.
+
+  after: [minify, bannerify],
+};
+
+// The DOM Builds
+var dom = {
+  entries: [
+    './build/modules/ReactDOMUMDEntry.js',
+  ],
+  outfile: './build/react-dom.js',
+  debug: false,
+  standalone: 'ReactDOM',
+  // Apply as global transform so that we also envify fbjs and any other deps
+  transforms: [shimSharedModules],
+  globalTransforms: [envifyDev],
+  plugins: [collapser],
+  after: [derequire, simpleBannerify],
+};
+
+var domMin = {
+  entries: [
+    './build/modules/ReactDOMUMDEntry.js',
+  ],
+  outfile: './build/react-dom.min.js',
+  debug: false,
+  standalone: 'ReactDOM',
+  // Envify twice. The first ensures that when we uglifyify, we have the right
+  // conditions to exclude requires. The global transform runs on deps.
+  transforms: [shimSharedModules, envifyProd, uglifyify],
+  globalTransforms: [envifyProd],
+  plugins: [collapser],
+  // No need to derequire because the minifier will mangle
+  // the "require" calls.
+
+  after: [minify, bannerify],
+};
+
+var domServer = {
+  entries: [
+    './build/modules/ReactDOMServerUMDEntry.js',
+  ],
+  outfile: './build/react-dom-server.js',
+  debug: false,
+  standalone: 'ReactDOMServer',
+  // Apply as global transform so that we also envify fbjs and any other deps
+  transforms: [shimSharedModules],
+  globalTransforms: [envifyDev],
+  plugins: [collapser],
+  after: [derequire, simpleBannerify],
+};
+
+var domServerMin = {
+  entries: [
+    './build/modules/ReactDOMServerUMDEntry.js',
+  ],
+  outfile: './build/react-dom-server.min.js',
+  debug: false,
+  standalone: 'ReactDOMServer',
+  // Envify twice. The first ensures that when we uglifyify, we have the right
+  // conditions to exclude requires. The global transform runs on deps.
+  transforms: [shimSharedModules, envifyProd, uglifyify],
   globalTransforms: [envifyProd],
   plugins: [collapser],
   // No need to derequire because the minifier will mangle
@@ -114,4 +199,8 @@ module.exports = {
   min: min,
   addons: addons,
   addonsMin: addonsMin,
+  dom: dom,
+  domMin: domMin,
+  domServer: domServer,
+  domServerMin: domServerMin,
 };

grunt/tasks/react-dom.js
@@ -1,30 +1,0 @@
-'use strict';
-
-var grunt = require('grunt');
-var UglifyJS = require('uglify-js');
-
-var LICENSE_TEMPLATE =
-  grunt.file.read('./grunt/data/header-template-extended.txt');
-
-function build(name, filename) {
-  var srcFile = `vendor/${filename}.js`;
-  var destFile = `build/${filename}.js`;
-  var destFileMin = `build/${filename}.min.js`;
-  var templateData = {
-    package: name,
-    version: grunt.config.data.pkg.version,
-  };
-  var header = grunt.template.process(
-    LICENSE_TEMPLATE,
-    {data: templateData}
-  );
-  var src = grunt.file.read(srcFile);
-  var srcMin = UglifyJS.minify(src, {fromString: true}).code;
-  grunt.file.write(destFile, header + src);
-  grunt.file.write(destFileMin, header + srcMin);
-}
-
-module.exports = function() {
-  build('ReactDOM', 'react-dom');
-  build('ReactDOMServer', 'react-dom-server');
-};

src/addons/ReactAddonsDOMDependencies.js
@@ -1,0 +1,36 @@
+/**
+ * Copyright 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule ReactAddonsDOMDependencies
+ */
+
+'use strict';
+
+var ReactDOM = require('ReactDOM');
+var ReactInstanceMap = require('ReactInstanceMap');
+
+exports.getReactDOM = function() {
+  return ReactDOM;
+};
+
+exports.getReactInstanceMap = function() {
+  return ReactInstanceMap;
+};
+
+if (__DEV__) {
+  var ReactPerf = require('ReactPerf');
+  var ReactTestUtils = require('ReactTestUtils');
+
+  exports.getReactPerf = function() {
+    return ReactPerf;
+  };
+
+  exports.getReactTestUtils = function() {
+    return ReactTestUtils;
+  };
+}

src/addons/ReactWithAddons.js
@@ -13,6 +13,7 @@
 
 var LinkedStateMixin = require('LinkedStateMixin');
 var React = require('React');
+var ReactAddonsDOMDependencies = require('ReactAddonsDOMDependencies');
 var ReactComponentWithPureRenderMixin =
   require('ReactComponentWithPureRenderMixin');
 var ReactCSSTransitionGroup = require('ReactCSSTransitionGroup');
@@ -34,8 +35,20 @@ React.addons = {
 };
 
 if (__DEV__) {
-  React.addons.Perf = require('ReactPerf');
-  React.addons.TestUtils = require('ReactTestUtils');
+  // For the UMD build we get these lazily from the global since they're tied
+  // to the DOM renderer and it hasn't loaded yet.
+  Object.defineProperty(React.addons, 'Perf', {
+    enumerable: true,
+    get: function() {
+      return ReactAddonsDOMDependencies.getReactPerf();
+    },
+  });
+  Object.defineProperty(React.addons, 'TestUtils', {
+    enumerable: true,
+    get: function() {
+      return ReactAddonsDOMDependencies.getReactTestUtils();
+    },
+  });
 }
 
 module.exports = React;

src/addons/transitions/ReactCSSTransitionGroupChild.js
@@ -12,7 +12,7 @@
 'use strict';
 
 var React = require('React');
-var ReactDOM = require('ReactDOM');
+var ReactAddonsDOMDependencies = require('ReactAddonsDOMDependencies');
 
 var CSSCore = require('CSSCore');
 var ReactTransitionEvents = require('ReactTransitionEvents');
@@ -54,7 +54,7 @@ var ReactCSSTransitionGroupChild = React.createClass({
   },
 
   transition: function(animationType, finishCallback, userSpecifiedDelay) {
-    var node = ReactDOM.findDOMNode(this);
+    var node = ReactAddonsDOMDependencies.getReactDOM().findDOMNode(this);
 
     if (!node) {
       if (finishCallback) {

commit 4e1c92e9e3c438b83c573b4b3811930d91361b71

Don't bundle ReactComponentTreeHook in production
Fixes #7492.
This was a build size regression introduced in #7164.

src/umd/ReactUMDEntry.js
@@ -17,8 +17,17 @@ var React = require('React');
 var ReactUMDEntry = Object.assign({
   __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: {
     ReactCurrentOwner: require('ReactCurrentOwner'),
-    ReactComponentTreeHook: require('ReactComponentTreeHook'),
   },
 }, React);
+
+if (__DEV__) {
+  Object.assign(
+    ReactUMDEntry.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
+    {
+      // ReactComponentTreeHook should not be included in production.
+      ReactComponentTreeHook: require('ReactComponentTreeHook'),
+    }
+  );
+}
 
 module.exports = ReactUMDEntry;

src/umd/ReactWithAddonsUMDEntry.js
@@ -17,8 +17,17 @@ var ReactWithAddons = require('ReactWithAddons');
 var ReactWithAddonsUMDEntry = Object.assign({
   __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: {
     ReactCurrentOwner: require('ReactCurrentOwner'),
-    ReactComponentTreeHook: require('ReactComponentTreeHook'),
   },
 }, ReactWithAddons);
+
+if (__DEV__) {
+  Object.assign(
+    ReactWithAddonsUMDEntry.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
+    {
+      // ReactComponentTreeHook should not be included in production.
+      ReactComponentTreeHook: require('ReactComponentTreeHook'),
+    }
+  );
+}
 
 module.exports = ReactWithAddonsUMDEntry;