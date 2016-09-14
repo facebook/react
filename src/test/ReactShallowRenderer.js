1dc53a156069f2d91b9baf1fa298d56f280138fb
Toru Kobayashi <koba0004@gmail.com>
2016-09-15 00:55:07 +0900

    Use ES classes to ReactShallowRenderer

--- src/test/ReactShallowRenderer.js
+++ src/test/ReactShallowRenderer.js
@@ -20,49 +20,30 @@ var ReactUpdates = require('ReactUpdates');
 var emptyObject = require('emptyObject');
 var invariant = require('invariant');
 
-/**
- * @class ReactShallowRenderer
- */
-var ReactShallowRenderer = function() {
-  this._instance = null;
-};
-
-ReactShallowRenderer.prototype.getMountedInstance = function() {
-  return this._instance ? this._instance._instance : null;
-};
-
 var nextDebugID = 1;
 
-var NoopInternalComponent = function(element) {
-  this._renderedOutput = element;
-  this._currentElement = element;
-
-  if (__DEV__) {
-    this._debugID = nextDebugID++;
-  }
-};
-
-NoopInternalComponent.prototype = {
-
-  mountComponent: function() {
-  },
-
-  receiveComponent: function(element) {
+class NoopInternalComponent {
+  constructor(element) {
     this._renderedOutput = element;
     this._currentElement = element;
-  },
 
-  getHostNode: function() {
+    if (__DEV__) {
+      this._debugID = nextDebugID++;
+    }
+  }
+  mountComponent() {}
+  receiveComponent(element) {
+    this._renderedOutput = element;
+    this._currentElement = element;
+  }
+  unmountComponent() {}
+  getHostNode() {
     return undefined;
-  },
-
-  unmountComponent: function() {
-  },
-
-  getPublicInstance: function() {
+  }
+  getPublicInstance() {
     return null;
-  },
-};
+  }
+}
 
 var ShallowComponentWrapper = function(element) {
   // TODO: Consolidate with instantiateReactComponent
@@ -87,69 +68,72 @@ Object.assign(
   }
 );
 
-ReactShallowRenderer.prototype.render = function(element, context) {
-  // Ensure we've done the default injections. This might not be true in the
-  // case of a simple test that only requires React and the TestUtils in
-  // conjunction with an inline-requires transform.
-  ReactDefaultInjection.inject();
-
-  invariant(
-    React.isValidElement(element),
-    'ReactShallowRenderer render(): Invalid component element.%s',
-    typeof element === 'function' ?
-      ' Instead of passing a component class, make sure to instantiate ' +
-      'it by passing it to React.createElement.' :
-      ''
-  );
-  invariant(
-    typeof element.type !== 'string',
-    'ReactShallowRenderer render(): Shallow rendering works only with custom ' +
-    'components, not primitives (%s). Instead of calling `.render(el)` and ' +
-    'inspecting the rendered output, look at `el.props` directly instead.',
-    element.type
-  );
-
-  if (!context) {
-    context = emptyObject;
-  }
-  ReactUpdates.batchedUpdates(_batchedRender, this, element, context);
-
-  return this.getRenderOutput();
-};
-
 function _batchedRender(renderer, element, context) {
   var transaction = ReactUpdates.ReactReconcileTransaction.getPooled(true);
   renderer._render(element, transaction, context);
   ReactUpdates.ReactReconcileTransaction.release(transaction);
 }
 
-ReactShallowRenderer.prototype.getRenderOutput = function() {
-  return (
-    (this._instance && this._instance._renderedComponent &&
-     this._instance._renderedComponent._renderedOutput)
-    || null
-  );
-};
-
-ReactShallowRenderer.prototype.unmount = function() {
-  if (this._instance) {
-    ReactReconciler.unmountComponent(this._instance, false);
+class ReactShallowRenderer {
+  _instance = null;
+  getMountedInstance() {
+    return this._instance ? this._instance._instance : null;
   }
-};
+  render(element, context) {
+    // Ensure we've done the default injections. This might not be true in the
+    // case of a simple test that only requires React and the TestUtils in
+    // conjunction with an inline-requires transform.
+    ReactDefaultInjection.inject();
 
-ReactShallowRenderer.prototype._render = function(element, transaction, context) {
-  if (this._instance) {
-    ReactReconciler.receiveComponent(
-      this._instance,
-      element,
-      transaction,
-      context
+    invariant(
+      React.isValidElement(element),
+      'ReactShallowRenderer render(): Invalid component element.%s',
+      typeof element === 'function' ?
+        ' Instead of passing a component class, make sure to instantiate ' +
+        'it by passing it to React.createElement.' :
+        ''
     );
-  } else {
-    var instance = new ShallowComponentWrapper(element);
-    ReactReconciler.mountComponent(instance, transaction, null, null, context, 0);
-    this._instance = instance;
+    invariant(
+      typeof element.type !== 'string',
+      'ReactShallowRenderer render(): Shallow rendering works only with custom ' +
+      'components, not primitives (%s). Instead of calling `.render(el)` and ' +
+      'inspecting the rendered output, look at `el.props` directly instead.',
+      element.type
+    );
+
+    if (!context) {
+      context = emptyObject;
+    }
+    ReactUpdates.batchedUpdates(_batchedRender, this, element, context);
+
+    return this.getRenderOutput();
   }
-};
+  getRenderOutput() {
+    return (
+      (this._instance && this._instance._renderedComponent &&
+      this._instance._renderedComponent._renderedOutput)
+      || null
+    );
+  }
+  unmount() {
+    if (this._instance) {
+      ReactReconciler.unmountComponent(this._instance, false);
+    }
+  }
+  _render(element, transaction, context) {
+    if (this._instance) {
+      ReactReconciler.receiveComponent(
+        this._instance,
+        element,
+        transaction,
+        context
+      );
+    } else {
+      var instance = new ShallowComponentWrapper(element);
+      ReactReconciler.mountComponent(instance, transaction, null, null, context, 0);
+      this._instance = instance;
+    }
+  }
+}
 
 module.exports = ReactShallowRenderer;
