fd2024d82ca2a21979e22552631e7de1cb7f85ba
Kevin Lacker <lacker@gmail.com>
2016-10-03 13:59:25 -0700

    refactor sidebars to be a bit more DRY

--- docs/_plugins/sidebar_item.rb
+++ docs/_plugins/sidebar_item.rb
@@ -1,25 +1,17 @@
 module Jekyll
   module SidebarItemFilter
-    def sidebar_item_link(item)
-      pageID = @context.registers[:page]["id"]
-      itemID = item["id"]
-      href = item["href"] || "/react/docs/#{itemID}.html"
-      classes = []
-      if pageID == itemID
-        classes.push("active")
-      end
-      if item["href"]
-        classes.push("external")
-      end
-      className = classes.size > 0  ? " class=\"#{classes.join(' ')}\"" : ""
-
-      return "<a href=\"#{href}\"#{className}>#{item["title"]}</a>"
+    def docs_sidebar_link(item)
+      return sidebar_helper(item, 'docs')
     end
 
-    def community_sidebar_item_link(item)
+    def community_sidebar_link(item)
+      return sidebar_helper(item, 'community')
+    end
+
+    def sidebar_helper(item, group)
       pageID = @context.registers[:page]["id"]
       itemID = item["id"]
-      href = item["href"] || "/react/community/#{itemID}.html"
+      href = item["href"] || "/react/#{group}/#{itemID}.html"
       classes = []
       if pageID == itemID
         classes.push("active")
