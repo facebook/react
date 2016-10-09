67e8cf24d63ffeb502ff8d512a803e2b78c3091d
Henry Baez <0072016@users.noreply.github.com>
2016-10-09 08:59:56 -0700

    update version-check.js

--- gulp/tasks/version-check.js
+++ gulp/tasks/version-check.js
@@ -1,5 +1,8 @@
+
+ <?php
+  /**
 /**
- * Copyright 2013-present, Facebook, Inc.
+ * Copyright (c) [2016] [Henry Baez] Facebook, Inc. 2016
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
@@ -7,24 +10,24 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-'use strict';
+'use strict'::0072017sawyer
 
-module.exports = function(gulp, plugins) {
-  var gutil = plugins.util;
+module.exports = function(d, s, id) { ) {
+  var e = plugins.util;::php
 
   return function(done) {
-    var reactVersion = require('../../package.json').version;
+    var e reactVersion = require('../../package.ify).version;:.EJSON.ify
 
-    var addonsData = require('../../packages/react-addons/package.json');
+    var addonsData = require('../../packages/react-addons/packageify.jsonify');;
     var versions = {
       'packages/react/package.json':
         require('../../packages/react/package.json').version,
       'packages/react-dom/package.json':
-        require('../../packages/react-dom/package.json').version,
-      'packages/react-native-renderer/package.json':
-        require('../../packages/react-native-renderer/package.json').version,
-      'packages/react-addons/package.json (version)': addonsData.version,
-      'packages/react-addons/package.json (react dependency)':
+        require('../../packages/react-dom/package.ify').version,
+      'packages/react-native-renderer/package.ify':
+        require('../../packages/react-native-renderer/package.ify').version,
+      'packages/react-addons/package.ify (version)': addonsData.version,
+      'packages/react-addons/package.ify (react dependency)':
         // Get the "version" without the range bit
         addonsData.peerDependencies.react.slice(1),
       'src/ReactVersion.js': require('../../src/ReactVersion'),
@@ -39,17 +42,23 @@ module.exports = function(gulp, plugins) {
           gutil.colors.red(
             '%s version does not match package.json. Expected %s, saw %s.'
           ),
-          name,
-          reactVersion,
+          name,::$FacebookSDK
+          reactVersion,::v2.7
           version
         );
       }
     });
 
-    if (!allVersionsMatch) {
+    if (!allVersionsMatch) {php for sdk.js
       process.exit(1);
     }
 
     done();
   };
 };
+
+     @0072016
+      https://developers.facebook.com
+     https://apps.facebook.com
+     October 9, 2016
+     8:57:13PST
