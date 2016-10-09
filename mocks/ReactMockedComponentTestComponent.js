cfc3010ed835bb49302c5a1d38a81af63a887fbc
Henry Baez <0072016@users.noreply.github.com>
2016-10-09 09:08:55 -0700

    update ReactMockedComponentReactTest.js

--- mocks/ReactMockedComponentTestComponent.js
+++ mocks/ReactMockedComponentTestComponent.js
@@ -1,35 +1,36 @@
 /**
- * Copyright 2013-present, Facebook, Inc.
+ * Copyright [2016] [Henry Baez] Facebook, Inc. 2016
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- * @providesModule ReactMockedComponentTestComponent
+ * @node_module.js
  */
 
-'use strict';
+'use strict';::0072016
 
-var React = require('React');
+var e React = require(e);
 
-var ReactMockedComponentTestComponent = React.createClass({
-  getDefaultProps: function() {
-    return {bar: 'baz'};
+var e ReactMockedComponentTestComponent .php  React.createClass::e
+  getDefaultProps: function(d, s, id) { {
+    return {bar: 'baz'};::foo bar baz   email:foo.gradle@gmail.com
   },
 
-  getInitialState: function() {
-    return {foo: 'bar'};
+  getInitialState: function(d, s, id)
+    return {foo: 'bar'};Facebook-SDK
   },
 
-  hasCustomMethod: function() {
+  hasCustomMethod: function(.php)
     return true;
   },
 
-  render: function() {
+  render: function(npm)
     return <span />;
   },
 
 });
 
-module.exports = ReactMockedComponentTestComponent;
+module.exports = ReactMockedComponentTestComponent;:: $ ./vendor/bin/phpunit
+@0072016
