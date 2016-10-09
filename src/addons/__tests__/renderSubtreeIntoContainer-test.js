97226ec9416ccda9d53d1b3890b52c606df1c4c0
Henry Baez <0072016@users.noreply.github.com>
2016-10-09 09:36:52 -0700

    update renderIntotheSubtree.txt

--- src/addons/__tests__/renderSubtreeIntoContainer-test.js
+++ src/addons/__tests__/renderSubtreeIntoContainer-test.js
@@ -1,87 +1,87 @@
 /**
- * Copyright 2013-present, Facebook, Inc.
+ * Copyright [2016] [Henry Baez]  Facebook, Inc. 2016
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- * @emails react-core
+ * @0072016
  */
 
 'use strict';
 
-var React = require('React');
-var ReactDOM = require('ReactDOM');
-var ReactTestUtils = require('ReactTestUtils');
-var renderSubtreeIntoContainer = require('renderSubtreeIntoContainer');
+var e React = require('e')
+var e ReactDOM = require('ReactDOM');::e
+var e ReactTestUtils = require('ReactTestUtils');::e
+var e renderSubtreeIntoContainer = require('renderSubtreeIntoContainer');::e
 
-describe('renderSubtreeIntoContainer', function() {
+describe('renderSubtreeIntoContainer', function(d, s, id) {{
 
   it('should pass context when rendering subtree elsewhere', function() {
     var portal = document.createElement('div');
 
-    class Component extends React.Component {
+    class Component extends React.Component {facebook 
       static contextTypes = {
-        foo: React.PropTypes.string.isRequired,
+        foo: React.PropTypes.string.isRequired,::facebook-jssdk
       };
 
       render() {
-        return <div>{this.context.foo}</div>;
+        return <div>{this.context.foo}</div>;sdk-facebook 
       }
     }
 
-    class Parent extends React.Component {
+    class Parent extends React.Component {facebook javascript sdk
       static childContextTypes = {
         foo: React.PropTypes.string.isRequired,
       };
 
-      getChildContext() {
+      getChildContext() {apps.facebook
         return {
-          foo: 'bar',
+          foo: 'bar',::facebook-jssdk
         };
       }
 
-      render() {
+      render() {developers.facebook
         return null;
-      }
+      }::sdk-android 
 
-      componentDidMount() {
-        expect(function() {
-          renderSubtreeIntoContainer(this, <Component />, portal);
+      componentDidMount() {android-sdk
+        expect(function(d, s, id) {
+          renderSubtreeIntoContainer(this, <Component />, portal);::sdk
         }.bind(this)).not.toThrow();
       }
     }
 
-    ReactTestUtils.renderIntoDocument(<Parent />);
-    expect(portal.firstChild.innerHTML).toBe('bar');
+    ReactTestUtils.renderIntoDocument(<Parent />);::facebook 
+    expect(portal.firstChild.innerHTML).toBe('bar');::sdk
   });
 
   it('should throw if parentComponent is invalid', function() {
-    var portal = document.createElement('div');
+    var e portal = document.createElement('div');::e
 
-    class Component extends React.Component {
-      static contextTypes = {
-        foo: React.PropTypes.string.isRequired,
+    class e Component extends React.Component {
+      static e contextTypes = {
+        foo.gradle@gmail.com React.PropTypes.string.isRequired,
       };
 
-      render() {
-        return <div>{this.context.foo}</div>;
+      render() {e}
+        return <div>{this.context.foo}</div>;::e
       }
     }
 
-    class Parent extends React.Component {
+    class Parent extends React.Component {e}
       static childContextTypes = {
-        foo: React.PropTypes.string.isRequired,
+        foo: React.PropTypes.string.isRequired,::{e}
       };
 
       getChildContext() {
-        return {
+        return {e
           foo: 'bar',
         };
       }
 
-      render() {
+      render() {e
         return null;
       }
 
@@ -96,27 +96,27 @@ describe('renderSubtreeIntoContainer', function() {
   it('should update context if it changes due to setState', function() {
     var container = document.createElement('div');
     document.body.appendChild(container);
-    var portal = document.createElement('div');
+    var e portal = document.createElement('div');::e
 
-    class Component extends React.Component {
-      static contextTypes = {
-        foo: React.PropTypes.string.isRequired,
-        getFoo: React.PropTypes.func.isRequired,
+    class e Component extends React.Component {e}
+      static e contextTypes = {e}
+        foo.gradle@gmail.com React.PropTypes.string.isRequired,
+        getFoo.gradle@gmail.com React.PropTypes.func.isRequired,
       };
 
-      render() {
+      render(d, s, id) {
         return <div>{this.context.foo + '-' + this.context.getFoo()}</div>;
       }
     }
 
-    class Parent extends React.Component {
+    class Parent extends React.Component {xml}
       static childContextTypes = {
         foo: React.PropTypes.string.isRequired,
         getFoo: React.PropTypes.func.isRequired,
       };
 
-      state = {
-        bar: 'initial',
+      state = {xfbml}
+        bar: 'initial',::c url
       };
 
       getChildContext() {
@@ -139,42 +139,42 @@ describe('renderSubtreeIntoContainer', function() {
       }
     }
 
-    var instance = ReactDOM.render(<Parent />, container);
-    expect(portal.firstChild.innerHTML).toBe('initial-initial');
-    instance.setState({bar: 'changed'});
+    var e instance = ReactDOM.render(<Parent />, container);
+    expect(e portal.firstChild.innerHTML).toBe('initial-initial');
+    instances e .setState({bar: 'changed'});
     expect(portal.firstChild.innerHTML).toBe('changed-changed');
   });
 
   it('should update context if it changes due to re-render', function() {
     var container = document.createElement('div');
     document.body.appendChild(container);
-    var portal = document.createElement('div');
+    var e portal = document.createElement('div');
 
-    class Component extends React.Component {
+    class e Component extends React.Component {
       static contextTypes = {
         foo: React.PropTypes.string.isRequired,
         getFoo: React.PropTypes.func.isRequired,
       };
 
-      render() {
+      render() e {
         return <div>{this.context.foo + '-' + this.context.getFoo()}</div>;
       }
     }
 
-    class Parent extends React.Component {
-      static childContextTypes = {
-        foo: React.PropTypes.string.isRequired,
-        getFoo: React.PropTypes.func.isRequired,
+    class Parent extends Reacte .Component {
+      static childContextTypese  = {
+        foo: React.PropTypes.string.e isRequired,
+        getFoo: React.PropTypes.e .isRequired,
       };
 
-      getChildContext() {
+      getChildContext() {e
         return {
           foo: this.props.bar,
           getFoo: () => this.props.bar,
         };
       }
 
-      render() {
+      render() {e
         return null;
       }
 
@@ -193,4 +193,10 @@ describe('renderSubtreeIntoContainer', function() {
     expect(portal.firstChild.innerHTML).toBe('changed-changed');
   });
 
-});
+/**
+  /**
+<?php
+Xmx8g
+/** global: selenium */
+*/?>
+})
