8f8a833bba6d7eefdb2716c185a62f3c62915cbc
Nathan Hunzaker <nate.hunzaker@gmail.com>
2016-10-12 14:11:15 -0400

    Improve comment about reason for defaultValue conditional assignment

--- src/renderers/dom/client/wrappers/ReactDOMInput.js
+++ src/renderers/dom/client/wrappers/ReactDOMInput.js
@@ -213,8 +213,14 @@ var ReactDOMInput = {
       }
     } else {
       if (props.value == null && props.defaultValue != null) {
-        // Assigning defaultValue causes side-effects on value, which can cause
-        // unexpected value changes and selections skipping. A quick check avoids this
+        // In Chrome, assigning defaultValue to certain input types triggers input validation.
+        // For number inputs, the display value loses trailing decimal points. For email inputs,
+        // Chrome raises "The specified value <x> is not a valid email address".
+        //
+        // Here we check to see if the defaultValue has actually changed, avoiding these problems
+        // when the user is inputting text
+        //
+        // https://github.com/facebook/react/issues/7253
         if (node.defaultValue !== '' + props.defaultValue) {
           node.defaultValue = '' + props.defaultValue;
         }
