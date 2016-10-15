85bffb831b65705eb99f4d18fc4204a3cfd73293
David Blurton <david.blurton@gmail.com>
2016-10-15 22:53:34 +0000

    Update test with new error code

--- src/renderers/dom/__tests__/ReactDOMProduction-test.js
+++ src/renderers/dom/__tests__/ReactDOMProduction-test.js
@@ -179,8 +179,8 @@ describe('ReactDOMProduction', () => {
       var container = document.createElement('div');
       ReactDOM.render(<Component />, container);
     }).toThrowError(
-      'Minified React error #109; visit ' +
-      'http://facebook.github.io/react/docs/error-decoder.html?invariant=109&args[]=Component&args[]=an%20array' +
+      'Minified React error #139; visit ' +
+      'http://facebook.github.io/react/docs/error-decoder.html?invariant=139&args[]=Component&args[]=an%20array' +
       ' for the full message or use the non-minified dev environment' +
       ' for full errors and additional helpful warnings.'
     );
