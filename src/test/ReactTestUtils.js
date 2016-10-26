d49dfe7da424c87b3ebab7ef664cc34ac2b644f5
Toru Kobayashi <koba0004@gmail.com>
2016-10-26 21:44:46 +0900

    Fix an argument name of TestUtils.renderIntoDocument (#8104)

--- src/test/ReactTestUtils.js
+++ src/test/ReactTestUtils.js
@@ -77,14 +77,14 @@ function findAllInRenderedTreeInternal(inst, test) {
  * @lends ReactTestUtils
  */
 var ReactTestUtils = {
-  renderIntoDocument: function(instance) {
+  renderIntoDocument: function(element) {
     var div = document.createElement('div');
     // None of our tests actually require attaching the container to the
     // DOM, and doing so creates a mess that we rely on test isolation to
     // clean up, so we're going to stop honoring the name of this method
     // (and probably rename it eventually) if no problems arise.
     // document.documentElement.appendChild(div);
-    return ReactDOM.render(instance, div);
+    return ReactDOM.render(element, div);
   },
 
   isElement: function(element) {
