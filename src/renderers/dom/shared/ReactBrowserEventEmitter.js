1d2b54c1e13add5cc21e15667584e0360b2b6413
Sebastian Markbage <sema@fb.com>
2016-11-07 11:29:25 -0800

    Track if SelectEventPlugin is attached on a per document basis
    This gets rid of the global flag on if something has listened to onSelect
    and instead reads the isListening map if all the events are covered.
    This is required if we want to attach events locally at roots.
    
    Could be slower perf wise to handle events. An alternative solution would
    be to attach a special flag on the listener map for the document so we
    don't have to check the full dependency list.
    
    However, my favorite solution would be to just eagerly attach all event
    listeners (except maybe wheel). Then we don't have to do any of this stuff
    on a per element basis.

--- src/renderers/dom/shared/ReactBrowserEventEmitter.js
+++ src/renderers/dom/shared/ReactBrowserEventEmitter.js
@@ -328,6 +328,22 @@ var ReactBrowserEventEmitter = Object.assign({}, ReactEventEmitterMixin, {
     }
   },
 
+  isListeningToAllDependencies: function(registrationName, mountAt) {
+    var isListening = getListeningForDocument(mountAt);
+    var dependencies =
+      EventPluginRegistry.registrationNameDependencies[registrationName];
+    for (var i = 0; i < dependencies.length; i++) {
+      var dependency = dependencies[i];
+      if (!(
+            isListening.hasOwnProperty(dependency) &&
+            isListening[dependency]
+          )) {
+        return false;
+      }
+    }
+    return true;
+  },
+
   trapBubbledEvent: function(topLevelType, handlerBaseName, handle) {
     return ReactBrowserEventEmitter.ReactEventListener.trapBubbledEvent(
       topLevelType,
