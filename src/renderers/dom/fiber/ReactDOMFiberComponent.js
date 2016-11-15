7c5ec423502537b4a1d33e29ec3c6be3961344f1
Sebastian Markbage <sema@fb.com>
2016-11-15 18:13:12 +0000

    Remove _domID
    Not used by Fiber

--- src/renderers/dom/fiber/ReactDOMFiberComponent.js
+++ src/renderers/dom/fiber/ReactDOMFiberComponent.js
@@ -71,32 +71,6 @@ function getDeclarationErrorAddendum(internalInstance) {
   return '';
 }
 
-function friendlyStringify(obj) {
-  if (typeof obj === 'object') {
-    if (Array.isArray(obj)) {
-      return '[' + obj.map(friendlyStringify).join(', ') + ']';
-    } else {
-      var pairs = [];
-      for (var key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-          var keyEscaped = /^[a-z$_][\w$_]*$/i.test(key) ?
-            key :
-            JSON.stringify(key);
-          pairs.push(keyEscaped + ': ' + friendlyStringify(obj[key]));
-        }
-      }
-      return '{' + pairs.join(', ') + '}';
-    }
-  } else if (typeof obj === 'string') {
-    return JSON.stringify(obj);
-  } else if (typeof obj === 'function') {
-    return '[function object]';
-  }
-  // Differs from JSON.stringify in that undefined because undefined and that
-  // inf and nan don't become null
-  return String(obj);
-}
-
 /**
  * @param {object} component
  * @param {?object} props
@@ -623,7 +597,6 @@ var ReactDOMFiberComponent = {
     // validateDangerousTag(tag);
     // workInProgress._tag = tag.toLowerCase();
 
-    workInProgress._domID = hostContainerInfo._idCounter++;
     workInProgress._hostParent = hostParent;
     workInProgress._hostContainerInfo = hostContainerInfo;
 
@@ -930,7 +903,6 @@ var ReactDOMFiberComponent = {
 
     workInProgress.unmountChildren(safely, skipLifecycle);
     ReactDOMComponentTree.uncacheNode(workInProgress);
-    workInProgress._domID = 0;
     workInProgress._wrapperState = null;
   },
 
