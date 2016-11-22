866b15f0717c1a80d319cbf6635222611547ad18
Edvin Erikson <edvin@rocketblast.com>
2016-11-22 22:08:33 +0100

    create React class component if we use Fiber

--- src/renderers/native/createReactNativeComponentClass.js
+++ src/renderers/native/createReactNativeComponentClass.js
@@ -12,7 +12,10 @@
 
 'use strict';
 
+var ReactNativeFeatureFlags = require('ReactNativeFeatureFlags');
 var ReactNativeBaseComponent = require('ReactNativeBaseComponent');
+var NativeMethodsMixin = require('NativeMethodsMixin');
+var React = require('React');
 
 // See also ReactNativeBaseComponent
 type ReactNativeBaseComponentViewConfig = {
@@ -28,19 +31,35 @@ type ReactNativeBaseComponentViewConfig = {
 var createReactNativeComponentClass = function(
   viewConfig: ReactNativeBaseComponentViewConfig
 ): ReactClass<any> {
-  var Constructor = function(element) {
-    this._currentElement = element;
-    this._topLevelWrapper = null;
-    this._hostParent = null;
-    this._hostContainerInfo = null;
-    this._rootNodeID = 0;
-    this._renderedChildren = null;
-  };
-  Constructor.displayName = viewConfig.uiViewClassName;
-  Constructor.viewConfig = viewConfig;
-  Constructor.propTypes = viewConfig.propTypes;
-  Constructor.prototype = new ReactNativeBaseComponent(viewConfig);
-  Constructor.prototype.constructor = Constructor;
+  var Constructor;
+  if (ReactNativeFeatureFlags.useFiber) {
+    Constructor = React.createClass({
+      displayName: viewConfig.uiViewClassName,
+      propTypes: viewConfig.propTypes,
+      viewConfig: viewConfig,
+      statics: {
+        viewConfig: viewConfig,
+      },
+      mixins: [NativeMethodsMixin],
+      render() {
+        return React.createElement(viewConfig.uiViewClassName, this.props);
+      }
+    });
+  } else {
+    Constructor = function(element) {
+      this._currentElement = element;
+      this._topLevelWrapper = null;
+      this._hostParent = null;
+      this._hostContainerInfo = null;
+      this._rootNodeID = 0;
+      this._renderedChildren = null;
+    };
+    Constructor.prototype = new ReactNativeBaseComponent(viewConfig);
+    Constructor.prototype.constructor = Constructor;
+    Constructor.displayName = viewConfig.uiViewClassName;
+    Constructor.viewConfig = viewConfig;
+    Constructor.propTypes = viewConfig.propTypes;
+  }
 
   return ((Constructor: any): ReactClass<any>);
 };
