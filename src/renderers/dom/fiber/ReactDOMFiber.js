deaf3e50c0667957b97d9b8b03b4142ffb794932
Brandon Dail <brandon.dail@formidable.com>
2017-01-04 15:52:35 -0600

    Throw when attempting to unmount at an invalid node

--- src/renderers/dom/fiber/ReactDOMFiber.js
+++ src/renderers/dom/fiber/ReactDOMFiber.js
@@ -352,6 +352,10 @@ var ReactDOM = {
   },
 
   unmountComponentAtNode(container : DOMContainerElement) {
+    invariant(
+      isValidContainer(container),
+      'unmountComponentAtNode(...): Target container is not a DOM element.'
+    );t
     warnAboutUnstableUse();
     if (container._reactRootContainer) {
       // Unmount should not be batched.
