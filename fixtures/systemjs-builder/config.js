cebff96b0707805715019142d119943915bf3c55
Dan Abramov <dan.abramov@gmail.com>
2017-01-05 16:45:49 +0000

    Add manual build fixtures

---
+++ fixtures/systemjs-builder/config.js
@@ -0,0 +1,6 @@
+System.config({
+  paths: {
+    react: '../../build/react-with-addons.js',
+    'react-dom': '../../build/react-dom.js',
+  },
+});
---
+++ fixtures/webpack-alias/input.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var CSSTransitionGroup = React.addons.CSSTransitionGroup;
+ReactDOM.render(
+  React.createElement(CSSTransitionGroup, {
+    transitionName: 'example',
+    transitionAppear: true,
+    transitionAppearTimeout: 500,
+    transitionEnterTimeout: 0,
+    transitionLeaveTimeout: 0,
+  }, React.createElement('h1', null,
+    'Hello World!'
+  )),
+  document.getElementById('container')
+);
