2f8a3e5100460581509bbb57a8e3eeea7534b4cd
Brandon Dail <brandon.dail@formidable.com>
2017-01-09 15:59:47 -0600

    Use absolute paths for resolve.root in webpack packaging fixtures
    Webpack requires absolute paths here

--- fixtures/packaging/webpack-alias/config.js
+++ fixtures/packaging/webpack-alias/config.js
@@ -1,10 +1,12 @@
+var path = require('path');
+
 module.exports = {
   entry: './input',
   output: {
     filename: 'output.js',
   },
   resolve: {
-    root: '../../../build/packages',
+    root: path.resolve('../../../build/packages'),
     alias: {
       'react': 'react/dist/react-with-addons',
       'react-dom': 'react-dom/dist/react-dom',
