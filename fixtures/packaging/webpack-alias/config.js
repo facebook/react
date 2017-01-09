429da399e94438226a3c93b2865879fb2d03a62b
Brandon Dail <brandon.dail@formidable.com>
2017-01-09 15:55:25 -0600

    Move build fixtures to fixtures/packaging, updated paths

---
+++ fixtures/packaging/webpack-alias/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './input',
+  output: {
+    filename: 'output.js',
+  },
+  resolve: {
+    root: '../../../build/packages',
+    alias: {
+      'react': 'react/dist/react-with-addons',
+      'react-dom': 'react-dom/dist/react-dom',
+    },
+  },
+};
---
+++ fixtures/packaging/webpack/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: './input',
+  output: {
+    filename: 'output.js',
+  },
+  resolve: {
+    root: '../../../build/packages/',
+  },
+};
