c61148ca58983724d883689456b2e891428c2b81
Piper Chester <piperchester@gmail.com>
2017-01-13 05:19:23 -0800

    Fix typo (#8770)

--- src/isomorphic/classic/types/ReactPropTypes.js
+++ src/isomorphic/classic/types/ReactPropTypes.js
@@ -141,7 +141,7 @@ function is(x, y) {
 
 /**
  * We use an Error-like object for backward compatibility as people may call
- * PropTypes directly and inspect their output. However we don't use real
+ * PropTypes directly and inspect their output. However, we don't use real
  * Errors anymore. We don't inspect their stack anyway, and creating them
  * is prohibitively expensive if they are created too often, such as what
  * happens in oneOfType() for any type before the one that matched.
