865c452c95f278896f685bd2f3bc1408eb930b2d
Andrew Clark <acdlite@fb.com>
2017-01-20 16:05:44 -0800

    Add failing test for bailing out on already completed work
    If we already completed a subtree, we should be able to bail out because
    the tree no longer has any priority.

--- src/renderers/shared/fiber/__tests__/ReactIncrementalSideEffects-test.js
+++ src/renderers/shared/fiber/__tests__/ReactIncrementalSideEffects-test.js
@@ -844,6 +844,96 @@ describe('ReactIncrementalSideEffects', () => {
     ]);
   });
 
+  it('does not complete already completed work', () => {
+    let ops = [];
+    let lowPri;
+    let highPri;
+
+    function LowPriDidComplete() {
+      ops.push('LowPriDidComplete');
+      // Because this is terminal, beginning work on LowPriDidComplete implies
+      // that LowPri will be completed before the scheduler yields.
+      return null;
+    }
+
+    class LowPri extends React.Component {
+      state = { step: 0 };
+      render() {
+        ops.push('LowPri');
+        lowPri = this;
+        return [
+          <span prop={this.state.step} />,
+          <LowPriDidComplete />,
+        ];
+      }
+    }
+
+    function LowPriSibling() {
+      ops.push('LowPriSibling');
+      return null;
+    }
+
+    class HighPri extends React.Component {
+      state = { step: 0 };
+      render() {
+        ops.push('HighPri');
+        highPri = this;
+        return <span prop={this.state.step} />;
+      }
+    }
+
+    function App() {
+      ops.push('App');
+      return [
+        <div>
+          <LowPri />
+          <LowPriSibling />
+        </div>,
+        <div><HighPri /></div>,
+      ];
+    }
+
+    ReactNoop.render(<App />);
+    ReactNoop.flush();
+    expect(ReactNoop.getChildren()).toEqual([
+      div(span(0)),
+      div(span(0)),
+    ]);
+    ops = [];
+
+    lowPri.setState({ step: 1 });
+    // Do just enough work to begin LowPri
+    ReactNoop.flushDeferredPri(30);
+    expect(ops).toEqual(['LowPri']);
+    // Now we'll do one more tick of work to complete LowPri. Because LowPri
+    // has a sibling, the parent div of LowPri has not yet completed.
+    ReactNoop.flushDeferredPri(10);
+    expect(ops).toEqual(['LowPri', 'LowPriDidComplete']);
+    expect(ReactNoop.getChildren()).toEqual([
+      div(span(0)), // Complete, but not yet updated
+      div(span(0)),
+    ]);
+    ops = [];
+
+    // Interrupt with high pri update
+    ReactNoop.performAnimationWork(() => highPri.setState({ step: 1 }));
+    ReactNoop.flushAnimationPri();
+    expect(ops).toEqual(['HighPri']);
+    expect(ReactNoop.getChildren()).toEqual([
+      div(span(0)), // Completed, but not yet updated
+      div(span(1)),
+    ]);
+    ops = [];
+
+    // If this is not enough to commit the rest of the work, that means we're
+    // not bailing out on the already-completed LowPri tree.
+    ReactNoop.flushDeferredPri(45);
+    expect(ReactNoop.getChildren()).toEqual([
+      div(span(1)),
+      div(span(1)),
+    ]);
+  });
+
   it('deprioritizes setStates that happens within a deprioritized tree', () => {
     var ops = [];
 
