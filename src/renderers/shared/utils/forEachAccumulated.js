e51eb308564886615fd4f6cb9a7eb86e735beac4
lokson <kapustka.maciek@gmail.com>
2017-01-24 03:44:10 +0100

    [docs] forEachAccumulated params

--- src/renderers/shared/utils/forEachAccumulated.js
+++ src/renderers/shared/utils/forEachAccumulated.js
@@ -18,6 +18,8 @@
  * simple utility that allows us to reason about a collection of items, but
  * handling the case when there is exactly one item (and we do not need to
  * allocate an array).
+ * @param {function} cb Callback invoked with each element or a collection.
+ * @param {?*} scope Scope used as `this` in a callback.
  */
 function forEachAccumulated<T>(
   arr: ?(T | Array<T>),
