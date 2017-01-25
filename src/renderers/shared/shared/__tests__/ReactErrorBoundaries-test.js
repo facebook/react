8617a700633d3ad5f12fd808d41c729499241e24
Edvin Erikson <edvinerikson@gmail.com>
2017-01-25 14:31:09 +0100

    stack error boundary failing test

--- src/renderers/shared/shared/__tests__/ReactErrorBoundaries-test.js
+++ src/renderers/shared/shared/__tests__/ReactErrorBoundaries-test.js
@@ -535,6 +535,47 @@ describe('ReactErrorBoundaries', () => {
     };
   });
 
+  it('catches update errors', () => {
+    ErrorBoundary = class extends React.Component {
+      state = {
+        error: null,
+      };
+      unstable_handleError(error) {
+        log.push('ErrorBoundary unstable_handleError');
+        this.setState({ error: error });
+      }
+      render() {
+        if (this.state.error) {
+          log.push('ErrorBoundary render error');
+          return <span>{this.state.error.message}</span>;
+        }
+        log.push('ErrorBoundary render children');
+        return this.props.children;
+      }
+    }
+
+    class Child extends React.Component {
+      render() {
+        log.push('Child render');
+        throw new Error('Child error');
+      }
+    }
+
+    var container = document.createElement('div');
+    ReactDOM.render(<ErrorBoundary><h1>Child</h1></ErrorBoundary>, container);
+    expect(log).toEqual([
+      'ErrorBoundary render children',
+    ]);
+    log = [];
+    expect(() => ReactDOM.render(<ErrorBoundary><Child /></ErrorBoundary>, container)).not.toThrow();
+    expect(log).toEqual([
+      'ErrorBoundary render children',
+      'Child render',
+      'ErrorBoundary unstable_handleError',
+      'ErrorBoundary render error',
+    ]);
+  });
+
   it('does not swallow exceptions on mounting without boundaries', () => {
     var container = document.createElement('div');
     expect(() => {
