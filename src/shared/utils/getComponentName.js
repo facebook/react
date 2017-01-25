6459b14711d28bc5964434232ce9a892a795cf5d
Edvin Erikson <edvinerikson@gmail.com>
2017-01-25 14:22:51 +0100

    Allow null in flow signature

--- src/shared/utils/getComponentName.js
+++ src/shared/utils/getComponentName.js
@@ -15,7 +15,7 @@
 import type { ReactInstance } from 'ReactInstanceType';
 import type { Fiber } from 'ReactFiber';
 
-function getComponentName(instanceOrFiber : ReactInstance | Fiber) : string {
+function getComponentName(instanceOrFiber : ReactInstance | Fiber) : string | null {
   if (typeof instanceOrFiber.getName === 'function') {
     // Stack reconciler
     const instance = ((instanceOrFiber : any) : ReactInstance);
