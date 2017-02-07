77c7792556e4697636d1eb5dc34042566185c26c
Toru Kobayashi <koba0004@gmail.com>
2017-02-07 14:22:28 +0900

    Remove React.__spread

--- src/isomorphic/__tests__/React-test.js
+++ src/isomorphic/__tests__/React-test.js
@@ -19,16 +19,6 @@ describe('React', () => {
     React = require('React');
   });
 
-  it('should log a deprecation warning once when using React.__spread', () => {
-    spyOn(console, 'error');
-    React.__spread({});
-    React.__spread({});
-    expectDev(console.error.calls.count()).toBe(1);
-    expectDev(console.error.calls.argsFor(0)[0]).toContain(
-      'React.__spread is deprecated and should not be used'
-    );
-  });
-
   it('should log a deprecation warning once when using React.createMixin', () => {
     spyOn(console, 'error');
     React.createMixin();
