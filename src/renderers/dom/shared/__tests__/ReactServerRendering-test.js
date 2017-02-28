08b4936b26f511c4b65ededaa8bf045c3bce8fe2
Sasha Aickin <xander76@yahoo.com>
2017-02-27 23:03:45 -0800

    Edited to comply with the react/jsx-space-before-closing eslint rule, which will soon be merged into master.

--- src/renderers/dom/shared/__tests__/ReactServerRendering-test.js
+++ src/renderers/dom/shared/__tests__/ReactServerRendering-test.js
@@ -664,20 +664,20 @@ describe('ReactDOMServer', () => {
 
   describe('basic rendering', function() {
     itRenders('should render a blank div', render =>
-      render(<div/>).then(e => expect(e.tagName.toLowerCase()).toBe('div')));
+      render(<div />).then(e => expect(e.tagName.toLowerCase()).toBe('div')));
 
     itRenders('should render a div with inline styles', render =>
-      render(<div style={{color:'red', width:'30px'}}/>).then(e => {
+      render(<div style={{color:'red', width:'30px'}} />).then(e => {
         expect(e.style.color).toBe('red');
         expect(e.style.width).toBe('30px');
       })
     );
 
     itRenders('should render a self-closing tag', render =>
-      render(<br/>).then(e => expect(e.tagName.toLowerCase()).toBe('br')));
+      render(<br />).then(e => expect(e.tagName.toLowerCase()).toBe('br')));
 
     itRenders('should render a self-closing tag as a child', render =>
-      render(<div><br/></div>).then(e => {
+      render(<div><br /></div>).then(e => {
         expect(e.childNodes.length).toBe(1);
         expect(e.firstChild.tagName.toLowerCase()).toBe('br');
       })
