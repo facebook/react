5f396e99c08395cc04f518ebd9db6594b7b9a61c
Andrew Clark <acdlite@fb.com>
2017-03-01 18:02:21 -0800

    CI should fail if error code and warning files are not up to date
    Similar to how scripts/fiber/record-tests works. Shouldn't be onerous
    because most commits do not change error or warning messages.
    
    If it fails, it prints a message with instructions on how to fix.
    
    Keeping these files in the repo will help us track changes.

--- scripts/circleci/test_extract_errors.sh
+++ scripts/circleci/test_extract_errors.sh
@@ -3,4 +3,16 @@
 set -e
 
 ./node_modules/.bin/gulp react:extract-errors
-git checkout -- scripts/error-codes/codes.json
+
+git --no-pager diff scripts/error-codes/codes.json
+ERROR_CODE_TEST_STATUS=$(git status --porcelain scripts/error-codes/codes.json)
+
+git --no-pager diff scripts/error-codes/warnings.txt
+WARNINGS_TEST_STATUS=$(git status --porcelain scripts/error-codes/warnings.txt)
+
+if [[ -z "$ERROR_CODE_TEST_STATUS" && -z "$WARNINGS_TEST_STATUS" ]]; then
+  exit 0
+else
+  RED=$(echo -en '\033[00;31m')
+  echo ${RED}"The error code and warnings files are out of date. Run gulp react:extract-errors and commit the result."
+fi
