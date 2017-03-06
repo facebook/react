4cffb44f623a9ccbe7e483c18ec1292903d25729
Sung Won Cho <mikeswcho@gmail.com>
2017-03-06 20:28:46 +1100

    Delete unused nodeType values

--- src/renderers/dom/shared/HTMLNodeType.js
+++ src/renderers/dom/shared/HTMLNodeType.js
@@ -18,18 +18,10 @@
 var HTMLNodeType = {
   ELEMENT_NODE: 1,
   TEXT_NODE: 3,
-  PROCESSING_INSTRUCTION_NODE: 7,
   COMMENT_NODE: 8,
   DOCUMENT_NODE: 9,
   DOCUMENT_TYPE_NODE: 10,
   DOCUMENT_FRAGMENT_NODE: 11,
-
-  // The following node types have been deprecated
-  ATTRIBUTE_NODE: 2,
-  CDATA_SECTION_NODE: 4,
-  ENTITY_REFERENCE_NODE: 5,
-  ENTITY_NODE: 6,
-  NOTATION_NODE: 12,
 };
 
 module.exports = HTMLNodeType;
