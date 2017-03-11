31a9719ad5272f381d96499a9fc2a53e4070aa8f
Dan Abramov <gaearon@fb.com>
2017-03-11 21:31:12 +0000

    Fix UMD build of ReactDOMFiber

--- src/renderers/shared/fiber/ReactFiberErrorLogger.js
+++ src/renderers/shared/fiber/ReactFiberErrorLogger.js
@@ -12,8 +12,8 @@
 
 'use strict';
 
-const emptyFunction = require('emptyFunction');
-const invariant = require('invariant');
+const emptyFunction = require('fbjs/lib/emptyFunction');
+const invariant = require('fbjs/lib/invariant');
 
 import type { CapturedError } from 'ReactFiberScheduler';
 
