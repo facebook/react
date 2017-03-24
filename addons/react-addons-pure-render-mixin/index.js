52f880f405f05b421ab58b2c9f90727abbd1f2bf
Andrew Clark <acdlite@fb.com>
2017-03-24 15:17:40 -0700

    react-addons-pure-render-mixin@15.5.0-alpha.0

---
+++ addons/react-addons-pure-render-mixin/index.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+'use strict';
+
+var shallowEqual = require('fbjs/lib/shallowEqual');
+
+module.exports = {
+  shouldComponentUpdate: function(nextProps, nextState) {
+    return (
+      !shallowEqual(this.props, nextProps) ||
+      !shallowEqual(this.state, nextState)
+    );
+  },
+};
