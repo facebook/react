76bf3137e01890702252662a17b48d2a6cdeacd6
Brian Vaughn <bvaughn@fb.com>
2017-03-27 13:04:10 -0700

    Added react-addons-linked-state-mixin with UMD builds and tests

---
+++ addons/react-addons-linked-state-mixin/react-addons-linked-state-mixin.js
@@ -0,0 +1,215 @@
+(function(f) {
+  if (
+    typeof exports === "object" &&
+    typeof module !== "undefined"
+  ) {
+    module.exports=f()
+  } else if (
+    typeof define === "function" &&
+    define.amd
+  ) {
+    define([],f)
+  } else {
+    var g;
+    if (typeof window !== "undefined") {
+      g = window
+    } else if (typeof global !== "undefined") {
+      g = global
+    } else if (typeof self !== "undefined") {
+      g = self
+    } else {
+      g = this
+    }
+
+    if (typeof g.React === "undefined") {
+      throw Error('React module should be required before LinkedStateMixin');
+    } else if (typeof g.React.addons === "undefined") {
+      g.React.addons = {};
+    }
+
+    g.React.addons.LinkedStateMixin = f()
+  }
+})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/**
+ * Copyright 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+'use strict';
+
+/**
+ * ReactLink encapsulates a common pattern in which a component wants to modify
+ * a prop received from its parent. ReactLink allows the parent to pass down a
+ * value coupled with a callback that, when invoked, expresses an intent to
+ * modify that value. For example:
+ *
+ * React.createClass({
+ *   getInitialState: function() {
+ *     return {value: ''};
+ *   },
+ *   render: function() {
+ *     var valueLink = new ReactLink(this.state.value, this._handleValueChange);
+ *     return <input valueLink={valueLink} />;
+ *   },
+ *   _handleValueChange: function(newValue) {
+ *     this.setState({value: newValue});
+ *   }
+ * });
+ *
+ * We have provided some sugary mixins to make the creation and
+ * consumption of ReactLink easier; see LinkedValueUtils and LinkedStateMixin.
+ */
+
+/**
+ * Deprecated: An an easy way to express two-way binding with React. 
+ * See https://facebook.github.io/react/docs/two-way-binding-helpers.html
+ *
+ * @param {*} value current value of the link
+ * @param {function} requestChange callback to request a change
+ */
+function ReactLink(value, requestChange) {
+  this.value = value;
+  this.requestChange = requestChange;
+}
+
+/**
+ * Creates a PropType that enforces the ReactLink API and optionally checks the
+ * type of the value being passed inside the link. Example:
+ *
+ * MyComponent.propTypes = {
+ *   tabIndexLink: ReactLink.PropTypes.link(React.PropTypes.number)
+ * }
+ */
+function createLinkTypeChecker(linkType) {
+  var shapes = {
+    value: linkType === undefined ?
+      React.PropTypes.any.isRequired :
+      linkType.isRequired,
+    requestChange: React.PropTypes.func.isRequired,
+  };
+  return React.PropTypes.shape(shapes);
+}
+
+ReactLink.PropTypes = {
+  link: createLinkTypeChecker,
+};
+
+var ReactStateSetters = {
+  /**
+   * Returns a function that calls the provided function, and uses the result
+   * of that to set the component's state.
+   *
+   * @param {ReactCompositeComponent} component
+   * @param {function} funcReturningState Returned callback uses this to
+   *                                      determine how to update state.
+   * @return {function} callback that when invoked uses funcReturningState to
+   *                    determined the object literal to setState.
+   */
+  createStateSetter: function(component, funcReturningState) {
+    return function(a, b, c, d, e, f) {
+      var partialState = funcReturningState.call(component, a, b, c, d, e, f);
+      if (partialState) {
+        component.setState(partialState);
+      }
+    };
+  },
+
+  /**
+   * Returns a single-argument callback that can be used to update a single
+   * key in the component's state.
+   *
+   * Note: this is memoized function, which makes it inexpensive to call.
+   *
+   * @param {ReactCompositeComponent} component
+   * @param {string} key The key in the state that you should update.
+   * @return {function} callback of 1 argument which calls setState() with
+   *                    the provided keyName and callback argument.
+   */
+  createStateKeySetter: function(component, key) {
+    // Memoize the setters.
+    var cache = component.__keySetters || (component.__keySetters = {});
+    return cache[key] || (cache[key] = createStateKeySetter(component, key));
+  },
+};
+
+function createStateKeySetter(component, key) {
+  // Partial state is allocated outside of the function closure so it can be
+  // reused with every call, avoiding memory allocation when this function
+  // is called.
+  var partialState = {};
+  return function stateKeySetter(value) {
+    partialState[key] = value;
+    component.setState(partialState);
+  };
+}
+
+ReactStateSetters.Mixin = {
+  /**
+   * Returns a function that calls the provided function, and uses the result
+   * of that to set the component's state.
+   *
+   * For example, these statements are equivalent:
+   *
+   *   this.setState({x: 1});
+   *   this.createStateSetter(function(xValue) {
+   *     return {x: xValue};
+   *   })(1);
+   *
+   * @param {function} funcReturningState Returned callback uses this to
+   *                                      determine how to update state.
+   * @return {function} callback that when invoked uses funcReturningState to
+   *                    determined the object literal to setState.
+   */
+  createStateSetter: function(funcReturningState) {
+    return ReactStateSetters.createStateSetter(this, funcReturningState);
+  },
+
+  /**
+   * Returns a single-argument callback that can be used to update a single
+   * key in the component's state.
+   *
+   * For example, these statements are equivalent:
+   *
+   *   this.setState({x: 1});
+   *   this.createStateKeySetter('x')(1);
+   *
+   * Note: this is memoized function, which makes it inexpensive to call.
+   *
+   * @param {string} key The key in the state that you should update.
+   * @return {function} callback of 1 argument which calls setState() with
+   *                    the provided keyName and callback argument.
+   */
+  createStateKeySetter: function(key) {
+    return ReactStateSetters.createStateKeySetter(this, key);
+  },
+};
+
+/**
+ * A simple mixin around ReactLink.forState().
+ * See https://facebook.github.io/react/docs/two-way-binding-helpers.html
+ */
+var LinkedStateMixin = {
+  /**
+   * Create a ReactLink that's linked to part of this component's state. The
+   * ReactLink will have the current value of this.state[key] and will call
+   * setState() when a change is requested.
+   *
+   * @param {string} key state key to update.
+   * @return {ReactLink} ReactLink instance linking to the state.
+   */
+  linkState: function(key) {
+    return new ReactLink(
+      this.state[key],
+      ReactStateSetters.createStateKeySetter(this, key)
+    );
+  },
+};
+
+module.exports = LinkedStateMixin;
+
+},{}]},{},[1])(1)
+});
---
+++ addons/react-addons-linked-state-mixin/test.js
@@ -0,0 +1,96 @@
+/**
+ * Copyright 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @emails react-core
+ */
+
+'use strict';
+
+let LinkedStateMixin;
+let React;
+let reactCreateClass;
+let ReactDOM;
+let ReactTestUtils;
+
+// For testing DOM Fiber.
+global.requestAnimationFrame = function(callback) {
+  setTimeout(callback);
+};
+
+global.requestIdleCallback = function(callback) {
+  setTimeout(() => {
+    callback({ timeRemaining() { return Infinity; } });
+  });
+};
+
+describe('LinkedStateMixin', () => {
+  beforeEach(() => {
+    jest.resetModules()
+
+    React = require('react');
+    ReactDOM = require('react-dom');
+    ReactTestUtils = require('react-addons-test-utils');
+    LinkedStateMixin = require('./index');
+  });
+
+  // https://facebook.github.io/react/docs/two-way-binding-helpers.html#linkedstatemixin-before-and-after
+  it('should work with valueLink', () => {
+    spyOn(console, 'error'); // Ignore deprecated valueLink message for now
+
+    const WithLink = React.createClass({
+      mixins: [LinkedStateMixin],
+      getInitialState: function() {
+        return {message: 'Hello!'};
+      },
+      render: function() {
+        return <input type="text" valueLink={this.linkState('message')} />;
+      }
+    });
+
+    const instance = ReactTestUtils.renderIntoDocument(
+      React.createElement(WithLink)
+    );
+
+    expect(instance.state.message).toBe('Hello!');
+
+    const node = ReactDOM.findDOMNode(instance);
+    node.value = 'Goodbye!';
+    ReactTestUtils.Simulate.change(node);
+
+    expect(instance.state.message).toBe('Goodbye!');
+  });
+
+  // https://facebook.github.io/react/docs/two-way-binding-helpers.html#linkedstatemixin-without-valuelink
+  it('should work without valueLink', () => {
+    const WithoutLink = React.createClass({
+      mixins: [LinkedStateMixin],
+      getInitialState: function() {
+        return {message: 'Hello!'};
+      },
+      render: function() {
+        var valueLink = this.linkState('message');
+        var handleChange = function(e) {
+          valueLink.requestChange(e.target.value);
+        };
+        return <input type="text" value={valueLink.value} onChange={handleChange} />;
+      }
+    });
+
+    const instance = ReactTestUtils.renderIntoDocument(
+      React.createElement(WithoutLink)
+    );
+
+    expect(instance.state.message).toBe('Hello!');
+
+    const node = ReactDOM.findDOMNode(instance);
+    node.value = 'Goodbye!';
+    ReactTestUtils.Simulate.change(node);
+
+    expect(instance.state.message).toBe('Goodbye!');
+  });
+});
