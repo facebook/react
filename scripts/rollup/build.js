ff86c3a17beac85fa3264bcbc98bf60f414e4783
Dominic Gannaway <dg@domgan.com>
2017-03-31 16:34:44 +0100

    better error output

--- scripts/rollup/build.js
+++ scripts/rollup/build.js
@@ -437,7 +437,14 @@ function createBundle({
   )).then(() => (
     createNodePackage(bundleType, name, filename)
   )).catch(error => {
-    console.error(error);
+    if (error.code) {
+      console.error(`\x1b[31m-- ${error.code} (${error.plugin}) --`);
+      console.error(error.message);
+      console.error(error.loc);
+      console.error(error.codeFrame);
+    } else {
+      console.error(error);
+    }
     process.exit(1);
   });
 }
