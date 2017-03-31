f42f38e1ba2a1d1693f3f45a9671c7bdbd81d397
Brian Vaughn <bvaughn@fb.com>
2017-03-31 14:50:24 -0700

    Renamed react-create-class to create-react-class and fixed up tests to run in addons folder

--- addons/create-react-class/create-react-class.js
+++ addons/create-react-class/create-react-class.js
@@ -25,7 +25,7 @@
       throw Error('React module should be required before createClass');
     }
 
-    g.ReactCreateClass = f();
+    g.createReactClass = f();
   }
 })(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (process){
