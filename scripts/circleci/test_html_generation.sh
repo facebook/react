9ac0159c82310ceefa7cfdf339bc83751130bff1
Dominic Gannaway <dg@domgan.com>
2017-03-31 15:06:32 +0100

    fixed test_html_generations.sh

--- scripts/circleci/test_html_generation.sh
+++ scripts/circleci/test_html_generation.sh
@@ -5,5 +5,5 @@ set -e
 echo 'Testing in server-render (HTML generation) mode...'
 printf '\nmodule.exports.useCreateElement = false;\n' \
   >> src/renderers/dom/shared/ReactDOMFeatureFlags.js
-./node_modules/.bin/grunt jest:normal
+npm run test
 git checkout -- src/renderers/dom/shared/ReactDOMFeatureFlags.js
