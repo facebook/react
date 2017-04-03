fbeea6d5d863f9f05ffa2c62cfd3de109d72e8f5
Dominic Gannaway <dg@domgan.com>
2017-04-03 13:41:54 +0100

    removed build from circlci

--- scripts/circleci/test_entry_point.sh
+++ scripts/circleci/test_entry_point.sh
@@ -27,7 +27,7 @@ if [ $((0 % CIRCLE_NODE_TOTAL)) -eq "$CIRCLE_NODE_INDEX" ]; then
   COMMANDS_TO_RUN+=('node ./scripts/tasks/eslint')
   COMMANDS_TO_RUN+=('node ./scripts/prettier/index')
   COMMANDS_TO_RUN+=('node ./scripts/tasks/flow')
-  COMMANDS_TO_RUN+=('./scripts/circleci/test_build.sh')
+  COMMANDS_TO_RUN+=('./scripts/circleci/test_coverage.sh')
   COMMANDS_TO_RUN+=('./scripts/circleci/track_stats.sh')
   COMMANDS_TO_RUN+=('npm run test')
 fi
