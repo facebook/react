e16e4c08fbad2831e3ee8fc1e0209e4fff5ea803
Dan Abramov <gaearon@fb.com>
2017-04-04 20:55:21 +0100

    Include LICENSE/PATENTS and fix up package.json files

--- scripts/rollup/build.js
+++ scripts/rollup/build.js
@@ -324,7 +324,11 @@ function copyNodePackageTemplate(packageName) {
 
   // if the package directory already exists, we skip copying to it
   if (!fs.existsSync(to) && fs.existsSync(from)) {
-    return asyncCopyTo(from, to);
+    return Promise.all([
+      asyncCopyTo(from, to),
+      asyncCopyTo(resolve('./LICENSE'), `${to}/LICENSE`),
+      asyncCopyTo(resolve('./PATENTS'), `${to}/PATENTS`),
+    ]);
   } else {
     return Promise.resolve();
   }
