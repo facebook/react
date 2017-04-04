d7e18865a252735cb7fb1bbb96de0290c4f637da
Dan Abramov <gaearon@fb.com>
2017-04-04 20:44:00 +0100

    Tweak fixtures build to work on Node 4

--- fixtures/packaging/build-all.js
+++ fixtures/packaging/build-all.js
@@ -1,9 +1,9 @@
-const { existsSync, statSync, readdirSync } = require('fs');
-const { join } = require('path');
-const { spawnSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const child_process = require('child_process');
 
-const fixtureDirs = readdirSync(__dirname).filter((file) => {
-  return statSync(join(__dirname, file)).isDirectory();
+const fixtureDirs = fs.readdirSync(__dirname).filter((file) => {
+  return fs.statSync(path.join(__dirname, file)).isDirectory();
 });
 
 const cmdArgs = [
@@ -11,34 +11,33 @@ const cmdArgs = [
   {cmd: 'npm', args: ['run', 'build']},
 ];
 
-
 function buildFixture(cmdArg, path) {
   const opts = {
     cwd: path,
     stdio: 'inherit',
   };
-  const result = spawnSync(cmdArg.cmd, cmdArg.args, opts);
+  const result = child_process.spawnSync(cmdArg.cmd, cmdArg.args, opts);
   if (result.status !== 0) {
     throw new Error(`Failed to build fixtures!`);
   }
 }
 
-for (const dir of fixtureDirs) {
-  for (const cmdArg of cmdArgs) {
+fixtureDirs.forEach(dir => {
+  cmdArgs.forEach(cmdArg => {
     // we only care about directories that have DEV and PROD directories in
     // otherwise they don't need to be built
-    const devPath = join(__dirname, dir, 'dev');
+    const devPath = path.join(__dirname, dir, 'dev');
 
-    if (existsSync(devPath)) {
+    if (fs.existsSync(devPath)) {
       buildFixture(cmdArg, devPath);
     }
-    const prodPath = join(__dirname, dir, 'prod');
+    const prodPath = path.join(__dirname, dir, 'prod');
 
-    if (existsSync(prodPath)) {
+    if (fs.existsSync(prodPath)) {
       buildFixture(cmdArg, prodPath);
     }
-  }
-}
+  });
+});
 
 console.log('-------------------------');
 console.log('All fixtures were built!');
