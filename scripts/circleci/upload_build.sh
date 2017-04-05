32b1c244e54725faac198740ac004437737c8e4c
Dominic Gannaway <dg@domgan.com>
2017-04-05 17:22:10 +0100

    updated the react filenames to include development or production

--- scripts/circleci/upload_build.sh
+++ scripts/circleci/upload_build.sh
@@ -4,12 +4,12 @@ set -e
 
 if [ -z $CI_PULL_REQUEST ] && [ -n "$BUILD_SERVER_ENDPOINT" ]; then
   curl \
-    -F "react=@build/dist/react.development.js" \
-    -F "react.min=@build/dist/react.production.min.js" \
-    -F "react-dom=@build/dist/react-dom.development.js" \
-    -F "react-dom.min=@build/dist/react-dom-production.min.js" \
-    -F "react-dom-server=@build/dist/react-dom-server.development.js" \
-    -F "react-dom-server.min=@build/dist/react-dom-server.production.min.js" \
+    -F "react.development=@build/dist/react.development.js" \
+    -F "react.production.min=@build/dist/react.production.min.js" \
+    -F "react-dom.development=@build/dist/react-dom.development.js" \
+    -F "react-dom.production.min=@build/dist/react-dom.production.min.js" \
+    -F "react-dom-server.development=@build/dist/react-dom-server.development.js" \
+    -F "react-dom-server.production.min=@build/dist/react-dom-server.production.min.js" \
     -F "commit=$CIRCLE_SHA1" \
     -F "date=`git log --format='%ct' -1`" \
     -F "pull_request=false" \
