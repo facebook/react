88d1efb16b09980d77ff0699767cb7af1c19fc42
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-04-04 20:29:35 -0400

    Remove MouseEvent.button polyfill
    MouseEvent.button is supported in every browser React targets:
    https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button

--- src/renderers/dom/shared/syntheticEvents/SyntheticMouseEvent.js
+++ src/renderers/dom/shared/syntheticEvents/SyntheticMouseEvent.js
@@ -31,20 +31,7 @@ var MouseEventInterface = {
   altKey: null,
   metaKey: null,
   getModifierState: getEventModifierState,
-  button: function(event) {
-    // Webkit, Firefox, IE9+
-    // which:  1 2 3
-    // button: 0 1 2 (standard)
-    var button = event.button;
-    if ('which' in event) {
-      return button;
-    }
-    // IE<9
-    // which:  undefined
-    // button: 0 0 0
-    // button: 1 4 2 (onmouseup)
-    return button === 2 ? 2 : button === 4 ? 1 : 0;
-  },
+  button: null,
   buttons: null,
   relatedTarget: function(event) {
     return event.relatedTarget ||
