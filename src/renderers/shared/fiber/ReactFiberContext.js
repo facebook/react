c373c977ba8c617dd6fc7b0834754a0c8fb0e71b
Dan Abramov <gaearon@fb.com>
2017-04-06 21:19:42 +0100

    Remove indirection in checking propTypes

--- src/renderers/shared/fiber/ReactFiberContext.js
+++ src/renderers/shared/fiber/ReactFiberContext.js
@@ -15,6 +15,7 @@
 import type {Fiber} from 'ReactFiber';
 import type {StackCursor} from 'ReactFiberStack';
 
+var React = require('react');
 var emptyObject = require('fbjs/lib/emptyObject');
 var getComponentName = require('getComponentName');
 var invariant = require('fbjs/lib/invariant');
@@ -33,7 +34,6 @@ const {
 } = require('ReactFiberStack');
 
 if (__DEV__) {
-  var checkReactTypeSpec = require('checkReactTypeSpec');
   var ReactDebugCurrentFrame = require('react/lib/ReactDebugCurrentFrame');
   var ReactDebugCurrentFiber = require('ReactDebugCurrentFiber');
   var {
@@ -105,7 +105,13 @@ exports.getMaskedContext = function(
   if (__DEV__) {
     const name = getComponentName(workInProgress) || 'Unknown';
     ReactDebugCurrentFrame.current = workInProgress;
-    checkReactTypeSpec(contextTypes, context, 'context', name);
+    React.checkPropTypes(
+      contextTypes,
+      context,
+      'context',
+      name,
+      ReactDebugCurrentFrame.getStackAddendum,
+    );
     ReactDebugCurrentFrame.current = null;
   }
 
@@ -212,7 +218,13 @@ function processChildContext(
     // TODO: remove this hack when we delete unstable_renderSubtree in Fiber.
     const workInProgress = isReconciling ? fiber : null;
     ReactDebugCurrentFrame.current = workInProgress;
-    checkReactTypeSpec(childContextTypes, childContext, 'child context', name);
+    React.checkPropTypes(
+      childContextTypes,
+      childContext,
+      'child context',
+      name,
+      ReactDebugCurrentFrame.getStackAddendum,
+    );
     ReactDebugCurrentFrame.current = null;
   }
 
