23a196d863f478a404683562312556c6ea50497b
Brian Vaughn <bvaughn@fb.com>
2017-04-14 10:37:51 -0700

    Disabled shallow rendering tests for stack since there is no stack-friendly shallow renderer

--- src/test/__tests__/ReactTestUtils-test.js
+++ src/test/__tests__/ReactTestUtils-test.js
@@ -11,6 +11,8 @@
 
 'use strict';
 
+var ReactDOMFeatureFlags = require('ReactDOMFeatureFlags');
+
 var createRenderer;
 var PropTypes;
 var React;
@@ -28,129 +30,11 @@ describe('ReactTestUtils', () => {
     ReactTestUtils = require('ReactTestUtils');
   });
 
-  it('should have shallow rendering', () => {
-    class SomeComponent extends React.Component {
-      render() {
-        return (
-          <div>
-            <span className="child1" />
-            <span className="child2" />
-          </div>
-        );
-      }
-    }
-
-    var shallowRenderer = createRenderer();
-    var result = shallowRenderer.render(<SomeComponent />);
-
-    expect(result.type).toBe('div');
-    expect(result.props.children).toEqual([
-      <span className="child1" />,
-      <span className="child2" />,
-    ]);
-  });
-
-  it('should shallow render a functional component', () => {
-    function SomeComponent() {
-      return (
-        <div>
-          <span className="child1" />
-          <span className="child2" />
-        </div>
-      );
-    }
-
-    var shallowRenderer = createRenderer();
-    var result = shallowRenderer.render(<SomeComponent />);
-
-    expect(result.type).toBe('div');
-    expect(result.props.children).toEqual([
-      <span className="child1" />,
-      <span className="child2" />,
-    ]);
-  });
-
-  it('should throw for invalid elements', () => {
-    class SomeComponent extends React.Component {
-      render() {
-        return <div />;
-      }
-    }
-
-    var shallowRenderer = createRenderer();
-    expect(() => shallowRenderer.render(SomeComponent)).toThrowError(
-      'ReactShallowRenderer render(): Invalid component element. Instead of ' +
-        'passing a component class, make sure to instantiate it by passing it ' +
-        'to React.createElement.',
-    );
-    expect(() => shallowRenderer.render(<div />)).toThrowError(
-      'ReactShallowRenderer render(): Shallow rendering works only with ' +
-        'custom components, not primitives (div). Instead of calling ' +
-        '`.render(el)` and inspecting the rendered output, look at `el.props` ' +
-        'directly instead.',
-    );
-  });
-
-  it('should have shallow unmounting', () => {
-    var componentWillUnmount = jest.fn();
-
-    class SomeComponent extends React.Component {
-      componentWillUnmount = componentWillUnmount;
-      render() {
-        return <div />;
-      }
-    }
-
-    var shallowRenderer = createRenderer();
-    shallowRenderer.render(<SomeComponent />);
-    shallowRenderer.unmount();
-
-    expect(componentWillUnmount).toBeCalled();
-  });
-
-  it('can shallow render to null', () => {
-    class SomeComponent extends React.Component {
-      render() {
-        return null;
-      }
-    }
-
-    var shallowRenderer = createRenderer();
-    var result = shallowRenderer.render(<SomeComponent />);
-
-    expect(result).toBe(null);
-  });
-
-  it('can shallow render with a ref', () => {
-    class SomeComponent extends React.Component {
-      render() {
-        return <div ref="hello" />;
-      }
-    }
-
-    var shallowRenderer = createRenderer();
-    // Shouldn't crash.
-    shallowRenderer.render(<SomeComponent />);
-  });
-
-  it('lets you update shallowly rendered components', () => {
-    class SomeComponent extends React.Component {
-      state = {clicked: false};
-
-      onClick = () => {
-        this.setState({clicked: true});
-      };
-
-      render() {
-        var className = this.state.clicked ? 'was-clicked' : '';
-
-        if (this.props.aNew === 'prop') {
-          return (
-            <a href="#" onClick={this.onClick} className={className}>
-              Test link
-            </a>
-          );
-        } else {
+  // Shallow renderer only implemented for Fiber in 16+
+  if (ReactDOMFeatureFlags.useFiber) {
+    it('should have shallow rendering', () => {
+      class SomeComponent extends React.Component {
+        render() {
           return (
             <div>
               <span className="child1" />
@@ -159,148 +43,269 @@ describe('ReactTestUtils', () => {
           );
         }
       }
-    }
 
-    var shallowRenderer = createRenderer();
-    var result = shallowRenderer.render(<SomeComponent />);
-    expect(result.type).toBe('div');
-    expect(result.props.children).toEqual([
-      <span className="child1" />,
-      <span className="child2" />,
-    ]);
+      var shallowRenderer = createRenderer();
+      var result = shallowRenderer.render(<SomeComponent />);
 
-    var updatedResult = shallowRenderer.render(<SomeComponent aNew="prop" />);
-    expect(updatedResult.type).toBe('a');
+      expect(result.type).toBe('div');
+      expect(result.props.children).toEqual([
+        <span className="child1" />,
+        <span className="child2" />,
+      ]);
+    });
 
-    var mockEvent = {};
-    updatedResult.props.onClick(mockEvent);
-
-    var updatedResultCausedByClick = shallowRenderer.getRenderOutput();
-    expect(updatedResultCausedByClick.type).toBe('a');
-    expect(updatedResultCausedByClick.props.className).toBe('was-clicked');
-  });
-
-  it('can access the mounted component instance', () => {
-    class SimpleComponent extends React.Component {
-      someMethod = () => {
-        return this.props.n;
-      };
-
-      render() {
-        return <div>{this.props.n}</div>;
-      }
-    }
-
-    var shallowRenderer = createRenderer();
-    shallowRenderer.render(<SimpleComponent n={5} />);
-    expect(shallowRenderer.getMountedInstance().someMethod()).toEqual(5);
-  });
-
-  it('can shallowly render components with contextTypes', () => {
-    class SimpleComponent extends React.Component {
-      static contextTypes = {
-        name: PropTypes.string,
-      };
-
-      render() {
-        return <div />;
-      }
-    }
-
-    var shallowRenderer = createRenderer();
-    var result = shallowRenderer.render(<SimpleComponent />);
-    expect(result).toEqual(<div />);
-  });
-
-  it('can shallowly render components with ref as function', () => {
-    class SimpleComponent extends React.Component {
-      state = {clicked: false};
-
-      handleUserClick = () => {
-        this.setState({clicked: true});
-      };
-
-      render() {
+    it('should shallow render a functional component', () => {
+      function SomeComponent() {
         return (
-          <div
-            ref={() => {}}
-            onClick={this.handleUserClick}
-            className={this.state.clicked ? 'clicked' : ''}
-          />
+          <div>
+            <span className="child1" />
+            <span className="child2" />
+          </div>
         );
       }
-    }
 
-    var shallowRenderer = createRenderer();
-    shallowRenderer.render(<SimpleComponent />);
-    var result = shallowRenderer.getRenderOutput();
-    expect(result.type).toEqual('div');
-    expect(result.props.className).toEqual('');
-    result.props.onClick();
+      var shallowRenderer = createRenderer();
+      var result = shallowRenderer.render(<SomeComponent />);
 
-    result = shallowRenderer.getRenderOutput();
-    expect(result.type).toEqual('div');
-    expect(result.props.className).toEqual('clicked');
-  });
-
-  it('can setState in componentWillMount when shallow rendering', () => {
-    class SimpleComponent extends React.Component {
-      componentWillMount() {
-        this.setState({groovy: 'doovy'});
-      }
-
-      render() {
-        return <div>{this.state.groovy}</div>;
-      }
-    }
-
-    var shallowRenderer = createRenderer();
-    var result = shallowRenderer.render(<SimpleComponent />);
-    expect(result).toEqual(<div>doovy</div>);
-  });
-
-  it('can pass context when shallowly rendering', () => {
-    class SimpleComponent extends React.Component {
-      static contextTypes = {
-        name: PropTypes.string,
-      };
-
-      render() {
-        return <div>{this.context.name}</div>;
-      }
-    }
-
-    var shallowRenderer = createRenderer();
-    var result = shallowRenderer.render(<SimpleComponent />, {
-      name: 'foo',
+      expect(result.type).toBe('div');
+      expect(result.props.children).toEqual([
+        <span className="child1" />,
+        <span className="child2" />,
+      ]);
     });
-    expect(result).toEqual(<div>foo</div>);
-  });
 
-  it('can fail context when shallowly rendering', () => {
-    spyOn(console, 'error');
-
-    class SimpleComponent extends React.Component {
-      static contextTypes = {
-        name: PropTypes.string.isRequired,
-      };
-
-      render() {
-        return <div>{this.context.name}</div>;
+    it('should throw for invalid elements', () => {
+      class SomeComponent extends React.Component {
+        render() {
+          return <div />;
+        }
       }
-    }
 
-    var shallowRenderer = createRenderer();
-    shallowRenderer.render(<SimpleComponent />);
-    expectDev(console.error.calls.count()).toBe(1);
-    expect(
-      console.error.calls.argsFor(0)[0].replace(/\(at .+?:\d+\)/g, '(at **)'),
-    ).toBe(
-      'Warning: Failed context type: The context `name` is marked as ' +
-        'required in `SimpleComponent`, but its value is `undefined`.\n' +
-        '    in SimpleComponent (at **)',
-    );
-  });
+      var shallowRenderer = createRenderer();
+      expect(() => shallowRenderer.render(SomeComponent)).toThrowError(
+        'ReactShallowRenderer render(): Invalid component element. Instead of ' +
+          'passing a component class, make sure to instantiate it by passing it ' +
+          'to React.createElement.',
+      );
+      expect(() => shallowRenderer.render(<div />)).toThrowError(
+        'ReactShallowRenderer render(): Shallow rendering works only with ' +
+          'custom components, not primitives (div). Instead of calling ' +
+          '`.render(el)` and inspecting the rendered output, look at `el.props` ' +
+          'directly instead.',
+      );
+    });
+
+    it('should have shallow unmounting', () => {
+      var componentWillUnmount = jest.fn();
+
+      class SomeComponent extends React.Component {
+        componentWillUnmount = componentWillUnmount;
+        render() {
+          return <div />;
+        }
+      }
+
+      var shallowRenderer = createRenderer();
+      shallowRenderer.render(<SomeComponent />);
+      shallowRenderer.unmount();
+
+      expect(componentWillUnmount).toBeCalled();
+    });
+
+    it('can shallow render to null', () => {
+      class SomeComponent extends React.Component {
+        render() {
+          return null;
+        }
+      }
+
+      var shallowRenderer = createRenderer();
+      var result = shallowRenderer.render(<SomeComponent />);
+
+      expect(result).toBe(null);
+    });
+
+    it('can shallow render with a ref', () => {
+      class SomeComponent extends React.Component {
+        render() {
+          return <div ref="hello" />;
+        }
+      }
+
+      var shallowRenderer = createRenderer();
+      // Shouldn't crash.
+      shallowRenderer.render(<SomeComponent />);
+    });
+
+    it('lets you update shallowly rendered components', () => {
+      class SomeComponent extends React.Component {
+        state = {clicked: false};
+
+        onClick = () => {
+          this.setState({clicked: true});
+        };
+
+        render() {
+          var className = this.state.clicked ? 'was-clicked' : '';
+
+          if (this.props.aNew === 'prop') {
+            return (
+              <a href="#" onClick={this.onClick} className={className}>
+                Test link
+              </a>
+            );
+          } else {
+            return (
+              <div>
+                <span className="child1" />
+                <span className="child2" />
+              </div>
+            );
+          }
+        }
+      }
+
+      var shallowRenderer = createRenderer();
+      var result = shallowRenderer.render(<SomeComponent />);
+      expect(result.type).toBe('div');
+      expect(result.props.children).toEqual([
+        <span className="child1" />,
+        <span className="child2" />,
+      ]);
+
+      var updatedResult = shallowRenderer.render(<SomeComponent aNew="prop" />);
+      expect(updatedResult.type).toBe('a');
+
+      var mockEvent = {};
+      updatedResult.props.onClick(mockEvent);
+
+      var updatedResultCausedByClick = shallowRenderer.getRenderOutput();
+      expect(updatedResultCausedByClick.type).toBe('a');
+      expect(updatedResultCausedByClick.props.className).toBe('was-clicked');
+    });
+
+    it('can access the mounted component instance', () => {
+      class SimpleComponent extends React.Component {
+        someMethod = () => {
+          return this.props.n;
+        };
+
+        render() {
+          return <div>{this.props.n}</div>;
+        }
+      }
+
+      var shallowRenderer = createRenderer();
+      shallowRenderer.render(<SimpleComponent n={5} />);
+      expect(shallowRenderer.getMountedInstance().someMethod()).toEqual(5);
+    });
+
+    it('can shallowly render components with contextTypes', () => {
+      class SimpleComponent extends React.Component {
+        static contextTypes = {
+          name: PropTypes.string,
+        };
+
+        render() {
+          return <div />;
+        }
+      }
+
+      var shallowRenderer = createRenderer();
+      var result = shallowRenderer.render(<SimpleComponent />);
+      expect(result).toEqual(<div />);
+    });
+
+    it('can shallowly render components with ref as function', () => {
+      class SimpleComponent extends React.Component {
+        state = {clicked: false};
+
+        handleUserClick = () => {
+          this.setState({clicked: true});
+        };
+
+        render() {
+          return (
+            <div
+              ref={() => {}}
+              onClick={this.handleUserClick}
+              className={this.state.clicked ? 'clicked' : ''}
+            />
+          );
+        }
+      }
+
+      var shallowRenderer = createRenderer();
+      shallowRenderer.render(<SimpleComponent />);
+      var result = shallowRenderer.getRenderOutput();
+      expect(result.type).toEqual('div');
+      expect(result.props.className).toEqual('');
+      result.props.onClick();
+
+      result = shallowRenderer.getRenderOutput();
+      expect(result.type).toEqual('div');
+      expect(result.props.className).toEqual('clicked');
+    });
+
+    it('can setState in componentWillMount when shallow rendering', () => {
+      class SimpleComponent extends React.Component {
+        componentWillMount() {
+          this.setState({groovy: 'doovy'});
+        }
+
+        render() {
+          return <div>{this.state.groovy}</div>;
+        }
+      }
+
+      var shallowRenderer = createRenderer();
+      var result = shallowRenderer.render(<SimpleComponent />);
+      expect(result).toEqual(<div>doovy</div>);
+    });
+
+    it('can pass context when shallowly rendering', () => {
+      class SimpleComponent extends React.Component {
+        static contextTypes = {
+          name: PropTypes.string,
+        };
+
+        render() {
+          return <div>{this.context.name}</div>;
+        }
+      }
+
+      var shallowRenderer = createRenderer();
+      var result = shallowRenderer.render(<SimpleComponent />, {
+        name: 'foo',
+      });
+      expect(result).toEqual(<div>foo</div>);
+    });
+
+    it('can fail context when shallowly rendering', () => {
+      spyOn(console, 'error');
+
+      class SimpleComponent extends React.Component {
+        static contextTypes = {
+          name: PropTypes.string.isRequired,
+        };
+
+        render() {
+          return <div>{this.context.name}</div>;
+        }
+      }
+
+      var shallowRenderer = createRenderer();
+      shallowRenderer.render(<SimpleComponent />);
+      expectDev(console.error.calls.count()).toBe(1);
+      expect(
+        console.error.calls.argsFor(0)[0].replace(/\(at .+?:\d+\)/g, '(at **)'),
+      ).toBe(
+        'Warning: Failed context type: The context `name` is marked as ' +
+          'required in `SimpleComponent`, but its value is `undefined`.\n' +
+          '    in SimpleComponent (at **)',
+      );
+    });
+  } // If fiber
 
   it('can scryRenderedDOMComponentsWithClass with TextComponent', () => {
     class Wrapper extends React.Component {
@@ -513,29 +518,32 @@ describe('ReactTestUtils', () => {
     );
   });
 
-  it('should throw when attempting to use ReactTestUtils.Simulate with shallow rendering', () => {
-    class SomeComponent extends React.Component {
-      render() {
-        return (
-          <div onClick={this.props.handleClick}>
-            hello, world.
-          </div>
-        );
+  // Shallow renderer only implemented for Fiber in 16+
+  if (ReactDOMFeatureFlags.useFiber) {
+    it('should throw when attempting to use ReactTestUtils.Simulate with shallow rendering', () => {
+      class SomeComponent extends React.Component {
+        render() {
+          return (
+            <div onClick={this.props.handleClick}>
+              hello, world.
+            </div>
+          );
+        }
       }
-    }
 
-    var handler = jasmine.createSpy('spy');
-    var shallowRenderer = createRenderer();
-    var result = shallowRenderer.render(
-      <SomeComponent handleClick={handler} />,
-    );
+      var handler = jasmine.createSpy('spy');
+      var shallowRenderer = createRenderer();
+      var result = shallowRenderer.render(
+        <SomeComponent handleClick={handler} />,
+      );
 
-    expect(() => ReactTestUtils.Simulate.click(result)).toThrowError(
-      'TestUtils.Simulate expects a component instance and not a ReactElement.' +
-        'TestUtils.Simulate will not work if you are using shallow rendering.',
-    );
-    expect(handler).not.toHaveBeenCalled();
-  });
+      expect(() => ReactTestUtils.Simulate.click(result)).toThrowError(
+        'TestUtils.Simulate expects a component instance and not a ReactElement.' +
+          'TestUtils.Simulate will not work if you are using shallow rendering.',
+      );
+      expect(handler).not.toHaveBeenCalled();
+    });
+  }
 
   it('should not warn when simulating events with extra properties', () => {
     spyOn(console, 'error');
