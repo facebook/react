a052724c1739a9419d4d5ece36ad6c0bf70572b4
Andrew Clark <acdlite@fb.com>
2017-04-17 14:54:59 -0700

    ReactDOM.unstable_asyncRender creates an async-by-default tree
    The default priority of updates in a async tree is LowPriority, rather
    than SynchronousPriority.
    
    Warns if you call unstable_asyncRender on a tree that was created with
    the normal, sync ReactDOM.render.

---
+++ src/renderers/dom/fiber/__tests__/ReactDOMFiberAsync-test.js
@@ -0,0 +1,75 @@
+var React = require('react');
+var ReactDOMFeatureFlags = require('ReactDOMFeatureFlags');
+
+var ReactDOM;
+
+describe('ReactDOMFiberAsync', () => {
+  var container;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    ReactDOM = require('react-dom');
+  });
+
+  it('renders synchronously by default', () => {
+    var ops = [];
+    ReactDOM.render(<div>Hi</div>, container, () => {
+      ops.push(container.textContent);
+    });
+    ReactDOM.render(<div>Bye</div>, container, () => {
+      ops.push(container.textContent);
+    });
+    expect(ops).toEqual(['Hi', 'Bye']);
+  });
+
+  if (ReactDOMFeatureFlags.useFiber) {
+    it('throws when calling async APIs when feature flag is disabled', () => {
+      expect(() => {
+        ReactDOM.unstable_asyncRender(<div>Hi</div>, container);
+      }).toThrow('ReactDOM.unstable_asyncRender is not a function');
+    });
+
+    describe('with feature flag enabled', () => {
+      beforeEach(() => {
+        jest.resetModules();
+        ReactDOMFeatureFlags = require('ReactDOMFeatureFlags');
+        container = document.createElement('div');
+        ReactDOMFeatureFlags.enableAsyncSubtreeAPI = true;
+        ReactDOM = require('react-dom');
+      });
+
+      it('unstable_asyncRender creates an async tree', () => {
+        ReactDOM.unstable_asyncRender(<div>Hi</div>, container);
+        expect(container.textContent).toEqual('');
+        jest.runAllTimers();
+        expect(container.textContent).toEqual('Hi');
+
+        ReactDOM.unstable_asyncRender(<div>Bye</div>, container);
+        expect(container.textContent).toEqual('Hi');
+        jest.runAllTimers();
+        expect(container.textContent).toEqual('Bye');
+      });
+
+      it('updates inside an async tree are async by default', () => {
+        let instance;
+        class Component extends React.Component {
+          state = {step: 0};
+          render() {
+            instance = this;
+            return <div>{this.state.step}</div>;
+          }
+        }
+
+        ReactDOM.unstable_asyncRender(<Component />, container);
+        expect(container.textContent).toEqual('');
+        jest.runAllTimers();
+        expect(container.textContent).toEqual('0');
+
+        instance.setState({step: 1});
+        expect(container.textContent).toEqual('0');
+        jest.runAllTimers();
+        expect(container.textContent).toEqual('1');
+      });
+    });
+  }
+});
