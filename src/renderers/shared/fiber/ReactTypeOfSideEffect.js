78de968b39118f9c55e04a4bd07b8e7a49633e96
Andrew Clark <acdlite@fb.com>
2017-04-18 15:58:18 -0700

    Use number type for bitfields
    Unions don't enumerate all the possible combinations of bitfields. I'm
    not actually sure why this was type-checking before.
    
    number doesn't provide much safety but it's more correct.

--- src/renderers/shared/fiber/ReactTypeOfSideEffect.js
+++ src/renderers/shared/fiber/ReactTypeOfSideEffect.js
@@ -12,16 +12,16 @@
 
 'use strict';
 
-export type TypeOfSideEffect = 0 | 1 | 2 | 3 | 4 | 8 | 16 | 32 | 64;
+export type TypeOfSideEffect = number;
 
 module.exports = {
-  NoEffect: 0, // 0b0000000
-  Placement: 1, // 0b0000001
-  Update: 2, // 0b0000010
+  NoEffect: 0, //           0b0000000
+  Placement: 1, //          0b0000001
+  Update: 2, //             0b0000010
   PlacementAndUpdate: 3, // 0b0000011
-  Deletion: 4, // 0b0000100
-  ContentReset: 8, // 0b0001000
-  Callback: 16, // 0b0010000
-  Err: 32, // 0b0100000
-  Ref: 64, // 0b1000000
+  Deletion: 4, //           0b0000100
+  ContentReset: 8, //       0b0001000
+  Callback: 16, //          0b0010000
+  Err: 32, //               0b0100000
+  Ref: 64, //               0b1000000
 };
