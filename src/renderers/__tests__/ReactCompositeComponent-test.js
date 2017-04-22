38631e6a453ef3096f7e3f3c082fb744db3cdc51
Abhay Nikam <nikam.abhay1@gmail.com>
2017-04-22 13:21:28 +0530

    Updated the testcases for the defaultProps warning.

--- src/renderers/__tests__/ReactCompositeComponent-test.js
+++ src/renderers/__tests__/ReactCompositeComponent-test.js
@@ -504,8 +504,8 @@ describe('ReactCompositeComponent', () => {
 
     expectDev(console.error.calls.count()).toBe(1);
     expectDev(console.error.calls.argsFor(0)[0]).toBe(
-      'Warning: defaultProps was defined as an instance property ' +
-        'on Component. Use a static property to define defaultProps instead.',
+      'Warning: setting defaultProps on a instance property for ' +
+        'Component will be ignored. Use a static property to define defaultProps instead.',
     );
   });
 
