76584fee936e4f05279391d55a9f665e1f96c2cb
Dominic Gannaway <dg@domgan.com>
2017-04-24 16:31:54 +0100

    tidied up the results

--- scripts/bench/runner.js
+++ scripts/bench/runner.js
@@ -12,7 +12,7 @@ const {
 } = require('./build');
 const argv = require('minimist')(process.argv.slice(2));
 const chalk = require('chalk');
-const Table = require('cli-table');
+const printResults = require('./stats');
 
 function getBenchmarkNames() {
   return readdirSync(join(__dirname, 'benchmarks')).filter(
@@ -37,9 +37,9 @@ async function runBenchmarks() {
 // from remote master (default React repo)
 async function benchmarkRemoteMaster() {
   return {
-    // we build the FB bundles from the React repo
+    // we build the bundles from the React repo
     bundles: await buildBenchmarkBundlesFromGitRepo(),
-    // we use these FB bundles to run the benchmarks
+    // we use these bundles to run the benchmarks
     benchmarks: await runBenchmarks(),
   };
 }
@@ -48,9 +48,9 @@ async function benchmarkRemoteMaster() {
 // of the local react repo
 async function benchmarkLocal(reactPath) {
   return {
-    // we build the FB bundles from the React repo
+    // we build the bundles from the React repo
     bundles: await buildBenchmarks(reactPath),
-    // we use these FB bundles to run the benchmarks
+    // we use these bundles to run the benchmarks
     benchmarks: await runBenchmarks(),
   };
 }
@@ -79,111 +79,6 @@ async function runRemoteBenchmarks(showResults) {
     printResults(null, remoteMasterResults);
   }
   return remoteMasterResults;
-}
-
-function percentChange(prev, current) {
-  console.log(prev, current);
-  const change = Math.floor((current - prev) / prev * 100);
-
-  if (change > 0) {
-    return chalk.red(`+${change} %`);
-  } else if (change <= 0) {
-    return chalk.green(change + ' %');
-  }
-}
-
-function printResults(localResults, remoteMasterResults) {
-  const head = [''];
-  if (remoteMasterResults) {
-    head.push(chalk.yellow.bold('Remote Master'));
-  }  
-  if (localResults) {
-    head.push(chalk.green.bold('Local (Current Branch)'));
-  }
-  if (localResults && remoteMasterResults) {
-    head.push('');
-  }
-  const table = new Table({ head });
-  const benchmarks = Object.keys(
-    (localResults && localResults.benchmarks) || (remoteMasterResults && remoteMasterResults.benchmarks)
-  );
-  benchmarks.forEach(benchmark => {
-    const rowHeader = [chalk.white.bold(benchmark)];
-    if (remoteMasterResults) {
-      rowHeader.push(chalk.white.bold('Time'));
-    }    
-    if (localResults) {
-      rowHeader.push(chalk.white.bold('Time'));
-    }
-    if (localResults && remoteMasterResults) {
-      rowHeader.push(chalk.white.bold('Diff'));
-    }
-    table.push(rowHeader);
-
-    const measurements = (
-      (localResults && localResults.benchmarks[benchmark].averages) 
-      || 
-      (remoteMasterResults && remoteMasterResults.benchmarks[benchmark].averages)
-    );
-    measurements.forEach((measurement, i) => {
-      const row = [
-        chalk.gray(measurement.entry),
-      ];
-      let remoteMean;
-      if (remoteMasterResults) {
-        remoteMean = remoteMasterResults.benchmarks[benchmark].averages[i].mean;
-        row.push(chalk.white(remoteMean + ' ms'));
-      }      
-      let localMean;
-      if (localResults) {
-        localMean = localResults.benchmarks[benchmark].averages[i].mean;
-        row.push(chalk.white(localMean + ' ms'));
-      }
-      if (localResults && remoteMasterResults) {
-        row.push(percentChange(remoteMean, localMean));
-      }
-      table.push(row);
-    });
-  });
-
-  const bunldesRowHeader = [chalk.white.bold('Bundles')];
-  if (remoteMasterResults) {
-    bunldesRowHeader.push(chalk.white.bold('Size'));
-  }    
-  if (localResults) {
-    bunldesRowHeader.push(chalk.white.bold('Size'));
-  }
-  if (localResults && remoteMasterResults) {
-    bunldesRowHeader.push(chalk.white.bold('Diff'));
-  }
-  table.push(bunldesRowHeader);
-
-  const bundles = Object.keys(
-    (localResults && localResults.bundles.bundleSizes)
-    || 
-    (remoteMasterResults && remoteMasterResults.bundles.bundleSizes)
-  );
-  bundles.forEach(bundle => {
-    const row = [
-      chalk.gray(bundle),
-    ];
-    let remoteSize;
-    if (remoteMasterResults) {
-      remoteSize = remoteMasterResults.bundles.bundleSizes[bundle].size;
-      row.push(chalk.white(remoteSize + ' kb'));
-    }      
-    let localSize;
-    if (localResults) {
-      localSize = localResults.bundles.bundleSizes[bundle].size;
-      row.push(chalk.white(localSize + ' kb'));
-    }
-    if (localResults && remoteMasterResults) {
-      row.push(percentChange(remoteSize, localSize));
-    }    
-    table.push(row);
-  });
-
-  console.log(table.toString());
 }
 
 async function compareLocalToMaster() {
---
+++ scripts/bench/stats.js
@@ -0,0 +1,110 @@
+'use strict';
+
+const chalk = require('chalk');
+const Table = require('cli-table');
+
+function percentChange(prev, current) {
+  const change = Math.floor((current - prev) / prev * 100);
+
+  if (change > 0) {
+    return chalk.red(`+${change} %`);
+  } else if (change <= 0) {
+    return chalk.green(change + ' %');
+  }
+}
+
+function printResults(localResults, remoteMasterResults) {
+  const head = [''];
+  if (remoteMasterResults) {
+    head.push(chalk.yellow.bold('Remote Master'));
+  }  
+  if (localResults) {
+    head.push(chalk.green.bold('Local (Current Branch)'));
+  }
+  if (localResults && remoteMasterResults) {
+    head.push('');
+  }
+  const table = new Table({ head });
+  const benchmarks = Object.keys(
+    (localResults && localResults.benchmarks) || (remoteMasterResults && remoteMasterResults.benchmarks)
+  );
+  benchmarks.forEach(benchmark => {
+    const rowHeader = [chalk.white.bold(benchmark)];
+    if (remoteMasterResults) {
+      rowHeader.push(chalk.white.bold('Time'));
+    }    
+    if (localResults) {
+      rowHeader.push(chalk.white.bold('Time'));
+    }
+    if (localResults && remoteMasterResults) {
+      rowHeader.push(chalk.white.bold('Diff'));
+    }
+    table.push(rowHeader);
+
+    const measurements = (
+      (localResults && localResults.benchmarks[benchmark].averages) 
+      || 
+      (remoteMasterResults && remoteMasterResults.benchmarks[benchmark].averages)
+    );
+    measurements.forEach((measurement, i) => {
+      const row = [
+        chalk.gray(measurement.entry),
+      ];
+      let remoteMean;
+      if (remoteMasterResults) {
+        remoteMean = remoteMasterResults.benchmarks[benchmark].averages[i].mean;
+        row.push(chalk.white(remoteMean + ' ms'));
+      }      
+      let localMean;
+      if (localResults) {
+        localMean = localResults.benchmarks[benchmark].averages[i].mean;
+        row.push(chalk.white(localMean + ' ms'));
+      }
+      if (localResults && remoteMasterResults) {
+        row.push(percentChange(remoteMean, localMean));
+      }
+      table.push(row);
+    });
+  });
+
+  const bunldesRowHeader = [chalk.white.bold('Bundles')];
+  if (remoteMasterResults) {
+    bunldesRowHeader.push(chalk.white.bold('Size'));
+  }    
+  if (localResults) {
+    bunldesRowHeader.push(chalk.white.bold('Size'));
+  }
+  if (localResults && remoteMasterResults) {
+    bunldesRowHeader.push(chalk.white.bold('Diff'));
+  }
+  table.push(bunldesRowHeader);
+
+  const bundles = Object.keys(
+    (localResults && localResults.bundles.bundleSizes)
+    || 
+    (remoteMasterResults && remoteMasterResults.bundles.bundleSizes)
+  );
+  bundles.forEach(bundle => {
+    const row = [
+      chalk.gray(bundle),
+    ];
+    let remoteSize;
+    if (remoteMasterResults) {
+      remoteSize = remoteMasterResults.bundles.bundleSizes[bundle].size;
+      row.push(chalk.white(remoteSize + ' kb'));
+    }      
+    let localSize;
+    if (localResults) {
+      localSize = localResults.bundles.bundleSizes[bundle].size;
+      row.push(chalk.white(localSize + ' kb'));
+    }
+    if (localResults && remoteMasterResults) {
+      row.push(percentChange(remoteSize, localSize));
+    }    
+    table.push(row);
+  });
+
+  console.log(table.toString());
+}
+
+module.exports = printResults;
