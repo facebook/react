be00694b9429ce3eca93cbc70a92f606de629861
Dan Abramov <dan.abramov@gmail.com>
2017-04-27 11:57:22 +0100

    Ensure we restore globals even if test fails + minor nits

--- src/renderers/shared/__tests__/ReactDOMFrameScheduling-test.js
+++ src/renderers/shared/__tests__/ReactDOMFrameScheduling-test.js
@@ -15,30 +15,41 @@ const ReactDOMFeatureFlags = require('ReactDOMFeatureFlags');
 const describeFiber = ReactDOMFeatureFlags.useFiber ? describe : xdescribe;
 
 describeFiber('ReactDOMFrameScheduling', () => {
-  it('throws when requestAnimationFrame is not polyfilled in the browser.', () => {
+  it('throws when requestAnimationFrame is not polyfilled in the browser', () => {
     const previousRAF = global.requestAnimationFrame;
-    global.requestAnimationFrame = undefined;
-    jest.resetModules();
-    expect(() => {
-      require('ReactDOM');
-    }).toThrow(
-      'React depends on requestAnimationFrame. Make sure that you load a ' +
-        'polyfill in older browsers.',
-    );
-    global.requestAnimationFrame = previousRAF;
+    try {
+      global.requestAnimationFrame = undefined;
+      jest.resetModules();
+      expect(() => {
+        require('ReactDOM');
+      }).toThrow(
+        'React depends on requestAnimationFrame. Make sure that you load a ' +
+          'polyfill in older browsers.',
+      );
+    } finally {
+      global.requestAnimationFrame = previousRAF;
+    }
   });
 
-  it('can import findDOMNode in Node environment.', () => {
+  // We're just testing importing, not using it.
+  // It is important because even isomorphic components may import it.
+  it('can import findDOMNode in Node environment', () => {
     const previousRAF = global.requestAnimationFrame;
     const previousRIC = global.requestIdleCallback;
-    global.requestAnimationFrame = undefined;
-    global.requestIdleCallback = undefined;
     const prevWindow = global.window;
-    delete global.window;
-    jest.resetModules();
-    require('ReactDOM');
-    global.requestAnimationFrame = previousRAF;
-    global.requestIdleCallback = previousRIC;
-    global.window = prevWindow;
+    try {
+      global.requestAnimationFrame = undefined;
+      global.requestIdleCallback = undefined;
+      // Simulate the Node environment:
+      delete global.window;
+      jest.resetModules();
+      expect(() => {
+        require('ReactDOM');
+      }).not.toThrow();
+    } finally {
+      global.requestAnimationFrame = previousRAF;
+      global.requestIdleCallback = previousRIC;
+      global.window = prevWindow;
+    }
   });
 });
