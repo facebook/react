3c538e4bd92a63e8d0916c11ca79c2950f448844
hypnos <Filipp.Riabchun@jetbrains.com>
2017-05-05 21:30:00 +0300

    [Fiber] Warn when trying to unmount a non-root React node

--- src/renderers/dom/fiber/ReactDOMFiber.js
+++ src/renderers/dom/fiber/ReactDOMFiber.js
@@ -524,6 +524,19 @@ var ReactDOM = {
         });
       });
     }
+
+    if (__DEV__) {
+      const rootEl = getReactRootElementInContainer(container);
+      const hasNonRootReactChild = !!(rootEl &&
+        ReactDOMComponentTree.getInstanceFromNode(rootEl));
+      warning(
+        !hasNonRootReactChild,
+        "unmountComponentAtNode(): The node you're attempting to unmount " +
+          'was rendered by React and is not a top-level container. ' +
+          'Instead, have the parent component update its state and ' +
+          'rerender in order to remove this component.',
+      );
+    }
   },
 
   findDOMNode: findDOMNode,
