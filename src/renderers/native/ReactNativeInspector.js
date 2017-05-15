ddfd05fba9062f1a25ce571faf2c92a3c18e4b63
Dominic Gannaway <dg@domgan.com>
2017-05-15 17:30:22 +0100

    fixed conflicts with master

---
+++ src/renderers/native/ReactNativeInspector.js
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule ReactNativeInspector
+ * @flow
+ */
+'use strict';
+
+const ReactNativeComponentTree = require('ReactNativeComponentTree');
+
+if (__DEV__) {
+  var traverseOwnerTreeUp = function (hierarchy, instance) {
+    if (instance) {
+      hierarchy.unshift(instance);
+      const owner = typeof instance.tag === 'number' ?
+        // Fiber
+        instance._debugOwner :
+        // Stack
+        instance._currentElement._owner;
+      traverseOwnerTreeUp(hierarchy, owner);
+    }
+  };
+
+  var getOwnerHierarchy = function (instance) {
+    var hierarchy = [];
+    traverseOwnerTreeUp(hierarchy, instance);
+    return hierarchy;
+  };
+
+  var lastNotNativeInstance = function (hierarchy) {
+    for (let i = hierarchy.length - 1; i > 1; i--) {
+      const instance = hierarchy[i];
+      if (!instance.viewConfig) {
+        return instance;
+      }
+    }
+    return hierarchy[0];
+  };
+
+  var getInspectorDataForViewTag = function(viewTag: any): Object {
+    const fiber = ReactNativeComponentTree.findFiberByHostInstance(viewTag);
+    const hierarchy = getOwnerHierarchy(fiber);
+    const instance = lastNotNativeInstance(hierarchy);
+    const props = instance.memoizedProps || {};
+    const source = instance._debugSource;
+    
+    return {
+      hierarchy,
+      instance,
+      props,
+      source,
+    };
+  };
+}
+
+module.exports = {
+  getInspectorDataForViewTag,
+};
+
