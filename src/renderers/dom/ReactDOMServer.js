d390cb02fcf0d74050c8b468643624b2477bd73b
Dan Abramov <gaearon@fb.com>
2017-06-08 20:14:47 +0100

    Delete Stack ReactDOMServer

--- src/renderers/dom/ReactDOMServer.js
+++ src/renderers/dom/ReactDOMServer.js
@@ -12,28 +12,15 @@
 'use strict';
 
 var ReactDOMInjection = require('ReactDOMInjection');
-var ReactDOMStackInjection = require('ReactDOMStackInjection');
-var ReactServerRendering = require('ReactServerRendering');
+var ReactServerRenderer = require('ReactServerRenderer');
 var ReactVersion = require('ReactVersion');
 
 ReactDOMInjection.inject();
-ReactDOMStackInjection.inject();
 
 var ReactDOMServer = {
-  renderToString: ReactServerRendering.renderToString,
-  renderToStaticMarkup: ReactServerRendering.renderToStaticMarkup,
+  renderToString: ReactServerRenderer.renderToString,
+  renderToStaticMarkup: ReactServerRenderer.renderToStaticMarkup,
   version: ReactVersion,
 };
-
-if (__DEV__) {
-  var ReactInstrumentation = require('ReactInstrumentation');
-  var ReactDOMUnknownPropertyHook = require('ReactDOMUnknownPropertyHook');
-  var ReactDOMNullInputValuePropHook = require('ReactDOMNullInputValuePropHook');
-  var ReactDOMInvalidARIAHook = require('ReactDOMInvalidARIAHook');
-
-  ReactInstrumentation.debugTool.addHook(ReactDOMUnknownPropertyHook);
-  ReactInstrumentation.debugTool.addHook(ReactDOMNullInputValuePropHook);
-  ReactInstrumentation.debugTool.addHook(ReactDOMInvalidARIAHook);
-}
 
 module.exports = ReactDOMServer;
