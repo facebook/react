1bbf44f72dc49c7ad5ca2f39e5ba1a8ef9749401
Dan Abramov <dan.abramov@gmail.com>
2017-06-09 00:51:02 +0100

    Make ReactControlledValuePropTypes DEV-only

--- src/renderers/dom/shared/utils/ReactControlledValuePropTypes.js
+++ src/renderers/dom/shared/utils/ReactControlledValuePropTypes.js
@@ -12,65 +12,74 @@
 'use strict';
 
 var PropTypes = require('prop-types');
-var ReactPropTypesSecret = require('ReactPropTypesSecret');
-
+var emptyFunction = require('fbjs/lib/emptyFunction');
 var warning = require('fbjs/lib/warning');
 
-var hasReadOnlyValue = {
-  button: true,
-  checkbox: true,
-  image: true,
-  hidden: true,
-  radio: true,
-  reset: true,
-  submit: true,
-};
-
-var propTypes = {
-  value: function(props, propName, componentName) {
-    if (
-      !props[propName] ||
-      hasReadOnlyValue[props.type] ||
-      props.onChange ||
-      props.readOnly ||
-      props.disabled
-    ) {
-      return null;
-    }
-    return new Error(
-      'You provided a `value` prop to a form field without an ' +
-        '`onChange` handler. This will render a read-only field. If ' +
-        'the field should be mutable use `defaultValue`. Otherwise, ' +
-        'set either `onChange` or `readOnly`.',
-    );
-  },
-  checked: function(props, propName, componentName) {
-    if (
-      !props[propName] ||
-      props.onChange ||
-      props.readOnly ||
-      props.disabled
-    ) {
-      return null;
-    }
-    return new Error(
-      'You provided a `checked` prop to a form field without an ' +
-        '`onChange` handler. This will render a read-only field. If ' +
-        'the field should be mutable use `defaultChecked`. Otherwise, ' +
-        'set either `onChange` or `readOnly`.',
-    );
-  },
-  onChange: PropTypes.func,
-};
-
-var loggedTypeFailures = {};
-
-/**
- * Provide a linked `value` attribute for controlled forms. You should not use
- * this outside of the ReactDOM controlled form components.
- */
 var ReactControlledValuePropTypes = {
-  checkPropTypes: function(tagName, props, getStack) {
+  checkPropTypes: emptyFunction,
+};
+
+if (__DEV__) {
+  var ReactPropTypesSecret = 'SECRET_DO_NOT_PASS_THIS_OR_YOU_WILL_BE_FIRED';
+
+  var hasReadOnlyValue = {
+    button: true,
+    checkbox: true,
+    image: true,
+    hidden: true,
+    radio: true,
+    reset: true,
+    submit: true,
+  };
+
+  var propTypes = {
+    value: function(props, propName, componentName) {
+      if (
+        !props[propName] ||
+        hasReadOnlyValue[props.type] ||
+        props.onChange ||
+        props.readOnly ||
+        props.disabled
+      ) {
+        return null;
+      }
+      return new Error(
+        'You provided a `value` prop to a form field without an ' +
+          '`onChange` handler. This will render a read-only field. If ' +
+          'the field should be mutable use `defaultValue`. Otherwise, ' +
+          'set either `onChange` or `readOnly`.',
+      );
+    },
+    checked: function(props, propName, componentName) {
+      if (
+        !props[propName] ||
+        props.onChange ||
+        props.readOnly ||
+        props.disabled
+      ) {
+        return null;
+      }
+      return new Error(
+        'You provided a `checked` prop to a form field without an ' +
+          '`onChange` handler. This will render a read-only field. If ' +
+          'the field should be mutable use `defaultChecked`. Otherwise, ' +
+          'set either `onChange` or `readOnly`.',
+      );
+    },
+    onChange: PropTypes.func,
+  };
+
+  var loggedTypeFailures = {};
+
+  /**
+   * Provide a linked `value` attribute for controlled forms. You should not use
+   * this outside of the ReactDOM controlled form components.
+   */
+  ReactControlledValuePropTypes.checkPropTypes = function(
+    tagName,
+    props,
+    getStack,
+  ) {
     for (var propName in propTypes) {
       if (propTypes.hasOwnProperty(propName)) {
         var error = propTypes[propName](
@@ -90,7 +99,7 @@ var ReactControlledValuePropTypes = {
         warning(false, 'Failed form propType: %s%s', error.message, getStack());
       }
     }
-  },
-};
+  };
+}
 
 module.exports = ReactControlledValuePropTypes;
