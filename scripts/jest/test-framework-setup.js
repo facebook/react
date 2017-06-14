1a7a75a5e11021b622c77de7dcac4d80d1d4cbba
Dan Abramov <gaearon@fb.com>
2017-06-14 18:47:55 +0100

    Switch tests at forwarding modules rather than via Jest

--- scripts/jest/test-framework-setup.js
+++ scripts/jest/test-framework-setup.js
@@ -2,9 +2,6 @@
 
 // We want to globally mock this but jest doesn't let us do that by default
 // for a file that already exists. So we have to explicitly mock it.
-jest.mock('ReactDOMStackEntry');
-jest.mock('ReactDOMServerStackEntry');
-jest.mock('ReactNativeStackEntry');
 jest.mock('ReactDOMFeatureFlags', () => {
   const flags = require.requireActual('ReactDOMFeatureFlags');
   return Object.assign({}, flags, {
