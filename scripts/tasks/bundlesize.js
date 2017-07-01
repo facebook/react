de801eb62edc5477d2cf057721a15c3513023336
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-07-01 23:13:39 +0530

    follow script style of tests

---
+++ scripts/tasks/bundlesize.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const path = require('path');
+const spawn = require('child_process').spawn;
+
+console.log('Running buildsize');
+
+const args = [
+  path.join('node_modules', '.bin', 'bundlesize'),
+];
+
+const bundlesize = spawn('node', args, {
+  stdio: 'inherit',
+});
+
+bundlesize.on('close', code => {
+  if (code === 1) {
+    console.error('Bundlesize failed!');
+  } else {
+    console.log('Bundlesize passed!');
+  }
+  process.exit(code);
+});
