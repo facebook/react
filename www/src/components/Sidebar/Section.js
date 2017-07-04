71887fbe320ef3356316792caa86f230d971b6db
Brian Vaughn <bvaughn@fb.com>
2017-07-04 11:27:58 -0700

    Refactored Sidebar to be a component with css-modules. Moved sticky behavior out of component in favor of using 'react-sticky' lib

---
+++ www/src/components/Sidebar/Section.js
@@ -0,0 +1,35 @@
+import cn from 'classnames';
+import Link from 'gatsby-link';
+import React from 'react';
+import {slugify} from './utils';
+import styles from './Section.module.scss';
+
+const Section = ({isActive, onClick, pathname, section}) => (
+  <div className={styles.Section}>
+    <h2
+      className={cn(styles.Header, {
+        [styles.ActiveHeader]: isActive,
+      })}>
+      <a onClick={onClick}>
+        {section.title}
+      </a>
+    </h2>
+    {isActive && (
+      <ul className={styles.List}>
+        {section.items.map(item => (
+          <li
+            className={cn(styles.ListItem, {
+              [styles.ActiveListItem]: pathname.includes(slugify(item.id)),
+            })}
+            key={item.id}>
+            <Link className={styles.Link} to={slugify(item.id)}>
+              {item.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+);
+
+export default Section;
--- www/src/templates/blog.js
+++ www/src/templates/blog.js
@@ -1,6 +1,7 @@
 import Link from 'gatsby-link';
-import Helmet from 'react-helmet';
 import React from 'react';
+import Helmet from 'react-helmet';
+import {Sticky, StickyContainer} from 'react-sticky';
 import PropTypes from 'prop-types';
 import Sidebar from '../components/Sidebar';
 
@@ -9,9 +10,9 @@ import Sidebar from '../components/Sidebar';
 const linkToTitle = link => link.replace(/-/g, ' ').replace('.html', '');
 
 const Article = ({data, location}) => (
-  <main className="site__main">
+  <div className="site__main">
     <div className="wrapper">
-      <div className="article" id="nav_bounds">
+      <StickyContainer className="article" id="nav_bounds">
         <article className="article__main below_nav">
           <div className="article__inner">
             <header className="article__header">
@@ -37,43 +38,52 @@ const Article = ({data, location}) => (
             className="article__body article__inner"
             dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}
           />
-
-
-
         </article>
 
-        <Sidebar data={data} location={location} />
-      </div>
+        <Sticky>
+          {({ style }) => (
+            <div
+              className="article__nav__wrapper below_nav"
+              style={{
+                ...style,
+                width: 'auto',
+              }}
+            >
+              <Sidebar data={data} location={location} />
+            </div>
+          )}
+        </Sticky>
+      </StickyContainer>
 
       <div className="article__traverse_nav">
         <ul className="traverse_nav">
           {/* TODO Read prev/next from index map, not this way */}
           {data.markdownRemark.frontmatter.prev &&
             <li className="traverse_nav__item">
-              <a href="#">
+              <div>
                 <div className="traverse_nav__label">Previous article</div>
                 <Link
                   className="traverse_nav__title underlined"
                   to={data.markdownRemark.frontmatter.prev}>
                   {linkToTitle(data.markdownRemark.frontmatter.prev)}
                 </Link>
-              </a>
+              </div>
             </li>}
           {data.markdownRemark.frontmatter.next &&
             <li className="traverse_nav__item">
-              <a href="#">
+              <div>
                 <div className="traverse_nav__label">Next article</div>
                 <Link
                   className="traverse_nav__title underlined"
                   to={data.markdownRemark.frontmatter.next}>
                   {linkToTitle(data.markdownRemark.frontmatter.next)}
                 </Link>
-              </a>
+              </div>
             </li>}
         </ul>
       </div>
     </div>
-  </main>
+  </div>
 );
 
 Article.propTypes = {
