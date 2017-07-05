75c1374a43275710eff36b1b4b9465d26a56e9e5
Brian Vaughn <bvaughn@fb.com>
2017-07-05 09:43:03 -0700

    Sidebar component syltes tweaking

--- www/src/components/Sidebar/Section.js
+++ www/src/components/Sidebar/Section.js
@@ -5,27 +5,28 @@ import styles from './Section.module.scss';
 import slugify from '../../utils/slugify';
 
 // TODO Account for redirect_from URLs somehow; they currently won't match.
-const isItemActive = (item, pathname) => pathname.includes(slugify(item.id))
+const isItemActive = (item, pathname) => pathname.includes(slugify(item.id));
 
 const Section = ({isActive, onClick, pathname, section}) => (
   <div className={styles.Section}>
-    <h2
-      className={cn(styles.Header, {
-        [styles.ActiveHeader]: isActive,
-      })}>
-      <a onClick={onClick}>
+    <h2 className={styles.Header}>
+      <a
+        className={cn(styles.HeaderLink, {
+          [styles.ActiveHeaderLink]: isActive,
+        })}
+        onClick={onClick}>
         {section.title}
       </a>
     </h2>
     {isActive &&
       <ul className={styles.List}>
         {section.items.map(item => (
-          <li
-            className={cn(styles.ListItem, {
-              [styles.ActiveListItem]: isItemActive(item, pathname),
-            })}
-            key={item.id}>
-            <Link className={styles.Link} to={slugify(item.id)}>
+          <li key={item.id}>
+            <Link
+              className={cn(styles.Link, {
+                [styles.ActiveLink]: isItemActive(item, pathname),
+              })}
+              to={slugify(item.id)}>
               {item.title}
             </Link>
           </li>
