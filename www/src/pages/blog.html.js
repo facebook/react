64c74f168afb87b49807d39888e988c99d752aa9
Brian Vaughn <bvaughn@fb.com>
2017-07-09 08:21:19 -0700

    Fixed blog.html.js index page to load full content and limit:1

--- www/src/pages/blog.html.js
+++ www/src/pages/blog.html.js
@@ -2,31 +2,30 @@ import MarkdownPage from '../components/MarkdownPage';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './blog.module.scss';
-import sectionList from '../../../docs/_data/nav_docs.yml';
 
-const BlogIndex = ({data}) => {
-  console.log(data);
-  console.log(data.allMarkdownRemark.edges[0]);
-  return (
-    <MarkdownPage
-      author={data.allMarkdownRemark.edges[0].node.frontmatter.author}
-      date={new Date(data.allMarkdownRemark.edges[0].node.fields.date)}
-      location={location}
-      markdownRemark={data.allMarkdownRemark.edges[0].node}
-      sectionList={sectionList}
-    />
-  );
-};
+const sectionList = []; // TODO Load 10 most recent blog entries; see nav_blog.html
+
+const BlogIndex = ({data}) => (
+  <MarkdownPage
+    author={data.allMarkdownRemark.edges[0].node.frontmatter.author}
+    date={new Date(data.allMarkdownRemark.edges[0].node.fields.date)}
+    location={location}
+    markdownRemark={data.allMarkdownRemark.edges[0].node}
+    sectionList={sectionList}
+  />
+);
 
 // eslint-disable-next-line no-undef
 export const pageQuery = graphql`
   query BlogPageQuery {
     allMarkdownRemark(
+      limit: 1,
       filter: { id: { regex: "/_posts/" } }
       sort: { fields: [fields___date], order: DESC }
     ) {
       edges {
         node {
+          html
           frontmatter {
             title
             author {
