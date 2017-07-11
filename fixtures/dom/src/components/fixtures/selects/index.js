f2dea9df38e798dc50750d1529db27b2157a2599
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-07-11 07:51:00 -0400

    Add test fixtures to cover disabled selected options

--- fixtures/dom/src/components/fixtures/selects/index.js
+++ fixtures/dom/src/components/fixtures/selects/index.js
@@ -1,6 +1,9 @@
 const React = window.React;
 const ReactDOM = window.ReactDOM;
 
+import FixtureSet from '../../FixtureSet';
+import TestCase from '../../TestCase';
+
 class SelectFixture extends React.Component {
   state = {value: ''};
   _nestedDOMNode = null;
@@ -31,35 +34,66 @@ class SelectFixture extends React.Component {
 
   render() {
     return (
-      <form>
-        <fieldset>
-          <legend>Controlled</legend>
-          <select value={this.state.value} onChange={this.onChange}>
-            <option value="">Select a color</option>
-            <option value="red">Red</option>
-            <option value="blue">Blue</option>
-            <option value="green">Green</option>
-          </select>
-          <span className="hint">Value: {this.state.value}</span>
-        </fieldset>
-        <fieldset>
-          <legend>Uncontrolled</legend>
-          <select defaultValue="">
-            <option value="">Select a color</option>
-            <option value="red">Red</option>
-            <option value="blue">Blue</option>
-            <option value="green">Green</option>
-          </select>
-          <span className="hint" />
-        </fieldset>
-        <fieldset>
-          <legend>Controlled in nested subtree</legend>
-          <div ref={node => (this._nestedDOMNode = node)} />
-          <span className="hint">
-            This should synchronize in both direction with the one above.
-          </span>
-        </fieldset>
-      </form>
+      <FixtureSet title="Selects" description="">
+        <form className="field-group">
+          <fieldset>
+            <legend>Controlled</legend>
+            <select value={this.state.value} onChange={this.onChange}>
+              <option value="">Select a color</option>
+              <option value="red">Red</option>
+              <option value="blue">Blue</option>
+              <option value="green">Green</option>
+            </select>
+            <span className="hint">Value: {this.state.value}</span>
+          </fieldset>
+          <fieldset>
+            <legend>Uncontrolled</legend>
+            <select defaultValue="">
+              <option value="">Select a color</option>
+              <option value="red">Red</option>
+              <option value="blue">Blue</option>
+              <option value="green">Green</option>
+            </select>
+          </fieldset>
+        </form>
+
+        <TestCase title="A selected disabled option" relatedIssues="2803">
+          <TestCase.Steps>
+            <li>Open the select</li>
+            <li>Select "1"</li>
+            <li>Attempt to reselect "Please select an item"</li>
+          </TestCase.Steps>
+
+          <TestCase.ExpectedResult>
+            The initial picked option should be "Please select an
+            item", however it should not be a selectable option.
+          </TestCase.ExpectedResult>
+
+          <div className="test-fixture">
+            <select defaultValue="">
+              <option value="" disabled>Please select an item</option>
+              <option>0</option>
+              <option>1</option>
+              <option>2</option>
+            </select>
+          </div>
+        </TestCase>
+
+        <TestCase title="An unselected disabled option" relatedIssues="2803">
+          <TestCase.ExpectedResult>
+            The initial picked option value should "0": the first non-disabled option.
+          </TestCase.ExpectedResult>
+
+          <div className="test-fixture">
+            <select defaultValue="">
+              <option disabled>Please select an item</option>
+              <option>0</option>
+              <option>1</option>
+              <option>2</option>
+            </select>
+          </div>
+        </TestCase>
+      </FixtureSet>
     );
   }
 }
