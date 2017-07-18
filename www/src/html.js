f748aeafeee7a6104fd079f743556d2b7c79c51c
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-07-18 09:53:50 -0700

    Added favicon file to www

--- www/src/html.js
+++ www/src/html.js
@@ -37,6 +37,7 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <link rel="icon" href="/favicon.ico">
           {this.props.headComponents}
           {css}
           <Typekit kitId="xnt6blw" />
