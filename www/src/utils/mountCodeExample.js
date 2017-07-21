3cfd442991b15ad4225a1528f71f4f75d3a8f97c
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-07-21 10:14:49 -0700

    Cleaned up home template (header and footer)

--- www/src/utils/mountCodeExample.js
+++ www/src/utils/mountCodeExample.js
@@ -1,16 +1,4 @@
-/**
- * Copyright (c) 2013-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @emails react-core
-*/
-
-'use strict';
-
 import CodeEditor from '../components/CodeEditor';
-import React from 'react';
 import ReactDOM from 'react-dom';
 
 // TODO This is a huge hack.
