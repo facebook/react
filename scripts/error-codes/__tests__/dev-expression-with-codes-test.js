19792a549c7b3519999c40164f0c5263caba1846
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-08-01 14:06:41 -0700

    Prettier

--- scripts/error-codes/__tests__/dev-expression-with-codes-test.js
+++ scripts/error-codes/__tests__/dev-expression-with-codes-test.js
@@ -56,7 +56,7 @@ var invariant = require('invariant');`
       "invariant(condition, 'Do not override existing functions.');",
       "var _prodInvariant = require('reactProdInvariant');\n\n" +
         '!condition ? ' +
-        "__DEV__ ? " +
+        '__DEV__ ? ' +
         "invariant(false, 'Do not override existing functions.') : " +
         `_prodInvariant('16') : void 0;`
     );
@@ -65,7 +65,7 @@ var invariant = require('invariant');`
   it('should only add `reactProdInvariant` once', () => {
     var expectedInvariantTransformResult =
       '!condition ? ' +
-      "__DEV__ ? " +
+      '__DEV__ ? ' +
       "invariant(false, 'Do not override existing functions.') : " +
       `_prodInvariant('16') : void 0;`;
 
@@ -86,7 +86,7 @@ ${expectedInvariantTransformResult}`
       "invariant(condition, 'Expected %s target to be an array; got %s', 'foo', 'bar');",
       "var _prodInvariant = require('reactProdInvariant');\n\n" +
         '!condition ? ' +
-        "__DEV__ ? " +
+        '__DEV__ ? ' +
         "invariant(false, 'Expected %s target to be an array; got %s', 'foo', 'bar') : " +
         `_prodInvariant('7', 'foo', 'bar') : void 0;`
     );
@@ -97,7 +97,7 @@ ${expectedInvariantTransformResult}`
       "invariant(condition, 'Expected a component class, ' + 'got %s.' + '%s', 'Foo', 'Bar');",
       "var _prodInvariant = require('reactProdInvariant');\n\n" +
         '!condition ? ' +
-        "__DEV__ ? " +
+        '__DEV__ ? ' +
         "invariant(false, 'Expected a component class, got %s.%s', 'Foo', 'Bar') : " +
         `_prodInvariant('18', 'Foo', 'Bar') : void 0;`
     );
