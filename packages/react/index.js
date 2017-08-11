8e280c8a7fde7f1ed11fcc1dcdbc6f3735876638
Flarnie Marchan <flarnie@fb.com>
2017-08-11 16:20:03 -0700

    Throw error to warn of mistaken loading of prod + dev React bundles
    **what is the change?:**
    Credit to @gaearon for coming up with a clever way to check for this. :)
    
    I mainly just did the manual testing and fixed a couple of typos in his
    idea.
    
    I'd like to do a follow-up PR to add a link and a page explaining this
    issue more and suggesting how to fix it.
    
    **why make this change?:**
    
    We want to warn for an unfortunate gotcha for
    the following situation -
    1. Wanting to shrink their JS bundle, an engineer runs it through
       'uglifyjs' or some other minifier. They assume this will also do
       dead-code-elimination, but the 'prod' and 'dev' checks are not
       envified yet and dev-only code does not get removed.
    2. Then they run it through browserify's 'envify' or some other tool to
       change all calls to 'process.env.NODE_ENV' to "production". This
       makes their code ready to ship, except the 'dev' only dead code is
       still in the bundle. Their bundle is twice as large as it needs to
       be, due to the dead code.
    
    This was a problem with the old build system before, but with our new
    build system output it's possible to detect and throw an informative
    error in this case.
    
    **test plan:**
    1. run the build in react as usual; `yarn build`
    2. manually run 'uglifyjs -mt' on 'build/packages/react/index.js' to
       simulate mistakenly minifying React before env variables are
       resolved, which skips dead code elimination.
    3. run the fixtures build - `cd fixtures/packaging && node
       ./build-all.js && serve ../..`
    4. Visit just the production browserify fixture -
       http://localhost:5000/fixtures/packaging/browserify/prod/
    5. You should see the error thrown indicating this problem has occurred.
    (Flarnie will insert a screenshot)
    6. Do the above steps with NO uglifyjs step, and verify that no error is
       thrown. When there is no minification applied, we don't assume that
       this mix-up has occurred.
    (Flarnie will insert a screenshot)
    
    **issue:**
    fixes #9589

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,6 +1,36 @@
 'use strict';
 
+function testMinification() {
+  if (testMinification.name === 'testMinification') {
+    // We are not minified.
+    // This might be a Node environment where DCE is not expected anyway.
+    return;
+  }
+  if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'development') {
+      throw new Error('This is unreachable');
+    }
+    try {
+      const source = testMinification.toString();
+      if (source.indexOf('toString') === -1) {
+        // We know for a fact the above line exists.
+        // Therefore the browser gave us invalid source.
+        return;
+      }
+      if (source.indexOf('unreachable') !== -1) {
+        // Dead code elimination would have stripped that branch
+        // because it is impossible to reach in production.
+        setTimeout(function() {
+          // Ensure it gets reported to production logging
+          throw new Error('React is running in production mode, but dead code elimination has not been applied.');
+        });
+      }
+    } catch (e) {}
+  }
+}
+
 if (process.env.NODE_ENV === 'production') {
+  testMinification();
   module.exports = require('./cjs/react.production.min.js');
 } else {
   module.exports = require('./cjs/react.development.js');
