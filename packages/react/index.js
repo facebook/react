441d661ca55bc6e2f5666eb19b2b844164970220
Flarnie Marchan <flarnie@fb.com>
2017-08-12 13:31:46 -0700

    Remove extra 'prod' env. check and add link to docs in error message
    **what is the change?:**
    Based on helpful feedback from @gaearon
    - removed outer check that `process.env.NODE_ENV` is "production" since
      we are only calling the `testMinification` method inside of another
      check for "production" environment.
    - Added an fburl that points to [our current docs on using the production version of React](https://facebook.github.io/react/docs/optimizing-performance.html#use-the-production-build)
    
    **why make this change?:**
    To save an extra layer of conditionals and to make the error message
    more clear.
    
    **test plan:**
    Same test plan as earlier -
    
    1. run the build in react as usual; yarn build
    2. manually run 'uglifyjs -mt' on 'build/packages/react/index.js' to
       simulate mistakenly minifying React before env variables are
       resolved, which skips dead code elimination.
    3. run the fixtures build - cd fixtures/packaging && node ./build-all.js && serve ../..
    4. Visit just the production browserify fixture -
       http://localhost:5000/fixtures/packaging/browserify/prod/
       You should see the error thrown indicating this problem has occurred.
       (Flarnie will insert a screenshot in comments on the PR)
    6. Do the above steps with NO uglifyjs step, and verify that no error is thrown. When there is no minification applied, we don't assume that this mix-up has occurred.
    (Flarnie will insert a screenshot in the comments on the PR.)
    
    **issue:**
    https://github.com/facebook/react/issues/9589

--- packages/react/index.js
+++ packages/react/index.js
@@ -6,27 +6,29 @@ function testMinification() {
     // This might be a Node environment where DCE is not expected anyway.
     return;
   }
-  if (process.env.NODE_ENV === 'production') {
-    if (process.env.NODE_ENV === 'development') {
-      throw new Error('This is unreachable');
-    }
-    try {
-      const source = testMinification.toString();
-      if (source.indexOf('toString') === -1) {
-        // We know for a fact the above line exists.
-        // Therefore the browser gave us invalid source.
-        return;
-      }
-      if (source.indexOf('unreachable') !== -1) {
-        // Dead code elimination would have stripped that branch
-        // because it is impossible to reach in production.
-        setTimeout(function() {
-          // Ensure it gets reported to production logging
-          throw new Error('React is running in production mode, but dead code elimination has not been applied.');
-        });
-      }
-    } catch (e) {}
+  if (process.env.NODE_ENV === 'development') {
+    // We expect this method only to be called in production.
+    throw new Error('This is unreachable');
   }
+  try {
+    const source = testMinification.toString();
+    if (source.indexOf('toString') === -1) {
+      // We know for a fact the above line exists.
+      // Therefore the browser gave us invalid source.
+      return;
+    }
+    if (source.indexOf('unreachable') !== -1) {
+      // Dead code elimination would have stripped that branch
+      // because it is impossible to reach in production.
+      setTimeout(function() {
+        // Ensure it gets reported to production logging
+        throw new Error('React is running in production mode, but dead code '
+                        + 'elimination has not been applied. Read how to correctly '
+                        + 'configure React for production: '
+                        + 'https://fburl.com/react-perf-use-the-production-build');
+      });
+    }
+  } catch (e) {}
 }
 
 if (process.env.NODE_ENV === 'production') {
