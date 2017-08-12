723192cf0913b4b0c2970d6df8424f820966d7d8
Sebastian Markbage <sema@fb.com>
2017-08-11 18:26:07 -0700

    Pass native event target to the event
    We normally pass the native event target to the event object. This ensures
    that we do the same thing here.
    
    We still want to schedule a "restore" on the virtual target though since
    those are the only nodes known to React.

--- src/renderers/dom/shared/eventPlugins/ChangeEventPlugin.js
+++ src/renderers/dom/shared/eventPlugins/ChangeEventPlugin.js
@@ -47,16 +47,21 @@ function shouldUseChangeEvent(elem) {
   );
 }
 
-function createAndAccumulateChangeEvent(inst, nativeEvent, target) {
+function createAndAccumulateChangeEvent(
+  inst,
+  nativeEvent,
+  nativeEventTarget,
+  virtualTarget,
+) {
   var event = SyntheticEvent.getPooled(
     eventTypes.change,
     inst,
     nativeEvent,
-    target,
+    nativeEventTarget,
   );
   event.type = 'change';
   // Flag this event loop as needing state restore.
-  ReactControlledComponent.enqueueStateRestore(target);
+  ReactControlledComponent.enqueueStateRestore(virtualTarget);
   EventPropagators.accumulateTwoPhaseDispatches(event);
   return event;
 }
@@ -174,6 +179,7 @@ var ChangeEventPlugin = {
         var event = createAndAccumulateChangeEvent(
           inst,
           nativeEvent,
+          nativeEventTarget,
           targetNode,
         );
         return event;
