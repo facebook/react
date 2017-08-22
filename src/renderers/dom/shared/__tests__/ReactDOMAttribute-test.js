706118eb03e1b675112df343c6063d0213aff23f
Flarnie Marchan <flarnie@fb.com>
2017-08-22 13:42:13 -0700

    ran prettier

--- src/renderers/dom/shared/__tests__/ReactDOMAttribute-test.js
+++ src/renderers/dom/shared/__tests__/ReactDOMAttribute-test.js
@@ -88,36 +88,38 @@ describe('ReactDOM unknown attribute', () => {
     });
 
     // TODO: get this test passing
-    xit('coerces unknown attributes to strings **and warns** with NaN, symbols, functions, and objects', () => {
-      var el = document.createElement('div');
-      spyOn(console, 'error');
+    xit(
+      'coerces unknown attributes to strings **and warns** with NaN, symbols, functions, and objects',
+      () => {
+        var el = document.createElement('div');
+        spyOn(console, 'error');
 
-      function testCoerceToString(value) {
-        ReactDOM.render(<div unknown="something" />, el);
-        expect(el.firstChild.getAttribute('unknown')).toBe('something');
-        expectDev(console.error.calls.count(0)).toBe(0);
-        ReactDOM.render(<div unknown={value} />, el);
-        expect(el.firstChild.getAttribute('unknown')).toBe(value + '');
-        expectDev(console.error.calls.count(0)).toBe(1);
-        // TODO: add specific expectations about what the warning says
-        // expectDev(normalizeCodeLocInfo(console.error.calls.argsFor(0)[0])).toBe(...
-        console.error.calls.reset();
-      }
+        function testCoerceToString(value) {
+          ReactDOM.render(<div unknown="something" />, el);
+          expect(el.firstChild.getAttribute('unknown')).toBe('something');
+          expectDev(console.error.calls.count(0)).toBe(0);
+          ReactDOM.render(<div unknown={value} />, el);
+          expect(el.firstChild.getAttribute('unknown')).toBe(value + '');
+          expectDev(console.error.calls.count(0)).toBe(1);
+          // TODO: add specific expectations about what the warning says
+          // expectDev(normalizeCodeLocInfo(console.error.calls.argsFor(0)[0])).toBe(...
+          console.error.calls.reset();
+        }
 
-      // TODO: this does not warn. We think it should.
-      testCoerceToString(NaN);
+        // TODO: this does not warn. We think it should.
+        testCoerceToString(NaN);
 
-      // TODO: either change what we expect or change our implementation
-      // this throws "TypeError: Cannot convert a Symbol value to a string"
-      // testCoerceToString(Symbol('foo'));
+        // TODO: either change what we expect or change our implementation
+        // this throws "TypeError: Cannot convert a Symbol value to a string"
+        // testCoerceToString(Symbol('foo'));
 
+        // TODO: either change what we expect or change our implementation
+        // this does not set it to the stringified function.
+        testCoerceToString(() => 'foo');
 
-      // TODO: either change what we expect or change our implementation
-      // this does not set it to the stringified function.
-      testCoerceToString(() => 'foo');
-
-      // TODO: this does not warn. We think it should.
-      testCoerceToString({hello: 'world'});
-    });
+        // TODO: this does not warn. We think it should.
+        testCoerceToString({hello: 'world'});
+      },
+    );
   });
 });
