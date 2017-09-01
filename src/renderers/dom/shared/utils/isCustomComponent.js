9ea369d7d28b5014c74bdb80675add34c69d113b
Dan Abramov <gaearon@fb.com>
2017-08-31 18:05:45 -0700

    Don't treat dashed SVG tags as custom elements

--- src/renderers/dom/shared/utils/isCustomComponent.js
+++ src/renderers/dom/shared/utils/isCustomComponent.js
@@ -13,7 +13,26 @@
 'use strict';
 
 function isCustomComponent(tagName: string, props: Object) {
-  return tagName.indexOf('-') >= 0 || typeof props.is === 'string';
+  if (tagName.indexOf('-') === -1) {
+    return typeof props.is === 'string';
+  }
+  switch (tagName) {
+    // These are reserved SVG and MathML elements.
+    // We don't mind this whitelist too much because we expect it to never grow.
+    // The alternative is to track the namespace in a few places which is convoluted.
+    // https://w3c.github.io/webcomponents/spec/custom/#custom-elements-core-concepts
+    case 'annotation-xml':
+    case 'color-profile':
+    case 'font-face':
+    case 'font-face-src':
+    case 'font-face-uri':
+    case 'font-face-format':
+    case 'font-face-name':
+    case 'missing-glyph':
+      return false;
+    default:
+      return true;
+  }
 }
 
 module.exports = isCustomComponent;
