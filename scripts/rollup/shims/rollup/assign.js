ff2a5032f9504975bc3bcddda25c30901da1dd09
Dan Abramov <dan.abramov@gmail.com>
2017-09-11 17:05:47 +0100

    Share Object.assign polyfill between UMD builds

---
+++ scripts/rollup/shims/rollup/assign.js
@@ -0,0 +1,4 @@
+var ReactInternals = require('react')
+  .__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+
+module.exports = ReactInternals.assign;
--- src/isomorphic/ReactEntry.js
+++ src/isomorphic/ReactEntry.js
@@ -52,6 +52,8 @@ var React = {
 
   __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: {
     ReactCurrentOwner: require('ReactCurrentOwner'),
+    // Used by renderers to avoid bundling object-assign twice in UMD bundles:
+    assign: require('object-assign'),
   },
 };
 
