211fad346b9f7dff08e850b2b889399ba6d3957b
Flarnie Marchan <flarnie@fb.com>
2017-09-14 16:42:41 +1000

    remove unused var

--- www/src/templates/home.js
+++ www/src/templates/home.js
@@ -30,7 +30,7 @@ class Home extends Component {
   }
 
   render() {
-    const {data, location} = this.props;
+    const {data} = this.props;
     const title = 'React - A JavaScript library for building user interfaces';
 
     return (
