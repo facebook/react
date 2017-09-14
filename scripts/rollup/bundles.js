4832ab0e2e49b0e09f9282d74a2db1ff343d4dbf
Dan Abramov <dan.abramov@gmail.com>
2017-09-14 01:22:00 +0100

    Bye bye redundant check

--- scripts/rollup/bundles.js
+++ scripts/rollup/bundles.js
@@ -374,7 +374,6 @@ function deepFreeze(o) {
   Object.freeze(o);
   Object.getOwnPropertyNames(o).forEach(function(prop) {
     if (
-      o.hasOwnProperty(prop) &&
       o[prop] !== null &&
       (typeof o[prop] === 'object' || typeof o[prop] === 'function') &&
       !Object.isFrozen(o[prop])
