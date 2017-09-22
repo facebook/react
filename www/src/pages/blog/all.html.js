e81e3c20ecfea27571bf513cf0c93666c547da2d
Joe Critchley <joecritch@gmail.com>
2017-09-22 16:05:01 +0100

    [Gatsby Docs Update] Extra styling for All Posts template (#10775)
    * Added some extra styling for All Posts template
    
    * prettier

--- www/src/pages/blog/all.html.js
+++ www/src/pages/blog/all.html.js
@@ -15,40 +15,79 @@ import Link from 'gatsby-link';
 import Container from 'components/Container';
 import Header from 'components/Header';
 import React from 'react';
-import {sharedStyles} from 'theme';
+import {colors, media, sharedStyles} from 'theme';
 import toCommaSeparatedList from 'utils/toCommaSeparatedList';
+import MetaTitle from 'templates/components/MetaTitle';
 
 const AllBlogPosts = ({data}) => (
   <Container>
-    <Header>All Posts</Header>
-    <ul>
-      {data.allMarkdownRemark.edges.map(({node}) => (
-        <li
+    <div css={sharedStyles.articleLayout.container}>
+      <div css={sharedStyles.articleLayout.content}>
+        <Header>All Posts</Header>
+        <ul
           css={{
-            marginTop: 10,
-          }}
-          key={node.fields.slug}>
-          <Link
-            css={sharedStyles.link}
-            key={node.fields.slug}
-            to={node.fields.slug}>
-            {node.frontmatter.title}
-          </Link>
-          {' '}
-          on
-          {' '}
-          {node.fields.date}
-          {' '}
-          by
-          {' '}
-          {toCommaSeparatedList(node.frontmatter.author, author => (
-            <span key={author.frontmatter.name}>
-              {author.frontmatter.name}
-            </span>
+            display: 'flex',
+            flexWrap: 'wrap',
+            marginLeft: -40,
+          }}>
+          {data.allMarkdownRemark.edges.map(({node}) => (
+            <li
+              css={{
+                paddingLeft: 40,
+                paddingTop: 40,
+                borderTop: '1px dotted #ececec',
+                paddingBottom: 40,
+                width: '100%',
+
+                [media.size('medium')]: {
+                  width: '50%',
+                },
+
+                [media.greaterThan('large')]: {
+                  width: '33.33%',
+                },
+              }}
+              key={node.fields.slug}>
+              <h2
+                css={{
+                  fontSize: 24,
+                  color: colors.dark,
+                  lineHeight: 1.3,
+                  fontWeight: 600,
+                }}>
+                <Link
+                  css={{
+                    borderBottom: '1px solid #ececec',
+                    ':hover': {
+                      borderBottomColor: colors.black,
+                    },
+                  }}
+                  key={node.fields.slug}
+                  to={node.fields.slug}>
+                  {node.frontmatter.title}
+                </Link>
+              </h2>
+              <MetaTitle>
+                {node.fields.date}
+              </MetaTitle>
+              <div
+                css={{
+                  color: colors.subtle,
+                  marginTop: -5,
+                }}>
+                by
+                {' '}
+                {toCommaSeparatedList(node.frontmatter.author, author => (
+                  <span key={author.frontmatter.name}>
+                    {author.frontmatter.name}
+                  </span>
+                ))}
+              </div>
+            </li>
           ))}
-        </li>
-      ))}
-    </ul>
+        </ul>
+      </div>
+    </div>
   </Container>
 );
 
