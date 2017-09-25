34551f7dbfd46646cb8d89d7e9a88c367dc9b92a
Dustan Kasten <dustan.kasten@gmail.com>
2017-09-25 17:27:27 -0400

    copy noop renderer into react-reconciler fixture

--- fixtures/reconciler/index.js
+++ fixtures/reconciler/index.js
@@ -1,9 +1,29 @@
-const React = require('react');
-const Reconciler = require('react-reconciler');
-const assert = require('assert');
-const A = Reconciler({});
-const B = Reconciler({});
 
-assert(global.valueStacks.length === 2);
-assert(global.valueStacks[0] !== global.valueStacks[1]);
-console.log('Ok!');
+var React = require('react');
+var Noop = require('./NoopRenderer');
+var assert = require('assert');
+
+class Comp extends React.Component {
+  render() {
+    return this.props.active ? 'Active Comp' : 'Deactive Comp';
+  }
+}
+const Children = (props) => props.children;
+const result = Noop.render(
+  <main>
+    <div>Hello</div>
+    <Children>
+      Hello world
+      <span>{'Number '}{42}</span>
+      <Comp active />
+    </Children>
+  </main>
+);
+
+console.log('Noop dumping Tree');
+Noop.dumpTree();
+
+Noop.flushDeferredPri();
+console.log('Noop dumping Tree');
+Noop.dumpTree();
+
