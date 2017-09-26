4b2e9d7387489e82f0128367c153f2c99afcea91
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-09-26 13:32:54 -0700

    Tutorial links now update active highlight on-click

--- www/src/templates/tutorial.js
+++ www/src/templates/tutorial.js
@@ -15,15 +15,26 @@ import React from 'react';
 import {createLinkTutorial} from 'utils/createLink';
 import {sectionListTutorial} from 'utils/sectionList';
 
-const Tutorial = ({data, location}) => (
-  <MarkdownPage
-    createLink={createLinkTutorial}
-    location={location}
-    markdownRemark={data.markdownRemark}
-    sectionList={sectionListTutorial}
-    titlePostfix=" - React"
-  />
-);
+const Tutorial = ({data, location}) => {
+  // HACK The injected location prop doesn't update when hash changes
+  // This might be a gatsby issue, or a react-router/history issue,
+  // Or we might be using either library incorrectly.
+  // For now this patch keeps the hash in sync by JIT copying it from window.
+  // The undefined check prevents us from breaking on production build.
+  if (typeof window !== 'undefined' && typeof window.location !== 'undefined') {
+    location.hash = window.location.hash;
+  }
+
+  return (
+    <MarkdownPage
+      createLink={createLinkTutorial}
+      location={location}
+      markdownRemark={data.markdownRemark}
+      sectionList={sectionListTutorial}
+      titlePostfix=" - React"
+    />
+  );
+};
 
 Tutorial.propTypes = {
   data: PropTypes.object.isRequired,
