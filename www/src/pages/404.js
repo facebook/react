880026f54d141175d858e1f1802d08e4e0f00461
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-09-27 10:26:50 -0700

    Prettied up 404 page (#10884)

--- www/src/pages/404.js
+++ www/src/pages/404.js
@@ -12,16 +12,24 @@
 import Container from 'components/Container';
 import Header from 'components/Header';
 import React from 'react';
+import {sharedStyles} from 'theme';
 
 const PageNotFound = () => (
   <Container>
-    <Header>Page Not Found</Header>
-    <p>
-      We couldn't find what you were looking for.
-    </p>
-    <p>
-      Please contact the owner of the site that linked you to the original URL and let them know their link is broken.
-    </p>
+    <div css={sharedStyles.articleLayout.container}>
+      <div css={sharedStyles.articleLayout.content}>
+        <Header>Page Not Found</Header>
+        <div css={sharedStyles.markdown}>
+          <p>
+            We couldn't find what you were looking for.
+          </p>
+          <p>
+            Please contact the owner of the site that linked you
+            to the original URL and let them know their link is broken.
+          </p>
+        </div>
+      </div>
+    </div>
   </Container>
 );
 
