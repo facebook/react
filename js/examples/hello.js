89d78180d5a9416ec9fe244f1fed1e8bcebc8fe9
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-09-28 15:02:32 -0700

    Deleted old Jekyll website

--- js/examples/hello.js
+++
@@ -1,4 +0,0 @@
-var name = Math.random() > 0.5 ? 'Jane' : 'John';
-var HELLO_COMPONENT = ('\nclass HelloMessage extends React.Component {\n  render() {\n    return <div>Hello {this.props.name}</div>;\n  }\n}\n\nReactDOM.render(<HelloMessage name="' + name + '" />, mountNode);\n').trim();
-
-ReactDOM.render(React.createElement(ReactPlayground, { codeText: HELLO_COMPONENT }), document.getElementById('helloExample'));
