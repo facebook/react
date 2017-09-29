887ccf2534ab010bc645984ccff390a3e9f2c671
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-09-29 08:26:47 -0700

    Redirect users from the old gh-pages (Jekyll website) to reactjs.org (#10941)
    * Deleted old Jekyll website
    * Added CNAME that points to reactjs.org

--- js/examples/timer.js
+++
@@ -1,3 +0,0 @@
-var TIMER_COMPONENT = '\nclass Timer extends React.Component {\n  constructor(props) {\n    super(props);\n    this.state = {secondsElapsed: 0};\n  }\n\n  tick() {\n    this.setState((prevState) => ({\n      secondsElapsed: prevState.secondsElapsed + 1\n    }));\n  }\n\n  componentDidMount() {\n    this.interval = setInterval(() => this.tick(), 1000);\n  }\n\n  componentWillUnmount() {\n    clearInterval(this.interval);\n  }\n\n  render() {\n    return (\n      <div>Seconds Elapsed: {this.state.secondsElapsed}</div>\n    );\n  }\n}\n\nReactDOM.render(<Timer />, mountNode);\n'.trim();
-
-ReactDOM.render(React.createElement(ReactPlayground, { codeText: TIMER_COMPONENT }), document.getElementById('timerExample'));
