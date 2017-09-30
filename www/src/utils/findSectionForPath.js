9568b8709714711b48c848045fc81467eb21e29d
Nic Bonetto <nick.bonetto@gmail.com>
2017-09-30 14:55:11 -0700

    Created slugId variable to compare with slugified item.id. #1

--- www/src/utils/findSectionForPath.js
+++ www/src/utils/findSectionForPath.js
@@ -17,14 +17,15 @@ import slugify from './slugify';
  */
 const findSectionForPath = (pathname, sections) => {
   let activeSection;
+  const slugId = pathname.split('/').slice(-1)[0];
 
   sections.forEach(section => {
     const match = section.items.some(
       item =>
-        pathname.includes(slugify(item.id)) ||
+       slugId === slugify(item.id) ||
         (item.subitems &&
           item.subitems.some(subitem =>
-            pathname.includes(slugify(subitem.id)),
+            slugId === slugify(subitem.id),
           )),
     );
     if (match) {
