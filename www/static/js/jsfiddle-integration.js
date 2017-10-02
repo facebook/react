d7897350f053c5e28b3870536ea1d1489df8ee42
Dan Abramov <dan.abramov@gmail.com>
2017-10-02 16:32:39 +0100

    Add legacy JSFiddle files

---
+++ www/static/js/jsfiddle-integration.js
@@ -0,0 +1,12 @@
+// Do not delete or move this file.
+// Many fiddles reference it so we have to keep it here.
+(function() {
+  var tag = document.querySelector(
+    'script[type="application/javascript;version=1.7"]'
+  );
+  if (!tag || tag.textContent.indexOf('window.onload=function(){') !== -1) {
+    alert('Bad JSFiddle configuration, please fork the original React JSFiddle');
+  }
+  tag.setAttribute('type', 'text/jsx;harmony=true');
+  tag.textContent = tag.textContent.replace(/^\/\/<!\[CDATA\[/, '');
+})();
