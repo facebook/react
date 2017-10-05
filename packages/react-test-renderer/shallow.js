d89b9ce2bedb615d31e1f0628941569284d04013
Dan Abramov <dan.abramov@gmail.com>
2017-10-05 14:59:54 +0100

    Add production bundles for Test and Shallow renderers

--- packages/react-test-renderer/shallow.js
+++ packages/react-test-renderer/shallow.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  throw Error('shallow renderer is not available in production mode.');
+  module.exports = require('./cjs/react-test-renderer-shallow.production.min.js');
 } else {
-  module.exports = require('./cjs/react-test-renderer-shallow.development');
+  module.exports = require('./cjs/react-test-renderer-shallow.development.js');
 }
