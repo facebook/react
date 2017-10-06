12144517f9bd09f44ae13a64c5a3522c982ed354
andreysaleba <andreysaleba@noorsoft.ru>
2017-10-06 23:40:45 +0300

    Added check to deduplicate function type warning calls on each compon… (#11120)
    * Added check to deduplicate function type warning calls on each component type
    
    * Added test to check that 'function type as React child' warning is deduplicated correctly by component type
    
    * Ran prettier on added code
    
    * Modified test checking deduplication of 'Functions are not valid as a React child' warning so it will check against rerendering component now

--- src/renderers/__tests__/ReactComponent-test.js
+++ src/renderers/__tests__/ReactComponent-test.js
@@ -542,5 +542,42 @@ describe('ReactComponent', () => {
       expect(container.innerHTML).toBe('Hello');
       expectDev(console.error.calls.count()).toBe(0);
     });
+
+    it('deduplicates function type warnings based on component type', () => {
+      spyOn(console, 'error');
+      class Foo extends React.PureComponent {
+        constructor() {
+          super();
+          this.state = {type: 'mushrooms'};
+        }
+        render() {
+          return (
+            <div>
+              {Foo}{Foo}
+              <span>{Foo}{Foo}</span>
+            </div>
+          );
+        }
+      }
+      var container = document.createElement('div');
+      var component = ReactDOM.render(<Foo />, container);
+      component.setState({type: 'portobello mushrooms'});
+      expectDev(console.error.calls.count()).toBe(2);
+      expectDev(normalizeCodeLocInfo(console.error.calls.argsFor(0)[0])).toBe(
+        'Warning: Functions are not valid as a React child. This may happen if ' +
+          'you return a Component instead of <Component /> from render. ' +
+          'Or maybe you meant to call this function rather than return it.\n' +
+          '    in div (at **)\n' +
+          '    in Foo (at **)',
+      );
+      expectDev(normalizeCodeLocInfo(console.error.calls.argsFor(1)[0])).toBe(
+        'Warning: Functions are not valid as a React child. This may happen if ' +
+          'you return a Component instead of <Component /> from render. ' +
+          'Or maybe you meant to call this function rather than return it.\n' +
+          '    in span (at **)\n' +
+          '    in div (at **)\n' +
+          '    in Foo (at **)',
+      );
+    });
   });
 });
