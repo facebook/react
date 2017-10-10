7e5bb9b851ac0100b22a11e178ab27dd5207896c
Brandon Dail <brandon.dail@formidable.com>
2017-10-10 09:35:37 -0700

    Add fixture for textarea placeholders

--- fixtures/dom/src/components/fixtures/textareas/index.js
+++ fixtures/dom/src/components/fixtures/textareas/index.js
@@ -1,34 +1,47 @@
+import FixtureSet from '../../FixtureSet';
+import TestCase from '../../TestCase';
+
 const React = window.React;
 
-class TextAreaFixtures extends React.Component {
+export default class TextAreaFixtures extends React.Component {
   state = {value: ''};
   onChange = event => {
     this.setState({value: event.target.value});
   };
   render() {
     return (
-      <div>
-        <form className="container">
-          <fieldset>
-            <legend>Controlled</legend>
-            <textarea value={this.state.value} onChange={this.onChange} />
-          </fieldset>
-
-          <fieldset>
-            <legend>Uncontrolled</legend>
-            <textarea defaultValue="" />
-          </fieldset>
-        </form>
-
-        <div className="container">
-          <h4>Controlled Output:</h4>
-          <div className="output">
-            {this.state.value}
+      <FixtureSet title="Textareas">
+        <TestCase
+          title="Kitchen Sink"
+          description="Verify that the controlled textarea displays its value under 'Controlled Output', and that both textareas can be typed in">
+          <div>
+            <form className="container">
+              <fieldset>
+                <legend>Controlled</legend>
+                <textarea value={this.state.value} onChange={this.onChange} />
+              </fieldset>
+              <fieldset>
+                <legend>Uncontrolled</legend>
+                <textarea defaultValue="" />
+              </fieldset>
+            </form>
+            <div className="container">
+              <h4>Controlled Output:</h4>
+              <div className="output">
+                {this.state.value}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </TestCase>
+        <TestCase title="Placeholders">
+          <TestCase.ExpectedResult>
+            The textarea should be rendered with the placeholder "Hello, world"
+          </TestCase.ExpectedResult>
+          <div style={{margin: '10px 0px'}}>
+            <textarea placeholder="Hello, world" />
+          </div>
+        </TestCase>
+      </FixtureSet>
     );
   }
 }
-
-module.exports = TextAreaFixtures;
