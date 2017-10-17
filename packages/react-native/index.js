30e7d8c038d1ee547117b039b71e6426f2f9b90f
Dan Abramov <dan.abramov@gmail.com>
2017-10-17 20:16:22 +0100

    Move src/renderers/native-*/* into packages/react-native-*/src/*

--- packages/react-native/index.js
+++ packages/react-native/index.js
@@ -1,5 +1,4 @@
 'use strict';
 
-// TODO: Move the source into packages
 // TODO: This package is a lie. Maybe rename to react-native-renderer?
-module.exports = require('ReactNativeFiberEntry');
+module.exports = require('./src/ReactNativeFiberEntry');
