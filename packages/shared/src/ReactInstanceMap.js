9b903aba3e78e9fc93a1c0c6a50a3760692bd33c
Dan Abramov <dan.abramov@gmail.com>
2017-10-19 15:25:29 +0100

    Only renderers should depend on reconciler code

--- packages/shared/src/ReactInstanceMap.js
+++ packages/shared/src/ReactInstanceMap.js
@@ -14,9 +14,11 @@
  * instance (key) and the internal representation (value). This allows public
  * methods to accept the user facing instance as an argument and map them back
  * to internal methods.
+ *
+ * Note that this module is currently shared and assumed to be stateless.
+ * If this becomes an actual Map, that will break.
  */
 
-// TODO: Replace this with ES6: var ReactInstanceMap = new Map();
 var ReactInstanceMap = {
   /**
    * This API should be called `delete` but we'd have to make sure to always
