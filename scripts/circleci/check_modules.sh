5458c2d40d870b0d5c5f6dbddae70a534a91d3c1
Dan Abramov <dan.abramov@gmail.com>
2017-10-25 17:29:36 +0100

    Try to fix bash again

--- scripts/circleci/check_modules.sh
+++ scripts/circleci/check_modules.sh
@@ -3,9 +3,8 @@
 set -e
 
 # Make sure we don't introduce accidental @providesModule annotations.
-EXPECTED='scripts/circleci/check_modules.sh
-scripts/rollup/header.js'
-ACTUAL=$(git grep -l @providesModule -- :^scripts/rollup/shims/*.js)
+EXPECTED='scripts/rollup/header.js'
+ACTUAL=$(git grep -l @providesModule -- './*.js' ':!scripts/rollup/shims/*.js')
 
 if [ "$EXPECTED" != "$ACTUAL" ]; then
   echo "@providesModule crept into some new files?"
