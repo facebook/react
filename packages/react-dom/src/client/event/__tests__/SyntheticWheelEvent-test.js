317ace40a39a87c2850aa145b66db919e05edec3
DouglasGimli <douglasgimli@gmail.com>
2017-10-25 17:41:19 -0200

    Replace: ReactTestUtils.SimulateNative to native Events()

--- packages/react-dom/src/client/event/__tests__/SyntheticWheelEvent-test.js
+++ packages/react-dom/src/client/event/__tests__/SyntheticWheelEvent-test.js
@@ -12,109 +12,128 @@
 describe('SyntheticWheelEvent', () => {
   var React;
   var ReactDOM;
-  var ReactTestUtils;
+  var createDispatchWheelEvent;
 
   beforeEach(() => {
     React = require('react');
     ReactDOM = require('react-dom');
-    ReactTestUtils = require('react-dom/test-utils');
+    createDispatchWheelEvent = (component, options = {}) => {
+      const event = new MouseEvent('wheel', {
+        bubbles: true,
+        cancelable: false,
+        ...options,
+      });
+      const node = ReactDOM.findDOMNode(component);
+      node.dispatchEvent(event);
+    } 
   });
 
   it('should normalize properties from the Event interface', () => {
-    let events = [];
+    var events = [];
     const container = document.createElement('div');
+    var onWheel = event => {
+      event.persist();
+      events.push(event);
+    }
     const component = ReactDOM.render(
-      <div
-        onWheel={e => {
-          e.persist();
-          events.push(e);
-        }}
-      />,
+      <div onWheel={onWheel} />,
       container
     );
-    ReactTestUtils.SimulateNative.wheel(component, {});
+    document.body.appendChild(container);
+    createDispatchWheelEvent(component);
+
     expect(events[0].target).toBe(component);
-    expect(events[0].type).toBe(undefined);
+    expect(events[0].type).toBe('wheel');
+
+    document.body.removeChild(container);
   });
 
   it('should normalize properties from the MouseEvent interface', () => {
-    let events = [];
+    var events = [];
     const container = document.createElement('div');
+    var onWheel = event => {
+      event.persist();
+      events.push(event);
+    }
     const component = ReactDOM.render(
-      <div
-        onWheel={e => {
-          e.persist();
-          events.push(e);
-        }}
-      />,
+      <div onWheel={onWheel} />,
       container
     );
+    document.body.appendChild(container);
+    createDispatchWheelEvent(component, {which: 2, button: 1});
 
-    ReactTestUtils.SimulateNative.wheel(component, {which: 2, button: 1});
     expect(events[0].button).toBe(1);
+
+    document.body.removeChild(container);
   });
 
   it('should normalize properties from the WheelEvent interface', () => {
-    let events = [];
+    var events = [];
     const container = document.createElement('div');
+    var onWheel = event => {
+      event.persist();
+      events.push(event);
+    }
     const component = ReactDOM.render(
-      <div
-        onWheel={e => {
-          e.persist();
-          events.push(e);
-        }}
-      />,
+      <div onWheel={onWheel} />,
       container
     );
+    document.body.appendChild(container);
 
-    ReactTestUtils.SimulateNative.wheel(component, {deltaX: 10, deltaY: -50});
+    createDispatchWheelEvent(component, {deltaX: 10, deltaY: -50});
     expect(events[0].deltaX).toBe(10);
     expect(events[0].deltaY).toBe(-50);
     
-    ReactTestUtils.SimulateNative.wheel(component, {wheelDeltaX: -10, wheelDeltaY: 50});
+    createDispatchWheelEvent(component, {wheelDeltaX: -10, wheelDeltaY: 50});
     expect(events[1].deltaX).toBe(10);
     expect(events[1].deltaY).toBe(-50);
+    
+    document.body.removeChild(container);
   });
 
   it('should be able to `preventDefault` and `stopPropagation`', () => {
-    let events = [];
+    var events = [];
     const container = document.createElement('div');
+    var onWheel = event => {
+      event.persist();
+      events.push(event);
+    }
     const component = ReactDOM.render(
-      <div
-        onWheel={e => {
-          e.persist();
-          events.push(e);
-        }}
-      />,
+      <div onWheel={onWheel} />,
       container
     );
-    
-    ReactTestUtils.SimulateNative.wheel(component, {});
+    document.body.appendChild(container);
+
+    createDispatchWheelEvent(component, {});
     expect(events[0].isDefaultPrevented()).toBe(false);
     events[0].preventDefault();
     expect(events[0].isDefaultPrevented()).toBe(true);
     
-    ReactTestUtils.SimulateNative.wheel(component, {});
+    createDispatchWheelEvent(component, {});
     expect(events[1].isPropagationStopped()).toBe(false);
     events[1].stopPropagation();
     expect(events[1].isPropagationStopped()).toBe(true);
+    
+    document.body.removeChild(container);
   });
 
   it('should be able to `persist`', () => {
-    let events = [];
+    var events = [];
     const container = document.createElement('div');
+    var onWheel = event => {
+      events.push(event);
+    }
     const component = ReactDOM.render(
-      <div
-        onWheel={e => {
-          events.push(e);
-        }}
-      />,
+      <div onWheel={onWheel} />,
       container
     );
-    
-    ReactTestUtils.SimulateNative.wheel(component, {});
+    document.body.appendChild(container);
+
+    createDispatchWheelEvent(component, {});
     expect(events[0].isPersistent()).toBe(false);
     events[0].persist();
     expect(events[0].isPersistent()).toBe(true);
+    
+    document.body.removeChild(container);
   });
 });
