0ca62d06e931f0683e94a9033cfd49c273c2ea0f
Dan Abramov <dan.abramov@gmail.com>
2017-11-04 17:06:59 +0000

    Style nits

--- packages/react-dom/src/events/__tests__/getEventKey-test.js
+++ packages/react-dom/src/events/__tests__/getEventKey-test.js
@@ -21,66 +21,48 @@ describe('getEventKey', () => {
   describe('when key is implemented in a browser', () => {
     describe('when key is not normalized', () => {
       it('returns a normalized value', () => {
+        let key = null;
         class Comp extends React.Component {
-          cb = e => {
-            this.key = e.key;
-          };
-
           render() {
-            return <input ref={n => (this.a = n)} onKeyDown={this.cb} />;
+            return <input onKeyDown={e => (key = e.key)} />;
           }
         }
 
         var container = document.createElement('div');
-        var instance = ReactDOM.render(<Comp />, container);
-
+        ReactDOM.render(<Comp />, container);
         document.body.appendChild(container);
-
-        var node = ReactDOM.findDOMNode(instance.a);
 
         var nativeEvent = new KeyboardEvent('keydown', {
           key: 'Del',
           bubbles: true,
           cancelable: true,
         });
-
-        node.dispatchEvent(nativeEvent);
-
-        expect(instance.key).toBe('Delete');
-
+        container.firstChild.dispatchEvent(nativeEvent);
+        expect(key).toBe('Delete');
         document.body.removeChild(container);
       });
     });
 
     describe('when key is normalized', () => {
       it('returns a key', () => {
+        let key = null;
         class Comp extends React.Component {
-          cb = e => {
-            this.key = e.key;
-          };
-
           render() {
-            return <input ref={n => (this.a = n)} onKeyDown={this.cb} />;
+            return onKeyDown={e => (key = e.key)} />;
           }
         }
 
         var container = document.createElement('div');
-        var instance = ReactDOM.render(<Comp />, container);
-
+        ReactDOM.render(<Comp />, container);
         document.body.appendChild(container);
-
-        var node = ReactDOM.findDOMNode(instance.a);
 
         var nativeEvent = new KeyboardEvent('keydown', {
           key: 'f',
           bubbles: true,
           cancelable: true,
         });
-
-        node.dispatchEvent(nativeEvent);
-
-        expect(instance.key).toBe('f');
-
+        container.firstChild.dispatchEvent(nativeEvent);
+        expect(key).toBe('f');
         document.body.removeChild(container);
       });
     });
@@ -90,66 +72,48 @@ describe('getEventKey', () => {
     describe('when event type is keypress', () => {
       describe('when charCode is 13', () => {
         it('returns "Enter"', () => {
+          let key = null;
           class Comp extends React.Component {
-            cb = e => {
-              this.key = e.key;
-            };
-
             render() {
-              return <input ref={n => (this.a = n)} onKeyPress={this.cb} />;
+              return <input onKeyPress={e => (key = e.key)} />;
             }
           }
 
           var container = document.createElement('div');
-          var instance = ReactDOM.render(<Comp />, container);
-
+          ReactDOM.render(<Comp />, container);
           document.body.appendChild(container);
-
-          var node = ReactDOM.findDOMNode(instance.a);
 
           var nativeEvent = new KeyboardEvent('keypress', {
             charCode: 13,
             bubbles: true,
             cancelable: true,
           });
-
-          node.dispatchEvent(nativeEvent);
-
-          expect(instance.key).toBe('Enter');
-
+          container.firstChild.dispatchEvent(nativeEvent);
+          expect(key).toBe('Enter');
           document.body.removeChild(container);
         });
       });
 
       describe('when charCode is not 13', () => {
         it('returns a string from a charCode', () => {
+          let key = null;
           class Comp extends React.Component {
-            cb = e => {
-              this.key = e.key;
-            };
-
             render() {
-              return <input ref={n => (this.a = n)} onKeyPress={this.cb} />;
+              return <input onKeyPress={e => (key = e.key)} />;
             }
           }
 
           var container = document.createElement('div');
-          var instance = ReactDOM.render(<Comp />, container);
-
+          ReactDOM.render(<Comp />, container);
           document.body.appendChild(container);
-
-          var node = ReactDOM.findDOMNode(instance.a);
 
           var nativeEvent = new KeyboardEvent('keypress', {
             charCode: 65,
             bubbles: true,
             cancelable: true,
           });
-
-          node.dispatchEvent(nativeEvent);
-
-          expect(instance.key).toBe('A');
-
+          container.firstChild.dispatchEvent(nativeEvent);
+          expect(key).toBe('A');
           document.body.removeChild(container);
         });
       });
@@ -158,66 +122,48 @@ describe('getEventKey', () => {
     describe('when event type is keydown or keyup', () => {
       describe('when keyCode is recognized', () => {
         it('returns a translated key', () => {
+          let key = null;
           class Comp extends React.Component {
-            cb = e => {
-              this.key = e.key;
-            };
-
             render() {
-              return <input ref={n => (this.a = n)} onKeyDown={this.cb} />;
+              return <input onKeyDown={e => (key = e.key)} />;
             }
           }
 
           var container = document.createElement('div');
-          var instance = ReactDOM.render(<Comp />, container);
-
+          ReactDOM.render(<Comp />, container);
           document.body.appendChild(container);
-
-          var node = ReactDOM.findDOMNode(instance.a);
 
           var nativeEvent = new KeyboardEvent('keydown', {
             keyCode: 45,
             bubbles: true,
             cancelable: true,
           });
-
-          node.dispatchEvent(nativeEvent);
-
-          expect(instance.key).toBe('Insert');
-
+          container.firstChild.dispatchEvent(nativeEvent);
+          expect(key).toBe('Insert');
           document.body.removeChild(container);
         });
       });
 
       describe('when keyCode is not recognized', () => {
         it('returns Unidentified', () => {
+          let key = null;
           class Comp extends React.Component {
-            cb = e => {
-              this.key = e.key;
-            };
-
             render() {
-              return <input ref={n => (this.a = n)} onKeyDown={this.cb} />;
+              return <input onKeyDown={e => (key = e.key)} />;
             }
           }
 
           var container = document.createElement('div');
-          var instance = ReactDOM.render(<Comp />, container);
-
+          ReactDOM.render(<Comp />, container);
           document.body.appendChild(container);
-
-          var node = ReactDOM.findDOMNode(instance.a);
 
           var nativeEvent = new KeyboardEvent('keydown', {
             keyCode: 1337,
             bubbles: true,
             cancelable: true,
           });
-
-          node.dispatchEvent(nativeEvent);
-
-          expect(instance.key).toBe('Unidentified');
-
+          container.firstChild.dispatchEvent(nativeEvent);
+          expect(key).toBe('Unidentified');
           document.body.removeChild(container);
         });
       });
