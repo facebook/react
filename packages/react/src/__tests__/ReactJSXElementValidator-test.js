d7b56ebba76236702a9c2401eab839920b583659
Kristofer Selbekk <selbeezy@gmail.com>
2017-11-09 17:50:01 +0100

    Add note about mistaken named / default export
    This commit adds a note about the possibility of erroneously
    mistaking named and default exports to an existing error message.

--- packages/react/src/__tests__/ReactJSXElementValidator-test.js
+++ packages/react/src/__tests__/ReactJSXElementValidator-test.js
@@ -330,7 +330,8 @@ describe('ReactJSXElementValidator', () => {
       'Warning: React.createElement: type is invalid -- expected a string ' +
         '(for built-in components) or a class/function (for composite ' +
         'components) but got: undefined. You likely forgot to export your ' +
-        "component from the file it's defined in." +
+        "component from the file it's defined in, or you might have mixed up " +
+        'default and named imports.' +
         '\n\nCheck your code at **.',
     );
     expectDev(normalizeCodeLocInfo(console.error.calls.argsFor(1)[0])).toBe(
