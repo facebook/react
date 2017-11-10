6eaab2d9e2d740055515918ff3a63eee2bc9a606
Matteo Vesprini-Heidrich <matteo.vesprini-heidrich@originate.com>
2017-11-10 16:55:34 -0500

    track each individual file that has already been annotated with flow

--- scripts/circleci/track_stats.sh
+++ scripts/circleci/track_stats.sh
@@ -4,5 +4,7 @@ set -e
 
 ALL_FILES=`find packages -name '*.js' | grep -v umd/ | grep -v __tests__ | grep -v __mocks__`
 COUNT_ALL_FILES=`echo "$ALL_FILES" | wc -l`
-COUNT_WITH_FLOW=`grep '@flow' $ALL_FILES | perl -pe 's/:.+//' | wc -l`
-node scripts/facts-tracker/index.js "flow-files" "$COUNT_WITH_FLOW/$COUNT_ALL_FILES"
+FLOW_ANNOTATED_FILES=`grep '@flow' $ALL_FILES | perl -pe 's/:.+//'`
+COUNT_FLOW_ANNOTATED_FILES=`echo "$FLOW_ANNOTATED_FILES" | wc -l`
+INLINED_FLOW_ANNOTATED_FILES=`echo "$FLOW_ANNOTATED_FILES" | perl -pe 's/\n/ /'`
+node scripts/facts-tracker/index.js "flow-files" "$INLINED_FLOW_ANNOTATED_FILES $COUNT_FLOW_ANNOTATED_FILES/$COUNT_ALL_FILES"
