42ee4c02d3ed6871b223bb6e93e6d86bd86ed97b
Tim Jacobi <timjacobi@fb.com>
2017-11-11 12:10:44 +0000

    run yarn prettier-all

--- packages/react-dom/src/events/__tests__/getEventTarget-test.js
+++ packages/react-dom/src/events/__tests__/getEventTarget-test.js
@@ -12,39 +12,43 @@
 var getEventTarget = require('../getEventTarget').default;
 
 describe('getEventTarget', () => {
-    describe('when target is present in nativeEvent', () => {
-        it('returns target', () => {
-            var target = document.createElement('div');
-            expect(getEventTarget({target: target})).toBe(target);
-        });
+  describe('when target is present in nativeEvent', () => {
+    it('returns target', () => {
+      var target = document.createElement('div');
+      expect(getEventTarget({target: target})).toBe(target);
     });
+  });
 
-    describe('when srcElement is present in nativeEvent', () => {
-        it('returns srcElement', () => {
-            var target = document.createElement('div');
-            expect(getEventTarget({srcElement: target})).toBe(target);
-        });
+  describe('when srcElement is present in nativeEvent', () => {
+    it('returns srcElement', () => {
+      var target = document.createElement('div');
+      expect(getEventTarget({srcElement: target})).toBe(target);
     });
+  });
 
-    describe('when neither target nor srcElement is present in nativeEvent', () => {
-        it('returns the window object', () => {
-            expect(getEventTarget({})).toBe(window);
-        });
+  describe('when neither target nor srcElement is present in nativeEvent', () => {
+    it('returns the window object', () => {
+      expect(getEventTarget({})).toBe(window);
     });
+  });
 
-    describe('when correspondingUseElement is present in target in nativeEvent', () => {
-        it('returns srcElement', () => {
-            var correspondingUseElement = document.createElement('div');
-            expect(getEventTarget({target: {correspondingUseElement: correspondingUseElement}})).toBe(correspondingUseElement);
-        });
+  describe('when correspondingUseElement is present in target in nativeEvent', () => {
+    it('returns srcElement', () => {
+      var correspondingUseElement = document.createElement('div');
+      expect(
+        getEventTarget({
+          target: {correspondingUseElement: correspondingUseElement},
+        }),
+      ).toBe(correspondingUseElement);
     });
+  });
 
-    describe('when target is a text node', () => {
-        it('returns the parent element', () => {
-            var parent = document.createElement('div');
-            var child = document.createTextNode('');
-            parent.appendChild(child);
-            expect(getEventTarget({target: child})).toBe(parent);
-        });
+  describe('when target is a text node', () => {
+    it('returns the parent element', () => {
+      var parent = document.createElement('div');
+      var child = document.createTextNode('');
+      parent.appendChild(child);
+      expect(getEventTarget({target: child})).toBe(parent);
     });
+  });
 });
