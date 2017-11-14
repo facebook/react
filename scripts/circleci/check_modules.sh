18cb5fcc97bfa14ab5d6de0643b6f793e13de951
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-11-14 14:41:41 -0800

    Improved clarity of check_modules.sh error message

--- scripts/circleci/check_modules.sh
+++ scripts/circleci/check_modules.sh
@@ -6,12 +6,15 @@ set -e
 EXPECTED='packages/react-cs-renderer/src/ReactNativeCSTypes.js
 packages/react-native-renderer/src/ReactNativeTypes.js
 packages/react-rt-renderer/src/ReactNativeRTTypes.js
-packages/shared/ReactTypes.js
 scripts/rollup/wrappers.js'
 ACTUAL=$(git grep -l @providesModule -- './*.js' ':!scripts/rollup/shims/*.js')
 
+# Colors
+red=$'\e[1;31m'
+end=$'\e[0m'
+
 if [ "$EXPECTED" != "$ACTUAL" ]; then
-  echo "@providesModule crept into some new files?"
+  printf "%s\n" "${red}ERROR: @providesModule crept into some new files?${end}"
   diff -u <(echo "$EXPECTED") <(echo "$ACTUAL") || true
   exit 1
 fi
