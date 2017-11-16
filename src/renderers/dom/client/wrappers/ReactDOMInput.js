4c75c5bf58dfa5e654ecfbdeb598655443e7733b
Jason Quense <monastic.panic@gmail.com>
2017-11-16 14:22:21 -0500

    cherry pick radio input fix

--- src/renderers/dom/client/wrappers/ReactDOMInput.js
+++ src/renderers/dom/client/wrappers/ReactDOMInput.js
@@ -13,7 +13,7 @@ var DOMPropertyOperations = require('DOMPropertyOperations');
 var LinkedValueUtils = require('LinkedValueUtils');
 var ReactDOMComponentTree = require('ReactDOMComponentTree');
 var ReactUpdates = require('ReactUpdates');
-
+var inputValueTracking = require('inputValueTracking');
 var invariant = require('invariant');
 var warning = require('warning');
 
@@ -348,6 +348,11 @@ function _handleChange(event) {
         'ReactDOMInput: Mixing React and non-React radio inputs with the ' +
           'same `name` is not supported.',
       );
+
+      // We need update the tracked value on the named cousin since the value
+      // was changed but the input saw no event or value set
+      inputValueTracking.updateValueIfChanged(otherNode);
+
       // If this is a controlled radio button group, forcing the input that
       // was previously checked to update will cause it to be come re-checked
       // as appropriate.
