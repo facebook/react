cfdf0fcbecc7953c24ed929079cd6cbe91f0054d
Dan <dan.abramov@gmail.com>
2017-11-22 11:26:00 +0000

    Make validateDOMNesting tests DEV-only

--- packages/react-dom/src/__tests__/validateDOMNesting-test.js
+++ packages/react-dom/src/__tests__/validateDOMNesting-test.js
@@ -131,7 +131,7 @@ function isTagStackValid(stack) {
   return true;
 }
 
-describe('ReactContextValidator', () => {
+describe('validateDOMNesting', () => {
   beforeEach(() => {
     jest.resetModules();
 
@@ -140,37 +140,45 @@ describe('ReactContextValidator', () => {
   });
 
   it('allows any tag with no context', () => {
-    // With renderToString (for example), we don't know where we're mounting the
-    // tag so we must err on the side of leniency.
-    var allTags = [].concat(specialTags, formattingTags, ['mysterytag']);
-    allTags.forEach(function(tag) {
-      expect(validateDOMNesting.isTagValidInContext(tag, null)).toBe(true);
-    });
+    if (__DEV__) {
+      // With renderToString (for example), we don't know where we're mounting the
+      // tag so we must err on the side of leniency.
+      var allTags = [].concat(specialTags, formattingTags, ['mysterytag']);
+      allTags.forEach(function(tag) {
+        expect(validateDOMNesting.isTagValidInContext(tag, null)).toBe(true);
+      });
+    }
   });
 
   it('allows valid nestings', () => {
-    expect(isTagStackValid(['table', 'tbody', 'tr', 'td', 'b'])).toBe(true);
-    expect(isTagStackValid(['body', 'datalist', 'option'])).toBe(true);
-    expect(isTagStackValid(['div', 'a', 'object', 'a'])).toBe(true);
-    expect(isTagStackValid(['div', 'p', 'button', 'p'])).toBe(true);
-    expect(isTagStackValid(['p', 'svg', 'foreignObject', 'p'])).toBe(true);
-    expect(isTagStackValid(['html', 'body', 'div'])).toBe(true);
+    if (__DEV__) {
+      expect(isTagStackValid(['table', 'tbody', 'tr', 'td', 'b'])).toBe(true);
+      expect(isTagStackValid(['body', 'datalist', 'option'])).toBe(true);
+      expect(isTagStackValid(['div', 'a', 'object', 'a'])).toBe(true);
+      expect(isTagStackValid(['div', 'p', 'button', 'p'])).toBe(true);
+      expect(isTagStackValid(['p', 'svg', 'foreignObject', 'p'])).toBe(true);
+      expect(isTagStackValid(['html', 'body', 'div'])).toBe(true);
 
-    // Invalid, but not changed by browser parsing so we allow them
-    expect(isTagStackValid(['div', 'ul', 'ul', 'li'])).toBe(true);
-    expect(isTagStackValid(['div', 'label', 'div'])).toBe(true);
-    expect(isTagStackValid(['div', 'ul', 'li', 'section', 'li'])).toBe(true);
-    expect(isTagStackValid(['div', 'ul', 'li', 'dd', 'li'])).toBe(true);
+      // Invalid, but not changed by browser parsing so we allow them
+      expect(isTagStackValid(['div', 'ul', 'ul', 'li'])).toBe(true);
+      expect(isTagStackValid(['div', 'label', 'div'])).toBe(true);
+      expect(isTagStackValid(['div', 'ul', 'li', 'section', 'li'])).toBe(true);
+      expect(isTagStackValid(['div', 'ul', 'li', 'dd', 'li'])).toBe(true);
+    }
   });
 
   it('prevents problematic nestings', () => {
-    expect(isTagStackValid(['a', 'a'])).toBe(false);
-    expect(isTagStackValid(['form', 'form'])).toBe(false);
-    expect(isTagStackValid(['p', 'p'])).toBe(false);
-    expect(isTagStackValid(['table', 'tr'])).toBe(false);
-    expect(isTagStackValid(['div', 'ul', 'li', 'div', 'li'])).toBe(false);
-    expect(isTagStackValid(['div', 'html'])).toBe(false);
-    expect(isTagStackValid(['body', 'body'])).toBe(false);
-    expect(isTagStackValid(['svg', 'foreignObject', 'body', 'p'])).toBe(false);
+    if (__DEV__) {
+      expect(isTagStackValid(['a', 'a'])).toBe(false);
+      expect(isTagStackValid(['form', 'form'])).toBe(false);
+      expect(isTagStackValid(['p', 'p'])).toBe(false);
+      expect(isTagStackValid(['table', 'tr'])).toBe(false);
+      expect(isTagStackValid(['div', 'ul', 'li', 'div', 'li'])).toBe(false);
+      expect(isTagStackValid(['div', 'html'])).toBe(false);
+      expect(isTagStackValid(['body', 'body'])).toBe(false);
+      expect(isTagStackValid(['svg', 'foreignObject', 'body', 'p'])).toBe(
+        false,
+      );
+    }
   });
 });
