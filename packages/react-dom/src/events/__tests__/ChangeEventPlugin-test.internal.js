7bb212459b939716ea8c31bd67ff3ff7f853e6c6
Dan Abramov <dan.abramov@gmail.com>
2017-11-24 22:00:21 +0000

    Add more details and fixes to tests

--- packages/react-dom/src/events/__tests__/ChangeEventPlugin-test.internal.js
+++ packages/react-dom/src/events/__tests__/ChangeEventPlugin-test.internal.js
@@ -52,18 +52,21 @@ describe('ChangeEventPlugin', () => {
       container,
     );
 
-    node.dispatchEvent(new Event('click'));
-    expect(called).toBe(0);
-
-    setUntrackedChecked.call(node, false);
-    node.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
-    expect(called).toBe(1);
-    expect(node.checked).toBe(true);
-
-    setUntrackedChecked.call(node, true);
-    node.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
-    expect(called).toBe(2);
     expect(node.checked).toBe(false);
+    node.dispatchEvent(
+      new MouseEvent('click', {bubbles: true, cancelable: true}),
+    );
+    // Note: unlike with text input events, dispatching `click` actually
+    // toggles the checkbox and updates its `checked` value.
+    expect(node.checked).toBe(true);
+    expect(called).toBe(1);
+
+    expect(node.checked).toBe(true);
+    node.dispatchEvent(
+      new MouseEvent('click', {bubbles: true, cancelable: true}),
+    );
+    expect(node.checked).toBe(false);
+    expect(called).toBe(2);
   });
 
   it('should not fire change setting the value programmatically', () => {
@@ -79,20 +82,36 @@ describe('ChangeEventPlugin', () => {
       container,
     );
 
+    // We try to avoid firing "duplicate" React change events.
+    // However, to tell which events are duplicates and should be ignored,
+    // we are tracking the "current" input value, and only respect events
+    // that occur after it changes. In this test, we verify that we can
+    // keep track of the "current" value even if it is set programatically.
+
+    // Set it programmatically.
     input.value = 'bar';
+    // Even if a DOM input event fires, React sees that the real input value now
+    // ('bar') is the same as the "current" one we already recorded.
     input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
+    expect(input.value).toBe('bar');
+    // In this case we don't expect to get a React event.
     expect(called).toBe(0);
 
+    // However, we can simulate user typing by calling the underlying setter.
     setUntrackedValue.call(input, 'foo');
+    // Now, when the event fires, the real input value ('foo') differs from the
+    // "current" one we previously recorded ('bar').
     input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
-
-    expect(called).toBe(1);
     expect(input.value).toBe('foo');
+    // In this case React should fire an event for it.
+    expect(called).toBe(1);
 
+    // Verify again that extra events without real changes are ignored.
     input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
     expect(called).toBe(1);
   });
 
+  // See a similar input test above for a detailed description of why.
   it('should not fire change when setting checked programmatically', () => {
     var called = 0;
 
@@ -102,39 +121,27 @@ describe('ChangeEventPlugin', () => {
     }
 
     var input = ReactDOM.render(
-      <input type="checkbox" onChange={cb} defaultChecked={true} />,
+      <input type="checkbox" onChange={cb} defaultChecked={false} />,
       container,
     );
 
-    //set programmatically
+    // Set the value, updating the "current" value that React tracks to true.
     input.checked = true;
-
+    // Under the hood, uncheck the box so that the click will "check" it again.
     setUntrackedChecked.call(input, false);
-    input.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
-    expect(called).toBe(0);
-
-    setUntrackedChecked.call(input, true);
-    input.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
-
-    expect(called).toBe(1);
-  });
-
-  it('should catch setting the value programmatically', () => {
-    var called = 0;
-
-    function cb(e) {
-      called++;
-      expect(e.type).toBe('change');
-    }
-
-    var input = ReactDOM.render(
-      <input type="text" onChange={cb} defaultValue="foo" />,
-      container,
+    input.dispatchEvent(
+      new MouseEvent('click', {bubbles: true, cancelable: true}),
     );
-
-    setUntrackedValue.call(input, 'bar');
+    expect(input.checked).toBe(true);
+    // We don't expect a React event because at the time of the click, the real
+    // checked value (true) was the same as the last recorded "current" value
+    // (also true).
     expect(called).toBe(0);
-    expect(getTrackedValue(input)).toBe('bar');
+
+    // However, simulating a normal click should fire a React event because the
+    // real value (false) would have changed from the last tracked value (true).
+    input.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
+    expect(called).toBe(1);
   });
 
   it('should unmount', () => {
@@ -170,28 +177,44 @@ describe('ChangeEventPlugin', () => {
       expect(e.type).toBe('change');
     }
 
-    var input = ReactDOM.render(<input type="text" onChange={cb} />, container);
+    var input;
+    ['text', 'number', 'range'].forEach(type => {
+      called = 0;
+      input = ReactDOM.render(<input type={type} onChange={cb} />, container);
+      setUntrackedValue.call(input, '42');
+      input.dispatchEvent(
+        new Event('change', {bubbles: true, cancelable: true}),
+      );
+      input.dispatchEvent(
+        new Event('change', {bubbles: true, cancelable: true}),
+      );
+      expect(called).toBe(1);
+      ReactDOM.unmountComponentAtNode(container);
 
-    setUntrackedValue.call(input, 'foo');
+      called = 0;
+      input = ReactDOM.render(<input type={type} onChange={cb} />, container);
+      setUntrackedValue.call(input, '42');
+      input.dispatchEvent(
+        new Event('input', {bubbles: true, cancelable: true}),
+      );
+      input.dispatchEvent(
+        new Event('input', {bubbles: true, cancelable: true}),
+      );
+      expect(called).toBe(1);
+      ReactDOM.unmountComponentAtNode(container);
 
-    input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
-    input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
-
-    expect(called).toBe(1);
-
-    setUntrackedValue.call(input, 'bar');
-
-    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
-    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
-
-    expect(called).toBe(2);
-
-    setUntrackedValue.call(input, 'foobar');
-
-    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
-    input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
-
-    expect(called).toBe(3);
+      called = 0;
+      input = ReactDOM.render(<input type={type} onChange={cb} />, container);
+      setUntrackedValue.call(input, '42');
+      input.dispatchEvent(
+        new Event('input', {bubbles: true, cancelable: true}),
+      );
+      input.dispatchEvent(
+        new Event('change', {bubbles: true, cancelable: true}),
+      );
+      expect(called).toBe(1);
+      ReactDOM.unmountComponentAtNode(container);
+    });
   });
 
   it('should listen for both change and input events when supported', () => {
@@ -207,10 +230,10 @@ describe('ChangeEventPlugin', () => {
       container,
     );
 
-    setUntrackedValue.call(input, '0');
+    setUntrackedValue.call(input, 'foo');
     input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
 
-    setUntrackedValue.call(input, '1');
+    setUntrackedValue.call(input, 'bar');
     input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
 
     expect(called).toBe(2);
