054c9557d88aa58ea6d3f4158c6f602a9cbccd78
Ethan Arrowood <ethan.arrowood@gmail.com>
2017-11-24 01:43:02 -0500

    Updated tests

--- packages/react-dom/src/events/__tests__/ChangeEventPlugin-test.internal.js
+++ packages/react-dom/src/events/__tests__/ChangeEventPlugin-test.internal.js
@@ -11,13 +11,10 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactTestUtils = require('react-dom/test-utils');
 
 function getTrackedValue(elem) {
   return elem.value;
 }
-
-
 
 var setUntrackedChecked = Object.getOwnPropertyDescriptor(
   HTMLInputElement.prototype,
@@ -29,207 +26,204 @@ var setUntrackedValue = Object.getOwnPropertyDescriptor(
   'value',
 ).set;
 
-function getEvent(type, opts) {
-  var event = document.createEvent('Event');
-  event.initEvent(type, true, true);
-  return event;
-}
-
-function dispatchEventOnNode(node, type) {
-  //console.log(`Dispatch ${type}`, node.checked);
-  var res = node.dispatchEvent(
-    new Event( type, {bubbles: true, cancelable: true} ));
-}
-
 describe('ChangeEventPlugin', () => {
-  it('should fire click for checkbox input', () => {
-    var called = 0;
+  var container;
 
-    function cb(e) {
-      called = 1;
-      expect(e.type).toBe('click');
-    }
-
-    var container = document.createElement('div');
-
-    var stub = ReactDOM.render(
-      <input type="checkbox" onClick={cb}/>,
-      container
-    );
-
+  beforeEach(() => {
+    container = document.createElement('div');
     document.body.appendChild(container);
+  });
 
-    var node = ReactDOM.findDOMNode(stub);
-
-    setUntrackedChecked.call(node, false); // track and set to false
-    dispatchEventOnNode(node, 'click'); // make it true
-
-    expect(called).toBe(1); // onClick has been called
-    expect(node.checked).toBe(true); // tracked property is now true
-
+  afterEach(() => {
     document.body.removeChild(container);
+    container = null;
   });
 
-  it('should catch setting the value programmatically', () => {
-    var container = document.createElement('div');
-    var ref = ReactDOM.render(
-      <input type="text" defaultValue="foo" />, container
-    );
-    var input = ReactDOM.findDOMNode(ref);
-    setUntrackedValue.call(input, 'bar');
-
-    expect(getTrackedValue(input)).toBe('bar');
-  });
-
-  it('should not fire change when setting the value programmatically', () => {
+  fit('should fire change for checkbox input', () => {
     var called = 0;
 
     function cb(e) {
-      called += 1;
+      called++;
       expect(e.type).toBe('change');
     }
 
-    var container = document.createElement('div');
-    var ref = ReactDOM.render(
-      <input type="text" onChange={cb} defaultValue="foo" />, container
+    var node = ReactDOM.render(
+      <input type="checkbox" onChange={cb} />,
+      container,
     );
-    var input = ReactDOM.findDOMNode(ref);
+
+    setUntrackedChecked.call(node, false);
+    node.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
+    expect(called).toBe(1);
+    expect(node.checked).toBe(true);
+
+    setUntrackedChecked.call(node, true);
+    node.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
+    expect(called).toBe(2);
+    expect(node.checked).toBe(false);
+  });
+
+  fit('should not fire change setting the value programmatically', () => {
+    var called = 0;
+
+    function cb(e) {
+      called++;
+      expect(e.type).toBe('change');
+    }
+
+    var input = ReactDOM.render(
+      <input type="text" defaultValue="foo" onChange={cb} />,
+      container,
+    );
 
     input.value = 'bar';
-    dispatchEventOnNode(input, 'change');
+    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
     expect(called).toBe(0);
 
     setUntrackedValue.call(input, 'foo');
+    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
+
+    expect(called).toBe(1);
     expect(input.value).toBe('foo');
 
-    dispatchEventOnNode(input, 'change');
+    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
     expect(called).toBe(1);
   });
 
-  it('should not fire change when setting checked programmatically', () => {
+  fit('should not fire change when setting checked programmatically', () => {
     var called = 0;
 
     function cb(e) {
-      called += 1;
+      called++;
       expect(e.type).toBe('change');
     }
 
-    var input = ReactTestUtils.renderIntoDocument(
+    var input = ReactDOM.render(
       <input type="checkbox" onChange={cb} defaultChecked={true} />,
+      container,
     );
 
+    //set programmatically
     input.checked = true;
-    input.dispatchEvent(getEvent('click'));
-    expect(called).toBe(0);
 
     setUntrackedChecked.call(input, false);
-    input.dispatchEvent(getEvent('click'));
+    input.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
+    expect(called).toBe(0);
+
+    setUntrackedChecked.call(input, true);
+    input.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
 
     expect(called).toBe(1);
   });
 
-  it('should unmount', () => {
-    var container = document.createElement('div');
+  fit('should catch setting the value programmatically', () => {
+    var input = ReactDOM.render(
+      <input type="text" defaultValue="foo" />,
+      container,
+    );
+    setUntrackedValue.call(input, 'bar');
+    expect(getTrackedValue(input)).toBe('bar');
+  });
+
+  fit('should unmount', () => {
     var input = ReactDOM.render(<input />, container);
 
     ReactDOM.unmountComponentAtNode(container);
   });
 
-  it('should only fire change for checked radio button once', () => {
+  fit('should only fire change for checked radio button once', () => {
     var called = 0;
 
     function cb(e) {
-      called += 1;
+      called++;
+      expect(e.type).toBe('change');
     }
 
-    var input = ReactTestUtils.renderIntoDocument(
+    var input = ReactDOM.render(
       <input type="radio" onChange={cb} />,
+      container,
     );
+
     setUntrackedChecked.call(input, true);
-    input.dispatchEvent(getEvent('click'));
-    input.dispatchEvent(getEvent('click'));
+    input.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
+    input.dispatchEvent(new Event('click', {bubbles: true, cancelable: true}));
     expect(called).toBe(1);
   });
 
-  it('should deduplicate input value change events', () => {
-    var input;
+  fit('should deduplicate input value change events', () => {
     var called = 0;
 
     function cb(e) {
-      called += 1;
+      called++;
       expect(e.type).toBe('change');
     }
 
-    [
-      <input type="text" onChange={cb} />,
-      <input type="number" onChange={cb} />,
-      <input type="range" onChange={cb} />,
-    ].forEach(function(element) {
-      called = 0;
-      input = ReactTestUtils.renderIntoDocument(element);
-
-      setUntrackedValue.call(input, '40');
-      input.dispatchEvent(getEvent('change'));
-      input.dispatchEvent(getEvent('change'));
-      expect(called).toBe(1);
-
-      called = 0;
-      input = ReactTestUtils.renderIntoDocument(element);
-      setUntrackedValue.call(input, '40');
-      input.dispatchEvent(getEvent('input'));
-      input.dispatchEvent(getEvent('input'));
-      expect(called).toBe(1);
-
-      called = 0;
-      input = ReactTestUtils.renderIntoDocument(element);
-      setUntrackedValue.call(input, '40');
-      input.dispatchEvent(getEvent('input'));
-      input.dispatchEvent(getEvent('change'));
-      expect(called).toBe(1);
-    });
-  });
-
-  it('should listen for both change and input events when supported', () => {
-    var called = 0;
-
-    function cb(e) {
-      called += 1;
-      expect(e.type).toBe('change');
-    }
-
-    var input = ReactTestUtils.renderIntoDocument(
-      <input type="range" onChange={cb} />,
-    );
-    setUntrackedValue.call(input, 'bar');
-
-    input.dispatchEvent(getEvent('input'));
+    var input = ReactDOM.render(<input type="text" onChange={cb} />, container);
 
     setUntrackedValue.call(input, 'foo');
 
-    input.dispatchEvent(getEvent('change'));
+    input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
+    input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
+
+    expect(called).toBe(1);
+
+    setUntrackedValue.call(input, 'bar');
+
+    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
+    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
+
+    expect(called).toBe(2);
+
+    setUntrackedValue.call(input, 'foobar');
+
+    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
+    input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
+
+    expect(called).toBe(3);
+  });
+
+  fit('should listen for both change and input events when supported', () => {
+    var called = 0;
+
+    function cb(e) {
+      called++;
+      expect(e.type).toBe('change');
+    }
+
+    var input = ReactDOM.render(
+      <input type="range" onChange={cb} />,
+      container,
+    );
+
+    setUntrackedValue.call(input, '0');
+    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
+
+    setUntrackedValue.call(input, '1');
+    input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
 
     expect(called).toBe(2);
   });
 
-  it('should only fire events when the value changes for range inputs', () => {
+  fit('should only fire events when the value changes for range inputs', () => {
     var called = 0;
 
     function cb(e) {
-      called += 1;
+      called++;
       expect(e.type).toBe('change');
     }
 
-    var input = ReactTestUtils.renderIntoDocument(
+    var input = ReactDOM.render(
       <input type="range" onChange={cb} />,
+      container,
     );
     setUntrackedValue.call(input, '40');
-    input.dispatchEvent(getEvent('input'));
-    input.dispatchEvent(getEvent('change'));
+    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
+    input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
 
     setUntrackedValue.call(input, 'foo');
+    input.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
+    input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
 
-    input.dispatchEvent(getEvent('input'));
-    input.dispatchEvent(getEvent('change'));
     expect(called).toBe(2);
   });
 });
