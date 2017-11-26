c56595fe85ffd39bb3b9934ef6639b47d79940df
HeroProtagonist <jstepper91@gmail.com>
2017-11-26 14:14:44 -0500

    Add function to validate warnings

--- scripts/eslint/index.js
+++ scripts/eslint/index.js
@@ -11,9 +11,22 @@ const CLIEngine = require('eslint').CLIEngine;
 const cli = new CLIEngine();
 const formatter = cli.getFormatter();
 
-module.exports = function lintOnFiles(filePatterns) {
+function lintOnFiles(filePatterns) {
   const report = cli.executeOnFiles(filePatterns);
   const formattedResults = formatter(report.results);
   console.log(formattedResults);
   return report;
+}
+
+function validWarnings(report) {
+  const {warningCount, results} = report;
+  const ignoreWanings = results.filter(
+    ({messages}) => messages[0] && messages[0].message.includes('File ignored')
+  );
+  return warningCount > 0 ? warningCount !== ignoreWanings.length : false;
+}
+
+module.exports = {
+  lintOnFiles,
+  validWarnings,
 };
