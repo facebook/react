51405fcacf4e25cf3cf6f3a3ab3a0d785b81ca32
Adrian Carolli <adrian.carolli1@gmail.com>
2017-11-26 10:38:32 -0500

    Seperate bundle specific tests
    - Moved the runYarnTask into utils since its being used two files now
    - Uncomment out checks I mistakenly committed

--- scripts/release/build-commands/run-automated-tests.js
+++ scripts/release/build-commands/run-automated-tests.js
@@ -2,30 +2,10 @@
 
 'use strict';
 
-const chalk = require('chalk');
-const {exec} = require('child-process-promise');
-const {logPromise} = require('../utils');
-
-const runYarnTask = async (cwd, task, errorMessage) => {
-  try {
-    await exec(`yarn ${task}`, {cwd});
-  } catch (error) {
-    throw Error(
-      chalk`
-      ${errorMessage}
-
-      {white ${error.stdout}}
-    `
-    );
-  }
-};
+const {logPromise, runYarnTask} = require('../utils');
 
 module.exports = async ({cwd}) => {
   await logPromise(runYarnTask(cwd, 'lint', 'Lint failed'), 'Running ESLint');
-  await logPromise(
-    runYarnTask(cwd, 'lint-build', 'Lint bundle failed'),
-    'Running ESLint on bundle'
-  );
   await logPromise(
     runYarnTask(cwd, 'flow', 'Flow failed'),
     'Running Flow checks'
@@ -38,24 +18,6 @@ module.exports = async ({cwd}) => {
   await logPromise(
     runYarnTask(cwd, 'test-prod', 'Jest tests failed in production'),
     'Running Jest tests in the production environment',
-    true
-  );
-  await logPromise(
-    runYarnTask(
-      cwd,
-      'test-build',
-      'Jest tests on the bundle failed in development'
-    ),
-    'Running Jest tests on the bundle in the development environment',
-    true
-  );
-  await logPromise(
-    runYarnTask(
-      cwd,
-      'test-build-prod',
-      'Jest tests on the bundle failed in production'
-    ),
-    'Running Jest tests on the bundle in the production environment',
     true
   );
 };
--- scripts/release/build.js
+++ scripts/release/build.js
@@ -12,36 +12,38 @@ const run = async () => {
 
   const addGitTag = require('./build-commands/add-git-tag');
   const buildArtifacts = require('./build-commands/build-artifacts');
-  // const checkCircleCiStatus = require('./build-commands/check-circle-ci-status');
-  // const checkEnvironmentVariables = require('./build-commands/check-environment-variables');
-  // const checkNpmPermissions = require('./build-commands/check-npm-permissions');
-  // const checkPackageDependencies = require('./build-commands/check-package-dependencies');
-  // const checkUncommittedChanges = require('./build-commands/check-uncommitted-changes');
-  // const installYarnDependencies = require('./build-commands/install-yarn-dependencies');
+  const checkCircleCiStatus = require('./build-commands/check-circle-ci-status');
+  const checkEnvironmentVariables = require('./build-commands/check-environment-variables');
+  const checkNpmPermissions = require('./build-commands/check-npm-permissions');
+  const checkPackageDependencies = require('./build-commands/check-package-dependencies');
+  const checkUncommittedChanges = require('./build-commands/check-uncommitted-changes');
+  const installYarnDependencies = require('./build-commands/install-yarn-dependencies');
   const parseBuildParameters = require('./build-commands/parse-build-parameters');
   const printPostBuildSummary = require('./build-commands/print-post-build-summary');
   const runAutomatedTests = require('./build-commands/run-automated-tests');
-  // const updateGit = require('./build-commands/update-git');
+  const runAutomatedBundleTests = require('./build-commands/run-automated-bundle-tests');
+  const updateGit = require('./build-commands/update-git');
   const updatePackageVersions = require('./build-commands/update-package-versions');
-  // const updateYarnDependencies = require('./build-commands/update-yarn-dependencies');
-  // const validateVersion = require('./build-commands/validate-version');
+  const updateYarnDependencies = require('./build-commands/update-yarn-dependencies');
+  const validateVersion = require('./build-commands/validate-version');
 
   try {
     const params = parseBuildParameters();
     params.packages = getPublicPackages();
 
-    // await checkEnvironmentVariables(params);
-    // await validateVersion(params);
-    // await checkUncommittedChanges(params);
-    // await checkNpmPermissions(params);
-    // await updateGit(params);
-    // await checkCircleCiStatus(params);
-    // await installYarnDependencies(params);
-    // await checkPackageDependencies(params);
-    // await updateYarnDependencies(params);
+    await checkEnvironmentVariables(params);
+    await validateVersion(params);
+    await checkUncommittedChanges(params);
+    await checkNpmPermissions(params);
+    await updateGit(params);
+    await checkCircleCiStatus(params);
+    await installYarnDependencies(params);
+    await checkPackageDependencies(params);
+    await updateYarnDependencies(params);
     await runAutomatedTests(params);
     await updatePackageVersions(params);
     await buildArtifacts(params);
+    await runAutomatedBundleTests(params);
     await addGitTag(params);
     await printPostBuildSummary(params);
   } catch (error) {
