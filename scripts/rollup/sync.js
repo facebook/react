a65a8abc65cd31c862f5e83eb24a8dc68458b521
Dan Abramov <dan.abramov@gmail.com>
2017-11-27 17:57:28 +0000

    Use async/await in Rollup scripts (#11669)

--- scripts/rollup/sync.js
+++ scripts/rollup/sync.js
@@ -11,18 +11,14 @@ const RELATIVE_RN_CS_PATH = 'xplat/js/RKJSModules/Libraries/CS/downstream/';
 const RELATIVE_RN_RT_PATH = 'xplat/js/RKJSModules/Libraries/RT/downstream/';
 const RELATIVE_WWW_PATH = 'html/shared/react/';
 
-function doSync(buildPath, destPath) {
+async function doSync(buildPath, destPath) {
   console.log(`${chalk.bgYellow.black(' SYNCING ')} React to ${destPath}`);
 
-  const promise = asyncCopyTo(buildPath, destPath);
-  promise.then(() => {
-    console.log(`${chalk.bgGreen.black(' SYNCED ')} React to ${destPath}`);
-  });
-
-  return promise;
+  await asyncCopyTo(buildPath, destPath);
+  console.log(`${chalk.bgGreen.black(' SYNCED ')} React to ${destPath}`);
 }
 
-function syncReactDom(buildPath, wwwPath) {
+async function syncReactDom(buildPath, wwwPath) {
   wwwPath = typeof wwwPath === 'string' ? wwwPath : DEFAULT_WWW_PATH;
 
   if (wwwPath.charAt(wwwPath.length - 1) !== '/') {
@@ -30,11 +26,14 @@ function syncReactDom(buildPath, wwwPath) {
   }
 
   const destPath = resolvePath(wwwPath + RELATIVE_WWW_PATH);
-
-  return doSync(buildPath, destPath);
+  await doSync(buildPath, destPath);
 }
 
-function syncReactNativeHelper(buildPath, fbSourcePath, relativeDestPath) {
+async function syncReactNativeHelper(
+  buildPath,
+  fbSourcePath,
+  relativeDestPath
+) {
   fbSourcePath =
     typeof fbSourcePath === 'string' ? fbSourcePath : DEFAULT_FB_SOURCE_PATH;
 
@@ -43,20 +42,19 @@ function syncReactNativeHelper(buildPath, fbSourcePath, relativeDestPath) {
   }
 
   const destPath = resolvePath(fbSourcePath + relativeDestPath);
-
-  return doSync(buildPath, destPath);
+  await doSync(buildPath, destPath);
 }
 
-function syncReactNative(buildPath, fbSourcePath) {
-  return syncReactNativeHelper(buildPath, fbSourcePath, RELATIVE_RN_PATH);
+async function syncReactNative(buildPath, fbSourcePath) {
+  await syncReactNativeHelper(buildPath, fbSourcePath, RELATIVE_RN_PATH);
 }
 
-function syncReactNativeCS(buildPath, fbSourcePath) {
-  return syncReactNativeHelper(buildPath, fbSourcePath, RELATIVE_RN_CS_PATH);
+async function syncReactNativeCS(buildPath, fbSourcePath) {
+  await syncReactNativeHelper(buildPath, fbSourcePath, RELATIVE_RN_CS_PATH);
 }
 
-function syncReactNativeRT(buildPath, fbSourcePath) {
-  return syncReactNativeHelper(buildPath, fbSourcePath, RELATIVE_RN_RT_PATH);
+async function syncReactNativeRT(buildPath, fbSourcePath) {
+  await syncReactNativeHelper(buildPath, fbSourcePath, RELATIVE_RN_RT_PATH);
 }
 
 module.exports = {
