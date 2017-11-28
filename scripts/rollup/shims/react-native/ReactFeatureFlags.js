f1ba9fdcfba7a214796868d91c33ed71bc137ce1
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-11-28 14:33:36 -0800

    Added ReactFeatureFlags shim for React Native

---
+++ scripts/rollup/shims/react-native/ReactFeatureFlags.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule ReactFeatureFlags
+ * @flow
+ * @format
+ */
+
+'use strict';
+
+var ReactFeatureFlags = {
+  debugRenderPhaseSideEffects: false,
+};
+
+module.exports = ReactFeatureFlags;
