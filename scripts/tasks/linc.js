7788bcdb26844a19c23d0f6d5ab4d20822e59c85
Jordan Tepper <jstepper91@gmail.com>
2017-11-28 08:54:46 -0500

    Do not fail `yarn linc` for ignored file warning (#11615) (#11641)
    * Add rule to ignore default handling of not linting hidden files
    
    * Undo changes
    
    * Add function to validate warnings
    
    * Use validateWarnings when reporting linc command
    
    * Restore files
    
    * Contain code to line file

--- scripts/tasks/linc.js
+++ scripts/tasks/linc.js
@@ -14,9 +14,17 @@ const changedFiles = [...listChangedFiles()];
 const jsFiles = changedFiles.filter(file => file.match(/.js$/g));
 
 const report = lintOnFiles(jsFiles);
-if (report.errorCount > 0 || report.warningCount > 0) {
+if (report.errorCount > 0 || getSourceCodeWarnings(report)) {
   console.log('Lint failed for changed files.');
   process.exit(1);
 } else {
   console.log('Lint passed for changed files.');
+}
+
+// Prevents failing if the only warnings are about ignored files (#11615)
+function getSourceCodeWarnings({warningCount, results}) {
+  const ignoreWanings = results.filter(
+    ({messages}) => messages[0] && messages[0].message.includes('File ignored')
+  );
+  return warningCount > 0 ? warningCount !== ignoreWanings.length : false;
 }
