854481a1e4ef5e972cc79bece6bc0fdd20cf7952
Dan Abramov <dan.abramov@gmail.com>
2017-11-30 20:54:19 +0000

    Inline HTML and SVG configs into DOMProperty

--- packages/react-dom/src/server/ReactDOMServerBrowser.js
+++ packages/react-dom/src/server/ReactDOMServerBrowser.js
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import '../shared/ReactDOMInjection';
 import ReactVersion from 'shared/ReactVersion';
 import invariant from 'fbjs/lib/invariant';
 
