7fc10aa1bd6d9b204ea51106adf6e126290eb632
Dan Abramov <dan.abramov@gmail.com>
2017-11-30 11:33:37 +0000

    Move fork config into its own file

--- scripts/rollup/modules.js
+++ scripts/rollup/modules.js
@@ -1,79 +1,11 @@
 'use strict';
 
 const path = require('path');
+const forks = require('./forks');
 const bundleTypes = require('./bundles').bundleTypes;
 
 const UMD_DEV = bundleTypes.UMD_DEV;
 const UMD_PROD = bundleTypes.UMD_PROD;
-const FB_DEV = bundleTypes.FB_DEV;
-const FB_PROD = bundleTypes.FB_PROD;
-
-// If you need to replace a file with another file for a specific environment,
-// add it to this list with the logic for choosing the right replacement.
-const forkedModules = Object.freeze({
-  // Optimization: for UMDs, use object-assign polyfill that is already a part
-  // of the React package instead of bundling it again.
-  'object-assign': (bundleType, entry) => {
-    if (bundleType !== UMD_DEV && bundleType !== UMD_PROD) {
-      // It's only relevant for UMD bundles since that's where the duplication
-      // happens. Other bundles just require('object-assign') anyway.
-      return null;
-    }
-    if (getDependencies(bundleType, entry).indexOf('react') === -1) {
-      // We can only apply the optimizations to bundle that depend on React
-      // because we read assign() from an object exposed on React internals.
-      return null;
-    }
-    // We can use the fork!
-    return 'shared/forks/object-assign.umd.js';
-  },
-
-  // We have a few forks for different environments.
-  'shared/ReactFeatureFlags': (bundleType, entry) => {
-    switch (entry) {
-      case 'react-native-renderer':
-        return 'shared/forks/ReactFeatureFlags.native.js';
-      case 'react-cs-renderer':
-        return 'shared/forks/ReactFeatureFlags.native-cs.js';
-      default:
-        switch (bundleType) {
-          case FB_DEV:
-          case FB_PROD:
-            return 'shared/forks/ReactFeatureFlags.www.js';
-        }
-    }
-    return null;
-  },
-
-  // This logic is forked on www to blacklist warnings.
-  'shared/lowPriorityWarning': (bundleType, entry) => {
-    switch (bundleType) {
-      case FB_DEV:
-      case FB_PROD:
-        return 'shared/forks/lowPriorityWarning.www.js';
-      default:
-        return null;
-    }
-  },
-
-  // In FB bundles, we preserve an inline require to ReactCurrentOwner.
-  // See the explanation in FB version of ReactCurrentOwner in www:
-  'react/src/ReactCurrentOwner': (bundleType, entry) => {
-    switch (bundleType) {
-      case FB_DEV:
-      case FB_PROD:
-        return 'react/src/forks/ReactCurrentOwner.www.js';
-      default:
-        return null;
-    }
-  },
-});
-
-// Bundles exporting globals that other modules rely on.
-const knownGlobals = Object.freeze({
-  react: 'React',
-  'react-dom': 'ReactDOM',
-});
 
 // For any external that is used in a DEV-only condition, explicitly
 // specify whether it has side effects during import or not. This lets
@@ -87,6 +19,12 @@ const importSideEffects = Object.freeze({
   'fbjs/lib/camelizeStyleName': HAS_NO_SIDE_EFFECTS_ON_IMPORT,
   'fbjs/lib/hyphenateStyleName': HAS_NO_SIDE_EFFECTS_ON_IMPORT,
   deepFreezeAndThrowOnMutationInDev: HAS_NO_SIDE_EFFECTS_ON_IMPORT,
+});
+
+// Bundles exporting globals that other modules rely on.
+const knownGlobals = Object.freeze({
+  react: 'React',
+  'react-dom': 'ReactDOM',
 });
 
 // Given ['react'] in bundle externals, returns { 'react': 'React' }.
@@ -120,15 +58,16 @@ function getDependencies(bundleType, entry) {
 
 // Hijacks some modules for optimization and integration reasons.
 function getForks(bundleType, entry) {
-  const forks = {};
-  Object.keys(forkedModules).forEach(srcModule => {
-    const targetModule = forkedModules[srcModule](bundleType, entry);
+  const forksForBundle = {};
+  Object.keys(forks).forEach(srcModule => {
+    const dependencies = getDependencies(bundleType, entry);
+    const targetModule = forks[srcModule](bundleType, entry, dependencies);
     if (targetModule === null) {
       return;
     }
-    forks[srcModule] = targetModule;
+    forksForBundle[srcModule] = targetModule;
   });
-  return forks;
+  return forksForBundle;
 }
 
 function getImportSideEffects() {
