cd30dc6ef515729445f2ca0ba83b54809ec7e302
Dan Abramov <dan.abramov@gmail.com>
2017-12-01 18:59:10 +0000

    Add a link to the bug URL

--- packages/react-reconciler/src/ReactFiber.js
+++ packages/react-reconciler/src/ReactFiber.js
@@ -44,7 +44,8 @@ if (__DEV__) {
     var testMap = new Map([[nonExtensibleObject, null]]);
     var testSet = new Set([nonExtensibleObject]);
     // This is necessary for Rollup to not consider these unused.
-    // TODO: report this as a Rollup bug.
+    // https://github.com/rollup/rollup/issues/1771
+    // TODO: we can remove these if Rollup fixes the bug.
     testMap.set(0, 0);
     testSet.add(0);
   } catch (e) {
