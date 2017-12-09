f23dd7150f270c843707f1a06ac12a6af63b7320
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-12-09 18:28:26 -0500

    Remove unused wheel event detection in isEventSupported (#11822)

--- packages/react-dom/src/events/isEventSupported.js
+++ packages/react-dom/src/events/isEventSupported.js
@@ -7,16 +7,6 @@
 
 import ExecutionEnvironment from 'fbjs/lib/ExecutionEnvironment';
 
-let useHasFeature;
-if (ExecutionEnvironment.canUseDOM) {
-  useHasFeature =
-    document.implementation &&
-    document.implementation.hasFeature &&
-    // always returns true in newer browsers as per the standard.
-    // @see http://dom.spec.whatwg.org/#dom-domimplementation-hasfeature
-    document.implementation.hasFeature('', '') !== true;
-}
-
 /**
  * Checks if an event is supported in the current execution environment.
  *
@@ -46,11 +36,6 @@ function isEventSupported(eventNameSuffix, capture) {
     const element = document.createElement('div');
     element.setAttribute(eventName, 'return;');
     isSupported = typeof element[eventName] === 'function';
-  }
-
-  if (!isSupported && useHasFeature && eventNameSuffix === 'wheel') {
-    // This is the only way to test support for the `wheel` event in IE9+.
-    isSupported = document.implementation.hasFeature('Events.wheel', '3.0');
   }
 
   return isSupported;
