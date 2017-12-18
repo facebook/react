247c5241706356033cd5bf1dc31197e058853e13
Dan Abramov <dan.abramov@gmail.com>
2017-12-18 18:56:38 +0000

    Update results.json from master before the build (#11882)

--- scripts/circleci/build.sh
+++ scripts/circleci/build.sh
@@ -2,6 +2,11 @@
 		
 set -e		
 
+# Update the local size measurements to the master version
+# so that the size diff printed at the end of the build is
+# accurate.
+curl -o scripts/rollup/results.json http://react.zpao.com/builds/master/latest/results.json
+
 yarn build --extract-errors
 # Note: since we run the full build including extracting error codes,
 # it is important that we *don't* reset the change to `scripts/error-codes/codes.json`.
