68fc971eef7e0cb5d5075730a70f34669046fd69
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-12-19 15:12:56 -0800

    More robustly handle unexpected warnings within try/catch

--- scripts/jest/setupTests.js
+++ scripts/jest/setupTests.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const chalk = require('chalk');
+
 if (process.env.REACT_CLASS_EQUIVALENCE_TEST) {
   // Inside the class equivalence tester, we have a custom environment, let's
   // require that instead.
@@ -60,27 +62,56 @@ if (process.env.REACT_CLASS_EQUIVALENCE_TEST) {
   });
 
   ['error', 'warn'].forEach(methodName => {
-    const oldMethod = console[methodName];
-    const newMethod = function(...args) {
-      oldMethod(...args);
-
-      throw new Error(
-        `Expected test not to call console.${methodName}(). ` +
-          'If the warning is expected, mock it out using ' +
-          `spyOnDev(console, '${methodName}') or spyOnProd(console, '${
-            methodName
-          }'), ` +
-          'and test that the warning occurs.'
-      );
+    const unexpectedConsoleCallStacks = [];
+    const newMethod = function(message) {
+      // Capture the call stack now so we can warn about it later.
+      // The call stack has helpful information for the test author.
+      // Don't throw yet though b'c it might be accidentally caught and suppressed.
+      const stack = new Error().stack;
+      unexpectedConsoleCallStacks.push([
+        stack.substr(stack.indexOf('\n') + 1),
+        message,
+      ]);
     };
 
     console[methodName] = newMethod;
+
+    env.beforeEach(() => {
+      unexpectedConsoleCallStacks.splice(0);
+    });
 
     env.afterEach(() => {
       if (console[methodName] !== newMethod && !isSpy(console[methodName])) {
         throw new Error(
           `Test did not tear down console.${methodName} mock properly.`
         );
+      }
+
+      if (unexpectedConsoleCallStacks.length > 0) {
+        const messages = unexpectedConsoleCallStacks.map(
+          ([stack, message]) =>
+            `${chalk.red(message)}\n` +
+            `${stack
+              .split('\n')
+              .map(line => chalk.gray(line))
+              .join('\n')}`
+        );
+
+        const message =
+          `Expected test not to call ${chalk.bold(
+            `console.${methodName}()`
+          )}.\n\n` +
+          'If the warning is expected, test for it explicitly by:\n' +
+          `1. Using the ${chalk.bold('.toWarnDev()')} / ${chalk.bold(
+            '.toLowPriorityWarnDev()'
+          )} matchers, or...\n` +
+          `2. Mock it out using ${chalk.bold('spyOnDev')}(console, '${
+            methodName
+          }') or ${chalk.bold('spyOnProd')}(console, '${
+            methodName
+          }'), and test that the warning occurs.`;
+
+        throw new Error(`${message}\n\n${messages.join('\n\n')}`);
       }
     });
   });
