ac836040bcc9617f898905421e3f519b467bec21
Dan Abramov <dan.abramov@gmail.com>
2018-01-03 15:10:43 +0000

    Prevent jsdom from logging intentionally thrown errors
    This relies on our existing special field that we use to mute errors.
    Perhaps, it would be better to instead rely on preventDefault() directly.
    I outlined a possible strategy here: https://github.com/facebook/react/issues/11098#issuecomment-355032539

--- scripts/jest/setupEnvironment.js
+++ scripts/jest/setupEnvironment.js
@@ -31,6 +31,14 @@ global.cancelIdleCallback = function(callbackID) {
 // for the few that specifically test the logging by shadowing this
 // property. In real apps, it would usually not be defined at all.
 Error.prototype.suppressReactErrorLogging = true;
-if (typeof DOMException === 'function') {
+if (typeof window !== 'undefined') {
   DOMException.prototype.suppressReactErrorLogging = true;
+  // Also prevent JSDOM from logging intentionally thrown errors.
+  // TODO: it might make sense to do it the other way around.
+  // https://github.com/facebook/react/issues/11098#issuecomment-355032539
+  window.addEventListener('error', event => {
+    if (event.error != null && event.error.suppressReactErrorLogging) {
+      event.preventDefault();
+    }
+  });
 }
