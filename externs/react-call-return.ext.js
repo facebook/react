67b2f3b015d33dfa4b6d2841982098d7d09762a8
Shrey Banga <shrey@quip.com>
2018-01-06 00:16:24 -0800

    Add externs for react-call-return

---
+++ externs/react-call-return.ext.js
@@ -0,0 +1,8 @@
+/** @suppress {duplicate} */
+var unstable_createCall;
+/** @suppress {duplicate} */
+var unstable_createReturn;
+/** @suppress {duplicate} */
+var unstable_isCall;
+/** @suppress {duplicate} */
+var unstable_isReturn;
