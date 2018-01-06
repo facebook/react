0f6dca5f019dfa18da9d995d7701b0b93c7aec86
Shrey Banga <shrey@quip.com>
2018-01-05 19:30:06 -0800

    Add externs for names used from fbjs
    These are treated as external dependencies in node bundles, so have to prevent
    renaming them. We should eventually still rename these in UMD bundles.

---
+++ externs/fbjs.ext.js
@@ -0,0 +1,29 @@
+/** @suppress {duplicate} */
+var ExecutionEnvironment;
+/** @suppress {duplicate} */
+var TouchEventUtils;
+/** @suppress {duplicate} */
+var camelizeStyleName;
+/** @suppress {duplicate} */
+var containsNode;
+/** @suppress {duplicate} */
+var emptyFunction;
+emptyFunction.thatReturns;
+emptyFunction.thatReturnsNull;
+emptyFunction.thatReturnsTrue;
+emptyFunction.thatReturnsFalse;
+emptyFunction.thatReturnsArgument;
+/** @suppress {duplicate} */
+var emptyObject;
+/** @suppress {duplicate} */
+var getActiveElement;
+/** @suppress {duplicate} */
+var hyphenateStyleName;
+/** @suppress {duplicate} */
+var invariant;
+/** @suppress {duplicate} */
+var memoizeStringOnly;
+/** @suppress {duplicate} */
+var shallowEqual;
+/** @suppress {duplicate} */
+var warning;
