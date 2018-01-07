6fb8cce0b788a78bee2fb4debe394efed9aef006
Shrey Banga <shrey@quip.com>
2018-01-06 19:47:45 -0800

    Clean-up ReactDOM externs
    Removed unnecessary annotations and updated the externs for React 16

--- externs/react-dom.ext.js
+++ externs/react-dom.ext.js
@@ -1,77 +1,17 @@
-/**
- * The ReactDOM global object.
- *
- * @type {!Object}
- * @const
- * @suppress {duplicate}
- */
+/** @suppress {duplicate} */
 var ReactDOM = {};
-
-/**
- * The current version of ReactDOM.
- *
- * @type {string}
- * @const
- */
 ReactDOM.version;
-
-/**
- * @param {React.Component} container
- * @param {Element} mountPoint
- * @param {Function=} opt_callback
- * @return {React.Component}
- */
-ReactDOM.render = function(container, mountPoint, opt_callback) {};
-
-/**
- * @param {Element} container
- * @return {boolean}
- */
-ReactDOM.unmountComponentAtNode = function(container) {};
-
-/**
- * @param {React.Component} component
- * @return {Element}
- */
-ReactDOM.findDOMNode = function(component) {};
-
-/**
- * @param {Function} callback Function which calls `setState`, `forceUpdate`, etc.
- * @param {*=} opt_a Optional argument to pass to the callback.
- * @param {*=} opt_b Optional argument to pass to the callback.
- * @param {*=} opt_c Optional argument to pass to the callback.
- * @param {*=} opt_d Optional argument to pass to the callback.
- * @param {*=} opt_e Optional argument to pass to the callback.
- * @param {*=} opt_f Optional argument to pass to the callback.
- */
-ReactDOM.unstable_batchedUpdates = function(
-  callback,
-  opt_a,
-  opt_b,
-  opt_c,
-  opt_d,
-  opt_e,
-  opt_f
-) {};
-
+ReactDOM.findDOMNode;
+ReactDOM.hydrate;
+ReactDOM.render;
+ReactDOM.unstable_renderSubtreeIntoContainer;
+ReactDOM.unmountComponentAtNode;
+ReactDOM.createPortal;
+ReactDOM.unstable_createPortal;
+ReactDOM.unstable_batchedUpdates;
 ReactDOM.unstable_deferredUpdates;
-
-/**
- * @param {React.Component} parentComponent The conceptual parent of this render tree.
- * @param {React.ReactElement} nextElement Component element to render.
- * @param {Element} container DOM element to render into.
- * @param {Function=} opt_callback function triggered on completion
- * @return {React.Component} Component instance rendered in `container`.
- */
-ReactDOM.unstable_renderSubtreeIntoContainer = function(
-  parentComponent,
-  nextElement,
-  container,
-  opt_callback
-) {};
-
-ReactDOM.hydrate = function() {};
-ReactDOM.createPortal = function() {};
+ReactDOM.flushSync;
+ReactDOM.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 
 /** @suppress {duplicate} */
 var ReactDOMServer = {};
