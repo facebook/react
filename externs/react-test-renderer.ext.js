23ef5a63f74f7dba95ec77ae17024124b568d17e
Shrey Banga <shrey@quip.com>
2018-01-06 21:04:58 -0800

    Add externs for test-renderer

---
+++ externs/react-test-renderer.ext.js
@@ -0,0 +1,12 @@
+/** @suppress {duplicate} */
+var ReactShallowRenderer;
+ReactShallowRenderer.createRenderer;
+ReactShallowRenderer.prototype.getMountedInstance;
+ReactShallowRenderer.prototype.getRenderedOutput;
+ReactShallowRenderer.prototype.render;
+ReactShallowRenderer.prototype.unmount;
+
+/** @suppress {duplicate} */
+var ReactTestRendererFiber;
+ReactTestRendererFiber.prototype.create;
+ReactTestRendererFiber.prototype.unstable_batchedUpdates;
