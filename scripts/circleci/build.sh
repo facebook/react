56379cb3c1329512e4fdc367d8c751e91809ad88
Orta Therox <orta.therox@gmail.com>
2018-01-14 18:57:59 -0500

    Remove the --extract-errors on the circle build

--- scripts/circleci/build.sh
+++ scripts/circleci/build.sh
@@ -7,7 +7,7 @@ set -e
 # accurate.
 curl -o scripts/rollup/results.json http://react.zpao.com/builds/master/latest/results.json
 
-yarn build --extract-errors
+yarn build
 # Note: since we run the full build including extracting error codes,
 # it is important that we *don't* reset the change to `scripts/error-codes/codes.json`.
 # When production bundle tests run later, it needs to be available.
