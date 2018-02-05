ad07be755d924191905b4995d1183492fb5e0401
Brian Vaughn <brian.david.vaughn@gmail.com>
2018-02-04 17:06:14 -0800

    Release script does a fresh Yarn install of deps (#12149)
    This would have caught the recent Yarn workspaces / semver issue sooner.

--- scripts/release/build-commands/install-yarn-dependencies.js
+++ scripts/release/build-commands/install-yarn-dependencies.js
@@ -5,7 +5,10 @@
 const {exec} = require('child-process-promise');
 const {logPromise} = require('../utils');
 
-const install = async ({cwd}) => await exec('yarn', {cwd});
+const install = async ({cwd}) => {
+  await exec('rm -rf node_modules', {cwd});
+  await exec('yarn', {cwd});
+};
 
 module.exports = async ({cwd}) => {
   return logPromise(install({cwd}), 'Installing NPM dependencies');
