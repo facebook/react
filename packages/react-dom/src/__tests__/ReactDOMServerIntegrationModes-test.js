7062b75636668ead78d4edb9d1ecfb1494f90dcb
C.T. Lin <chentsulin@gmail.com>
2018-02-07 19:38:08 +0800

    Merge StrictMode and AsyncMode tests into Modes file

--- packages/react-dom/src/__tests__/ReactDOMServerIntegrationModes-test.js
+++ packages/react-dom/src/__tests__/ReactDOMServerIntegrationModes-test.js
@@ -36,6 +36,74 @@ describe('ReactDOMServerIntegration', () => {
     resetModules();
   });
 
+  describe('React.StrictMode', () => {
+    itRenders('a strict mode with one child', async render => {
+      let e = await render(
+        <React.StrictMode>
+          <div>text1</div>
+        </React.StrictMode>,
+      );
+      let parent = e.parentNode;
+      expect(parent.childNodes[0].tagName).toBe('DIV');
+    });
+
+    itRenders('a strict mode with several children', async render => {
+      let Header = props => {
+        return <p>header</p>;
+      };
+      let Footer = props => {
+        return (
+          <React.StrictMode>
+            <h2>footer</h2>
+            <h3>about</h3>
+          </React.StrictMode>
+        );
+      };
+      let e = await render(
+        <React.StrictMode>
+          <div>text1</div>
+          <span>text2</span>
+          <Header />
+          <Footer />
+        </React.StrictMode>,
+      );
+      let parent = e.parentNode;
+      expect(parent.childNodes[0].tagName).toBe('DIV');
+      expect(parent.childNodes[1].tagName).toBe('SPAN');
+      expect(parent.childNodes[2].tagName).toBe('P');
+      expect(parent.childNodes[3].tagName).toBe('H2');
+      expect(parent.childNodes[4].tagName).toBe('H3');
+    });
+
+    itRenders('a nested strict mode', async render => {
+      let e = await render(
+        <React.StrictMode>
+          <React.StrictMode>
+            <div>text1</div>
+          </React.StrictMode>
+          <span>text2</span>
+          <React.StrictMode>
+            <React.StrictMode>
+              <React.StrictMode>
+                {null}
+                <p />
+              </React.StrictMode>
+              {false}
+            </React.StrictMode>
+          </React.StrictMode>
+        </React.StrictMode>,
+      );
+      let parent = e.parentNode;
+      expect(parent.childNodes[0].tagName).toBe('DIV');
+      expect(parent.childNodes[1].tagName).toBe('SPAN');
+      expect(parent.childNodes[2].tagName).toBe('P');
+    });
+
+    itRenders('an empty strict mode', async render => {
+      expect(await render(<React.StrictMode />)).toBe(null);
+    });
+  });
+
   describe('React.unstable_AsyncMode', () => {
     itRenders('an async mode with one child', async render => {
       let e = await render(
