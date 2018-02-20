7bae7f86917e209705c5ecfcd805fa94504d5310
Brandon Dail <brandondail@fb.com>
2018-02-20 09:51:34 -0800

    Implement test suite

--- packages/request-idle-callback-polyfill/src/__tests__/RequestIdleCallback-test.js
+++ packages/request-idle-callback-polyfill/src/__tests__/RequestIdleCallback-test.js
@@ -3,39 +3,141 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- * 
- * @desc adopted from w3c/web-platform-tests
- * @see https://github.com/w3c/web-platform-tests/tree/d977904a0af38c7e2d28c6e1327fb437c2b2e0da/requestidlecallback
  *
  * @emails react-core
  */
 
 'use strict';
 
+let highResolutionTimer;
 let requestIdleCallback;
+let cancelIdleCallback;
+let animationFrameCallbacks;
+
+// Overwritten global methods
+let previousPostMessage;
+let previousRAF;
+
+function performanceNow() {
+  return highResolutionTimer;
+}
+
+/**
+ * A synchronous version of jsdom's postMessage. Meant
+ * to work with mockRunNextFrame.
+ */
+function postMessage(message, targetOrign) {
+  const event = new MessageEvent('message', {data: message});
+  event.initEvent('message', false, false);
+  // MessageEvent.source is defined as read-only and null in jsdom.
+  // Override the getter so the event.source check doesn't cause early
+  // returns in idleTick.
+  Object.defineProperty(event, 'source', {
+    value: window,
+  });
+  window.dispatchEvent(event);
+}
+
+function requestAnimationFrame(callback) {
+  animationFrameCallbacks.push(callback);
+}
+
+function mockRunNextFrame() {
+  const callbacksToRun = animationFrameCallbacks.slice();
+  const animationFrameStart = highResolutionTimer++;
+  animationFrameCallbacks.length = 0;
+  callbacksToRun.forEach(cb => cb(animationFrameStart));
+}
+
+function mockLongRunningCode() {
+  highResolutionTimer += 100;
+}
 
 describe('RequestIdleCallback', () => {
-  beforeEach(() => {
-    jest.resetModules();
+  beforeAll(() => {
+    // When error supression is enabled, jest is not reporting expect failures
+    // inside of idle callbacks.
+    Error.prototype.suppressReactErrorLogging = false;
+    previousRAF = window.requestAnimationFrame;
+    previousPostMessage = window.postMessage;
+    window.postMessage = postMessage;
+    window.performance = {now: performanceNow};
+    window.requestAnimationFrame = requestAnimationFrame;
+  });
 
-    requestIdleCallback = require('request-idle-callback-polyfill');
+  afterAll(() => {
+    window.requestAnimationFrame = previousRAF;
+    window.postMessage = previousPostMessage;
+    previousPostMessage = null;
+    previousRAF = null;
+    delete window.performance;
+  });
+
+  beforeEach(() => {
+    animationFrameCallbacks = [];
+    highResolutionTimer = 0xf000;
+    jest.resetModules();
+    requestIdleCallback = require('request-idle-callback-polyfill')
+      .requestIdleCallback;
+    cancelIdleCallback = require('request-idle-callback-polyfill')
+      .cancelIdleCallback;
   });
 
   describe('requestIdleCallback', () => {
-      it('should be a function', () => {});
-      it('returns a number', () => {});
-      it('exceptions are reported to error handlers', () => {});
-      it('nested callbacks get a new idle period', () => {});
-      it('nested callbacks dont get the same deadline', () => {});
-      it('invoked at least once before the timeout', () => {});
-      it('callbacks invoked in order (called iteratively)', () => {});
-      it('callbacks invoked in order (called recursively)', () => {});
+    it('returns a number', () => {
+      const callback = jest.fn();
+      expect(typeof requestIdleCallback(callback)).toBe('number');
+    });
+    it('executes callbacks asynchronously', () => {
+      const callback = jest.fn();
+      requestIdleCallback(callback);
+      expect(callback).not.toBeCalled();
+      mockRunNextFrame();
+      expect(callback).toBeCalled();
+    });
+    it('cancels callbacks', () => {
+      const callback = jest.fn();
+      const handle = requestIdleCallback(callback);
+      cancelIdleCallback(handle);
+      mockRunNextFrame();
+      expect(callback).not.toBeCalled();
+    });
 
+    it('passes a deadline to the callback', () => {
+      const callback = jest.fn(deadline => {
+        expect(deadline.didTimeout).toBe(false);
+        expect(deadline.timeRemaining()).toBeGreaterThan(0);
+        mockLongRunningCode();
+        expect(deadline.timeRemaining()).toBe(0);
+      });
+      requestIdleCallback(callback);
+      mockRunNextFrame();
+      expect(callback).toBeCalled();
+    });
 
-  });
+    it('stops executing callbacks if the deadline expires', () => {
+      const ops = [];
+      requestIdleCallback(() => ops.push('first'));
+      requestIdleCallback(() => {
+        ops.push('second');
+        mockLongRunningCode();
+      });
+      requestIdleCallback(() => ops.push('third'));
+      expect(ops).toEqual([]);
+      mockRunNextFrame();
+      expect(ops).toEqual(['first', 'second']);
+      mockRunNextFrame();
+      expect(ops).toEqual(['first', 'second', 'third']);
+    });
 
-  descibe('cancelIdleCallback', () => {
-    it('should be a function', () => {});
-    it('cancels a callback', () => {});
+    it('executes callbacks that timeout', () => {
+      const callback = jest.fn(deadline => {
+        expect(deadline.didTimeout).toBe(true);
+        expect(deadline.timeRemaining()).toBe(0);
+      });
+      requestIdleCallback(callback, {timeout: 100});
+      jest.runAllTimers();
+      expect(callback).toBeCalled();
+    });
   });
 });
