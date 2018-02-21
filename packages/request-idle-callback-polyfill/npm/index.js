299737d283d8628bcb2ff80c6c607d8635270597
Brandon Dail <brandondail@fb.com>
2018-02-21 11:08:01 -0800

    Export a PROD and DEV build

--- packages/request-idle-callback-polyfill/npm/index.js
+++ packages/request-idle-callback-polyfill/npm/index.js
@@ -1,10 +1,7 @@
 'use strict';
 
-// @TODO figure out if we need a prod/dev build for this?
-// if (process.env.NODE_ENV === 'production') {
-//   module.exports = require('./cjs/{FILE}');
-// } else {
-//   module.exports = require('./cjs/{FILE}');
-// }
-
-module.exports = require('./cjs/request-idle-callback-polyfill.js');
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./cjs/request-idle-callback-polyfill.production.min.js');
+} else {
+  module.exports = require('./cjs/request-idle-callback-polyfill.development.js');
+}
