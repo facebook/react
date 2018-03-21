62560425563c7be15c7c99298aa4e58dc3714a26
Léo Andrès <leo@ndrs.fr>
2018-03-21 16:03:09 +0000

    Clean shell scripts (#12365)

--- scripts/circleci/test_coverage.sh
+++ scripts/circleci/test_coverage.sh
@@ -3,8 +3,8 @@
 set -e
 
 yarn test --coverage --maxWorkers=2
-if [ -z $CI_PULL_REQUEST ]; then
-  cat ./coverage/lcov.info | ./node_modules/.bin/coveralls
+if [ -z "$CI_PULL_REQUEST" ]; then
+  ./node_modules/.bin/coveralls < ./coverage/lcov.info
 fi
 
 # TODO: should we also track prod code coverage somehow?
--- scripts/circleci/upload_build.sh
+++ scripts/circleci/upload_build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ -z $CI_PULL_REQUEST ] && [ -n "$BUILD_SERVER_ENDPOINT" ]; then
+if [ -z "$CI_PULL_REQUEST" ] && [ -n "$BUILD_SERVER_ENDPOINT" ]; then
   curl \
     -F "react.development=@build/dist/react.development.js" \
     -F "react.production.min=@build/dist/react.production.min.js" \
@@ -12,9 +12,9 @@ if [ -z $CI_PULL_REQUEST ] && [ -n "$BUILD_SERVER_ENDPOINT" ]; then
     -F "react-dom-server.browser.production.min=@build/dist/react-dom-server.browser.production.min.js" \
     -F "results.json=@build/../scripts/rollup/results.json" \
     -F "commit=$CIRCLE_SHA1" \
-    -F "date=`git log --format='%ct' -1`" \
+    -F "date=$(git log --format='%ct' -1)" \
     -F "pull_request=false" \
     -F "token=$BUILD_SERVER_TOKEN" \
     -F "branch=$CIRCLE_BRANCH" \
-    $BUILD_SERVER_ENDPOINT
+    "$BUILD_SERVER_ENDPOINT"
 fi
