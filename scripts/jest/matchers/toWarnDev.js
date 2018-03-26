ea3dbf02e62709528688f7c4952a43aee32fda42
Brian Vaughn <brian.david.vaughn@gmail.com>
2018-03-26 09:56:56 -0700

    Workaround jest-diff single line string limitation

--- scripts/jest/matchers/toWarnDev.js
+++ scripts/jest/matchers/toWarnDev.js
@@ -2,6 +2,13 @@
 
 const jestDiff = require('jest-diff');
 
+function diffString(a, b) {
+  // jest-diff does not currently handle single line strings correctly
+  // The easiest work around is to ensure that both strings are multiline
+  // https://github.com/facebook/jest/issues/5657
+  return jestDiff(a + '\n', b + '\n');
+}
+
 function normalizeCodeLocInfo(str) {
   return str && str.replace(/at .+?:\d+/g, 'at **');
 }
@@ -49,11 +56,11 @@ const createMatcherFor = consoleMethod =>
         } else if (expectedMessages.length === 1) {
           errorMessage =
             'Unexpected warning recorded: ' +
-            jestDiff(normalizedMessage, expectedMessages[0]);
+            diffString(normalizedMessage, expectedMessages[0]);
         } else {
           errorMessage =
             'Unexpected warning recorded: ' +
-            jestDiff([normalizedMessage], expectedMessages);
+            diffString([normalizedMessage], expectedMessages);
         }
 
         // Record the call stack for unexpected warnings.
