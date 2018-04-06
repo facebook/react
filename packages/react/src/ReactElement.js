44df599071aedc1cf25b28f20c79dba23f938fea
Nicole Levy <n.levy@raceroster.com>
2018-04-05 21:07:23 -0400

    Revert "better check"
    This reverts commit 273370758eafa54d329577b3dc942c70587eccd3.

--- packages/react/src/ReactElement.js
+++ packages/react/src/ReactElement.js
@@ -293,7 +293,7 @@ export function cloneAndReplaceKey(oldElement, newKey) {
  */
 export function cloneElement(element, config, children) {
   invariant(
-    element != null,
+    !(element === null || element === undefined),
     'Cannot clone a null or undefined element.'
   );
 
