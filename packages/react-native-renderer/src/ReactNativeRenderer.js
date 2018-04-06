8513a0986195d21276a294c0663b6d3b23b7b3bf
Sebastian Markbage <sema@fb.com>
2018-04-06 16:32:55 -0700

    Remove flushSync from React Native
    There are no plans to enable async in the old renderer. In the new renderer
    it only really makes sense to do from the main thread and probably from
    native since it'll have to yield to native first.

--- packages/react-native-renderer/src/ReactNativeRenderer.js
+++ packages/react-native-renderer/src/ReactNativeRenderer.js
@@ -96,8 +96,6 @@ const ReactNativeRenderer: ReactNativeType = {
 
   unstable_batchedUpdates: ReactGenericBatching.batchedUpdates,
 
-  flushSync: ReactNativeFiberRenderer.flushSync,
-
   __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: {
     // Used as a mixin in many createClass-based components
     NativeMethodsMixin,
