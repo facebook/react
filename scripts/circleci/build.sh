c27a99812e75e73d9fad88c97ac8b8db452012c1
Dan Abramov <dan.abramov@gmail.com>
2018-04-11 18:46:02 +0100

    [Danger] Minor fixes (#12606)
    * Don't download bundle stats from master on CI
    
    This was temporarily necessary in the past because we didn't have the logic that downloads actual *merge base* stats.
    
    We do have that now as part of the Danger script. So we can remove this.
    
    * Use absolute threshold for whether to show a change
    
    * Download master stats, but only for other master builds
    
    * Rewrite sizes

--- scripts/circleci/build.sh
+++ scripts/circleci/build.sh
@@ -2,10 +2,13 @@
 		
 set -e		
 
-# Update the local size measurements to the master version
-# so that the size diff printed at the end of the build is
-# accurate.
-curl -o scripts/rollup/results.json http://react.zpao.com/builds/master/latest/results.json
+# On master, download the bundle sizes from last master build so that
+# the size printed in the CI logs for master commits is accurate.
+# We don't do it for pull requests because those are compared against
+# the merge base by Dangerfile instead. See https://github.com/facebook/react/pull/12606.
+if [ -z "$CI_PULL_REQUEST" ]; then
+  curl -o scripts/rollup/results.json http://react.zpao.com/builds/master/latest/results.json
+fi
 
 yarn build --extract-errors
 # Note: since we run the full build including extracting error codes,
