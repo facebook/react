1591c8ebab6151f3cae59ad42e3c15acc52cd67b
Dan Abramov <dan.abramov@gmail.com>
2018-04-16 15:42:10 +0100

    Update GCC (#12618)

--- scripts/rollup/build.js
+++ scripts/rollup/build.js
@@ -68,6 +68,7 @@ const closureOptions = {
   applyInputSourceMaps: false,
   useTypesForOptimization: false,
   processCommonJsModules: false,
+  rewritePolyfills: false,
 };
 
 function getBabelConfig(updateBabelOptions, bundleType, filename) {
