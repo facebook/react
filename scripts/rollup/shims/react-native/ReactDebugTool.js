369dd4fb1742917cb14486e33bb0e59bda62b23d
Timothy Yung <yungsters@gmail.com>
2018-05-14 17:47:47 -0700

    Update headers for React Native shims (#12806)

--- scripts/rollup/shims/react-native/ReactDebugTool.js
+++ scripts/rollup/shims/react-native/ReactDebugTool.js
@@ -4,7 +4,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @providesModule ReactDebugTool
+ * @format
+ * @flow
  */
 
 'use strict';
