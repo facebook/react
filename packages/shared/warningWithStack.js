2bff275c39fff22f594fba13b5e3c57af49e6b87
Dan <dan.abramov@gmail.com>
2018-07-06 21:46:29 +0100

    Add shared/warningWithStack

---
+++ packages/shared/warningWithStack.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import warning from 'shared/warning';
+import {ReactDebugCurrentFrame} from 'shared/ReactGlobalSharedState';
+
+let warningWithStack = warning;
+
+if (__DEV__) {
+  // TODO: wrap calls to it with a transform
+  warningWithStack = function(condition, format, ...args) {
+    if (!condition) {
+      const stack = ReactDebugCurrentFrame.getStackAddendum() || '';
+      warning(false, format + '%s', ...args, stack);
+    }
+  };
+}
+
+export default warningWithStack;
