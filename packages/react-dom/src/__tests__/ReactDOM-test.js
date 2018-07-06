f762b3abb1d951b366f3c8a2354aeedb7fbc32ff
Dan Abramov <dan.abramov@gmail.com>
2018-07-06 16:43:43 +0100

    Run react-dom SSR import test in jsdom-less environment (#13157)

--- packages/react-dom/src/__tests__/ReactDOM-test.js
+++ packages/react-dom/src/__tests__/ReactDOM-test.js
@@ -446,26 +446,4 @@ describe('ReactDOM', () => {
       global.requestAnimationFrame = previousRAF;
     }
   });
-
-  // We're just testing importing, not using it.
-  // It is important because even isomorphic components may import it.
-  it('can import findDOMNode in Node environment', () => {
-    const previousRAF = global.requestAnimationFrame;
-    const previousRIC = global.requestIdleCallback;
-    const prevWindow = global.window;
-    try {
-      global.requestAnimationFrame = undefined;
-      global.requestIdleCallback = undefined;
-      // Simulate the Node environment:
-      delete global.window;
-      jest.resetModules();
-      expect(() => {
-        require('react-dom');
-      }).not.toThrow();
-    } finally {
-      global.requestAnimationFrame = previousRAF;
-      global.requestIdleCallback = previousRIC;
-      global.window = prevWindow;
-    }
-  });
 });
