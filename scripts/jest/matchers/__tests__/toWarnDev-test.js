ac7215df956b51179ee1ab96ccbf9a38030902af
Dan Abramov <dan.abramov@gmail.com>
2018-07-16 20:35:43 +0100

    Fix matcher tests to be DEV-only

--- scripts/jest/matchers/__tests__/toWarnDev-test.js
+++ scripts/jest/matchers/__tests__/toWarnDev-test.js
@@ -12,126 +12,140 @@
 describe('toWarnDev', () => {
   it('does not fail if a warning contains a stack', () => {
     expect(() => {
-      console.error('Hello\n    in div');
+      if (__DEV__) {
+        console.error('Hello\n    in div');
+      }
     }).toWarnDev('Hello');
   });
 
   it('does not fail if all warnings contain a stack', () => {
     expect(() => {
-      console.error('Hello\n    in div');
-      console.error('Good day\n    in div');
-      console.error('Bye\n    in div');
+      if (__DEV__) {
+        console.error('Hello\n    in div');
+        console.error('Good day\n    in div');
+        console.error('Bye\n    in div');
+      }
     }).toWarnDev(['Hello', 'Good day', 'Bye']);
   });
 
   it('does not fail if warnings without stack explicitly opt out', () => {
     expect(() => {
-      console.error('Hello');
+      if (__DEV__) {
+        console.error('Hello');
+      }
     }).toWarnDev('Hello', {withoutStack: true});
     expect(() => {
-      console.error('Hello');
-      console.error('Good day');
-      console.error('Bye');
+      if (__DEV__) {
+        console.error('Hello');
+        console.error('Good day');
+        console.error('Bye');
+      }
     }).toWarnDev(['Hello', 'Good day', 'Bye'], {withoutStack: true});
   });
 
   it('does not fail when expected stack-less warning number matches the actual one', () => {
     expect(() => {
-      console.error('Hello\n    in div');
-      console.error('Good day');
-      console.error('Bye\n    in div');
+      if (__DEV__) {
+        console.error('Hello\n    in div');
+        console.error('Good day');
+        console.error('Bye\n    in div');
+      }
     }).toWarnDev(['Hello', 'Good day', 'Bye'], {withoutStack: 1});
   });
 
-  it('fails if a warning does not contain a stack', () => {
-    expect(() => {
+  if (__DEV__) {
+    it('fails if a warning does not contain a stack', () => {
       expect(() => {
-        console.error('Hello');
-      }).toWarnDev('Hello');
-    }).toThrow(
-      'Received warning unexpectedly does not include a component stack'
-    );
-  });
+        expect(() => {
+          console.error('Hello');
+        }).toWarnDev('Hello');
+      }).toThrow(
+        'Received warning unexpectedly does not include a component stack'
+      );
+    });
 
-  it('fails if some warnings do not contain a stack', () => {
-    expect(() => {
+    it('fails if some warnings do not contain a stack', () => {
       expect(() => {
-        console.error('Hello\n    in div');
-        console.error('Good day\n    in div');
-        console.error('Bye');
-      }).toWarnDev(['Hello', 'Good day', 'Bye']);
-    }).toThrow(
-      'Received warning unexpectedly does not include a component stack'
-    );
-    expect(() => {
+        expect(() => {
+          console.error('Hello\n    in div');
+          console.error('Good day\n    in div');
+          console.error('Bye');
+        }).toWarnDev(['Hello', 'Good day', 'Bye']);
+      }).toThrow(
+        'Received warning unexpectedly does not include a component stack'
+      );
       expect(() => {
-        console.error('Hello');
-        console.error('Good day\n    in div');
-        console.error('Bye\n    in div');
-      }).toWarnDev(['Hello', 'Good day', 'Bye']);
-    }).toThrow(
-      'Received warning unexpectedly does not include a component stack'
-    );
-    expect(() => {
+        expect(() => {
+          console.error('Hello');
+          console.error('Good day\n    in div');
+          console.error('Bye\n    in div');
+        }).toWarnDev(['Hello', 'Good day', 'Bye']);
+      }).toThrow(
+        'Received warning unexpectedly does not include a component stack'
+      );
       expect(() => {
-        console.error('Hello\n    in div');
-        console.error('Good day');
-        console.error('Bye\n    in div');
-      }).toWarnDev(['Hello', 'Good day', 'Bye']);
-    }).toThrow(
-      'Received warning unexpectedly does not include a component stack'
-    );
-    expect(() => {
+        expect(() => {
+          console.error('Hello\n    in div');
+          console.error('Good day');
+          console.error('Bye\n    in div');
+        }).toWarnDev(['Hello', 'Good day', 'Bye']);
+      }).toThrow(
+        'Received warning unexpectedly does not include a component stack'
+      );
       expect(() => {
-        console.error('Hello');
-        console.error('Good day');
-        console.error('Bye');
-      }).toWarnDev(['Hello', 'Good day', 'Bye']);
-    }).toThrow(
-      'Received warning unexpectedly does not include a component stack'
-    );
-  });
+        expect(() => {
+          console.error('Hello');
+          console.error('Good day');
+          console.error('Bye');
+        }).toWarnDev(['Hello', 'Good day', 'Bye']);
+      }).toThrow(
+        'Received warning unexpectedly does not include a component stack'
+      );
+    });
 
-  it('fails if warning is expected to not have a stack, but does', () => {
-    expect(() => {
+    it('fails if warning is expected to not have a stack, but does', () => {
       expect(() => {
-        console.error('Hello\n    in div');
-      }).toWarnDev('Hello', {withoutStack: true});
-    }).toThrow('Received warning unexpectedly includes a component stack');
-    expect(() => {
+        expect(() => {
+          console.error('Hello\n    in div');
+        }).toWarnDev('Hello', {withoutStack: true});
+      }).toThrow('Received warning unexpectedly includes a component stack');
       expect(() => {
-        console.error('Hello\n    in div');
-        console.error('Good day');
-        console.error('Bye\n    in div');
-      }).toWarnDev(['Hello', 'Good day', 'Bye'], {withoutStack: true});
-    }).toThrow('Received warning unexpectedly includes a component stack');
-  });
+        expect(() => {
+          console.error('Hello\n    in div');
+          console.error('Good day');
+          console.error('Bye\n    in div');
+        }).toWarnDev(['Hello', 'Good day', 'Bye'], {withoutStack: true});
+      }).toThrow('Received warning unexpectedly includes a component stack');
+    });
 
-  it('fails if expected stack-less warning number does not match the actual one', () => {
-    expect(() => {
+    it('fails if expected stack-less warning number does not match the actual one', () => {
       expect(() => {
-        console.error('Hello\n    in div');
-        console.error('Good day');
-        console.error('Bye\n    in div');
-      }).toWarnDev(['Hello', 'Good day', 'Bye'], {withoutStack: 4});
-    }).toThrow('Expected 4 warnings without a component stack but received 1');
-  });
+        expect(() => {
+          console.error('Hello\n    in div');
+          console.error('Good day');
+          console.error('Bye\n    in div');
+        }).toWarnDev(['Hello', 'Good day', 'Bye'], {withoutStack: 4});
+      }).toThrow(
+        'Expected 4 warnings without a component stack but received 1'
+      );
+    });
 
-  it('fails if withoutStack is invalid', () => {
-    expect(() => {
+    it('fails if withoutStack is invalid', () => {
       expect(() => {
-        console.error('Hi');
-      }).toWarnDev('Hi', {withoutStack: null});
-    }).toThrow('Instead received object');
-    expect(() => {
+        expect(() => {
+          console.error('Hi');
+        }).toWarnDev('Hi', {withoutStack: null});
+      }).toThrow('Instead received object');
       expect(() => {
-        console.error('Hi');
-      }).toWarnDev('Hi', {withoutStack: {}});
-    }).toThrow('Instead received object');
-    expect(() => {
+        expect(() => {
+          console.error('Hi');
+        }).toWarnDev('Hi', {withoutStack: {}});
+      }).toThrow('Instead received object');
       expect(() => {
-        console.error('Hi');
-      }).toWarnDev('Hi', {withoutStack: 'haha'});
-    }).toThrow('Instead received string');
-  });
+        expect(() => {
+          console.error('Hi');
+        }).toWarnDev('Hi', {withoutStack: 'haha'});
+      }).toThrow('Instead received string');
+    });
+  }
 });
