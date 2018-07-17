d12fd5c4cb8d70f034f396076a3aeb75e94e3e2f
Dan Abramov <dan.abramov@gmail.com>
2018-07-17 01:11:56 +0100

    Set current fiber during before-mutation traversal (#13219)

--- packages/react-reconciler/src/ReactFiberScheduler.js
+++ packages/react-reconciler/src/ReactFiberScheduler.js
@@ -428,8 +428,11 @@ function commitAllHostEffects() {
 
 function commitBeforeMutationLifecycles() {
   while (nextEffect !== null) {
-    const effectTag = nextEffect.effectTag;
+    if (__DEV__) {
+      ReactCurrentFiber.setCurrentFiber(nextEffect);
+    }
 
+    const effectTag = nextEffect.effectTag;
     if (effectTag & Snapshot) {
       recordEffect();
       const current = nextEffect.alternate;
@@ -439,6 +442,10 @@ function commitBeforeMutationLifecycles() {
     // Don't cleanup effects yet;
     // This will be done by commitAllLifeCycles()
     nextEffect = nextEffect.nextEffect;
+  }
+
+  if (__DEV__) {
+    ReactCurrentFiber.resetCurrentFiber();
   }
 }
 
