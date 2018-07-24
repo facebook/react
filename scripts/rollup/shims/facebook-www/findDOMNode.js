baaa7f64aafda5f84f8a0d887887d6ad868cbab6
Brian Vaughn <bvaughn@fb.com>
2018-07-24 14:38:49 -0700

    Prettier

--- scripts/rollup/shims/facebook-www/findDOMNode.js
+++ scripts/rollup/shims/facebook-www/findDOMNode.js
@@ -1,12 +1,12 @@
-/**	
- * Copyright (c) 2013-present, Facebook, Inc.	
- *	
- * This source code is licensed under the MIT license found in the	
- * LICENSE file in the root directory of this source tree.	
- *	
- * @flow	
- * @format	
- */	
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
 
 'use strict';
 
