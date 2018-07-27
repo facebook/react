e603c64d90913f96b49ac03962f9c3d37f30495d
Andrew Clark <acdlite@fb.com>
2018-07-27 11:18:11 -0700

    Remove unnecessary branching from updateContextProvider
    This code had gotten unnecessarily complex after some recent changes.
    Cleaned it up a bit.

--- packages/react/src/ReactContext.js
+++ packages/react/src/ReactContext.js
@@ -50,13 +50,12 @@ export function createContext<T>(
   const context: ReactContext<T> = {
     $$typeof: REACT_CONTEXT_TYPE,
     _calculateChangedBits: calculateChangedBits,
-    _defaultValue: defaultValue,
-    _currentValue: defaultValue,
     // As a workaround to support multiple concurrent renderers, we categorize
     // some renderers as primary and others as secondary. We only expect
     // there to be two concurrent renderers at most: React Native (primary) and
     // Fabric (secondary); React DOM (primary) and React ART (secondary).
     // Secondary renderers store their context values on separate fields.
+    _currentValue: defaultValue,
     _currentValue2: defaultValue,
     _changedBits: 0,
     _changedBits2: 0,
