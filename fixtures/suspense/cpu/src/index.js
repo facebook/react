580b610a5873ace9cd165240d9db2c98c31181f8
Brian Vaughn <bvaughn@fb.com>
2018-08-01 10:48:59 -0700

    Prettier

--- fixtures/suspense/cpu/src/index.js
+++ fixtures/suspense/cpu/src/index.js
@@ -1,5 +1,5 @@
-import React, { Component, PureComponent, unstable_AsyncMode } from 'react';
-import { flushSync, render, unstable_deferredUpdates } from 'react-dom';
+import React, {Component, PureComponent, unstable_AsyncMode} from 'react';
+import {flushSync, render, unstable_deferredUpdates} from 'react-dom';
 import _ from 'lodash';
 import Charts from './Charts';
 import Clock from './Clock';
@@ -21,7 +21,8 @@ class App extends PureComponent {
       return cachedData.get(input);
     }
     const multiplier = input.length !== 0 ? input.length : 1;
-    const complexity = (parseInt(window.location.search.substring(1), 10) / 100 * 25) || 25;
+    const complexity =
+      parseInt(window.location.search.substring(1), 10) / 100 * 25 || 25;
     const data = _.range(5).map(t =>
       _.range(complexity * multiplier).map((j, i) => {
         return {
@@ -35,20 +36,20 @@ class App extends PureComponent {
   }
 
   componentDidMount() {
-    window.addEventListener('keydown', (e) => {
+    window.addEventListener('keydown', e => {
       if (e.key.toLowerCase() === '?') {
         e.preventDefault();
         this.setState(state => ({
-          showClock: !state.showClock
+          showClock: !state.showClock,
         }));
       }
     });
   }
 
-  handleChartClick = (e) => {
+  handleChartClick = e => {
     if (this.state.showDemo) {
       if (e.shiftKey) {
-        this.setState({ showDemo: false });
+        this.setState({showDemo: false});
       }
       return;
     }
@@ -64,33 +65,28 @@ class App extends PureComponent {
     this._ignoreClick = true;
 
     unstable_deferredUpdates(() => {
-      this.setState(
-        {showDemo: true},
-        () => {
-          this._ignoreClick = false;
-        }
-      );
+      this.setState({showDemo: true}, () => {
+        this._ignoreClick = false;
+      });
     });
   };
 
   debouncedHandleChange = _.debounce(value => {
     if (this.state.strategy === 'debounced') {
       flushSync(() => {
-        this.setState({ value: value });
+        this.setState({value: value});
       });
     }
   }, 1000);
 
   renderOption(strategy, label) {
-    const { strategy: currentStrategy } = this.state;
+    const {strategy: currentStrategy} = this.state;
     return (
-      <label className={
-        strategy === currentStrategy ? 'selected' : null
-      }>
+      <label className={strategy === currentStrategy ? 'selected' : null}>
         <input
           type="radio"
           checked={strategy === currentStrategy}
-          onChange={() => this.setState({ strategy })}
+          onChange={() => this.setState({strategy})}
         />
         {label}
       </label>
@@ -99,17 +95,17 @@ class App extends PureComponent {
 
   handleChange = e => {
     const value = e.target.value;
-    const { strategy } = this.state;
+    const {strategy} = this.state;
     switch (strategy) {
       case 'sync':
-        this.setState({ value });
+        this.setState({value});
         break;
       case 'debounced':
         this.debouncedHandleChange(value);
         break;
       case 'async':
         unstable_deferredUpdates(() => {
-          this.setState({ value });
+          this.setState({value});
         });
         break;
       default:
@@ -118,8 +114,9 @@ class App extends PureComponent {
   };
 
   render() {
-    const Wrapper = this.state.strategy === 'async' ? unstable_AsyncMode : 'div';
-    const { showClock } = this.state;
+    const Wrapper =
+      this.state.strategy === 'async' ? unstable_AsyncMode : 'div';
+    const {showClock} = this.state;
     const data = this.getStreamData(this.state.value);
     return (
       <div className="container">
@@ -136,13 +133,10 @@ class App extends PureComponent {
         />
         <Wrapper>
           <div className="demo" onClick={this.handleChartClick}>
-            {this.state.showDemo &&
-              <Charts
-                data={data}
-                onClick={this.handleChartClick}
-              />
-            }
-            <div style={{ display: showClock ? 'block' : 'none' }}>
+            {this.state.showDemo && (
+              <Charts data={data} onClick={this.handleChartClick} />
+            )}
+            <div style={{display: showClock ? 'block' : 'none'}}>
               <Clock />
             </div>
           </div>
