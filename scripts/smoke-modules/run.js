b4ee260766091e13a8f27311bce6cb78a099c329
Bogdan Chadkin <trysound@yandex.ru>
2018-08-04 00:39:43 +0300

    Add smoke tests for index.mjs entry points

---
+++ scripts/smoke-modules/run.js
@@ -0,0 +1,10 @@
+const {execSync} = require('child_process');
+const glob = require('glob');
+
+const entries = glob.sync('build/node_modules/**/index.mjs');
+
+entries.forEach(entry => {
+  execSync(`node --experimental-modules "./${entry}"`);
+});
+
+console.info(`\n${entries.length} .mjs entry points are run successfully!\n`);
