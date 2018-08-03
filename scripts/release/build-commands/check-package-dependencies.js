dadb86d98bae632438199ab565969dae57eda1db
Clark Du <clark.du@betbright.com>
2018-08-03 17:18:07 +0100

    refactor: remove promise on checkModule

--- scripts/release/build-commands/check-package-dependencies.js
+++ scripts/release/build-commands/check-package-dependencies.js
@@ -39,7 +39,7 @@ const check = async ({cwd, packages}) => {
     });
   };
 
-  await Promise.all(dependencies.map(checkModule));
+  dependencies.map(checkModule);
 
   if (invalidDependencies.length) {
     throw Error(
