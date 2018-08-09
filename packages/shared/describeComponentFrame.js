5dfd232412ee5a496c321d33d5b39333bc3b3d61
Billy Janitsch <billyjanitsch@gmail.com>
2018-08-08 22:33:30 -0400

    Improve display of filenames in component stack (#12059)
    * Improve display of filenames in component stack
    
    * Add explanatory comment
    
    * tests: add tests for component stack trace displaying
    
    * Tweak test
    
    * Rewrite test and revert implementation
    
    * Extract a variable
    
    * Rewrite implementation

--- packages/shared/describeComponentFrame.js
+++ packages/shared/describeComponentFrame.js
@@ -7,22 +7,31 @@
  * @flow
  */
 
+const BEFORE_SLASH_RE = /^(.*)[\\\/]/;
+
 export default function(
   name: null | string,
   source: any,
   ownerName: null | string,
 ) {
-  return (
-    '\n    in ' +
-    (name || 'Unknown') +
-    (source
-      ? ' (at ' +
-        source.fileName.replace(/^.*[\\\/]/, '') +
-        ':' +
-        source.lineNumber +
-        ')'
-      : ownerName
-        ? ' (created by ' + ownerName + ')'
-        : '')
-  );
+  let sourceInfo = '';
+  if (source) {
+    let path = source.fileName;
+    let fileName = path.replace(BEFORE_SLASH_RE, '');
+    if (/^index\./.test(fileName)) {
+      // Special case: include closest folder name for `index.*` filenames.
+      const match = path.match(BEFORE_SLASH_RE);
+      if (match) {
+        const pathBeforeSlash = match[1];
+        if (pathBeforeSlash) {
+          const folderName = pathBeforeSlash.replace(BEFORE_SLASH_RE, '');
+          fileName = folderName + '/' + fileName;
+        }
+      }
+    }
+    sourceInfo = ' (at ' + fileName + ':' + source.lineNumber + ')';
+  } else if (ownerName) {
+    sourceInfo = ' (created by ' + ownerName + ')';
+  }
+  return '\n    in ' + (name || 'Unknown') + sourceInfo;
 }
