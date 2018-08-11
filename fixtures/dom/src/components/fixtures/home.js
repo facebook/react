80c4bcf1fe94949e03267ed23e0b6821486fd48d
Nate Hunzaker <nate.hunzaker@gmail.com>
2018-08-10 20:23:47 -0700

    Update browser support fields

--- fixtures/dom/src/components/fixtures/home.js
+++ fixtures/dom/src/components/fixtures/home.js
@@ -6,54 +6,63 @@ export default function Home() {
       <h1>DOM Test Fixtures</h1>
       <p>
         Use this site to test browser quirks and other behavior that can not be
-        captured through unit tests. React is tested in:
+        captured through unit tests.
       </p>
-      <table>
-        <thead>
-          <tr>
-            <th>Browser</th>
-            <th>Versions</th>
-            <th>Why/Notes</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Chrome - Desktop</td>
-            <td>49, Latest</td>
-            <td>49 is the last release for Windows XP</td>
-          </tr>
-          <tr>
-            <td>Chrome - Android</td>
-            <td>Latest</td>
-            <td>N/A</td>
-          </tr>
-          <tr>
-            <td>Firefox Desktop</td>
-            <td>ESR, Latest</td>
-            <td>The long term support release is used by many institutions</td>
-          </tr>
-          <tr>
-            <td>Internet Explorer</td>
-            <td>9, 10, 11</td>
-            <td>For great justice</td>
-          </tr>
-          <tr>
-            <td>Microsoft Edge</td>
-            <td>14, Latest</td>
-            <td>N/A</td>
-          </tr>
-          <tr>
-            <td>Safari - Desktop</td>
-            <td>7.1, Latest</td>
-            <td>N/A</td>
-          </tr>
-          <tr>
-            <td>Safari - iOS</td>
-            <td>9, Latest</td>
-            <td>N/A</td>
-          </tr>
-        </tbody>
-      </table>
+      <section>
+        <h2>Tested Browsers</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Browser</th>
+              <th>Versions</th>
+              <th>Why/Notes</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Chrome - Desktop</td>
+              <td>49, Latest</td>
+              <td>49 is the last release for Windows XP</td>
+            </tr>
+            <tr>
+              <td>Chrome - Android</td>
+              <td>Latest</td>
+              <td>N/A</td>
+            </tr>
+            <tr>
+              <td>Firefox Desktop</td>
+              <td>
+                <a href="https://www.mozilla.org/en-US/firefox/organizations/">
+                  ESR
+                </a>, Latest
+              </td>
+              <td>
+                The long term support release is used by many institutions
+              </td>
+            </tr>
+            <tr>
+              <td>Internet Explorer</td>
+              <td>9, 10, 11</td>
+              <td>N/A</td>
+            </tr>
+            <tr>
+              <td>Microsoft Edge</td>
+              <td>14, Latest</td>
+              <td>N/A</td>
+            </tr>
+            <tr>
+              <td>Safari - Desktop</td>
+              <td>7, Latest</td>
+              <td>N/A</td>
+            </tr>
+            <tr>
+              <td>Safari - iOS</td>
+              <td>7, Latest</td>
+              <td>N/A</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
       <section>
         <h2>How do I test browsers I don't have access to?</h2>
         <p>
@@ -72,8 +81,8 @@ export default function Home() {
         <p>
           These services cost money, however a few maintainers have access to a
           subscription. There is no obligation to subscribe to these services;
-          feel free to ping a maintainer or mention browsers that need
-          additional testing.
+          feel free to ping a maintainer or mention that need more testing is
+          required.
         </p>
       </section>
     </main>
