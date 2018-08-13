546cb074e4d0dc24a6f45774b099e53860621d4d
Nate Hunzaker <nate.hunzaker@gmail.com>
2018-08-13 16:08:32 -0700

    Pull in react-live for better editing

--- fixtures/dom/src/components/fixtures/hydration/index.js
+++ fixtures/dom/src/components/fixtures/hydration/index.js
@@ -1,5 +1,6 @@
 import './hydration.css';
 import {SAMPLE_CODE} from './data';
+import {LiveProvider, LiveEditor, LiveError} from 'react-live';
 import * as buble from 'buble';
 
 const React = window.React;
@@ -7,12 +8,19 @@ const React = window.React;
 class Hydration extends React.Component {
   state = {
     code: SAMPLE_CODE,
+    transformed: buble.transform(SAMPLE_CODE).code,
     hydrate: true,
   };
 
-  setInput = event => {
-    const {name, value} = event.target;
-    this.setState({[name]: value});
+  setCode = code => {
+    try {
+      this.setState({
+        code: code,
+        transformed: buble.transform(code).code,
+      });
+    } catch (_error) {
+      // Do nothing, ReactLive will show the error
+    }
   };
 
   setCheckbox = event => {
@@ -23,36 +31,38 @@ class Hydration extends React.Component {
   render() {
     const {code, hydrate} = this.state;
 
-    const src = `/renderer.html?code=${escape(
-      buble.transform(code).code
-    )}&hydrate=${hydrate}`;
+    const transformed = buble.transform(code).code;
+    const src = `/renderer.html?code=${escape(transformed)}&hydrate=${hydrate}`;
 
     return (
-      <div className="hydration">
-        <section className="hydration-editor">
-          <header className="hydration-options">
-            <label htmlFor="hydrate">
-              <input
-                id="hydrate"
-                name="hydrate"
-                type="checkbox"
-                checked={hydrate}
-                onChange={this.setCheckbox}
-              />
-              Hydrate
-            </label>
-          </header>
+      <LiveProvider code={code}>
+        <div className="hydration">
+          <section className="hydration-editor">
+            <header className="hydration-options">
+              <label htmlFor="hydrate">
+                <input
+                  id="hydrate"
+                  name="hydrate"
+                  type="checkbox"
+                  checked={hydrate}
+                  onChange={this.setCheckbox}
+                />
+                Hydrate
+              </label>
+            </header>
 
-          <div className="hydration-code">
-            <textarea onChange={this.setInput} value={code} />
-          </div>
-        </section>
-        <iframe
-          className="hydration-frame"
-          title="Hydration Preview"
-          src={src}
-        />
-      </div>
+            <div className="hydration-code">
+              <LiveEditor onChange={this.setCode} />
+            </div>
+          </section>
+          <iframe
+            className="hydration-frame"
+            title="Hydration Preview"
+            src={src}
+          />
+          <LiveError className="hydration-code-error" />
+        </div>
+      </LiveProvider>
     );
   }
 }
