fe959eea731b4de0c970f753dfa9d0f6b010b63d
Andrew Clark <acdlite@me.com>
2018-08-16 09:43:32 -0700

    React.lazy (#13398)
    Lazily starts loading a component the first time it's rendered. The
    implementation is fairly simple and could be left to userspace, but since
    this is an important use case, there's value in standardization.

---
+++ packages/react/src/ReactLazy.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+type Thenable<T, R> = {
+  then(resolve: (T) => mixed, reject: (mixed) => mixed): R,
+};
+
+export function lazy<T, R>(ctor: () => Thenable<T, R>) {
+  let thenable = null;
+  return {
+    then(resolve, reject) {
+      if (thenable === null) {
+        // Lazily create thenable by wrapping in an extra thenable.
+        thenable = ctor();
+        ctor = null;
+      }
+      return thenable.then(resolve, reject);
+    },
+    // React uses these fields to store the result.
+    _reactStatus: -1,
+    _reactResult: null,
+  };
+}
