01229969558b2167e3b0221052ac9b08904bbd47
Dan Abramov <dan.abramov@gmail.com>
2018-08-20 16:42:25 +0100

    Fix lint

--- packages/shared/getComponentName.js
+++ packages/shared/getComponentName.js
@@ -21,10 +21,7 @@ import {
   REACT_STRICT_MODE_TYPE,
   REACT_PLACEHOLDER_TYPE,
 } from 'shared/ReactSymbols';
-import {
-  getResultFromResolvedThenable,
-  refineResolvedThenable,
-} from 'shared/ReactLazyComponent';
+import {refineResolvedThenable} from 'shared/ReactLazyComponent';
 
 function getComponentName(type: mixed): string | null {
   if (type == null) {
