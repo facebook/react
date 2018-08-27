310b87bea986b39e57786d635932abeeae44f54f
Nate Hunzaker <nate.hunzaker@gmail.com>
2018-08-27 20:41:15 +0000

    Add WeakSet to eslint config

--- scripts/rollup/validate/eslintrc.rn.js
+++ scripts/rollup/validate/eslintrc.rn.js
@@ -12,6 +12,7 @@ module.exports = {
     Symbol: true,
     Proxy: true,
     WeakMap: true,
+    WeakSet: true,
     // Vendor specific
     MSApp: true,
     __REACT_DEVTOOLS_GLOBAL_HOOK__: true,
--- scripts/rollup/validate/eslintrc.umd.js
+++ scripts/rollup/validate/eslintrc.umd.js
@@ -11,6 +11,7 @@ module.exports = {
     Symbol: true,
     Proxy: true,
     WeakMap: true,
+    WeakSet: true,
     // Vendor specific
     MSApp: true,
     __REACT_DEVTOOLS_GLOBAL_HOOK__: true,
