b77673baa4c32e53ea9982260c53682ac86f7c79
Brian Vaughn <bvaughn@fb.com>
2018-08-29 17:13:37 -0700

    Fixed fixtures/packaging

--- fixtures/packaging/rjs/prod/config.js
+++ fixtures/packaging/rjs/prod/config.js
@@ -4,6 +4,8 @@ module.exports = {
   out: 'output.js',
   optimize: 'none',
   paths: {
+    'interaction-tracking':
+      '../../../../build/dist/interaction-tracking.production.min',
     react: '../../../../build/dist/react.production.min',
     'react-dom': '../../../../build/dist/react-dom.production.min',
   },
