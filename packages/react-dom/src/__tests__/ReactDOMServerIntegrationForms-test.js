c21bab6940b253af28f8eb99d7344d92d0babb31
Dan Abramov <dan.abramov@gmail.com>
2018-08-31 16:15:05 +0100

    Add SSR regression test for #6119

--- packages/react-dom/src/__tests__/ReactDOMServerIntegrationForms-test.js
+++ packages/react-dom/src/__tests__/ReactDOMServerIntegrationForms-test.js
@@ -445,6 +445,16 @@ describe('ReactDOMServerIntegration', () => {
       );
     });
 
+    describe('progress', function() {
+      itRenders('a progress in an indeterminate state', async render => {
+        // Regression test for https://github.com/facebook/react/issues/6119
+        const e = await render(<progress value={null} />);
+        expect(e.hasAttribute('value')).toBe(false);
+        const e2 = await render(<progress value={50} />);
+        expect(e2.getAttribute('value')).toBe('50');
+      });
+    });
+
     describe('user interaction', function() {
       let ControlledInput,
         ControlledTextArea,
