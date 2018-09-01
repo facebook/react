152d8d624c6e9f3db15ab12df8486f3a74d48d4b
Brian Vaughn <bvaughn@fb.com>
2018-09-01 12:28:26 -0700

    Add test for SECRET_INTERNALS object too

--- packages/react-scheduler/src/__tests__/SchedulingUMDBundle-test.js
+++ packages/react-scheduler/src/__tests__/SchedulingUMDBundle-test.js
@@ -9,17 +9,32 @@
 'use strict';
 
 describe('Scheduling UMD bundle', () => {
-  beforeEach(() => jest.resetModules());
+  beforeEach(() => {
+    // Fool SECRET_INTERNALS object into including UMD forwarding methods.
+    global.__UMD__ = true;
+
+    jest.resetModules();
+  });
 
   it('should define the same scheduling API', () => {
     const umdAPI = require('../../npm/umd/react-scheduler');
     const cjsAPI = require('../../index');
+    const secretInternalsAPI = require('react/src/ReactSharedInternals')
+      .default;
     expect(Object.keys(umdAPI).sort()).toEqual(Object.keys(cjsAPI).sort());
+    expect(Object.keys(umdAPI).sort()).toEqual(
+      Object.keys(secretInternalsAPI.Scheduler).sort(),
+    );
   });
 
   it('should define the same tracking API', () => {
     const umdAPI = require('../../npm/umd/react-scheduler-tracking');
     const cjsAPI = require('../../tracking');
+    const secretInternalsAPI = require('react/src/ReactSharedInternals')
+      .default;
     expect(Object.keys(umdAPI).sort()).toEqual(Object.keys(cjsAPI).sort());
+    expect(Object.keys(umdAPI).sort()).toEqual(
+      Object.keys(secretInternalsAPI.SchedulerTracking).sort(),
+    );
   });
 });
