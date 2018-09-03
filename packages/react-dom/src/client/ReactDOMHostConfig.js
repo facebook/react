0b74e95d7b7fabbd0cf502d0f3254a607ed30478
Fredrik Höglund <fredrik.hoglund@gmail.com>
2018-09-03 18:17:53 +0200

    Ignore noscript content on the client (#13537)
    * Ignore noscript content on the client (#11423)
    
    * Fix failing test for ignoring noscript content
    
    * Add a ServerIntegration test for noscript

--- packages/react-dom/src/client/ReactDOMHostConfig.js
+++ packages/react-dom/src/client/ReactDOMHostConfig.js
@@ -247,6 +247,7 @@ export function shouldSetTextContent(type: string, props: Props): boolean {
   return (
     type === 'textarea' ||
     type === 'option' ||
+    type === 'noscript' ||
     typeof props.children === 'string' ||
     typeof props.children === 'number' ||
     (typeof props.dangerouslySetInnerHTML === 'object' &&
