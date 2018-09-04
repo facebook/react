d4eba96daa8b3ef428c7a8d3fb005fb3c1981168
Nathan Hunzaker <nate.hunzaker@gmail.com>
2018-09-04 09:44:41 -0700

    Revert newline change

--- packages/react-dom/src/events/__tests__/ChangeEventPlugin-test.internal.js
+++ packages/react-dom/src/events/__tests__/ChangeEventPlugin-test.internal.js
@@ -108,7 +108,6 @@ describe('ChangeEventPlugin', () => {
     // make it `true` again. Thus, at the time of the event, React should not
     // consider it a change from the initial `true` value.
     setUntrackedChecked.call(node, false);
-
     node.dispatchEvent(
       new MouseEvent('click', {bubbles: true, cancelable: true}),
     );
