b87aabdfe1b7461e7331abb3601d9e6bb27544bc
Héctor Ramos <165856+hramos@users.noreply.github.com>
2018-09-07 15:11:23 -0700

    Drop the year from Facebook copyright headers and the LICENSE file. (#13593)

--- scripts/rollup/wrappers.js
+++ scripts/rollup/wrappers.js
@@ -20,7 +20,7 @@ const RN_FB_PROFILING = Bundles.bundleTypes.RN_FB_PROFILING;
 
 const RECONCILER = Bundles.moduleTypes.RECONCILER;
 
-const license = ` * Copyright (c) 2013-present, Facebook, Inc.
+const license = ` * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.`;
