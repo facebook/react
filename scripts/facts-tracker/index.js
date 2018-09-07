9beb66f0b4ff31613a2fb0d21f2ef26aaa668552
Héctor Ramos <165856+hramos@users.noreply.github.com>
2018-09-07 15:11:23 -0700

    Drop the year from Facebook copyright headers and the LICENSE file. (#13593)

--- scripts/facts-tracker/index.js
+++ scripts/facts-tracker/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /**
- * Copyright (c) 2013-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
