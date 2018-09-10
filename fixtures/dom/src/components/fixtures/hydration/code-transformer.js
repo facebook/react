c1843bfb856aae653dc8cdb15616ac0cf47fe8fe
Nate Hunzaker <nate.hunzaker@gmail.com>
2018-09-10 10:14:48 -0700

    Do not highlight errors from Babel. Add setPrototypeOf polyfill
    This commit fixes an error in Safari 7.1 where Chalk highlighted Babel
    errors caused a crash when setPrototypeOf was called within the
    library.
    
    This is also an issue on IE9, however this fix does not resolve issues
    in that browser.

--- fixtures/dom/src/components/fixtures/hydration/code-transformer.js
+++ fixtures/dom/src/components/fixtures/hydration/code-transformer.js
@@ -7,11 +7,18 @@ import 'core-js/es6/weak-set';
 import 'core-js/es6/number';
 import 'core-js/es6/string';
 import 'core-js/es6/array';
+import 'core-js/modules/es6.object.set-prototype-of';
 
 import {transform} from '@babel/standalone';
 
-const presets = ['es2015', 'stage-3', 'react'];
+const options = {
+  presets: ['es2015', 'stage-3', 'react'],
+  // This is important for IE9 and Safari 7.1. Setting this to false
+  // prevents highlighting in error messages. We also don't use this
+  // when reporting errors
+  highlightCode: false
+}
 
 export function compile(raw) {
-  return transform(raw, {presets}).code;
+  return transform(raw, options).code
 }
