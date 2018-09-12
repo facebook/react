c2d6ddab6d1d2c2bf27148af9505bb7f791c7bcd
Dan Abramov <dan.abramov@gmail.com>
2018-09-12 16:11:56 +0100

    Only run ReactDOM tests for Fire

--- scripts/jest/config.source-fire.js
+++ scripts/jest/config.source-fire.js
@@ -3,6 +3,7 @@
 const baseConfig = require('./config.base');
 
 module.exports = Object.assign({}, baseConfig, {
+  roots: ['<rootDir>/packages/react-dom'],
   setupFiles: [
     ...baseConfig.setupFiles,
     require.resolve('./setupFire.js'),
