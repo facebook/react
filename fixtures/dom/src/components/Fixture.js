e6fce101ddcaf62bc7fb24d076e02847413a5749
edwinfrc <edwin_frc@hotmail.com>
2018-09-14 18:53:43 -0600

    Update Fixture.js

--- fixtures/dom/src/components/Fixture.js
+++ fixtures/dom/src/components/Fixture.js
@@ -1,18 +1,12 @@
-const PropTypes = window.PropTypes;
-const React = window.React;
-
+const PropTypes = window.PropTypes,React = window.React;
 const propTypes = {
   children: PropTypes.node.isRequired,
 };
-
 class Fixture extends React.Component {
   render() {
     const {children} = this.props;
-
     return <div className="test-fixture">{children}</div>;
   }
 }
-
 Fixture.propTypes = propTypes;
-
 export default Fixture;
