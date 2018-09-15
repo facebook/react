07241b5d06f1c9ce03ece5a9ad573bced1610ec6
edwinfrc <edwin_frc@hotmail.com>
2018-09-14 18:56:50 -0600

    Update Header.js

--- fixtures/dom/src/components/Header.js
+++ fixtures/dom/src/components/Header.js
@@ -5,13 +5,13 @@ const React = window.React;
 class Header extends React.Component {
   constructor(props, context) {
     super(props, context);
-    const query = parse(window.location.search);
-    const version = query.version || 'local';
-    const versions = [version];
+    const query = parse(window.location.search),
+          version = query.version || 'local',
+          versions = [version];
     this.state = {version, versions};
   }
-  componentWillMount() {
-    getVersionTags().then(tags => {
+ componentWillMount(){
+    getVersionTags().then(tags=>{
       let versions = tags.map(tag => tag.name.slice(1));
       versions = [`local`, ...versions];
       this.setState({versions});
@@ -20,16 +20,12 @@ class Header extends React.Component {
   handleVersionChange(event) {
     const query = parse(window.location.search);
     query.version = event.target.value;
-    if (query.version === 'local') {
-      delete query.version;
-    }
+    if (query.version === 'local'){delete query.version;}
     window.location.search = stringify(query);
   }
-  handleFixtureChange(event) {
-    window.location.pathname = event.target.value;
-  }
-  render() {
-    return (
+  handleFixtureChange(event){window.location.pathname = event.target.value;}
+  render(){
+    return(
       <header className="header">
         <div className="header__inner">
           <span className="header__logo">
@@ -41,7 +37,6 @@ class Header extends React.Component {
             />
             React Sandbox (v{React.version})
           </span>
-
           <div className="header-controls">
             <label htmlFor="example">
               <span className="sr-only">Select an example</span>
@@ -70,13 +65,9 @@ class Header extends React.Component {
             </label>
             <label htmlFor="react_version">
               <span className="sr-only">Select a version to test</span>
-              <select
-                value={this.state.version}
-                onChange={this.handleVersionChange}>
+              <select value={this.state.version} onChange={this.handleVersionChange}>
                 {this.state.versions.map(version => (
-                  <option key={version} value={version}>
-                    {version}
-                  </option>
+                  <option key={version} value={version}>{version}</option>
                 ))}
               </select>
             </label>
@@ -86,5 +77,4 @@ class Header extends React.Component {
     );
   }
 }
-
 export default Header;
