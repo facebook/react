c5f51032edab7f208cf74a0ce19d6fdcda1bb7c2
mapkeji <mapkeji26@gmail.com>
2018-09-24 21:20:32 +0300

    Import type first in ReactCurrentFiber

--- packages/react-reconciler/src/ReactCurrentFiber.js
+++ packages/react-reconciler/src/ReactCurrentFiber.js
@@ -7,6 +7,8 @@
  * @flow
  */
 
+import type {Fiber} from './ReactFiber';
+
 import ReactSharedInternals from 'shared/ReactSharedInternals';
 import {
   IndeterminateComponent,
@@ -21,8 +23,6 @@ import describeComponentFrame from 'shared/describeComponentFrame';
 import getComponentName from 'shared/getComponentName';
 
 const ReactDebugCurrentFrame = ReactSharedInternals.ReactDebugCurrentFrame;
-
-import type {Fiber} from './ReactFiber';
 
 type LifeCyclePhase = 'render' | 'getChildContext';
 
