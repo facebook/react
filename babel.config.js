54a5eb2f418542d073edef9148f427203c924e00
mapkeji <mapkeji26@gmail.com>
2018-10-06 00:02:01 +0300

    Replace .babelrc with babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: ['@babel/react', '@babel/preset-flow'],
+  ignore: ['third_party'],
+  plugins: [
+    '@babel/proposal-class-properties',
+    'syntax-trailing-function-commas',
+    ['@babel/proposal-object-rest-spread', {useBuiltIns: true}],
+    '@babel/transform-template-literals',
+    '@babel/transform-literals',
+    '@babel/plugin-transform-arrow-functions',
+    '@babel/transform-block-scoped-functions',
+    ['@babel/transform-classes', {loose: true}],
+    '@babel/transform-object-super',
+    '@babel/transform-shorthand-properties',
+    '@babel/transform-computed-properties',
+    '@babel/transform-for-of',
+    ['@babel/transform-spread', {loose: true}],
+    '@babel/transform-parameters',
+    ['@babel/transform-destructuring', {loose: true}],
+    ['@babel/transform-block-scoping', {throwIfClosureRequired: true}],
+  ],
+};
