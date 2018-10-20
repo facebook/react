5c466bbf426d26ea556a66efcbb317e23285443d
Andrew Clark <acdlite@fb.com>
2018-10-19 20:30:25 -0700

    Don't need to set expirationTime, only childExpirationTime

--- packages/react-reconciler/src/ReactFiberBeginWork.js
+++ packages/react-reconciler/src/ReactFiberBeginWork.js
@@ -1093,7 +1093,7 @@ function updateSuspenseComponent(
         ));
         fallbackChildFragment.effectTag |= Placement;
         child = primaryChildFragment;
-        primaryChildFragment.expirationTime = primaryChildFragment.childExpirationTime = NoWork;
+        primaryChildFragment.childExpirationTime = NoWork;
         // Skip the primary children, and continue working on the
         // fallback children.
         next = fallbackChildFragment;
@@ -1148,7 +1148,7 @@ function updateSuspenseComponent(
         ));
         fallbackChildFragment.effectTag |= Placement;
         child = primaryChildFragment;
-        primaryChildFragment.expirationTime = primaryChildFragment.childExpirationTime = NoWork;
+        primaryChildFragment.childExpirationTime = NoWork;
         // Skip the primary children, and continue working on the
         // fallback children.
         next = fallbackChildFragment;
@@ -1466,7 +1466,7 @@ function beginWork(
             const nextState = workInProgress.memoizedState;
             const nextDidTimeout = nextState !== null && nextState.didTimeout;
             if (nextDidTimeout) {
-              child.expirationTime = child.childExpirationTime = NoWork;
+              child.childExpirationTime = NoWork;
               return child.sibling;
             } else {
               return child;
