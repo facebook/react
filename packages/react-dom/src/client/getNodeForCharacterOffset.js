82710097f604bfaef30adb34e1ce3654070f47c6
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-10-21 23:18:32 +0500

    Add flow types in getNodeForCharacterOffset (#13908)

--- packages/react-dom/src/client/getNodeForCharacterOffset.js
+++ packages/react-dom/src/client/getNodeForCharacterOffset.js
@@ -3,6 +3,8 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
  */
 
 import {TEXT_NODE} from '../shared/HTMLNodeType';
@@ -43,7 +45,7 @@ function getSiblingNode(node) {
  * @param {number} offset
  * @return {?object}
  */
-function getNodeForCharacterOffset(root, offset) {
+function getNodeForCharacterOffset(root: Element, offset: number): ?Object {
   let node = getLeafNode(root);
   let nodeStart = 0;
   let nodeEnd = 0;
