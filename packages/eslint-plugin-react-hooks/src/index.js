b31b5606947474ef4737025be8d87f8b325532cd
Caleb Meredith <calebmeredith8@gmail.com>
2018-10-23 12:35:56 -0700

    Add ESLint rule for React Hooks

---
+++ packages/eslint-plugin-react-hooks/src/index.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+import RuleOfHooks from './RulesOfHooks';
+
+export const rules = {
+  'rules-of-hooks': RuleOfHooks,
+};
