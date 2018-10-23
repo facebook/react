37e075dc20f571497e68993e7a85b2bcdae277dd
Andrew Clark <git@andrewclark.io>
2018-10-23 14:04:06 -0700

    Revert accidentally committed existence check

--- packages/react-reconciler/src/ReactFiberCommitWork.js
+++ packages/react-reconciler/src/ReactFiberCommitWork.js
@@ -499,13 +499,7 @@ function commitUnmount(current: Fiber): void {
     case ClassComponent: {
       safelyDetachRef(current);
       const instance = current.stateNode;
-      if (
-        // Typically, a component that mounted will have an instance. However,
-        // outside of concurrent mode, a suspended component may commit without
-        // an instance, so we need to check whether it exists.
-        instance !== null &&
-        typeof instance.componentWillUnmount === 'function'
-      ) {
+      if (typeof instance.componentWillUnmount === 'function') {
         safelyCallComponentWillUnmount(current, instance);
       }
       return;
