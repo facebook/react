a155b173aec23aff7c33c3dc40c180bed96099c1
Sebastian Markbage <sema@fb.com>
2018-11-07 19:07:05 -0800

    Properly include fizz.node.js
    Also use forwarding to it from fizz.js in builds so that tests covers
    this.

--- packages/react-dom/npm/fizz.js
+++ packages/react-dom/npm/fizz.js
@@ -1,7 +1,3 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./cjs/react-dom-fizz.production.min.js');
-} else {
-  module.exports = require('./cjs/react-dom-fizz.development.js');
-}
+module.exports = require('./fizz.node');
