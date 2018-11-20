ab84d4e985866eaf1c2e925a8e4b21e10c1fec4e
raunofreiberg <freiberggg@gmail.com>
2018-11-21 01:25:56 +0800

    Run Prettier

--- packages/react/src/ReactLazy.js
+++ packages/react/src/ReactLazy.js
@@ -11,7 +11,9 @@ import type {LazyComponent, Thenable} from 'shared/ReactLazyComponent';
 
 import {REACT_LAZY_TYPE} from 'shared/ReactSymbols';
 
-export function lazy<T>(ctor: () => Thenable<{ default: T }, mixed>): LazyComponent<T> {
+export function lazy<T>(
+  ctor: () => Thenable<{default: T}, mixed>,
+): LazyComponent<T> {
   return {
     $$typeof: REACT_LAZY_TYPE,
     _ctor: ctor,
