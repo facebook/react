6f795b2f4c04f93b3a40caddf355d26e8953f2d1
raunofreiberg <freiberggg@gmail.com>
2018-11-21 01:25:15 +0800

    tests: use shared shallowEqual func instead

--- packages/react-dom/src/__tests__/ReactCompositeComponent-test.js
+++ packages/react-dom/src/__tests__/ReactCompositeComponent-test.js
@@ -5,10 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  *
  * @emails react-core
- * @flow
  */
 
 'use strict';
+
+import shallowEqual from 'shared/shallowEqual';
 
 let ChildUpdates;
 let MorphingComponent;
@@ -20,41 +21,6 @@ let ReactTestUtils;
 let PropTypes;
 
 describe('ReactCompositeComponent', () => {
-  const hasOwnProperty = Object.prototype.hasOwnProperty;
-
-  /**
-   * Performs equality by iterating through keys on an object and returning false
-   * when any key has values which are not strictly equal between the arguments.
-   * Returns true when the values of all keys are strictly equal.
-   */
-  function shallowEqual(objA: mixed, objB: mixed): boolean {
-    if (Object.is(objA, objB)) {
-      return true;
-    }
-    if (
-      typeof objA !== 'object' ||
-      objA === null ||
-      typeof objB !== 'object' ||
-      objB === null
-    ) {
-      return false;
-    }
-    const keysA = Object.keys(objA);
-    const keysB = Object.keys(objB);
-    if (keysA.length !== keysB.length) {
-      return false;
-    }
-    for (let i = 0; i < keysA.length; i++) {
-      if (
-        !hasOwnProperty.call(objB, keysA[i]) ||
-        !Object.is(objA[keysA[i]], objB[keysA[i]])
-      ) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   function shallowCompare(instance, nextProps, nextState) {
     return (
       !shallowEqual(instance.props, nextProps) ||
