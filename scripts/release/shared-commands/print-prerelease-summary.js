38c8acadb14cdf1677cc0f360c32e04e9d21bbdf
Brian Vaughn <bvaughn@fb.com>
2018-11-23 12:17:15 -0800

    Nit about pushstate-server usage

--- scripts/release/shared-commands/print-prerelease-summary.js
+++ scripts/release/shared-commands/print-prerelease-summary.js
@@ -25,10 +25,9 @@ module.exports = ({cwd}) => {
     1. Open {path ./fixtures/packaging/babel-standalone/dev.html} in the browser.
     2. It should say {quote "Hello world!"}
     3. Next go to {path ./fixtures/packaging} and run {command node build-all.js}
-    4. Install the pushstate-server module ({command npm install -g pushstate-server})
-    5. Go to the repo root and {command pushstate-server -s .}
-    6. Open {link http://localhost:9000/fixtures/packaging}
-    7. Verify every iframe shows {quote "Hello world!"}
+    4. Go to the repo root and {command npx pushstate-server . 9000}
+    5. Open {link http://localhost:9000/fixtures/packaging}
+    6. Verify every iframe shows {quote "Hello world!"}
 
     After completing the above steps, you can publish this release by running:
     {path   ${publishPath}}
