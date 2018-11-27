610bae6494d46a18fd999e4ce27c731c0989fc9d
mapkeji <mapkeji26@gmail.com>
2018-11-27 10:12:37 +0300

    Fix flow

--- packages/shared/objectIs.js
+++ packages/shared/objectIs.js
@@ -11,7 +11,7 @@
  * inlined Object.is polyfill to avoid requiring consumers ship their own
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is
  */
-function is(x, y) {
+function is(x: any, y: any) {
   return (
     (x === y && (x !== 0 || 1 / x === 1 / (y: any))) || (x !== x && y !== y) // eslint-disable-line no-self-compare
   );
