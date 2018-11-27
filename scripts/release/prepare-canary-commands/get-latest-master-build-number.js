f017a0253ea24dffe1d580bafab486d1c3c7dd7f
Brian Vaughn <bvaughn@fb.com>
2018-11-27 15:15:06 -0800

    Filter to retrieve the latest successful master build

--- scripts/release/prepare-canary-commands/get-latest-master-build-number.js
+++ scripts/release/prepare-canary-commands/get-latest-master-build-number.js
@@ -9,7 +9,9 @@ const run = async () => {
   // https://circleci.com/docs/api/#recent-builds-for-a-project-branch
   const metadataURL = `https://circleci.com/api/v1.1/project/github/facebook/react/tree/master`;
   const metadata = await http.get(metadataURL, true);
-  const build = metadata.find(entry => entry.branch === 'master').build_num;
+  const build = metadata.find(
+    entry => entry.branch === 'master' && entry.status === 'success'
+  ).build_num;
 
   return build;
 };
