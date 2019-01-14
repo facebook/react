5b51f021373955c0ea951cb8adf603cf85438d95
Sunil Pai <threepointone@oculus.com>
2019-01-14 16:35:56 +0000

    make a fork for ReactCurrentDispatcher (#14588)

--- scripts/rollup/forks.js
+++ scripts/rollup/forks.js
@@ -218,6 +218,19 @@ const forks = Object.freeze({
     }
   },
 
+  // Similarly, we preserve an inline require to ReactCurrentDispatcher.
+  // See the explanation in FB version of ReactCurrentDispatcher in www:
+  'react/src/ReactCurrentDispatcher': (bundleType, entry) => {
+    switch (bundleType) {
+      case FB_WWW_DEV:
+      case FB_WWW_PROD:
+      case FB_WWW_PROFILING:
+        return 'react/src/forks/ReactCurrentDispatcher.www.js';
+      default:
+        return null;
+    }
+  },
+
   // Different wrapping/reporting for caught errors.
   'shared/invokeGuardedCallbackImpl': (bundleType, entry) => {
     switch (bundleType) {
