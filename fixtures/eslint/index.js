437dfc4556fb5a5f61a91a019be3e6c202f672c6
Dan Abramov <dan.abramov@gmail.com>
2019-01-16 23:52:21 +0000

    Update index.js

--- fixtures/eslint/index.js
+++ fixtures/eslint/index.js
@@ -1,8 +1,8 @@
 // This is a testing playground for our lint rules.
 
 // 1. Run yarn && yarn start
-// 2. Open this folder as the project root in VSCode with ESLint extension
-// 3. Changes to the rule code should get picked up without restarting ESLint server
+// 2. "File > Add Folder to Workspace" this specific folder in VSCode with ESLint extension
+// 3. Changes to the rule source should get picked up without restarting ESLint server
 
 function Foo() {
   if (condition) {
