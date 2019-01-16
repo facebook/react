a2fae5c222313a0c401d9aa1b31ff297d413576a
Dan Abramov <dan.abramov@gmail.com>
2019-01-16 23:46:12 +0000

    Update index.js

--- fixtures/eslint/index.js
+++ fixtures/eslint/index.js
@@ -1,7 +1,7 @@
 // This is a testing playground for our lint rules.
 
 // 1. Run yarn && yarn start
-// 2. Edit this file in VSCode with ESLint extension
+// 2. Open this folder as the project root in VSCode with ESLint extension
 // 3. Changes to the rule code should get picked up without restarting ESLint server
 
 function Foo() {
