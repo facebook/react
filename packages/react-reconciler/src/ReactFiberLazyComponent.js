220dc2c929ba3e45fd21cf47dad52b5410feccf4
Grant Nestor <grantnestor@gmail.com>
2019-01-15 18:16:22 -0800

    Allow for named imports in React.lazy

--- packages/react-reconciler/src/ReactFiberLazyComponent.js
+++ packages/react-reconciler/src/ReactFiberLazyComponent.js
@@ -48,22 +48,24 @@ export function readLazyComponentType<T>(lazyComponent: LazyComponent<T>): T {
       const ctor = lazyComponent._ctor;
       const thenable = ctor();
       thenable.then(
-        moduleObject => {
+        resolvedComponent => {
           if (lazyComponent._status === Pending) {
-            const defaultExport = moduleObject.default;
+            if (resolvedComponent.default) {
+              resolvedComponent = resolvedComponent.default;
+            }
             if (__DEV__) {
-              if (defaultExport === undefined) {
+              if (resolvedComponent === undefined) {
                 warning(
                   false,
-                  'lazy: Expected the result of a dynamic import() call. ' +
+                  'lazy: Expected a promise that resolves to a React component. ' +
                     'Instead received: %s\n\nYour code should look like: \n  ' +
                     "const MyComponent = lazy(() => import('./MyComponent'))",
-                  moduleObject,
+                  resolvedComponent,
                 );
               }
             }
             lazyComponent._status = Resolved;
-            lazyComponent._result = defaultExport;
+            lazyComponent._result = resolvedComponent;
           }
         },
         error => {
