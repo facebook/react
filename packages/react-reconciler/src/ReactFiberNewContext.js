c33b093cd8db9d8e4c16cf417c2a4c95d84328a0
Dan Abramov <dan.abramov@gmail.com>
2019-01-23 16:01:19 +0000

    Tweak context invariant message

--- packages/react-reconciler/src/ReactFiberNewContext.js
+++ packages/react-reconciler/src/ReactFiberNewContext.js
@@ -330,8 +330,10 @@ export function readContext<T>(
     if (lastContextDependency === null) {
       invariant(
         currentlyRenderingFiber !== null,
-        'Context can only be read while React is ' +
-          'rendering, e.g. inside the render method or getDerivedStateFromProps.',
+        'Context can only be read while React is rendering. ' +
+          'In classes, you can read it in the render method or getDerivedStateFromProps. ' +
+          'In function components, you can read it directly in the function body, but not ' +
+          'inside Hooks like useReducer() or useMemo().',
       );
 
       // This is the first dependency for this component. Create a new list.
