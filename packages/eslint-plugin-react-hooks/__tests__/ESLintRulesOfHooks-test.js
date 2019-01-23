cb1e8a5864b6da9f0dd8fec73e940449a9701941
Yurick <yurick.hauschild@gmail.com>
2019-01-22 23:30:04 -0200

    Fix issue with multiple code branches

--- packages/eslint-plugin-react-hooks/__tests__/ESLintRulesOfHooks-test.js
+++ packages/eslint-plugin-react-hooks/__tests__/ESLintRulesOfHooks-test.js
@@ -270,6 +270,17 @@ eslintTester.run('react-hooks', ReactHooksESLintRule, {
         useState();
       }
     `,
+    `
+      // Valid because the loop doesn't change the order of hooks calls.
+      function RegressionTest() {
+        const res = [];
+        const additionalCond = true;
+        for (let i = 0; i !== 10 && additionalCond; ++i ) {
+          res.push(i);
+        }
+        React.useLayoutEffect(() => {});
+      }
+    `,
   ],
   invalid: [
     {
