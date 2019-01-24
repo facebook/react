8928f8accbc1d4fd4849e44e9096521665a59722
Yurick <yurick.hauschild@gmail.com>
2019-01-24 20:23:11 -0200

    Add performance test

--- packages/eslint-plugin-react-hooks/__tests__/ESLintRulesOfHooks-test.js
+++ packages/eslint-plugin-react-hooks/__tests__/ESLintRulesOfHooks-test.js
@@ -281,6 +281,64 @@ eslintTester.run('react-hooks', ReactHooksESLintRule, {
         React.useLayoutEffect(() => {});
       }
     `,
+    `
+      // Is valid but hard to compute by brute-forcing
+      function MyComponent() {
+        // 40 conditions
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+        if (c) {} else {}
+
+        // 10 hooks
+        useHook();
+        useHook();
+        useHook();
+        useHook();
+        useHook();
+        useHook();
+        useHook();
+        useHook();
+        useHook();
+        useHook();
+      }
+    `,
   ],
   invalid: [
     {
