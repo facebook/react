91fd158c72fa5d987df0fde8ea8c0d2a6b31a2b5
Ben Revell <benrevell.96@gmail.com>
2019-02-06 12:23:53 +0000

    chore: call the new function

--- packages/react-dom/src/test-utils/ReactTestUtils.js
+++ packages/react-dom/src/test-utils/ReactTestUtils.js
@@ -419,7 +419,7 @@ const ReactTestUtils = {
         );
       }
     }
-    ReactDOM.render(<div />, getActContainerElement);
+    ReactDOM.render(<div />, getActContainerElement());
     // we want the user to not expect a return,
     // but we want to warn if they use it like they can await on it.
     return {
