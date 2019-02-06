e8404c7945cb019b690acb6eec9776599c4860db
Brian Vaughn <bvaughn@fb.com>
2019-02-06 15:38:00 +0000

    Add current owner ref fallback for newer renderers with older react versions

--- packages/shared/ReactSharedInternals.js
+++ packages/shared/ReactSharedInternals.js
@@ -10,4 +10,19 @@ import React from 'react';
 const ReactSharedInternals =
   React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 
+// Add fallback for newer renderers running with older react package versions.
+// Current owner and dispatcher used to share the same ref,
+// but PR #14548 split them out to better support the react-debug-tools package.
+if (!ReactSharedInternals.hasOwnProperty('ReactCurrentDispatcher')) {
+  const {ReactCurrentOwner} = ReactSharedInternals;
+  ReactSharedInternals.ReactCurrentDispatcher = {
+    get current() {
+      return ReactCurrentOwner.currentDispatcher;
+    },
+    set current(value) {
+      ReactCurrentOwner.currentDispatcher = value;
+    },
+  };
+}
+
 export default ReactSharedInternals;
