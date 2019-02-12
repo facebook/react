2907c3e648280a21e7ad981515ebf57eb99f0329
Sebastian Markbåge <sebastian@calyptus.eu>
2019-02-11 21:25:44 -0800

    Partial Hydration (#14717)
    * Basic partial hydration test
    
    * Render comments around Suspense components
    
    We need this to be able to identify how far to skip ahead if we're not
    going to hydrate this subtree yet.
    
    * Add DehydratedSuspenseComponent type of work
    
    Will be used for Suspense boundaries that are left with their server
    rendered content intact.
    
    * Add comment node as hydratable instance type as placeholder for suspense
    
    * Skip past nodes within the Suspense boundary
    
    This lets us continue hydrating sibling nodes.
    
    * A dehydrated suspense boundary comment should be considered a sibling
    
    * Retry hydrating at offscreen pri or after ping if suspended
    
    * Enter hydration state when retrying dehydrated suspense boundary
    
    * Delete all children within a dehydrated suspense boundary when it's deleted
    
    * Delete server rendered content when props change before hydration completes
    
    * Make test internal
    
    * Wrap in act
    
    * Change SSR Fixture to use Partial Hydration
    
    This requires the enableSuspenseServerRenderer flag to be manually enabled
    for the build to work.
    
    * Changes to any parent Context forces clearing dehydrated content
    
    We mark dehydrated boundaries as having child work, since they might have
    components that read from the changed context.
    
    We check this in beginWork and if it does we treat it as if the input
    has changed (same as if props changes).
    
    * Wrap in feature flag
    
    * Treat Suspense boundaries without fallbacks as if not-boundaries
    
    These don't come into play for purposes of hydration.
    
    * Fix clearing of nested suspense boundaries
    
    * ping -> retry
    
    Co-Authored-By: sebmarkbage <sebastian@calyptus.eu>
    
    * Typo
    
    Co-Authored-By: sebmarkbage <sebastian@calyptus.eu>
    
    * Use didReceiveUpdate instead of manually comparing props
    
    * Leave comment for why it's ok to ignore the timeout

--- packages/shared/ReactWorkTags.js
+++ packages/shared/ReactWorkTags.js
@@ -46,3 +46,4 @@ export const MemoComponent = 14;
 export const SimpleMemoComponent = 15;
 export const LazyComponent = 16;
 export const IncompleteClassComponent = 17;
+export const DehydratedSuspenseComponent = 18;
