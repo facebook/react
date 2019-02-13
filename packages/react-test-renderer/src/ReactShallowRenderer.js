0e4135e8c2f1bc9cd14f439190aa28865395e4b1
Dan Abramov <dan.abramov@gmail.com>
2019-02-13 16:52:14 +0000

    Revert "[ShallowRenderer] Queue/rerender on dispatched action after render component with hooks (#14802)" (#14839)
    This reverts commit 6d4038f0a638d82e9e528f02cc5a86afb410cf11.

--- packages/react-test-renderer/src/ReactShallowRenderer.js
+++ packages/react-test-renderer/src/ReactShallowRenderer.js
@@ -384,7 +384,7 @@ class ReactShallowRenderer {
         'an infinite loop.',
     );
 
-    if (componentIdentity === this._previousComponentIdentity) {
+    if (componentIdentity === this._currentlyRenderingComponent) {
       // This is a render phase update. Stash it in a lazily-created map of
       // queue -> linked list of updates. After this render pass, we'll restart
       // and apply the stashed updates on top of the work-in-progress hook.
@@ -408,13 +408,10 @@ class ReactShallowRenderer {
         }
         lastRenderPhaseUpdate.next = update;
       }
-
-      if (!this._rendering) {
-        this.render(this._element, this._context);
-      }
     } else {
       // This means an update has happened after the function component has
-      // returned from a different component.
+      // returned. On the server this is a no-op. In React Fiber, the update
+      // would be scheduled for a future render.
     }
   }
 
