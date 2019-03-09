4d0a38449da7ecd4482412ada1148af4f6e42728
Andrew Clark <git@andrewclark.io>
2019-03-08 20:37:34 -0800

    Revert "test-persistent should skip files without noop"
    Seb objected to adding shelljs as a dep and I'm too lazy to worry about
    Windows support so whatever I'll just revert this.

--- scripts/jest/config.source-persistent.js
+++ scripts/jest/config.source-persistent.js
@@ -1,28 +1,9 @@
 'use strict';
 
-const path = require('path');
-const {find, grep} = require('shelljs');
 const baseConfig = require('./config.base');
 
-// Search for all the test files that reference react-noop-renderer
-const packagesDir = path.join(process.cwd(), 'packages');
-const testRegex = new RegExp(baseConfig.testRegex);
-const testFiles = find('packages').filter(filename => testRegex.test(filename));
-const testMatchFilesThatReferenceNoop = grep(
-  '-l',
-  'react-noop-renderer',
-  testFiles
-)
-  .split('\n')
-  .map(filename => `**/${path.relative(packagesDir, filename)}`);
-
 module.exports = Object.assign({}, baseConfig, {
-  // Override base regex so that we only match the files we found above.
-  testRegex: null,
-  testMatch: testMatchFilesThatReferenceNoop,
   modulePathIgnorePatterns: [
-    ...baseConfig.modulePathIgnorePatterns,
-    // These files only work in mutation mode
     'ReactIncrementalPerf',
     'ReactIncrementalUpdatesMinimalism',
     'ReactIncrementalTriangle',
