d822d4bbe7fefee12dfd87cd799119714ffa5bac
Mateusz <kulek1@users.noreply.github.com>
2019-03-14 22:40:09 +0100

    Don't set the first option as selected in select tag with `size` attribute  (#14242)
    * Set 'size' attribute to select tag if it occurs before appending options
    
    * Add comment about why size is assigned on select create. Tests
    
    I added some more clarification for why size must be set on select
    element creation:
    
    - In the source code
    - In the DOM test fixture
    - In a unit test
    
    * Use let, not const in select tag stub assignment

--- packages/react-dom/src/client/ReactDOMComponent.js
+++ packages/react-dom/src/client/ReactDOMComponent.js
@@ -419,14 +419,25 @@ export function createElement(
       // See discussion in https://github.com/facebook/react/pull/6896
       // and discussion in https://bugzilla.mozilla.org/show_bug.cgi?id=1276240
       domElement = ownerDocument.createElement(type);
-      // Normally attributes are assigned in `setInitialDOMProperties`, however the `multiple`
-      // attribute on `select`s needs to be added before `option`s are inserted. This prevents
-      // a bug where the `select` does not scroll to the correct option because singular
-      // `select` elements automatically pick the first item.
+      // Normally attributes are assigned in `setInitialDOMProperties`, however the `multiple` and `size`
+      // attributes on `select`s needs to be added before `option`s are inserted.
+      // This prevents:
+      // - a bug where the `select` does not scroll to the correct option because singular
+      //  `select` elements automatically pick the first item #13222
+      // - a bug where the `select` set the first item as selected despite the `size` attribute #14239
       // See https://github.com/facebook/react/issues/13222
-      if (type === 'select' && props.multiple) {
+      // and https://github.com/facebook/react/issues/14239
+      if (type === 'select') {
         const node = ((domElement: any): HTMLSelectElement);
-        node.multiple = true;
+        if (props.multiple) {
+          node.multiple = true;
+        } else if (props.size) {
+          // Setting a size greater than 1 causes a select to behave like `multiple=true`, where
+          // it is possible that no option is selected.
+          //
+          // This is only necessary when a select in "single selection mode".
+          node.size = props.size;
+        }
       }
     }
   } else {
