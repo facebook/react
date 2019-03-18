d1a8abe94f7b5e918a3973dc6e701b183d35531d
Jared Palmer <jared@palmer.net>
2019-03-18 10:18:28 -0400

    Update other renderers + change call to action

--- packages/react-dom/src/server/ReactPartialRendererHooks.js
+++ packages/react-dom/src/server/ReactPartialRendererHooks.js
@@ -59,10 +59,10 @@ function resolveCurrentlyRenderingComponent(): Object {
     currentlyRenderingComponent !== null,
     'Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for' +
       ' one of the following reasons:\n' +
-      '1. You might have mismatching versions of React and React DOM\n' +
+      '1. You might have mismatching versions of React and the renderer (such as React DOM)\n' +
       '2. You might be breaking the Rules of Hooks\n' +
       '3. You might have more than one copy of React in the same app\n' +
-      'See https://fb.me/react-invalid-hook-call for more information.',
+      'See https://fb.me/react-invalid-hook-call for tips about how to debug and fix this problem.',
   );
   if (__DEV__) {
     warning(
