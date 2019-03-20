c05b4b81f91c0b43a02e101d6a37b3de768f017b
Dan Abramov <dan.abramov@gmail.com>
2019-03-20 13:40:36 +0000

    Link to useLayoutEffect gist in a warning (#15158)

--- packages/react-dom/src/server/ReactPartialRendererHooks.js
+++ packages/react-dom/src/server/ReactPartialRendererHooks.js
@@ -393,7 +393,8 @@ export function useLayoutEffect(
       "be encoded into the server renderer's output format. This will lead " +
       'to a mismatch between the initial, non-hydrated UI and the intended ' +
       'UI. To avoid this, useLayoutEffect should only be used in ' +
-      'components that render exclusively on the client.',
+      'components that render exclusively on the client. ' +
+      'See https://fb.me/react-uselayouteffect-ssr for common fixes.',
   );
 }
 
