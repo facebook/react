ba994445154e9546645c0701b1666c4f0406cdcc
Brian Vaughn <bvaughn@fb.com>
2019-04-08 18:34:48 -0700

    Use @reach MenuButton for owner stack menu

--- src/devtools/views/DevTools.js
+++ src/devtools/views/DevTools.js
@@ -14,6 +14,7 @@ import ReactLogo from './ReactLogo';
 
 import styles from './DevTools.css';
 
+import '@reach/menu-button/styles.css';
 import './root.css';
 
 import type { Bridge } from '../../types';
