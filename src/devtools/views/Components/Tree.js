0281a0d905e9f961c8c881183f67b2c33cb03b8a
Dan Abramov <dan.abramov@gmail.com>
2019-04-11 13:20:32 +0100

    Add a missing dep

--- src/devtools/views/Components/Tree.js
+++ src/devtools/views/Components/Tree.js
@@ -154,6 +154,7 @@ export default function Tree(props: Props) {
     };
   }, [
     selectedElementID,
+    selectChildElementInTree,
     selectNextElementInTree,
     selectParentElementInTree,
     selectPreviousElementInTree,
