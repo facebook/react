7caa1b2a0ef1d935971edeb202d776fbefd1ffa2
Sunil Pai <threepointone@fb.com>
2019-04-14 19:40:16 +0100

    augh prettier

--- fixtures/act/src/index.js
+++ fixtures/act/src/index.js
@@ -44,64 +44,64 @@ async function testMixRenderers() {
 
 async function testARTDOM() {
   ARTCurrentMode.setCurrent(ARTSVGMode);
-  let setState
+  let setState;
   function TestComponent(props) {
     // eslint-disable-next-line no-unused-vars
-    const [state, _setState] = React.useState(0)
-    setState = _setState
-        
+    const [state, _setState] = React.useState(0);
+    setState = _setState;
+
     return (
       <Surface width={150} height={200}>
         <Shape
-        fill="#3C5A99"
-        key="b"
-        scale={0.5}
-        x={50}
-        y={50}
-        title="This is an F"
-        cursor="pointer">
-        M64.564,38.583H54l0.008-5.834c0-3.035,0.293-4.666,4.657-4.666
-        h5.833V16.429h-9.33c-11.213,0-15.159,5.654-15.159,15.16v6.994
-        h-6.99v11.652h6.99v33.815H54V50.235h9.331L64.564,38.583z
-      </Shape>
+          fill="#3C5A99"
+          key="b"
+          scale={0.5}
+          x={50}
+          y={50}
+          title="This is an F"
+          cursor="pointer">
+          M64.564,38.583H54l0.008-5.834c0-3.035,0.293-4.666,4.657-4.666
+          h5.833V16.429h-9.33c-11.213,0-15.159,5.654-15.159,15.16v6.994
+          h-6.99v11.652h6.99v33.815H54V50.235h9.331L64.564,38.583z
+        </Shape>
       </Surface>
     );
   }
   ReactDOM.render(<TestComponent />, window.root);
   ReactTestUtils.act(() => {
-    setState(2)  
-  })  
+    setState(2);
+  });
 }
 
 async function testARTTest() {
   ARTCurrentMode.setCurrent(ARTSVGMode);
-  let setState
+  let setState;
   function TestComponent(props) {
     // eslint-disable-next-line no-unused-vars
-    const [state, _setState] = React.useState(0)
-    setState = _setState
-        
+    const [state, _setState] = React.useState(0);
+    setState = _setState;
+
     return (
       <Surface width={150} height={200}>
         <Shape
-        fill="#3C5A99"
-        key="b"
-        scale={0.5}
-        x={50}
-        y={50}
-        title="This is an F"
-        cursor="pointer">
-        M64.564,38.583H54l0.008-5.834c0-3.035,0.293-4.666,4.657-4.666
-        h5.833V16.429h-9.33c-11.213,0-15.159,5.654-15.159,15.16v6.994
-        h-6.99v11.652h6.99v33.815H54V50.235h9.331L64.564,38.583z
-      </Shape>
+          fill="#3C5A99"
+          key="b"
+          scale={0.5}
+          x={50}
+          y={50}
+          title="This is an F"
+          cursor="pointer">
+          M64.564,38.583H54l0.008-5.834c0-3.035,0.293-4.666,4.657-4.666
+          h5.833V16.429h-9.33c-11.213,0-15.159,5.654-15.159,15.16v6.994
+          h-6.99v11.652h6.99v33.815H54V50.235h9.331L64.564,38.583z
+        </Shape>
       </Surface>
     );
   }
   ReactTestRenderer.create(<TestComponent />);
   ReactTestRenderer.act(() => {
-    setState(2)  
-  })  
+    setState(2);
+  });
 }
 
 async function run() {
