d320bbed28e4fcda4f1f457128a76878b5b58f82
Andrew Clark <git@andrewclark.io>
2019-04-29 16:10:39 -0700

    Use lowercase entry points for event modules
    Matches npm convention

--- packages/react-events/npm/Drag.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./cjs/react-events-drag.production.min.js');
-} else {
-  module.exports = require('./cjs/react-events-drag.development.js');
-}
