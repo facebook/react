1aa0f63e6a0e53ce9940d8403b5a990f89c0ef46
Dan Abramov <dan.abramov@gmail.com>
2019-05-11 02:52:15 +0100

    Add a minimal failing test for hot reload

---
+++ packages/react-dom/src/__tests__/ReactFresh-test.internal.js
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails react-core
+ */
+
+'use strict';
+
+let React;
+let ReactDOM;
+let act;
+
+describe('ReactFresh', () => {
+  let container;
+  let familiesByID;
+  let familiesByFn;
+
+  beforeEach(() => {
+    jest.resetModules();
+    React = require('react');
+    ReactDOM = require('react-dom');
+    act = require('react-dom/test-utils').act;
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    familiesByID = new Map();
+    familiesByFn = new WeakMap();
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+    familiesByFn = null;
+  });
+
+  function render(version) {
+    const Component = version();
+    act(() => {
+      ReactDOM.render(<Component />, container);
+    });
+  }
+
+  function patch(version) {
+    const Component = version();
+    // TODO: use a DevTools Hook to force update instead.
+    act(() => {
+      ReactDOM.render(<Component />, container);
+    });
+  }
+
+  function __register__(fn, id) {
+    let family = familiesByID.get(id);
+    if (family === undefined) {
+      family = {current: null};
+      familiesByID.set(id, family);
+    }
+    family.current = fn;
+    familiesByFn.set(fn, family);
+    // TODO: record which families were updated.
+    // TODO: invalidation based on signatures.
+  }
+
+  it('can preserve state', () => {
+    if (__DEV__) {
+      render(() => {
+        function Hello() {
+          const [val, setVal] = React.useState(0);
+          return (
+            <p style={{color: 'blue'}} onClick={() => setVal(val + 1)}>
+              {val}
+            </p>
+          );
+        }
+        __register__(Hello, 'Hello');
+        return Hello;
+      });
+
+      // Bump the state before patching.
+      const el = container.firstChild;
+      expect(el.textContent).toBe('0');
+      expect(el.style.color).toBe('blue');
+      act(() => {
+        el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+      });
+      expect(el.textContent).toBe('1');
+
+      // Perform a hot update.
+      patch(() => {
+        function Hello() {
+          const [val, setVal] = React.useState(0);
+          return (
+            <p style={{color: 'red'}} onClick={() => setVal(val + 1)}>
+              {val}
+            </p>
+          );
+        }
+        __register__(Hello, 'Hello');
+        return Hello;
+      });
+
+      // Assert the state was preserved but color changed.
+      expect(container.firstChild).toBe(el);
+      expect(el.textContent).toBe('1');
+      expect(el.style.color).toBe('red');
+
+      // Bump the state again.
+      act(() => {
+        el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+      });
+      expect(container.firstChild).toBe(el);
+      expect(el.textContent).toBe('2');
+      expect(el.style.color).toBe('red');
+    }
+  });
+});
