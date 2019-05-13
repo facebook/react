4ac685db026018644900004a39755c723b6efc9c
Dan Abramov <dan.abramov@gmail.com>
2019-05-13 02:36:57 +0100

    Add missing __DEV__ to tests

--- packages/react-dom/src/__tests__/ReactFresh-test.internal.js
+++ packages/react-dom/src/__tests__/ReactFresh-test.internal.js
@@ -187,100 +187,169 @@ describe('ReactFresh', () => {
   });
 
   it('can preserve state for forwardRef', () => {
-    let OuterV1 = render(() => {
-      function Hello() {
-        const [val, setVal] = React.useState(0);
-        return (
-          <p style={{color: 'blue'}} onClick={() => setVal(val + 1)}>
-            {val}
-          </p>
-        );
-      }
-      __register__(Hello, 'Hello');
-
-      const Outer = React.forwardRef(() => <Hello />);
-      __register__(Outer, 'Outer');
-      return Outer;
-    });
-
-    // Bump the state before patching.
-    const el = container.firstChild;
-    expect(el.textContent).toBe('0');
-    expect(el.style.color).toBe('blue');
-    act(() => {
-      el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    });
-    expect(el.textContent).toBe('1');
-
-    // Perform a hot update.
-    let OuterV2 = patch(() => {
-      function Hello() {
-        const [val, setVal] = React.useState(0);
-        return (
-          <p style={{color: 'red'}} onClick={() => setVal(val + 1)}>
-            {val}
-          </p>
-        );
-      }
-      __register__(Hello, 'Hello');
-
-      const Outer = React.forwardRef(() => <Hello />);
-      __register__(Outer, 'Outer');
-      return Outer;
-    });
-
-    // Assert the state was preserved but color changed.
-    expect(container.firstChild).toBe(el);
-    expect(el.textContent).toBe('1');
-    expect(el.style.color).toBe('red');
-
-    // Bump the state again.
-    act(() => {
-      el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    });
-    expect(container.firstChild).toBe(el);
-    expect(el.textContent).toBe('2');
-    expect(el.style.color).toBe('red');
-
-    // Perform top-down renders with both fresh and stale types.
-    // Neither should change the state or color.
-    // They should always resolve to the latest version.
-    render(() => OuterV1);
-    render(() => OuterV2);
-    render(() => OuterV1);
-    expect(container.firstChild).toBe(el);
-    expect(el.textContent).toBe('2');
-    expect(el.style.color).toBe('red');
-
-    // Finally, a render with incompatible type should reset it.
-    render(() => {
-      function Hello() {
-        const [val, setVal] = React.useState(0);
-        return (
-          <p style={{color: 'blue'}} onClick={() => setVal(val + 1)}>
-            {val}
-          </p>
-        );
-      }
-      __register__(Hello, 'Hello');
-
-      // Note: no forwardRef wrapper this time.
-      return Hello;
-    });
-
-    expect(container.firstChild).not.toBe(el);
-    const newEl = container.firstChild;
-    expect(newEl.textContent).toBe('0');
-    expect(newEl.style.color).toBe('blue');
-  });
-
-  it('should not consider two forwardRefs around the same type to be equivalent', () => {
-    let ParentV1 = render(
-      () => {
+    if (__DEV__) {
+      let OuterV1 = render(() => {
         function Hello() {
           const [val, setVal] = React.useState(0);
           return (
             <p style={{color: 'blue'}} onClick={() => setVal(val + 1)}>
+              {val}
+            </p>
+          );
+        }
+        __register__(Hello, 'Hello');
+
+        const Outer = React.forwardRef(() => <Hello />);
+        __register__(Outer, 'Outer');
+        return Outer;
+      });
+
+      // Bump the state before patching.
+      const el = container.firstChild;
+      expect(el.textContent).toBe('0');
+      expect(el.style.color).toBe('blue');
+      act(() => {
+        el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+      });
+      expect(el.textContent).toBe('1');
+
+      // Perform a hot update.
+      let OuterV2 = patch(() => {
+        function Hello() {
+          const [val, setVal] = React.useState(0);
+          return (
+            <p style={{color: 'red'}} onClick={() => setVal(val + 1)}>
+              {val}
+            </p>
+          );
+        }
+        __register__(Hello, 'Hello');
+
+        const Outer = React.forwardRef(() => <Hello />);
+        __register__(Outer, 'Outer');
+        return Outer;
+      });
+
+      // Assert the state was preserved but color changed.
+      expect(container.firstChild).toBe(el);
+      expect(el.textContent).toBe('1');
+      expect(el.style.color).toBe('red');
+
+      // Bump the state again.
+      act(() => {
+        el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+      });
+      expect(container.firstChild).toBe(el);
+      expect(el.textContent).toBe('2');
+      expect(el.style.color).toBe('red');
+
+      // Perform top-down renders with both fresh and stale types.
+      // Neither should change the state or color.
+      // They should always resolve to the latest version.
+      render(() => OuterV1);
+      render(() => OuterV2);
+      render(() => OuterV1);
+      expect(container.firstChild).toBe(el);
+      expect(el.textContent).toBe('2');
+      expect(el.style.color).toBe('red');
+
+      // Finally, a render with incompatible type should reset it.
+      render(() => {
+        function Hello() {
+          const [val, setVal] = React.useState(0);
+          return (
+            <p style={{color: 'blue'}} onClick={() => setVal(val + 1)}>
+              {val}
+            </p>
+          );
+        }
+        __register__(Hello, 'Hello');
+
+        // Note: no forwardRef wrapper this time.
+        return Hello;
+      });
+
+      expect(container.firstChild).not.toBe(el);
+      const newEl = container.firstChild;
+      expect(newEl.textContent).toBe('0');
+      expect(newEl.style.color).toBe('blue');
+    }
+  });
+
+  it('should not consider two forwardRefs around the same type to be equivalent', () => {
+    if (__DEV__) {
+      let ParentV1 = render(
+        () => {
+          function Hello() {
+            const [val, setVal] = React.useState(0);
+            return (
+              <p style={{color: 'blue'}} onClick={() => setVal(val + 1)}>
+                {val}
+              </p>
+            );
+          }
+          __register__(Hello, 'Hello');
+
+          function renderInner() {
+            return <Hello />;
+          }
+          // Both of these are wrappers around the same inner function.
+          // They should be treated as distinct types across reloads.
+          let ForwardRefA = React.forwardRef(renderInner);
+          __register__(ForwardRefA, 'ForwardRefA');
+          let ForwardRefB = React.forwardRef(renderInner);
+          __register__(ForwardRefB, 'ForwardRefB');
+
+          function Parent({cond}) {
+            return cond ? <ForwardRefA /> : <ForwardRefB />;
+          }
+          __register__(Parent, 'Parent');
+
+          return Parent;
+        },
+        {cond: true},
+      );
+
+      // Bump the state before switching up types.
+      let el = container.firstChild;
+      expect(el.textContent).toBe('0');
+      expect(el.style.color).toBe('blue');
+      act(() => {
+        el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+      });
+      expect(el.textContent).toBe('1');
+
+      // Switching up the inner types should reset the state.
+      render(() => ParentV1, {cond: false});
+      expect(el).not.toBe(container.firstChild);
+      el = container.firstChild;
+      expect(el.textContent).toBe('0');
+      expect(el.style.color).toBe('blue');
+
+      act(() => {
+        el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+      });
+      expect(el.textContent).toBe('1');
+
+      // Switch them up back again.
+      render(() => ParentV1, {cond: true});
+      expect(el).not.toBe(container.firstChild);
+      el = container.firstChild;
+      expect(el.textContent).toBe('0');
+      expect(el.style.color).toBe('blue');
+
+      // Now bump up the state to prepare for patching.
+      act(() => {
+        el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+      });
+      expect(el.textContent).toBe('1');
+
+      // Patch to change the color.
+      let ParentV2 = patch(() => {
+        function Hello() {
+          const [val, setVal] = React.useState(0);
+          return (
+            <p style={{color: 'red'}} onClick={() => setVal(val + 1)}>
               {val}
             </p>
           );
@@ -303,204 +372,143 @@ describe('ReactFresh', () => {
         __register__(Parent, 'Parent');
 
         return Parent;
-      },
-      {cond: true},
-    );
+      });
 
-    // Bump the state before switching up types.
-    let el = container.firstChild;
-    expect(el.textContent).toBe('0');
-    expect(el.style.color).toBe('blue');
-    act(() => {
-      el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    });
-    expect(el.textContent).toBe('1');
+      // The state should be intact; the color should change.
+      expect(el).toBe(container.firstChild);
+      expect(el.textContent).toBe('1');
+      expect(el.style.color).toBe('red');
 
-    // Switching up the inner types should reset the state.
-    render(() => ParentV1, {cond: false});
-    expect(el).not.toBe(container.firstChild);
-    el = container.firstChild;
-    expect(el.textContent).toBe('0');
-    expect(el.style.color).toBe('blue');
+      // Switching up the condition should still reset the state.
+      render(() => ParentV2, {cond: false});
+      expect(el).not.toBe(container.firstChild);
+      el = container.firstChild;
+      expect(el.textContent).toBe('0');
+      expect(el.style.color).toBe('red');
 
-    act(() => {
-      el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    });
-    expect(el.textContent).toBe('1');
+      // Now bump up the state to prepare for top-level renders.
+      act(() => {
+        el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+      });
+      expect(el).toBe(container.firstChild);
+      expect(el.textContent).toBe('1');
+      expect(el.style.color).toBe('red');
 
-    // Switch them up back again.
-    render(() => ParentV1, {cond: true});
-    expect(el).not.toBe(container.firstChild);
-    el = container.firstChild;
-    expect(el.textContent).toBe('0');
-    expect(el.style.color).toBe('blue');
-
-    // Now bump up the state to prepare for patching.
-    act(() => {
-      el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    });
-    expect(el.textContent).toBe('1');
-
-    // Patch to change the color.
-    let ParentV2 = patch(() => {
-      function Hello() {
-        const [val, setVal] = React.useState(0);
-        return (
-          <p style={{color: 'red'}} onClick={() => setVal(val + 1)}>
-            {val}
-          </p>
-        );
-      }
-      __register__(Hello, 'Hello');
-
-      function renderInner() {
-        return <Hello />;
-      }
-      // Both of these are wrappers around the same inner function.
-      // They should be treated as distinct types across reloads.
-      let ForwardRefA = React.forwardRef(renderInner);
-      __register__(ForwardRefA, 'ForwardRefA');
-      let ForwardRefB = React.forwardRef(renderInner);
-      __register__(ForwardRefB, 'ForwardRefB');
-
-      function Parent({cond}) {
-        return cond ? <ForwardRefA /> : <ForwardRefB />;
-      }
-      __register__(Parent, 'Parent');
-
-      return Parent;
-    });
-
-    // The state should be intact; the color should change.
-    expect(el).toBe(container.firstChild);
-    expect(el.textContent).toBe('1');
-    expect(el.style.color).toBe('red');
-
-    // Switching up the condition should still reset the state.
-    render(() => ParentV2, {cond: false});
-    expect(el).not.toBe(container.firstChild);
-    el = container.firstChild;
-    expect(el.textContent).toBe('0');
-    expect(el.style.color).toBe('red');
-
-    // Now bump up the state to prepare for top-level renders.
-    act(() => {
-      el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    });
-    expect(el).toBe(container.firstChild);
-    expect(el.textContent).toBe('1');
-    expect(el.style.color).toBe('red');
-
-    // Finally, verify using top-level render with stale type keeps state.
-    render(() => ParentV1);
-    render(() => ParentV2);
-    render(() => ParentV1);
-    expect(container.firstChild).toBe(el);
-    expect(el.textContent).toBe('1');
-    expect(el.style.color).toBe('red');
+      // Finally, verify using top-level render with stale type keeps state.
+      render(() => ParentV1);
+      render(() => ParentV2);
+      render(() => ParentV1);
+      expect(container.firstChild).toBe(el);
+      expect(el.textContent).toBe('1');
+      expect(el.style.color).toBe('red');
+    }
   });
 
   it('can update forwardRef render function with its wrapper', () => {
-    render(() => {
-      function Hello({color}) {
-        const [val, setVal] = React.useState(0);
-        return (
-          <p style={{color}} onClick={() => setVal(val + 1)}>
-            {val}
-          </p>
-        );
-      }
-      __register__(Hello, 'Hello');
+    if (__DEV__) {
+      render(() => {
+        function Hello({color}) {
+          const [val, setVal] = React.useState(0);
+          return (
+            <p style={{color}} onClick={() => setVal(val + 1)}>
+              {val}
+            </p>
+          );
+        }
+        __register__(Hello, 'Hello');
 
-      const Outer = React.forwardRef(() => <Hello color="blue" />);
-      __register__(Outer, 'Outer');
-      return Outer;
-    });
+        const Outer = React.forwardRef(() => <Hello color="blue" />);
+        __register__(Outer, 'Outer');
+        return Outer;
+      });
 
-    // Bump the state before patching.
-    const el = container.firstChild;
-    expect(el.textContent).toBe('0');
-    expect(el.style.color).toBe('blue');
-    act(() => {
-      el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    });
-    expect(el.textContent).toBe('1');
+      // Bump the state before patching.
+      const el = container.firstChild;
+      expect(el.textContent).toBe('0');
+      expect(el.style.color).toBe('blue');
+      act(() => {
+        el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+      });
+      expect(el.textContent).toBe('1');
 
-    // Perform a hot update.
-    patch(() => {
-      function Hello({color}) {
-        const [val, setVal] = React.useState(0);
-        return (
-          <p style={{color}} onClick={() => setVal(val + 1)}>
-            {val}
-          </p>
-        );
-      }
-      __register__(Hello, 'Hello');
+      // Perform a hot update.
+      patch(() => {
+        function Hello({color}) {
+          const [val, setVal] = React.useState(0);
+          return (
+            <p style={{color}} onClick={() => setVal(val + 1)}>
+              {val}
+            </p>
+          );
+        }
+        __register__(Hello, 'Hello');
 
-      const Outer = React.forwardRef(() => <Hello color="red" />);
-      __register__(Outer, 'Outer');
-      return Outer;
-    });
+        const Outer = React.forwardRef(() => <Hello color="red" />);
+        __register__(Outer, 'Outer');
+        return Outer;
+      });
 
-    // Assert the state was preserved but color changed.
-    expect(container.firstChild).toBe(el);
-    expect(el.textContent).toBe('1');
-    expect(el.style.color).toBe('red');
+      // Assert the state was preserved but color changed.
+      expect(container.firstChild).toBe(el);
+      expect(el.textContent).toBe('1');
+      expect(el.style.color).toBe('red');
+    }
   });
 
   it('can update forwardRef render function in isolation', () => {
-    render(() => {
-      function Hello({color}) {
-        const [val, setVal] = React.useState(0);
-        return (
-          <p style={{color}} onClick={() => setVal(val + 1)}>
-            {val}
-          </p>
-        );
-      }
-      __register__(Hello, 'Hello');
+    if (__DEV__) {
+      render(() => {
+        function Hello({color}) {
+          const [val, setVal] = React.useState(0);
+          return (
+            <p style={{color}} onClick={() => setVal(val + 1)}>
+              {val}
+            </p>
+          );
+        }
+        __register__(Hello, 'Hello');
 
-      function renderHello() {
-        return <Hello color="blue" />;
-      }
-      __register__(renderHello, 'renderHello');
+        function renderHello() {
+          return <Hello color="blue" />;
+        }
+        __register__(renderHello, 'renderHello');
 
-      return React.forwardRef(renderHello);
-    });
+        return React.forwardRef(renderHello);
+      });
 
-    // Bump the state before patching.
-    const el = container.firstChild;
-    expect(el.textContent).toBe('0');
-    expect(el.style.color).toBe('blue');
-    act(() => {
-      el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    });
-    expect(el.textContent).toBe('1');
+      // Bump the state before patching.
+      const el = container.firstChild;
+      expect(el.textContent).toBe('0');
+      expect(el.style.color).toBe('blue');
+      act(() => {
+        el.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+      });
+      expect(el.textContent).toBe('1');
 
-    // Perform a hot update of just the rendering function.
-    patch(() => {
-      function Hello({color}) {
-        const [val, setVal] = React.useState(0);
-        return (
-          <p style={{color}} onClick={() => setVal(val + 1)}>
-            {val}
-          </p>
-        );
-      }
-      __register__(Hello, 'Hello');
+      // Perform a hot update of just the rendering function.
+      patch(() => {
+        function Hello({color}) {
+          const [val, setVal] = React.useState(0);
+          return (
+            <p style={{color}} onClick={() => setVal(val + 1)}>
+              {val}
+            </p>
+          );
+        }
+        __register__(Hello, 'Hello');
 
-      function renderHello() {
-        return <Hello color="red" />;
-      }
-      __register__(renderHello, 'renderHello');
+        function renderHello() {
+          return <Hello color="red" />;
+        }
+        __register__(renderHello, 'renderHello');
 
-      // Not updating the wrapper.
-    });
+        // Not updating the wrapper.
+      });
 
-    // Assert the state was preserved but color changed.
-    expect(container.firstChild).toBe(el);
-    expect(el.textContent).toBe('1');
-    expect(el.style.color).toBe('red');
+      // Assert the state was preserved but color changed.
+      expect(container.firstChild).toBe(el);
+      expect(el.textContent).toBe('1');
+      expect(el.style.color).toBe('red');
+    }
   });
 });
