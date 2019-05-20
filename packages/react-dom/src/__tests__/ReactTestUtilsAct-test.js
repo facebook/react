3c2f42d56ef4d0cd47f5bcd3e7cce269af6b2acf
Dominic Gannaway <dg@domgan.com>
2019-05-20 23:36:07 +0100

    Remove .only

--- packages/react-dom/src/__tests__/ReactTestUtilsAct-test.js
+++ packages/react-dom/src/__tests__/ReactTestUtilsAct-test.js
@@ -83,7 +83,7 @@ function runActTests(label, render, unmount) {
         expect(Scheduler).toHaveYielded([100]);
       });
 
-      it.only('flushes effects on every call', () => {
+      it('flushes effects on every call', () => {
         function App() {
           let [ctr, setCtr] = React.useState(0);
           React.useEffect(() => {
