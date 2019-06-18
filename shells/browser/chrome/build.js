0e4005e0a19761bff83c0329b4d46078c689ff37
Brian Vaughn <bvaughn@fb.com>
2019-06-18 15:44:22 -0700

    Added command to build Chrome CRX

--- shells/browser/chrome/build.js
+++ shells/browser/chrome/build.js
@@ -1,10 +1,31 @@
 #!/usr/bin/env node
 
 const chalk = require('chalk');
+const { execSync } = require('child_process');
+const { existsSync } = require('fs');
+const { join, relative } = require('path');
+const { argv } = require('yargs');
 const build = require('../shared/build');
 
 const main = async () => {
+  const { crx, keyPath } = argv;
+
+  if (crx) {
+    if (!keyPath || !existsSync(keyPath)) {
+      console.error('Must specify a key file (.pem) to build CRX');
+      process.exit(1);
+    }
+  }
+
   await build('chrome');
+
+  if (crx) {
+    const cwd = join(__dirname, 'build');
+    const relativeKeyPath = join(relative(cwd, process.cwd()), keyPath);
+    execSync(`crx pack ./unpacked -o ReactDevTools.crx -p ${relativeKeyPath}`, {
+      cwd,
+    });
+  }
 
   console.log(chalk.green('\nThe Chrome extension has been built!'));
   console.log(chalk.green('You can test this build by running:'));
