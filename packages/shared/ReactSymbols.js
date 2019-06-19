687f26717c40f6bb254a7d541777de67df0905d5
muji.wyh <muji.wyh@alibaba-inc.com>
2019-06-19 17:46:13 +0800

    Remote hard code like hasSymbol ? Symbol.for('react.element') : 0xeac7

--- packages/shared/ReactSymbols.js
+++ packages/shared/ReactSymbols.js
@@ -10,55 +10,28 @@
 // The Symbol used to tag the ReactElement-like types. If there is no native Symbol
 // nor polyfill, then a plain number is used for performance.
 const hasSymbol = typeof Symbol === 'function' && Symbol.for;
+const getReactType = (typeName, fallback) => hasSymbol ? Symbol.for(typeName) : fallback;
 
-export const REACT_ELEMENT_TYPE = hasSymbol
-  ? Symbol.for('react.element')
-  : 0xeac7;
-export const REACT_PORTAL_TYPE = hasSymbol
-  ? Symbol.for('react.portal')
-  : 0xeaca;
-export const REACT_FRAGMENT_TYPE = hasSymbol
-  ? Symbol.for('react.fragment')
-  : 0xeacb;
-export const REACT_STRICT_MODE_TYPE = hasSymbol
-  ? Symbol.for('react.strict_mode')
-  : 0xeacc;
-export const REACT_PROFILER_TYPE = hasSymbol
-  ? Symbol.for('react.profiler')
-  : 0xead2;
-export const REACT_PROVIDER_TYPE = hasSymbol
-  ? Symbol.for('react.provider')
-  : 0xeacd;
-export const REACT_CONTEXT_TYPE = hasSymbol
-  ? Symbol.for('react.context')
-  : 0xeace;
+export const REACT_ELEMENT_TYPE = getReactType('react.element', 0xeac7);
+export const REACT_PORTAL_TYPE = getReactType('react.portal', 0xeaca);
+export const REACT_FRAGMENT_TYPE = getReactType('react.fragment', 0xeacb);
+export const REACT_STRICT_MODE_TYPE = getReactType('react.strict_mode', 0xeacc);
+export const REACT_PROFILER_TYPE = getReactType('react.profiler', 0xead2);
+export const REACT_PROVIDER_TYPE = getReactType('react.provider', 0xeacd);
+export const REACT_CONTEXT_TYPE = getReactType('react.context', 0xeace);
 // TODO: We don't use AsyncMode or ConcurrentMode anymore. They were temporary
 // (unstable) APIs that have been removed. Can we remove the symbols?
-export const REACT_ASYNC_MODE_TYPE = hasSymbol
-  ? Symbol.for('react.async_mode')
-  : 0xeacf;
-export const REACT_CONCURRENT_MODE_TYPE = hasSymbol
-  ? Symbol.for('react.concurrent_mode')
-  : 0xeacf;
-export const REACT_FORWARD_REF_TYPE = hasSymbol
-  ? Symbol.for('react.forward_ref')
-  : 0xead0;
-export const REACT_SUSPENSE_TYPE = hasSymbol
-  ? Symbol.for('react.suspense')
-  : 0xead1;
+export const REACT_ASYNC_MODE_TYPE = getReactType('react.async_mode', 0xeacf);
+export const REACT_CONCURRENT_MODE_TYPE = getReactType('react.concurrent_mode', 0xeacf);
+export const REACT_FORWARD_REF_TYPE = getReactType('react.forward_ref', 0xead0);
+export const REACT_SUSPENSE_TYPE = getReactType('react.suspense', 0xead1);
 export const REACT_MEMO_TYPE = hasSymbol ? Symbol.for('react.memo') : 0xead3;
 export const REACT_LAZY_TYPE = hasSymbol ? Symbol.for('react.lazy') : 0xead4;
-export const REACT_EVENT_COMPONENT_TYPE = hasSymbol
-  ? Symbol.for('react.event_component')
-  : 0xead5;
-export const REACT_EVENT_TARGET_TYPE = hasSymbol
-  ? Symbol.for('react.event_target')
-  : 0xead6;
+export const REACT_EVENT_COMPONENT_TYPE = getReactType('react.event_component', 0xead5)
+export const REACT_EVENT_TARGET_TYPE = getReactType('react.event_target', 0xead6)
 
 // React event targets
-export const REACT_EVENT_TARGET_TOUCH_HIT = hasSymbol
-  ? Symbol.for('react.event_target.touch_hit')
-  : 0xead7;
+export const REACT_EVENT_TARGET_TOUCH_HIT = getReactType('react.event_target.touch_hit', 0xead7)
 
 const MAYBE_ITERATOR_SYMBOL = typeof Symbol === 'function' && Symbol.iterator;
 const FAUX_ITERATOR_SYMBOL = '@@iterator';
