d8157d334eef9740244347512dd106cc11ae4940
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-06-23 14:10:27 +0530

    Fix
    linting errors

--- packages/react/npm/index.js
+++ packages/react/npm/index.js
@@ -1,4 +1,4 @@
 'use strict';
 
-const fileName = process.env.NODE_ENV === 'production' ? 'react.production.min.js' : 'react.development.js';
-module.exports = require(`./cjs/${fileName}`);
+var fileName = process.env.NODE_ENV === 'production' ? 'react.production.min.js' : 'react.development.js';
+module.exports = require('./cjs/' + fileName);
