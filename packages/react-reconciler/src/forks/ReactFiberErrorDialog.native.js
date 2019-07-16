7af7722229faf0abd6ac382bdafc83d44b4b00c9
Moti Zilberman <motiz88@gmail.com>
2019-07-16 09:22:55 +0100

    Move ReactFiberErrorDialog RN fork into RN itself

--- packages/react-reconciler/src/forks/ReactFiberErrorDialog.native.js
+++ packages/react-reconciler/src/forks/ReactFiberErrorDialog.native.js
@@ -10,41 +10,14 @@
 import type {CapturedError} from '../ReactCapturedValue';
 
 // Module provided by RN:
-import {ExceptionsManager} from 'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface';
+import {ReactFiberErrorDialog as RNImpl} from 'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface';
+import invariant from 'shared/invariant';
 
-/**
- * Intercept lifecycle errors and ensure they are shown with the correct stack
- * trace within the native redbox component.
- */
+invariant(
+  typeof RNImpl.showErrorDialog === 'function',
+  'Expected ReactFiberErrorDialog.showErrorDialog to be a function.',
+);
+
 export function showErrorDialog(capturedError: CapturedError): boolean {
-  const {componentStack, error} = capturedError;
-
-  let errorToHandle: Error;
-
-  // Typically Errors are thrown but eg strings or null can be thrown as well.
-  if (error instanceof Error) {
-    const {message, name} = error;
-
-    const summary = message ? `${name}: ${message}` : name;
-
-    errorToHandle = error;
-
-    try {
-      errorToHandle.message = `${summary}\n\nThis error is located at:${componentStack}`;
-    } catch (e) {}
-  } else if (typeof error === 'string') {
-    errorToHandle = new Error(
-      `${error}\n\nThis error is located at:${componentStack}`,
-    );
-  } else {
-    errorToHandle = new Error(`Unspecified error at:${componentStack}`);
-  }
-
-  ExceptionsManager.handleException(errorToHandle, false);
-
-  // Return false here to prevent ReactFiberErrorLogger default behavior of
-  // logging error details to console.error. Calls to console.error are
-  // automatically routed to the native redbox controller, which we've already
-  // done above by calling ExceptionsManager.
-  return false;
+  return RNImpl.showErrorDialog(capturedError);
 }
