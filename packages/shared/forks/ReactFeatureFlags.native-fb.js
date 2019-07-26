c177c8963cb877cd9ed750b9ef665de1cbb47de9
Luna Ruan <luna@fb.com>
2019-07-25 19:34:20 -0700

    added warnAboutStringRefs to more files

--- packages/shared/forks/ReactFeatureFlags.native-fb.js
+++ packages/shared/forks/ReactFeatureFlags.native-fb.js
@@ -39,6 +39,7 @@ export const revertPassiveEffectsChange = false;
 export const enableUserBlockingEvents = false;
 export const enableSuspenseCallback = false;
 export const warnAboutDefaultPropsOnFunctionComponents = false;
+export const warnAboutStringRefs = false;
 
 // Only used in www builds.
 export function addUserTimingListener() {
