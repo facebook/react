6404e89cc955f837d0b894a380c705c90604b801
Ivaylo Hristov <ishristov@gmail.com>
2019-07-31 17:28:54 +0300

    Fix TypeError in getActiveElement
    During tests (with react-testing-library) a test randomly fail
    with "TypeError: Cannot read property 'body' of null" in the getActiveElement.
    It appears that sometimes the document is null but the check in the file only
    checks for undefined. This proposed change checks for both null or undefined
    which should prevent the failures.
    
    https://github.com/facebook/react/issues/15691

--- packages/react-dom/src/client/getActiveElement.js
+++ packages/react-dom/src/client/getActiveElement.js
@@ -9,7 +9,7 @@
 
 export default function getActiveElement(doc: ?Document): ?Element {
   doc = doc || (typeof document !== 'undefined' ? document : undefined);
-  if (typeof doc === 'undefined') {
+  if (doc == null) {
     return null;
   }
   try {
