555ed39c61078a5bb25c4f533bc6338934d9e154
yelmor <yelmor@outlook.com>
2019-08-01 00:19:15 +0800

    use helper function to create react symbol

--- packages/shared/ReactSymbols.js
+++ packages/shared/ReactSymbols.js
@@ -11,52 +11,34 @@
 // nor polyfill, then a plain number is used for performance.
 const hasSymbol = typeof Symbol === 'function' && Symbol.for;
 
-export const REACT_ELEMENT_TYPE = hasSymbol
-  ? Symbol.for('react.element')
-  : 0xeac7;
-export const REACT_PORTAL_TYPE = hasSymbol
-  ? Symbol.for('react.portal')
-  : 0xeaca;
-export const REACT_FRAGMENT_TYPE = hasSymbol
-  ? Symbol.for('react.fragment')
-  : 0xeacb;
-export const REACT_STRICT_MODE_TYPE = hasSymbol
-  ? Symbol.for('react.strict_mode')
-  : 0xeacc;
-export const REACT_PROFILER_TYPE = hasSymbol
-  ? Symbol.for('react.profiler')
-  : 0xead2;
-export const REACT_PROVIDER_TYPE = hasSymbol
-  ? Symbol.for('react.provider')
-  : 0xeacd;
-export const REACT_CONTEXT_TYPE = hasSymbol
-  ? Symbol.for('react.context')
-  : 0xeace;
+function createSymbol(symbolKey: string, numberKey: number) {
+  return hasSymbol ? Symbol.for(symbolKey) : numberKey;
+}
+
+export const REACT_ELEMENT_TYPE = createSymbol('react.element', 0xeac7);
+export const REACT_PORTAL_TYPE = createSymbol('react.portal', 0xeaca);
+export const REACT_FRAGMENT_TYPE = createSymbol('react.fragment', 0xeacb);
+export const REACT_STRICT_MODE_TYPE = createSymbol('react.strict_mode', 0xeacc);
+export const REACT_PROFILER_TYPE = createSymbol('react.profiler', 0xead2);
+export const REACT_PROVIDER_TYPE = createSymbol('react.provider', 0xeacd);
+export const REACT_CONTEXT_TYPE = createSymbol('react.context', 0xeace);
 // TODO: We don't use AsyncMode or ConcurrentMode anymore. They were temporary
 // (unstable) APIs that have been removed. Can we remove the symbols?
-export const REACT_ASYNC_MODE_TYPE = hasSymbol
-  ? Symbol.for('react.async_mode')
-  : 0xeacf;
-export const REACT_CONCURRENT_MODE_TYPE = hasSymbol
-  ? Symbol.for('react.concurrent_mode')
-  : 0xeacf;
-export const REACT_FORWARD_REF_TYPE = hasSymbol
-  ? Symbol.for('react.forward_ref')
-  : 0xead0;
-export const REACT_SUSPENSE_TYPE = hasSymbol
-  ? Symbol.for('react.suspense')
-  : 0xead1;
-export const REACT_SUSPENSE_LIST_TYPE = hasSymbol
-  ? Symbol.for('react.suspense_list')
-  : 0xead8;
-export const REACT_MEMO_TYPE = hasSymbol ? Symbol.for('react.memo') : 0xead3;
-export const REACT_LAZY_TYPE = hasSymbol ? Symbol.for('react.lazy') : 0xead4;
-export const REACT_FUNDAMENTAL_TYPE = hasSymbol
-  ? Symbol.for('react.fundamental')
-  : 0xead5;
-export const REACT_RESPONDER_TYPE = hasSymbol
-  ? Symbol.for('react.responder')
-  : 0xead6;
+export const REACT_ASYNC_MODE_TYPE = createSymbol('react.async_mode', 0xeacf);
+export const REACT_CONCURRENT_MODE_TYPE = createSymbol(
+  'react.concurrent_mode',
+  0xeacf,
+);
+export const REACT_FORWARD_REF_TYPE = createSymbol('react.forward_ref', 0xead0);
+export const REACT_SUSPENSE_TYPE = createSymbol('react.suspense', 0xead1);
+export const REACT_SUSPENSE_LIST_TYPE = createSymbol(
+  'react.suspense_list',
+  0xead8,
+);
+export const REACT_MEMO_TYPE = createSymbol('react.memo', 0xead3);
+export const REACT_LAZY_TYPE = createSymbol('react.lazy', 0xead4);
+export const REACT_FUNDAMENTAL_TYPE = createSymbol('react.fundamental', 0xead5);
+export const REACT_RESPONDER_TYPE = createSymbol('react.responder', 0xead6);
 
 const MAYBE_ITERATOR_SYMBOL = typeof Symbol === 'function' && Symbol.iterator;
 const FAUX_ITERATOR_SYMBOL = '@@iterator';
