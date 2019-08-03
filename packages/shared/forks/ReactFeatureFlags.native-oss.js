6491199dcb365b733eab5cae87ded630d1b60089
Andrew Clark <git@andrewclark.io>
2019-08-02 17:26:08 -0700

    Rendering tasks should not jump the queue
    When React schedules a rendering task, it passes a `timeout` option
    based on its expiration time. This is intended to avoid starvation
    by other React updates. However, it also affects the relative priority
    of React tasks and other Scheduler tasks at the same level, like
    data processing.
    
    This adds a feature flag to disable passing a `timeout` option to
    Scheduler. React tasks will always append themselves to the end of
    the queue, without jumping ahead of already scheduled tasks.
    
    This does not affect the order in which React updates within a single
    root are processed, but it could affect updates across multiple roots.
    
    This also doesn't remove the expiration from Scheduler. It only means
    that React tasks are not given special treatment.

--- packages/shared/forks/ReactFeatureFlags.native-oss.js
+++ packages/shared/forks/ReactFeatureFlags.native-oss.js
@@ -36,6 +36,7 @@ export const enableUserBlockingEvents = false;
 export const enableSuspenseCallback = false;
 export const warnAboutDefaultPropsOnFunctionComponents = false;
 export const disableLegacyContext = false;
+export const disableSchedulerTimeoutBasedOnReactExpirationTime = false;
 
 // Only used in www builds.
 export function addUserTimingListener() {
