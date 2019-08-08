faa6e32797cb7067102ff122ee57a670bf91640e
Luna Ruan <luna@fb.com>
2019-08-08 13:32:01 -0700

    fixed jest config.source-persistent.js

--- scripts/jest/config.source-persistent.js
+++ scripts/jest/config.source-persistent.js
@@ -9,6 +9,12 @@ module.exports = Object.assign({}, baseConfig, {
     'ReactIncrementalTriangle',
     'ReactIncrementalReflection',
     'forwardRef',
+    // ReactFreshBabelPlugin is only available for dev.
+    // We need two tests here because otherwise, ReactFreshBabelPlugin-test will
+    // fail due to obsolete snapshots
+    process.env.NODE_ENV === 'development'
+      ? '<rootDir>/packages/react-refresh/src/__tests__/ReactFreshBabelPluginProd-test.js'
+      : '<rootDir>/packages/react-refresh/src/__tests__/ReactFreshBabelPlugin-test.js',
   ],
   setupFiles: [
     ...baseConfig.setupFiles,
