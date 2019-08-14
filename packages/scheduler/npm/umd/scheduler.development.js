935526e121add025ff74df8f7dde06de27bf7c2e
Dan Abramov <dan.abramov@gmail.com>
2019-08-14 18:30:45 +0100

    Fix copy paste mistake

--- packages/scheduler/npm/umd/scheduler.development.js
+++ packages/scheduler/npm/umd/scheduler.development.js
@@ -162,7 +162,7 @@
     },
     get unstable_sharedProfilingBuffer() {
       return global.React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
-        .Scheduler.unstable_getFirstCallbackNode;
+        .Scheduler.unstable_sharedProfilingBuffer;
     },
   });
 });
