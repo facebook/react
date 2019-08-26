6e54d96930cfe15097d66924c7974aa0cf85f491
Dan <dan.abramov@gmail.com>
2019-08-26 21:16:28 +0100

    Don't ignore dependencies for render phase update

--- packages/react-reconciler/src/ReactFiberHooks.js
+++ packages/react-reconciler/src/ReactFiberHooks.js
@@ -428,6 +428,11 @@ export function renderWithHooks(
     do {
       didScheduleRenderPhaseUpdate = false;
       numberOfReRenders += 1;
+      if (__DEV__) {
+        // Even when hot reloading, allow dependencies to stabilize
+        // after first render to prevent infinite render phase updates.
+        ignorePreviousDependencies = false;
+      }
 
       // Start over from the beginning of the list
       nextCurrentHook = current !== null ? current.memoizedState : null;
