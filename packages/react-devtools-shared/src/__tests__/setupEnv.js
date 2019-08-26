177f357d9da5b21050ddd92084a6f0ce67c09e03
Brian Vaughn <bvaughn@fb.com>
2019-08-26 13:46:00 -0700

    Updated DevTools test setup to no longer mock test renerer

--- packages/react-devtools-shared/src/__tests__/setupEnv.js
+++ packages/react-devtools-shared/src/__tests__/setupEnv.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// Import test renderer and then reset modules for two reasons:
-// 1. We don't want test renderer connected to the DevTools hooks.
-// 2. This enables us to work around the on-renderer limitation.
-// TODO This (or the resetModules below) might be the cause of the hooks erorr in some tests
-const ReactTestRenderer = require.requireActual('react-test-renderer');
-
-jest.resetModules();
-
-jest.mock('react-test-renderer', () => ReactTestRenderer);
-
 // DevTools stores preferences between sessions in localStorage
 if (!global.hasOwnProperty('localStorage')) {
   global.localStorage = require('local-storage-fallback').default;
