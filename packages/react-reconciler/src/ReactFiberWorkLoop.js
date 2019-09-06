d98537db3ef079b47a3d741fe2c3d3e97b8254b8
NE-SmallTown <ne_smalltown@163.com>
2019-09-06 17:21:43 +0800

    Fix typo: wnless -> unless

--- packages/react-reconciler/src/ReactFiberWorkLoop.js
+++ packages/react-reconciler/src/ReactFiberWorkLoop.js
@@ -406,7 +406,7 @@ export function scheduleUpdateOnFiber(
     } else {
       scheduleCallbackForRoot(root, ImmediatePriority, Sync);
       if (executionContext === NoContext) {
-        // Flush the synchronous work now, wnless we're already working or inside
+        // Flush the synchronous work now, unless we're already working or inside
         // a batch. This is intentionally inside scheduleUpdateOnFiber instead of
         // scheduleCallbackForFiber to preserve the ability to schedule a callback
         // without immediately flushing it. We only do this for user-initiated
