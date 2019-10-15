6d5cc7f5a3046081c2fe27cb92f4b8f8ccd10a82
Pedro Luiz Cabral Salomon Prado <pedroprado010@users.noreply.github.com>
2019-10-15 13:11:05 -0300

    Remove unused case

--- packages/react-devtools-shared/src/backend/renderer.js
+++ packages/react-devtools-shared/src/backend/renderer.js
@@ -1867,17 +1867,6 @@ export function attach(
       if (fiber === null) {
         return null;
       }
-      // Special case for a timed-out Suspense.
-      const isTimedOutSuspense =
-        fiber.tag === SuspenseComponent && fiber.memoizedState !== null;
-      if (isTimedOutSuspense) {
-        // A timed-out Suspense's findDOMNode is useless.
-        // Try our best to find the fallback directly.
-        const maybeFallbackFiber = fiber.child && fiber.child.sibling;
-        if (maybeFallbackFiber != null) {
-          fiber = maybeFallbackFiber;
-        }
-      }
       const hostFibers = findAllCurrentHostFibers(id);
       return hostFibers.map(hostFiber => hostFiber.stateNode).filter(Boolean);
     } catch (err) {
