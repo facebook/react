2c92c9f26c6b26d35d0f8a2361a6bd8a8a259fb9
Deniz Susman <susmandeniz@gmail.com>
2019-10-16 10:46:29 +0200

    Typo fix in comment

--- packages/react-dom/src/client/ReactDOMComponentTree.js
+++ packages/react-dom/src/client/ReactDOMComponentTree.js
@@ -34,7 +34,7 @@ export function markContainerAsRoot(hostRoot, node) {
 // If the target node is part of a hydrated or not yet rendered subtree, then
 // this may also return a SuspenseComponent or HostRoot to indicate that.
 // Conceptually the HostRoot fiber is a child of the Container node. So if you
-// pass the Container node as the targetNode, you wiill not actually get the
+// pass the Container node as the targetNode, you will not actually get the
 // HostRoot back. To get to the HostRoot, you need to pass a child of it.
 // The same thing applies to Suspense boundaries.
 export function getClosestInstanceFromNode(targetNode) {
