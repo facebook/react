c87ca41ffc72523c121f2c76eabf823710a0e030
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2019-10-23 12:08:03 +0300

    use window.getComputedStyle for always existing read only CSSStyleDeclaration to extract vendor prefixed event names

--- packages/react-dom/src/events/getVendorPrefixedEventName.js
+++ packages/react-dom/src/events/getVendorPrefixedEventName.js
@@ -48,7 +48,7 @@ let style = {};
  * Bootstrap if a DOM exists.
  */
 if (canUseDOM) {
-  style = document.createElement('div').style;
+  style = window.getComputedStyle(document.createElement('div'));
 
   // On some platforms, in particular some releases of Android 4.x,
   // the un-prefixed "animation" and "transition" properties are defined on the
