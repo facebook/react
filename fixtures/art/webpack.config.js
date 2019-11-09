98c36d91b91e7ba4b278fe925b2f150694eb4d80
Scott Morse <scottmorsedev@gmail.com>
2019-11-09 11:32:17 -0600

    Fix '__DEV__ is not defined' error in react-art fixture

--- fixtures/art/webpack.config.js
+++ fixtures/art/webpack.config.js
@@ -22,9 +22,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('development'),
-      },
+      '__DEV__': true,
     }),
   ],
   resolve: {
