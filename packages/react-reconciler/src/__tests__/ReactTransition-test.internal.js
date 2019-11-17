1f2da0babd723f1b479f84cef5a0ea4fedac3959
Andrew Clark <git@andrewclark.io>
2019-11-17 13:59:25 -0800

    Forgot to mark test as experimental (#17391)

--- packages/react-reconciler/src/__tests__/ReactTransition-test.internal.js
+++ packages/react-reconciler/src/__tests__/ReactTransition-test.internal.js
@@ -19,7 +19,7 @@ let useState;
 let useTransition;
 let act;
 
-describe('ReactHooksWithNoopRenderer', () => {
+describe('ReactTransition', () => {
   beforeEach(() => {
     jest.resetModules();
 
@@ -59,44 +59,47 @@ describe('ReactHooksWithNoopRenderer', () => {
     return Component;
   }
 
-  it('isPending works even if called from outside an input event', async () => {
-    const Async = createAsyncText('Async');
-    let start;
-    function App() {
-      const [show, setShow] = useState(false);
-      const [startTransition, isPending] = useTransition();
-      start = () => startTransition(() => setShow(true));
-      return (
-        <Suspense fallback={<Text text="Loading..." />}>
-          {isPending ? <Text text="Pending..." /> : null}
-          {show ? <Async /> : <Text text="(empty)" />}
-        </Suspense>
-      );
-    }
+  it.experimental(
+    'isPending works even if called from outside an input event',
+    async () => {
+      const Async = createAsyncText('Async');
+      let start;
+      function App() {
+        const [show, setShow] = useState(false);
+        const [startTransition, isPending] = useTransition();
+        start = () => startTransition(() => setShow(true));
+        return (
+          <Suspense fallback={<Text text="Loading..." />}>
+            {isPending ? <Text text="Pending..." /> : null}
+            {show ? <Async /> : <Text text="(empty)" />}
+          </Suspense>
+        );
+      }
 
-    const root = ReactNoop.createRoot();
+      const root = ReactNoop.createRoot();
 
-    await act(async () => {
-      root.render(<App />);
-    });
-    expect(Scheduler).toHaveYielded(['(empty)']);
-    expect(root).toMatchRenderedOutput('(empty)');
+      await act(async () => {
+        root.render(<App />);
+      });
+      expect(Scheduler).toHaveYielded(['(empty)']);
+      expect(root).toMatchRenderedOutput('(empty)');
 
-    await act(async () => {
-      start();
+      await act(async () => {
+        start();
 
-      expect(Scheduler).toFlushAndYield([
-        'Pending...',
-        '(empty)',
-        'Suspend! [Async]',
-        'Loading...',
-      ]);
+        expect(Scheduler).toFlushAndYield([
+          'Pending...',
+          '(empty)',
+          'Suspend! [Async]',
+          'Loading...',
+        ]);
 
-      expect(root).toMatchRenderedOutput('Pending...(empty)');
+        expect(root).toMatchRenderedOutput('Pending...(empty)');
 
-      await Async.resolve();
-    });
-    expect(Scheduler).toHaveYielded(['Async']);
-    expect(root).toMatchRenderedOutput('Async');
-  });
+        await Async.resolve();
+      });
+      expect(Scheduler).toHaveYielded(['Async']);
+      expect(root).toMatchRenderedOutput('Async');
+    },
+  );
 });
