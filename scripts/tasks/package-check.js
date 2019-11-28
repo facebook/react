f3879264e51fa505865adf889a515ac8d8191173
Chris Trevino <darthtrevino@gmail.com>
2019-11-28 11:02:22 -0800

    Use the conditionl-exports based dual-entry model described in the node docs

--- scripts/tasks/package-check.js
+++ scripts/tasks/package-check.js
@@ -13,6 +13,11 @@ const path = require('path');
 const packagesDir = path.join(__dirname, '../../build/node_modules');
 const packages = fs.readdirSync(packagesDir);
 
+process.on('unhandledRejection', err => {
+  console.error(err);
+  process.exit(1);
+});
+
 packages.forEach(packageName => {
   const packageDir = path.join(packagesDir, packageName);
   const packageJson = require(path.join(packageDir, 'package.json'));
@@ -27,14 +32,34 @@ packages.forEach(packageName => {
       process.exit(1);
     }
   }
-  if (packageJson.module != null) {
-    if (!fs.existsSync(path.join(packageDir, packageJson.module))) {
-      console.error(
-        `package.json for build module ${
-          packageJson.name
-        } contains a 'module' entry that does not exist`
+  if (packageJson.exports != null) {
+    if (packageJson.exports.require != null) {
+      const cjsEntryFileName = path.join(
+        packageDir,
+        packageJson.exports.require
       );
-      process.exit(1);
+      if (!fs.existsSync(cjsEntryFileName)) {
+        console.error(
+          `package.json for build module ${
+            packageJson.name
+          } contains a 'module' entry that does not exist`
+        );
+        process.exit(1);
+      }
+    }
+    if (packageJson.exports.default != null) {
+      const esmEntryFileName = path.join(
+        packageDir,
+        packageJson.exports.default
+      );
+      if (!fs.existsSync(esmEntryFileName)) {
+        console.error(
+          `package.json for build module ${
+            packageJson.name
+          } contains a 'module' entry that does not exist`
+        );
+        process.exit(1);
+      }
     }
   }
 });
