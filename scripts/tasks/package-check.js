45b594e084ae0083091d3e17334c7d76fb89d319
Chris Trevino <darthtrevino@gmail.com>
2019-11-28 11:20:19 -0800

    update package-check script to verify require() and import() on packages using --experimenta-conditional-exports feature

--- scripts/tasks/package-check.js
+++ scripts/tasks/package-check.js
@@ -9,6 +9,18 @@
 
 const fs = require('fs');
 const path = require('path');
+const {JSDOM} = require('jsdom');
+
+const {window} = new JSDOM(
+  `<body>
+  <script>document.body.appendChild(document.createElement("hr"));</script>
+</body>`,
+  {runScripts: 'dangerously'}
+);
+
+global.window = window;
+global.document = window.document;
+global.navigator = window.navigator;
 
 const packagesDir = path.join(__dirname, '../../build/node_modules');
 const packages = fs.readdirSync(packagesDir);
@@ -18,48 +30,69 @@ process.on('unhandledRejection', err => {
   process.exit(1);
 });
 
-packages.forEach(packageName => {
+function failHardIf(predicate, message) {
+  if (predicate) {
+    console.error(message);
+    process.exit(1);
+  }
+}
+
+packages.forEach(async packageName => {
+  console.log(`inspect ${packageName}`);
   const packageDir = path.join(packagesDir, packageName);
   const packageJson = require(path.join(packageDir, 'package.json'));
 
   if (packageJson.main != null) {
-    if (!fs.existsSync(path.join(packageDir, packageJson.main))) {
-      console.error(
-        `package.json for build module ${
-          packageJson.name
-        } contains a 'main' entry that does not exist`
-      );
-      process.exit(1);
-    }
+    failHardIf(
+      !fs.existsSync(path.join(packageDir, packageJson.main)),
+      `package.json for build module ${
+        packageJson.name
+      } contains a 'main' entry that does not exist`
+    );
+
+    const exported = require(packageDir);
+    failHardIf(
+      !exported,
+      `package.json for build module ${
+        packageJson.name
+      } does not export anything from main`
+    );
   }
+
   if (packageJson.exports != null) {
     if (packageJson.exports.require != null) {
+      console.log('\tinspect cjs export');
       const cjsEntryFileName = path.join(
         packageDir,
         packageJson.exports.require
       );
-      if (!fs.existsSync(cjsEntryFileName)) {
-        console.error(
-          `package.json for build module ${
-            packageJson.name
-          } contains a 'module' entry that does not exist`
-        );
-        process.exit(1);
-      }
+      failHardIf(
+        !fs.existsSync(cjsEntryFileName),
+        `package.json for build module ${
+          packageJson.name
+        } contains a 'module' entry that does not exist`
+      );
     }
     if (packageJson.exports.default != null) {
+      console.log('\tinspect esm export');
       const esmEntryFileName = path.join(
         packageDir,
         packageJson.exports.default
       );
-      if (!fs.existsSync(esmEntryFileName)) {
-        console.error(
-          `package.json for build module ${
-            packageJson.name
-          } contains a 'module' entry that does not exist`
-        );
-        process.exit(1);
-      }
+      failHardIf(
+        !fs.existsSync(esmEntryFileName),
+        `package.json for build module ${
+          packageJson.name
+        } contains a 'module' entry that does not exist`
+      );
+
+      const result = await import(packageDir);
+      failHardIf(
+        !result,
+        `package.json for build module ${
+          packageJson.name
+        } does not export anything from esm`
+      );
     }
   }
 });
