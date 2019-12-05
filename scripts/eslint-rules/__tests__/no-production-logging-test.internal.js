38f5068684ccab9acf120731305da3f2fe7eeeff
Laura González <walaura@fb.com>
2019-12-05 16:25:42 +0000

    tidy tests

--- scripts/eslint-rules/__tests__/no-production-logging-test.internal.js
+++ scripts/eslint-rules/__tests__/no-production-logging-test.internal.js
@@ -15,23 +15,23 @@ const ruleTester = new RuleTester();
 
 ruleTester.run('no-production-logging', rule, {
   valid: [
+    /* wrapped calls */
     {
-      code: 'if (__DEV__) {warningWithoutStack(test)}',
+      code: 'if (__DEV__) { warningWithoutStack(test) }',
     },
     {
-      code: 'if (__DEV__) {warning(test)}',
+      code: 'if (__DEV__) { warning(test) }',
     },
+    /* calls wrapped on an outer bound */
     {
       code: 'if (__DEV__) { if (potato) { while (true) { warning(test) }}}',
     },
+    /* don't do anything to arbitrary fn's or invariants */
     {
       code: 'normalFunctionCall(test)',
     },
     {
       code: 'invariant(test)',
-    },
-    {
-      code: 'if (__DEV__) {normalFunctionCall(test)}',
     },
   ],
   invalid: [
