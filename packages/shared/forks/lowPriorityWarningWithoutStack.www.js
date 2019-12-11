37598141d4e52622f83f890d5f1def209fdcb286
Dan <dan.abramov@gmail.com>
2019-12-11 03:06:13 +0000

    Fix up www shims to match expected API

--- packages/shared/forks/lowPriorityWarningWithoutStack.www.js
+++ packages/shared/forks/lowPriorityWarningWithoutStack.www.js
@@ -5,10 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// This "lowPriorityWarning" is an external module
-const lowPriorityWarning = require('lowPriorityWarning');
+// This refers to a WWW module.
+const lowPriorityWarningWWW = require('lowPriorityWarning');
 
-export default function(...args) {
-  // eslint-disable-next-line react-internal/warning-args, react-internal/no-production-logging
-  return lowPriorityWarning(args);
+export default function lowPriorityWarningWithoutStack(format, ...args) {
+  return lowPriorityWarningWWW(false, format, ...args);
 }
--- packages/shared/forks/warningWithoutStack.www.js
+++ packages/shared/forks/warningWithoutStack.www.js
@@ -5,9 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const warning = require('lowPriorityWarning');
+// This refers to a WWW module.
+const warningWWW = require('warning');
 
-export default function(...args) {
-  // eslint-disable-next-line react-internal/warning-args, react-internal/no-production-logging
-  return warning(args);
+export default function warningWithoutStack(format, ...args) {
+  return warningWWW(false, format, ...args);
 }
