bf1ed10c54560b8856ce363c25f6403b023e39f3
Dominic Gannaway <dg@domgan.com>
2019-12-20 14:59:35 +0000

    [react-dom] Refactor event priority handling to its own module
    Fix lint
    
    Revise comment
    
    Revise strategy
    
    Did some code golfing
    
    Added comment about iteration
    
    Remove cyclic dependency
    
    Fix typo
    
    Extend comment
    
    Fix typo

--- packages/react-dom/src/events/SimpleEventPlugin.js
+++ packages/react-dom/src/events/SimpleEventPlugin.js
@@ -7,28 +7,23 @@
  * @flow
  */
 
-import type {EventPriority} from 'shared/ReactTypes';
 import type {
   TopLevelType,
   DOMTopLevelEventType,
 } from 'legacy-events/TopLevelEventTypes';
-import type {
-  DispatchConfig,
-  ReactSyntheticEvent,
-} from 'legacy-events/ReactSyntheticEventType';
+import type {ReactSyntheticEvent} from 'legacy-events/ReactSyntheticEventType';
 import type {Fiber} from 'react-reconciler/src/ReactFiber';
-import type {EventTypes, PluginModule} from 'legacy-events/PluginModuleType';
+import type {PluginModule} from 'legacy-events/PluginModuleType';
 import type {EventSystemFlags} from 'legacy-events/EventSystemFlags';
 
-import {
-  DiscreteEvent,
-  UserBlockingEvent,
-  ContinuousEvent,
-} from 'shared/ReactTypes';
 import {accumulateTwoPhaseDispatches} from 'legacy-events/EventPropagators';
 import SyntheticEvent from 'legacy-events/SyntheticEvent';
 
 import * as DOMTopLevelEventTypes from './DOMTopLevelEventTypes';
+import {
+  topLevelEventsToDispatchConfig,
+  simpleEventPluginEventTypes,
+} from './DOMEventProperties';
 
 import SyntheticAnimationEvent from './SyntheticAnimationEvent';
 import SyntheticClipboardEvent from './SyntheticClipboardEvent';
@@ -42,163 +37,6 @@ import SyntheticTransitionEvent from './SyntheticTransitionEvent';
 import SyntheticUIEvent from './SyntheticUIEvent';
 import SyntheticWheelEvent from './SyntheticWheelEvent';
 import getEventCharCode from './getEventCharCode';
-
-/**
- * Turns
- * ['abort', ...]
- * into
- * eventTypes = {
- *   'abort': {
- *     phasedRegistrationNames: {
- *       bubbled: 'onAbort',
- *       captured: 'onAbortCapture',
- *     },
- *     dependencies: [TOP_ABORT],
- *   },
- *   ...
- * };
- * topLevelEventsToDispatchConfig = new Map([
- *   [TOP_ABORT, { sameConfig }],
- * ]);
- */
-
-type EventTuple = [DOMTopLevelEventType, string, EventPriority];
-
-const eventTuples: Array<EventTuple> = [
-  // Discrete events
-  [DOMTopLevelEventTypes.TOP_BLUR, 'blur', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_CANCEL, 'cancel', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_CLICK, 'click', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_CLOSE, 'close', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_CONTEXT_MENU, 'contextMenu', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_COPY, 'copy', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_CUT, 'cut', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_AUX_CLICK, 'auxClick', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_DOUBLE_CLICK, 'doubleClick', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_DRAG_END, 'dragEnd', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_DRAG_START, 'dragStart', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_DROP, 'drop', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_FOCUS, 'focus', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_INPUT, 'input', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_INVALID, 'invalid', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_KEY_DOWN, 'keyDown', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_KEY_PRESS, 'keyPress', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_KEY_UP, 'keyUp', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_MOUSE_DOWN, 'mouseDown', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_MOUSE_UP, 'mouseUp', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_PASTE, 'paste', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_PAUSE, 'pause', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_PLAY, 'play', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_POINTER_CANCEL, 'pointerCancel', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_POINTER_DOWN, 'pointerDown', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_POINTER_UP, 'pointerUp', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_RATE_CHANGE, 'rateChange', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_RESET, 'reset', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_SEEKED, 'seeked', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_SUBMIT, 'submit', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_TOUCH_CANCEL, 'touchCancel', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_TOUCH_END, 'touchEnd', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_TOUCH_START, 'touchStart', DiscreteEvent],
-  [DOMTopLevelEventTypes.TOP_VOLUME_CHANGE, 'volumeChange', DiscreteEvent],
-
-  // User-blocking events
-  [DOMTopLevelEventTypes.TOP_DRAG, 'drag', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_DRAG_ENTER, 'dragEnter', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_DRAG_EXIT, 'dragExit', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_DRAG_LEAVE, 'dragLeave', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_DRAG_OVER, 'dragOver', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_MOUSE_MOVE, 'mouseMove', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_MOUSE_OUT, 'mouseOut', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_MOUSE_OVER, 'mouseOver', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_POINTER_MOVE, 'pointerMove', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_POINTER_OUT, 'pointerOut', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_POINTER_OVER, 'pointerOver', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_SCROLL, 'scroll', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_TOGGLE, 'toggle', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_TOUCH_MOVE, 'touchMove', UserBlockingEvent],
-  [DOMTopLevelEventTypes.TOP_WHEEL, 'wheel', UserBlockingEvent],
-
-  // Continuous events
-  [DOMTopLevelEventTypes.TOP_ABORT, 'abort', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_ANIMATION_END, 'animationEnd', ContinuousEvent],
-  [
-    DOMTopLevelEventTypes.TOP_ANIMATION_ITERATION,
-    'animationIteration',
-    ContinuousEvent,
-  ],
-  [
-    DOMTopLevelEventTypes.TOP_ANIMATION_START,
-    'animationStart',
-    ContinuousEvent,
-  ],
-  [DOMTopLevelEventTypes.TOP_CAN_PLAY, 'canPlay', ContinuousEvent],
-  [
-    DOMTopLevelEventTypes.TOP_CAN_PLAY_THROUGH,
-    'canPlayThrough',
-    ContinuousEvent,
-  ],
-  [
-    DOMTopLevelEventTypes.TOP_DURATION_CHANGE,
-    'durationChange',
-    ContinuousEvent,
-  ],
-  [DOMTopLevelEventTypes.TOP_EMPTIED, 'emptied', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_ENCRYPTED, 'encrypted', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_ENDED, 'ended', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_ERROR, 'error', ContinuousEvent],
-  [
-    DOMTopLevelEventTypes.TOP_GOT_POINTER_CAPTURE,
-    'gotPointerCapture',
-    ContinuousEvent,
-  ],
-  [DOMTopLevelEventTypes.TOP_LOAD, 'load', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_LOADED_DATA, 'loadedData', ContinuousEvent],
-  [
-    DOMTopLevelEventTypes.TOP_LOADED_METADATA,
-    'loadedMetadata',
-    ContinuousEvent,
-  ],
-  [DOMTopLevelEventTypes.TOP_LOAD_START, 'loadStart', ContinuousEvent],
-  [
-    DOMTopLevelEventTypes.TOP_LOST_POINTER_CAPTURE,
-    'lostPointerCapture',
-    ContinuousEvent,
-  ],
-  [DOMTopLevelEventTypes.TOP_PLAYING, 'playing', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_PROGRESS, 'progress', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_SEEKING, 'seeking', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_STALLED, 'stalled', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_SUSPEND, 'suspend', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_TIME_UPDATE, 'timeUpdate', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_TRANSITION_END, 'transitionEnd', ContinuousEvent],
-  [DOMTopLevelEventTypes.TOP_WAITING, 'waiting', ContinuousEvent],
-];
-
-const eventTypes: EventTypes = {};
-const topLevelEventsToDispatchConfig: {
-  [key: TopLevelType]: DispatchConfig,
-} = {};
-
-for (let i = 0; i < eventTuples.length; i++) {
-  const eventTuple = eventTuples[i];
-  const topEvent = eventTuple[0];
-  const event = eventTuple[1];
-  const eventPriority = eventTuple[2];
-
-  const capitalizedEvent = event[0].toUpperCase() + event.slice(1);
-  const onEvent = 'on' + capitalizedEvent;
-
-  const config = {
-    phasedRegistrationNames: {
-      bubbled: onEvent,
-      captured: onEvent + 'Capture',
-    },
-    dependencies: [topEvent],
-    eventPriority,
-  };
-  eventTypes[event] = config;
-  topLevelEventsToDispatchConfig[topEvent] = config;
-}
 
 // Only used in DEV for exhaustiveness validation.
 const knownHTMLTopLevelTypes: Array<DOMTopLevelEventType> = [
@@ -235,16 +73,10 @@ const knownHTMLTopLevelTypes: Array<DOMTopLevelEventType> = [
   DOMTopLevelEventTypes.TOP_WAITING,
 ];
 
-const SimpleEventPlugin: PluginModule<MouseEvent> & {
-  getEventPriority: (topLevelType: TopLevelType) => EventPriority,
-} = {
-  eventTypes: eventTypes,
-
-  getEventPriority(topLevelType: TopLevelType): EventPriority {
-    const config = topLevelEventsToDispatchConfig[topLevelType];
-    return config !== undefined ? config.eventPriority : ContinuousEvent;
-  },
-
+const SimpleEventPlugin: PluginModule<MouseEvent> = {
+  // simpleEventPluginEventTypes gets populated from
+  // the DOMEventProperties module.
+  eventTypes: simpleEventPluginEventTypes,
   extractEvents: function(
     topLevelType: TopLevelType,
     targetInst: null | Fiber,
@@ -252,7 +84,7 @@ const SimpleEventPlugin: PluginModule<MouseEvent> & {
     nativeEventTarget: null | EventTarget,
     eventSystemFlags: EventSystemFlags,
   ): null | ReactSyntheticEvent {
-    const dispatchConfig = topLevelEventsToDispatchConfig[topLevelType];
+    const dispatchConfig = topLevelEventsToDispatchConfig.get(topLevelType);
     if (!dispatchConfig) {
       return null;
     }
