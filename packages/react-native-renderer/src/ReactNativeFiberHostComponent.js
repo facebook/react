b5e4b15105b9ea399735f01325c7e3230772eba7
Eli White <github@eli-white.com>
2020-01-17 15:04:27 -0800

    [Native] Migrate focus/blur to call TextInputState with the host component

--- packages/react-native-renderer/src/ReactNativeFiberHostComponent.js
+++ packages/react-native-renderer/src/ReactNativeFiberHostComponent.js
@@ -40,11 +40,11 @@ class ReactNativeFiberHostComponent {
   }
 
   blur() {
-    TextInputState.blurTextInput(this._nativeTag);
+    TextInputState.blurTextInput(this);
   }
 
   focus() {
-    TextInputState.focusTextInput(this._nativeTag);
+    TextInputState.focusTextInput(this);
   }
 
   measure(callback: MeasureOnSuccessCallback) {
