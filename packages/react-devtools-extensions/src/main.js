bcd307860cbaf022b3eda03d092401f77bf69e5c
wfnuser <wfnuser@126.com>
2020-02-01 14:50:25 +0800

    fix: check window.__REACT_DEVTOOLS_GLOBAL_HOOK__ not null before use it in main.js

--- packages/react-devtools-extensions/src/main.js
+++ packages/react-devtools-extensions/src/main.js
@@ -271,7 +271,7 @@ function createPanelIfReactLoaded() {
         // When the user chooses a different node in the browser Elements tab,
         // copy it over to the hook object so that we can sync the selection.
         chrome.devtools.inspectedWindow.eval(
-          '(window.__REACT_DEVTOOLS_GLOBAL_HOOK__.$0 !== $0) ?' +
+          '(window.__REACT_DEVTOOLS_GLOBAL_HOOK__ && window.__REACT_DEVTOOLS_GLOBAL_HOOK__.$0 !== $0) ?' +
             '(window.__REACT_DEVTOOLS_GLOBAL_HOOK__.$0 = $0, true) :' +
             'false',
           (didSelectionChange, evalError) => {
