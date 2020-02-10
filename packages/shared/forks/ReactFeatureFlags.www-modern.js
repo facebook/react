c3d4f6aaa4d803659629ebe458e6cd205c9631df
Dan Abramov <dan.abramov@gmail.com>
2020-02-10 20:30:14 +0000

    Disable a bunch of classic features for modern builds

--- packages/shared/forks/ReactFeatureFlags.www-modern.js
+++ packages/shared/forks/ReactFeatureFlags.www-modern.js
@@ -19,13 +19,7 @@ export const {
   warnAboutShorthandPropertyCollision,
 } = require('ReactFeatureFlags');
 
-// In www, we have experimental support for gathering data
-// from User Timing API calls in production. By default, we
-// only emit performance.mark/measure calls in __DEV__. But if
-// somebody calls addUserTimingListener() which is exposed as an
-// experimental FB-only export, we call performance.mark/measure
-// as long as there is more than a single listener.
-export let enableUserTimingAPI = __DEV__;
+export let enableUserTimingAPI = false;
 
 export const enableProfilerTimer = __PROFILE__;
 export const enableSchedulerTracing = __PROFILE__;
@@ -33,7 +27,7 @@ export const enableSchedulerDebugging = true;
 
 export const replayFailedUnitOfWorkWithInvokeGuardedCallback = false;
 export const warnAboutDeprecatedLifecycles = true;
-export const disableLegacyContext = false;
+export const disableLegacyContext = true;
 export const warnAboutStringRefs = false;
 export const warnAboutDefaultPropsOnFunctionComponents = false;
 export const disableSchedulerTimeoutBasedOnReactExpirationTime = false;
@@ -49,31 +43,8 @@ export const enableChunksAPI = __EXPERIMENTAL__;
 
 export const disableJavaScriptURLs = true;
 
-let refCount = 0;
 export function addUserTimingListener() {
-  if (__DEV__) {
-    // Noop.
-    return () => {};
-  }
-  refCount++;
-  updateFlagOutsideOfReactCallStack();
-  return () => {
-    refCount--;
-    updateFlagOutsideOfReactCallStack();
-  };
-}
-
-// The flag is intentionally updated in a timeout.
-// We don't support toggling it during reconciliation or
-// commit since that would cause mismatching user timing API calls.
-let timeout = null;
-function updateFlagOutsideOfReactCallStack() {
-  if (!timeout) {
-    timeout = setTimeout(() => {
-      timeout = null;
-      enableUserTimingAPI = refCount > 0;
-    });
-  }
+  throw new Error('Not implemented.');
 }
 
 export const enableDeprecatedFlareAPI = true;
@@ -94,15 +65,15 @@ export const enableNativeTargetAsInstance = false;
 
 export const disableCreateFactory = false;
 
-export const disableTextareaChildren = false;
+export const disableTextareaChildren = true;
 
-export const disableMapsAsChildren = false;
+export const disableMapsAsChildren = true;
 
-export const disableUnstableRenderSubtreeIntoContainer = false;
+export const disableUnstableRenderSubtreeIntoContainer = true;
 
 export const warnUnstableRenderSubtreeIntoContainer = false;
 
-export const disableUnstableCreatePortal = false;
+export const disableUnstableCreatePortal = true;
 
 export const isTestEnvironment = false;
 
