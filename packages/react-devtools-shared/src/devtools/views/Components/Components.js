3ba1909dbe8c78936f6001c7ad0df98ed670261c
Hristo Kanchev <hristokkanchev@gmail.com>
2020-02-24 21:16:52 +0100

    feat: DevTools - Extracted code from Components fil.

--- packages/react-devtools-shared/src/devtools/views/Components/Components.js
+++ packages/react-devtools-shared/src/devtools/views/Components/Components.js
@@ -8,17 +8,17 @@
  */
 
 import * as React from 'react';
-import {Suspense, Fragment, useRef, useEffect, useReducer} from 'react';
+import {Suspense, Fragment } from 'react';
 import Tree from './Tree';
 import SelectedElement from './SelectedElement';
 import {InspectedElementContextController} from './InspectedElementContext';
 import {NativeStyleContextController} from './NativeStyleEditor/context';
 import {OwnersListContextController} from './OwnersListContext';
+import ComponentsResizer from './ComponentsResizer';
 import portaledContent from '../portaledContent';
 import {ModalDialog} from '../ModalDialog';
 import SettingsModal from 'react-devtools-shared/src/devtools/views/Settings/SettingsModal';
 import {SettingsModalContextController} from 'react-devtools-shared/src/devtools/views/Settings/SettingsModalContext';
-import {useLocalStorage} from '../hooks';
 
 import styles from './Components.css';
 
@@ -27,15 +27,13 @@ function Components(_: {||}) {
     <SettingsModalContextController>
       <OwnersListContextController>
         <InspectedElementContextController>
-          <ComponentResizer>
-            {({resizeElementRef, onResizeStart, resizeElementStyles}) => (
+          <ComponentsResizer>
+            {({resizeElementRef, onResizeStart}) => (
               <Fragment>
                 <div
                   ref={resizeElementRef}
                   className={styles.TreeWrapper}
-                  style={{
-                    ...resizeElementStyles,
-                  }}>
+                >
                   <Tree />
                 </div>
                 <div className={styles.ResizeBarWrapper}>
@@ -55,201 +53,10 @@ function Components(_: {||}) {
                 <SettingsModal />
               </Fragment>
             )}
-          </ComponentResizer>
+          </ComponentsResizer>
         </InspectedElementContextController>
       </OwnersListContextController>
     </SettingsModalContextController>
-  );
-}
-
-type HorizontalResizeDirection = 'HORIZONTAL';
-type VerticalResizeDirection = 'VERTICAL';
-
-const RESIZE_DIRECTIONS: {|
-  HORIZONTAL: HorizontalResizeDirection,
-  VERTICAL: VerticalResizeDirection,
-|} = {
-  HORIZONTAL: 'HORIZONTAL',
-  VERTICAL: 'VERTICAL',
-};
-
-function createResizeReducer(wrapperRef) {
-  const LOCAL_STORAGE_RESIZE_ELEMENT_PERCENTAGE_HORIZONTAL_KEY = `React::DevTools::resizedElementPercentage::${RESIZE_DIRECTIONS.HORIZONTAL}`;
-  const LOCAL_STORAGE_RESIZE_ELEMENT_PERCENTAGE_VERTICAL_KEY = `React::DevTools::resizedElementPercentage::${RESIZE_DIRECTIONS.VERTICAL}`;
-  const [
-    horizontalPercentage,
-    setHorizontalPercentage,
-  ] = useLocalStorage<string>(
-    LOCAL_STORAGE_RESIZE_ELEMENT_PERCENTAGE_HORIZONTAL_KEY,
-    '65%',
-  );
-  const [verticalPercentage, setVerticalPercentage] = useLocalStorage<string>(
-    LOCAL_STORAGE_RESIZE_ELEMENT_PERCENTAGE_VERTICAL_KEY,
-    '50%',
-  );
-  const resizeTimeout = useRef(null);
-
-  const getResizeDirection: Function = ref => () => {
-    if (ref.current != null) {
-      const VERTICAL_MODE_MAX_WIDTH: number = 600;
-      const {width} = ref.current.getBoundingClientRect();
-
-      return width > VERTICAL_MODE_MAX_WIDTH
-        ? RESIZE_DIRECTIONS.HORIZONTAL
-        : RESIZE_DIRECTIONS.VERTICAL;
-    }
-
-    return RESIZE_DIRECTIONS.HORIZONTAL;
-  };
-
-  // We need to watch/set for changes to this ref in order to get the correct resize direction.
-  useEffect(() => {
-    if (wrapperRef.current != null) {
-      dispatch({type: 'setWrapperRef', payload: wrapperRef});
-    }
-  }, [wrapperRef]);
-
-  const initialState = {
-    wrapperRef: wrapperRef,
-    isResizing: false,
-    horizontalPercentage,
-    verticalPercentage,
-    getResizeDirection: getResizeDirection(wrapperRef),
-  };
-
-  const ACTION_TYPES = {
-    SET_IS_RESIZING: 'setIsResizing',
-    SET_WRAPPER_REF: 'setWrapperRef',
-    SET_HORIZONTAL_PERCENTAGE: 'setHorizontalPercentage',
-    SET_VERTICAL_PERCENTAGE: 'setVerticalPercentage',
-  };
-
-  // eslint-disable-next-line no-shadow
-  const [state, dispatch] = useReducer((state, action) => {
-    switch (action.type) {
-      case ACTION_TYPES.SET_IS_RESIZING:
-        return {
-          ...state,
-          isResizing: action.payload,
-        };
-      case ACTION_TYPES.SET_WRAPPER_REF:
-        return {
-          ...state,
-          wrapperRef: action.payload,
-          getResizeDirection: getResizeDirection(action.payload),
-        };
-      case ACTION_TYPES.SET_HORIZONTAL_PERCENTAGE:
-      case ACTION_TYPES.SET_VERTICAL_PERCENTAGE:
-        const percentageState = {
-          [action.type === ACTION_TYPES.SET_HORIZONTAL_PERCENTAGE
-            ? 'horizontalPercentage'
-            : 'verticalPercentage']: action.payload,
-        };
-
-        clearTimeout(resizeTimeout.current);
-        resizeTimeout.current = setTimeout(() => {
-          if (action.type === ACTION_TYPES.SET_HORIZONTAL_PERCENTAGE) {
-            setHorizontalPercentage(action.payload);
-          } else {
-            setVerticalPercentage(action.payload);
-          }
-        }, 400);
-
-        return {
-          ...state,
-          ...percentageState,
-        };
-      default:
-        return state;
-    }
-  }, initialState);
-
-  return [state, dispatch, ACTION_TYPES];
-}
-
-function ComponentResizer({children}): {|children: Function|} {
-  const componentsWrapperRef = useRef<HTMLDivElement>(null);
-  const resizeElementRef = useRef<HTMLElement>(null);
-  const [state, dispatch, ACTION_TYPES] = createResizeReducer(
-    componentsWrapperRef,
-  );
-
-  const onResizeStart = () =>
-    dispatch({type: ACTION_TYPES.SET_IS_RESIZING, payload: true});
-  const onResizeEnd = () =>
-    dispatch({type: ACTION_TYPES.SET_IS_RESIZING, payload: false});
-  const onResize = e => {
-    if (
-      !state.isResizing ||
-      state.wrapperRef.current === null ||
-      resizeElementRef.current === null
-    ) {
-      return;
-    }
-
-    e.preventDefault();
-
-    const {
-      height,
-      width,
-      left,
-      top,
-    } = state.wrapperRef.current.getBoundingClientRect();
-    const resizeDirection = state.getResizeDirection();
-
-    const currentMousePosition: number =
-      resizeDirection === RESIZE_DIRECTIONS.HORIZONTAL
-        ? e.clientX - left
-        : e.clientY - top;
-    const BOUNDARY_PADDING: number = 42;
-    const boundary: {|
-      min: number,
-      max: number,
-    |} = {
-      min: BOUNDARY_PADDING,
-      max:
-        resizeDirection === RESIZE_DIRECTIONS.HORIZONTAL
-          ? width - BOUNDARY_PADDING
-          : height - BOUNDARY_PADDING,
-    };
-    const isMousePositionInBounds: boolean =
-      currentMousePosition > boundary.min &&
-      currentMousePosition < boundary.max;
-
-    if (isMousePositionInBounds) {
-      const resizedElementDimension: number =
-        resizeDirection === RESIZE_DIRECTIONS.HORIZONTAL ? width : height;
-      const updatedFlexBasisValue: string = `${(currentMousePosition /
-        resizedElementDimension) *
-        100}%`;
-      const SET_PERCENTAGE_ACTION =
-        resizeDirection === RESIZE_DIRECTIONS.HORIZONTAL
-          ? ACTION_TYPES.SET_HORIZONTAL_PERCENTAGE
-          : ACTION_TYPES.SET_VERTICAL_PERCENTAGE;
-
-      resizeElementRef.current.style.flexBasis = updatedFlexBasisValue;
-      dispatch({type: SET_PERCENTAGE_ACTION, payload: updatedFlexBasisValue});
-    }
-  };
-
-  const resizeElementStyles = {
-    flexBasis:
-      state.getResizeDirection() === RESIZE_DIRECTIONS.HORIZONTAL
-        ? state.horizontalPercentage
-        : state.verticalPercentage,
-  };
-
-  return (
-    <div
-      ref={componentsWrapperRef}
-      className={styles.ComponentsWrapper}
-      {...(state.isResizing && {
-        onMouseMove: onResize,
-        onMouseLeave: onResizeEnd,
-        onMouseUp: onResizeEnd,
-      })}>
-      {children({resizeElementRef, onResizeStart, resizeElementStyles})}
-    </div>
   );
 }
 
---
+++ packages/react-devtools-shared/src/devtools/views/Components/ComponentsResizer.js
@@ -0,0 +1,207 @@
+import type {ElementRef} from 'react';
+
+import * as React from 'react';
+import {useEffect, useLayoutEffect, useReducer, useRef} from 'react';
+import {
+  localStorageGetItem,
+  localStorageSetItem,
+} from 'react-devtools-shared/src/storage';
+import styles from './ComponentsResizer.css';
+
+const LOCAL_STORAGE_KEY = 'React::DevTools::createResizeReducer';
+const VERTICAL_MODE_MAX_WIDTH = 600;
+const MINIMUM_SIZE = 50;
+
+type Props = {|
+  children: ({
+    resizeElementRef: ElementRef<HTMLElement>,
+    onResizeStart: () => void,
+  }) => React$Node,
+|};
+
+export default function ComponentsResizer({children}: Props) {
+  const wrapperElementRef = useRef<HTMLDivElement>(null);
+  const resizeElementRef = useRef<HTMLElement>(null);
+  const [state, dispatch] = createResizeReducer(
+    wrapperElementRef,
+    resizeElementRef,
+  );
+
+  const {isResizing} = state;
+
+  const onResizeStart = () =>
+    dispatch({type: 'ACTION_SET_IS_RESIZING', payload: true});
+  const onResizeEnd = () =>
+    dispatch({type: 'ACTION_SET_IS_RESIZING', payload: false});
+
+  const onResize = event => {
+    const resizeElement = resizeElementRef.current;
+    const wrapperElement = wrapperElementRef.current;
+
+    if (!isResizing || wrapperElement === null || resizeElement === null) {
+      return;
+    }
+
+    event.preventDefault();
+
+    const orientation = getOrientation(wrapperElementRef);
+
+    const {height, width, left, top} = wrapperElement.getBoundingClientRect();
+
+    const currentMousePosition =
+      orientation === 'horizontal' ? event.clientX - left : event.clientY - top;
+
+    const boundaryMin = MINIMUM_SIZE;
+    const boundaryMax =
+      orientation === 'horizontal'
+        ? width - MINIMUM_SIZE
+        : height - MINIMUM_SIZE;
+
+    const isMousePositionInBounds =
+      currentMousePosition > boundaryMin && currentMousePosition < boundaryMax;
+
+    if (isMousePositionInBounds) {
+      const resizedElementDimension =
+        orientation === 'horizontal' ? width : height;
+      const actionType =
+        orientation === 'horizontal'
+          ? 'ACTION_SET_HORIZONTAL_PERCENTAGE'
+          : 'ACTION_SET_VERTICAL_PERCENTAGE';
+
+      resizeElement.style.flexBasis = `${(currentMousePosition /
+        resizedElementDimension) *
+      100}%`;
+
+      dispatch({
+        type: actionType,
+        payload: currentMousePosition / resizedElementDimension,
+      });
+    }
+  };
+
+  return (
+    <div
+      ref={wrapperElementRef}
+      className={styles.ComponentsWrapper}
+      {...(isResizing && {
+        onMouseMove: onResize,
+        onMouseLeave: onResizeEnd,
+        onMouseUp: onResizeEnd,
+      })}>
+      {children({resizeElementRef, onResizeStart})}
+    </div>
+  );
+}
+
+type Orientation = 'horizontal' | 'vertical';
+
+type ResizeActionType =
+  | 'ACTION_SET_DID_MOUNT'
+  | 'ACTION_SET_IS_RESIZING'
+  | 'ACTION_SET_HORIZONTAL_PERCENTAGE'
+  | 'ACTION_SET_VERTICAL_PERCENTAGE';
+
+type ResizeAction = {|
+  type: ResizeActionType,
+  payload: any,
+|};
+
+type ResizeState = {|
+  horizontalPercentage: number,
+  isResizing: boolean,
+  verticalPercentage: number,
+|};
+
+function initResizeState(): ResizeState {
+  let horizontalPercentage = 0.65;
+  let verticalPercentage = 0.5;
+
+  try {
+    let data = localStorageGetItem(LOCAL_STORAGE_KEY);
+    if (data != null) {
+      data = JSON.parse(data);
+      horizontalPercentage = data.horizontalPercentage;
+      verticalPercentage = data.verticalPercentage;
+    }
+  } catch (error) {}
+
+  return {
+    horizontalPercentage,
+    isResizing: false,
+    verticalPercentage,
+  };
+}
+
+function resizeReducer(state: ResizeState, action: ResizeAction): ResizeState {
+  switch (action.type) {
+    case 'ACTION_SET_IS_RESIZING':
+      return {
+        ...state,
+        isResizing: action.payload,
+      };
+    case 'ACTION_SET_HORIZONTAL_PERCENTAGE':
+      return {
+        ...state,
+        horizontalPercentage: action.payload,
+      };
+    case 'ACTION_SET_VERTICAL_PERCENTAGE':
+      return {
+        ...state,
+        verticalPercentage: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+function getOrientation(
+  wrapperElementRef: ElementRef<HTMLElement>,
+): null | Orientation {
+  const wrapperElement = wrapperElementRef.current;
+  if (wrapperElement != null) {
+    const {width} = wrapperElement.getBoundingClientRect();
+    return width > VERTICAL_MODE_MAX_WIDTH ? 'horizontal' : 'vertical';
+  }
+  return null;
+}
+
+function createResizeReducer(wrapperElementRef, resizeElementRef) {
+  const [state, dispatch] = useReducer<ResizeState, ResizeAction>(
+    resizeReducer,
+    null,
+    initResizeState,
+  );
+  const {horizontalPercentage, verticalPercentage} = state;
+  const orientationRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const orientation = getOrientation(wrapperElementRef);
+
+    if (orientation !== orientationRef.current) {
+      orientationRef.current = orientation;
+
+      const percentage =
+        orientation === 'horizontal'
+          ? horizontalPercentage
+          : verticalPercentage;
+      const resizeElement = resizeElementRef.current;
+      resizeElement.style.flexBasis = `${percentage * 100}%`;
+    }
+  });
+
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      localStorageSetItem(
+        LOCAL_STORAGE_KEY,
+        JSON.stringify({
+          horizontalPercentage,
+          verticalPercentage,
+        }),
+      );
+    }, 500);
+
+    return () => clearTimeout(timeoutID);
+  }, [horizontalPercentage, verticalPercentage]);
+
+  return [state, dispatch];
+}
