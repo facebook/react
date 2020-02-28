79a25125b19c2d6deaf03d4ab0dfdd6082377d97
Simen Bekkhus <sbekkhus91@gmail.com>
2020-02-28 03:01:17 +0100

    feat: add recommended config eslint rule (#14762)
    * feat: add recommended config eslint rule
    
    * add exhaustive-deps to recommended as well

--- packages/eslint-plugin-react-hooks/src/index.js
+++ packages/eslint-plugin-react-hooks/src/index.js
@@ -10,6 +10,16 @@
 import RuleOfHooks from './RulesOfHooks';
 import ExhaustiveDeps from './ExhaustiveDeps';
 
+export const configs = {
+  recommended: {
+    plugins: ['react-hooks'],
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+};
+
 export const rules = {
   'rules-of-hooks': RuleOfHooks,
   'exhaustive-deps': ExhaustiveDeps,
