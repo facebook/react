88ddf3b234f6a9ce0aaa928013432e7e46f1b5a9
Sebastian Markbage <sema@fb.com>
2020-03-06 15:59:47 -0800

    Rename FizzStreamer to FizzServer for consistency

--- packages/react-dom/src/server/ReactDOMFizzServerBrowser.js
+++ packages/react-dom/src/server/ReactDOMFizzServerBrowser.js
@@ -13,7 +13,7 @@ import {
   createRequest,
   startWork,
   startFlowing,
-} from 'react-server/src/ReactFizzStreamer';
+} from 'react-server/src/ReactFizzServer';
 
 function renderToReadableStream(children: ReactNodeList): ReadableStream {
   let request;
