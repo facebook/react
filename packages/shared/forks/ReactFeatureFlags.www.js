526c12f49ee6a0e371d73ecd638596777feac536
Brian Vaughn <bvaughn@fb.com>
2020-03-10 10:40:45 -0700

    Enable enableProfilerCommitHooks flag for FB (#18230)

--- packages/shared/forks/ReactFeatureFlags.www.js
+++ packages/shared/forks/ReactFeatureFlags.www.js
@@ -39,7 +39,7 @@ export const {
 export let enableUserTimingAPI = __DEV__ && !__EXPERIMENTAL__;
 
 export const enableProfilerTimer = __PROFILE__;
-export const enableProfilerCommitHooks = false;
+export const enableProfilerCommitHooks = __PROFILE__;
 export const enableSchedulerTracing = __PROFILE__;
 export const enableSchedulerDebugging = true;
 
