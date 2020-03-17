1fae59aee4392802fbe27690c8540dda0390c2d2
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-17 21:57:08 +0100

    test: Better place for partial renderer deprecated lifecycle warning

--- packages/react-dom/src/__tests__/ReactDOMServerLifecycles-test.js
+++ packages/react-dom/src/__tests__/ReactDOMServerLifecycles-test.js
@@ -213,7 +213,7 @@ describe('ReactDOMServerLifecycles', () => {
   it('should invoke both deprecated and new lifecycles if both are present', () => {
     const log = [];
 
-    class MyComponent extends React.Component {
+    class Component extends React.Component {
       componentWillMount() {
         log.push('componentWillMount');
       }
@@ -225,10 +225,8 @@ describe('ReactDOMServerLifecycles', () => {
       }
     }
 
-    expect(() => ReactDOMServer.renderToString(<MyComponent />)).toWarnDev(
-      'componentWillMount has been renamed, and is not recommended for use. See https://fb.me/react-unsafe-component-lifecycles for details.\n\n' +
-        '* Move code from componentWillMount to componentDidMount (preferred in most cases) or the constructor.\n\n' +
-        'Please update the following components: MyComponent',
+    expect(() => ReactDOMServer.renderToString(<Component />)).toWarnDev(
+      'componentWillMount has been renamed',
     );
     expect(log).toEqual(['componentWillMount', 'UNSAFE_componentWillMount']);
   });
@@ -287,19 +285,21 @@ describe('ReactDOMServerLifecycles', () => {
   });
 
   it('should warn about deprecated lifecycle hooks', () => {
-    class Component extends React.Component {
+    class MyComponent extends React.Component {
       componentWillMount() {}
       render() {
         return null;
       }
     }
 
-    expect(() => ReactDOMServer.renderToString(<Component />)).toWarnDev(
-      'componentWillMount has been renamed',
+    expect(() => ReactDOMServer.renderToString(<MyComponent />)).toWarnDev(
+      'componentWillMount has been renamed, and is not recommended for use. See https://fb.me/react-unsafe-component-lifecycles for details.\n\n' +
+        '* Move code from componentWillMount to componentDidMount (preferred in most cases) or the constructor.\n\n' +
+        'Please update the following components: MyComponent',
     );
 
     // De-duped
-    ReactDOMServer.renderToString(<Component />);
+    ReactDOMServer.renderToString(<MyComponent />);
   });
 
   describe('react-lifecycles-compat', () => {
