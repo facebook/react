67eff2733556d041704772ba224b75f388308797
Nick Reiley <bloomber111@gmail.com>
2020-03-25 03:27:12 +0500

    add regression tests to ensure attributes are working

--- fixtures/dom/src/components/fixtures/email-inputs/index.js
+++ fixtures/dom/src/components/fixtures/email-inputs/index.js
@@ -1,6 +1,8 @@
 import FixtureSet from '../../FixtureSet';
 import TestCase from '../../TestCase';
 import JumpingCursorTestCase from './JumpingCursorTestCase';
+import EmailEnabledAttributesTestCase from './EmailEnabledAttributesTestCase';
+import EmailDisabledAttributesTestCase from './EmailDisabledAttributesTestCase';
 
 const React = window.React;
 
@@ -22,6 +24,42 @@ function EmailInputs() {
         <TestCase.ExpectedResult>Cursor not moving.</TestCase.ExpectedResult>
 
         <JumpingCursorTestCase />
+      </TestCase>
+
+      <TestCase
+        title="Attributes enabled"
+        description={`
+          Test enabled pattern, maxlength, multiple attributes.
+        `}>
+        <TestCase.Steps>
+          <li>Type after existing text ',b@tt.com'</li>
+          <li>Try to type spaces after typed text</li>
+        </TestCase.Steps>
+
+        <TestCase.ExpectedResult>
+          Spaces not added. When cursor hovered over input, popup "Please match
+          the requested format." is showed.
+        </TestCase.ExpectedResult>
+
+        <EmailEnabledAttributesTestCase />
+      </TestCase>
+
+      <TestCase
+        title="Attributes disabled"
+        description={`
+          Test disabled maxlength, multiple attributes.
+        `}>
+        <TestCase.Steps>
+          <li>Type after existing text ',b@tt.com'</li>
+          <li>Try to type spaces after typed text</li>
+        </TestCase.Steps>
+
+        <TestCase.ExpectedResult>
+          Spaces are added freely. When cursor hovered over input, popup "A part
+          following '@' should not contain the symbol ','." is showed.
+        </TestCase.ExpectedResult>
+
+        <EmailDisabledAttributesTestCase />
       </TestCase>
     </FixtureSet>
   );
