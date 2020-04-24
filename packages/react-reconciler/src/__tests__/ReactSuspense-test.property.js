0e0ac2ff0f7baa4d56741916d05c3c42100f0892
Nicolas DUBIEN <github@dubien.org>
2020-04-24 10:15:37 +0200

    Quick and dirty nicer output draft

--- packages/react-reconciler/src/__tests__/ReactSuspense-test.property.js
+++ packages/react-reconciler/src/__tests__/ReactSuspense-test.property.js
@@ -31,6 +31,22 @@ function AsyncText({text, readOrThrow}) {
   return <span>{text}</span>;
 }
 
+function withCustomError(gens, customToString) {
+  return fc.record(gens).map(out => {
+    return {
+      ...out,
+      toString() {
+        return `
+Raw failure:
+${fc.stringify(out)}
+
+Formated failure:
+${customToString(out)}`;
+      },
+    };
+  });
+}
+
 describe('ReactSuspense', () => {
   beforeEach(beforeEachAction);
 
@@ -38,27 +54,103 @@ describe('ReactSuspense', () => {
     await fc.assert(
       fc
         .asyncProperty(
-          // Scheduler able to re-order operations
-          fc.scheduler(),
-          // The initial text defined in the App component
-          fc.stringOf(fc.hexa()),
-          // Array of updates with the associated priority
-          fc.array(
-            fc.record({
-              // Priority of the task
-              priority: fc.constantFrom(
-                Scheduler.unstable_ImmediatePriority,
-                Scheduler.unstable_UserBlockingPriority,
-                Scheduler.unstable_NormalPriority,
-                Scheduler.unstable_IdlePriority,
-                Scheduler.unstable_LowPriority,
+          withCustomError(
+            {
+              // Scheduler able to re-order operations
+              s: fc.scheduler(),
+              // The initial text defined in the App component
+              initialText: fc.stringOf(fc.hexa()),
+              // Array of updates with the associated priority
+              textUpdates: fc.array(
+                fc.record({
+                  // Priority of the task
+                  priority: fc.constantFrom(
+                    Scheduler.unstable_ImmediatePriority,
+                    Scheduler.unstable_UserBlockingPriority,
+                    Scheduler.unstable_NormalPriority,
+                    Scheduler.unstable_IdlePriority,
+                    Scheduler.unstable_LowPriority,
+                  ),
+                  // Value to set for text
+                  text: fc.stringOf(fc.hexa()),
+                }),
               ),
-              // Value to set for text
-              text: fc.stringOf(fc.hexa()),
-            }),
+            },
+            ({s, initialText, textUpdates}) => {
+              // This list of actions could be directly exposed by fast-check
+              const actions = s
+                .toString()
+                .split('\n')
+                .filter(line => line.includes('-> [task#'))
+                .map(line =>
+                  JSON.parse(line.split('::', 2)[1].split(' resolved')[0]),
+                );
+              return `
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { unstable_runWithPriority } from "scheduler";
+
+function createResource(name) {
+  let resolved = false;
+  let resolve;
+  let promise = new Promise(r => (resolve = r));
+  return {
+    name,
+    read() {
+      console.log(\`Reading \${JSON.stringify(name)}\`);
+      if (!resolved) throw promise;
+    },
+    resolve() {
+      console.log(\`Resolving \${JSON.stringify(name)}\`);
+      resolved = true;
+      resolve();
+    }
+  };
+}
+
+let _setText;
+const resources = new Map();
+${actions
+  .map(a => {
+    const escapedText = JSON.stringify(a.text);
+    return `resources.set(${escapedText}, createResource(${escapedText}));`;
+  })
+  .join('\n')}
+
+${actions
+  .map((a, id) => {
+    const escapedText = JSON.stringify(a.text);
+    const delay = (id + 1) * 1000;
+    if (a.type === 'request') {
+      return `setTimeout(() => resources.get(${escapedText}).resolve(), ${delay});`;
+    } else if (a.type === 'scheduler') {
+      return `setTimeout(() => unstable_runWithPriority(${a.priority}, () => _setText(${escapedText})), ${delay});`;
+    }
+    return '// UNEXPECTED ACTION...';
+  })
+  .join('\n')}
+
+function App() {
+  const [text, setText] = useState(${JSON.stringify(initialText)});
+  _setText = setText;
+  const res = resources.get(text);
+  res.read();
+  return <span>{res.name}</span>;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(
+  rootElement
+).render(
+  <React.Suspense fallback={<h1>Loading the app...</h1>}>
+    <App />
+  </React.Suspense>
+);
+              `;
+            },
           ),
           // The code under test
-          async (s, initialText, textUpdates) => {
+          async ({s, initialText, textUpdates}) => {
             // We simulate a cache: string -> Promise
             // It may contain successes and rejections
             const cache = new Map();
@@ -74,7 +166,7 @@ describe('ReactSuspense', () => {
                 // Not yet queried
                 const promise = s.schedule(
                   Promise.resolve(),
-                  `Request for ${JSON.stringify(text)}`,
+                  JSON.stringify({type: 'request', text}),
                 );
                 const cachedValue = {promise, resolvedWith: null};
                 promise.then(success => (cachedValue.resolvedWith = {success}));
@@ -105,9 +197,7 @@ describe('ReactSuspense', () => {
             s.scheduleSequence(
               textUpdates.map(update => {
                 return {
-                  label: `Scheduling ${JSON.stringify(
-                    update.text,
-                  )} with priority ${update.priority}`,
+                  label: JSON.stringify({type: 'scheduler', ...update}),
                   builder: async () =>
                     Scheduler.unstable_runWithPriority(update.priority, () => {
                       setText(update.text);
