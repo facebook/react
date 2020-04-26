ef9d1bd1e6cc5f1a10352040ce636283e4d2f831
ChrisDobby <dobby@chrisjdobson.co.uk>
2020-04-26 09:37:16 +0100

    added style for selected path to css

--- packages/react-devtools-shared/src/devtools/views/Profiler/ChartNode.js
+++ packages/react-devtools-shared/src/devtools/views/Profiler/ChartNode.js
@@ -54,8 +54,9 @@ export default function ChartNode({
         width="4"
         height="4">
         <path
+          className={styles.SelectedPath}
           d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2"
-          style={{strokeWidth: 2.4, stroke: color}}
+          style={{stroke: color}}
         />
       </pattern>
     </defs>
