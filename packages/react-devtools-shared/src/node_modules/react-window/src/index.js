bfd6d4b378a1cb2c3a8c9a96a8f0911aed7f2716
Pirate Praveen <praveen@debian.org>
2020-04-28 15:34:20 +0530

    Add react-window as devDependency and drop embedded copy

--- packages/react-devtools-shared/src/node_modules/react-window/src/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-export { default as VariableSizeGrid } from './VariableSizeGrid';
-export { default as VariableSizeList } from './VariableSizeList';
-export { default as FixedSizeGrid } from './FixedSizeGrid';
-export { default as FixedSizeList } from './FixedSizeList';
-
-export { default as areEqual } from './areEqual';
-export { default as shouldComponentUpdate } from './shouldComponentUpdate';
