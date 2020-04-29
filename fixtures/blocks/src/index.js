53d68b33ff6ff6acd04b5a077fdc86627d319e08
Dan Abramov <dan.abramov@gmail.com>
2020-04-29 02:23:09 +0100

    [Blocks] Add Client Fixture (#18773)
    * [Blocks] Add Client Fixture
    
    * Add more TODOs

---
+++ fixtures/blocks/src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
