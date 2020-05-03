bcc8104a847ecd88a99f5186c126b203ba554b57
Dan Abramov <dan.abramov@me.com>
2020-05-03 23:02:37 +0100

    Simplify the test

--- packages/react-reconciler/src/__tests__/ReactBlocks-test.js
+++ packages/react-reconciler/src/__tests__/ReactBlocks-test.js
@@ -260,29 +260,20 @@ describe('ReactBlocks', () => {
 
   // @gate experimental
   it('can load a nested block after an update', async () => {
-    const loadChild = block(
-      function Child(props, data) {
-        return <span>Name: {data.name}</span>;
-      },
-      function load(name) {
-        return {
-          name: readString(name),
-        };
-      },
-    );
+    function Child() {
+      return <span>Name: {readString('Sebastian')}</span>;
+    }
 
     const loadParent = block(
       function Parent(props, data) {
         return (
           <Suspense fallback="Loading...">
-            {data.Child ? <data.Child /> : <span>Empty</span>}
+            {data.name ? <Child /> : <span>Empty</span>}
           </Suspense>
         );
       },
       function load(name) {
-        return {
-          Child: name !== null ? loadChild(name) : null,
-        };
+        return {name};
       },
     );
 
