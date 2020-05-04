f9d5918db478d0bc751235aae4605b35ddc5348c
Simen Bekkhus <sbekkhus91@gmail.com>
2020-05-04 19:25:21 +0200

    more, not covered by linter pattern

--- scripts/jest/setupTests.build.js
+++ scripts/jest/setupTests.build.js
@@ -1,6 +1,6 @@
 'use strict';
 
-jest.mock('scheduler', () => require.requireActual('scheduler/unstable_mock'));
+jest.mock('scheduler', () => jest.requireActual('scheduler/unstable_mock'));
 jest.mock('scheduler/src/SchedulerHostConfig', () =>
-  require.requireActual('scheduler/src/forks/SchedulerHostConfig.mock.js')
+  jest.requireActual('scheduler/src/forks/SchedulerHostConfig.mock.js')
 );
