fb3f0acad9ac0b5756724d53eddaa444767dca07
Brian Vaughn <bvaughn@fb.com>
2020-05-07 13:13:47 -0700

    Disable Webpack setImmediate polyfill for DevTools (#18860)
    * Upgrade Webpack deps to latet
    
    * Disable Webpack setImmediate polyfill

--- packages/react-devtools-extensions/webpack.config.js
+++ packages/react-devtools-extensions/webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
     path: __dirname + '/build',
     filename: '[name].js',
   },
+  node: {
+    // Don't define a polyfill on window.setImmediate
+    setImmediate: false,
+  },
   resolve: {
     alias: {
       react: resolve(builtModulesDir, 'react'),
