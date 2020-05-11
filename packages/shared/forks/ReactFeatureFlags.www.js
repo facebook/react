ef0bf8e31cda331cbee4d3acf74e76795e311391
Dominic Gannaway <trueadm@users.noreply.github.com>
2020-05-11 23:29:24 +0100

    Revert "Hard code enableComponentStacks in www (#18869)" (#18890)
    This reverts commit fd696df4729fb68cf5c3f1a8125524299a5892fc.

--- packages/shared/forks/ReactFeatureFlags.www.js
+++ packages/shared/forks/ReactFeatureFlags.www.js
@@ -20,6 +20,7 @@ export const {
   enableTrustedTypesIntegration,
   disableSchedulerTimeoutBasedOnReactExpirationTime,
   warnAboutSpreadingKeyToJSX,
+  enableComponentStackLocations,
   replayFailedUnitOfWorkWithInvokeGuardedCallback,
   enableModernEventSystem,
   enableFilterEmptyStringAttributesDOM,
@@ -67,8 +68,6 @@ export const enableSuspenseCallback = true;
 export const disableTextareaChildren = __EXPERIMENTAL__;
 
 export const warnUnstableRenderSubtreeIntoContainer = false;
-
-export const enableComponentStackLocations = true;
 
 // Enable forked reconciler. Piggy-backing on the "variant" global so that we
 // don't have to add another test dimension. The build system will compile this
