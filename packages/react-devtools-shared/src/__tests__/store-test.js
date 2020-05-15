d92bc4c36c56725c63957bc3d6ecd4e3c5babdd4
Brian Vaughn <brian.david.vaughn@gmail.com>
2020-05-15 09:27:16 -0700

    Reverted accidental "unstable_" prefix rename b'c of old branch.

--- packages/react-devtools-shared/src/__tests__/store-test.js
+++ packages/react-devtools-shared/src/__tests__/store-test.js
@@ -352,18 +352,19 @@ describe('Store', () => {
       };
       const Wrapper = ({shouldSuspense}) => (
         <React.Fragment>
-          <React.unstable_SuspenseList revealOrder="forwards" tail="collapsed">
+          <React.SuspenseList revealOrder="forwards" tail="collapsed">
             <Component key="A" />
             <React.Suspense fallback={<Loading />}>
               {shouldSuspense ? <SuspendingComponent /> : <Component key="B" />}
             </React.Suspense>
             <Component key="C" />
-          </React.unstable_SuspenseList>
+          </React.SuspenseList>
         </React.Fragment>
       );
 
       const container = document.createElement('div');
-      const root = ReactDOM.unstable_createRoot(container);
+      const root = ReactDOM.
+      createRoot(container);
       act(() => {
         root.render(<Wrapper shouldSuspense={true} />);
       });
