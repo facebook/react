94611ebe5cbe4f9d4f5adf6a9a5044aae980978f
Damien Maillard <damien.maillard@dailymotion.com>
2020-05-20 12:57:22 +0200

    Move iframe files to fixtures directory

---
+++ fixtures/devtools/iframe/server.js
@@ -0,0 +1,14 @@
+const finalhandler = require('finalhandler');
+const http = require('http');
+const serveStatic = require('serve-static');
+
+// Serve fixtures folder
+const serve = serveStatic(__dirname);
+
+// Create server
+const server = http.createServer(function onRequest(req, res) {
+  serve(req, res, finalhandler(req, res));
+});
+
+// Listen
+server.listen(3000);
