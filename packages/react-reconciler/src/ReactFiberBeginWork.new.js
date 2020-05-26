03e6b8ba2fb1c2820d2a5286701ff2b40590b438
Andrew Clark <git@andrewclark.io>
2020-05-25 18:16:53 -0700

    Make LegacyHidden match semantics of old fork (#18998)
    Facebook currently relies on being able to hydrate hidden HTML. So
    skipping those trees is a regression.
    
    We don't have a proper solution for this in the new API yet. So I'm
    reverting it to match the old behavior.
    
    Now the server renderer will treat LegacyHidden the same as a fragment,
    with no other special behavior. We can only get away with this because
    we assume that every instance of LegacyHidden is accompanied by a host
    component wrapper. In the hidden mode, the host component is given a
    `hidden` attribute, which ensures that the initial HTML is not visible.
    To support the use of LegacyHidden as a true fragment, without an extra
    DOM node, we will have to hide the initial HTML in some other way.

--- packages/react-reconciler/src/ReactFiberBeginWork.new.js
+++ packages/react-reconciler/src/ReactFiberBeginWork.new.js
@@ -167,7 +167,6 @@ import {
   reenterHydrationStateFromDehydratedSuspenseInstance,
   resetHydrationState,
   tryToClaimNextHydratableInstance,
-  getIsHydrating,
   warnIfHydrating,
 } from './ReactFiberHydrationContext.new';
 import {
@@ -584,13 +583,7 @@ function updateOffscreenComponent(
       };
       workInProgress.memoizedState = nextState;
       pushRenderLanes(workInProgress, renderLanes);
-    } else if (
-      !includesSomeLane(renderLanes, (OffscreenLane: Lane)) ||
-      // Server renderer does not render hidden subtrees, so if we're hydrating
-      // we should always bail out and schedule a subsequent render pass, to
-      // force a client render. Even if we're already at Offscreen priority.
-      (current === null && getIsHydrating())
-    ) {
+    } else if (!includesSomeLane(renderLanes, (OffscreenLane: Lane))) {
       let nextBaseLanes;
       if (prevState !== null) {
         const prevBaseLanes = prevState.baseLanes;
