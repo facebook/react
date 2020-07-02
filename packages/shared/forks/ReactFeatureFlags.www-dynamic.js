142d5c1491e09a09b39cf3638778b1c335d9b566
Kartik Choudhary <kartikc.918@gmail.com>
2020-07-02 11:21:43 +0530

    Add  __PROFILE__ to enableDebugTracing

--- packages/shared/forks/ReactFeatureFlags.www-dynamic.js
+++ packages/shared/forks/ReactFeatureFlags.www-dynamic.js
@@ -17,7 +17,8 @@ export const warnAboutSpreadingKeyToJSX = __VARIANT__;
 export const disableInputAttributeSyncing = __VARIANT__;
 export const enableFilterEmptyStringAttributesDOM = __VARIANT__;
 export const enableLegacyFBSupport = __VARIANT__;
-export const enableDebugTracing = !__VARIANT__;
+// TODO: These features do not currently exist in the new reconciler fork.
+export const enableDebugTracing = !__VARIANT__ && __PROFILE__;
 export const enableSchedulingProfiler = !__VARIANT__;
 
 // This only has an effect in the new reconciler. But also, the new reconciler
