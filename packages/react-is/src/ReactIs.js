6ab3229ef06b859f49783b6f4c85a17ad579707a
Behnam Mohammadi <itten@live.com>
2020-07-03 23:00:45 +0430

    remove nested switch

--- packages/react-is/src/ReactIs.js
+++ packages/react-is/src/ReactIs.js
@@ -28,29 +28,26 @@ export function typeOf(object: any) {
   if (typeof object === 'object' && object !== null) {
     const $$typeof = object.$$typeof;
     switch ($$typeof) {
-      case REACT_ELEMENT_TYPE:
-        const type = object.type;
-
-        switch (type) {
-          case REACT_FRAGMENT_TYPE:
-          case REACT_PROFILER_TYPE:
-          case REACT_STRICT_MODE_TYPE:
-          case REACT_SUSPENSE_TYPE:
-            return type;
-          default:
-            const $$typeofType = type && type.$$typeof;
-
-            switch ($$typeofType) {
-              case REACT_CONTEXT_TYPE:
-              case REACT_FORWARD_REF_TYPE:
-              case REACT_LAZY_TYPE:
-              case REACT_MEMO_TYPE:
-              case REACT_PROVIDER_TYPE:
-                return $$typeofType;
-              default:
-                return $$typeof;
-            }
-        }
+      case REACT_ELEMENT_TYPE: {
+        return (
+          {
+            [REACT_ASYNC_MODE_TYPE]: REACT_ASYNC_MODE_TYPE,
+            [REACT_CONCURRENT_MODE_TYPE]: REACT_CONCURRENT_MODE_TYPE,
+            [REACT_FRAGMENT_TYPE]: REACT_FRAGMENT_TYPE,
+            [REACT_PROFILER_TYPE]: REACT_PROFILER_TYPE,
+            [REACT_STRICT_MODE_TYPE]: REACT_STRICT_MODE_TYPE,
+            [REACT_SUSPENSE_TYPE]: REACT_SUSPENSE_TYPE,
+          }[object.type] ||
+          {
+            [REACT_CONTEXT_TYPE]: REACT_CONTEXT_TYPE,
+            [REACT_FORWARD_REF_TYPE]: REACT_FORWARD_REF_TYPE,
+            [REACT_LAZY_TYPE]: REACT_LAZY_TYPE,
+            [REACT_MEMO_TYPE]: REACT_MEMO_TYPE,
+            [REACT_PROVIDER_TYPE]: REACT_PROVIDER_TYPE,
+          }[object.type && object.type.$$typeof] ||
+          $$typeof
+        );
+      }
       case REACT_PORTAL_TYPE:
         return $$typeof;
     }
