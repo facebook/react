21fdac0dd81916b175eebd1a4d36e766e6635bff
pmaccart <pmaccart@gmail.com>
2020-07-07 18:40:07 -0700

    reset currentlyRenderingComponent to null

--- packages/react-dom/src/server/ReactPartialRendererHooks.js
+++ packages/react-dom/src/server/ReactPartialRendererHooks.js
@@ -212,6 +212,7 @@ export function resetHooksState(): void {
     isInHookUserCodeInDev = false;
   }
 
+  currentlyRenderingComponent = null;
   didScheduleRenderPhaseUpdate = false;
   firstWorkInProgressHook = null;
   numberOfReRenders = 0;
