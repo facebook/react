b85b47630be57c7031b0a9ab741cf858dc0ca215
Phil MacCart <pmaccart@gmail.com>
2020-07-07 19:10:23 -0700

    Fix state leaking when a function component throws on server render (#19212)
    * add unit test asserting internal hooks state is reset
    
    * Reset internal hooks state before rendering
    
    * reset hooks state on error
    
    * Use expect...toThrow instead of try/catch in test
    
    * reset dev-only hooks state inside resetHooksState
    
    * reset currentlyRenderingComponent to null

--- packages/react-dom/src/__tests__/ReactDOMServerIntegrationHooks-test.js
+++ packages/react-dom/src/__tests__/ReactDOMServerIntegrationHooks-test.js
@@ -867,6 +867,44 @@ describe('ReactDOMServerHooks', () => {
     });
   });
 
+  it('renders successfully after a component using hooks throws an error', () => {
+    function ThrowingComponent() {
+      const [value, dispatch] = useReducer((state, action) => {
+        return state + 1;
+      }, 0);
+
+      // throw an error if the count gets too high during the re-render phase
+      if (value >= 3) {
+        throw new Error('Error from ThrowingComponent');
+      } else {
+        // dispatch to trigger a re-render of the component
+        dispatch();
+      }
+
+      return <div>{value}</div>;
+    }
+
+    function NonThrowingComponent() {
+      const [count] = useState(0);
+      return <div>{count}</div>;
+    }
+
+    // First, render a component that will throw an error during a re-render triggered
+    // by a dispatch call.
+    expect(() => ReactDOMServer.renderToString(<ThrowingComponent />)).toThrow(
+      'Error from ThrowingComponent',
+    );
+
+    // Next, assert that we can render a function component using hooks immediately
+    // after an error occurred, which indictates the internal hooks state has been
+    // reset.
+    const container = document.createElement('div');
+    container.innerHTML = ReactDOMServer.renderToString(
+      <NonThrowingComponent />,
+    );
+    expect(container.children[0].textContent).toEqual('0');
+  });
+
   if (__EXPERIMENTAL__) {
     describe('useOpaqueIdentifier', () => {
       it('generates unique ids for server string render', async () => {
