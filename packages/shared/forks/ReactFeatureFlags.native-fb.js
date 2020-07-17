00575787bc7241982adb2fde4cb03759379166b5
Dan Abramov <dan.abramov@me.com>
2020-07-17 14:08:44 +0100

    Remove form event delegation flag

--- packages/shared/forks/ReactFeatureFlags.native-fb.js
+++ packages/shared/forks/ReactFeatureFlags.native-fb.js
@@ -44,7 +44,6 @@ export const warnAboutSpreadingKeyToJSX = false;
 export const enableComponentStackLocations = false;
 export const enableLegacyFBSupport = false;
 export const enableFilterEmptyStringAttributesDOM = false;
-export const enableFormEventDelegation = false;
 
 export const enableNewReconciler = false;
 export const deferRenderPhaseUpdateToNextBatch = true;
