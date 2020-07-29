9050609a9a5da069dd2a5bef0e97ba0b47d3f476
Rick Hanlon <rickhanlonii@gmail.com>
2020-07-29 14:55:15 -0400

    Updates from review

--- packages/scheduler/src/forks/SchedulerHostConfig.post-task.js
+++ packages/scheduler/src/forks/SchedulerHostConfig.post-task.js
@@ -6,30 +6,20 @@
  */
 
 // Capture local references to native APIs, in case a polyfill overrides them.
-const date = window.Date;
 const perf = window.performance;
 const setTimeout = window.setTimeout;
 const clearTimeout = window.clearTimeout;
-
-// This check should be done upstream but do it again for a clear failure message.
-if (global.scheduler === undefined || global.scheduler.postTask === undefined) {
-  throw new Error('Cannot use postTask Scheduler without global.scheduler');
-}
 
 function postTask(callback) {
   // Use experimental Chrome Scheduler postTask API.
   global.scheduler.postTask(callback);
 }
 
-let getNow;
-if (typeof perf === 'object' && typeof perf.now === 'function') {
-  getNow = () => perf.now();
-} else {
-  const initialTime = date.now();
-  getNow = () => date.now() - initialTime;
+function getNow() {
+  return perf.now();
 }
 
-let isMessageLoopRunning = false;
+let isTaskLoopRunning = false;
 let scheduledHostCallback = null;
 let taskTimeoutID = -1;
 
@@ -37,36 +27,24 @@ let taskTimeoutID = -1;
 // thread, like user events. By default, it yields multiple times per frame.
 // It does not attempt to align with frame boundaries, since most tasks don't
 // need to be frame aligned; for those that do, use requestAnimationFrame.
-let yieldInterval = 5;
+const yieldInterval = 5;
 let deadline = 0;
 
 // `isInputPending` is not available. Since we have no way of knowing if
 // there's pending input, always yield at the end of the frame.
-export const shouldYieldToHost = function() {
+export function shouldYieldToHost() {
   return getNow() >= deadline;
-};
+}
 
-// Since we yield every frame regardless, `requestPaint` has no effect.
-export const requestPaint = function() {};
+export function requestPaint() {
+  // Since we yield every frame regardless, `requestPaint` has no effect.
+}
 
-export const forceFrameRate = function(fps) {
-  if (fps < 0 || fps > 125) {
-    // Using console['error'] to evade Babel and ESLint
-    console['error'](
-      'forceFrameRate takes a positive int between 0 and 125, ' +
-        'forcing frame rates higher than 125 fps is not unsupported',
-    );
-    return;
-  }
-  if (fps > 0) {
-    yieldInterval = Math.floor(1000 / fps);
-  } else {
-    // reset the framerate
-    yieldInterval = 5;
-  }
-};
+export function forceFrameRate(fps) {
+  // No-op
+}
 
-const performWorkUntilDeadline = () => {
+function performWorkUntilDeadline() {
   if (scheduledHostCallback !== null) {
     const currentTime = getNow();
     // Yield after `yieldInterval` ms, regardless of where we are in the vsync
@@ -77,7 +55,7 @@ const performWorkUntilDeadline = () => {
     try {
       const hasMoreWork = scheduledHostCallback(hasTimeRemaining, currentTime);
       if (!hasMoreWork) {
-        isMessageLoopRunning = false;
+        isTaskLoopRunning = false;
         scheduledHostCallback = null;
       } else {
         // If there's more work, schedule the next message event at the end
@@ -91,31 +69,31 @@ const performWorkUntilDeadline = () => {
       throw error;
     }
   } else {
-    isMessageLoopRunning = false;
+    isTaskLoopRunning = false;
   }
-};
+}
 
-export const requestHostCallback = function(callback) {
+export function requestHostCallback(callback) {
   scheduledHostCallback = callback;
-  if (!isMessageLoopRunning) {
-    isMessageLoopRunning = true;
+  if (!isTaskLoopRunning) {
+    isTaskLoopRunning = true;
     postTask(performWorkUntilDeadline);
   }
-};
+}
 
-export const cancelHostCallback = function() {
+export function cancelHostCallback() {
   scheduledHostCallback = null;
-};
+}
 
-export const requestHostTimeout = function(callback, ms) {
+export function requestHostTimeout(callback, ms) {
   taskTimeoutID = setTimeout(() => {
     callback(getNow());
   }, ms);
-};
+}
 
-export const cancelHostTimeout = function() {
+export function cancelHostTimeout() {
   clearTimeout(taskTimeoutID);
   taskTimeoutID = -1;
-};
+}
 
 export const getCurrentTime = getNow;
