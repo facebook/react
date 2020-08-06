2c89368172c2d1495b6a4f91f2a64ee6269408b2
Dan Abramov <dan.abramov@me.com>
2020-08-06 23:56:21 +0100

    Use Consumer API

--- fixtures/nesting/src/legacy/Greeting.js
+++ fixtures/nesting/src/legacy/Greeting.js
@@ -13,33 +13,33 @@ store.subscribe(() => {
 });
 
 class AboutSection extends Component {
-  static contextType = ThemeContext;
-
   componentDidMount() {
     // The modern app is wrapped in StrictMode,
     // but the legacy bits can still use old APIs.
     findDOMNode(this);
   }
-
   render() {
-    const theme = this.context;
     return (
-      <div style={{border: '1px dashed black', padding: 20}}>
-        <h3>src/legacy/Greeting.js</h3>
-        <h4 style={{color: theme}}>
-          This component is rendered by the nested React ({React.version}).
-        </h4>
-        <Clock />
-        <p>
-          Counter: {this.props.counter}{' '}
-          <button onClick={() => this.props.dispatch({type: 'increment'})}>
-            +
-          </button>
-        </p>
-        <b>
-          <Link to="/">Go to Home</Link>
-        </b>
-      </div>
+      <ThemeContext.Consumer>
+        {theme => (
+          <div style={{border: '1px dashed black', padding: 20}}>
+            <h3>src/legacy/Greeting.js</h3>
+            <h4 style={{color: theme}}>
+              This component is rendered by the nested React ({React.version}).
+            </h4>
+            <Clock />
+            <p>
+              Counter: {this.props.counter}{' '}
+              <button onClick={() => this.props.dispatch({type: 'increment'})}>
+                +
+              </button>
+            </p>
+            <b>
+              <Link to="/">Go to Home</Link>
+            </b>
+          </div>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
