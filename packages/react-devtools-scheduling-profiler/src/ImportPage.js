0386bd0da22cce1608f42072e64b35443eae871b
Brian Vaughn <bvaughn@fb.com>
2020-08-21 14:06:01 -0400

    Tweaked landing page styles (#19665)
    * Tweaked landing page styles
    
    * Update GitHub link
    
    Co-authored-by: E-Liang Tan <eliang@eliangtan.com>

--- packages/react-devtools-scheduling-profiler/src/ImportPage.js
+++ packages/react-devtools-scheduling-profiler/src/ImportPage.js
@@ -45,71 +45,68 @@ export default function ImportPage({onDataImported}: Props) {
 
   return (
     <div className={style.App}>
-      <div className={style.container}>
-        <div className={style.card}>
-          <div className={style.cardcontainer}>
-            <div className={style.row}>
-              <div className={style.column}>
-                <img
-                  src={profilerBrowser}
-                  className={style.browserScreenshot}
-                  alt="logo"
-                />
-              </div>
-              <div className={style.columncontent}>
-                <h2>React Concurrent Mode Profiler</h2>
-                <hr />
-                <p>
-                  Import a captured{' '}
-                  <a
-                    className={style.link}
-                    href="https://developers.google.com/web/tools/chrome-devtools/evaluate-performance">
-                    performance profile
-                  </a>{' '}
-                  from Chrome Devtools.
-                  <br />
-                  To zoom, scroll while holding down <kbd>Ctrl</kbd> or{' '}
-                  <kbd>Shift</kbd>
-                </p>
-                <p className={style.legendKey}>
-                  <svg height="20" width="20">
-                    <circle cx="10" cy="10" r="5" fill="#ff718e" />
-                  </svg>
-                  State Update Scheduled
-                  <br />
-                  <svg height="20" width="20">
-                    <circle cx="10" cy="10" r="5" fill="#9fc3f3" />
-                  </svg>
-                  State Update Scheduled
-                  <br />
-                  <svg height="20" width="20">
-                    <circle cx="10" cy="10" r="5" fill="#a6e59f" />
-                  </svg>
-                  Suspended
-                </p>
+      <div className={style.Card}>
+        <div className={style.Row}>
+          <div className={style.Column}>
+            <img
+              src={profilerBrowser}
+              className={style.Screenshot}
+              alt="logo"
+            />
+          </div>
+          <div className={style.Column}>
+            <h2 className={style.Header}>React Concurrent Mode Profiler</h2>
+            <p>
+              Import a captured{' '}
+              <a
+                className={style.Link}
+                href="https://developers.google.com/web/tools/chrome-devtools/evaluate-performance">
+                performance profile
+              </a>{' '}
+              from Chrome Devtools. To zoom, scroll while holding down{' '}
+              <kbd>Ctrl</kbd> or <kbd>Shift</kbd>
+            </p>
+            <ul className={style.LegendKey}>
+              <li className={style.LegendItem}>
+                <svg height="20" width="20">
+                  <circle cx="10" cy="10" r="5" fill="#ff718e" />
+                </svg>
+                State Update Scheduled
+              </li>
+              <li className={style.LegendItem}>
+                <svg height="20" width="20">
+                  <circle cx="10" cy="10" r="5" fill="#9fc3f3" />
+                </svg>
+                State Update Scheduled
+              </li>
+              <li className={style.LegendItem}>
+                <svg height="20" width="20">
+                  <circle cx="10" cy="10" r="5" fill="#a6e59f" />
+                </svg>
+                Suspended
+              </li>
+            </ul>
 
-                <div className={style.buttongrp}>
-                  <label htmlFor="upload">
-                    <button
-                      className={style.ImportButton}
-                      onClick={() => upload.current && upload.current.click()}>
-                      Import
-                    </button>
-                    <input
-                      type="file"
-                      ref={upload}
-                      className={style.inputbtn}
-                      onChange={handleProfilerInput}
-                      accept="application/json"
-                    />
-                  </label>
-                  <a href="https://github.com/MLH-Fellowship/scheduling-profiler-prototype">
-                    <button className={style.ViewSourceButton}>
-                      <span>Source </span>
-                    </button>
-                  </a>
-                </div>
-              </div>
+            <div className={style.Buttons}>
+              <label htmlFor="upload">
+                <button
+                  className={style.ImportButton}
+                  onClick={() => upload.current && upload.current.click()}>
+                  Import
+                </button>
+                <input
+                  type="file"
+                  ref={upload}
+                  className={style.ImportButtonFile}
+                  onChange={handleProfilerInput}
+                  accept="application/json"
+                />
+              </label>
+              <a href="https://github.com/facebook/react/tree/master/packages/react-devtools-scheduling-profiler">
+                <button className={style.ViewSourceButton}>
+                  <span>Source </span>
+                </button>
+              </a>
             </div>
           </div>
         </div>
