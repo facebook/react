2823bfbe6ccae2ebe24ecbfe2ba039bf92f0b98b
chinonso johnson <johnsonchinonso19@gmail.com>
2020-08-27 12:06:03 +0100

    --no-edit

--- packages/shared/ConsolePatchingDev.js
+++ packages/shared/ConsolePatchingDev.js
@@ -12,7 +12,7 @@
 // lazily which won't cover if the log function was extracted eagerly.
 // We could also eagerly patch the method.
 
-import {getEnableDoubleLogging} from '../react-devtools-shared/src/utils'
+import {getEnableDoubleLogging} from '../react-devtools-shared/src/utils';
 
 let disabledDepth = 0;
 let prevLog;
@@ -28,36 +28,32 @@ disabledLog.__reactDisabledLog = true;
 
 export function disableLogs(): void {
   if (__DEV__) {
-    console.log(getEnableDoubleLogging())
-    if(getEnableDoubleLogging()) {
-      if (disabledDepth === 0) {
-        /* eslint-disable react-internal/no-production-logging */
-        prevLog = console.log;
-        prevInfo = console.info;
-        prevWarn = console.warn;
-        prevError = console.error;
-        prevGroup = console.group;
-        prevGroupCollapsed = console.groupCollapsed;
-        prevGroupEnd = console.groupEnd;
-        // https://github.com/facebook/react/issues/19099
-        const props = {
-          configurable: true,
-          enumerable: true,
-          value: disabledLog,
-          writable: true,
-        };
-        // $FlowFixMe Flow thinks console is immutable.
-        Object.defineProperties(console, {
-          info: props,
-          log: props,
-          warn: props,
-          error: props,
-          group: props,
-          groupCollapsed: props,
-          groupEnd: props,
-        });
-    }
-
+    if (disabledDepth === 0) {
+      /* eslint-disable react-internal/no-production-logging */
+      prevLog = console.log;
+      prevInfo = console.info;
+      prevWarn = console.warn;
+      prevError = console.error;
+      prevGroup = console.group;
+      prevGroupCollapsed = console.groupCollapsed;
+      prevGroupEnd = console.groupEnd;
+      // https://github.com/facebook/react/issues/19099
+      const props = {
+        configurable: true,
+        enumerable: true,
+        value: disabledLog,
+        writable: true,
+      };
+      // $FlowFixMe Flow thinks console is immutable.
+      Object.defineProperties(console, {
+        info: props,
+        log: props,
+        warn: props,
+        error: props,
+        group: props,
+        groupCollapsed: props,
+        groupEnd: props,
+      });
       /* eslint-enable react-internal/no-production-logging */
     }
     disabledDepth++;
@@ -65,9 +61,7 @@ export function disableLogs(): void {
 }
 
 export function reenableLogs(): void {
-
   if (__DEV__) {
-
     disabledDepth--;
     if (disabledDepth === 0) {
       /* eslint-disable react-internal/no-production-logging */
