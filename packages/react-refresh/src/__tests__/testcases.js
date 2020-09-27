775abee6153128bcbe540c8ec5fd4809290e410d
Jack Works <jackworks@protonmail.com>
2020-09-27 16:13:04 +0800

    test: add a new case

--- packages/react-refresh/src/__tests__/testcases.js
+++ packages/react-refresh/src/__tests__/testcases.js
@@ -543,6 +543,16 @@ function App() {
 `,
 ]);
 
+addTestCase(
+  'should recognize reset comment',
+  `
+// @refresh reset
+function App() {useState(0)}
+// Should not be reset?
+function Not() {useState(0)}
+`,
+);
+
 /**
  *
  * @param {string} name Test case name
