d548a66547b6ec29e2aef3d8426d788a8c966e40
Brian Vaughn <bvaughn@fb.com>
2020-09-30 15:57:06 -0400

    Add TODO

--- packages/shared/forks/ReactFeatureFlags.www.js
+++ packages/shared/forks/ReactFeatureFlags.www.js
@@ -37,6 +37,7 @@ export const {
 export const enableProfilerTimer = __PROFILE__;
 
 // Temporarily disable commit hooks flag to verify it does not cause a regression.
+// TODO (brian) Re-enable this flag if performance is mostly neutral.
 export const enableProfilerCommitHooks = false;
 
 // Logs additional User Timing API marks for use with an experimental profiling tool.
