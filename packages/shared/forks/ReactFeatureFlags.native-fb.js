32e5c1b957593a126a16844e45c0deb926bb9af1
Brian Vaughn <bvaughn@fb.com>
2020-10-18 09:20:16 -0400

    useRef: Warn about reading or writing mutable values during render
    Reading or writing a ref value during render is only safe if you are implementing the lazy initialization pattern.
    
    Other types of reading are unsafe as the ref is a mutable source.
    
    Other types of writing are unsafe as they are effectively side effects.

--- packages/shared/forks/ReactFeatureFlags.native-fb.js
+++ packages/shared/forks/ReactFeatureFlags.native-fb.js
@@ -51,6 +51,7 @@ export const decoupleUpdatePriorityFromScheduler = false;
 export const enableDiscreteEventFlushingChange = false;
 
 export const enableDoubleInvokingEffects = false;
+export const enableUseRefMutationWarning = false;
 
 // Flow magic to verify the exports of this file match the original version.
 // eslint-disable-next-line no-unused-vars
--- packages/shared/forks/ReactFeatureFlags.testing.www.js
+++ packages/shared/forks/ReactFeatureFlags.testing.www.js
@@ -50,6 +50,7 @@ export const decoupleUpdatePriorityFromScheduler = false;
 export const enableDiscreteEventFlushingChange = true;
 
 export const enableDoubleInvokingEffects = false;
+export const enableUseRefMutationWarning = false;
 
 // Flow magic to verify the exports of this file match the original version.
 // eslint-disable-next-line no-unused-vars
