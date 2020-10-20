58a89a6f279da8b8e9f583b64eb53e1af3805e2e
Rick Hanlon <rickhanlonii@gmail.com>
2020-10-20 14:39:39 -0400

    Move SchedulerNoDom check to npm/index.js

--- packages/scheduler/index.js
+++ packages/scheduler/index.js
@@ -7,37 +7,4 @@
 
 'use strict';
 
-const isBrowserEnvironment =
-  typeof window !== 'undefined' && typeof MessageChannel === 'function';
-
-// Select the correct scheduler for this environment.
-// TODO: this should be handled upstream by importing the correct scheduler.
-let Scheduler;
-if (isBrowserEnvironment) {
-  Scheduler = require('./src/forks/SchedulerDOM');
-} else {
-  Scheduler = require('./src/forks/SchedulerNoDOM');
-}
-
-export const unstable_ImmediatePriority = Scheduler.unstable_ImmediatePriority;
-export const unstable_UserBlockingPriority =
-  Scheduler.unstable_UserBlockingPriority;
-export const unstable_NormalPriority = Scheduler.unstable_NormalPriority;
-export const unstable_IdlePriority = Scheduler.unstable_IdlePriority;
-export const unstable_LowPriority = Scheduler.unstable_LowPriority;
-export const unstable_runWithPriority = Scheduler.unstable_runWithPriority;
-export const unstable_next = Scheduler.unstable_next;
-export const unstable_scheduleCallback = Scheduler.unstable_scheduleCallback;
-export const unstable_cancelCallback = Scheduler.unstable_cancelCallback;
-export const unstable_wrapCallback = Scheduler.unstable_wrapCallback;
-export const unstable_getCurrentPriorityLevel =
-  Scheduler.unstable_getCurrentPriorityLevel;
-export const unstable_shouldYield = Scheduler.unstable_shouldYield;
-export const unstable_requestPaint = Scheduler.unstable_requestPaint;
-export const unstable_continueExecution = Scheduler.unstable_continueExecution;
-export const unstable_pauseExecution = Scheduler.unstable_pauseExecution;
-export const unstable_getFirstCallbackNode =
-  Scheduler.unstable_getFirstCallbackNode;
-export const unstable_now = Scheduler.unstable_now;
-export const unstable_forceFrameRate = Scheduler.unstable_forceFrameRate;
-export const unstable_Profiling = Scheduler.unstable_Profiling;
+export * from './src/forks/SchedulerDOM';
--- packages/scheduler/npm/index.js
+++ packages/scheduler/npm/index.js
@@ -1,7 +1,15 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'production') {
+const isBrowserEnvironment =
+  typeof window !== 'undefined' && typeof MessageChannel === 'function';
+
+// Select the correct scheduler for this environment.
+if (process.env.NODE_ENV === 'production' && isBrowserEnvironment) {
   module.exports = require('./cjs/scheduler.production.min.js');
-} else {
+} else if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./cjs/scheduler_no_dom.production.min.js');
+} else if (isBrowserEnvironment) {
   module.exports = require('./cjs/scheduler.development.js');
+} else {
+  module.exports = require('./cjs/scheduler_no_dom.development.js');
 }
