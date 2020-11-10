a581a3b90fe112e8a5e752ba10edba0b42f302ea
Alphabet Codes <40604284+alphabet-codes@users.noreply.github.com>
2020-11-10 17:36:58 +0100

    Update packages/react-refresh/src/ReactFreshRuntime.js
    Fixed warning message spacing.
    
    Co-authored-by: Brian Vaughn <brian.david.vaughn@gmail.com>

--- packages/react-refresh/src/ReactFreshRuntime.js
+++ packages/react-refresh/src/ReactFreshRuntime.js
@@ -467,8 +467,8 @@ export function injectIntoGlobalHook(globalObject: any): void {
     // Checks if DevTools hook is disabled
     if (hook.isDisabled) {
       console['warn'](
-        'Something has shimmed the React DevTools global hook (REACT_DEVTOOLS_GLOBAL_HOOK).' +
-          'Fast Refresh is not compatible with this shim and will be disabled.  ',
+        'Something has shimmed the React DevTools global hook (REACT_DEVTOOLS_GLOBAL_HOOK). ' +
+          'Fast Refresh is not compatible with this shim and will be disabled.',
       );
     }
     // Here, we just want to get a reference to scheduleRefresh.
