d96f5f36b900f2b2e0737401282675b746f485e2
Sebastian Markbage <sema@fb.com>
2020-12-04 20:40:05 -0500

    Use build folder in prod

--- fixtures/flight/server/handler.server.js
+++ fixtures/flight/server/handler.server.js
@@ -8,8 +8,9 @@ import * as React from 'react';
 module.exports = function(req, res) {
   // const m = require('../src/App.server.js');
   import('../src/App.server.js').then(m => {
+    const dist = process.env.NODE_ENV === 'development' ? 'dist' : 'build';
     readFile(
-      resolve(__dirname, '../dist/react-transport-manifest.json'),
+      resolve(__dirname, `../${dist}/react-transport-manifest.json`),
       'utf8',
       (err, data) => {
         if (err) {
