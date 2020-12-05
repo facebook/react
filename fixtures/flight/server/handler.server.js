0db61a08befe6406aa93568708224d1cca2aff7d
Sebastian Markbåge <sema@fb.com>
2020-12-04 21:00:57 -0500

    Fix Flight Prod Fixture (#20382)
    * Don't use async/await
    
    Babel transpilation fails for some reason in prod.
    
    * Set up production runner command
    
    Uses python because meh. Just to show it's static.
    
    * Use build folder in prod

--- fixtures/flight/server/handler.server.js
+++ fixtures/flight/server/handler.server.js
@@ -1,20 +1,27 @@
 'use strict';
 
 import {pipeToNodeWritable} from 'react-transport-dom-webpack/server';
-import {readFileSync} from 'fs';
+import {readFile} from 'fs';
 import {resolve} from 'path';
 import * as React from 'react';
 
-module.exports = async function(req, res) {
-  const m = await import('../src/App.server.js');
+module.exports = function(req, res) {
   // const m = require('../src/App.server.js');
-  const App = m.default.default || m.default;
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  const moduleMap = JSON.parse(
-    readFileSync(
-      resolve(__dirname, '../dist/react-transport-manifest.json'),
-      'utf8'
-    )
-  );
-  pipeToNodeWritable(<App />, res, moduleMap);
+  import('../src/App.server.js').then(m => {
+    const dist = process.env.NODE_ENV === 'development' ? 'dist' : 'build';
+    readFile(
+      resolve(__dirname, `../${dist}/react-transport-manifest.json`),
+      'utf8',
+      (err, data) => {
+        if (err) {
+          throw err;
+        }
+
+        const App = m.default.default || m.default;
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        const moduleMap = JSON.parse(data);
+        pipeToNodeWritable(<App />, res, moduleMap);
+      }
+    );
+  });
 };
