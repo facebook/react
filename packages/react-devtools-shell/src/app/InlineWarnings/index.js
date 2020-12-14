5b3fb893b3609953bd1bebfc401a158bd82d7abd
eps1lon <silbermann.sebastian@gmail.com>
2020-12-14 21:29:01 +0100

    Expand showcase with error and warn/error mix

--- packages/react-devtools-shell/src/app/InlineWarnings/index.js
+++ packages/react-devtools-shell/src/app/InlineWarnings/index.js
@@ -36,6 +36,76 @@ function WarnOnUnmount() {
   return null;
 }
 
+function ErrorDuringRender() {
+  console.error('This error fires during every render');
+  return null;
+}
+
+function ErrorOnMount() {
+  useEffect(() => {
+    console.error('This error fires on initial mount only');
+  }, []);
+  return null;
+}
+
+function ErrorOnUpdate() {
+  const didMountRef = useRef(false);
+  useEffect(() => {
+    if (didMountRef.current) {
+      console.error('This error fires on every update');
+    } else {
+      didMountRef.current = true;
+    }
+  });
+  return null;
+}
+
+function ErrorOnUnmount() {
+  useEffect(() => {
+    return () => {
+      console.error('This error fires on unmount');
+    };
+  }, []);
+  return null;
+}
+
+function ErrorAndWarningDuringRender() {
+  console.warn('This warning fires during every render');
+  console.error('This error fires during every render');
+  return null;
+}
+
+function ErrorAndWarningOnMount() {
+  useEffect(() => {
+    console.warn('This warning fires on initial mount only');
+    console.error('This error fires on initial mount only');
+  }, []);
+  return null;
+}
+
+function ErrorAndWarningOnUpdate() {
+  const didMountRef = useRef(false);
+  useEffect(() => {
+    if (didMountRef.current) {
+      console.warn('This warning fires on every update');
+      console.error('This error fires on every update');
+    } else {
+      didMountRef.current = true;
+    }
+  });
+  return null;
+}
+
+function ErrorAndWarningOnUnmount() {
+  useEffect(() => {
+    return () => {
+      console.warn('This warning fires on unmount');
+      console.error('This error fires on unmount');
+    };
+  }, []);
+  return null;
+}
+
 export default function ElementTypes() {
   const [count, setCount] = useState(0);
   const handleClick = () => setCount(count + 1);
@@ -47,6 +117,14 @@ export default function ElementTypes() {
       <WarnOnMount />
       <WarnOnUpdate />
       {count === 0 ? <WarnOnUnmount /> : null}
+      <ErrorDuringRender />
+      <ErrorOnMount />
+      <ErrorOnUpdate />
+      {count === 0 ? <ErrorOnUnmount /> : null}
+      <ErrorAndWarningDuringRender />
+      <ErrorAndWarningOnMount />
+      <ErrorAndWarningOnUpdate />
+      {count === 0 ? <ErrorAndWarningOnUnmount /> : null}
     </Fragment>
   );
 }
