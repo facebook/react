4e7e36cbfdaac82cebdc59db601ff906483559b7
Andrew Clark <git@andrewclark.io>
2021-01-11 19:26:57 -0600

    Migrate build tests to combined workflow

--- scripts/jest/config.build-devtools.js
+++ scripts/jest/config.build-devtools.js
@@ -4,6 +4,9 @@ const {readdirSync, statSync} = require('fs');
 const {join} = require('path');
 const baseConfig = require('./config.base');
 
+const NODE_MODULES_DIR =
+  process.env.RELEASE_CHANNEL === 'stable' ? 'oss-stable' : 'oss-experimental';
+
 // Find all folders in packages/* with package.json
 const packagesRoot = join(__dirname, '..', '..', 'packages');
 const packages = readdirSync(packagesRoot).filter(dir => {
@@ -32,11 +35,13 @@ moduleNameMapper['react-devtools-feature-flags'] =
 // Map packages to bundles
 packages.forEach(name => {
   // Root entry point
-  moduleNameMapper[`^${name}$`] = `<rootDir>/build/node_modules/${name}`;
+  moduleNameMapper[
+    `^${name}$`
+  ] = `<rootDir>/build2/${NODE_MODULES_DIR}/${name}`;
   // Named entry points
   moduleNameMapper[
     `^${name}\/([^\/]+)$`
-  ] = `<rootDir>/build/node_modules/${name}/$1`;
+  ] = `<rootDir>/build2/${NODE_MODULES_DIR}/${name}/$1`;
 });
 
 // Allow tests to import shared code (e.g. feature flags, getStackByFiberInDevAndProd)
@@ -50,7 +55,7 @@ module.exports = Object.assign({}, baseConfig, {
   // Don't run bundle tests on -test.internal.* files
   testPathIgnorePatterns: ['/node_modules/', '-test.internal.js$'],
   // Exclude the build output from transforms
-  transformIgnorePatterns: ['/node_modules/', '<rootDir>/build/'],
+  transformIgnorePatterns: ['/node_modules/', '<rootDir>/build2/'],
   testRegex: 'packages/react-devtools-shared/src/__tests__/[^]+.test.js$',
   snapshotSerializers: [
     require.resolve(
