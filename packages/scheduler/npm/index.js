e46d711d526d9588b2207d5e6fbe36177cba0552
amensum <amensum@ya.ru>
2021-02-11 00:50:07 +0300

    This reverts commit 10e587c5

--- packages/scheduler/npm/index.js
+++ packages/scheduler/npm/index.js
@@ -1,24 +1,9 @@
 'use strict';
 
-// Duplicated from 'react/packages/shared/ExecutionEnvironment.js'
-var canUseDom = !!(
-  typeof window !== 'undefined' &&
-  typeof window.document !== 'undefined' &&
-  typeof window.document.createElement !== 'undefined'
-);
-
-// Node environment has cycled global reference
-var isCycledGlobal = !!(
-  typeof global !== 'undefined' &&
-  global.global === global
-);
-
-if (canUseDom && !isCycledGlobal) {
-  if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./cjs/scheduler.production.min.js');
-  } else {
-    module.exports = require('./cjs/scheduler.development.js');
-  }
-} else {
+if (typeof window === 'undefined' || typeof MessageChannel !== 'function') {
   module.exports = require('./unstable_no_dom');
+} else if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./cjs/scheduler.production.min.js');
+} else {
+  module.exports = require('./cjs/scheduler.development.js');
 }
