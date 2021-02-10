b06a1cccdfe6343e62e756123b8f01bb2395aae0
amensum <amensum@ya.ru>
2021-02-10 23:23:14 +0300

    This reverts commit 40c38f91

--- packages/scheduler/npm/index.js
+++ packages/scheduler/npm/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
-if (typeof process !== 'undefined') {
+if (typeof window === 'undefined' || typeof MessageChannel !== 'function') {
   module.exports = require('./unstable_no_dom');
+} else if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./cjs/scheduler.production.min.js');
 } else {
-  if (__DEV__) {
-    module.exports = require('./cjs/scheduler.development.js');
-  } else {
-    module.exports = require('./cjs/scheduler.production.min.js');
-  }
+  module.exports = require('./cjs/scheduler.development.js');
 }
