6cdc35972d59d08451a529f1ee29787851f0e583
neroneroffy <haitaocapslock@outlook.com>
2021-02-11 12:26:26 +0800

    fix comments of markUpdateLaneFromFiberToRoot (#20546)

--- packages/react-reconciler/src/ReactFiberWorkLoop.new.js
+++ packages/react-reconciler/src/ReactFiberWorkLoop.new.js
@@ -645,7 +645,7 @@ function markUpdateLaneFromFiberToRoot(
       warnAboutUpdateOnNotYetMountedFiberInDEV(sourceFiber);
     }
   }
-  // Walk the parent path to the root and update the child expiration time.
+  // Walk the parent path to the root and update the child lanes.
   let node = sourceFiber;
   let parent = sourceFiber.return;
   while (parent !== null) {
