114ab52953bea3d78785e25300e70cce56f307e9
Andrew Clark <git@andrewclark.io>
2021-02-10 18:25:33 -0600

    Make remaining empty lanes Transition lanes (#20793)
    Doesn't really have any effect now because we batch everything together
    anyway but will allow for more parallelism once we support that.

--- packages/react-reconciler/src/ReactFiberLane.new.js
+++ packages/react-reconciler/src/ReactFiberLane.new.js
@@ -87,38 +87,43 @@ export const SyncBatchedLane: Lane = /*                 */ 0b0000000000000000000
 export const InputDiscreteHydrationLane: Lane = /*      */ 0b0000000000000000000000000000100;
 const InputDiscreteLane: Lanes = /*                     */ 0b0000000000000000000000000001000;
 
-const InputContinuousHydrationLane: Lane = /*           */ 0b0000000000000000000000000100000;
-const InputContinuousLane: Lanes = /*                   */ 0b0000000000000000000000001000000;
+const InputContinuousHydrationLane: Lane = /*           */ 0b0000000000000000000000000010000;
+const InputContinuousLane: Lanes = /*                   */ 0b0000000000000000000000000100000;
 
-export const DefaultHydrationLane: Lane = /*            */ 0b0000000000000000000000100000000;
-export const DefaultLane: Lanes = /*                    */ 0b0000000000000000000001000000000;
+export const DefaultHydrationLane: Lane = /*            */ 0b0000000000000000000000001000000;
+export const DefaultLane: Lanes = /*                    */ 0b0000000000000000000000010000000;
 
-const TransitionHydrationLane: Lane = /*                */ 0b0000000000000000001000000000000;
-const TransitionLanes: Lanes = /*                       */ 0b0000000001111111110000000000000;
-const TransitionLane1: Lane = /*                        */ 0b0000000000000000010000000000000;
-const TransitionLane2: Lane = /*                        */ 0b0000000000000000100000000000000;
-const TransitionLane3: Lane = /*                        */ 0b0000000000000001000000000000000;
-const TransitionLane4: Lane = /*                        */ 0b0000000000000010000000000000000;
-const TransitionLane5: Lane = /*                        */ 0b0000000000000100000000000000000;
-const TransitionLane6: Lane = /*                        */ 0b0000000000001000000000000000000;
-const TransitionLane7: Lane = /*                        */ 0b0000000000010000000000000000000;
-const TransitionLane8: Lane = /*                        */ 0b0000000000100000000000000000000;
-const TransitionLane9: Lane = /*                        */ 0b0000000001000000000000000000000;
+const TransitionLanes: Lanes = /*                       */ 0b0000000011111111111111100000000;
+const TransitionHydrationLane: Lane = /*                */ 0b0000000000000000000000100000000;
+const TransitionLane1: Lane = /*                        */ 0b0000000000000000000001000000000;
+const TransitionLane2: Lane = /*                        */ 0b0000000000000000000010000000000;
+const TransitionLane3: Lane = /*                        */ 0b0000000000000000000100000000000;
+const TransitionLane4: Lane = /*                        */ 0b0000000000000000001000000000000;
+const TransitionLane5: Lane = /*                        */ 0b0000000000000000010000000000000;
+const TransitionLane6: Lane = /*                        */ 0b0000000000000000100000000000000;
+const TransitionLane7: Lane = /*                        */ 0b0000000000000001000000000000000;
+const TransitionLane8: Lane = /*                        */ 0b0000000000000010000000000000000;
+const TransitionLane9: Lane = /*                        */ 0b0000000000000100000000000000000;
+const TransitionLane10: Lane = /*                       */ 0b0000000000001000000000000000000;
+const TransitionLane11: Lane = /*                       */ 0b0000000000010000000000000000000;
+const TransitionLane12: Lane = /*                       */ 0b0000000000100000000000000000000;
+const TransitionLane13: Lane = /*                       */ 0b0000000001000000000000000000000;
+const TransitionLane14: Lane = /*                       */ 0b0000000010000000000000000000000;
 
-const RetryLanes: Lanes = /*                            */ 0b0000011110000000000000000000000;
-const RetryLane1: Lane = /*                             */ 0b0000000010000000000000000000000;
-const RetryLane2: Lane = /*                             */ 0b0000000100000000000000000000000;
-const RetryLane3: Lane = /*                             */ 0b0000001000000000000000000000000;
-const RetryLane4: Lane = /*                             */ 0b0000010000000000000000000000000;
+const RetryLanes: Lanes = /*                            */ 0b0000111100000000000000000000000;
+const RetryLane1: Lane = /*                             */ 0b0000000100000000000000000000000;
+const RetryLane2: Lane = /*                             */ 0b0000001000000000000000000000000;
+const RetryLane3: Lane = /*                             */ 0b0000010000000000000000000000000;
+const RetryLane4: Lane = /*                             */ 0b0000100000000000000000000000000;
 
 export const SomeRetryLane: Lane = RetryLane1;
 
-export const SelectiveHydrationLane: Lane = /*          */ 0b0000100000000000000000000000000;
+export const SelectiveHydrationLane: Lane = /*          */ 0b0001000000000000000000000000000;
 
-const NonIdleLanes = /*                                 */ 0b0000111111111111111111111111111;
+const NonIdleLanes = /*                                 */ 0b0001111111111111111111111111111;
 
-export const IdleHydrationLane: Lane = /*               */ 0b0001000000000000000000000000000;
-const IdleLane: Lanes = /*                              */ 0b0010000000000000000000000000000;
+export const IdleHydrationLane: Lane = /*               */ 0b0010000000000000000000000000000;
+const IdleLane: Lanes = /*                              */ 0b0100000000000000000000000000000;
 
 export const OffscreenLane: Lane = /*                   */ 0b1000000000000000000000000000000;
 
@@ -179,6 +184,11 @@ function getHighestPriorityLanes(lanes: Lanes | Lane): Lanes {
     case TransitionLane7:
     case TransitionLane8:
     case TransitionLane9:
+    case TransitionLane10:
+    case TransitionLane11:
+    case TransitionLane12:
+    case TransitionLane13:
+    case TransitionLane14:
       return_highestLanePriority = TransitionPriority;
       return lanes & TransitionLanes;
     case RetryLane1:
