0c6db5f6d97da62925c83a03041ead145c91ab12
amensum <amensum@ya.ru>
2021-02-12 03:44:09 +0300

    This reverts commit c1394eac

--- packages/scheduler/npm/index.js
+++ packages/scheduler/npm/index.js
@@ -1,22 +1,9 @@
 'use strict';
 
-// Duplicated from 'react/packages/shared/ExecutionEnvironment.js'
-var canUseDom = !!(
-  typeof window !== 'undefined' &&
-  typeof window.document !== 'undefined' &&
-  typeof window.document.createElement !== 'undefined'
-);
-
-// Checks if this is global space
-// eslint-disable-next-line no-undef
-var isThisGlobal = !!(typeof globalThis !== 'undefined' && globalThis === this);
-
-if (canUseDom && isThisGlobal) {
-  if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./cjs/scheduler.production.min.js');
-  } else {
-    module.exports = require('./cjs/scheduler.development.js');
-  }
-} else {
+if (typeof window === 'undefined' || typeof MessageChannel !== 'function') {
   module.exports = require('./unstable_no_dom');
+} else if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./cjs/scheduler.production.min.js');
+} else {
+  module.exports = require('./cjs/scheduler.development.js');
 }
