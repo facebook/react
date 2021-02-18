f1bfb4439f1fedeafe4f58e6e85b947dbfffc5a2
Brian Vaughn <bvaughn@fb.com>
2021-02-18 10:48:40 -0500

    Disabled Scheduler sampling profiler for OSS builds

--- packages/scheduler/src/SchedulerFeatureFlags.js
+++ packages/scheduler/src/SchedulerFeatureFlags.js
@@ -8,7 +8,7 @@
 
 export const enableSchedulerDebugging = false;
 export const enableIsInputPending = false;
-export const enableProfiling = __PROFILE__;
+export const enableProfiling = false;
 
 // TODO: enable to fix https://github.com/facebook/react/issues/20756.
 export const enableSetImmediate = __VARIANT__;
