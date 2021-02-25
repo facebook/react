e3e2bc455f5e4ad7a3fa74b81d035cdb6baaca2b
Chris Dobson <chrisdobby.dev@gmail.com>
2021-02-25 12:14:44 +0000

    remove unused import

--- packages/react-devtools-shared/src/bridge.js
+++ packages/react-devtools-shared/src/bridge.js
@@ -17,7 +17,6 @@ import type {
   RendererID,
 } from 'react-devtools-shared/src/backend/types';
 import type {StyleAndLayout as StyleAndLayoutPayload} from 'react-devtools-shared/src/backend/NativeStyleEditor/types';
-import {boolean} from 'yargs';
 
 const BATCH_DURATION = 100;
 
