c0c33d3f624c0ac514e29203e120a7b1bbf02960
Dan Abramov <dan.abramov@me.com>
2021-03-01 16:50:17 +0000

    Land enableSetImmediate feature flag

--- packages/scheduler/src/SchedulerFeatureFlags.js
+++ packages/scheduler/src/SchedulerFeatureFlags.js
@@ -9,6 +9,3 @@
 export const enableSchedulerDebugging = false;
 export const enableIsInputPending = false;
 export const enableProfiling = __VARIANT__;
-
-// TODO: enable to fix https://github.com/facebook/react/issues/20756.
-export const enableSetImmediate = __VARIANT__;
