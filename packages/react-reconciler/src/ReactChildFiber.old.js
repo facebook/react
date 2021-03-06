32d61f08ae1f1b0a26a29734dca9d9ea6ab06501
taichiyi <taichiyi@163.com>
2021-03-06 14:05:17 +0800

    Optimize: reduce double counting of isObject

--- packages/react-reconciler/src/ReactChildFiber.old.js
+++ packages/react-reconciler/src/ReactChildFiber.old.js
@@ -1221,20 +1221,19 @@ function ChildReconciler(shouldTrackSideEffects) {
     // not as a fragment. Nested arrays on the other hand will be treated as
     // fragment nodes. Recursion happens at the normal flow.
 
+    // Handle object types
+    const isObject = typeof newChild === 'object' && newChild !== null;
+
     // Handle top level unkeyed fragments as if they were arrays.
     // This leads to an ambiguity between <>{[...]}</> and <>...</>.
     // We treat the ambiguous cases above the same.
     const isUnkeyedTopLevelFragment =
-      typeof newChild === 'object' &&
-      newChild !== null &&
+      isObject &&
       newChild.type === REACT_FRAGMENT_TYPE &&
       newChild.key === null;
     if (isUnkeyedTopLevelFragment) {
       newChild = newChild.props.children;
     }
-
-    // Handle object types
-    const isObject = typeof newChild === 'object' && newChild !== null;
 
     if (isObject) {
       switch (newChild.$$typeof) {
