03c8868ed4a9ea51503dfc294e36ab679f2336aa
eps1lon <silbermann.sebastian@gmail.com>
2021-03-15 10:03:02 +0100

    Make download-experimental-build runnable

--- scripts/release/theme.js
+++ scripts/release/theme.js
@@ -10,7 +10,7 @@ const colors = {
   yellow: '#ffd600',
 };
 
-const theme = chalk.constructor();
+const theme = new chalk.Instance();
 theme.package = theme.hex(colors.green);
 theme.version = theme.hex(colors.yellow);
 theme.tag = theme.hex(colors.yellow);
