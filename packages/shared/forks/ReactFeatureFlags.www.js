5dc10864c612b2cd5fd8d7938de50c4e03f6bcfd
Rick Hanlon <rickhanlonii@gmail.com>
2021-04-02 20:33:49 -0400

    Add enableSyncDefaultUpdates feature flag

--- packages/shared/forks/ReactFeatureFlags.www.js
+++ packages/shared/forks/ReactFeatureFlags.www.js
@@ -93,6 +93,9 @@ export const enableNewReconciler = __VARIANT__;
 
 export const enableRecursiveCommitTraversal = false;
 
+// WWW is on the new sync behavior by default unless overridden by roots.
+export const enableSyncDefaultUpdates = __VARIANT__;
+
 // Flow magic to verify the exports of this file match the original version.
 // eslint-disable-next-line no-unused-vars
 type Check<_X, Y: _X, X: Y = _X> = null;
