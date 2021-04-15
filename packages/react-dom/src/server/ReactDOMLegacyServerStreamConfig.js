d194f022472a7b25b1ad17c37cf0ffc38e4d2bff
Sebastian Markbage <sema@fb.com>
2021-04-14 22:01:55 -0400

    Hack to filter extra comments for testing purposes

--- packages/react-dom/src/server/ReactDOMLegacyServerStreamConfig.js
+++ packages/react-dom/src/server/ReactDOMLegacyServerStreamConfig.js
@@ -26,10 +26,21 @@ export function flushBuffered(destination: Destination) {}
 
 export function beginWriting(destination: Destination) {}
 
+let prevWasCommentSegmenter = false;
 export function writeChunk(
   destination: Destination,
   chunk: Chunk | PrecomputedChunk,
 ): boolean {
+  if (prevWasCommentSegmenter) {
+    prevWasCommentSegmenter = false;
+    if (chunk[0] !== '<') {
+      destination.push('<!-- -->');
+    }
+  }
+  if (chunk === '<!-- -->') {
+    prevWasCommentSegmenter = true;
+    return true;
+  }
   return destination.push(chunk);
 }
 
