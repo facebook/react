3c427ad6be54dad69f5172c0ada38c35be6d3894
Rick Hanlon <rickhanlonii@gmail.com>
2021-04-19 21:44:26 -0400

    Turn off enableSyncDefaultUpdates in test renderer

--- packages/shared/forks/ReactFeatureFlags.test-renderer.www.js
+++ packages/shared/forks/ReactFeatureFlags.test-renderer.www.js
@@ -59,7 +59,7 @@ export const enableUseRefAccessWarning = false;
 export const enableRecursiveCommitTraversal = false;
 export const disableSchedulerTimeoutInWorkLoop = false;
 export const enableLazyContextPropagation = false;
-export const enableSyncDefaultUpdates = true;
+export const enableSyncDefaultUpdates = false;
 
 // Flow magic to verify the exports of this file match the original version.
 // eslint-disable-next-line no-unused-vars
