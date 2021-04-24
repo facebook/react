48740429b4a74e984193e4e2d364d461e4fdc3be
Andrew Clark <git@andrewclark.io>
2021-04-24 18:32:48 -0500

    Expiration: Do nothing except disable time slicing (#21345)
    We have a feature called "expiration" whose purpose is to prevent
    a concurrent update from being starved by higher priority events.
    If a lane is CPU-bound for too long, we finish the rest of the work
    synchronously without allowing further interruptions.
    
    In the current implementation, we do this in sort of a roundabout way:
    once a lane is determined to have expired, we entangle it with SyncLane
    and switch to the synchronous work loop.
    
    There are a few flaws with the approach. One is that SyncLane has a
    particular semantic meaning besides its non-yieldiness. For example,
    `flushSync` will force remaining Sync work to finish; currently, that
    also includes expired work, which isn't an intended behavior, but rather
    an artifact of the implementation.
    
    An event worse example is that passive effects triggered by a Sync
    update are flushed synchronously, before paint, so that its result
    is guaranteed to be observed by the next discrete event. But expired
    work has no such requirement: we're flushing expired effects before
    paint unnecessarily.
    
    Aside from the behaviorial implications, the current implementation has
    proven to be fragile: more than once, we've accidentally regressed
    performance due to a subtle change in how expiration is handled.
    
    This PR aims to radically simplify how we model starvation protection by
    scaling back the implementation as much as possible. In this new model,
    if a lane is expired, we disable time slicing. That's it. We don't
    entangle it with SyncLane. The only thing we do is skip the call to
    `shouldYield` in between each time slice. This is identical to how we
    model synchronous-by-default updates in React 18.

--- packages/react-reconciler/src/__tests__/ReactSchedulerIntegration-test.js
+++ packages/react-reconciler/src/__tests__/ReactSchedulerIntegration-test.js
@@ -300,10 +300,9 @@ describe(
           ReactNoop.render(<App />);
         });
 
-        ReactNoop.flushSync();
-
         // Because the render expired, React should finish the tree without
         // consulting `shouldYield` again
+        Scheduler.unstable_flushNumberOfYields(1);
         expect(Scheduler).toHaveYielded(['B', 'C']);
       });
     });
