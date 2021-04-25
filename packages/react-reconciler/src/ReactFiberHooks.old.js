ff956204a89dadd8ee6ad398402e343c6f32e684
Hugh Winkler <hughw@hughw.net>
2021-04-25 10:34:55 -0500

    Enhance hooks error message
    Several commenters on issue https://github.com/facebook/react/issues/13991 encountered the
    "Hooks can only be called inside the body of a function component" message because they
    invoked a function component without JSX decoration (that is, without calling
    React.createElement). That pattern may be a common source of error because it works
    as the developer intended until at some later data they add, for example, a call to
    `useState`.
    
    I had been successfully using a function component within another component, invoking it as:
    
      const Outer = () => Inner({a: 1})
    
    Later, I added a call to `useState` within `Inner()`, and encountered the hooks error
    message. To avoid the problem I changed it to
    
      const Outer = () => <Inner a="1" />
    
    (https://github.com/facebook/react/issues/13991#issuecomment-826332942)

--- packages/react-reconciler/src/ReactFiberHooks.old.js
+++ packages/react-reconciler/src/ReactFiberHooks.old.js
@@ -297,7 +297,8 @@ function throwInvalidHookError() {
       ' one of the following reasons:\n' +
       '1. You might have mismatching versions of React and the renderer (such as React DOM)\n' +
       '2. You might be breaking the Rules of Hooks\n' +
-      '3. You might have more than one copy of React in the same app\n' +
+      '3. You might have more than one copy of React in the same app\n' + 
+          '4. You might be invoking the function directly rather than using React.createElement\n' + +
       'See https://reactjs.org/link/invalid-hook-call for tips about how to debug and fix this problem.',
   );
 }
