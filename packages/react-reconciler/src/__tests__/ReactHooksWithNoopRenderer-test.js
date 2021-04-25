0eb1c966086462dfba33faa98888c85c83c58095
Hugh Winkler <hughw@hughw.net>
2021-04-25 11:45:50 -0500

    Run prettier-all

--- packages/react-reconciler/src/__tests__/ReactHooksWithNoopRenderer-test.js
+++ packages/react-reconciler/src/__tests__/ReactHooksWithNoopRenderer-test.js
@@ -211,8 +211,8 @@ describe('ReactHooksWithNoopRenderer', () => {
         ' one of the following reasons:\n' +
         '1. You might have mismatching versions of React and the renderer (such as React DOM)\n' +
         '2. You might be breaking the Rules of Hooks\n' +
-        '3. You might have more than one copy of React in the same app\n' + 
-          '4. You might be invoking the function directly rather than using React.createElement\n' +
+        '3. You might have more than one copy of React in the same app\n' +
+        '4. You might be invoking the function directly rather than using React.createElement\n' +
         'See https://reactjs.org/link/invalid-hook-call for tips about how to debug and fix this problem.',
     );
 
@@ -242,8 +242,8 @@ describe('ReactHooksWithNoopRenderer', () => {
             'for one of the following reasons:\n' +
             '1. You might have mismatching versions of React and the renderer (such as React DOM)\n' +
             '2. You might be breaking the Rules of Hooks\n' +
-            '3. You might have more than one copy of React in the same app\n' + 
-          '4. You might be invoking the function directly rather than using React.createElement\n' +
+            '3. You might have more than one copy of React in the same app\n' +
+            '4. You might be invoking the function directly rather than using React.createElement\n' +
             'See https://reactjs.org/link/invalid-hook-call for tips about how to debug and fix this problem.',
         ),
       ).toErrorDev(
@@ -274,8 +274,8 @@ describe('ReactHooksWithNoopRenderer', () => {
         ' one of the following reasons:\n' +
         '1. You might have mismatching versions of React and the renderer (such as React DOM)\n' +
         '2. You might be breaking the Rules of Hooks\n' +
-        '3. You might have more than one copy of React in the same app\n' + 
-          '4. You might be invoking the function directly rather than using React.createElement\n' +
+        '3. You might have more than one copy of React in the same app\n' +
+        '4. You might be invoking the function directly rather than using React.createElement\n' +
         'See https://reactjs.org/link/invalid-hook-call for tips about how to debug and fix this problem.',
       {withoutStack: true},
     );
