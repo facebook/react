ab302c8626329b890cb2b879372c614fcc56d303
Brian Vaughn <bvaughn@fb.com>
2021-05-04 10:38:51 -0400

    DevTools Store emits errors before throwing
    The Store should never throw an Error without also emitting an event. Otherwise Store errors will be invisible to users, but the downstream errors they cause will be reported as bugs. (For example, github.com/facebook/react/issues/21402)
    
    Emitting an error event allows the ErrorBoundary to show the original error.
    
    Throwing is still valuable for local development and for unit testing the Store itself.

--- packages/react-devtools-shared/src/devtools/views/Profiler/Profiler.js
+++ packages/react-devtools-shared/src/devtools/views/Profiler/Profiler.js
@@ -26,7 +26,6 @@ import SettingsModal from 'react-devtools-shared/src/devtools/views/Settings/Set
 import SettingsModalContextToggle from 'react-devtools-shared/src/devtools/views/Settings/SettingsModalContextToggle';
 import {SettingsModalContextController} from 'react-devtools-shared/src/devtools/views/Settings/SettingsModalContext';
 import portaledContent from '../portaledContent';
-import Store from '../../store';
 
 import styles from './Profiler.css';
 
@@ -188,10 +187,4 @@ const RecordingInProgress = () => (
   </div>
 );
 
-function onErrorRetry(store: Store) {
-  // If an error happened in the Profiler,
-  // we should clear data on retry (or it will just happen again).
-  store.profilerStore.profilingData = null;
-}
-
-export default portaledContent(Profiler, onErrorRetry);
+export default portaledContent(Profiler);
