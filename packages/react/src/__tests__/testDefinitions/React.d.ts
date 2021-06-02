a8a4742f1c54493df00da648a3f9d26e3db9c8b5
Sebastian Markbåge <sema@fb.com>
2021-06-02 14:36:59 -0400

    Convert ES6/TypeScript/CoffeeScript Tests to createRoot + act (#21598)
    * Convert ES6/TypeScript CoffeeScript Tests to createRoot + act
    
    * Change expectation for WWW+VARIANT because the deferRenderPhaseUpdateToNextBatch flag breaks this behavior

--- packages/react/src/__tests__/testDefinitions/React.d.ts
+++ packages/react/src/__tests__/testDefinitions/React.d.ts
@@ -12,6 +12,8 @@
  * just helpers for the unit test.
  */
 
+declare let global: any;
+
 declare module 'react' {
   export class Component {
     props: any;
@@ -24,4 +26,5 @@ declare module 'react' {
   }
   export let PropTypes : any;
   export function createElement(tag : any, props ?: any, ...children : any[]) : any
+  export function createRef(): any;
 }
