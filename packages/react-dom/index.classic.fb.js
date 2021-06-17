568dc3532e25b30eee5072de08503b1bbc4f065d
Sebastian Markbåge <sema@fb.com>
2021-06-17 14:29:30 -0400

    Remove unstable_createRoot from internal builds (#21698)
    These callsites were already removed as far as I can tell.

--- packages/react-dom/index.classic.fb.js
+++ packages/react-dom/index.classic.fb.js
@@ -22,7 +22,6 @@ export {
   __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
   createPortal,
   createRoot,
-  createRoot as unstable_createRoot, // TODO Remove once callsites use createRoot
   hydrateRoot,
   findDOMNode,
   flushSync,
--- packages/react-dom/index.js
+++ packages/react-dom/index.js
@@ -13,7 +13,6 @@ export {
   __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
   createPortal,
   createRoot,
-  createRoot as unstable_createRoot,
   hydrateRoot,
   findDOMNode,
   flushSync,
