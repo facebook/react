dd9ecb9ede5fb2b0a6f91ff1b4e2dbd0d6fba7cc
Andrew Clark <git@andrewclark.io>
2021-06-22 14:54:10 -0400

    Remove unused internal fields
    These were used by the old act implementation. No longer needed.

--- packages/react/src/ReactSharedInternals.js
+++ packages/react/src/ReactSharedInternals.js
@@ -11,13 +11,11 @@ import ReactCurrentBatchConfig from './ReactCurrentBatchConfig';
 import ReactCurrentActQueue from './ReactCurrentActQueue';
 import ReactCurrentOwner from './ReactCurrentOwner';
 import ReactDebugCurrentFrame from './ReactDebugCurrentFrame';
-import IsSomeRendererActing from './IsSomeRendererActing';
 
 const ReactSharedInternals = {
   ReactCurrentDispatcher,
   ReactCurrentBatchConfig,
   ReactCurrentOwner,
-  IsSomeRendererActing,
   // Used by renderers to avoid bundling object-assign twice in UMD bundles:
   assign,
 };
