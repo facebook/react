1a63add87767314d1065e5d440717310f6311480
Andrew Clark <git@andrewclark.io>
2021-07-10 13:31:20 -0400

    Failing test: Legacy mode sync passive effects
    In concurrent roots, if a render is synchronous, we flush its passive
    effects synchronously. In legacy roots, we don't do this because all
    updates are synchronous — so we need to flush at the beginning of the
    next event. This is how `discreteUpdates` worked.

--- packages/react-reconciler/src/__tests__/ReactFlushSync-test.js
+++ packages/react-reconciler/src/__tests__/ReactFlushSync-test.js
@@ -128,7 +128,7 @@ describe('ReactFlushSync', () => {
     });
   });
 
-  test('do not flush passive effects synchronously in legacy mode', async () => {
+  test('do not flush passive effects synchronously after render in legacy mode', async () => {
     function App() {
       useEffect(() => {
         Scheduler.unstable_yieldValue('Effect');
@@ -150,6 +150,40 @@ describe('ReactFlushSync', () => {
     });
     // Effect flushes after paint.
     expect(Scheduler).toHaveYielded(['Effect']);
+  });
+
+  test('flush pending passive effects before scope is called in legacy mode', async () => {
+    let currentStep = 0;
+
+    function App({step}) {
+      useEffect(() => {
+        currentStep = step;
+        Scheduler.unstable_yieldValue('Effect: ' + step);
+      }, [step]);
+      return <Text text={step} />;
+    }
+
+    const root = ReactNoop.createLegacyRoot();
+    await act(async () => {
+      ReactNoop.flushSync(() => {
+        root.render(<App step={1} />);
+      });
+      expect(Scheduler).toHaveYielded([
+        1,
+        // Because we're in legacy mode, we shouldn't have flushed the passive
+        // effects yet.
+      ]);
+      expect(root).toMatchRenderedOutput('1');
+
+      ReactNoop.flushSync(() => {
+        // This should render step 2 because the passive effect has already
+        // fired, before the scope function is called.
+        root.render(<App step={currentStep + 1} />);
+      });
+      expect(Scheduler).toHaveYielded(['Effect: 1', 2]);
+      expect(root).toMatchRenderedOutput('2');
+    });
+    expect(Scheduler).toHaveYielded(['Effect: 2']);
   });
 
   test("do not flush passive effects synchronously when they aren't the result of a sync render", async () => {
