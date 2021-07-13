f8a77830f081659aac6eca1e317e618b62ba3111
Angel Siblani <84080393+angelseblani@users.noreply.github.com>
2021-07-14 01:45:40 +0300

    npm i react
    https://npmjs.com/package/react

---
+++ Platform.js
@@ -0,0 +1,13 @@
+<script>
+  (function(w, d){
+   var id='embedly-platform', n = 'script';
+   if (!d.getElementById(id)){
+     w.embedly = w.embedly || function() {(w.embedly.q = w.embedly.q || []).push(arguments);};
+     var e = d.createElement(n); e.id = id; e.async=1;
+     e.src = ('https:' === document.location.protocol ? 'https' : 'http') + '://cdn.embedly.com/widgets/platform.js';
+     var s = d.getElementsByTagName(n)[0];
+     s.parentNode.insertBefore(e, s);
+   }
+  })(window, document);
+</script>
+<script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
