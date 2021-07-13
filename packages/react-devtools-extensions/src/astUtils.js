7a6f71cc767979509333982ad2afce9016e3018d
Brian Vaughn <bvaughn@fb.com>
2021-07-13 12:58:56 -0400

    Adjust Error stack columns numbers by 1
    To account for differences between error stacks (1-based) and ASTs (0-based). In practice this change should not make an observable difference.

--- packages/react-devtools-extensions/src/astUtils.js
+++ packages/react-devtools-extensions/src/astUtils.js
@@ -40,6 +40,17 @@ function checkNodeLocation(
     return false;
   }
 
+  // Column numbers are representated differently between tools/engines.
+  // Error.prototype.stack columns are 1-based (like most IDEs) but ASTs are 0-based.
+  //
+  // In practice this will probably never matter,
+  // because this code matches the 1-based Error stack location for the hook Identifier (e.g. useState)
+  // with the larger 0-based VariableDeclarator (e.g. [foo, setFoo] = useState())
+  // so the ranges should always overlap.
+  //
+  // For more info see https://github.com/facebook/react/pull/21833#discussion_r666831276
+  column -= 1;
+
   if (
     (line === start.line && column < start.column) ||
     (line === end.line && column > end.column)
