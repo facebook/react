6f3fcbd6fac90d97f48a4c6e270b2124a333d714
Shubham Pandey <shubham9411@gmail.com>
2021-07-30 18:26:55 +0530

    Some remaining instances of master to main (#21982)
    Co-authored-by: Shubham Pandey <shubham.pandey@mfine.co>

--- packages/react-devtools-extensions/flow-typed/npm/react-test-renderer_v16.x.x.js
+++ packages/react-devtools-extensions/flow-typed/npm/react-test-renderer_v16.x.x.js
@@ -2,7 +2,7 @@
 // flow-typed version: 9c71eca8ef/react-test-renderer_v16.x.x/flow_>=v0.47.x
 
 // Type definitions for react-test-renderer 16.x.x
-// Ported from: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/main/types/react-test-renderer
+// Ported from: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react-test-renderer
 
 'use strict';
 
