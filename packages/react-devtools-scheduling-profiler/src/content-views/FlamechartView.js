797b5a70b9fe959392c08651e06a9edc1b6c5391
Brian Vaughn <bvaughn@fb.com>
2021-08-03 09:40:45 -0400

    Added borders/bars to Flamechart view

--- packages/react-devtools-scheduling-profiler/src/content-views/FlamechartView.js
+++ packages/react-devtools-scheduling-profiler/src/content-views/FlamechartView.js
@@ -128,7 +128,7 @@ class FlamechartStackLayerView extends View {
       visibleArea,
     } = this;
 
-    context.fillStyle = COLORS.BACKGROUND;
+    context.fillStyle = COLORS.PRIORITY_BACKGROUND;
     context.fillRect(
       visibleArea.origin.x,
       visibleArea.origin.y,
@@ -173,6 +173,31 @@ class FlamechartStackLayerView extends View {
       );
 
       drawText(name, context, nodeRect, drawableRect, width);
+    }
+
+    // Render bottom border.
+    const borderFrame: Rect = {
+      origin: {
+        x: frame.origin.x,
+        y: frame.origin.y + FLAMECHART_FRAME_HEIGHT - BORDER_SIZE,
+      },
+      size: {
+        width: frame.size.width,
+        height: BORDER_SIZE,
+      },
+    };
+    if (rectIntersectsRect(borderFrame, visibleArea)) {
+      const borderDrawableRect = intersectionOfRects(
+        borderFrame,
+        visibleArea,
+      );
+      context.fillStyle = COLORS.PRIORITY_BORDER;
+      context.fillRect(
+        borderDrawableRect.origin.x,
+        borderDrawableRect.origin.y,
+        borderDrawableRect.size.width,
+        borderDrawableRect.size.height,
+      );
     }
   }
 
