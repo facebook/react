cdce54d8c4461b134cbb4a89b7ce35d288245d32
afzal <afzalsayed96@gmail.com>
2021-08-08 12:50:27 +0530

    Add failing test case

--- packages/eslint-plugin-react-hooks/__tests__/ESLintRulesOfHooks-test.js
+++ packages/eslint-plugin-react-hooks/__tests__/ESLintRulesOfHooks-test.js
@@ -629,6 +629,17 @@ const tests = {
     },
     {
       code: `
+        // Invalid because it's dangerous and might not warn otherwise.
+        // This *must* be invalid.
+
+        function List(props) {
+          return props.items.map(useState);
+        }
+      `,
+      errors: [genericError('useState')],
+    },
+    {
+      code: `
         // Currently invalid because it violates the convention and removes the "taint"
         // from a hook. We *could* make it valid to avoid some false positives but let's
         // ensure that we don't break the "renderItem" and "normalFunctionWithConditionalHook"
