9a6636244fa7633fcdf7c4c9e828575a54a7eb51
Brian Vaughn <bvaughn@fb.com>
2021-08-10 09:27:22 -0700

    Split resizable views into sub-folder with each class having its own file. I was getting confused about which class I was looking at while grepping within the file.

--- packages/react-devtools-scheduling-profiler/src/view-base/index.js
+++ packages/react-devtools-scheduling-profiler/src/view-base/index.js
@@ -9,11 +9,11 @@
 
 export * from './BackgroundColorView';
 export * from './HorizontalPanAndZoomView';
-export * from './ResizableView';
 export * from './Surface';
 export * from './VerticalScrollView';
 export * from './VerticalScrollOverflowView';
 export * from './View';
 export * from './geometry';
 export * from './layouter';
+export * from './resizable';
 export * from './useCanvasInteraction';
