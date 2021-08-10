e9b2028b3280c15138bd92b0d27ffa066de3d5ca
Sota <5866096+sota000@users.noreply.github.com>
2021-08-10 13:14:11 -0700

    Show a soft error when a text string or number is supplied as a child to non text wrappers (#21953)
    * Show soft errors when a text string or number is supplied as a child instead of throwing an error
    
    * bring __DEV__ check first so that things inside get removed in prod.
    
    * fix lint

--- packages/react-native-renderer/src/ReactFabricHostConfig.js
+++ packages/react-native-renderer/src/ReactFabricHostConfig.js
@@ -22,8 +22,6 @@ import type {
 import {mountSafeCallback_NOT_REALLY_SAFE} from './NativeMethodsMixinUtils';
 import {create, diff} from './ReactNativeAttributePayload';
 
-import invariant from 'shared/invariant';
-
 import {dispatchEvent} from './ReactFabricEventEmitter';
 
 import {
@@ -264,10 +262,11 @@ export function createTextInstance(
   hostContext: HostContext,
   internalInstanceHandle: Object,
 ): TextInstance {
-  invariant(
-    hostContext.isInAParentText,
-    'Text strings must be rendered within a <Text> component.',
-  );
+  if (__DEV__) {
+    if (!hostContext.isInAParentText) {
+      console.error('Text strings must be rendered within a <Text> component.');
+    }
+  }
 
   const tag = nextReactTag;
   nextReactTag += 2;
