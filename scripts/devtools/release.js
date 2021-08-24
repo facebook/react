41e5f77df33a7312144fa1c6ab766e727ab9ac93
Brian Vaughn <bvaughn@fb.com>
2021-08-24 10:27:04 -0700

    Added build and test and publish info to release script

--- scripts/devtools/release.js
+++ scripts/devtools/release.js
@@ -7,6 +7,8 @@ const {exec} = require('child-process-promise');
 const {readFileSync, writeFileSync} = require('fs');
 const {readJsonSync, writeJsonSync} = require('fs-extra');
 const inquirer = require('inquirer');
+const {homedir} = require('os');
+const createLogger = require('progress-estimator');
 const {join} = require('path');
 const semver = require('semver');
 
@@ -23,6 +25,12 @@ const MANIFEST_PATHS = [
   'packages/react-devtools-extensions/firefox/manifest.json',
 ];
 
+const NPM_PACKAGES = [
+  'react-devtools',
+  'react-devtools-core',
+  'react-devtools-inline',
+];
+
 const CHANGELOG_PATH = 'packages/react-devtools/CHANGELOG.md';
 
 const PULL_REQUEST_BASE_URL = 'https://github.com/facebook/react/pull/';
@@ -33,7 +41,14 @@ const ROOT_PATH = join(__dirname, '..', '..');
 
 const DRY_RUN = process.argv.includes('--dry');
 
+const logger = createLogger({
+  storagePath: join(__dirname, '.progress-estimator'),
+});
+
+// This is the primary control function for this script.
 async function main() {
+  await checkNPMPermissions();
+
   const releaseType = await getReleaseType();
 
   const path = join(ROOT_PATH, PACKAGE_PATHS[0]);
@@ -47,9 +62,13 @@ async function main() {
   updatePackageVersions(previousVersion, nextVersion);
   updateManifestVersions(previousVersion, nextVersion);
 
+  console.log('');
   console.log(
-    `\nPackages and manifests have been updated from version \x1b[1m${previousVersion}\x1b[0m to \x1b[1m${nextVersion}\x1b[0m\n`
+    `Packages and manifests have been updated from version ${chalk.bold(
+      previousVersion
+    )} to ${chalk.bold(nextVersion)}`
   );
+  console.log('');
 
   const sha = await getPreviousCommitSha();
   const commitLog = await getCommitLog(sha);
@@ -59,13 +78,178 @@ async function main() {
   await reviewChangelogPrompt();
 
   await commitPendingChanges(previousVersion, nextVersion);
+
+  const archivePath = await archiveGitRevision();
+  const buildID = await downloadLatestReactBuild();
+
+  await buildAndTestInlinePackage();
+  await buildAndTestStandalonePackage();
+  await buildAndTestExtensions();
+
+  await publishToNPM();
+
+  await printFinalInstructions(buildID, archivePath);
+}
+
+async function archiveGitRevision() {
+  const desktopPath = join(homedir(), 'Desktop');
+  const archivePath = join(desktopPath, 'DevTools.tgz');
+
+  console.log(`Creating git archive at ${chalk.dim(archivePath)}`);
+  console.log('');
+
+  if (!DRY_RUN) {
+    await exec(`git archive main | gzip > ${archivePath}`, {cwd: ROOT_PATH});
+  }
+
+  return archivePath;
+}
+
+async function buildAndTestExtensions() {
+  const extensionsPackagePath = join(
+    ROOT_PATH,
+    'packages',
+    'react-devtools-extensions'
+  );
+  const buildExtensionsPromise = exec('yarn build', {
+    cwd: extensionsPackagePath,
+  });
+
+  await logger(
+    buildExtensionsPromise,
+    `Building browser extensions ${chalk.dim('(this may take a minute)')}`,
+    {
+      estimate: 60000,
+    }
+  );
+
+  console.log('');
+  console.log(`Extensions have been build for Chrome, Edge, and Firefox.`);
+  console.log('');
+  console.log('Smoke test each extension before continuing:');
+  console.log(`  ${chalk.bold.green('cd ' + extensionsPackagePath)}`);
+  console.log('');
+  console.log(`  ${chalk.dim('# Test Chrome extension')}`);
+  console.log(`  ${chalk.bold.green('yarn test:chrome')}`);
+  console.log('');
+  console.log(`  ${chalk.dim('# Test Edge extension')}`);
+  console.log(`  ${chalk.bold.green('yarn test:edge')}`);
+  console.log('');
+  console.log(`  ${chalk.dim('# Firefox Chrome extension')}`);
+  console.log(`  ${chalk.bold.green('yarn test:firefox')}`);
+
+  await confirmContinue();
+}
+
+async function buildAndTestStandalonePackage() {
+  const corePackagePath = join(ROOT_PATH, 'packages', 'react-devtools-core');
+  const buildCorePromise = exec('yarn build', {cwd: corePackagePath});
+
+  await logger(
+    buildCorePromise,
+    `Building ${chalk.bold('react-devtools-core')} package.`,
+    {
+      estimate: 20000,
+    }
+  );
+
+  const standalonePackagePath = join(ROOT_PATH, 'packages', 'react-devtools');
+  const safariFixturePath = join(
+    ROOT_PATH,
+    'fixtures',
+    'devtools',
+    'standalone',
+    'index.html'
+  );
+
+  console.log('');
+  console.log(
+    `Test the ${chalk.bold('react-devtools-core')} target before continuing:`
+  );
+  console.log(`  ${chalk.bold.green('cd ' + standalonePackagePath)}`);
+  console.log(`  ${chalk.bold.green('yarn start')}`);
+  console.log('');
+  console.log(
+    'The following fixture can be useful for testing Safari integration:'
+  );
+  console.log(`  ${chalk.dim(safariFixturePath)}`);
+
+  await confirmContinue();
+}
+
+async function buildAndTestInlinePackage() {
+  const inlinePackagePath = join(
+    ROOT_PATH,
+    'packages',
+    'react-devtools-inline'
+  );
+  const buildPromise = exec('yarn build', {cwd: inlinePackagePath});
+
+  await logger(
+    buildPromise,
+    `Building ${chalk.bold('react-devtools-inline')} package.`,
+    {
+      estimate: 10000,
+    }
+  );
+
+  const shellPackagePath = join(ROOT_PATH, 'packages', 'react-devtools-shell');
+
+  console.log('');
+  console.log(`Built ${chalk.bold('react-devtools-inline')} target.`);
+  console.log('');
+  console.log('Test this build before continuing:');
+  console.log(`  ${chalk.bold.green('cd ' + shellPackagePath)}`);
+  console.log(`  ${chalk.bold.green('yarn start')}`);
+
+  await confirmContinue();
+}
+
+async function checkNPMPermissions() {
+  const currentUser = await execRead('npm whoami');
+  const failedProjects = [];
+
+  const checkProject = async project => {
+    const owners = (await execRead(`npm owner ls ${project}`))
+      .split('\n')
+      .filter(owner => owner)
+      .map(owner => owner.split(' ')[0]);
+
+    if (!owners.includes(currentUser)) {
+      failedProjects.push(project);
+    }
+  };
+
+  await logger(
+    Promise.all(NPM_PACKAGES.map(checkProject)),
+    `Checking NPM permissions for ${chalk.bold(currentUser)}.`,
+    {estimate: 2500}
+  );
+
+  console.log('');
+
+  if (failedProjects.length) {
+    console.error(chalk.bold('Insufficient NPM permissions'));
+    console.error('');
+    console.error(
+      `NPM user {underline ${currentUser}} is not an owner for: ${chalk.bold(
+        failedProjects.join(', ')
+      )}`
+    );
+    console.error(
+      'Please contact a React team member to be added to the above project(s).'
+    );
+    process.exit(1);
+  }
 }
 
 async function commitPendingChanges(previousVersion, nextVersion) {
+  console.log('');
+  console.log('Committing revision and changelog.');
+  console.log(chalk.dim('  git add .'));
   console.log(
     chalk.dim(
-      '  git add . \n' +
-        `  git commit -m "React DevTools ${previousVersion} -> ${nextVersion}"`
+      `  git commit -m "React DevTools ${previousVersion} -> ${nextVersion}"`
     )
   );
 
@@ -75,6 +259,75 @@ async function commitPendingChanges(previousVersion, nextVersion) {
       git commit -m "React DevTools ${previousVersion} -> ${nextVersion}"
     `);
   }
+
+  console.log('');
+  console.log(`Please push this commit before continuing:`);
+  console.log(`  ${chalk.bold.green('git push')}`);
+
+  await confirmContinue();
+}
+
+async function confirmContinue() {
+  console.log('');
+
+  const {confirm} = await inquirer.prompt({
+    name: 'confirm',
+    type: 'confirm',
+    message: 'Continue the release?',
+  });
+  if (!confirm) {
+    process.exit(0);
+  }
+
+  console.log('');
+}
+
+async function downloadLatestReactBuild() {
+  const releaseScriptPath = join(ROOT_PATH, 'scripts', 'release');
+  const installPromise = exec('yarn install', {cwd: releaseScriptPath});
+
+  await logger(installPromise, 'Installing release script dependencies.', {
+    estimate: 5000,
+  });
+
+  const {commit} = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'commit',
+      message: 'Which React version (commit) should be used?',
+      default: 'main',
+    },
+  ]);
+  console.log('');
+
+  const downloadScriptPath = join(
+    releaseScriptPath,
+    'download-experimental-build.js'
+  );
+  const downloadPromise = execRead(
+    `"${downloadScriptPath}" --commit=${commit}`
+  );
+
+  const output = await logger(
+    downloadPromise,
+    'Downloading React artifacts from CI.',
+    {estimate: 15000}
+  );
+
+  const match = output.match('--build=([0-9]+)');
+  if (match.length === 0) {
+    console.error(`No build ID found in "${output}"`);
+    process.exit(1);
+  }
+
+  const buildID = match[1];
+
+  console.log('');
+  console.log(`Downloaded artiacts for CI build ${chalk.bold(buildID)}.`);
+
+  await confirmContinue();
+
+  return buildID;
 }
 
 async function execRead(command, options) {
@@ -115,7 +368,7 @@ async function getPreviousCommitSha() {
   lines.split('\n').forEach((line, index) => {
     const [hash, message, date] = line.split(':::');
     choices.push({
-      name: `\x1b[1m${hash}\x1b[0m \x1b[2m- ${date} -\x1b[0m ${message}`,
+      name: `${chalk.bold(hash)} ${chalk.dim(date)} ${message}`,
       value: hash,
       short: date,
     });
@@ -155,23 +408,84 @@ async function getReleaseType() {
   return releaseType;
 }
 
-async function reviewChangelogPrompt() {
+async function printFinalInstructions(buildID, archivePath) {
+  console.log('');
   console.log(
-    '\nThe changelog has been updated with commits since the previous release:' +
-      `\n  \x1b[1m${CHANGELOG_PATH}\x1b[0m` +
-      '\n\nPlease review the new entries and fill in missing GitHub usernames:' +
-      '\n'
+    'You are now ready to publish the extension to Chrome, Edge, and Firefox:'
   );
+  console.log(
+    `  ${chalk.blue.underline(
+      'https://fburl.com/publish-react-devtools-extensions'
+    )}`
+  );
+  console.log('');
+  console.log('When publishing to Firefox, remember the following:');
+  console.log(`  Build id: ${chalk.bold(buildID)}`);
+  console.log(`  Git archive: ${chalk.bold(archivePath)}`);
+  console.log('');
+  console.log('Also consider syncing this release to Facebook:');
+  console.log(`  ${chalk.bold.green('js1 upgrade react-devtools')}`);
+}
 
-  const {confirm} = await inquirer.prompt({
-    name: 'confirm',
-    type: 'confirm',
-    message: 'Continue the release?',
-  });
+async function publishToNPM() {
+  const {otp} = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'otp',
+      message: 'Please provide an NPM two-factor auth token:',
+    },
+  ]);
 
-  if (!confirm) {
+  console.log('');
+
+  if (!otp) {
+    console.error(`Invalid OTP provided: "${chalk.bold(otp)}"`);
     process.exit(0);
   }
+
+  for (let index = 0; index < NPM_PACKAGES.length; index++) {
+    const npmPackage = NPM_PACKAGES[index];
+    const packagePath = join(ROOT_PATH, 'packages', npmPackage);
+
+    if (DRY_RUN) {
+      console.log(`Publishing package ${chalk.bold(npmPackage)}`);
+      console.log(chalk.dim(`  npm publish --otp=${otp}`));
+    } else {
+      const publishPromise = exec(`npm publish --otp=${otp}`, {
+        cwd: packagePath,
+      });
+
+      await logger(
+        publishPromise,
+        `Publishing package ${chalk.bold(npmPackage)}`,
+        {
+          estimate: 2500,
+        }
+      );
+    }
+  }
+}
+
+async function reviewChangelogPrompt() {
+  console.log('');
+  console.log(
+    'The changelog has been updated with commits since the previous release:'
+  );
+  console.log(`  ${chalk.bold(CHANGELOG_PATH)}`);
+  console.log('');
+  console.log('Please review the new entries and modify the text to:');
+  console.log('  1. Organize the list into Features vs Bugfixes');
+  console.log(
+    '  1. Filter out any non-user-visible changes (e.g. typo fixes or changes to build/release scripts)'
+  );
+  console.log(
+    '  1. Combine related PRs into a single bullet list (e.g. this scheduling profiler bullet)'
+  );
+  console.log(
+    '  1. Replacing the "USERNAME" placeholder text with the GitHub username(s) for people who worked on each PR.'
+  );
+
+  await confirmContinue();
 }
 
 function updateChangelog(nextVersion, commitLog) {
