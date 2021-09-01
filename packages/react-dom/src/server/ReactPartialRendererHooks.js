6178d82d78fc0ba679701dce0814348fc148d6f3
Sergey Petrov <sekk1e@yandex.ru>
2021-09-01 18:02:49 +0700

    - console.error for useLayoutEffect on server

--- packages/react-dom/src/server/ReactPartialRendererHooks.js
+++ packages/react-dom/src/server/ReactPartialRendererHooks.js
@@ -391,14 +391,6 @@ export function useLayoutEffect(
 ) {
   if (__DEV__) {
     currentHookNameInDev = 'useLayoutEffect';
-    console.error(
-      'useLayoutEffect does nothing on the server, because its effect cannot ' +
-        "be encoded into the server renderer's output format. This will lead " +
-        'to a mismatch between the initial, non-hydrated UI and the intended ' +
-        'UI. To avoid this, useLayoutEffect should only be used in ' +
-        'components that render exclusively on the client. ' +
-        'See https://reactjs.org/link/uselayouteffect-ssr for common fixes.',
-    );
   }
 }
 
