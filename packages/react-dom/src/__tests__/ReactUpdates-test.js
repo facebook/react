082f49f6efe79245424a4173af8cbe132b42487c
Andrew Clark <git@andrewclark.io>
2021-09-08 20:38:49 -0400

    Add test that triggers infinite update loop
    In 18, passive effects are flushed synchronously if they are the
    result of a synchronous update. We have a guard for infinite update
    loops that occur in the layout phase, but it doesn't currently work for
    synchronous updates from a passive effect.
    
    The reason this probably hasn't come up yet is because synchronous
    updates inside the passive effect phase are relatively rare: you either
    have to imperatively dispatch a discrete event, like `el.focus`, or you
    have to call `ReactDOM.flushSync`, which triggers a warning. (In
    general, updates inside a passive effect are not encouraged.)
    
    I discovered this because `useSyncExternalStore` does sometimes
    trigger updates inside the passive effect phase.
    
    This commit adds a failing test to prove the issue exists. I will fix
    it in the next commit.

--- packages/react-dom/src/__tests__/ReactUpdates-test.js
+++ packages/react-dom/src/__tests__/ReactUpdates-test.js
@@ -1594,6 +1594,7 @@ describe('ReactUpdates', () => {
     });
   });
 
+  // TODO: Replace this branch with @gate pragmas
   if (__DEV__) {
     it('warns about a deferred infinite update loop with useEffect', () => {
       function NonTerminating() {
@@ -1684,4 +1685,35 @@ describe('ReactUpdates', () => {
       expect(container.textContent).toBe('1000');
     });
   }
+
+  it('prevents infinite update loop triggered by synchronous updates in useEffect', () => {
+    // Ignore flushSync warning
+    spyOnDev(console, 'error');
+
+    function NonTerminating() {
+      const [step, setStep] = React.useState(0);
+      React.useEffect(() => {
+        // Other examples of synchronous updates in useEffect are imperative
+        // event dispatches like `el.focus`, or `useSyncExternalStore`, which
+        // may schedule a synchronous update upon subscribing if it detects
+        // that the store has been mutated since the initial render.
+        //
+        // (Originally I wrote this test using `el.focus` but those errors
+        // get dispatched in a JSDOM event and I don't know how to "catch" those
+        // so that they don't fail the test.)
+        ReactDOM.flushSync(() => {
+          setStep(step + 1);
+        });
+      }, [step]);
+      return step;
+    }
+
+    const container = document.createElement('div');
+    const root = ReactDOM.createRoot(container);
+    expect(() => {
+      ReactDOM.flushSync(() => {
+        root.render(<NonTerminating />);
+      });
+    }).toThrow('Maximum update depth exceeded');
+  });
 });
