02a790dafea0844dd4b3b1bb58b4090fcfc6bbdb
Biki-das <bikid475@gmail.com>
2021-09-16 11:59:22 +0530

    fixed comments in file

--- packages/react-devtools-scheduling-profiler/src/import-worker/preprocessData.js
+++ packages/react-devtools-scheduling-profiler/src/import-worker/preprocessData.js
@@ -574,7 +574,7 @@ function processTimelineEvent(
           }
         }
 
-        // TODO (scheduling profiler) Maybe we should calculate depth in post,
+        // TODO (scheduling profiler) Maybe we should calculate depth in the post,
         // so unresolved Suspense requests don't take up space.
         // We can't know if they'll be resolved or not at this point.
         // We'll just give them a default (fake) duration width.
@@ -677,7 +677,7 @@ function processTimelineEvent(
           const stopTime = nativeEvent.timestamp + nativeEvent.duration;
 
           // If React work was scheduled during an event handler, and the event had a long duration,
-          // it might be because the React render was long and stretched the event.
+          // it might be because the React renders were long and stretched the event.
           // It might also be that the React work was short and that something else stretched the event.
           // Make a note of this event for now and we'll examine the batch of React render work later.
           // (We can't know until we're done processing the React update anyway.)
@@ -917,7 +917,7 @@ export default async function preprocessData(
       profilerData.batchUIDToMeasuresMap.size === 0
     ) {
       // No profiler version could indicate data was logged using an older build of React,
-      // before an explicitly profiler version was included in the logging data.
+      // before an explicit profiler version was included in the logging data.
       // But it could also indicate that the website was either not using React, or using a production build.
       // The easiest way to check for this case is to see if the data contains any scheduled updates or render work.
       throw new InvalidProfileError(
