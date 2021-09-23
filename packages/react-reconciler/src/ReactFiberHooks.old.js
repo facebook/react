996cf65f9bb45e5641aed21fc531062fd7cacbdf
Biki-das <bikid475@gmail.com>
2021-09-23 10:27:28 +0530

    typo fix

--- packages/react-reconciler/src/ReactFiberHooks.old.js
+++ packages/react-reconciler/src/ReactFiberHooks.old.js
@@ -1133,7 +1133,7 @@ function useMutableSource<Source, Snapshot>(
       try {
         latestSetSnapshot(latestGetSnapshot(source._source));
 
-        // Record a pending mutable source update with the same expiration time.
+        // Record a pending mutable source update with the same lane.
         const lane = requestUpdateLane(fiber);
 
         markRootMutableRead(root, lane);
