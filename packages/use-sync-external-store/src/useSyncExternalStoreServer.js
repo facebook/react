5fce1871cbef857836be444d19e8ee1d46f65f9e
Marco Salazar <salazarm@fb.com>
2021-09-28 14:42:10 -0400

    just change useSyncExternalStoreServer

--- packages/use-sync-external-store/src/useSyncExternalStoreServer.js
+++ packages/use-sync-external-store/src/useSyncExternalStoreServer.js
@@ -7,23 +7,10 @@
  * @flow
  */
 
-import invariant from 'shared/invariant';
-import {isShim} from './useSyncExternalStoreClient';
-
 export function useSyncExternalStore<T>(
   subscribe: (() => void) => () => void,
   getSnapshot: () => T,
   getServerSnapshot?: () => T,
 ): T {
-  if (isShim) {
-    return getSnapshot();
-  }
-  if (getServerSnapshot === undefined) {
-    invariant(
-      false,
-      'Missing getServerSnapshot, which is required for server-' +
-        'rendered content.',
-    );
-  }
-  return getServerSnapshot();
+  return getSnapshot();
 }
