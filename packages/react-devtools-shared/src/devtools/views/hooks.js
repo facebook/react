7ed35ac743858d05305b1e6a428befc5b7e7e5b5
BIKI DAS <bikid475@gmail.com>
2021-10-01 23:19:01 +0530

    Update hooks.js

--- packages/react-devtools-shared/src/devtools/views/hooks.js
+++ packages/react-devtools-shared/src/devtools/views/hooks.js
@@ -239,7 +239,7 @@ export function useModalDismissSignal(
       ownerDocument = ((modalRef.current: any): HTMLDivElement).ownerDocument;
       ownerDocument.addEventListener('keydown', handleDocumentKeyDown);
       if (dismissOnClickOutside) {
-        ownerDocument.addEventListener('click', handleDocumentClick);
+        ownerDocument.addEventListener('click', handleDocumentClick,true);
         ownerDocument.removeEventListener('click', handleDocumentClick,true);
 
       }
