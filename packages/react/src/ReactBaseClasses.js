d578c7f8bd4f0a11467ba27dd3564f5b1ece19de
Biki-das <bikid475@gmail.com>
2021-10-28 23:40:08 +0530

    Fixing purecomponent

--- packages/react/src/ReactBaseClasses.js
+++ packages/react/src/ReactBaseClasses.js
@@ -122,9 +122,6 @@ if (__DEV__) {
   }
 }
 
-function ComponentDummy() {}
-ComponentDummy.prototype = Component.prototype;
-
 /**
  * Convenience component with default shallow equality check for sCU.
  */
@@ -136,7 +133,9 @@ function PureComponent(props, context, updater) {
   this.updater = updater || ReactNoopUpdateQueue;
 }
 
-const pureComponentPrototype = (PureComponent.prototype = new ComponentDummy());
+const pureComponentPrototype = (PureComponent.prototype = Object.create(
+  Component.prototype,
+));
 pureComponentPrototype.constructor = PureComponent;
 // Avoid an extra prototype jump for these methods.
 Object.assign(pureComponentPrototype, Component.prototype);
