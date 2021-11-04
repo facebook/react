60da9f8afe6e14d23f02deb448a0dda60842a258
Esteban Las <lasesteban@gmail.com>
2021-11-04 17:01:36 +0100

    Fix benchmark runner

--- scripts/bench/benchmarks/pe-functional-components/build.js
+++ scripts/bench/benchmarks/pe-functional-components/build.js
@@ -5,11 +5,11 @@ const {join} = require('path');
 async function build(reactPath, asyncCopyTo) {
   // copy the UMD bundles
   await asyncCopyTo(
-    join(reactPath, 'build', 'dist', 'react.production.min.js'),
+    join(reactPath, 'build', 'node_modules', 'react', 'umd', 'react.production.min.js'),
     join(__dirname, 'react.production.min.js')
   );
   await asyncCopyTo(
-    join(reactPath, 'build', 'dist', 'react-dom.production.min.js'),
+    join(reactPath, 'build', 'node_modules', 'react-dom', 'umd', 'react-dom.production.min.js'),
     join(__dirname, 'react-dom.production.min.js')
   );
 }
