dd0529f3bf1feb0a6cc74780e08e6b009bba9e97
oz9un <okultekin@etu.edu.tr>
2021-11-04 11:22:18 +0300

    Company name change Facebook -> Meta.

--- scripts/rollup/wrappers.js
+++ scripts/rollup/wrappers.js
@@ -47,7 +47,7 @@ function registerInternalModuleStop(globalName) {
   return String(readFileSync(path)).trim();
 }
 
-const license = ` * Copyright (c) Facebook, Inc. and its affiliates.
+const license = ` * Copyright (c) Meta, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.`;
