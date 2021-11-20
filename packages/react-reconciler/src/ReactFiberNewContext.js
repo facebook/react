bb3309874eccc55dcc91ff843bd30742d90fec85
eps1lon <silbermann.sebastian@gmail.com>
2021-11-20 13:46:47 +0100

    Don't warn about concurrent provider renders if we finished rendering

--- packages/react-reconciler/src/ReactFiberNewContext.js
+++ packages/react-reconciler/src/ReactFiberNewContext.js
@@ -141,6 +141,7 @@ export function popProvider(
     } else {
       context._currentValue = currentValue;
     }
+    context._currentRenderer = null;
   } else {
     if (
       enableServerContext &&
@@ -150,6 +151,7 @@ export function popProvider(
     } else {
       context._currentValue2 = currentValue;
     }
+    context._currentRenderer2 = null;
   }
 }
 
