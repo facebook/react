f2a59df48bec2352a4dd3b5415282a4ea240a4f8
salazarm <salazarm@users.noreply.github.com>
2021-12-08 11:21:02 -0500

    Remove unstableAvoidThisFallback from OSS (#22884)
    * avoidThisFallbackFlag
    
    * avoidThisFallback flag
    
    * missed a spot
    
    * rm gating

--- packages/shared/forks/ReactFeatureFlags.www.js
+++ packages/shared/forks/ReactFeatureFlags.www.js
@@ -50,7 +50,7 @@ export const enableProfilerNestedUpdateScheduledHook =
 export const enableUpdaterTracking = __PROFILE__;
 
 export const enableSuspenseLayoutEffectSemantics = true;
-export const enableSuspenseAvoidThisFallback = false;
+export const enableSuspenseAvoidThisFallback = true;
 
 // Logs additional User Timing API marks for use with an experimental profiling tool.
 export const enableSchedulingProfiler =
