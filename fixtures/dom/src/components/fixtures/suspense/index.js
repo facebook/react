3076fa2361159c7dac39045755f0638ddf80baca
mohammadreza <mohammad0reza79@gmail.com>
2021-12-20 15:37:40 +0330

    21761 fixed

--- fixtures/dom/src/components/fixtures/suspense/index.js
+++ fixtures/dom/src/components/fixtures/suspense/index.js
@@ -256,19 +256,19 @@ class TextInputFixtures extends React.Component {
             <SuspendyTree>
               <video controls>
                 <source
-                  src="http://techslides.com/demos/sample-videos/small.webm"
+                  src={window.matchMedia("(min-width: 1024px)").matches?"http://techslides.com/demos/sample-videos/small.webm" :""}
                   type="video/webm"
                 />
                 <source
-                  src="http://techslides.com/demos/sample-videos/small.ogv"
+                  src={window.matchMedia("(min-width: 1024px)").matches?"http://techslides.com/demos/sample-videos/small.ogv":""}
                   type="video/ogg"
                 />
                 <source
-                  src="http://techslides.com/demos/sample-videos/small.mp4"
+                  src={window.matchMedia("(min-width: 1024px)").matches?"http://techslides.com/demos/sample-videos/small.mp4":""}
                   type="video/mp4"
                 />
                 <source
-                  src="http://techslides.com/demos/sample-videos/small.3gp"
+                  src={window.matchMedia("(min-width: 1024px)").matches?"http://techslides.com/demos/sample-videos/small.3gp":""}
                   type="video/3gp"
                 />
               </video>
