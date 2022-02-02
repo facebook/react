127ec64bba53c5841be43ae04f7e5cfb42c663c8
Fran Dios <fran.dios@shopify.com>
2022-02-02 17:25:42 +0900

    fix: Flow ignored path

--- scripts/shared/inlinedHostConfigs.js
+++ scripts/shared/inlinedHostConfigs.js
@@ -81,6 +81,7 @@ module.exports = [
       'react-server-dom-vite',
       'react-server-dom-vite/writer',
       'react-server-dom-vite/writer.node.server',
+      'react-server-dom-vite/esm/react-server-dom-vite-writer.node.server.js',
       'react-server-dom-vite/src/ReactFlightDOMServerNode.js', // react-server-dom-vite/writer.node.server
       'react-client/src/ReactFlightClientStream.js', // We can only type check this in streaming configurations.
       'react-interactions',
