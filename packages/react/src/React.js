a6987bee730052dccdddd4645e15b1ce458fd9a6
Luna Ruan <lunaris.ruan@gmail.com>
2022-02-11 11:42:55 -0500

    add <TracingMarker> component boilerplate (#23275)
    - Add Tracing Marker component type to React exports
    - Add reconciler work tag
    - Add devtools work tag
    - Add boilerplate for the cache to render children
    
    No functionality yet

--- packages/react/src/React.js
+++ packages/react/src/React.js
@@ -19,6 +19,7 @@ import {
   REACT_OFFSCREEN_TYPE,
   REACT_SCOPE_TYPE,
   REACT_CACHE_TYPE,
+  REACT_TRACING_MARKER_TYPE,
 } from 'shared/ReactSymbols';
 
 import {Component, PureComponent} from './ReactBaseClasses';
@@ -126,6 +127,8 @@ export {
   REACT_CACHE_TYPE as unstable_Cache,
   // enableScopeAPI
   REACT_SCOPE_TYPE as unstable_Scope,
+  // enableTransitionTracing
+  REACT_TRACING_MARKER_TYPE as unstable_TracingMarker,
   useId,
   act,
 };
