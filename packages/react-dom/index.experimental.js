a2327442157265873bf63f1124c142322f41f6de
Sebastian Markbåge <sebastian@calyptus.eu>
2022-02-28 14:38:13 -0500

    Remove flushControlled from experimental releases (#23384)
    This brings the exports on npm to parity which simplifies things a bit.
    
    We also don't plan to release this. It is used by Draft.js but that caller
    will need to switch to flushSync.

--- packages/react-dom/index.experimental.js
+++ packages/react-dom/index.experimental.js
@@ -18,7 +18,6 @@ export {
   render,
   unmountComponentAtNode,
   unstable_batchedUpdates,
-  unstable_flushControlled,
   unstable_renderSubtreeIntoContainer,
   unstable_runWithPriority, // DO NOT USE: Temporarily exposed to migrate off of Scheduler.runWithPriority.
   version,
