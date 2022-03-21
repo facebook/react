7793f4da4a5f7f6e018ee78343e81a44feafe8d1
David McCabe <davemccabe@gmail.com>
2022-03-20 17:26:49 -0700

    Update packages/shared/ReactFeatureFlags.js
    Co-authored-by: Ricky <rickhanlonii@gmail.com>

--- packages/shared/ReactFeatureFlags.js
+++ packages/shared/ReactFeatureFlags.js
@@ -96,7 +96,7 @@ export const enableLegacyFBSupport = false;
 // -----------------------------------------------------------------------------
 
 export const enableCache = __EXPERIMENTAL__;
-export const enableCacheElement = true;
+export const enableCacheElement = __EXPERIMENTAL__;
 
 export const enableTransitionTracing = false;
 
