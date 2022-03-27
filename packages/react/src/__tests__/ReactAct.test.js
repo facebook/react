5ab20d1159def172e4e322341fd7a7206d3b23d0
Gao Sheng <gaosheng08@meituan.com>
2022-03-28 01:10:14 +0800

    Fix ReactAct `flushActQueue` error handler

---
+++ packages/react/src/__tests__/ReactAct.test.js
@@ -0,0 +1,22 @@
+import ReactCurrentActQueue from '../ReactCurrentActQueue';
+import { act } from '../ReactAct';
+
+describe('act', () => {
+  it('If something throws, leave the remaining callbacks on the queue.', () => {
+    ReactCurrentActQueue.current = [
+      () => null,
+      () => throw new Error(),
+      () => null
+    ];
+    ReactCurrentActQueue.didScheduleLegacyUpdate = true;
+
+    try {
+      act();
+    } catch {}
+
+    expect(ReactCurrentActQueue.current.length).toBe(1);
+  });
+});
+
+
+
