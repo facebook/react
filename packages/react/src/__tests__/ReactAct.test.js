53ce186b4d7e5a9cc38b1c97ee849529f656e99a
Gao Sheng <gaosheng08@meituan.com>
2022-03-28 11:12:54 +0800

    Fix test

--- packages/react/src/__tests__/ReactAct.test.js
+++ packages/react/src/__tests__/ReactAct.test.js
@@ -2,20 +2,23 @@ import ReactCurrentActQueue from '../ReactCurrentActQueue';
 import {act} from '../ReactAct';
 
 describe('act', () => {
-  it('If something throws, leave the remaining callbacks on the queue.', () => {
-    ReactCurrentActQueue.current = [
-      () => null,
-      () => {
-        throw new Error();
-      },
-      () => null,
-    ];
-    ReactCurrentActQueue.didScheduleLegacyUpdate = true;
+  (__DEV__ ? it : it.skip)(
+    'If something throws, leave the remaining callbacks on the queue.',
+    () => {
+      ReactCurrentActQueue.current = [
+        () => null,
+        () => {
+          throw new Error();
+        },
+        () => null,
+      ];
+      ReactCurrentActQueue.didScheduleLegacyUpdate = true;
 
-    try {
-      act();
-    } catch {}
+      try {
+        act();
+      } catch {}
 
-    expect(ReactCurrentActQueue.current.length).toBe(1);
-  });
+      expect(ReactCurrentActQueue.current.length).toBe(1);
+    },
+  );
 });
