becaffd29e28205c20cc74b47c87fcc55ed2c013
Rick Hanlon <rickhanlonii@gmail.com>
2022-04-12 18:13:51 -0400

    Temporarily feature flag numeric fallback for symbols

--- packages/shared/forks/ReactFeatureFlags.www-dynamic.js
+++ packages/shared/forks/ReactFeatureFlags.www-dynamic.js
@@ -28,7 +28,7 @@ export const consoleManagedByDevToolsDuringStrictMode = __VARIANT__;
 export const enableClientRenderFallbackOnHydrationMismatch = __VARIANT__;
 export const enableClientRenderFallbackOnTextMismatch = __VARIANT__;
 export const enableTransitionTracing = __VARIANT__;
-
+export const enableSymbolFallbackForWWW = __VARIANT__;
 // Enable this flag to help with concurrent mode debugging.
 // It logs information to the console about React scheduling, rendering, and commit phases.
 //
