5ef1583423b5dde88295f9a992922b3f971e4de1
Andrew Clark <git@andrewclark.io>
2022-04-26 16:48:15 -0400

    Bump `@next` versions
    18.2 will be the next release

--- ReactVersions.js
+++ ReactVersions.js
@@ -18,25 +18,25 @@
 //
 //   0.0.0-experimental-241c4467e-20200129
 
-const ReactVersion = '18.1.0';
+const ReactVersion = '18.2.0';
 
 // The label used by the @next channel. Represents the upcoming release's
 // stability. Could be "alpha", "beta", "rc", etc.
 const nextChannelLabel = 'next';
 
 const stablePackages = {
-  'eslint-plugin-react-hooks': '4.5.0',
-  'jest-react': '0.13.1',
+  'eslint-plugin-react-hooks': '4.6.0',
+  'jest-react': '0.14.0',
   react: ReactVersion,
   'react-art': ReactVersion,
   'react-dom': ReactVersion,
   'react-is': ReactVersion,
-  'react-reconciler': '0.28.0',
-  'react-refresh': '0.13.0',
+  'react-reconciler': '0.29.0',
+  'react-refresh': '0.14.0',
   'react-test-renderer': ReactVersion,
-  'use-subscription': '1.7.0',
-  'use-sync-external-store': '1.1.0',
-  scheduler: '0.22.0',
+  'use-subscription': '1.8.0',
+  'use-sync-external-store': '1.2.0',
+  scheduler: '0.23.0',
 };
 
 // These packages do not exist in the @next or @latest channel, only
