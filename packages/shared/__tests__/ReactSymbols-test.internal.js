6cbf0f7fac772c3fd18706d73af00f3fd06a55b5
Ricky <rickhanlonii@gmail.com>
2022-05-03 17:12:23 -0400

    Fork ReactSymbols (#24484)
    * Fork ReactSymbols
    
    * Fix tests
    
    * Update jest config

--- packages/shared/__tests__/ReactSymbols-test.internal.js
+++ packages/shared/__tests__/ReactSymbols-test.internal.js
@@ -32,7 +32,7 @@ describe('ReactSymbols', () => {
     const originalSymbolFor = global.Symbol.for;
     global.Symbol.for = null;
     try {
-      const entries = Object.entries(require('shared/ReactSymbols')).filter(
+      const entries = Object.entries(require('shared/ReactSymbols.www')).filter(
         // REACT_ASYNC_MODE_TYPE and REACT_CONCURRENT_MODE_TYPE have the same numeric value
         // for legacy backwards compatibility
         ([key]) => key !== 'REACT_ASYNC_MODE_TYPE',
