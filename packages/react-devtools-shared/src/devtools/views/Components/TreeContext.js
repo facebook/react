d4acbe85d55b6048d65b6ff258b7996516d4aa58
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-05-05 11:46:57 -0400

    Fixed possible undefined error in TreeContext reducer (#24501)

--- packages/react-devtools-shared/src/devtools/views/Components/TreeContext.js
+++ packages/react-devtools-shared/src/devtools/views/Components/TreeContext.js
@@ -377,11 +377,10 @@ function reduceTreeState(store: Store, state: State, action: Action): State {
         }
         break;
       case 'SELECT_PREVIOUS_ELEMENT_WITH_ERROR_OR_WARNING_IN_TREE': {
-        if (store.errorCount === 0 && store.warningCount === 0) {
+        const elementIndicesWithErrorsOrWarnings = store.getElementsWithErrorsAndWarnings();
+        if (elementIndicesWithErrorsOrWarnings.length === 0) {
           return state;
         }
-
-        const elementIndicesWithErrorsOrWarnings = store.getElementsWithErrorsAndWarnings();
 
         let flatIndex = 0;
         if (selectedElementIndex !== null) {
@@ -419,11 +418,10 @@ function reduceTreeState(store: Store, state: State, action: Action): State {
         break;
       }
       case 'SELECT_NEXT_ELEMENT_WITH_ERROR_OR_WARNING_IN_TREE': {
-        if (store.errorCount === 0 && store.warningCount === 0) {
+        const elementIndicesWithErrorsOrWarnings = store.getElementsWithErrorsAndWarnings();
+        if (elementIndicesWithErrorsOrWarnings.length === 0) {
           return state;
         }
-
-        const elementIndicesWithErrorsOrWarnings = store.getElementsWithErrorsAndWarnings();
 
         let flatIndex = -1;
         if (selectedElementIndex !== null) {
