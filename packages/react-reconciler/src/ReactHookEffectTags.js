7cd923f565d28a189717a77665cab472d29fc887
“konghai” <574980606@qq.com>
2022-07-12 22:16:41 +0800

    style: formatting

--- packages/react-reconciler/src/ReactHookEffectTags.js
+++ packages/react-reconciler/src/ReactHookEffectTags.js
@@ -15,6 +15,6 @@ export const NoFlags = /*   */ 0b0000;
 export const HasEffect = /* */ 0b0001;
 
 // Represents the phase in which the effect (not the clean-up) fires.
-export const Insertion = /* */ 0b0010;
+export const Insertion = /*  */ 0b0010;
 export const Layout = /*    */ 0b0100;
 export const Passive = /*   */ 0b1000;
