454ecb92b48d2cf9f08b60d0b67918baec02dddb
Tim Neutkens <tim@timneutkens.nl>
2022-08-18 14:54:32 +0100

    Remove unused files

--- fixtures/flight/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
