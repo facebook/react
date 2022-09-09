197184ffb5bf1a740ec0d7c6a542cc7f1f239251
Mofei Zhang <feifei0@fb.com>
2022-09-09 16:48:34 -0400

    experimental_use(context) for server components and ssr

--- packages/react-server/src/ReactFlightHooks.js
+++ packages/react-server/src/ReactFlightHooks.js
@@ -200,8 +200,12 @@ function use<T>(usable: Usable<T>): T {
           }
         }
       }
-    } else {
-      // TODO: Add support for Context
+    } else if (
+      usable.$$typeof != null &&
+      usable.$$typeof === REACT_SERVER_CONTEXT_TYPE
+    ) {
+      const context: ReactServerContext<T> = (usable: any);
+      return readContext(context);
     }
   }
 
