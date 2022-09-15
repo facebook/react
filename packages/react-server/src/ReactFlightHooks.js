1a7ac9ac84681292773e03fe8b7f82f8db387f95
Jiachi Liu <inbox@huozhi.im>
2022-09-15 16:30:41 +0200

    prettier

--- packages/react-server/src/ReactFlightHooks.js
+++ packages/react-server/src/ReactFlightHooks.js
@@ -49,9 +49,9 @@ function readContext<T>(context: ReactServerContext<T>): T {
   if (__DEV__) {
     if (context.$$typeof !== REACT_SERVER_CONTEXT_TYPE) {
       console.error(
-        `Only createServerContext is supported in Server Components. ${
-          `createContext found${context.displayName ? ` in ${context.displayName}` : ''}.`
-        }`
+        `Only createServerContext is supported in Server Components. ${`createContext found${
+          context.displayName ? ` in ${context.displayName}` : ''
+        }.`}`,
       );
     }
     if (currentCache === null) {
