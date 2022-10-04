15a9caff1055beb70b72e17c5b0816031922a430
Jan Kassens <jkassens@meta.com>
2022-10-04 11:04:40 -0400

    Flow upgrade to 0.152
    - 0.147 removes access to Object.prototype via the global object.
    - 0.149 removed deprecated config options
    
    [ghstack-poisoned]

--- packages/react-devtools-shared/src/devtools/views/Components/KeyValue.js
+++ packages/react-devtools-shared/src/devtools/views/Components/KeyValue.js
@@ -33,6 +33,8 @@ import type {Element} from 'react-devtools-shared/src/devtools/views/Components/
 import type {Element as ReactElement} from 'react';
 import type {FrontendBridge} from 'react-devtools-shared/src/bridge';
 
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
 type Type = 'props' | 'state' | 'context' | 'hooks';
 
 type KeyValueProps = {
