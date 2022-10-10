aa9988e5e669122ed0d76c72f2dea159eef872b4
Josh Story <story@hey.com>
2022-10-10 11:06:22 -0700

    Server render fork for react-dom (#25436)
    Publish an aliasable entry for `react-dom` top level package exports for use in server environments. This is a stub containing only the exports that we expect to retain in the top level once 19 is released

---
+++ packages/react-dom/server-rendering-stub.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+// Export all exports so that they're available in tests.
+// We can't use export * from in Flow for some reason.
+
+import ReactVersion from 'shared/ReactVersion';
+export {ReactVersion as version};
+
+export {default as __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED} from './src/ReactDOMSharedInternals';
+
+export {
+  createPortal,
+  flushSync,
+} from './src/server/ReactDOMServerRenderingStub';
