9cdf8a99edcfd94d7420835ea663edca04237527
Andrew Clark <git@andrewclark.io>
2022-10-18 11:19:24 -0400

    [Codemod] Update copyright header to Meta (#25315)
    * Facebook -> Meta in copyright
    
    rg --files | xargs sed -i 's#Copyright (c) Facebook, Inc. and its affiliates.#Copyright (c) Meta Platforms, Inc. and affiliates.#g'
    
    * Manual tweaks

--- fixtures/legacy-jsx-runtimes/react-16/cjs/react-jsx-runtime.development.js
+++ fixtures/legacy-jsx-runtimes/react-16/cjs/react-jsx-runtime.development.js
@@ -1,7 +1,7 @@
 /** @license React v16.14.0
  * react-jsx-runtime.development.js
  *
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
