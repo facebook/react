c635807875630e7057777e898372eed43e3b0a24
Andrew Clark <git@andrewclark.io>
2022-10-20 22:08:23 -0400

    Support `use` in `act` testing API (#25523)
    `use` can avoid suspending on already resolved data by yielding to
    microtasks. In a real, browser environment, we do this by scheduling a
    platform task (i.e. postTask).
    
    In a test environment, tasks are scheduled on a special internal queue
    so that they can be flushed by the `act` testing API. So we need to add
    support for this in `act`.
    
    This behavior only works if you `await` the thenable returned by the
    `act` call. We currently do not require that users do this. So I added a
    warning, but it only fires if `use` was called. The old Suspense pattern
    will not trigger a warning. This is to avoid breaking existing tests
    that use Suspense.
    
    The implementation of `act` has gotten extremely complicated because of
    the subtle changes in behavior over the years, and our commitment to
    maintaining backwards compatibility. We really should consider being
    more restrictive in a future major release.
    
    The changes are a bit confusing so I did my best to add inline comments
    explaining how it works.
    
    ## Test plan
    
    I ran this against Facebook's internal Jest test suite to confirm
    nothing broke

--- packages/react/src/ReactCurrentActQueue.js
+++ packages/react/src/ReactCurrentActQueue.js
@@ -7,7 +7,7 @@
  * @flow
  */
 
-type RendererTask = boolean => RendererTask | null;
+export type RendererTask = boolean => RendererTask | null;
 
 const ReactCurrentActQueue = {
   current: (null: null | Array<RendererTask>),
@@ -15,6 +15,11 @@ const ReactCurrentActQueue = {
   // Used to reproduce behavior of `batchedUpdates` in legacy mode.
   isBatchingLegacy: false,
   didScheduleLegacyUpdate: false,
+
+  // Tracks whether something called `use` during the current batch of work.
+  // Determines whether we should yield to microtasks to unwrap already resolved
+  // promises without suspending.
+  didUsePromise: false,
 };
 
 export default ReactCurrentActQueue;
