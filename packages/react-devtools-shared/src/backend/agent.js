fecc288b7dce182fbc3056b3f98a445a95fb1e68
Robert Balicki <robertbalicki@fb.com>
2022-10-25 13:25:33 +1100

    [react devtools] Device storage support (#25452)
    # Summary
    * This PR adds support for persisting certain settings to device
    storage, allowing e.g. RN apps to properly patch the console when
    restarted.
    * The device storage APIs have signature `getConsolePatchSettings()` and
    `setConsolePatchSettings(string)`, in iOS, are thin wrappers around the
    `Library/Settings` turbomodule, and wrap a new TM that uses the `SharedPreferences` class in Android.
    * Pass device storage getters/setters from RN to DevTools'
    `connectToDevtools`. The setters are then used to populate values on
    `window`. Later, the console is patched using these values.
    * If we receive a notification from DevTools that the console patching
    fields have been updated, we write values back to local storage.
    * See https://github.com/facebook/react-native/pull/34903
    
    # How did you test this change?
    Manual testing, `yarn run test-build-devtools`, `yarn run prettier`,
    `yarn run flow dom`
    
    ## Manual testing setup:
    
    ### React DevTools Frontend
    * Get the DevTools frontend in flipper:
    * `nvm install -g react-devtools-core`, then replace that package with a
    symlink to the local package
      * enable "use globally installed devtools" in flipper
    * yarn run start in react-devtools, etc. as well
    
    ### React DevTools Backend
    * `yarn run build:backend` in react-devtools-core, then copy-paste that
    file to the expo app's node_modules directory
    
    ### React Native
    * A local version of React Native can be patched in by modifying an expo
    app's package.json, as in `"react-native":
    "rbalicki2/react-native#branch-name"`
    
    # Versioning safety
    * There are three versioned modules to worry about: react native, the
    devtools frontend and the devtools backend.
    * The react devtools backend checks for whether a `cachedSettingsStore`
    is passed from react native. If not (e.g. if React Native is outdated),
    then no behavior changes.
    * The devtools backend reads the patched console values from the cached
    settings store. However, if nothing has been stored, for example because
    the frontend is outdated or has never synced its settings, then behavior
    doesn't change.
    * The devtools frontend sends no new messages. However, if it did send a
    new message (e.g. "store this value at this key"), and the backend was
    outdated, that message would be silently ignored.

--- packages/react-devtools-shared/src/backend/agent.js
+++ packages/react-devtools-shared/src/backend/agent.js
@@ -25,7 +25,7 @@ import {
   initialize as setupTraceUpdates,
   toggleEnabled as setTraceUpdatesEnabled,
 } from './views/TraceUpdates';
-import {patch as patchConsole} from './console';
+import {patch as patchConsole, type ConsolePatchSettings} from './console';
 import {currentBridgeProtocol} from 'react-devtools-shared/src/bridge';
 
 import type {BackendBridge} from 'react-devtools-shared/src/bridge';
@@ -712,11 +712,11 @@ export default class Agent extends EventEmitter<{
     showInlineWarningsAndErrors,
     hideConsoleLogsInStrictMode,
     browserTheme,
-  }) => {
-    // If the frontend preference has change,
-    // or in the case of React Native- if the backend is just finding out the preference-
+  }: ConsolePatchSettings) => {
+    // If the frontend preferences have changed,
+    // or in the case of React Native- if the backend is just finding out the preferences-
     // then reinstall the console overrides.
-    // It's safe to call these methods multiple times, so we don't need to worry about that.
+    // It's safe to call `patchConsole` multiple times.
     patchConsole({
       appendComponentStack,
       breakOnConsoleErrors,
