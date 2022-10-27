758855539fa8381e6987772ae467fbb0e7d0da45
Sebastian Markbåge <sebastian@calyptus.eu>
2022-10-27 17:52:53 -0400

    Remove old react-fetch, react-fs and react-pg libraries (#25577)
    To avoid confusion. We are patching `fetch`, and only `fetch`, for a
    small fix scoped to react renders elsewhere, but this code is not it.
    
    This code was for the strategy used in the original [React Server
    Components demo](https://github.com/reactjs/server-components-demo).
    Which [we
    announced](https://reactjs.org/blog/2022/06/15/react-labs-what-we-have-been-working-on-june-2022.html)
    that we're moving away from in favor of [First class support for
    promises and async/await](https://github.com/reactjs/rfcs/pull/229).
    
    We might explore using these package for other instrumentation in the
    future but not now and not like this.

--- packages/react-fs/npm/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-throw new Error(
-  'React FS cannot be used outside a react-server environment. ' +
-    'You must configure Node.js using the `--conditions react-server` flag.'
-);
