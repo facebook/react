6de70ef00a28b8efebe73a7314128ce8b099cc19
Andrew Clark <git@andrewclark.io>
2022-10-29 17:46:54 -0700

    Clean up vestige of useOpaqueIdentifier (#25587)
    Found some code that was left over from the experimental
    useOpaqueIdentifier hook, which we eventually replaced with useId.
    
    This deletes it.

--- packages/react-reconciler/src/ReactFiberWorkLoop.old.js
+++ packages/react-reconciler/src/ReactFiberWorkLoop.old.js
@@ -205,7 +205,6 @@ import {resetContextDependencies} from './ReactFiberNewContext.old';
 import {
   resetHooksAfterThrow,
   ContextOnlyDispatcher,
-  getIsUpdatingOpaqueValueInRenderPhaseInDEV,
 } from './ReactFiberHooks.old';
 import {DefaultCacheDispatcher} from './ReactFiberCache.old';
 import {
@@ -3638,10 +3637,7 @@ if (__DEV__) {
 
 function warnAboutRenderPhaseUpdatesInDEV(fiber) {
   if (__DEV__) {
-    if (
-      ReactCurrentDebugFiberIsRenderingInDEV &&
-      !getIsUpdatingOpaqueValueInRenderPhaseInDEV()
-    ) {
+    if (ReactCurrentDebugFiberIsRenderingInDEV) {
       switch (fiber.tag) {
         case FunctionComponent:
         case ForwardRef:
