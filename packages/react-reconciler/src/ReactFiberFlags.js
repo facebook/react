bf2a030eb6c9e737308a78193f0415208ad166b1
Mengdi Chen <mengdi@fb.com>
2022-11-10 16:14:46 -0500

    better order of flags

--- packages/react-reconciler/src/ReactFiberFlags.js
+++ packages/react-reconciler/src/ReactFiberFlags.js
@@ -14,19 +14,20 @@ export type Flags = number;
 // Don't change these two values. They're used by React Dev Tools.
 export const NoFlags = /*                      */ 0b00000000000000000000000000;
 export const PerformedWork = /*                */ 0b00000000000000000000000001;
+export const Placement = /*                    */ 0b00000000000000000000000010;
+export const DidCapture = /*                   */ 0b00000000000000000001000000;
+export const Hydrating = /*                    */ 0b00000000000000100000000000;
+export const Incomplete = /*                   */ 0b00000000000100000000000000;
 
 // You can change the rest (and add more).
-export const Placement = /*                    */ 0b00000000000000000000000010;
 export const Update = /*                       */ 0b00000000000000000000000100;
 export const ChildDeletion = /*                */ 0b00000000000000000000001000;
 export const ContentReset = /*                 */ 0b00000000000000000000010000;
 export const Callback = /*                     */ 0b00000000000000000000100000;
-export const DidCapture = /*                   */ 0b00000000000000000001000000;
 export const ForceClientRender = /*            */ 0b00000000000000000010000000;
 export const Ref = /*                          */ 0b00000000000000000100000000;
 export const Snapshot = /*                     */ 0b00000000000000001000000000;
 export const Passive = /*                      */ 0b00000000000000010000000000;
-export const Hydrating = /*                    */ 0b00000000000000100000000000;
 export const Visibility = /*                   */ 0b00000000000001000000000000;
 export const StoreConsistency = /*             */ 0b00000000000010000000000000;
 
@@ -37,7 +38,6 @@ export const LifecycleEffectMask =
 export const HostEffectMask = /*               */ 0b00000000000011111111111111;
 
 // These are not really side effects, but we still reuse this field.
-export const Incomplete = /*                   */ 0b00000000000100000000000000;
 export const ShouldCapture = /*                */ 0b00000000001000000000000000;
 export const ForceUpdateForLegacySuspense = /* */ 0b00000000010000000000000000;
 export const DidPropagateContext = /*          */ 0b00000000100000000000000000;
