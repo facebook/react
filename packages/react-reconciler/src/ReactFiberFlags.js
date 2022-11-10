b1a3e0278904b7d99c1b8be0a2a66cd222a6b9ff
Mengdi Chen <mengdi@fb.com>
2022-11-10 16:19:33 -0500

    update comment

--- packages/react-reconciler/src/ReactFiberFlags.js
+++ packages/react-reconciler/src/ReactFiberFlags.js
@@ -11,7 +11,7 @@ import {enableCreateEventHandleAPI} from 'shared/ReactFeatureFlags';
 
 export type Flags = number;
 
-// Don't change these two values. They're used by React Dev Tools.
+// Don't change these values. They're used by React Dev Tools.
 export const NoFlags = /*                      */ 0b00000000000000000000000000;
 export const PerformedWork = /*                */ 0b00000000000000000000000001;
 export const Placement = /*                    */ 0b00000000000000000000000010;
