0cf6694c8288c29739774fb18b779a7ea5f68de0
Mengdi Chen <mengdi@fb.com>
2022-11-11 15:18:06 -0500

    [DevTools] bug fix for Hydrating fibers (#25663)
    ## Summary
    
    This PR is to fix a bug: an "element cannot be found" error when
    hydrating Server Components
    
    ### The problem
    <img width="1061" alt="image"
    src="https://user-images.githubusercontent.com/1001890/201206046-ac32a5e3-b08a-4dc2-99f4-221dad504b28.png">
    
    To reproduce:
    1. setting up a vercel next.js 13 playground locally
    https://github.com/vercel/app-playground
    2. visit http://localhost:3000/loading
    3. click "electronics" button to navigate to
    http://localhost:3000/loading/electronics to trigger hydrating
    4. inspect one of the skeleton card UI from React DevTools extension
    
    ### The root cause & fix
    This bug was introduced in #22527. When syncing reconciler changes, the
    value of `Hydrating` was copied from another variable `Visibility` (one
    more zero in the binary number).
    To avoid this kind of issue in the future, a new file `ReactFiberFlags`
    is created following the same format of the one in reconciler, so that
    it's easier to sync the number without making mistakes.
    The reconciler fiber flag file is also updated to reflect which of the
    flags are used in devtools
    
    ## How did you test this change?
    
    I build it locally and the bug no longer exist on
    http://localhost:3000/loading

--- packages/react-reconciler/src/ReactFiberFlags.js
+++ packages/react-reconciler/src/ReactFiberFlags.js
@@ -11,22 +11,22 @@ import {enableCreateEventHandleAPI} from 'shared/ReactFeatureFlags';
 
 export type Flags = number;
 
-// Don't change these two values. They're used by React Dev Tools.
+// Don't change these values. They're used by React Dev Tools.
 export const NoFlags = /*                      */ 0b00000000000000000000000000;
 export const PerformedWork = /*                */ 0b00000000000000000000000001;
+export const Placement = /*                    */ 0b00000000000000000000000010;
+export const DidCapture = /*                   */ 0b00000000000000000001000000;
+export const Hydrating = /*                    */ 0b00000000000000100000000000;
 
 // You can change the rest (and add more).
-export const Placement = /*                    */ 0b00000000000000000000000010;
 export const Update = /*                       */ 0b00000000000000000000000100;
 export const ChildDeletion = /*                */ 0b00000000000000000000001000;
 export const ContentReset = /*                 */ 0b00000000000000000000010000;
 export const Callback = /*                     */ 0b00000000000000000000100000;
-export const DidCapture = /*                   */ 0b00000000000000000001000000;
 export const ForceClientRender = /*            */ 0b00000000000000000010000000;
 export const Ref = /*                          */ 0b00000000000000000100000000;
 export const Snapshot = /*                     */ 0b00000000000000001000000000;
 export const Passive = /*                      */ 0b00000000000000010000000000;
-export const Hydrating = /*                    */ 0b00000000000000100000000000;
 export const Visibility = /*                   */ 0b00000000000001000000000000;
 export const StoreConsistency = /*             */ 0b00000000000010000000000000;
 
