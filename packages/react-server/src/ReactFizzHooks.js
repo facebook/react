c40c780f9323b5d017f7d537d9bbb44281b50e92
Andrew Clark <git@andrewclark.io>
2022-12-05 17:22:13 -0500

    Revert "Revert "experimental_use(context) for server components and ssr (#25226)""
    This reverts commit 0dfb986763b417fdfc1c67af81e4c2acc2faec84.
    
    This reverts commit 3613284dceac38de4ef5a7c93c4897600ee6923b.
    
    implements the experimental use(context) API for the server components (Flight) and SSR (Fizz) runtimes

--- packages/react-server/src/ReactFizzHooks.js
+++ packages/react-server/src/ReactFizzHooks.js
@@ -40,7 +40,11 @@ import {
   enableUseMemoCacheHook,
 } from 'shared/ReactFeatureFlags';
 import is from 'shared/objectIs';
-import {REACT_MEMO_CACHE_SENTINEL} from 'shared/ReactSymbols';
+import {
+  REACT_SERVER_CONTEXT_TYPE,
+  REACT_CONTEXT_TYPE,
+  REACT_MEMO_CACHE_SENTINEL,
+} from 'shared/ReactSymbols';
 
 type BasicStateAction<S> = (S => S) | S;
 type Dispatch<A> = A => void;
@@ -644,8 +648,12 @@ function use<T>(usable: Usable<T>): T {
           }
         }
       }
-    } else {
-      // TODO: Add support for Context
+    } else if (
+      usable.$$typeof === REACT_CONTEXT_TYPE ||
+      usable.$$typeof === REACT_SERVER_CONTEXT_TYPE
+    ) {
+      const context: ReactContext<T> = (usable: any);
+      return readContext(context);
     }
   }
 
