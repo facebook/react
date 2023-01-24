0647372813bbd3d12b3d1bc9f19d582431e8bd0e
Faruk Munshi <informfaruk@gmail.com>
2023-01-24 16:09:31 +0530

    remove type annotation from param

--- packages/react-dom-bindings/src/shared/sanitizeURL.js
+++ packages/react-dom-bindings/src/shared/sanitizeURL.js
@@ -19,11 +19,12 @@ import {disableJavaScriptURLs} from 'shared/ReactFeatureFlags';
 // https://infra.spec.whatwg.org/#c0-control-or-space
 
 /* eslint-disable max-len */
-const isJavaScriptProtocol = /^[\u0000-\u001F ]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*\:/i;
+const isJavaScriptProtocol =
+  /^[\u0000-\u001F ]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*\:/i;
 
 let didWarn = false;
 
-function sanitizeURL(url: string) {
+function sanitizeURL(url) {
   if (disableJavaScriptURLs) {
     if (isJavaScriptProtocol.test(url)) {
       throw new Error(
