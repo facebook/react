4da145043f7b2bb1f25a3c072f200e01f7fa143a
Jan Kassens <jkassens@meta.com>
2023-01-30 16:23:27 -0500

    fix lint error by removing flow from `invertObject`

--- scripts/error-codes/invertObject.js
+++ scripts/error-codes/invertObject.js
@@ -3,19 +3,16 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- *
- * @flow
  */
 'use strict';
 
-import type {ErrorMap} from './Types';
 /**
  * turns
  *   { 'MUCH ERROR': '0', 'SUCH WRONG': '1' }
  * into
  *   { 0: 'MUCH ERROR', 1: 'SUCH WRONG' }
  */
-function invertObject(targetObj /*: ErrorMap */) /*: ErrorMap */ {
+function invertObject(targetObj) {
   const result /*: {[string]: string} */ = {};
   const mapKeys = Object.keys(targetObj);
 
