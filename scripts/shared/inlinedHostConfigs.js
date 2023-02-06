ab493d98e4a1cb876ddb394ffe9d358ffc4a7e61
Sebastian Markbage <sebastian@calyptus.eu>
2023-02-06 14:34:59 -0500

    Rename "bun" config to "dom-bun"
    This configuration is specifically for rendering the dom packages with bun.

--- scripts/shared/inlinedHostConfigs.js
+++ scripts/shared/inlinedHostConfigs.js
@@ -46,7 +46,7 @@ module.exports = [
     isServerSupported: true,
   },
   {
-    shortName: 'bun',
+    shortName: 'dom-bun',
     entryPoints: ['react-dom', 'react-dom/src/server/ReactDOMFizzServerBun.js'],
     paths: [
       'react-dom',
