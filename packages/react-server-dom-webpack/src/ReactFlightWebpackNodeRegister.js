05a688066f075a386caf65436ce52bb2572ee900
Sebastian Markbage <sebastian@calyptus.eu>
2023-02-05 21:11:46 -0500

    Fix error message

--- packages/react-server-dom-webpack/src/ReactFlightWebpackNodeRegister.js
+++ packages/react-server-dom-webpack/src/ReactFlightWebpackNodeRegister.js
@@ -102,9 +102,9 @@ module.exports = function register() {
           target.default = Object.defineProperties(
             (function () {
               throw new Error(
-                `Attempted to call the default export of ${moduleId} from the server` +
+                `Attempted to call the default export of ${moduleId} from the server ` +
                   `but it's on the client. It's not possible to invoke a client function from ` +
-                  `the server, it can only be rendered as a Component or passed to props of a` +
+                  `the server, it can only be rendered as a Component or passed to props of a ` +
                   `Client Component.`,
               );
             }: any),
