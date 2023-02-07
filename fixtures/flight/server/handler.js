f0cf832e1d0c8544c36aa8b310960885a11a847c
Sebastian Markbåge <sebastian@calyptus.eu>
2023-02-07 12:09:29 -0500

    Update Flight Fixture to "use client" instead of .client.js (#26118)
    This updates the Flight fixture to support the new ESM loaders in newer
    versions of Node.js.
    
    It also uses native fetch since react-fetch is gone now. (This part
    requires Node 18 to run the fixture.)
    
    I also updated everything to use the `"use client"` convention instead
    of file name based convention.
    
    The biggest hack here is that the Webpack plugin now just writes every
    `.js` file in the manifest. This needs to be more scoped. In practice,
    this new convention effectively requires you to traverse the server
    graph first to find the actual used files. This is enough to at least
    run our own fixture though.
    
    I didn't update the "blocks" fixture.
    
    More details in each commit message.

--- fixtures/flight/server/handler.js
+++ fixtures/flight/server/handler.js
@@ -6,8 +6,8 @@ const {resolve} = require('path');
 const React = require('react');
 
 module.exports = function (req, res) {
-  // const m = require('../src/App.server.js');
-  import('../src/App.server.js').then(m => {
+  // const m = require('../src/App.js');
+  import('../src/App.js').then(m => {
     const dist = process.env.NODE_ENV === 'development' ? 'dist' : 'build';
     readFile(
       resolve(__dirname, `../${dist}/react-client-manifest.json`),
--- fixtures/flight/src/ShowMore.js
+++ fixtures/flight/src/ShowMore.js
@@ -1,3 +1,5 @@
+'use client';
+
 import * as React from 'react';
 
 import Container from './Container.js';
