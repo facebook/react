45e4c98fff3175f867759e06e57f40d92891b317
Sebastian Markbage <sebastian@calyptus.eu>
2023-02-17 14:44:26 -0500

    Update fixtures to use the folder convention of build-combined

--- fixtures/packaging/webpack/dev/config.js
+++ fixtures/packaging/webpack/dev/config.js
@@ -6,6 +6,6 @@ module.exports = {
     filename: 'output.js',
   },
   resolve: {
-    root: path.resolve('../../../../build/node_modules/'),
+    root: path.resolve('../../../../build/oss-experimental/'),
   },
 };
