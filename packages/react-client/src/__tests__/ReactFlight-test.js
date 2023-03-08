f9af3854d3a10bd7e8093de8004a655f0a90482e
eps1lon <silbermann.sebastian@gmail.com>
2023-03-08 21:36:16 +0100

    Add test for rendering undefined

--- packages/react-client/src/__tests__/ReactFlight-test.js
+++ packages/react-client/src/__tests__/ReactFlight-test.js
@@ -197,6 +197,22 @@ describe('ReactFlight', () => {
     expect(ReactNoop).toMatchRenderedOutput(<span>ABC</span>);
   });
 
+  it('can render undefined', async () => {
+    function Undefined() {
+      return undefined;
+    }
+
+    const model = <Undefined />;
+
+    const transport = ReactNoopFlightServer.render(model);
+
+    await act(async () => {
+      ReactNoop.render(await ReactNoopFlightClient.read(transport));
+    });
+
+    expect(ReactNoop).toMatchRenderedOutput(null);
+  });
+
   it('can render a lazy component as a shared component on the server', async () => {
     function SharedComponent({text}) {
       return (
