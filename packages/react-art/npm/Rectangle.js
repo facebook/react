5670ee478a9bdf295b6c6136767923a7cec048eb
Andrew Clark <git@andrewclark.io>
2023-03-11 17:34:31 -0500

    fix: rectangle.js

--- packages/react-art/npm/Rectangle.js
+++ packages/react-art/npm/Rectangle.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Copyright (c) Meta Platforms, Inc. & affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
