d2a176defd79826464a355e807ed85dfcfd13109
Rick Hanlon <rickhanlonii@fb.com>
2023-03-15 10:50:50 -0400

    Remove layout effect warning on the server

--- packages/react-server/src/ReactFizzHooks.js
+++ packages/react-server/src/ReactFizzHooks.js
@@ -440,23 +440,6 @@ function useRef<T>(initialValue: T): {current: T} {
   }
 }
 
-export function useLayoutEffect(
-  create: () => (() => void) | void,
-  inputs: Array<mixed> | void | null,
-) {
-  if (__DEV__) {
-    currentHookNameInDev = 'useLayoutEffect';
-    console.error(
-      'useLayoutEffect does nothing on the server, because its effect cannot ' +
-        "be encoded into the server renderer's output format. This will lead " +
-        'to a mismatch between the initial, non-hydrated UI and the intended ' +
-        'UI. To avoid this, useLayoutEffect should only be used in ' +
-        'components that render exclusively on the client. ' +
-        'See https://reactjs.org/link/uselayouteffect-ssr for common fixes.',
-    );
-  }
-}
-
 function dispatchAction<A>(
   componentIdentity: Object,
   queue: UpdateQueue<A>,
@@ -633,7 +616,7 @@ export const HooksDispatcher: Dispatcher = {
   useRef,
   useState,
   useInsertionEffect: noop,
-  useLayoutEffect,
+  useLayoutEffect: noop,
   useCallback,
   // useImperativeHandle is not run in the server environment
   useImperativeHandle: noop,
