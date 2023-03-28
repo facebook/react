5cbe6258bc436b1683080a6d978c27849f1d9a22
Mengdi Chen <mengdi@fb.com>
2023-03-28 16:31:34 -0400

    Remove unnecessary CIRCLE_CI_API_TOKEN checks (#26499)
    Token is not required for GET

--- scripts/release/shared-commands/download-build-artifacts.js
+++ scripts/release/shared-commands/download-build-artifacts.js
@@ -22,15 +22,9 @@ const run = async ({build, cwd, releaseChannel}) => {
   }
 
   // Download and extract artifact
-  const {CIRCLE_CI_API_TOKEN} = process.env;
-  if (CIRCLE_CI_API_TOKEN == null) {
-    throw new Error(
-      `Expected a CircleCI token to download artifacts, got ${CIRCLE_CI_API_TOKEN}`
-    );
-  }
   await exec(`rm -rf ./build`, {cwd});
   await exec(
-    `curl -L $(fwdproxy-config curl) ${buildArtifacts.url} -H "Circle-Token: ${CIRCLE_CI_API_TOKEN}" | tar -xvz`,
+    `curl -L $(fwdproxy-config curl) ${buildArtifacts.url} | tar -xvz`,
     {
       cwd,
     }
