d02e39a39285316fae4b9b8fee1e1c54e99ab4a4
Sebastian Markbage <sebastian@calyptus.eu>
2023-03-29 23:20:57 -0400

    Move ReactDOMFloat to react-dom/src/
    This is not really part of the bindings, it's more part of the package
    entry points. /shared/ is not really right neither because it's more
    like an isomorphic entry point and not some utility.

--- packages/react-dom/src/server/ReactDOMServerRenderingStub.js
+++ packages/react-dom/src/server/ReactDOMServerRenderingStub.js
@@ -7,12 +7,7 @@
  * @flow
  */
 
-export {
-  preinit,
-  preload,
-  preconnect,
-  prefetchDNS,
-} from 'react-dom-bindings/src/shared/ReactDOMFloat';
+export {preinit, preload, preconnect, prefetchDNS} from '../ReactDOMFloat';
 
 export function createPortal() {
   throw new Error(
