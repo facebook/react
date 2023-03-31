888874673f81c08d9c3cfd4a56e2e93fd728894c
Andrew Clark <git@andrewclark.io>
2023-03-31 15:35:48 -0400

    Allow transitions to interrupt Suspensey commits (#26531)
    I originally made it so that a Suspensey commit — i.e. a commit that's
    waiting for a stylesheet, image, or font to load before proceeding —
    could not be interrupted by transitions. My reasoning was that Suspensey
    commits always time out after a short interval, anyway, so if the
    incoming update isn't urgent, it's better to wait to commit the current
    frame instead of throwing it away.
    
    I don't think this rationale was correct, for a few reasons. There are
    some cases where we'll suspend for a longer duration, like stylesheets —
    it's nearly always a bad idea to show content before its styles have
    loaded, so we're going to be extend this timeout to be really long.
    
    But even in the case where the timeout is shorter, like fonts, if you
    get a new update, it's possible (even likely) that update will allow us
    to avoid showing a fallback, like by navigating to a different page. So
    we might as well try.
    
    The behavior now matches our behavior for interrupting a suspended
    render phase (i.e. `use`), which makes sense because they're not that
    conceptually different.

--- packages/react-reconciler/src/ReactFiberRootScheduler.js
+++ packages/react-reconciler/src/ReactFiberRootScheduler.js
@@ -18,7 +18,6 @@ import {
   SyncLane,
   getHighestPriorityLane,
   getNextLanes,
-  includesOnlyNonUrgentLanes,
   includesSyncLane,
   markStarvedLanesAsExpired,
 } from './ReactFiberLane';
@@ -292,14 +291,16 @@ function scheduleTaskForRootDuringMicrotask(
 
   const existingCallbackNode = root.callbackNode;
   if (
+    // Check if there's nothing to work on
     nextLanes === NoLanes ||
     // If this root is currently suspended and waiting for data to resolve, don't
     // schedule a task to render it. We'll either wait for a ping, or wait to
     // receive an update.
-    (isWorkLoopSuspendedOnData() && root === workInProgressRoot) ||
-    // We should only interrupt a pending commit if the new update
-    // is urgent.
-    (root.cancelPendingCommit !== null && includesOnlyNonUrgentLanes(nextLanes))
+    //
+    // Suspended render phase
+    (root === workInProgressRoot && isWorkLoopSuspendedOnData()) ||
+    // Suspended commit phase
+    root.cancelPendingCommit !== null
   ) {
     // Fast path: There's nothing to work on.
     if (existingCallbackNode !== null) {
