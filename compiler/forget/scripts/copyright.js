fd91191c9478ebb1ec3227a8d028515a40215338
Joe Savona <joesavona@fb.com>
2023-04-05 12:21:47 -0700

    Script to add/fix copyright
    Adds a script to automate adding/updating the copyright header to all 
    appropriate files. For now i've excluded fixture inputs, just because it would 
    impact fixture outputs too, but we can add them in a later PR if we want.

---
+++ compiler/forget/scripts/copyright.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+"use strict";
+
+const fs = require("fs");
+const glob = require("glob");
+
+const META_COPYRIGHT_COMMENT_BLOCK = `/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */`;
+
+const files = glob.sync("**/*.{js,ts,tsx,jsx}", {
+  ignore: [
+    "**/dist/**",
+    "**/node_modules/**",
+    "test262/**",
+    "**/__tests__/fixtures/**/*.js",
+  ],
+});
+
+const updatedFiles = new Map();
+let hasErrors = false;
+files.forEach((file) => {
+  try {
+    const result = processFile(file);
+    if (result != null) {
+      updatedFiles.set(file, result);
+    }
+  } catch (e) {
+    console.error(e);
+    hasErrors = true;
+  }
+});
+if (hasErrors) {
+  console.error("Update failed");
+  process.exit(1);
+} else {
+  for (const [file, source] of updatedFiles) {
+    fs.writeFileSync(file, source, "utf8");
+  }
+  console.log("Update complete");
+}
+
+function processFile(file) {
+  let source = fs.readFileSync(file, "utf8");
+
+  if (source.indexOf(META_COPYRIGHT_COMMENT_BLOCK) === 0) {
+    return null;
+  }
+  if (/^\/\*\*/.test(source)) {
+    source = source.replace(/\/\*\*[^\/]+\//, META_COPYRIGHT_COMMENT_BLOCK);
+  } else {
+    source = `${META_COPYRIGHT_COMMENT_BLOCK}\n\n${source}`;
+  }
+  return source;
+}
