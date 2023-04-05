592b911e7f37721ce4146af1314152bedcb85250
Joe Savona <joesavona@fb.com>
2023-04-05 15:47:40 -0700

    fix main

--- compiler/forget/packages/make-read-only-util/jest.config.js
+++ compiler/forget/packages/make-read-only-util/jest.config.js
@@ -3,7 +3,9 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- */dist/types').InitialOptionsTsJest} */
+ */
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
