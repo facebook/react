aa600e8f2b15152262523390af27b12d31e97f80
Sophie Alpert <git@sophiebits.com>
2023-04-14 17:41:48 -0700

    Fix escaping in ReactDOMInput code
    JSON.stringify isn't the right thing here. Luckily this doesn't look to have any security impact.

--- packages/react-dom-bindings/src/client/ReactFiberConfigDOM.js
+++ packages/react-dom-bindings/src/client/ReactFiberConfigDOM.js
@@ -107,6 +107,7 @@ import {
   getValueDescriptorExpectingObjectForWarning,
   getValueDescriptorExpectingEnumForWarning,
 } from '../shared/ReactDOMResourceValidation';
+import escapeSelectorAttributeValueInsideDoubleQuotes from './escapeSelectorAttributeValueInsideDoubleQuotes';
 
 export type Type = string;
 export type Props = {
@@ -2985,19 +2986,6 @@ export function mountHoistable(
 
 export function unmountHoistable(instance: Instance): void {
   (instance.parentNode: any).removeChild(instance);
-}
-
-// When passing user input into querySelector(All) the embedded string must not alter
-// the semantics of the query. This escape function is safe to use when we know the
-// provided value is going to be wrapped in double quotes as part of an attribute selector
-// Do not use it anywhere else
-// we escape double quotes and backslashes
-const escapeSelectorAttributeValueInsideDoubleQuotesRegex = /[\n\"\\]/g;
-function escapeSelectorAttributeValueInsideDoubleQuotes(value: string): string {
-  return value.replace(
-    escapeSelectorAttributeValueInsideDoubleQuotesRegex,
-    ch => '\\' + ch.charCodeAt(0).toString(16),
-  );
 }
 
 export function isHostHoistableType(
