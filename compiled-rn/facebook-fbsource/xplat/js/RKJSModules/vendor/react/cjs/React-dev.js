8a410d6c0eb56e2ca09e6743cf64e1bb45a1ae99
sophiebits <sophiebits@users.noreply.github.com>
2023-04-18 18:08:16 +0000

    Add assertions about <input> value dirty state (#26626)
    Since this is an observable behavior and is hard to think about, seems
    good to have tests for this.
    
    The expected value included in each test is the behavior that existed
    prior to #26546.
    
    DiffTrain build for commit https://github.com/facebook/react/commit/1b4a0daba8aba71f6fbf0c3b88633c51207a6ec7.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
@@ -27,7 +27,7 @@ if (
 }
           "use strict";
 
-var ReactVersion = "18.3.0-next-b433c379d-20230418";
+var ReactVersion = "18.3.0-next-1b4a0daba-20230418";
 
 // ATTENTION
 // When adding new symbols to this file,
--- compiled-rn/facebook-fbsource/xplat/js/react-native-github/Libraries/Renderer/implementations/ReactFabric-dev.fb.js
+++ compiled-rn/facebook-fbsource/xplat/js/react-native-github/Libraries/Renderer/implementations/ReactFabric-dev.fb.js
@@ -27052,7 +27052,7 @@ function createFiberRoot(
   return root;
 }
 
-var ReactVersion = "18.3.0-next-b433c379d-20230418";
+var ReactVersion = "18.3.0-next-1b4a0daba-20230418";
 
 function createPortal$1(
   children,
