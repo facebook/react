a05d4b96a597eade48604717e36269e65ac98806
sophiebits <sophiebits@users.noreply.github.com>
2023-04-20 01:46:39 +0000

    Add two event system cleanup TODOs (#26678)
    There is so much old stuff in these files. I am weeping.
    
    DiffTrain build for commit https://github.com/facebook/react/commit/22d5942675fcbd8b15b532284b49db4cb00d7144.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
@@ -642,7 +642,7 @@ exports.useSyncExternalStore = function (
   );
 };
 exports.useTransition = useTransition;
-exports.version = "18.3.0-next-767f52237-20230419";
+exports.version = "18.3.0-next-22d594267-20230419";
 
           /* global __REACT_DEVTOOLS_GLOBAL_HOOK__ */
 if (
