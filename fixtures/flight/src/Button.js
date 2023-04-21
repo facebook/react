9cfd6968cb0f45ac2fafa40b3e5f2a57735bde7d
Andrew Clark <git@andrewclark.io>
2023-04-21 13:40:53 -0400

    Add error boundary to Flight fixture
    Errors in form actions are now rethrown during render (#26689), so we
    can handle the using an error boundary.

--- fixtures/flight/src/Button.js
+++ fixtures/flight/src/Button.js
@@ -1,27 +1,30 @@
 'use client';
 
 import * as React from 'react';
+import {flushSync} from 'react-dom';
+import ErrorBoundary from './ErrorBoundary.js';
 
 export default function Button({action, children}) {
   const [isPending, setIsPending] = React.useState(false);
 
   return (
-    <form>
-      <button
-        disabled={isPending}
-        formAction={async () => {
-          setIsPending(true);
-          try {
-            const result = await action();
-            console.log(result);
-          } catch (error) {
-            console.error(error);
-          } finally {
-            setIsPending(false);
-          }
-        }}>
-        {children}
-      </button>
-    </form>
+    <ErrorBoundary>
+      <form>
+        <button
+          disabled={isPending}
+          formAction={async () => {
+            // TODO: Migrate to useFormPending once that exists
+            flushSync(() => setIsPending(true));
+            try {
+              const result = await action();
+              console.log(result);
+            } finally {
+              React.startTransition(() => setIsPending(false));
+            }
+          }}>
+          {children}
+        </button>
+      </form>
+    </ErrorBoundary>
   );
 }
--- fixtures/flight/src/Form.js
+++ fixtures/flight/src/Form.js
@@ -1,25 +1,28 @@
 'use client';
 
 import * as React from 'react';
+import {flushSync} from 'react-dom';
+import ErrorBoundary from './ErrorBoundary.js';
 
 export default function Form({action, children}) {
   const [isPending, setIsPending] = React.useState(false);
 
   return (
-    <form
-      action={async formData => {
-        setIsPending(true);
-        try {
-          const result = await action(formData);
-          alert(result);
-        } catch (error) {
-          console.error(error);
-        } finally {
-          setIsPending(false);
-        }
-      }}>
-      <input name="name" />
-      <button>Say Hi</button>
-    </form>
+    <ErrorBoundary>
+      <form
+        action={async formData => {
+          // TODO: Migrate to useFormPending once that exists
+          flushSync(() => setIsPending(true));
+          try {
+            const result = await action(formData);
+            alert(result);
+          } finally {
+            React.startTransition(() => setIsPending(false));
+          }
+        }}>
+        <input name="name" />
+        <button>Say Hi</button>
+      </form>
+    </ErrorBoundary>
   );
 }
