49e9133a4b916ab966fb2e794cdaf0a3fc160632
kassens <kassens@users.noreply.github.com>
2023-05-25 17:43:19 +0000

    [flow] upgrade to 0.206.0 (#26850)
    Small upgrade. The one impact was deprecation of `$Shape` where it seems
    like we can just use a plain object with optional key.
    
    DiffTrain build for commit https://github.com/facebook/react/commit/18dedde6a7d63ffafd2f7e135f9edf424eee0bcd.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
@@ -27,7 +27,7 @@ if (
 }
           "use strict";
 
-var ReactVersion = "18.3.0-canary-9a72e6227-20230525";
+var ReactVersion = "18.3.0-canary-18dedde6a-20230525";
 
 // ATTENTION
 // When adding new symbols to this file,
--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
@@ -642,4 +642,4 @@ exports.useSyncExternalStore = function (
   );
 };
 exports.useTransition = useTransition;
-exports.version = "18.3.0-canary-9a72e6227-20230525";
+exports.version = "18.3.0-canary-18dedde6a-20230525";
