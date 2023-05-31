853b44c7f8bf6ddc82dff85fd8e208d2a1892f9c
Yasin Silavi <ys.silavi@gmail.com>
2023-05-31 19:43:32 +0330

    Combine if statement for hook call validation in development mode

--- packages/react/src/ReactHooks.js
+++ packages/react/src/ReactHooks.js
@@ -25,17 +25,15 @@ type Dispatch<A> = A => void;
 
 function resolveDispatcher() {
   const dispatcher = ReactCurrentDispatcher.current;
-  if (__DEV__) {
-    if (dispatcher === null) {
-      console.error(
-        'Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for' +
-          ' one of the following reasons:\n' +
-          '1. You might have mismatching versions of React and the renderer (such as React DOM)\n' +
-          '2. You might be breaking the Rules of Hooks\n' +
-          '3. You might have more than one copy of React in the same app\n' +
-          'See https://reactjs.org/link/invalid-hook-call for tips about how to debug and fix this problem.',
-      );
-    }
+  if (__DEV__ && dispatcher === null) {
+    console.error(
+      'Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for' +
+        ' one of the following reasons:\n' +
+        '1. You might have mismatching versions of React and the renderer (such as React DOM)\n' +
+        '2. You might be breaking the Rules of Hooks\n' +
+        '3. You might have more than one copy of React in the same app\n' +
+        'See https://reactjs.org/link/invalid-hook-call for tips about how to debug and fix this problem.',
+    );
   }
   // Will result in a null access error if accessed outside render phase. We
   // intentionally don't throw our own error because this is in a hot path.
