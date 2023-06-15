c416fb16ca427fd022e33372acd1e1f756f02c70
Lauren Tan <poteto@users.noreply.github.com>
2023-06-15 12:52:56 -0400

    [make-read-only] Make tsconfig more consistent with other pkgs

--- compiler/forget/packages/make-read-only-util/src/__tests__/makeReadOnly-test.ts
+++ compiler/forget/packages/make-read-only-util/src/__tests__/makeReadOnly-test.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import buildMakeReadOnly from "./makeReadOnly";
+import buildMakeReadOnly from "../makeReadOnly";
 
 describe("makeReadOnly", () => {
   let logger: jest.Func;
