71916b1a729edd62856ad45190309fb6ca5545f2
mbouchriha <mbouchriha31@gmail.com>
2023-07-13 09:43:23 +0100

    updte

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   plugins: [
-    '@babel/plugin-syntax-jsx',
     '@babel/plugin-transform-react-jsx',
     '@babel/plugin-transform-flow-strip-types',
     ['@babel/plugin-proposal-class-properties', {loose: true}],
