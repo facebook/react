28688ade9f1ce1c2ad435b7ae8951073445b58d7
Mofei Zhang <feifei0@meta.com>
2023-08-17 11:03:18 -0400

    [sprout] Add copyright headers etc

--- compiler/forget/packages/sprout/src/runner-worker.ts
+++ compiler/forget/packages/sprout/src/runner-worker.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import type { runReactForgetBabelPlugin as RunReactForgetBabelPlugin } from "babel-plugin-react-forget/src/Babel/RunReactForgetBabelPlugin";
 import {
   COMPILER_PATH,
