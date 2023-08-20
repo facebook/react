a5c698890706078624f2410ebbeafa8704f2f5ba
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-08-20 12:29:27 +0300

    Revert "Update transform-react-version-pragma.js"

--- scripts/babel/transform-react-version-pragma.js
+++ scripts/babel/transform-react-version-pragma.js
@@ -2,26 +2,30 @@
 
 /* eslint-disable no-for-of-loops/no-for-of-loops */
 
-// Import the getComments function from a separate module.
 const getComments = require('./getComments');
 
-// Define a constant for the string prefix used to identify version conditions in comments.
 const GATE_VERSION_STR = '@reactVersion ';
-
-// Get the REACT_VERSION environment variable.
 const REACT_VERSION_ENV = process.env.REACT_VERSION;
 
-// Define the main transformation function for the Babel plugin.
 function transform(babel) {
-  const { types: t } = babel;
+  const {types: t} = babel;
 
-  // This function processes comments to build a condition string based on a specific prefix.
+  // Runs tests conditionally based on the version of react (semver range) we are running
+  // Input:
+  //   @reactVersion >= 17.0
+  //   test('some test', () => {/*...*/})
+  //
+  // Output:
+  //    @reactVersion >= 17.0
+  //   _test_react_version('>= 17.0', 'some test', () => {/*...*/});
+  //
+  // See info about semver ranges here:
+  // https://www.npmjs.com/package/semver
   function buildGateVersionCondition(comments) {
     if (!comments) {
       return null;
     }
 
-    // Use the reduce method to accumulate conditions from comments.
     const resultingCondition = comments.reduce(
       (accumulatedCondition, commentLine) => {
         const commentStr = commentLine.value.trim();
@@ -30,10 +34,7 @@ function transform(babel) {
           return accumulatedCondition;
         }
 
-        // Extract the condition part of the comment after the prefix.
         const condition = commentStr.slice(GATE_VERSION_STR.length);
-
-        // If this is the first condition, return it; otherwise, concatenate with a space.
         if (accumulatedCondition === null) {
           return condition;
         }
@@ -43,7 +44,6 @@ function transform(babel) {
       null
     );
 
-    // If no condition was found, return null; otherwise, return it as a string literal.
     if (resultingCondition === null) {
       return null;
     }
@@ -51,18 +51,14 @@ function transform(babel) {
     return t.stringLiteral(resultingCondition);
   }
 
-  // Define the Babel plugin object with a name and a visitor that processes ExpressionStatements.
   return {
     name: 'transform-react-version-pragma',
     visitor: {
       ExpressionStatement(path) {
         const statement = path.node;
         const expression = statement.expression;
-
         if (expression.type === 'CallExpression') {
           const callee = expression.callee;
-
-          // Check if the callee is an Identifier (test, it, fit).
           switch (callee.type) {
             case 'Identifier': {
               if (
@@ -70,13 +66,8 @@ function transform(babel) {
                 callee.name === 'it' ||
                 callee.name === 'fit'
               ) {
-                // Get comments associated with the current expression.
                 const comments = getComments(path);
-
-                // Build the condition based on comments.
                 const condition = buildGateVersionCondition(comments);
-
-                // Modify the callee and arguments based on the condition.
                 if (condition !== null) {
                   callee.name =
                     callee.name === 'fit'
@@ -97,13 +88,8 @@ function transform(babel) {
                 callee.property.type === 'Identifier' &&
                 callee.property.name === 'only'
               ) {
-                // Get comments associated with the current expression.
                 const comments = getComments(path);
-
-                // Build the condition based on comments.
                 const condition = buildGateVersionCondition(comments);
-
-                // Modify the expression to use the appropriate function based on the condition.
                 if (condition !== null) {
                   statement.expression = t.callExpression(
                     t.identifier('_test_react_version_focus'),
@@ -126,6 +112,4 @@ function transform(babel) {
   };
 }
 
-// Export the transform function as the module's main export.
 module.exports = transform;
-
