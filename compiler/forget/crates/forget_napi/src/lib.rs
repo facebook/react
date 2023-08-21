9c13e4413c270da3acfbe0956a71fb90fc6c8475
Joe Savona <joesavona@fb.com>
2023-08-21 16:04:03 -0700

    [rust] Expose parse function to napi
    Updates the `forget_napi` crate to call the parser and semantic analyzer and 
    convert their values to JS.

--- compiler/forget/crates/forget_napi/src/lib.rs
+++ compiler/forget/crates/forget_napi/src/lib.rs
@@ -1,17 +1,115 @@
+use forget_diagnostics::Diagnostic;
+use forget_semantic_analysis::{analyze, AnalyzeOptions};
 use napi_derive::napi;
 
+pub const GLOBALS: &[&str] = &[
+    "AggregateError",
+    "Array",
+    "ArrayBuffer",
+    "AsyncFunction",
+    "AsyncGenerator",
+    "AsyncGeneratorFunction",
+    "AsyncIterator",
+    "Atomics",
+    "BigInt",
+    "BigInt64Array",
+    "BigUint64Array",
+    "Boolean",
+    "DataView",
+    "Date",
+    "decodeURI",
+    "decodeURIComponent",
+    "encodeURI",
+    "encodeURIComponent",
+    "Error",
+    "escape",
+    "eval",
+    "EvalError",
+    "FinalizationRegistry",
+    "Float32Array",
+    "Float64Array",
+    "Function",
+    "Generator",
+    "GeneratorFunction",
+    "globalThis",
+    "Infinity",
+    "Int16Array",
+    "Int32Array",
+    "Int8Array",
+    // "InternalError",  // non-standard
+    "Intl",
+    "isFinite",
+    "isNaN",
+    "Iterator",
+    "JSON",
+    "Map",
+    "Math",
+    "NaN",
+    "Number",
+    "Object",
+    "parseFloat",
+    "parseInt",
+    "Promise",
+    "Proxy",
+    "RangeError",
+    "ReferenceError",
+    "Reflect",
+    "RegExp",
+    "Set",
+    "SharedArrayBuffer",
+    "String",
+    "Symbol",
+    "SyntaxError",
+    "TypeError",
+    "Uint16Array",
+    "Uint32Array",
+    "Uint8Array",
+    "Uint8ClampedArray",
+    "undefined",
+    "unescape",
+    "URIError",
+    "WeakMap",
+    "WeakRef",
+    "WeakSet",
+];
+
 #[napi]
-pub fn add(left: i32, right: i32) -> i32 {
-    left + right
+pub fn parse(source: String, options: ParseOptions) -> ParseResult {
+    let program = match forget_hermes_parser::parse(&source, &options.file) {
+        Ok(program) => program,
+        Err(diagnostics) => {
+            return ParseResult {
+                program: None,
+                diagnostics: convert_diagnostics(diagnostics),
+            };
+        }
+    };
+    let mut analysis = analyze(
+        &program,
+        AnalyzeOptions {
+            globals: GLOBALS.iter().map(|s| s.to_string()).collect(),
+        },
+    );
+    ParseResult {
+        program: Some(serde_json::to_string(&program).unwrap()),
+        diagnostics: convert_diagnostics(analysis.diagnostics()),
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn convert_diagnostics(diagnostics: Vec<Diagnostic>) -> Vec<String> {
+    diagnostics
+        .into_iter()
+        .map(|diagnostic| format!("{}", diagnostic))
+        .collect()
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[napi(object)]
+pub struct ParseOptions {
+    pub file: String,
+}
+
+#[napi(object)]
+pub struct ParseResult {
+    pub program: Option<String>,
+    pub diagnostics: Vec<String>,
 }
