db45991c33ab1988952fc6594a8e8b2a33ab808d
Josh Story <story@hey.com>
2023-08-25 08:39:13 -0700

    Implements react-server-dom-turbopack

---
+++ packages/react-server-dom-turbopack/src/ReactFlightClientConfigTargetTurbopackBrowser.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+export type ModuleLoading = null;
+
+export function prepareDestinationWithChunks(
+  moduleLoading: ModuleLoading,
+  chunks: mixed,
+) {
+  // In the browser we don't need to prepare our destination since the browser is the Destination
+}
---
+++ packages/react-server-dom-turbopack/src/ReactFlightDOMClientNode.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import type {Thenable} from 'shared/ReactTypes.js';
+
+import type {Response} from 'react-client/src/ReactFlightClient';
+
+import type {
+  SSRManifest,
+  ModuleLoading,
+} from 'react-client/src/ReactFlightClientConfig';
+
+import type {Readable} from 'stream';
+
+import {
+  createResponse,
+  getRoot,
+  reportGlobalError,
+  processBinaryChunk,
+  close,
+} from 'react-client/src/ReactFlightClient';
+
+import {createServerReference as createServerReferenceImpl} from 'react-client/src/ReactFlightReplyClient';
+
+export type Options = {
+  moduleLoading?: $NonMaybeType<ModuleLoading>,
+};
+
+function noServerCall() {
+  throw new Error(
+    'Server Functions cannot be called during initial render. ' +
+      'This would create a fetch waterfall. Try to use a Server Component ' +
+      'to pass data to Client Components instead.',
+  );
+}
+
+export function createServerReference<A: Iterable<any>, T>(
+  id: any,
+  callServer: any,
+): (...A) => Promise<T> {
+  return createServerReferenceImpl(id, noServerCall);
+}
+
+function createFromNodeStream<T>(
+  stream: Readable,
+  moduleMap: $NonMaybeType<SSRManifest>,
+  options?: Options,
+): Thenable<T> {
+  const moduleLoading =
+    options && options.moduleLoading ? options.moduleLoading : null;
+  const response: Response = createResponse(
+    moduleMap,
+    moduleLoading,
+    noServerCall,
+  );
+  stream.on('data', chunk => {
+    processBinaryChunk(response, chunk);
+  });
+  stream.on('error', error => {
+    reportGlobalError(response, error);
+  });
+  stream.on('end', () => close(response));
+  return getRoot(response);
+}
+
+export {createFromNodeStream};
