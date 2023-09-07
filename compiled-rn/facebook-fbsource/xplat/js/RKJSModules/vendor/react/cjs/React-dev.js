e19570a39f20ad65b8ee987a61f5aa90866b6d7d
acdlite <acdlite@users.noreply.github.com>
2023-09-07 20:10:49 +0000

    useFormState: Emit comment to mark whether state matches (#27307)
    A planned feature of useFormState is that if the page load is the result
    of an MPA-style form submission — i.e. a form was submitted before it
    was hydrated, using Server Actions — the state of the hook should
    transfer to the next page.
    
    I haven't implemented that part yet, but as a prerequisite, we need some
    way for Fizz to indicate whether a useFormState hook was rendered using
    the "postback" state. That way we can do all state matching logic on the
    server without having to replicate it on the client, too.
    
    The approach here is to emit a comment node for each useFormState hook.
    We use one of two comment types: `<!--F-->` for a normal useFormState
    hook, and `<!--F!-->` for a hook that was rendered using the postback
    state. React will read these markers during hydration. This is similar
    to how we encode Suspense boundaries.
    
    Again, the actual matching algorithm is not yet implemented — for now,
    the "not matching" marker is always emitted.
    
    We can optimize this further by not emitting any markers for a render
    that is not the result of a form postback, which I'll do in subsequent
    PRs.
    
    DiffTrain build for commit https://github.com/facebook/react/commit/8b26f07a883bb341c20283c0099bf5ee6f87bd1f.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
@@ -27,7 +27,7 @@ if (
 }
           "use strict";
 
-var ReactVersion = "18.3.0-canary-3566de59e-20230907";
+var ReactVersion = "18.3.0-canary-8b26f07a8-20230907";
 
 // ATTENTION
 // When adding new symbols to this file,
--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
@@ -619,7 +619,7 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-3566de59e-20230907";
+exports.version = "18.3.0-canary-8b26f07a8-20230907";
 
           /* global __REACT_DEVTOOLS_GLOBAL_HOOK__ */
 if (
