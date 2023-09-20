bdfa4e69a253c5e0559f09f0a8e76ec0f9f9fe8c
acdlite <acdlite@users.noreply.github.com>
2023-09-20 21:18:00 +0000

    useFormState: Hash the component key path for more compact output (#27397)
    To support MPA-style form submissions, useFormState sends down a key
    that represents the identity of the hook on the page. It's based on the
    key path of the component within the React tree; for deeply nested
    hooks, this keypath can become very long. We can hash the key to make it
    shorter.
    
    Adds a method called createFastHash to the Stream Config interface.
    We're not using this for security or obfuscation, only to generate a
    more compact key without sacrificing too much collision resistance.
    
    - In Node.js builds, createFastHash uses the built-in crypto module.
    - In Bun builds, createFastHash uses Bun.hash. See:
    https://bun.sh/docs/api/hashing#bun-hash
    
    I have not yet implemented createFastHash in the Edge, Browser, or FB
    (Hermes) stream configs because those environments do not have a
    built-in hashing function that meets our requirements. (We can't use the
    web standard `crypto` API because those methods are async, and yielding
    to the main thread is too costly to be worth it for this particular use
    case.) We'll likely use a pure JS implementation in those environments;
    for now, they just return the original string without hashing it. I'll
    address this in separate PRs.
    
    DiffTrain build for commit https://github.com/facebook/react/commit/2b3d5826836ac59f8446281976762d594e55d97e.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
@@ -23998,7 +23998,7 @@ function createFiberRoot(
   return root;
 }
 
-var ReactVersion = "18.3.0-canary-1f4936660-20230920";
+var ReactVersion = "18.3.0-canary-2b3d58268-20230920";
 
 // Might add PROFILE later.
 
--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
@@ -619,7 +619,7 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-1f4936660-20230920";
+exports.version = "18.3.0-canary-2b3d58268-20230920";
 
           /* global __REACT_DEVTOOLS_GLOBAL_HOOK__ */
 if (
