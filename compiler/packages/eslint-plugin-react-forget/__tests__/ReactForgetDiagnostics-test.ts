d49eb8b580b535ea234275e10aa166cae461cd74
Lauren Tan <poteto@users.noreply.github.com>
2023-11-02 12:43:35 -0400

    [ez] Run copyright script

--- compiler/packages/eslint-plugin-react-forget/__tests__/ReactForgetDiagnostics-test.ts
+++ compiler/packages/eslint-plugin-react-forget/__tests__/ReactForgetDiagnostics-test.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import BabelESLintParser from "@babel/eslint-parser";
 import { RuleTester as ESLintTester } from "eslint";
 import ReactForgetDiagnostics from "../src/rules/ReactForgetDiagnostics";
