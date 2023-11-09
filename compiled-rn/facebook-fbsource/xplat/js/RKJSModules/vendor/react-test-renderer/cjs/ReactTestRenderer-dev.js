d74c9b4ab81c4a9ba360157f819e0b5762928803
gnoff <gnoff@users.noreply.github.com>
2023-11-09 01:56:20 +0000

    [Static][Fizz] bootstrap scripts should only emit once (#27674)
    I introduced a bug in a recent change to how bootstrap scripts are
    handled. Rather than clearing out the bootstrap script state from
    ResumableState on completion of the prerender I did it during the
    flushing phase which comes later after the postponed state has likely
    been serialized. We should freeze these objects in dev so this is not
    possible to do easily in test (nor in actual code in real systems).
    
    This fixes the bug by eliminating the bootstrap config during
    getPostponedState which is before the state can be serialized.
    
    DiffTrain build for commit https://github.com/facebook/react/commit/746890329452cbec8685eb3466847c5f17d9dc77.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
@@ -25356,7 +25356,7 @@ if (__DEV__) {
       return root;
     }
 
-    var ReactVersion = "18.3.0-canary-7508dcd5c-20231108";
+    var ReactVersion = "18.3.0-canary-746890329-20231108";
 
     // Might add PROFILE later.
 
