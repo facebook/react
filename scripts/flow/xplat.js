e9c785ae8dbdbbe43eca55dfc05166fa1e4eca25
Jan Kassens <jkassens@meta.com>
2023-11-22 10:45:08 -0500

    Add dynamic `disableModulePatternComponents` flag for native-fb

--- scripts/flow/xplat.js
+++ scripts/flow/xplat.js
@@ -8,10 +8,11 @@
  */
 
 declare module 'ReactNativeInternalFeatureFlags' {
-  declare export var enableUseRefAccessWarning: boolean;
+  declare export var alwaysThrottleRetries: boolean;
+  declare export var disableModulePatternComponents: boolean;
   declare export var enableDeferRootSchedulingToMicrotask: boolean;
   declare export var enableUnifiedSyncLane: boolean;
-  declare export var alwaysThrottleRetries: boolean;
-  declare export var useMicrotasksForSchedulingInFabric: boolean;
+  declare export var enableUseRefAccessWarning: boolean;
   declare export var passChildrenWhenCloningPersistedNodes: boolean;
+  declare export var useMicrotasksForSchedulingInFabric: boolean;
 }
