50ae73c513521e9aaed0c193b1796731a11dcf80
wasimtikki120 <wasimraja1231wlk@gmail.com>
2023-11-28 16:46:32 +0530

    devtool not in codesandbox

--- packages/react-devtools-shell/src/app/devtools.js
+++ packages/react-devtools-shell/src/app/devtools.js
@@ -1,43 +1,39 @@
 /** @flow */
 
-import {createElement} from 'react';
-import {createRoot} from 'react-dom/client';
+import { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
 import {
   activate as activateBackend,
   initialize as initializeBackend,
 } from 'react-devtools-inline/backend';
-import {initialize as initializeFrontend} from 'react-devtools-inline/frontend';
-import {initDevTools} from 'react-devtools-shared/src/devtools';
+import { initialize as initializeFrontend } from 'react-devtools-inline/frontend';
+import { initDevTools } from 'react-devtools-shared/src/devtools';
 
-// This is a pretty gross hack to make the runtime loaded named-hooks-code work.
-// TODO (Webpack 5) Hoepfully we can remove this once we upgrade to Webpack 5.
+// Set the Webpack public path
+// Adjust the path based on your project structure
 // $FlowFixMe[cannot-resolve-name]
-__webpack_public_path__ = '/dist/'; // eslint-disable-line no-undef
+__webpack_public_path__ = '/dist/';
 
 const iframe = ((document.getElementById('target'): any): HTMLIFrameElement);
 
-const {contentDocument, contentWindow} = iframe;
+const { contentDocument, contentWindow } = iframe;
 
 // Helps with positioning Overlay UI.
 contentWindow.__REACT_DEVTOOLS_TARGET_WINDOW__ = window;
 
 initializeBackend(contentWindow);
 
-// Initialize the front end and activate the backend early so that we are able
-// to pass console settings in local storage to the backend before initial render
+// Initialize the front end and activate the backend early
 const DevTools = initializeFrontend(contentWindow);
 
 // Activate the backend only once the DevTools frontend Store has been initialized.
-// Otherwise the Store may miss important initial tree op codes.
 activateBackend(contentWindow);
 
 const container = ((document.getElementById('devtools'): any): HTMLElement);
 
 let isTestAppMounted = true;
 
-const mountButton = ((document.getElementById(
-  'mountButton',
-): any): HTMLButtonElement);
+const mountButton = ((document.getElementById('mountButton'): any): HTMLButtonElement);
 mountButton.addEventListener('click', function () {
   if (isTestAppMounted) {
     if (typeof window.unmountTestApp === 'function') {
@@ -54,33 +50,12 @@ mountButton.addEventListener('click', function () {
   }
 });
 
-// TODO (Webpack 5) Hopefully we can remove this prop after the Webpack 5 migration.
+// Dynamic import function for hookNames module
 function hookNamesModuleLoaderFunction() {
   return import('react-devtools-inline/hookNames');
 }
 
-inject('dist/app-index.js', () => {
-  initDevTools({
-    connect(cb) {
-      const root = createRoot(container);
-      root.render(
-        createElement(DevTools, {
-          browserTheme: 'light',
-          enabledInspectedElementContextMenu: true,
-          hookNamesModuleLoaderFunction,
-          showTabBar: true,
-          warnIfLegacyBackendDetected: true,
-          warnIfUnsupportedVersionDetected: true,
-        }),
-      );
-    },
-
-    onReload(reloadFn) {
-      iframe.onload = reloadFn;
-    },
-  });
-});
-
+// Inject the specified script into the content document
 function inject(sourcePath: string, callback: () => void) {
   const script = contentDocument.createElement('script');
   script.onload = callback;
@@ -88,3 +63,29 @@ function inject(sourcePath: string, callback: () => void) {
 
   ((contentDocument.body: any): HTMLBodyElement).appendChild(script);
 }
+
+// DevTools initialization
+initDevTools({
+  connect(cb) {
+    const root = createRoot(container);
+    root.render(
+      createElement(DevTools, {
+        browserTheme: 'light',
+        enabledInspectedElementContextMenu: true,
+        hookNamesModuleLoaderFunction,
+        showTabBar: true,
+        warnIfLegacyBackendDetected: true,
+        warnIfUnsupportedVersionDetected: true,
+      }),
+    );
+  },
+
+  onReload(reloadFn) {
+    iframe.onload = reloadFn;
+  },
+});
+
+// Handle injection of the specified script
+inject('dist/app-index.js', () => {
+  // Code to execute after script injection, if needed
+});
