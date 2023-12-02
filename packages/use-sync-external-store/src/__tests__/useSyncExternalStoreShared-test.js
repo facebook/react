e6ed8ab5cead89b53c980a51cf73e2bf4fde3883
Alexander Danilov <d.gentlee@gmail.com>
2023-12-02 04:33:23 +0300

    add test that fails

--- packages/use-sync-external-store/src/__tests__/useSyncExternalStoreShared-test.js
+++ packages/use-sync-external-store/src/__tests__/useSyncExternalStoreShared-test.js
@@ -624,6 +624,46 @@ describe('Shared useSyncExternalStore behavior (shim and built-in)', () => {
     expect(container.textContent).toEqual('NaN');
   });
 
+  test('selector is not called when snaphot has not changed', async () => {
+    const store = createExternalStore({a: 0});
+
+    function selector(state) {
+      Scheduler.log('Selector');
+      return state.a;
+    }
+
+    function isEqual(a, b) {
+      return a === b;
+    }
+
+    function App() {
+      Scheduler.log('App');
+      const a = useSyncExternalStoreWithSelector(
+        store.subscribe,
+        store.getState,
+        null,
+        selector,
+        isEqual,
+      );
+      return <Text text={'A' + a} />;
+    }
+
+    const container = document.createElement('div');
+    const root = createRoot(container);
+    await act(() => root.render(<App />));
+
+    assertLog(['App', 'Selector', 'A0']);
+    expect(container.textContent).toEqual('A0');
+
+    await act(() => store.set(store.getState()));
+    await act(() => store.set({a: 0}));
+    assertLog(['Selector']);
+
+    await act(() => store.set(store.getState()));
+    await act(() => store.set(store.getState()));
+    assertLog([]);
+  });
+
   describe('extra features implemented in user-space', () => {
     // The selector implementation uses the lazy ref initialization pattern
     // @gate !(enableUseRefAccessWarning && __DEV__)
