8b36d2c7b9ef9ebdc94c9fc73195c7ecdbfe4537
Josh Story <story@hey.com>
2023-12-06 09:49:37 -0800

    We are going to remove test-utils and this change moves references to act from this entrypoint to react

--- packages/react-refresh/src/__tests__/ReactFreshIntegration-test.js
+++ packages/react-refresh/src/__tests__/ReactFreshIntegration-test.js
@@ -31,7 +31,7 @@ describe('ReactFreshIntegration', () => {
       ReactFreshRuntime = require('react-refresh/runtime');
       ReactFreshRuntime.injectIntoGlobalHook(global);
       ReactDOM = require('react-dom');
-      act = require('react-dom/test-utils').act;
+      act = React.unstable_act;
       container = document.createElement('div');
       document.body.appendChild(container);
       exportsObj = undefined;
