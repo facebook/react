a1635091f2369dce0934e3eeb894d295220e6312
kassens <kassens@users.noreply.github.com>
2023-12-08 18:49:06 +0000

    Add header to ReactAllWarnings Meta-only file (#27819)
    Adds missing headers to the generated file.
    
    DiffTrain build for [8ff2c236a233a78c18d6f524dd74baec9153fd37](https://github.com/facebook/react/commit/8ff2c236a233a78c18d6f524dd74baec9153fd37)

--- compiled/facebook-www/__test_utils__/ReactAllWarnings.js
+++ compiled/facebook-www/__test_utils__/ReactAllWarnings.js
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ * @noformat
+ * @oncall react_core
+ */
+
 export default [
   "\"%s\" is not a supported revealOrder on <SuspenseList />. Did you mean \"together\", \"forwards\" or \"backwards\"?",
   "\"%s\" is not a supported value for tail on <SuspenseList />. Did you mean \"collapsed\" or \"hidden\"?",
