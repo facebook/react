a985119f5d3c44746b7af877ef5a3599ec31dce9
eps1lon <eps1lon@users.noreply.github.com>
2024-01-19 17:28:10 +0000

    Convert ReactFreshMultipleRenderer to createRoot (#28000)
    DiffTrain build for commit https://github.com/facebook/react/commit/4c58fc2ad8f1055f9b543147b283e5a3f795fd27.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
@@ -539,4 +539,4 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-601dba821-20240119";
+exports.version = "18.3.0-canary-4c58fc2ad-20240119";
