2000d9fded2dc0246ccebb702705b269753a77d7
eps1lon <eps1lon@users.noreply.github.com>
2024-01-22 14:13:26 +0000

    Convert describeComponentFrame to createRoot (#28001)
    DiffTrain build for commit https://github.com/facebook/react/commit/e1d20fc0c0b671601389b8c48a84a38e9c535654.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
@@ -25540,7 +25540,7 @@ if (__DEV__) {
       return root;
     }
 
-    var ReactVersion = "18.3.0-canary-206934f02-20240122";
+    var ReactVersion = "18.3.0-canary-e1d20fc0c-20240122";
 
     // Might add PROFILE later.
 
