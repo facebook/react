b446d3cf927b398c69224c8c4a8c887073f48771
jackpope <jackpope@users.noreply.github.com>
2024-01-26 22:22:16 +0000

    Use createRoot in ReactTestUtilsActUnmockedScheduler-test (#28086)
    DiffTrain build for commit https://github.com/facebook/react/commit/38997cf19ac5223a7c748ba975b40d649b9c4c52.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
@@ -24,7 +24,7 @@ if (__DEV__) {
     ) {
       __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStart(new Error());
     }
-    var ReactVersion = "18.3.0-canary-766eac46b-20240126";
+    var ReactVersion = "18.3.0-canary-38997cf19-20240126";
 
     // ATTENTION
     // When adding new symbols to this file,
