a26934da59e7fa981d93d9a53053b4782fa34e0d
Ricky Hanlon <rickhanlonii@gmail.com>
2024-01-27 19:22:11 -0500

    Add ReactDOMClient to ServerIntegrationReconnecting

--- packages/react-dom/src/__tests__/ReactDOMServerIntegrationReconnecting-test.js
+++ packages/react-dom/src/__tests__/ReactDOMServerIntegrationReconnecting-test.js
@@ -12,7 +12,7 @@
 const ReactDOMServerIntegrationUtils = require('./utils/ReactDOMServerIntegrationTestUtils');
 
 let React;
-let ReactDOM;
+let ReactDOMClient;
 let ReactDOMServer;
 let ReactTestUtils;
 
@@ -21,13 +21,13 @@ function initModules() {
   jest.resetModules();
 
   React = require('react');
-  ReactDOM = require('react-dom');
+  ReactDOMClient = require('react-dom/client');
   ReactDOMServer = require('react-dom/server');
   ReactTestUtils = require('react-dom/test-utils');
 
   // Make them available to the helpers.
   return {
-    ReactDOM,
+    ReactDOMClient,
     ReactDOMServer,
     ReactTestUtils,
   };
@@ -123,8 +123,8 @@ describe('ReactDOMServerIntegration', () => {
       it('should error reconnecting different attribute values', () =>
         expectMarkupMismatch(<div id="foo" />, <div id="bar" />));
 
-      it('can explicitly ignore errors reconnecting different element types of children', () =>
-        expectMarkupMatch(
+      it('should error reconnecting different element types of children', () =>
+        expectMarkupMismatch(
           <div>
             <div />
           </div>,
@@ -354,8 +354,8 @@ describe('ReactDOMServerIntegration', () => {
           <div>{''}</div>,
         ));
 
-      it('can explicitly ignore reconnecting more children', () =>
-        expectMarkupMatch(
+      it('can not ignore reconnecting more children', () =>
+        expectMarkupMismatch(
           <div>
             <div />
           </div>,
@@ -365,8 +365,8 @@ describe('ReactDOMServerIntegration', () => {
           </div>,
         ));
 
-      it('can explicitly ignore reconnecting fewer children', () =>
-        expectMarkupMatch(
+      it('can not ignore reconnecting fewer children', () =>
+        expectMarkupMismatch(
           <div>
             <div />
             <div />
@@ -376,8 +376,8 @@ describe('ReactDOMServerIntegration', () => {
           </div>,
         ));
 
-      it('can explicitly ignore reconnecting reordered children', () =>
-        expectMarkupMatch(
+      it('can not ignore reconnecting reordered children', () =>
+        expectMarkupMismatch(
           <div suppressHydrationWarning={true}>
             <div />
             <span />
