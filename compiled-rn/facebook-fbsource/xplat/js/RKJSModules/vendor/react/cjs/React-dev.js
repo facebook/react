e5162748dd317757d9c8c17890da11ea66dde026
rickhanlonii <rickhanlonii@users.noreply.github.com>
2024-02-01 23:36:52 +0000

    Add ReactDOMClient to ServerIntegrationBasic (#28133)
    ## Overview
    
    Branched off https://github.com/facebook/react/pull/28130
    
    In `hydrateRoot`, we now error if you pass `undefined`:
    
    ```
    Warning: Must provide initial children as second argument to hydrateRoot.
    ```
    
    So we expect 1 error for this now.
    
    DiffTrain build for commit https://github.com/facebook/react/commit/278199b3deeee73f969a242c777c2d38406f8bfe.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
@@ -24,7 +24,7 @@ if (__DEV__) {
     ) {
       __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStart(new Error());
     }
-    var ReactVersion = "18.3.0-canary-c0d927713-20240201";
+    var ReactVersion = "18.3.0-canary-278199b3d-20240201";
 
     // ATTENTION
     // When adding new symbols to this file,
--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
@@ -541,7 +541,7 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-c0d927713-20240201";
+exports.version = "18.3.0-canary-278199b3d-20240201";
 "undefined" !== typeof __REACT_DEVTOOLS_GLOBAL_HOOK__ &&
   "function" ===
     typeof __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStop &&
