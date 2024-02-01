53b12e46a17549ec7644e13c126440ed2f3629fd
Andrew Clark <git@andrewclark.io>
2024-02-01 13:28:14 -0500

    Add stable React.act export (#28160)
    Starting in version 19, users can import the `act` testing API from the
    `react` package instead of using a renderer specific API, like
    `react-dom/test-utils`.

--- packages/react-server-dom-webpack/src/__tests__/ReactFlightDOMForm-test.js
+++ packages/react-server-dom-webpack/src/__tests__/ReactFlightDOMForm-test.js
@@ -54,7 +54,7 @@ describe('ReactFlightDOMForm', () => {
     ReactServerDOMClient = require('react-server-dom-webpack/client.edge');
     ReactDOMServer = require('react-dom/server.edge');
     ReactDOMClient = require('react-dom/client');
-    act = React.unstable_act;
+    act = React.act;
     useFormState = require('react-dom').useFormState;
     container = document.createElement('div');
     document.body.appendChild(container);
