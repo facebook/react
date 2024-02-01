b2e00b2d714fdbff121bc405c4f22a91189c428d
rickhanlonii <rickhanlonii@users.noreply.github.com>
2024-02-01 23:37:11 +0000

    Add ReactDOMClient to ServerIntegrationReconnecting (#28136)
    ## Overview
    
    Branched off https://github.com/facebook/react/pull/28130
    
    ## Why
    
    In https://github.com/facebook/react/pull/24276 we changed the new root
    behavior to error when a client-render is forced for certain cases, so
    these now expect a mismatch even though they're using
    `suppressHydrationWarning`.
    
    DiffTrain build for commit https://github.com/facebook/react/commit/4bd5e3ea5c449fdd2089f6bdfb07fb1120b0eec7.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
@@ -24,7 +24,7 @@ if (__DEV__) {
     ) {
       __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStart(new Error());
     }
-    var ReactVersion = "18.3.0-canary-fa6674b5b-20240201";
+    var ReactVersion = "18.3.0-canary-4bd5e3ea5-20240201";
 
     // ATTENTION
     // When adding new symbols to this file,
--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
@@ -541,7 +541,7 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-fa6674b5b-20240201";
+exports.version = "18.3.0-canary-4bd5e3ea5-20240201";
 "undefined" !== typeof __REACT_DEVTOOLS_GLOBAL_HOOK__ &&
   "function" ===
     typeof __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStop &&
