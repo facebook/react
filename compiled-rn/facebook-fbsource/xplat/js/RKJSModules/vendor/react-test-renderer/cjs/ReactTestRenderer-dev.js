a9212f840afda1c1ac23b83807e4b30c901b888a
eps1lon <eps1lon@users.noreply.github.com>
2024-02-02 08:23:26 +0000

    Convert createReactClassIntegration to createRoot (#28196)
    DiffTrain build for commit https://github.com/facebook/react/commit/02946228e1af3744878d964a58ea7cd55b563468.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
@@ -25622,7 +25622,7 @@ if (__DEV__) {
       return root;
     }
 
-    var ReactVersion = "18.3.0-canary-7026806d4-20240202";
+    var ReactVersion = "18.3.0-canary-02946228e-20240202";
 
     // Might add PROFILE later.
 
