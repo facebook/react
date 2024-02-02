a53d27f1ce089da8c2f99c344a9e9a38d4c0cba7
eps1lon <eps1lon@users.noreply.github.com>
2024-02-02 08:20:57 +0000

    Convert ReactCompositeComponentDOMMinimalism to createRoot (#28194)
    Not sure if this was also meant to test findDOMNode. But sounded like it
    was more interested in the rendering aspect and findDOMNode was just
    used as a utility. If we want to keep the findDOMNode tests, I'd just
    rename it to a legacy test to indicate it needs to be flagged.
    
    DiffTrain build for commit https://github.com/facebook/react/commit/c4e9ed3fefcd744b48e9cce39df0f52cbc014e1a.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
@@ -25622,7 +25622,7 @@ if (__DEV__) {
       return root;
     }
 
-    var ReactVersion = "18.3.0-canary-3cfe0cc0c-20240202";
+    var ReactVersion = "18.3.0-canary-c4e9ed3fe-20240202";
 
     // Might add PROFILE later.
 
