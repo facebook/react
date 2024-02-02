40a99355d35181d67dd30107d81de74c603fa1d6
eps1lon <eps1lon@users.noreply.github.com>
2024-02-02 08:18:36 +0000

    Remove usage of /test-utils in ReactLegacyCompositeComponent (#28201)
    DiffTrain build for commit https://github.com/facebook/react/commit/11aa263844cc90b3677782c9b8bafa3fe22fd389.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
@@ -24,7 +24,7 @@ if (__DEV__) {
     ) {
       __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStart(new Error());
     }
-    var ReactVersion = "18.3.0-canary-fa8a34bfa-20240202";
+    var ReactVersion = "18.3.0-canary-11aa26384-20240202";
 
     // ATTENTION
     // When adding new symbols to this file,
--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
@@ -545,4 +545,4 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-fa8a34bfa-20240202";
+exports.version = "18.3.0-canary-11aa26384-20240202";
