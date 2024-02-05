4e28831a36183609bbaf7b2ee1d72d1a88cc266f
hoxyq <hoxyq@users.noreply.github.com>
2024-02-05 17:22:00 +0000

    chore: use versioned render in profilerChangeDescriptions test (#28221)
    DiffTrain build for commit https://github.com/facebook/react/commit/761d5750ce51cec527a311a36190426fbc3470db.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
@@ -24,7 +24,7 @@ if (__DEV__) {
     ) {
       __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStart(new Error());
     }
-    var ReactVersion = "18.3.0-canary-4f82bf4f5-20240205";
+    var ReactVersion = "18.3.0-canary-761d5750c-20240205";
 
     // ATTENTION
     // When adding new symbols to this file,
--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
@@ -541,7 +541,7 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-4f82bf4f5-20240205";
+exports.version = "18.3.0-canary-761d5750c-20240205";
 "undefined" !== typeof __REACT_DEVTOOLS_GLOBAL_HOOK__ &&
   "function" ===
     typeof __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStop &&
