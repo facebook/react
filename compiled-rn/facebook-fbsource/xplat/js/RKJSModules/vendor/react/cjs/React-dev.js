11cce81d6a03448e6700c8cfee751cb7c8676b39
hoxyq <hoxyq@users.noreply.github.com>
2024-02-05 15:58:03 +0000

    chore: add versioned render implementation for DevTools tests (#28210)
    Adding getter-functions for renderer implementations, which can be used
    for jest tests. If we are testing against React with version < 18, we
    are going to use legacy rendering, otherwise the concurrent one.
    
    DiffTrain build for commit https://github.com/facebook/react/commit/596827f6a72f6c4df0181e941c524fcbd0048914.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-dev.js
@@ -24,7 +24,7 @@ if (__DEV__) {
     ) {
       __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStart(new Error());
     }
-    var ReactVersion = "18.3.0-canary-9a1db2d21-20240205";
+    var ReactVersion = "18.3.0-canary-596827f6a-20240205";
 
     // ATTENTION
     // When adding new symbols to this file,
