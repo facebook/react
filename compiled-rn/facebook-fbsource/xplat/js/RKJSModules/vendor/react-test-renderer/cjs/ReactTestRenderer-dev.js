571f0bd014afdcce7889b1d9ec8d102e24fd5c29
hoxyq <hoxyq@users.noreply.github.com>
2024-02-05 17:19:56 +0000

    chore: use versioned render in storeOwners test (#28215)
    DiffTrain build for commit https://github.com/facebook/react/commit/4f82bf4f59a91e81191f39fa328a3d26aba6371a.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
@@ -25622,7 +25622,7 @@ if (__DEV__) {
       return root;
     }
 
-    var ReactVersion = "18.3.0-canary-7e77e29ca-20240205";
+    var ReactVersion = "18.3.0-canary-4f82bf4f5-20240205";
 
     // Might add PROFILE later.
 
