138b6100c90a5567dc167d46ac0f2f4b71402a62
hoxyq <hoxyq@users.noreply.github.com>
2024-02-05 17:19:33 +0000

    chore: use versioned render in componentStacks test (#28214)
    DiffTrain build for commit https://github.com/facebook/react/commit/7e77e29ca93857b68bfc31bcd5cd566b33b7cd15.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
@@ -545,4 +545,4 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-09c076944-20240205";
+exports.version = "18.3.0-canary-7e77e29ca-20240205";
