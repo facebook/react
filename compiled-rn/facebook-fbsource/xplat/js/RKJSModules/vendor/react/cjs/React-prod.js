d6ea4c78a5d897bb73c94e7edf6c18620ee9955d
hoxyq <hoxyq@users.noreply.github.com>
2024-02-06 16:29:37 +0000

    chore: use versioned render in storeComponentFilters test (#28241)
    DiffTrain build for commit https://github.com/facebook/react/commit/12d56fca3da06328ac61d86976afa728b97b49d6.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
@@ -543,4 +543,4 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-ad720f36e-20240206";
+exports.version = "18.3.0-canary-12d56fca3-20240206";
