3076c11c70451f5945fb3af823e03525835e1efc
eps1lon <eps1lon@users.noreply.github.com>
2024-02-06 19:02:19 +0000

    [Fresh] Update the list of built-in hooks (#27864)
    DiffTrain build for commit https://github.com/facebook/react/commit/7cbd026ff4ff833db2a2f2271f95cb2b91cb3895.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
@@ -25725,7 +25725,7 @@ if (__DEV__) {
       return root;
     }
 
-    var ReactVersion = "18.3.0-canary-30e2938e0-20240206";
+    var ReactVersion = "18.3.0-canary-7cbd026ff-20240206";
 
     // Might add PROFILE later.
 
