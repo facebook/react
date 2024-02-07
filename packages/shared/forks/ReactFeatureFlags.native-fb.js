5bef02e7f43f070d273a5dacbb8263a22f4422f0
Tianyu Yao <skyyao@fb.com>
2024-02-07 15:32:20 -0800

    Revert "Remove revertRemovalOfSiblingPrerendering killswitch (#26549)"
    This reverts commit 7b0642bb989ec659c6c9891ea16daa0420caab4d.

--- packages/shared/forks/ReactFeatureFlags.native-fb.js
+++ packages/shared/forks/ReactFeatureFlags.native-fb.js
@@ -54,6 +54,7 @@ export const enableScopeAPI = false;
 export const enableCreateEventHandleAPI = false;
 export const enableSuspenseCallback = false;
 export const disableLegacyContext = false;
+export const revertRemovalOfSiblingPrerendering = false;
 export const enableTrustedTypesIntegration = false;
 export const disableTextareaChildren = false;
 export const enableSuspenseAvoidThisFallback = false;
