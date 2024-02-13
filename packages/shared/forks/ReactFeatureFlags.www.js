5d445b50a670d6cdc52943ca4e5ab3422792c57c
Ricky <rickhanlonii@gmail.com>
2024-02-13 11:48:56 -0500

    Land enableAsyncActions enableFormActions in www (#28315)

--- packages/shared/forks/ReactFeatureFlags.www.js
+++ packages/shared/forks/ReactFeatureFlags.www.js
@@ -27,8 +27,6 @@ export const {
   enableRetryLaneExpiration,
   enableTransitionTracing,
   enableDeferRootSchedulingToMicrotask,
-  enableAsyncActions,
-  enableFormActions,
   alwaysThrottleRetries,
   enableDO_NOT_USE_disableStrictPassiveEffect,
   disableSchedulerTimeoutInWorkLoop,
@@ -61,6 +59,8 @@ export const enableUseMemoCacheHook = true;
 export const enableUseEffectEventHook = true;
 export const enableClientRenderFallbackOnTextMismatch = false;
 export const enableFilterEmptyStringAttributesDOM = true;
+export const enableFormActions = true;
+export const enableAsyncActions = true;
 
 // Logs additional User Timing API marks for use with an experimental profiling tool.
 export const enableSchedulingProfiler: boolean =
