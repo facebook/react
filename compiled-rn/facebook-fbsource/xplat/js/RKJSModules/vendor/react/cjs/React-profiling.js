32643b0eb3e79f195fe86faed677a03eaa737a6d
jackpope <jackpope@users.noreply.github.com>
2024-02-16 20:22:44 +0000

    Add useModernStrictMode as dynamic flag on www (#28346)
    ## Summary
    
    Preparing modern strict mode rollout with dynamic feature flag
    
    ## How did you test this change?
    
    ![Screenshot 2024-02-15 at 10 09
    49 AM](https://github.com/facebook/react/assets/8965173/9e90efc2-3578-4e63-ae2c-63d4a4e194b3)
    
    DiffTrain build for commit https://github.com/facebook/react/commit/ef72271c2d1234c9d1e1358f8083021089a50faa.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-profiling.js
@@ -586,7 +586,7 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-a9cc32511-20240215";
+exports.version = "18.3.0-canary-ef72271c2-20240216";
 "undefined" !== typeof __REACT_DEVTOOLS_GLOBAL_HOOK__ &&
   "function" ===
     typeof __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStop &&
