1b3a35c614bbd865ba2f60a1654174fa00a1deb2
Matt Carroll <mattca@meta.com>
2024-02-15 23:48:05 -0800

    [DevTools] Add React Conf banner in component inspector panel

--- packages/react-devtools-extensions/src/panel.js
+++ packages/react-devtools-extensions/src/panel.js
@@ -17,3 +17,19 @@ window.injectStyles = getLinkTags => {
     }
   }
 };
+
+// Banner event listener to close the banner when "X" button is clicked
+const closeButton = document.querySelector('.dismissable-banner .close');
+closeButton.addEventListener('click', () => {
+  // Get the parent element of the "X" (the banner) and remove it
+  const banner = closeButton.parentElement;
+  banner.remove();
+});
+
+// Remove the banner if React Conf is over (after May 16, 2024)
+const currentDate = new Date();
+const may162024 = new Date('May 16, 2024');
+if (currentDate > may162024) {
+  const banner = document.querySelector('.dismissable-banner');
+  banner.remove();
+}
