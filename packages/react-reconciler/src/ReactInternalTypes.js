4597dc7baba704f8b7c4dad6b3d87e81ced27a32
bubucuo <bubucuo@users.noreply.github.com>
2024-02-21 02:02:09 +0800

    refactor: remove dead code of fiber
    The `nextEffect` property of the `fiber` is currently not being used and has become dead code. It can be safely removed.

--- packages/react-reconciler/src/ReactInternalTypes.js
+++ packages/react-reconciler/src/ReactInternalTypes.js
@@ -157,9 +157,6 @@ export type Fiber = {
   subtreeFlags: Flags,
   deletions: Array<Fiber> | null,
 
-  // Singly linked list fast path to the next fiber with side-effects.
-  nextEffect: Fiber | null,
-
   // The first and last fiber with side-effect within this subtree. This allows
   // us to reuse a slice of the linked list when we reuse the work done within
   // this fiber.
