96be9661633224375ab6ab43bb59dbd03887570f
Sebastian Markbage <sebastian@calyptus.eu>
2024-02-20 23:02:38 -0500

    Badge replayed logs by the environment name
    On the server we can use a combination of ANSI and CSS. The ANSI
    is used in the terminal while the CSS is ignored, the CSS overrides the
    ANSI in Chrome inspector.
    
    For ANSI we use the inverted colors (flip foreground/background) instead of
    any specific set of colors since we don't know what color scheme is used.
    
    In the browser, we can't use ANSI because Safari and Firefox will print
    them in the inspector, but we can use CSS there.

--- packages/react-client/src/ReactFlightClientConsoleConfigServer.js
+++ packages/react-client/src/ReactFlightClientConsoleConfigServer.js
@@ -7,6 +7,21 @@
  * @flow
  */
 
+import normalizeConsoleFormat from 'shared/normalizeConsoleFormat';
+
+// This flips color using ANSI, then sets a color styling, then resets.
+const badgeFormat = ' \x1b[0m\x1b[7m%c%s\x1b[0m%c';
+// Same badge styling as DevTools.
+const badgeStyle =
+  // We use a fixed background if light-dark is not supported, otherwise
+  // we use a transparent background.
+  'background: #e6e6e6;' +
+  'background: light-dark(rgba(0,0,0,0.1), rgba(255,255,255,0.25));' +
+  'color: #000000; ' +
+  'color: light-dark(#000000, #ffffff); ' +
+  'border-radius: 2px';
+const pad = ' ';
+
 export function printToConsole(
   methodName: string,
   args: Array<any>,
@@ -24,11 +39,30 @@ export function printToConsole(
     }
     case 'assert': {
       // assert takes formatting options as the second argument.
+      const newArgs = args.slice(0);
+
+      if (typeof args[1] === 'string') {
+        newArgs[1] = normalizeConsoleFormat(args[1], args, 2) + badgeFormat;
+      } else {
+        newArgs.splice(1, 0, normalizeConsoleFormat('', args, 1) + badgeFormat);
+      }
+
+      newArgs.push(badgeStyle, pad + badgeName + pad, '');
+
       // eslint-disable-next-line react-internal/no-production-logging
       console.assert.apply(console, args);
       return;
     }
     default: {
+      const newArgs = args.slice(0);
+      if (typeof args[0] === 'string') {
+        newArgs[0] = normalizeConsoleFormat(args[0], args, 1) + badgeFormat;
+      } else {
+        newArgs.unshift(normalizeConsoleFormat('', args, 0) + badgeFormat);
+      }
+
+      newArgs.push(badgeStyle, pad + badgeName + pad, '');
+
       // eslint-disable-next-line react-internal/no-production-logging
       console[methodName].apply(console, args);
       return;
