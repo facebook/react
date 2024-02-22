ec780c6a069c33310ac14ae6061d9e5a63b418c2
Tim Yung <yungsters@fb.com>
2024-02-22 15:55:48 -0800

    Add `enableComponentStackLocations` to `ReactNativeInternalFeatureFlags` libdef

--- scripts/flow/xplat.js
+++ scripts/flow/xplat.js
@@ -9,6 +9,7 @@
 
 declare module 'ReactNativeInternalFeatureFlags' {
   declare export var alwaysThrottleRetries: boolean;
+  declare export var enableComponentStackLocations: boolean;
   declare export var enableDeferRootSchedulingToMicrotask: boolean;
   declare export var enableUseRefAccessWarning: boolean;
   declare export var passChildrenWhenCloningPersistedNodes: boolean;
