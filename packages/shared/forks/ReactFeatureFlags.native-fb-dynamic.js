0fc7886ab24906175536a33b303b566b002c4cff
Rick Hanlon <rickhanlonii@fb.com>
2024-02-23 10:01:05 -0500

    [RN] Move unifiedSyncLane back to dynamic

--- packages/shared/forks/ReactFeatureFlags.native-fb-dynamic.js
+++ packages/shared/forks/ReactFeatureFlags.native-fb-dynamic.js
@@ -26,6 +26,7 @@ export const enableDeferRootSchedulingToMicrotask = __VARIANT__;
 export const enableUseRefAccessWarning = __VARIANT__;
 export const passChildrenWhenCloningPersistedNodes = __VARIANT__;
 export const useMicrotasksForSchedulingInFabric = __VARIANT__;
+export const enableUnifiedSyncLane = __VARIANT__;
 
 // Flow magic to verify the exports of this file match the original version.
 ((((null: any): ExportsType): DynamicFlagsType): ExportsType);
