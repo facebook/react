034130c02ffb47b0026059b57d17e9b080976ff3
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-03-03 14:44:27 -0800

    Fix Broken Links In Documentation (#28321)
    ## Summary
    
    I used [link-inspector](https://github.com/justindhillon/link-inspector)
    to find a bunch of broken links in this projects documentation. Here is
    what I fixed:
    
    https://www.html5rocks.com/en/tutorials/canvas/hidpi/ -->
    https://web.dev/articles/canvas-hidipi
    
    
    https://w3c.github.io/webcomponents/spec/custom/#custom-elements-core-concepts
    --> https://www.w3.org/TR/custom-elements/
    
    
    https://github.com/facebookarchive/fixed-data-table/blob/main/src/vendor_upstream/dom/normalizeWheel.js
    -->
    https://github.com/facebookarchive/fixed-data-table/blob/master/src/vendor_upstream/dom/normalizeWheel.js
    
    https://upload.wikimedia.org/wikipedia/commons/e/ee/Atom_%282%29.png -->
    https://upload.wikimedia.org/wikipedia/commons/1/1b/Atom.png
    
    ## Support my work
    
    I used [link-inspector](https://github.com/justindhillon/link-inspector)
    to find and fix this issue. If you find this PR useful, give the repo a
    :star:

--- packages/react-dom-bindings/src/server/ReactFizzConfigDOM.js
+++ packages/react-dom-bindings/src/server/ReactFizzConfigDOM.js
@@ -3710,7 +3710,7 @@ export function pushStartInstance(
       return pushSelfClosing(target, props, type);
     }
     // These are reserved SVG and MathML elements, that are never custom elements.
-    // https://w3c.github.io/webcomponents/spec/custom/#custom-elements-core-concepts
+    // https://html.spec.whatwg.org/multipage/custom-elements.html#custom-elements-core-concepts
     case 'annotation-xml':
     case 'color-profile':
     case 'font-face':
