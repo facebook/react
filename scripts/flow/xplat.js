706d95f486fbdec35b771ea4aaf3e78feb907249
Timothy Yung <yungsters@meta.com>
2024-03-08 16:14:09 -0800

    Configure Dynamic Feature Flags for React Native (#28527)
    ## Summary
    
    This PR is a subset of https://github.com/facebook/react/pull/28425,
    which only includes the feature flags that will be configured as
    dynamic.
    
    The following list summarizes the feature flag changes:
    
    * RN FB
      * Change to Dynamic
        * consoleManagedByDevToolsDuringStrictMode
        * enableAsyncActions
        * enableDeferRootSchedulingToMicrotask
        * enableRenderableContext
        * useModernStrictMode
    
    ## How did you test this change?
    
    Ran the following successfully:
    
    ```
    $ yarn test
    $ yarn flow native
    $ yarn flow fabric
    ```

--- scripts/flow/xplat.js
+++ scripts/flow/xplat.js
@@ -9,11 +9,15 @@
 
 declare module 'ReactNativeInternalFeatureFlags' {
   declare export var alwaysThrottleRetries: boolean;
+  declare export var consoleManagedByDevToolsDuringStrictMode: boolean;
+  declare export var enableAsyncActions: boolean;
   declare export var enableComponentStackLocations: boolean;
   declare export var enableDeferRootSchedulingToMicrotask: boolean;
+  declare export var enableInfiniteRenderLoopDetection: boolean;
+  declare export var enableRenderableContext: boolean;
+  declare export var enableUnifiedSyncLane: boolean;
   declare export var enableUseRefAccessWarning: boolean;
   declare export var passChildrenWhenCloningPersistedNodes: boolean;
   declare export var useMicrotasksForSchedulingInFabric: boolean;
-  declare export var enableUnifiedSyncLane: boolean;
-  declare export var enableInfiniteRenderLoopDetection: boolean;
+  declare export var useModernStrictMode: boolean;
 }
