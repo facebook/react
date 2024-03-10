ec18c3e06a310c53d108d724952a94063ea74c30
chirokas <vscode.d@gmail.com>
2024-03-10 18:52:48 +0800

    Lint

--- packages/react-dom-bindings/src/events/plugins/SimpleEventPlugin.js
+++ packages/react-dom-bindings/src/events/plugins/SimpleEventPlugin.js
@@ -23,7 +23,6 @@ import {
   SyntheticTouchEvent,
   SyntheticAnimationEvent,
   SyntheticTransitionEvent,
-  SyntheticUIEvent,
   SyntheticWheelEvent,
   SyntheticClipboardEvent,
   SyntheticPointerEvent,
