064bfc94671b85f81594f1f137ad40973dea0e1b
eps1lon <eps1lon@users.noreply.github.com>
2024-03-11 21:16:27 +0000

    Remove ReactTestUtils from ReactBrowserEventEmitter-test (#28533)
    DiffTrain build for commit https://github.com/facebook/react/commit/7d6f1e3c13686272c1d76813992d4e782fcb84e6.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-prod.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-prod.js
@@ -9147,7 +9147,7 @@ var devToolsConfig$jscomp$inline_1016 = {
     throw Error("TestRenderer does not support findFiberByHostInstance()");
   },
   bundleType: 0,
-  version: "18.3.0-canary-58cd0ef35-20240311",
+  version: "18.3.0-canary-7d6f1e3c1-20240311",
   rendererPackageName: "react-test-renderer"
 };
 var internals$jscomp$inline_1194 = {
@@ -9178,7 +9178,7 @@ var internals$jscomp$inline_1194 = {
   scheduleRoot: null,
   setRefreshHandler: null,
   getCurrentFiber: null,
-  reconcilerVersion: "18.3.0-canary-58cd0ef35-20240311"
+  reconcilerVersion: "18.3.0-canary-7d6f1e3c1-20240311"
 };
 if ("undefined" !== typeof __REACT_DEVTOOLS_GLOBAL_HOOK__) {
   var hook$jscomp$inline_1195 = __REACT_DEVTOOLS_GLOBAL_HOOK__;
--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react/cjs/React-prod.js
@@ -628,4 +628,4 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactCurrentDispatcher.current.useTransition();
 };
-exports.version = "18.3.0-canary-58cd0ef35-20240311";
+exports.version = "18.3.0-canary-7d6f1e3c1-20240311";
