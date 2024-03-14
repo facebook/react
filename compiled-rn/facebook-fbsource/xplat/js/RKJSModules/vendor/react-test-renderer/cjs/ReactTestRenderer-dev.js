ecb9703945273481ac4c9ba1bac9da20d9f99b52
jackpope <jackpope@users.noreply.github.com>
2024-03-14 15:53:47 +0000

    Make `enableNewBooleanProps` www dynamic (#28559)
    Feature: https://github.com/facebook/react/pull/24730
    
    DiffTrain build for commit https://github.com/facebook/react/commit/a870b2d5494351d75b68c3d9baf03a52fd40a8ef.

--- compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
+++ compiled-rn/facebook-fbsource/xplat/js/RKJSModules/vendor/react-test-renderer/cjs/ReactTestRenderer-dev.js
@@ -25454,7 +25454,7 @@ if (__DEV__) {
       return root;
     }
 
-    var ReactVersion = "18.3.0-canary-7268dacf7-20240314";
+    var ReactVersion = "18.3.0-canary-a870b2d54-20240314";
 
     // Might add PROFILE later.
 
