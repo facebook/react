fabd6d3a64aa20aca940142a29a14a82d1cefd31
Ricky <rickhanlonii@gmail.com>
2024-03-22 13:02:04 -0400

    Better NEXT_MAJOR support for RN flags (#28583)
    The `__NEXT_MAJOR__` value in the RN flags doesn't make sense because:
    
    a) The flags are for the next RN major, since it only impacts the
    renderers
    b) The flags are off, so they're not currently in the next major, they
    need enabled
    c) the flag script didn't support it
    
    This PR adds two aliases to the RN file:
    - `__TODO_NEXT_RN_MAJOR__`: flags that need enabled before the next RN
    major.
    - `__NEXT_RN_MAJOR__`: flags that have been enabled since the last RN
    major.
    
    These values will need to be manually kept up to date when we cut a RN
    version, but once RN switches to the canary build and aligns all the
    flags, this entire file can be deleted.
    
    ## Script screen
    Notably, I added a TODO value and a legend that prints at the end of the
    script:
    
    <img width="1078" alt="Screenshot 2024-03-18 at 8 11 27 PM"
    src="https://github.com/facebook/react/assets/2440089/14da9066-f77d-437f-8188-830a31a843c5">

--- packages/shared/forks/ReactFeatureFlags.native-oss.js
+++ packages/shared/forks/ReactFeatureFlags.native-oss.js
@@ -10,98 +10,109 @@
 import typeof * as FeatureFlagsType from 'shared/ReactFeatureFlags';
 import typeof * as ExportsType from './ReactFeatureFlags.native-oss';
 
-// -----------------------------------------------------------------------------
-// Ready for next major.
-//
-// Alias __NEXT_MAJOR__ to false for easier skimming.
-// -----------------------------------------------------------------------------
-const __NEXT_MAJOR__ = false;
+// TODO: Align these flags with canary and delete this file once RN ships from Canary.
 
+// -----------------------------------------------------------------------------
+// TODO for next React Native major.
+//
+// Alias __TODO_NEXT_RN_MAJOR__ to false for easier skimming.
+// -----------------------------------------------------------------------------
+const __TODO_NEXT_RN_MAJOR__ = false;
+export const enableRefAsProp = __TODO_NEXT_RN_MAJOR__;
+export const disableStringRefs = __TODO_NEXT_RN_MAJOR__;
+export const disableLegacyMode = __TODO_NEXT_RN_MAJOR__;
+export const enableBigIntSupport = __TODO_NEXT_RN_MAJOR__;
+export const useModernStrictMode = __TODO_NEXT_RN_MAJOR__;
+export const enableReactTestRendererWarning = __TODO_NEXT_RN_MAJOR__;
+export const enableAsyncActions = __TODO_NEXT_RN_MAJOR__;
+export const consoleManagedByDevToolsDuringStrictMode = __TODO_NEXT_RN_MAJOR__;
+export const enableDeferRootSchedulingToMicrotask = __TODO_NEXT_RN_MAJOR__;
+export const alwaysThrottleDisappearingFallbacks = __TODO_NEXT_RN_MAJOR__;
+export const alwaysThrottleRetries = __TODO_NEXT_RN_MAJOR__;
+export const enableInfiniteRenderLoopDetection = __TODO_NEXT_RN_MAJOR__;
+export const enableComponentStackLocations = __TODO_NEXT_RN_MAJOR__;
+export const disableModulePatternComponents = __TODO_NEXT_RN_MAJOR__;
+
+// -----------------------------------------------------------------------------
+// These are ready to flip after the next React npm release (or RN switches to
+// Canary, but can't flip before then because of react/renderer mismatches.
+// -----------------------------------------------------------------------------
+export const enableCache = __TODO_NEXT_RN_MAJOR__;
+export const enableRenderableContext = __TODO_NEXT_RN_MAJOR__;
+
+// -----------------------------------------------------------------------------
+// Already enabled for next React Native major.
+// Hardcode these to true after the next RN major.
+//
+// Alias __NEXT_RN_MAJOR__ to true for easier skimming.
+// -----------------------------------------------------------------------------
+const __NEXT_RN_MAJOR__ = true;
+export const disableClientCache = __NEXT_RN_MAJOR__;
+export const disableLegacyContext = __NEXT_RN_MAJOR__;
+export const enableCacheElement = __NEXT_RN_MAJOR__;
+export const enableTaint = __NEXT_RN_MAJOR__;
+export const enableUnifiedSyncLane = __NEXT_RN_MAJOR__;
+export const enableFizzExternalRuntime = __NEXT_RN_MAJOR__; // DOM-only
+export const disableJavaScriptURLs = __NEXT_RN_MAJOR__; // DOM-only
+export const enableFormActions = __NEXT_RN_MAJOR__; // DOM-only
+export const enableBinaryFlight = __NEXT_RN_MAJOR__; // DOM-only
+export const enableCustomElementPropertySupport = __NEXT_RN_MAJOR__; // DOM-only
+export const enableServerComponentKeys = __NEXT_RN_MAJOR__;
+export const enableServerComponentLogs = __NEXT_RN_MAJOR__;
+
+// DEV-only but enabled in the next RN Major.
+// Not supported by flag script to avoid the special case.
 export const debugRenderPhaseSideEffectsForStrictMode = __DEV__;
+
+// TODO: decide on React 19
+export const enableUseMemoCacheHook = false;
+export const enableUseDeferredValueInitialArg = __EXPERIMENTAL__;
+
+// -----------------------------------------------------------------------------
+// All other flags
+// -----------------------------------------------------------------------------
+export const enableCPUSuspense = false;
 export const enableDebugTracing = false;
 export const enableAsyncDebugInfo = false;
 export const enableSchedulingProfiler = false;
-export const enableProfilerTimer = __PROFILE__;
-export const enableProfilerCommitHooks = __PROFILE__;
-export const enableProfilerNestedUpdatePhase = __PROFILE__;
-export const enableUpdaterTracking = __PROFILE__;
-export const enableCache = __NEXT_MAJOR__;
 export const enableLegacyCache = false;
-export const enableCacheElement = true;
 export const enableFetchInstrumentation = false;
-export const enableFormActions = true; // Doesn't affect Native
-export const enableBinaryFlight = true;
-export const enableTaint = true;
 export const enablePostpone = false;
-export const disableJavaScriptURLs = true;
 export const disableCommentsAsDOMContainers = true;
 export const disableInputAttributeSyncing = false;
 export const disableIEWorkarounds = true;
 export const enableScopeAPI = false;
 export const enableCreateEventHandleAPI = false;
 export const enableSuspenseCallback = false;
-export const disableLegacyContext = true;
 export const enableTrustedTypesIntegration = false;
 export const disableTextareaChildren = false;
-export const disableModulePatternComponents = false;
 export const enableSuspenseAvoidThisFallback = false;
 export const enableSuspenseAvoidThisFallbackFizz = false;
-export const enableCPUSuspense = false;
-export const enableUseMemoCacheHook = false;
 export const enableUseEffectEventHook = false;
 export const enableClientRenderFallbackOnTextMismatch = true;
-export const enableComponentStackLocations = false;
 export const enableLegacyFBSupport = false;
 export const enableFilterEmptyStringAttributesDOM = true;
 export const enableGetInspectorDataForInstanceInProduction = false;
-export const enableRenderableContext = false;
-
 export const enableRetryLaneExpiration = false;
 export const retryLaneExpirationMs = 5000;
 export const syncLaneExpirationMs = 250;
 export const transitionLaneExpirationMs = 5000;
-
 export const enableUseRefAccessWarning = false;
-
 export const disableSchedulerTimeoutInWorkLoop = false;
 export const enableLazyContextPropagation = false;
 export const enableLegacyHidden = false;
 export const forceConcurrentByDefaultForTesting = false;
-export const enableUnifiedSyncLane = true;
 export const allowConcurrentByDefault = false;
-export const enableCustomElementPropertySupport = true;
 export const enableNewBooleanProps = true;
-
-export const consoleManagedByDevToolsDuringStrictMode = false;
-
 export const enableTransitionTracing = false;
-
-export const useModernStrictMode = false;
 export const enableDO_NOT_USE_disableStrictPassiveEffect = false;
-export const enableFizzExternalRuntime = true;
-export const enableDeferRootSchedulingToMicrotask = false;
-export const enableInfiniteRenderLoopDetection = false;
-
-export const enableAsyncActions = false;
-
-export const alwaysThrottleDisappearingFallbacks = false;
-export const alwaysThrottleRetries = false;
-
 export const passChildrenWhenCloningPersistedNodes = false;
-export const enableUseDeferredValueInitialArg = __EXPERIMENTAL__;
-export const disableClientCache = true;
 
-export const enableServerComponentKeys = true;
-export const enableServerComponentLogs = true;
-
-// TODO: Should turn this on in next "major" RN release.
-export const enableRefAsProp = false;
-export const disableStringRefs = false;
-
-export const enableReactTestRendererWarning = false;
-
-export const enableBigIntSupport = false;
-export const disableLegacyMode = false;
+// Profiling Only
+export const enableProfilerTimer = __PROFILE__;
+export const enableProfilerCommitHooks = __PROFILE__;
+export const enableProfilerNestedUpdatePhase = __PROFILE__;
+export const enableUpdaterTracking = __PROFILE__;
 
 // Flow magic to verify the exports of this file match the original version.
 ((((null: any): ExportsType): FeatureFlagsType): ExportsType);
