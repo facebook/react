1de6c5aea207ac2beac375aa80287acfcfb567a0
Sathya Gunasekaran <gsathya.ceg@gmail.com>
2024-04-02 12:03:26 +0100

    [eslint] Don't lint generated files
    drive by fix multi line comments in .eslintrc

--- compiler/packages/babel-plugin-react-forget/.eslintrc.js
+++ compiler/packages/babel-plugin-react-forget/.eslintrc.js
@@ -9,44 +9,56 @@
 module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
-    // We prefer using const where variables are not reassigned, but occassional mistakes
-    // aren't a major issue
+    /*
+     * We prefer using const where variables are not reassigned, but occassional mistakes
+     * aren't a major issue
+     */
     "prefer-const": "off",
 
     // Not valuable enough to enable
     "no-useless-escape": "off",
 
-    // There are valid use cases for loops with constant conditions where the body contains the
-    // break
+    /*
+     * There are valid use cases for loops with constant conditions where the body contains the
+     * break
+     */
     "no-constant-condition": "off",
 
     // eslint only knows about builtin control flow (eg throw, return, break) and not custom ones
     // like invariant.
     "no-fallthrough": "off",
 
-    // Low-value: this fires even for declarations that capture references which wouldn't be as
-    // obvious if the declaration was lifted to the parent root
+    /*
+     * Low-value: this fires even for declarations that capture references which wouldn't be as
+     * obvious if the declaration was lifted to the parent root
+     */
     "no-inner-declarations": "off",
 
     "multiline-comment-style": ["error", "starred-block"],
 
     "@typescript-eslint/no-empty-function": "off",
 
-    // Explicitly casting to/through any is sometimes required, often for error messages to
-    // assertExhaustive()
+    /*
+     * Explicitly casting to/through any is sometimes required, often for error messages to
+     * assertExhaustive()
+     */
     "@typescript-eslint/no-explicit-any": "off",
 
-    // We use non-null assertions carefully. Ideally, there would be a TS option to codegen
-    // a non-null check at the assertion site.
+    /*
+     * We use non-null assertions carefully. Ideally, there would be a TS option to codegen
+     * a non-null check at the assertion site.
+     */
     "@typescript-eslint/no-non-null-assertion": "off",
 
     // Being explicit provides value in cases where inference may later change
     "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/explicit-function-return-type": "error",
 
-    // Unused variables are frequently a bug. Prefix unused variables with an _ to fix, but note
-    // that eslint won't warn you that an underscore prefixed variable is used and that the prefix
-    // should be dropped.
+    /*
+     * Unused variables are frequently a bug. Prefix unused variables with an _ to fix, but note
+     * that eslint won't warn you that an underscore prefixed variable is used and that the prefix
+     * should be dropped.
+     */
     "@typescript-eslint/no-unused-vars": [
       "error",
       {
@@ -67,10 +79,14 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
-  ignorePatterns: ["src/__tests__/**/*", "src/**/*.d.ts"],
-
-  // If rules need to be disabled then the rule is insufficiently high signal
-  // and should be diasbled altogether or customized (in either case via a standalone PR)
+  ignorePatterns: ["src/__tests__/**/*", "src/**/*.d.ts", "dist/**/*"],
+  env: {
+    node: true,
+  },
+  /*
+   * If rules need to be disabled then the rule is insufficiently high signal
+   * and should be diasbled altogether or customized (in either case via a standalone PR)
+   */
   noInlineConfig: true,
   reportUnusedDisableDirectives: true,
 };
