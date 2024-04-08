9a6cb241504027688c166f7fedba8bd443cb9639
Lauren Tan <poteto@users.noreply.github.com>
2024-04-08 10:55:55 -0400

    [ez] Fix comments to be star blocks
    ghstack-source-id: 6a9e844a3ba548d3bf2b7373dc023ad42c2e719b
    Pull Request resolved: https://github.com/facebook/react-forget/pull/2826

--- compiler/packages/babel-plugin-react-forget/src/CompilerError.ts
+++ compiler/packages/babel-plugin-react-forget/src/CompilerError.ts
@@ -9,17 +9,29 @@ import type { SourceLocation } from "./HIR";
 import { assertExhaustive } from "./Utils/utils";
 
 export enum ErrorSeverity {
-  // Invalid JS syntax, or valid syntax that is semantically invalid which may indicate some misunderstanding on the user’s part.
+  /**
+   * Invalid JS syntax, or valid syntax that is semantically invalid which may indicate some
+   * misunderstanding on the user’s part.
+   */
   InvalidJS = "InvalidJS",
-  // Code that breaks the rules of React.
+  /**
+   * Code that breaks the rules of React.
+   */
   InvalidReact = "InvalidReact",
-  // Incorrect configuration of the compiler.
+  /**
+   * Incorrect configuration of the compiler.
+   */
   InvalidConfig = "InvalidConfig",
-  // Code that can reasonably occur and that doesn't break any rules, but is unsafe to perserve memoization.
+  /**
+   * Code that can reasonably occur and that doesn't break any rules, but is unsafe to perserve
+   * memoization.
+   */
   CannotPreserveMemoization = "CannotPreserveMemoization",
-  // Unhandled syntax that we don't support yet.
+  /**
+   * Unhandled syntax that we don't support yet.
+   */
   Todo = "Todo",
-  /*
+  /**
    * An unexpected internal error in the compiler that indicates critical issues that can panic
    * the compiler.
    */
