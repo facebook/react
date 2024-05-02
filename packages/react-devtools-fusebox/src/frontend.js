97f9a9a51ae34e64d7f5d71314655aac88c2b1cb
Moti Zilberman <moti@meta.com>
2024-05-02 13:35:28 +0100

    feat[react-devtools-fusebox]: enable inspected element context menu

--- packages/react-devtools-fusebox/src/frontend.js
+++ packages/react-devtools-fusebox/src/frontend.js
@@ -62,6 +62,7 @@ export function initialize(
       store={store}
       showTabBar={true}
       warnIfLegacyBackendDetected={true}
+      enabledInspectedElementContextMenu={true}
     />,
   );
 }
