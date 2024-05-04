4db593f4c8c3f80d96f0426458844f2010f7df75
Marco Krooß <zkamekyu@gmail.com>
2024-05-04 14:29:26 +0000

    refactored shebangs

--- fixtures/eslint/watch.sh
+++ fixtures/eslint/watch.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 (cd ../.. && yarn build eslint --type=NODE_DEV)
 (cd ../.. && watchman-make --make 'yarn build eslint --type=NODE_DEV' -p 'packages/eslint-plugin-*/**/*' -t ignored)
--- scripts/circleci/pack_and_store_devtools_artifacts.sh
+++ scripts/circleci/pack_and_store_devtools_artifacts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
