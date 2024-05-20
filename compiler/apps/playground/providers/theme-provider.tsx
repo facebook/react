ebeca4c038f082d3de50b522078fbd0b46af3bf7
DevMirza <pzhafeez@gmail.com>
2024-05-20 10:11:34 +0000

    init basic dark mode setup for testing

---
+++ compiler/apps/playground/providers/theme-provider.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
