4051d5b5c35675b08538fa9fd7b123d019dd92ea
Ayc0 <ayc0.benj@gmail.com>
2024-05-23 09:11:43 +0200

    [compiler:eslint] avoid loading the user config when running Babel + BabelPluginReactCompiler

--- compiler/packages/eslint-plugin-react-compiler/src/rules/ReactCompilerRule.ts
+++ compiler/packages/eslint-plugin-react-compiler/src/rules/ReactCompilerRule.ts
@@ -135,6 +135,9 @@ const rule: Rule.RuleModule = {
     if (babelAST != null) {
       try {
         transformFromAstSync(babelAST, sourceCode, {
+          // Avoid having conflicting configs between our config using `[PluginProposalPrivateMethods, { loose: true }]`
+          // and the user config (that can have a different config for @babel/plugin-transform-class-properties, @babel/plugin-transform-private-methods, or @babel/plugin-transform-private-property-in-object)
+          configFile: false,
           filename,
           highlightCode: false,
           retainLines: true,
