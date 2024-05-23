2e3e6a9b1cc97ec91248be74565e7ccbf6946067
Sebastian Markbåge <sebastian@calyptus.eu>
2024-05-23 12:25:23 -0400

    Unify ReactFiberCurrentOwner and ReactCurrentFiber (#29038)
    We previously had two slightly different concepts for "current fiber".
    
    There's the "owner" which is set inside of class components in prod if
    string refs are enabled, and sometimes inside function components in DEV
    but not other contexts.
    
    Then we have the "current fiber" which is only set in DEV for various
    warnings but is enabled in a bunch of contexts.
    
    This unifies them into a single "current fiber".
    
    The concept of string refs shouldn't really exist so this should really
    be a DEV only concept. In the meantime, this sets the current fiber
    inside class render only in prod, however, in DEV it's now enabled in
    more contexts which can affect the string refs. That was already the
    case that a string ref in a Function component was only connecting to
    the owner in prod. Any string ref associated with any non-class won't
    work regardless so that's not an issue. The practical change here is
    that an element with a string ref created inside a life-cycle associated
    with a class will work in DEV but not in prod. Since we need the current
    fiber to be available in more contexts in DEV for the debugging
    purposes. That wouldn't affect any old code since it would have a broken
    ref anyway. New code shouldn't use string refs anyway.
    
    The other implication is that "owner" doesn't necessarily mean
    "rendering" since we need the "owner" to track other debug information
    like stacks - in other contexts like useEffect, life cycles, etc.
    Internally we have a separate `isRendering` flag that actually means
    we're rendering but even that is a very overloaded concept. So anything
    that uses "owner" to imply rendering might be wrong with this change.
    
    This is a first step to a larger refactor for tracking current rendering
    information.
    
    ---------
    
    Co-authored-by: Sebastian Silbermann <silbermann.sebastian@gmail.com>

--- packages/react-reconciler/src/ReactFiberWorkLoop.js
+++ packages/react-reconciler/src/ReactFiberWorkLoop.js
@@ -206,7 +206,6 @@ import {
   ContextOnlyDispatcher,
 } from './ReactFiberHooks';
 import {DefaultAsyncDispatcher} from './ReactFiberAsyncDispatcher';
-import {setCurrentOwner} from './ReactFiberCurrentOwner';
 import {
   createCapturedValueAtFiber,
   type CapturedValue,
@@ -232,8 +231,9 @@ import ReactStrictModeWarnings from './ReactStrictModeWarnings';
 import {
   isRendering as ReactCurrentDebugFiberIsRenderingInDEV,
   current as ReactCurrentFiberCurrent,
-  resetCurrentFiber as resetCurrentDebugFiberInDEV,
-  setCurrentFiber as setCurrentDebugFiberInDEV,
+  resetCurrentDebugFiberInDEV,
+  setCurrentDebugFiberInDEV,
+  resetCurrentFiber,
 } from './ReactCurrentFiber';
 import {
   isDevToolsPresent,
@@ -1686,9 +1686,8 @@ function handleThrow(root: FiberRoot, thrownValue: any): void {
   // These should be reset immediately because they're only supposed to be set
   // when React is executing user code.
   resetHooksAfterThrow();
-  resetCurrentDebugFiberInDEV();
   if (__DEV__ || !disableStringRefs) {
-    setCurrentOwner(null);
+    resetCurrentFiber();
   }
 
   if (thrownValue === SuspenseException) {
@@ -2386,17 +2385,15 @@ function performUnitOfWork(unitOfWork: Fiber): void {
     next = beginWork(current, unitOfWork, entangledRenderLanes);
   }
 
-  resetCurrentDebugFiberInDEV();
+  if (__DEV__ || !disableStringRefs) {
+    resetCurrentFiber();
+  }
   unitOfWork.memoizedProps = unitOfWork.pendingProps;
   if (next === null) {
     // If this doesn't spawn new work, complete the current work.
     completeUnitOfWork(unitOfWork);
   } else {
     workInProgress = next;
-  }
-
-  if (__DEV__ || !disableStringRefs) {
-    setCurrentOwner(null);
   }
 }
 
@@ -2408,7 +2405,6 @@ function replaySuspendedUnitOfWork(unitOfWork: Fiber): void {
   setCurrentDebugFiberInDEV(unitOfWork);
 
   let next;
-  setCurrentDebugFiberInDEV(unitOfWork);
   const isProfilingMode =
     enableProfilerTimer && (unitOfWork.mode & ProfileMode) !== NoMode;
   if (isProfilingMode) {
@@ -2501,17 +2497,15 @@ function replaySuspendedUnitOfWork(unitOfWork: Fiber): void {
   // The begin phase finished successfully without suspending. Return to the
   // normal work loop.
 
-  resetCurrentDebugFiberInDEV();
+  if (__DEV__ || !disableStringRefs) {
+    resetCurrentFiber();
+  }
   unitOfWork.memoizedProps = unitOfWork.pendingProps;
   if (next === null) {
     // If this doesn't spawn new work, complete the current work.
     completeUnitOfWork(unitOfWork);
   } else {
     workInProgress = next;
-  }
-
-  if (__DEV__ || !disableStringRefs) {
-    setCurrentOwner(null);
   }
 }
 
@@ -2901,11 +2895,6 @@ function commitRootImpl(
 
     const prevExecutionContext = executionContext;
     executionContext |= CommitContext;
-
-    // Reset this to null before calling lifecycles
-    if (__DEV__ || !disableStringRefs) {
-      setCurrentOwner(null);
-    }
 
     // The commit phase is broken into several sub-phases. We do a separate pass
     // of the effect list for each phase: all mutation effects come before all
