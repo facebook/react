0af082e4c98c3af40cd03c166472be9ff01a0907
jackpope <jackpope@users.noreply.github.com>
2024-05-28 19:00:29 +0000

    Clean up disableDOMTestUtils (#29610)
    `disableDOMTestUtils` and the FB build `ReactTestUtilsFB` allowed us to
    finish migrating internal callsites off of ReactTestUtils. Now that
    usage is cleaned up, we can remove the flag, build artifact, and test
    coverage for the deprecated utility methods.
    
    DiffTrain build for [2787eebe52864356252a280fd811cd9d52807a82](https://github.com/facebook/react/commit/2787eebe52864356252a280fd811cd9d52807a82)

--- compiled/facebook-www/__test_utils__/ReactAllWarnings.js
+++ compiled/facebook-www/__test_utils__/ReactAllWarnings.js
@@ -245,7 +245,6 @@ export default [
   "ReactDOM.unstable_renderSubtreeIntoContainer() has not been supported since React 18. Consider using a portal instead. Until you switch to the createRoot API, your app will behave as if it's running React 17. Learn more: https://react.dev/link/switch-to-createroot",
   "ReactDOM.unstable_renderSubtreeIntoContainer() was removed in React 19. Consider using a portal instead.",
   "ReactDOM.useFormState has been renamed to React.useActionState. Please update %s to use React.useActionState.",
-  "ReactTestUtils.mockComponent() is deprecated. Use shallow rendering or jest.mock() instead.\n\nSee https://react.dev/link/test-utils-mock-component for more information.",
   "Received NaN for the `%s` attribute. If this is expected, cast the value to a string.",
   "Received `%s` for a non-boolean attribute `%s`.\n\nIf you want to write it to the DOM, pass a string instead: %s=\"%s\" or %s={value.toString()}.",
   "Received `%s` for a non-boolean attribute `%s`.\n\nIf you want to write it to the DOM, pass a string instead: %s=\"%s\" or %s={value.toString()}.\n\nIf you used to conditionally omit it with %s={condition && value}, pass %s={condition ? value : undefined} instead.",
