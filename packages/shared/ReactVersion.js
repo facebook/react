f5e89e81423b6eeb7d85828ebdd9b99f9bfdcb07
Jack Pope <jackpope@meta.com>
2024-05-28 10:58:47 -0400

    f

--- packages/shared/ReactVersion.js
+++ packages/shared/ReactVersion.js
@@ -1,1 +1,15 @@
-export default '19.0.0-PLACEHOLDER';
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// TODO: this is special because it gets imported during build.
+//
+// It exists as a placeholder so that DevTools can support work tag changes between releases.
+// When we next publish a release, update the matching TODO in backend/renderer.js
+// TODO: This module is used both by the release scripts and to expose a version
+// at runtime. We should instead inject the version number as part of the build
+// process, and use the ReactVersions.js module as the single source of truth.
+export default '19.0.0';
