eb3f84b5be81550e62006181b2f3cf048ba681ce
eps1lon <sebastian.silbermann@vercel.com>
2024-06-02 23:43:34 +0200

    Avoid closure
    Fixes
    ````
    SyntaxError: ~/react/packages/react-reconciler/src/ReactFiberLane.js: Compiling let/const in this block would add a closure (throwIfClosureRequired).
      614 |   if (updateLane !== IdleLane) {
      615 |     if (enableUpdaterTracking) {
    > 616 |       if (isDevToolsPresent) {
          |                              ^
      617 |         // transfer pending updaters from pingedLanes to updateLane
      618 |         const pendingUpdatersLaneMap = root.pendingUpdatersLaneMap;
      619 |         const updaters = pendingUpdatersLaneMap[laneToIndex(updateLane)];
      ```

--- packages/react-reconciler/src/ReactFiberLane.js
+++ packages/react-reconciler/src/ReactFiberLane.js
@@ -625,26 +625,7 @@ export function markRootUpdated(root: FiberRoot, updateLane: Lane) {
   // idle updates until after all the regular updates have finished; there's no
   // way it could unblock a transition.
   if (updateLane !== IdleLane) {
-    if (enableUpdaterTracking) {
-      if (isDevToolsPresent) {
-        // transfer pending updaters from pingedLanes to updateLane
-        const pendingUpdatersLaneMap = root.pendingUpdatersLaneMap;
-        const updaters = pendingUpdatersLaneMap[laneToIndex(updateLane)];
-        let lanes = root.pingedLanes;
-        while (lanes > 0) {
-          const index = laneToIndex(lanes);
-          const lane = 1 << index;
-
-          const pingedUpdaters = pendingUpdatersLaneMap[index];
-          pingedUpdaters.forEach(pingedUpdater => {
-            updaters.add(pingedUpdater);
-          });
-          pingedUpdaters.clear();
-
-          lanes &= ~lane;
-        }
-      }
-    }
+    movePendingUpdatersToLane(root, root.pingedLanes, updateLane);
 
     root.suspendedLanes = NoLanes;
     root.pingedLanes = NoLanes;
@@ -922,6 +903,35 @@ export function addFiberToLanesMap(
 
     const updaters = pendingUpdatersLaneMap[index];
     updaters.add(fiber);
+
+    lanes &= ~lane;
+  }
+}
+
+function movePendingUpdatersToLane(
+  root: FiberRoot,
+  sourceLanes: Lanes,
+  targetLane: Lane,
+) {
+  if (!enableUpdaterTracking) {
+    return;
+  }
+  if (!isDevToolsPresent) {
+    return;
+  }
+  const pendingUpdatersLaneMap = root.pendingUpdatersLaneMap;
+  const targetIndex = laneToIndex(targetLane);
+  const targetUpdaters = pendingUpdatersLaneMap[targetIndex];
+  let lanes = sourceLanes;
+  while (lanes > 0) {
+    const index = laneToIndex(lanes);
+    const lane = 1 << index;
+
+    const sourceUpdaters = pendingUpdatersLaneMap[index];
+    sourceUpdaters.forEach(sourceUpdater => {
+      targetUpdaters.add(sourceUpdater);
+    });
+    sourceUpdaters.clear();
 
     lanes &= ~lane;
   }
