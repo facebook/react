5ad2c37273b7c192011010c6bdb9ac0a4c9c232f
Ricky <rickhanlonii@gmail.com>
2024-06-03 10:09:23 -0400

    Skip empty sync commits (both repos) (#29707)
    Requires https://github.com/facebook/react/pull/29706
    
    The strategy here is to:
    - Checkout the builds/facebook-www branch
    - Read the current sync'd VERSION
    - Checkout out main and sync new build
    - sed/{new version string}/{old version string}
    - Run git status, skip sync if clean
    - Otherwise, sed/{old version string}/{new version string} and push
    commit
    
    This means that:
    - We're using the real version strings from the builds
    - We are checking the last commit on the branch for the real last
    version
    - We're skipping any commits that won't result in changes
    - ???
    - Profit!

--- scripts/rollup/build-all-release-channels.js
+++ scripts/rollup/build-all-release-channels.js
@@ -167,10 +167,14 @@ function processStable(buildDir) {
         fs.renameSync(filePath, filePath.replace('.js', '.classic.js'));
       }
     }
+    const versionString =
+      ReactVersion + '-www-classic-' + sha + '-' + dateString;
     updatePlaceholderReactVersionInCompiledArtifacts(
       buildDir + '/facebook-www',
-      ReactVersion + '-www-classic-' + sha + '-' + dateString
+      versionString
     );
+    // Also save a file with the version number
+    fs.writeFileSync(buildDir + '/facebook-www/VERSION_CLASSIC', versionString);
   }
 
   if (fs.existsSync(buildDir + '/sizes')) {
@@ -213,9 +217,28 @@ function processExperimental(buildDir, version) {
         fs.renameSync(filePath, filePath.replace('.js', '.modern.js'));
       }
     }
+    const versionString =
+      ReactVersion + '-www-modern-' + sha + '-' + dateString;
     updatePlaceholderReactVersionInCompiledArtifacts(
       buildDir + '/facebook-www',
-      ReactVersion + '-www-modern-' + sha + '-' + dateString
+      versionString
+    );
+
+    // Also save a file with the version number
+    fs.writeFileSync(buildDir + '/facebook-www/VERSION_MODERN', versionString);
+  }
+
+  if (fs.existsSync(buildDir + '/facebook-react-native')) {
+    const versionString = ReactVersion + '-native-fb-' + sha + '-' + dateString;
+    updatePlaceholderReactVersionInCompiledArtifacts(
+      buildDir + '/facebook-react-native',
+      versionString
+    );
+
+    // Also save a file with the version number
+    fs.writeFileSync(
+      buildDir + '/facebook-react-native/VERSION_NATIVE_FB',
+      versionString
     );
   }
 
