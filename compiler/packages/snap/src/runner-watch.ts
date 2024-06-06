b6ac776454cbe7462e9c31d189eb6645d1c5af82
Joe Savona <joesavona@fb.com>
2024-06-06 09:31:30 -0700

    compiler: revert #29720 to unbreak filter mode in snap
    ghstack-source-id: fbfdd2b906e62879f9e2f11fbd3b23a67f60d8bc
    Pull Request resolved: https://github.com/facebook/react/pull/29780

--- compiler/packages/snap/src/runner-watch.ts
+++ compiler/packages/snap/src/runner-watch.ts
@@ -153,8 +153,8 @@ function subscribeFilterFile(
     } else if (
       events.findIndex((event) => event.path.includes(FILTER_FILENAME)) !== -1
     ) {
+      state.filter = await readTestFilter();
       if (state.mode.filter) {
-        state.filter = await readTestFilter();
         state.mode.action = RunnerAction.Test;
         onChange(state);
       }
@@ -218,7 +218,7 @@ export async function makeWatchRunner(
       action: RunnerAction.Test,
       filter: filterMode,
     },
-    filter: filterMode ? await readTestFilter() : null,
+    filter: await readTestFilter(),
   };
 
   subscribeTsc(state, onChange);
