23a7c2dc3d59b84e8576732ac64bb310ccf29a9b
Sebastian Markbage <sebastian@calyptus.eu>
2024-06-20 11:50:53 +0200

    Tests
    We cannot use JSX because we need to use the react-server runtime.

--- packages/react-html/src/__tests__/ReactHTMLServer-test.js
+++ packages/react-html/src/__tests__/ReactHTMLServer-test.js
@@ -27,10 +27,83 @@ describe('ReactHTML', () => {
 
   it('should be able to render a simple component', async () => {
     function Component() {
-      return <div>hello world</div>;
+      // We can't use JSX because that's client-JSX in our tests.
+      return React.createElement('div', null, 'hello world');
     }
 
-    const html = await ReactHTML.renderToMarkup(<Component />);
+    const html = await ReactHTML.renderToMarkup(React.createElement(Component));
     expect(html).toBe('<div>hello world</div>');
+  });
+
+  it('supports the useId Hook', async () => {
+    function Component() {
+      const firstNameId = React.useId();
+      const lastNameId = React.useId();
+      // We can't use JSX because that's client-JSX in our tests.
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h2',
+          {
+            id: firstNameId,
+          },
+          'First',
+        ),
+        React.createElement(
+          'p',
+          {
+            'aria-labelledby': firstNameId,
+          },
+          'Sebastian',
+        ),
+        React.createElement(
+          'h2',
+          {
+            id: lastNameId,
+          },
+          'Last',
+        ),
+        React.createElement(
+          'p',
+          {
+            'aria-labelledby': lastNameId,
+          },
+          'Smith',
+        ),
+      );
+    }
+
+    const html = await ReactHTML.renderToMarkup(React.createElement(Component));
+    const container = document.createElement('div');
+    container.innerHTML = html;
+
+    expect(container.getElementsByTagName('h2')[0].id).toBe(
+      container.getElementsByTagName('p')[0].getAttribute('aria-labelledby'),
+    );
+    expect(container.getElementsByTagName('h2')[1].id).toBe(
+      container.getElementsByTagName('p')[1].getAttribute('aria-labelledby'),
+    );
+
+    // It's not the same id between them.
+    expect(container.getElementsByTagName('h2')[0].id).not.toBe(
+      container.getElementsByTagName('p')[1].getAttribute('aria-labelledby'),
+    );
+  });
+
+  // @gate enableCache
+  it('supports cache', async () => {
+    let counter = 0;
+    const getCount = React.cache(() => {
+      return counter++;
+    });
+    function Component() {
+      const a = getCount();
+      const b = getCount();
+      return React.createElement('div', null, a, b);
+    }
+
+    const html = await ReactHTML.renderToMarkup(React.createElement(Component));
+    expect(html).toBe('<div>00</div>');
   });
 });
