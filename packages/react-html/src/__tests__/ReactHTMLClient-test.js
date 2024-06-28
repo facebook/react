58af67a8f8761e56b5d02486a304c96edc547760
Sebastian Markbåge <sebastian@calyptus.eu>
2024-06-28 16:19:20 +0200

    Only build react-html in experimental channel (#30129)
    Even though the whole package is private right now. Once we publish it,
    it'll likely be just the experimental channel first before upgrading to
    stable.
    
    This means it gets excluded from the built packages.

--- packages/react-html/src/__tests__/ReactHTMLClient-test.js
+++ packages/react-html/src/__tests__/ReactHTMLClient-test.js
@@ -12,141 +12,152 @@
 let React;
 let ReactHTML;
 
-describe('ReactHTML', () => {
-  beforeEach(() => {
-    jest.resetModules();
-    React = require('react');
-    ReactHTML = require('react-html');
-  });
-
-  it('should be able to render a simple component', async () => {
-    function Component() {
-      return <div>hello world</div>;
+if (!__EXPERIMENTAL__) {
+  it('should not be built in stable', () => {
+    try {
+      require('react-html');
+    } catch (x) {
+      return;
     }
-
-    const html = await ReactHTML.renderToMarkup(<Component />);
-    expect(html).toBe('<div>hello world</div>');
+    throw new Error('Expected react-html not to exist in stable.');
   });
-
-  it('should prefix html tags with a doctype', async () => {
-    const html = await ReactHTML.renderToMarkup(
-      <html>
-        <body>hello</body>
-      </html>,
-    );
-    expect(html).toBe(
-      '<!DOCTYPE html><html><head></head><body>hello</body></html>',
-    );
-  });
-
-  it('should error on useState', async () => {
-    function Component() {
-      const [state] = React.useState('hello');
-      return <div>{state}</div>;
-    }
-
-    await expect(async () => {
-      await ReactHTML.renderToMarkup(<Component />);
-    }).rejects.toThrow();
-  });
-
-  it('should error on refs passed to host components', async () => {
-    function Component() {
-      const ref = React.createRef();
-      return <div ref={ref} />;
-    }
-
-    await expect(async () => {
-      await ReactHTML.renderToMarkup(<Component />);
-    }).rejects.toThrow();
-  });
-
-  it('should error on callbacks passed to event handlers', async () => {
-    function Component() {
-      function onClick() {
-        // This won't be able to be called.
-      }
-      return <div onClick={onClick} />;
-    }
-
-    await expect(async () => {
-      await ReactHTML.renderToMarkup(<Component />);
-    }).rejects.toThrow();
-  });
-
-  it('supports the useId Hook', async () => {
-    function Component() {
-      const firstNameId = React.useId();
-      const lastNameId = React.useId();
-      return React.createElement(
-        'div',
-        null,
-        React.createElement(
-          'h2',
-          {
-            id: firstNameId,
-          },
-          'First',
-        ),
-        React.createElement(
-          'p',
-          {
-            'aria-labelledby': firstNameId,
-          },
-          'Sebastian',
-        ),
-        React.createElement(
-          'h2',
-          {
-            id: lastNameId,
-          },
-          'Last',
-        ),
-        React.createElement(
-          'p',
-          {
-            'aria-labelledby': lastNameId,
-          },
-          'Smith',
-        ),
-      );
-    }
-
-    const html = await ReactHTML.renderToMarkup(<Component />);
-    const container = document.createElement('div');
-    container.innerHTML = html;
-
-    expect(container.getElementsByTagName('h2')[0].id).toBe(
-      container.getElementsByTagName('p')[0].getAttribute('aria-labelledby'),
-    );
-    expect(container.getElementsByTagName('h2')[1].id).toBe(
-      container.getElementsByTagName('p')[1].getAttribute('aria-labelledby'),
-    );
-
-    // It's not the same id between them.
-    expect(container.getElementsByTagName('h2')[0].id).not.toBe(
-      container.getElementsByTagName('p')[1].getAttribute('aria-labelledby'),
-    );
-  });
-
-  // @gate disableClientCache
-  it('does NOT support cache yet because it is a client component', async () => {
-    let counter = 0;
-    const getCount = React.cache(() => {
-      return counter++;
+} else {
+  describe('ReactHTML', () => {
+    beforeEach(() => {
+      jest.resetModules();
+      React = require('react');
+      ReactHTML = require('react-html');
     });
-    function Component() {
-      const a = getCount();
-      const b = getCount();
-      return (
-        <div>
-          {a}
-          {b}
-        </div>
-      );
-    }
 
-    const html = await ReactHTML.renderToMarkup(<Component />);
-    expect(html).toBe('<div>01</div>');
+    it('should be able to render a simple component', async () => {
+      function Component() {
+        return <div>hello world</div>;
+      }
+
+      const html = await ReactHTML.renderToMarkup(<Component />);
+      expect(html).toBe('<div>hello world</div>');
+    });
+
+    it('should prefix html tags with a doctype', async () => {
+      const html = await ReactHTML.renderToMarkup(
+        <html>
+          <body>hello</body>
+        </html>,
+      );
+      expect(html).toBe(
+        '<!DOCTYPE html><html><head></head><body>hello</body></html>',
+      );
+    });
+
+    it('should error on useState', async () => {
+      function Component() {
+        const [state] = React.useState('hello');
+        return <div>{state}</div>;
+      }
+
+      await expect(async () => {
+        await ReactHTML.renderToMarkup(<Component />);
+      }).rejects.toThrow();
+    });
+
+    it('should error on refs passed to host components', async () => {
+      function Component() {
+        const ref = React.createRef();
+        return <div ref={ref} />;
+      }
+
+      await expect(async () => {
+        await ReactHTML.renderToMarkup(<Component />);
+      }).rejects.toThrow();
+    });
+
+    it('should error on callbacks passed to event handlers', async () => {
+      function Component() {
+        function onClick() {
+          // This won't be able to be called.
+        }
+        return <div onClick={onClick} />;
+      }
+
+      await expect(async () => {
+        await ReactHTML.renderToMarkup(<Component />);
+      }).rejects.toThrow();
+    });
+
+    it('supports the useId Hook', async () => {
+      function Component() {
+        const firstNameId = React.useId();
+        const lastNameId = React.useId();
+        return React.createElement(
+          'div',
+          null,
+          React.createElement(
+            'h2',
+            {
+              id: firstNameId,
+            },
+            'First',
+          ),
+          React.createElement(
+            'p',
+            {
+              'aria-labelledby': firstNameId,
+            },
+            'Sebastian',
+          ),
+          React.createElement(
+            'h2',
+            {
+              id: lastNameId,
+            },
+            'Last',
+          ),
+          React.createElement(
+            'p',
+            {
+              'aria-labelledby': lastNameId,
+            },
+            'Smith',
+          ),
+        );
+      }
+
+      const html = await ReactHTML.renderToMarkup(<Component />);
+      const container = document.createElement('div');
+      container.innerHTML = html;
+
+      expect(container.getElementsByTagName('h2')[0].id).toBe(
+        container.getElementsByTagName('p')[0].getAttribute('aria-labelledby'),
+      );
+      expect(container.getElementsByTagName('h2')[1].id).toBe(
+        container.getElementsByTagName('p')[1].getAttribute('aria-labelledby'),
+      );
+
+      // It's not the same id between them.
+      expect(container.getElementsByTagName('h2')[0].id).not.toBe(
+        container.getElementsByTagName('p')[1].getAttribute('aria-labelledby'),
+      );
+    });
+
+    // @gate disableClientCache
+    it('does NOT support cache yet because it is a client component', async () => {
+      let counter = 0;
+      const getCount = React.cache(() => {
+        return counter++;
+      });
+      function Component() {
+        const a = getCount();
+        const b = getCount();
+        return (
+          <div>
+            {a}
+            {b}
+          </div>
+        );
+      }
+
+      const html = await ReactHTML.renderToMarkup(<Component />);
+      expect(html).toBe('<div>01</div>');
+    });
   });
-});
+}
--- packages/react-html/src/__tests__/ReactHTMLServer-test.js
+++ packages/react-html/src/__tests__/ReactHTMLServer-test.js
@@ -15,150 +15,178 @@ global.TextEncoder = require('util').TextEncoder;
 let React;
 let ReactHTML;
 
-describe('ReactHTML', () => {
-  beforeEach(() => {
-    jest.resetModules();
-    // We run in the react-server condition.
-    jest.mock('react', () => require('react/react.react-server'));
-    jest.mock('react-html', () =>
-      require('react-html/react-html.react-server'),
-    );
-
-    React = require('react');
-    ReactHTML = require('react-html');
-  });
-
-  it('should be able to render a simple component', async () => {
-    function Component() {
-      // We can't use JSX because that's client-JSX in our tests.
-      return React.createElement('div', null, 'hello world');
+if (!__EXPERIMENTAL__) {
+  it('should not be built in stable', () => {
+    try {
+      require('react-html');
+    } catch (x) {
+      return;
     }
-
-    const html = await ReactHTML.renderToMarkup(React.createElement(Component));
-    expect(html).toBe('<div>hello world</div>');
+    throw new Error('Expected react-html not to exist in stable.');
   });
-
-  it('should prefix html tags with a doctype', async () => {
-    const html = await ReactHTML.renderToMarkup(
-      // We can't use JSX because that's client-JSX in our tests.
-      React.createElement(
-        'html',
-        null,
-        React.createElement('body', null, 'hello'),
-      ),
-    );
-    expect(html).toBe(
-      '<!DOCTYPE html><html><head></head><body>hello</body></html>',
-    );
-  });
-
-  it('should error on useState', async () => {
-    function Component() {
-      const [state] = React.useState('hello');
-      // We can't use JSX because that's client-JSX in our tests.
-      return React.createElement('div', null, state);
-    }
-
-    await expect(async () => {
-      await ReactHTML.renderToMarkup(React.createElement(Component));
-    }).rejects.toThrow();
-  });
-
-  it('should error on refs passed to host components', async () => {
-    function Component() {
-      const ref = React.createRef();
-      // We can't use JSX because that's client-JSX in our tests.
-      return React.createElement('div', {ref});
-    }
-
-    await expect(async () => {
-      await ReactHTML.renderToMarkup(React.createElement(Component));
-    }).rejects.toThrow();
-  });
-
-  it('should error on callbacks passed to event handlers', async () => {
-    function Component() {
-      function onClick() {
-        // This won't be able to be called.
+} else {
+  describe('ReactHTML', () => {
+    beforeEach(() => {
+      jest.resetModules();
+      // We run in the react-server condition.
+      jest.mock('react', () => require('react/react.react-server'));
+      if (__EXPERIMENTAL__) {
+        jest.mock('react-html', () =>
+          require('react-html/react-html.react-server'),
+        );
       }
-      // We can't use JSX because that's client-JSX in our tests.
-      return React.createElement('div', {onClick});
-    }
 
-    await expect(async () => {
-      await ReactHTML.renderToMarkup(React.createElement(Component));
-    }).rejects.toThrow();
-  });
+      React = require('react');
+      if (__EXPERIMENTAL__) {
+        ReactHTML = require('react-html');
+      } else {
+        try {
+          require('react-html/react-html.react-server');
+        } catch (x) {
+          return;
+        }
+        throw new Error('Expected react-html not to exist in stable.');
+      }
+    });
 
-  it('supports the useId Hook', async () => {
-    function Component() {
-      const firstNameId = React.useId();
-      const lastNameId = React.useId();
-      // We can't use JSX because that's client-JSX in our tests.
-      return React.createElement(
-        'div',
-        null,
+    it('should be able to render a simple component', async () => {
+      function Component() {
+        // We can't use JSX because that's client-JSX in our tests.
+        return React.createElement('div', null, 'hello world');
+      }
+
+      const html = await ReactHTML.renderToMarkup(
+        React.createElement(Component),
+      );
+      expect(html).toBe('<div>hello world</div>');
+    });
+
+    it('should prefix html tags with a doctype', async () => {
+      const html = await ReactHTML.renderToMarkup(
+        // We can't use JSX because that's client-JSX in our tests.
         React.createElement(
-          'h2',
-          {
-            id: firstNameId,
-          },
-          'First',
-        ),
-        React.createElement(
-          'p',
-          {
-            'aria-labelledby': firstNameId,
-          },
-          'Sebastian',
-        ),
-        React.createElement(
-          'h2',
-          {
-            id: lastNameId,
-          },
-          'Last',
-        ),
-        React.createElement(
-          'p',
-          {
-            'aria-labelledby': lastNameId,
-          },
-          'Smith',
+          'html',
+          null,
+          React.createElement('body', null, 'hello'),
         ),
       );
-    }
-
-    const html = await ReactHTML.renderToMarkup(React.createElement(Component));
-    const container = document.createElement('div');
-    container.innerHTML = html;
-
-    expect(container.getElementsByTagName('h2')[0].id).toBe(
-      container.getElementsByTagName('p')[0].getAttribute('aria-labelledby'),
-    );
-    expect(container.getElementsByTagName('h2')[1].id).toBe(
-      container.getElementsByTagName('p')[1].getAttribute('aria-labelledby'),
-    );
-
-    // It's not the same id between them.
-    expect(container.getElementsByTagName('h2')[0].id).not.toBe(
-      container.getElementsByTagName('p')[1].getAttribute('aria-labelledby'),
-    );
-  });
-
-  // @gate enableCache
-  it('supports cache', async () => {
-    let counter = 0;
-    const getCount = React.cache(() => {
-      return counter++;
+      expect(html).toBe(
+        '<!DOCTYPE html><html><head></head><body>hello</body></html>',
+      );
     });
-    function Component() {
-      const a = getCount();
-      const b = getCount();
-      return React.createElement('div', null, a, b);
-    }
 
-    const html = await ReactHTML.renderToMarkup(React.createElement(Component));
-    expect(html).toBe('<div>00</div>');
+    it('should error on useState', async () => {
+      function Component() {
+        const [state] = React.useState('hello');
+        // We can't use JSX because that's client-JSX in our tests.
+        return React.createElement('div', null, state);
+      }
+
+      await expect(async () => {
+        await ReactHTML.renderToMarkup(React.createElement(Component));
+      }).rejects.toThrow();
+    });
+
+    it('should error on refs passed to host components', async () => {
+      function Component() {
+        const ref = React.createRef();
+        // We can't use JSX because that's client-JSX in our tests.
+        return React.createElement('div', {ref});
+      }
+
+      await expect(async () => {
+        await ReactHTML.renderToMarkup(React.createElement(Component));
+      }).rejects.toThrow();
+    });
+
+    it('should error on callbacks passed to event handlers', async () => {
+      function Component() {
+        function onClick() {
+          // This won't be able to be called.
+        }
+        // We can't use JSX because that's client-JSX in our tests.
+        return React.createElement('div', {onClick});
+      }
+
+      await expect(async () => {
+        await ReactHTML.renderToMarkup(React.createElement(Component));
+      }).rejects.toThrow();
+    });
+
+    it('supports the useId Hook', async () => {
+      function Component() {
+        const firstNameId = React.useId();
+        const lastNameId = React.useId();
+        // We can't use JSX because that's client-JSX in our tests.
+        return React.createElement(
+          'div',
+          null,
+          React.createElement(
+            'h2',
+            {
+              id: firstNameId,
+            },
+            'First',
+          ),
+          React.createElement(
+            'p',
+            {
+              'aria-labelledby': firstNameId,
+            },
+            'Sebastian',
+          ),
+          React.createElement(
+            'h2',
+            {
+              id: lastNameId,
+            },
+            'Last',
+          ),
+          React.createElement(
+            'p',
+            {
+              'aria-labelledby': lastNameId,
+            },
+            'Smith',
+          ),
+        );
+      }
+
+      const html = await ReactHTML.renderToMarkup(
+        React.createElement(Component),
+      );
+      const container = document.createElement('div');
+      container.innerHTML = html;
+
+      expect(container.getElementsByTagName('h2')[0].id).toBe(
+        container.getElementsByTagName('p')[0].getAttribute('aria-labelledby'),
+      );
+      expect(container.getElementsByTagName('h2')[1].id).toBe(
+        container.getElementsByTagName('p')[1].getAttribute('aria-labelledby'),
+      );
+
+      // It's not the same id between them.
+      expect(container.getElementsByTagName('h2')[0].id).not.toBe(
+        container.getElementsByTagName('p')[1].getAttribute('aria-labelledby'),
+      );
+    });
+
+    // @gate enableCache
+    it('supports cache', async () => {
+      let counter = 0;
+      const getCount = React.cache(() => {
+        return counter++;
+      });
+      function Component() {
+        const a = getCount();
+        const b = getCount();
+        return React.createElement('div', null, a, b);
+      }
+
+      const html = await ReactHTML.renderToMarkup(
+        React.createElement(Component),
+      );
+      expect(html).toBe('<div>00</div>');
+    });
   });
-});
+}
