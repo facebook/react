309e146193c7b84d1c7a60d1a2ab2d6c836ba515
Sebastian Markbåge <sebastian@calyptus.eu>
2024-07-02 16:31:35 -0400

    Implement onError signature for renderToMarkup (#30170)
    Stacked on #30132.
    
    This way we can get parent and owner stacks from the error.
    
    This forces us to confront multiple errors and whether or not a Flight
    error that ends up being unobservable needs to really reject the render.
    
    This implements stashing of Flight errors with a digest and only errors
    if they end up erroring the Fizz render too. At this point they'll have
    parent stacks so we can surface those.

--- packages/react-html/src/ReactHTMLServer.js
+++ packages/react-html/src/ReactHTMLServer.js
@@ -9,8 +9,12 @@
 
 import type {ReactNodeList} from 'shared/ReactTypes';
 import type {LazyComponent} from 'react/src/ReactLazy';
+import type {ErrorInfo} from 'react-server/src/ReactFizzServer';
 
 import ReactVersion from 'shared/ReactVersion';
+
+import ReactSharedInternalsServer from 'react-server/src/ReactSharedInternalsServer';
+import ReactSharedInternalsClient from 'shared/ReactSharedInternals';
 
 import {
   createRequest as createFlightRequest,
@@ -62,6 +66,7 @@ type ReactMarkupNodeList =
 type MarkupOptions = {
   identifierPrefix?: string,
   signal?: AbortSignal,
+  onError?: (error: mixed, errorInfo: ErrorInfo) => ?string,
 };
 
 function noServerCallOrFormAction() {
@@ -109,17 +114,60 @@ export function renderToMarkup(
         reject(error);
       },
     };
-    function onError(error: mixed) {
+
+    let stashErrorIdx = 1;
+    const stashedErrors: Map<string, mixed> = new Map();
+
+    function handleFlightError(error: mixed): string {
+      // For Flight errors we don't immediately reject, because they might not matter
+      // to the output of the HTML. We stash the error with a digest in case we need
+      // to get to the original error from the Fizz render.
+      const id = '' + stashErrorIdx++;
+      stashedErrors.set(id, error);
+      return id;
+    }
+
+    function handleError(error: mixed, errorInfo: ErrorInfo) {
+      if (typeof error === 'object' && error !== null) {
+        const id = error.digest;
+        // Note that the original error might be `undefined` so we need a has check.
+        if (typeof id === 'string' && stashedErrors.has(id)) {
+          // Get the original error thrown inside Flight.
+          error = stashedErrors.get(id);
+        }
+      }
+
       // Any error rejects the promise, regardless of where it happened.
       // Unlike other React SSR we don't want to put Suspense boundaries into
       // client rendering mode because there's no client rendering here.
       reject(error);
+
+      const onError = options && options.onError;
+      if (onError) {
+        if (__DEV__) {
+          const prevGetCurrentStackImpl =
+            ReactSharedInternalsServer.getCurrentStack;
+          // We're inside a "client" callback from Fizz but we only have access to the
+          // "server" runtime so to get access to a stack trace within this callback we
+          // need to override it to get it from the client runtime.
+          ReactSharedInternalsServer.getCurrentStack =
+            ReactSharedInternalsClient.getCurrentStack;
+          try {
+            onError(error, errorInfo);
+          } finally {
+            ReactSharedInternalsServer.getCurrentStack =
+              prevGetCurrentStackImpl;
+          }
+        } else {
+          onError(error, errorInfo);
+        }
+      }
     }
     const flightRequest = createFlightRequest(
       // $FlowFixMe: This should be a subtype but not everything is typed covariant.
       children,
       null,
-      onError,
+      handleFlightError,
       options ? options.identifierPrefix : undefined,
       undefined,
       'Markup',
@@ -153,7 +201,7 @@ export function renderToMarkup(
       ),
       createRootFormatContext(),
       Infinity,
-      onError,
+      handleError,
       undefined,
       undefined,
       undefined,
