ff89ba734668fdac06e8de476486830bbf9e0785
Sebastian Markbåge <sebastian@calyptus.eu>
2024-07-12 14:39:38 -0400

    Disable consoleWithStackDev Transform except in RN/WWW (#30313)
    Stacked on #30308.
    
    This is now a noop module so we can stop applying the transform of
    console.error using the Babel plugin in the mainline builds. I'm keeping
    the transform for RN/WWW for now although it might be nice if the
    transform moved into those systems as it gets synced instead of keeping
    it upstream.
    
    In jest tests we're already not running the forks for RN/WWW so we don't
    need it at all there.

--- packages/shared/consoleWithStackDev.js
+++ packages/shared/consoleWithStackDev.js
@@ -5,36 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export function setSuppressWarning(newSuppressWarning) {
-  // TODO: Noop. Delete.
-}
-
-// In DEV, calls to console.warn and console.error get replaced
-// by calls to these methods by a Babel plugin.
+// We expect that our Rollup, Jest, and Flow configurations
+// always shim this module with the corresponding environment
+// (either rn or www).
 //
-// In PROD (or in packages without access to React internals),
-// they are left as they are instead.
+// We should never resolve to this file, but it exists to make
+// sure that if we *do* accidentally break the configuration,
+// the failure isn't silent.
 
-export function warn(format, ...args) {
-  if (__DEV__) {
-    printWarning('warn', format, args);
-  }
-}
-
-export function error(format, ...args) {
-  if (__DEV__) {
-    printWarning('error', format, args);
-  }
-}
-
-function printWarning(level, format, args) {
-  // When changing this logic, you might want to also
-  // update consoleWithStackDev.www.js as well.
-  if (__DEV__) {
-    args.unshift(format);
-    // We intentionally don't use spread (or .apply) directly because it
-    // breaks IE9: https://github.com/facebook/react/issues/13610
-    // eslint-disable-next-line react-internal/no-production-logging
-    Function.prototype.apply.call(console[level], console, args);
-  }
+export function setSuppressWarning() {
+  // TODO: Delete this and error when even importing this module.
 }
