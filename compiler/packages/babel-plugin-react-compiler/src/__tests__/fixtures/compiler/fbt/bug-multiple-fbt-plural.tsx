81180acb7b2d22548f6b77db94a07f05f10f11fe
Mofei Zhang <feifei0@meta.com>
2024-07-23 14:58:49 -0400

    Update on "[compiler][repro] Test fixture for fbt plural bug"
    [ghstack-poisoned]

--- compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/fbt/bug-multiple-fbt-plural.tsx
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/fbt/bug-multiple-fbt-plural.tsx
@@ -7,6 +7,19 @@ import fbt from 'fbt';
  *   (kind: ok) 1 rewrite to Rust · 2 months traveling
  *   Forget:
  *   (kind: ok) 1 rewrites to Rust · 2 months traveling
+ *
+ * The root issue here is that fbt:plural reads `.start` and `.end` from
+ * babel nodes to slice into source strings. (fbt:enum suffers from the same
+ * problem).
+ * See:
+ *  - [getRawSource](https://github.com/facebook/fbt/blob/main/packages/babel-plugin-fbt/src/FbtUtil.js#L666-L673)
+ *  - [getArgCode](https://github.com/facebook/fbt/blob/main/packages/babel-plugin-fbt/src/fbt-nodes/FbtArguments.js#L88-L97)
+ *  - [_getStringVariationCombinations](https://github.com/facebook/fbt/blob/main/packages/babel-plugin-fbt/src/JSFbtBuilder.js#L297)
+ *
+ * Specifically, the `count` node requires that a `.start/.end` be attached
+ * (see [code in FbtPluralNode](https://github.com/facebook/fbt/blob/main/packages/babel-plugin-fbt/src/fbt-nodes/FbtPluralNode.js#L87-L90))
+ *
+ * In this fixture, `count` nodes are the `rewrites` and `months` identifiers.
  */
 function Foo({rewrites, months}) {
   return (
