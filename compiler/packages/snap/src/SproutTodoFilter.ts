a15e8d7cdc6289c8685588d2821cb7c904141e3e
Mofei Zhang <feifei0@meta.com>
2024-07-26 17:36:03 -0400

    [compiler][repro] Test fixture for fbt plural bug
    ghstack-source-id: 222e7312c081faf36702a1c790a4e336b5bcc928
    Pull Request resolved: https://github.com/facebook/react/pull/30432

--- compiler/packages/snap/src/SproutTodoFilter.ts
+++ compiler/packages/snap/src/SproutTodoFilter.ts
@@ -485,6 +485,7 @@ const skipFilter = new Set([
   'rules-of-hooks/rules-of-hooks-69521d94fa03',
 
   // bugs
+  'fbt/bug-multiple-fbt-plural',
   'fbt/bug-fbt-preserve-whitespace-param',
   'bug-invalid-hoisting-functionexpr',
   'original-reactive-scopes-fork/bug-nonmutating-capture-in-unsplittable-memo-block',
