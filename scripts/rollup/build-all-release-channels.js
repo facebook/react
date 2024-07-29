6e293859f66925767ca4346f3f7b8087aa1f97d4
Lauren Tan <poteto@users.noreply.github.com>
2024-07-29 15:38:31 -0400

    Update
    [ghstack-poisoned]

--- scripts/rollup/build-all-release-channels.js
+++ scripts/rollup/build-all-release-channels.js
@@ -77,8 +77,8 @@ const argv = yargs.wrap(yargs.terminalWidth()).options({
   ci: {
     describe: 'Run tests in CI',
     requiresArg: false,
-    type: 'choices',
-    choices: ['circleci', 'github'],
+    type: 'boolean',
+    default: false,
   },
   type: {
     describe: `Build the given bundle type. (${Object.values(
@@ -110,7 +110,7 @@ const argv = yargs.wrap(yargs.terminalWidth()).options({
 }).argv;
 
 async function main() {
-  if (argv.ci === 'github') {
+  if (argv.ci === true) {
     await buildEverything(argv.index, argv.total);
     switch (argv.releaseChannel) {
       case 'stable': {
@@ -139,7 +139,7 @@ async function main() {
     // Then merge the experimental folder into the stable one. processExperimental
     // will have already removed conflicting files.
     //
-    // In CI, merging is handled automatically by CircleCI's workspace feature.
+    // In CI, merging is handled by the GitHub Download Artifacts plugin.
     mergeDirsSync(experimentalDir + '/', stableDir + '/');
 
     // Now restore the combined directory back to its original name
