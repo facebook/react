f06a62e4dc4f33069e839c9209e2bc882e122f58
ns-op <ns@efficient-engineering.com>
2024-08-02 19:53:40 -0400

    add debug

--- replay_commits_benchmark.py
+++ replay_commits_benchmark.py
@@ -321,6 +321,11 @@ def push_commits_one_by_one(args, repo, commits):
             export_blob(data, path)
 
         for folder in [".circleci", ".github"]:
+            
+            print(f"{folder}")
+            print(f"{args['working_repo_dir']}")
+            print(f"{config_path}")
+
             source_dir = os.path.join(args['working_repo_dir'], "/", config_path, folder)
             destination_dir = os.path.join(args['working_repo_dir'], "/", folder)
 
