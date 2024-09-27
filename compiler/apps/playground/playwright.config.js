dbb8133deeffa82a82fc0a31d47625edb90feb49
Lauren Tan <poteto@users.noreply.github.com>
2024-09-27 11:14:59 -0400

    Update
    [ghstack-poisoned]

--- compiler/apps/playground/playwright.config.js
+++ compiler/apps/playground/playwright.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: 'yarn build:compiler && yarn dev',
+    command: 'yarn dev',
     url: baseURL,
     timeout: 300 * 1000,
     reuseExistingServer: !process.env.CI,
