511f10e4d5c64bb857817eb6a474a3ca352e0da1
Lauren Tan <poteto@users.noreply.github.com>
2024-10-07 15:18:21 -0400

    Update
    [ghstack-poisoned]

--- compiler/packages/snap/src/sprout/evaluator.ts
+++ compiler/packages/snap/src/sprout/evaluator.ts
@@ -282,7 +282,7 @@ export function doEval(source: string): EvaluatorResult {
         } else {
           return {
             kind: "exception",
-            value: e.message,
+            value: e.stack,
           };
         }
       }
