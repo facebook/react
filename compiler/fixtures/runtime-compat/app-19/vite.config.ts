b9c4870c81343fcbe10112563b68da6b5bb9430a
lauren <poteto@users.noreply.github.com>
2024-10-09 20:32:52 -0400

    [compiler] Consume compiled lib in react 19 app
    it works

--- compiler/fixtures/runtime-compat/app-19/vite.config.ts
+++ compiler/fixtures/runtime-compat/app-19/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {plugins: [['babel-plugin-react-compiler', {target: '19'}]]},
+    }),
+  ],
 });
