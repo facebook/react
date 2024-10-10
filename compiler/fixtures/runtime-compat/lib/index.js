5aeeb582b261f83ae137260e7e7a8e3a4859abc3
lauren <poteto@users.noreply.github.com>
2024-10-09 20:42:57 -0400

    [compiler] Compile lib
    Add and compile a simple hook with rollup and babel.

--- compiler/fixtures/runtime-compat/lib/index.js
+++ compiler/fixtures/runtime-compat/lib/index.js
@@ -1,1 +1,13 @@
-throw new Error('Not implemented yet');
+import {useState, useEffect} from 'react';
+
+export function useTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  return time;
+}
