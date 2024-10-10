8535869f14403b46b5ed450291b6d1ff3974cbd0
lauren <poteto@users.noreply.github.com>
2024-10-09 20:42:57 -0400

    [compiler] Consume compiled lib in react 19 app
    it works

--- compiler/fixtures/runtime-compat/app-19/src/App.tsx
+++ compiler/fixtures/runtime-compat/app-19/src/App.tsx
@@ -1,33 +1,14 @@
-import {useState} from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import 'react';
 import './App.css';
+// @ts-expect-error no types
+import {useTime} from 'runtime-compat-lib';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const time = useTime();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React 19</h1>
+      <span>Current time: {time.toLocaleString()}</span>
     </>
   );
 }
