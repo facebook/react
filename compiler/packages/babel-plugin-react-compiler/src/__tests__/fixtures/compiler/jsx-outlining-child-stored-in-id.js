c91b3b090ad406fcd103483de0abb6adf44b6f48
Sathya Gunasekaran <gsathya.ceg@gmail.com>
2024-10-17 18:15:32 +0100

    JSX Outlining (#30956)
    Currently, the react compiler can not compile within callbacks which can
    potentially cause over rendering. Consider this example:
    ```jsx
    function Component(countries, onDelete) {
      const name = useFoo();
      return countries.map(() => {
        return (
          <Foo>
            <Bar name={name}/>
            <Baz onclick={onDelete} />
          </Foo>
        );
      });
    }
    ```
    
    In this case, there's no memoization of the nested jsx elements. But
    instead if we were to manually refactor the nested jsx into separate
    component like this:
    ```jsx
    function Component(countries, onDelete) {
      const name = useFoo();
      return countries.map(() => {
        return <Temp name={name} onDelete={onDelete} />;
      });
    }
    
    function Temp({ name, onDelete }) {
      return (
        <Foo>
          <Bar name={name} />
          <Baz onclick={onDelete} />
        </Foo>
      );
    }
    
    ```
    
    The compiler can now optimise both these components:
    ```jsx
    function Component(countries, onDelete) {
      const $ = _c(4);
      const name = useFoo();
      let t0;
      if ($[0] !== name || $[1] !== onDelete || $[2] !== countries) {
        t0 = countries.map(() => <Temp name={name} onDelete={onDelete} />);
        $[0] = name;
        $[1] = onDelete;
        $[2] = countries;
        $[3] = t0;
      } else {
        t0 = $[3];
      }
      return t0;
    }
    
    function Temp(t0) {
      const $ = _c(7);
      const { name, onDelete } = t0;
      let t1;
      if ($[0] !== name) {
        t1 = <Bar name={name} />;
        $[0] = name;
        $[1] = t1;
      } else {
        t1 = $[1];
      }
      let t2;
      if ($[2] !== onDelete) {
        t2 = <Baz onclick={onDelete} />;
        $[2] = onDelete;
        $[3] = t2;
      } else {
        t2 = $[3];
      }
      let t3;
      if ($[4] !== t1 || $[5] !== t2) {
        t3 = (
          <Foo>
            {t1}
            {t2}
          </Foo>
        );
        $[4] = t1;
        $[5] = t2;
        $[6] = t3;
      } else {
        t3 = $[6];
      }
      return t3;
    }
    ```
    
    Now, when `countries` is updated by adding one single value, only the
    newly added value is re-rendered and not the entire list. Rather than
    having to do this manually, this PR teaches the react compiler to do
    this transformation.
    
    This PR adds a new pass (`OutlineJsx`) to capture nested jsx statements
    and outline them in a separate component. This newly outlined component
    can then by memoized by the compiler, giving us more fine grained
    rendering.

---
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/jsx-outlining-child-stored-in-id.js
@@ -0,0 +1,40 @@
+// @enableJsxOutlining
+function Component(arr) {
+  const x = useX();
+  return arr.map(i => {
+    <>
+      {arr.map((i, id) => {
+        let child = (
+          <Bar x={x}>
+            <Baz i={i}></Baz>
+          </Bar>
+        );
+
+        let jsx = <div>{child}</div>;
+        return jsx;
+      })}
+    </>;
+  });
+}
+
+function Bar({x, children}) {
+  return (
+    <>
+      {x}
+      {children}
+    </>
+  );
+}
+
+function Baz({i}) {
+  return <>{i}</>;
+}
+
+function useX() {
+  return 'x';
+}
+
+export const FIXTURE_ENTRYPOINT = {
+  fn: Component,
+  params: [{arr: ['foo', 'bar']}],
+};
