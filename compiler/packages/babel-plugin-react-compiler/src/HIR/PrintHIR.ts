8e5c4c1bd61a8e0fdfae3e38c9d30f3f6e11dcce
Mofei Zhang <feifei0@meta.com>
2024-10-24 11:11:15 -0700

    [compiler] Delete LoweredFunction.dependencies and hoisted instructions
    LoweredFunction dependencies were exclusively used for dependency extraction (in `propagateScopeDeps`). Now that we have a `propagateScopeDepsHIR` that recursively traverses into nested functions, we can delete `dependencies` and their associated artificial `LoadLocal`/`PropertyLoad` instructions.
    
    '

--- compiler/packages/babel-plugin-react-compiler/src/HIR/PrintHIR.ts
+++ compiler/packages/babel-plugin-react-compiler/src/HIR/PrintHIR.ts
@@ -538,9 +538,6 @@ export function printInstructionValue(instrValue: ReactiveValue): string {
         .split('\n')
         .map(line => `      ${line}`)
         .join('\n');
-      const deps = instrValue.loweredFunc.dependencies
-        .map(dep => printPlace(dep))
-        .join(',');
       const context = instrValue.loweredFunc.func.context
         .map(dep => printPlace(dep))
         .join(',');
@@ -557,7 +554,7 @@ export function printInstructionValue(instrValue: ReactiveValue): string {
           })
           .join(', ') ?? '';
       const type = printType(instrValue.loweredFunc.func.returnType).trim();
-      value = `${kind} ${name} @deps[${deps}] @context[${context}] @effects[${effects}]${type !== '' ? ` return${type}` : ''}:\n${fn}`;
+      value = `${kind} ${name} @context[${context}] @effects[${effects}]${type !== '' ? ` return${type}` : ''}:\n${fn}`;
       break;
     }
     case 'TaggedTemplateExpression': {
--- compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/constant-propagation-into-function-expressions.expect.md
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/constant-propagation-into-function-expressions.expect.md
@@ -18,18 +18,19 @@ function Component(props) {
 import { c as _c } from "react/compiler-runtime";
 function Component(props) {
   const $ = _c(1);
+
+  const onEvent = _temp;
   let t0;
   if ($[0] === Symbol.for("react.memo_cache_sentinel")) {
-    const onEvent = () => {
-      console.log(42);
-    };
-
     t0 = <Foo onEvent={onEvent} />;
     $[0] = t0;
   } else {
     t0 = $[0];
   }
   return t0;
+}
+function _temp() {
+  console.log(42);
 }
 
 ```
