2655cc86e003586035c2fc456cd6451b53305e8e
Saul Lee <solleedata@gmail.com>
2024-10-30 13:10:31 +0900

    test: add fixture for `valueOf` returning `this`

---
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/error.object-shape-value-of.js
@@ -0,0 +1,18 @@
+import {ValidateMemoization} from 'shared-runtime';
+
+function Component() {
+  const x = {
+    valueOf() {
+      return this;
+    }
+  };
+  x.valueOf().y = true;
+
+  return <ValidateMemoization inputs={[x]} output={x} />;
+}
+
+export const FIXTURE_ENTRYPOINT = {
+  fn: Component,
+  params: [{}],
+  sequentialRenders: [{}, {}],
+};
