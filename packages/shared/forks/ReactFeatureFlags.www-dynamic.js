7c0c424efe9590471cb423b992b0ec8d7f4efb3c
Jan Kassens <jkassens@meta.com>
2024-11-06 14:00:30 -0500

    [www] set disableLegacyMode to true

--- packages/shared/forks/ReactFeatureFlags.www-dynamic.js
+++ packages/shared/forks/ReactFeatureFlags.www-dynamic.js
@@ -16,7 +16,6 @@
 export const alwaysThrottleRetries = true;
 export const disableDefaultPropsExceptForClasses = __VARIANT__;
 export const disableLegacyContextForFunctionComponents = __VARIANT__;
-export const disableLegacyMode = __VARIANT__;
 export const disableSchedulerTimeoutInWorkLoop = __VARIANT__;
 export const enableDeferRootSchedulingToMicrotask = __VARIANT__;
 export const enableDO_NOT_USE_disableStrictPassiveEffect = __VARIANT__;
