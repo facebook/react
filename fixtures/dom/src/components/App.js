d3db12b09dd84686dd8950c502b2fd3c6f44b999
Vincent Anjiri <vincentanjiri12@gmail.com>
2024-11-07 18:45:57 +0300

    Convert App Component to Functional Component
    - Replaced class-based component with a stateless functional component.
    - Removed class component lifecycle methods, as they were not needed.

--- fixtures/dom/src/components/App.js
+++ fixtures/dom/src/components/App.js
@@ -1,18 +1,15 @@
+import React from 'react';
 import Header from './Header';
-import Fixtures from './fixtures';
+import Fixtures from './Fixtures';
 import '../style.css';
 
-const React = window.React;
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Fixtures />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Fixtures />
+    </div>
+  );
 }
 
 export default App;
