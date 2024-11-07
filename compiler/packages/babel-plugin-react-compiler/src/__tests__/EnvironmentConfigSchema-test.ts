1d5ca87f2c354777fc1255bdad43c0560b7e4547
Gabriel Inzirillo <gabriel@inzirillo.dev>
2024-11-07 22:05:24 +0100

    Fix tests

--- compiler/packages/babel-plugin-react-compiler/src/__tests__/EnvironmentConfigSchema-test.ts
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/EnvironmentConfigSchema-test.ts
@@ -1,13 +1,20 @@
-import {validateEnvironmentConfig} from '../HIR/Environment';
-import {CompilerError} from '../CompilerError';
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-describe('EnvironmentConfigSchema', () => {
+import {CompilerError, validateEnvironmentConfig} from '..';
+
+describe('validateEnvironmentConfig()', () => {
   it('should throw a CompilerError for unknown properties', () => {
     const invalidConfig = {
       unknownProperty: true,
     };
 
     expect(() => {
+      // @ts-expect-error - This is to simulate the usage outside of the package when the types are not available or if used with javascript.
       validateEnvironmentConfig(invalidConfig);
     }).toThrow(CompilerError);
   });
