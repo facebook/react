a35a1a47a8cf19c008254f8a94a0393c7fc52808
suji park <charmmming7@gmail.com>
2024-11-10 20:10:18 +0900

    Prevents type errors from occurring when queue values ​​are applied as null.

--- packages/react-reconciler/src/ReactFiberHooks.js
+++ packages/react-reconciler/src/ReactFiberHooks.js
@@ -3200,7 +3200,7 @@ export function startHostTransition<F>(
 
 function ensureFormComponentIsStateful(formFiber: Fiber) {
   const existingStateHook: Hook | null = formFiber.memoizedState;
-  if (existingStateHook !== null) {
+  if (existingStateHook !== null && existingStateHook.next !== null) {
     // This fiber was already upgraded to be stateful.
     return existingStateHook;
   }
@@ -3288,6 +3288,9 @@ export function requestFormReset(formFiber: Fiber) {
   }
 
   const stateHook = ensureFormComponentIsStateful(formFiber);
+  const isEmpty = stateHook === null || stateHook.length === 0;
+  if (isEmpty) return;
+
   const newResetState = {};
   const resetStateHook: Hook = (stateHook.next: any);
   const resetStateQueue = resetStateHook.queue;
@@ -3537,6 +3540,8 @@ function dispatchSetStateInternal<S, A>(
   action: A,
   lane: Lane,
 ): boolean {
+  const isEmptyQueue = queue === null || queue.length === 0;
+  if (isEmptyQueue) return;
   const update: Update<S, A> = {
     lane,
     revertLane: NoLane,
