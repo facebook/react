2ec26bc4323673d1f2035191d3aaf0a18b20d488
mofeiZ <34200447+mofeiZ@users.noreply.github.com>
2024-11-11 18:04:29 -0500

    [compiler] Repro for mutable range edge case (#31479)
    See test fixtures

--- compiler/packages/snap/src/SproutTodoFilter.ts
+++ compiler/packages/snap/src/SproutTodoFilter.ts
@@ -480,6 +480,8 @@ const skipFilter = new Set([
   'fbt/bug-fbt-plural-multiple-mixed-call-tag',
   'bug-object-expression-computed-key-modified-during-after-construction-hoisted-sequence-expr',
   'bug-invalid-hoisting-functionexpr',
+  'bug-aliased-capture-aliased-mutate',
+  'bug-aliased-capture-mutate',
   'bug-functiondecl-hoisting',
   'bug-try-catch-maybe-null-dependency',
   'reduce-reactive-deps/bug-infer-function-cond-access-not-hoisted',
