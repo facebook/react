0e9d7631d65ef3afa776f7e2ff630d349bcd8ea4
lauren <poteto@users.noreply.github.com>
2024-11-11 10:54:28 -0500

    [crud] Basic implementation
    This PR introduces a new experimental hook `useResourceEffect`, which is something that we're doing some very early initial tests on.
    
    This may likely not pan out and will be removed or modified if so. Please do not rely on it as it will break.

--- packages/react-reconciler/src/ReactFiberCallUserSpace.js
+++ packages/react-reconciler/src/ReactFiberCallUserSpace.js
@@ -14,6 +14,7 @@ import type {CapturedValue} from './ReactCapturedValue';
 
 import {isRendering, setIsRendering} from './ReactCurrentFiber';
 import {captureCommitPhaseError} from './ReactFiberWorkLoop';
+import {enableUseResourceEffectHook} from 'shared/ReactFeatureFlags';
 
 // These indirections exists so we can exclude its stack frame in DEV (and anything below it).
 // TODO: Consider marking the whole bundle instead of these boundaries.
@@ -177,11 +178,15 @@ export const callComponentWillUnmountInDEV: (
 
 const callCreate = {
   'react-stack-bottom-frame': function (effect: Effect): (() => void) | void {
-    const create = effect.create;
-    const inst = effect.inst;
-    const destroy = create();
-    inst.destroy = destroy;
-    return destroy;
+    if (!enableUseResourceEffectHook) {
+      const create = effect.create;
+      const inst = effect.inst;
+      const destroy = create();
+      // $FlowFixMe[incompatible-type] (@poteto)
+      inst.destroy = destroy;
+      // $FlowFixMe[incompatible-return] (@poteto)
+      return destroy;
+    }
   },
 };
 
