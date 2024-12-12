c86542b240375f95dfd14af5b8ec39e29881e74d
Andrew Clark <git@andrewclark.io>
2024-12-12 14:10:46 -0500

    Bump next prerelease version numbers (#31676)
    Updates the version numbers in the prerelease (canary and experimental)
    channels.
    
    ---------
    
    Co-authored-by: Jack Pope <jackpope1@gmail.com>

--- ReactVersions.js
+++ ReactVersions.js
@@ -7,18 +7,18 @@
 //
 // The @latest channel uses the version as-is, e.g.:
 //
-//   19.0.0
+//   19.1.0
 //
 // The @canary channel appends additional information, with the scheme
 // <version>-<label>-<commit_sha>, e.g.:
 //
-//   19.0.0-canary-a1c2d3e4
+//   19.1.0-canary-a1c2d3e4
 //
 // The @experimental channel doesn't include a version, only a date and a sha, e.g.:
 //
 //   0.0.0-experimental-241c4467e-20200129
 
-const ReactVersion = '19.0.0';
+const ReactVersion = '19.1.0';
 
 // The label used by the @canary channel. Represents the upcoming release's
 // stability. Most of the time, this will be "canary", but we may temporarily
@@ -33,8 +33,8 @@ const canaryChannelLabel = 'rc';
 const rcNumber = 1;
 
 const stablePackages = {
-  'eslint-plugin-react-hooks': '5.1.0',
-  'jest-react': '0.16.0',
+  'eslint-plugin-react-hooks': '5.2.0',
+  'jest-react': '0.17.0',
   react: ReactVersion,
   'react-art': ReactVersion,
   'react-dom': ReactVersion,
@@ -42,12 +42,12 @@ const stablePackages = {
   'react-server-dom-turbopack': ReactVersion,
   'react-server-dom-parcel': ReactVersion,
   'react-is': ReactVersion,
-  'react-reconciler': '0.31.0',
-  'react-refresh': '0.16.0',
+  'react-reconciler': '0.32.0',
+  'react-refresh': '0.17.0',
   'react-test-renderer': ReactVersion,
-  'use-subscription': '1.10.0',
-  'use-sync-external-store': '1.4.0',
-  scheduler: '0.25.0',
+  'use-subscription': '1.11.0',
+  'use-sync-external-store': '1.5.0',
+  scheduler: '0.26.0',
 };
 
 // These packages do not exist in the @canary or @latest channel, only
