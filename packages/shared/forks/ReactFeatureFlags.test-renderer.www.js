d7fad4e8331e7d5e60bddb8dea8b8891d186f651
Rick Hanlon <rickhanlonii@fb.com>
2024-12-16 10:00:39 -0500

    Enable disableDefaultPropsExceptForClasses

--- packages/shared/forks/ReactFeatureFlags.test-renderer.www.js
+++ packages/shared/forks/ReactFeatureFlags.test-renderer.www.js
@@ -73,7 +73,7 @@ export const enableInfiniteRenderLoopDetection = false;
 export const enableReactTestRendererWarning = false;
 export const disableLegacyMode = true;
 
-export const disableDefaultPropsExceptForClasses = false;
+export const disableDefaultPropsExceptForClasses = true;
 
 export const renameElementSymbol = false;
 
