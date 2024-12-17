363f1eea3a31ec9005a853fda33f14a3ad5564cb
gautamSH20 <gs841400@gmail.com>
2024-12-17 11:02:33 +0530

    changing frameYieldMS from 5 to 16.7

--- packages/scheduler/src/SchedulerFeatureFlags.js
+++ packages/scheduler/src/SchedulerFeatureFlags.js
@@ -9,7 +9,7 @@
 
 export const enableSchedulerDebugging = false;
 export const enableProfiling = false;
-export const frameYieldMs = 5;
+export const frameYieldMs = 16.7;
 
 export const userBlockingPriorityTimeout = 250;
 export const normalPriorityTimeout = 5000;
