2a5cb955d471519d746a02d62732f500d68de41f
Hazem Ali <y3m@hotmail.com>
2024-12-25 11:08:21 +0200

    fix-performance-import: runner.ts

--- compiler/packages/snap/src/runner.ts
+++ compiler/packages/snap/src/runner.ts
@@ -22,6 +22,7 @@ import {
   watchSrc,
 } from './runner-watch';
 import * as runnerWorker from './runner-worker';
+import { performance } from 'perf_hooks';
 
 const WORKER_PATH = require.resolve('./runner-worker.js');
 const NUM_WORKERS = cpus().length - 1;
