841a9d731ab05ff11df423f00962b5d37582d3b0
Rick Hanlon <rickhanlonii@fb.com>
2025-01-05 15:06:22 -0500

    fix test

--- packages/react-dom/src/__tests__/ReactErrorBoundaries-test.internal.js
+++ packages/react-dom/src/__tests__/ReactErrorBoundaries-test.internal.js
@@ -2401,9 +2401,7 @@ describe('ReactErrorBoundaries', () => {
     }).rejects.toThrow(
       'Element type is invalid: ' +
         'expected a string (for built-in components) or a ' +
-        'class/function (for composite components) but got: undefined. ' +
-        "You likely forgot to export your component from the file it's defined in, " +
-        'or you might have mixed up default and named imports.',
+        'class/function (for composite components) but got: undefined.',
     );
     if (!gate('enableOwnerStacks')) {
       assertConsoleErrorDev(
