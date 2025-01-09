20b497614d4a2a6240f14cb24e71a050521ee66f
Sebastian Markbage <sebastian@calyptus.eu>
2025-01-08 23:14:41 -0500

    Wait for fonts before committing the view transition

--- fixtures/view-transition/src/components/Chrome.js
+++ fixtures/view-transition/src/components/Chrome.js
@@ -12,6 +12,16 @@ export default class Chrome extends Component {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="shortcut icon" href="favicon.ico" />
           <link rel="stylesheet" href={assets['main.css']} />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
+            rel="stylesheet"
+          />
           <title>{this.props.title}</title>
         </head>
         <body>
