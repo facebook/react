f5af43493b40cefc3ee29783180d3d6a9eb49373
Mofei Zhang <feifei0@meta.com>
2025-01-19 15:18:17 -0500

    [compiler][ez] Patch for JSX escape sequences in @babel/generator
    Fall back to using JSXExpressionContainer for strings potentially containing escape sequences (a single backslash) to fix  https://github.com/facebook/react/issues/32123. This is an extension of https://github.com/facebook/react/pull/29079

--- compiler/packages/babel-plugin-react-compiler/src/ReactiveScopes/CodegenReactiveFunction.ts
+++ compiler/packages/babel-plugin-react-compiler/src/ReactiveScopes/CodegenReactiveFunction.ts
@@ -2269,7 +2269,7 @@ function codegenInstructionValue(
  * https://en.wikipedia.org/wiki/List_of_Unicode_characters#Control_codes
  */
 const STRING_REQUIRES_EXPR_CONTAINER_PATTERN =
-  /[\u{0000}-\u{001F}\u{007F}\u{0080}-\u{FFFF}]|"/u;
+  /[\u{0000}-\u{001F}\u{007F}\u{0080}-\u{FFFF}]|"|\\/u;
 function codegenJsxAttribute(
   cx: Context,
   attribute: JsxAttribute,
--- compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/jsx-preserve-escape-character.expect.md
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/jsx-preserve-escape-character.expect.md
@@ -38,7 +38,7 @@ function MyApp() {
   const $ = _c(1);
   let t0;
   if ($[0] === Symbol.for("react.memo_cache_sentinel")) {
-    t0 = <input pattern="\\w" />;
+    t0 = <input pattern={"\\w"} />;
     $[0] = t0;
   } else {
     t0 = $[0];
