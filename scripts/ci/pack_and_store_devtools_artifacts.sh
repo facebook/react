0c88b6452141e5b5dc7ea337f0ebe8246d599c66
Lauren Tan <poteto@users.noreply.github.com>
2025-01-29 16:18:43 -0500

    [ci] Try to parallelize devtools builds
    Building DevTools is currently the long pole for the runtime CI job. Let's see if we can get the overall runtime for runtime build and test down by speeding this one step up.

--- scripts/ci/pack_and_store_devtools_artifacts.sh
+++ scripts/ci/pack_and_store_devtools_artifacts.sh
@@ -17,9 +17,16 @@ npm pack
 mv ./react-devtools-inline*.tgz ../../build/devtools/
 
 cd ../react-devtools-extensions
-yarn build
-mv ./chrome/build/ReactDevTools.zip ../../build/devtools/chrome-extension.zip
-mv ./firefox/build/ReactDevTools.zip ../../build/devtools/firefox-extension.zip
+if [[ -n "$1" ]]; then
+  yarn build:$1
+  if [[ "$1" != "edge" ]]; then
+    mv ./$1/build/ReactDevTools.zip ../../build/devtools/$1-extension.zip
+  fi
+else
+  yarn build
+  mv ./chrome/build/ReactDevTools.zip ../../build/devtools/chrome-extension.zip
+  mv ./firefox/build/ReactDevTools.zip ../../build/devtools/firefox-extension.zip
+fi
 
 # Compress all DevTools artifacts into a single tarball for easy download
 cd ../../build/devtools
