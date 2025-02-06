cb1fef44f056461c08a4b5db4efafde99da096cc
Eli White <github@eli-white.com>
2025-02-05 18:08:14 -0800

    Adding a test for nested ternaries

---
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/constant-propagation-ternary-nested-unary.js
@@ -0,0 +1,32 @@
+// @enableTernaryConstantPropagation
+import {Stringify} from 'shared-runtime';
+
+function foo() {
+  let _b;
+  const b = true;
+  _b = !b ? 'bar' : b ? 'foo' : 'baz';
+
+  return (
+    <Stringify
+      value={{
+        _b,
+        b0: !true,
+        n0: !0,
+        n1: !1,
+        n2: !2,
+        n3: !-1,
+        s0: !'',
+        s1: !'a',
+        s2: !'ab',
+        u: !undefined,
+        n: !null,
+      }}
+    />
+  );
+}
+
+export const FIXTURE_ENTRYPOINT = {
+  fn: foo,
+  params: [],
+  isComponent: false,
+};
