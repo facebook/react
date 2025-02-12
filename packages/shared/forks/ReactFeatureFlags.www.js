703de005d90e03db1294bcf93bc87e1c3a21bc7c
Rick Hanlon <rickhanlonii@fb.com>
2025-02-11 18:42:46 -0800

    [flags] make enableComponentPerformanceTrack dynamic

--- packages/shared/forks/ReactFeatureFlags.www.js
+++ packages/shared/forks/ReactFeatureFlags.www.js
@@ -39,6 +39,7 @@ export const {
   enableRemoveConsolePatches,
   enableFastAddPropertiesInDiffing,
   enableViewTransition,
+  enableComponentPerformanceTrack,
 } = dynamicFeatureFlags;
 
 // On WWW, __EXPERIMENTAL__ is used for a new modern build.
@@ -63,8 +64,6 @@ export const enableYieldingBeforePassive = false;
 export const enableThrottledScheduling = false;
 
 export const enableHydrationLaneScheduling = true;
-
-export const enableComponentPerformanceTrack = false;
 
 // Logs additional User Timing API marks for use with an experimental profiling tool.
 export const enableSchedulingProfiler: boolean =
