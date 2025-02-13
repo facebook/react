9e7a65ae1024d207e4328f5576bfe8a9f9bde520
Lauren Tan <poteto@users.noreply.github.com>
2025-02-13 16:06:06 -0500

    [eslint] Run typescript as part of setupGlobal
    Now that eslint-plugin-react-hooks is written in typescript, we need a
    pre-step in jest to build the plugin prior to running tests. It's a bit
    unfortunate that there are effectively 2 build setups for the eslint
    plugin: one from rollup as part of the main React build, and a
    standalone tsup (esbuild) step for tests. We should unify these in a
    follow up PR.

--- scripts/jest/setupGlobal.js
+++ scripts/jest/setupGlobal.js
@@ -1,7 +1,20 @@
 /* eslint-disable */
 
-module.exports = () => {
+const {join} = require('path');
+const {exec} = require('child-process-promise');
+
+module.exports = async () => {
   // can use beforeEach/afterEach or setupEnvironment.js in node >= 13: https://github.com/nodejs/node/pull/20026
   // jest's `setupFiles` is too late: https://stackoverflow.com/a/56482581/3406963
   process.env.TZ = 'UTC';
+
+  const cwd = join(
+    __dirname,
+    '..',
+    '..',
+    'packages',
+    'eslint-plugin-react-hooks'
+  );
+  // Run TypeScript on eslint-plugin-react-hooks prior to tests
+  return await exec('yarn build', {cwd});
 };
