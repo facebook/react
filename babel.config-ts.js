b0b88b2dd664a701972970ef7d0deb2804e198ba
michael faith <michaelfaith@users.noreply.github.com>
2025-02-15 16:34:15 -0600

    build: change jest config to use babel for ts transforms
    This change updates the base jest config to use `babel-jest` with the typescript preset to transform `.ts` files.  The existing `preprocessor`, which intercepts ts files, appears to be pretty old and wasn't implemented with entire packages of typescript source in mind.

---
+++ babel.config-ts.js
@@ -0,0 +1,15 @@
+/**
+ * This file is purely being used for local jest runs, and doesn't participate in the build process.
+ */
+'use strict';
+
+module.exports = {
+  plugins: [
+    '@babel/plugin-syntax-jsx',
+    '@babel/plugin-transform-flow-strip-types',
+  ],
+  presets: [
+    ['@babel/preset-env', {targets: {node: 'current'}}],
+    '@babel/preset-typescript',
+  ],
+};
