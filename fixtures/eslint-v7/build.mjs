a61ea0cf1cabbbc8fc4c265661191f373c0fad87
michael faith <michaelfaith@users.noreply.github.com>
2025-02-16 14:13:01 -0600

    ci: add workflow for running the eslint plugin e2e tests
    This change adds a workflow for PR builds, that runs the e2e tests for `eslint-plugin-react-hooks` created in #32396

--- fixtures/eslint-v7/build.mjs
+++ fixtures/eslint-v7/build.mjs
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
 import {exec} from 'node:child_process';
+import {dirname, resolve} from 'node:path';
+import {fileURLToPath} from 'node:url';
 
-exec('cd ../.. && yarn build -r stable eslint-plugin-react-hooks');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+exec('yarn build -r stable eslint-plugin-react-hooks', {
+  cwd: resolve(__dirname, '..', '..'),
+});
