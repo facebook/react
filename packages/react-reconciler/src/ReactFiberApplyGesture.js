cd9828423b2674c3936823a24bb7fced98e80472
Sebastian Markbage <sebastian@calyptus.eu>
2025-02-20 15:12:54 -0500

    Add place holders for the "commit" phase for gestures
    Essentially ReactFiberApplyGesture works as a fork of ReactFiberCommitWork
    which has its own phase that don't really commit but emulate what a commit
    into the destination state would look like before getting reverted.
    
    This is all done within a startViewTransition.
    
    To handle any sync flushing that happens in the gaps of startViewTransition
    callbacks, we use the same pendingEffectsStatus strategy except with a
    different sequence. If flushPendingEffects is called early it just goes
    through it as normal. However it probably should just abort the transition
    instead.

---
+++ packages/react-reconciler/src/ReactFiberApplyGesture.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import type {Fiber, FiberRoot} from './ReactInternalTypes';
+
+// Apply mutations to enter the destination state of a gesture render.
+// These will be reverted in later phases.
+export function applyDestinationMutations(
+  root: FiberRoot,
+  finishedWork: Fiber,
+): void {
+  // TODO
+}
+
+// Measure where boundaries should end up in the destination state.
+// This doesn't do any mutations itself and needs to run after all
+// other mutations have been applied.
+export function measureDestinationLayout(
+  root: FiberRoot,
+  finishedWork: Fiber,
+): void {
+  // TODO
+}
+
+// Revert mutations applied earlier but keep insertions outside the viewport.
+export function revertDestinationMutations(
+  root: FiberRoot,
+  finishedWork: Fiber,
+): void {
+  // TODO
+}
+
+// Revert insertions and apply view transition names to the "new" (current) state.
+export function applyDepartureTransitions(
+  root: FiberRoot,
+  finishedWork: Fiber,
+): void {
+  // TODO
+}
+
+// Revert transition names and start/adjust animations on the started View Transition.
+export function startGestureAnimations(
+  root: FiberRoot,
+  finishedWork: Fiber,
+): void {
+  // TODO
+}
