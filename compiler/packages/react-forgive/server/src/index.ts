92e65ca68f6bfc6be515ccacaa918e33b63911df
lauren <poteto@users.noreply.github.com>
2025-02-25 18:55:49 -0500

    [forgive] Add basic codelens provider (#32476)
    Adds a first codelens provider for successfully compiled functions. A
    later PR will add an actual command that will fire when the codelens is
    clicked
    
    ![Screenshot 2025-02-25 at 6 40
    20 PM](https://github.com/user-attachments/assets/924586e0-f70a-45d1-b0e6-a89af9371c8d)

--- compiler/packages/react-forgive/server/src/index.ts
+++ compiler/packages/react-forgive/server/src/index.ts
@@ -7,6 +7,7 @@
 
 import {TextDocument} from 'vscode-languageserver-textdocument';
 import {
+  CodeLens,
   createConnection,
   type InitializeParams,
   type InitializeResult,
@@ -14,10 +15,15 @@ import {
   TextDocuments,
   TextDocumentSyncKind,
 } from 'vscode-languageserver/node';
-import {compile} from './compiler';
+import {compile, lastResult} from './compiler';
 import {type PluginOptions} from 'babel-plugin-react-compiler/src';
 import {resolveReactConfig} from './compiler/options';
-import {type BabelFileResult} from '@babel/core';
+import {
+  CompileSuccessEvent,
+  defaultOptions,
+  LoggerEvent,
+} from 'babel-plugin-react-compiler/src/Entrypoint/Options';
+import {babelLocationToRange, getRangeFirstCharacter} from './compiler/compat';
 
 const SUPPORTED_LANGUAGE_IDS = new Set([
   'javascript',
@@ -30,11 +36,21 @@ const connection = createConnection(ProposedFeatures.all);
 const documents = new TextDocuments(TextDocument);
 
 let compilerOptions: PluginOptions | null = null;
-let lastResult: BabelFileResult | null = null;
+let compiledFns: Set<CompileSuccessEvent> = new Set();
 
 connection.onInitialize((_params: InitializeParams) => {
   // TODO(@poteto) get config fr
-  compilerOptions = resolveReactConfig('.');
+  compilerOptions = resolveReactConfig('.') ?? defaultOptions;
+  compilerOptions = {
+    ...compilerOptions,
+    logger: {
+      logEvent(_filename: string | null, event: LoggerEvent) {
+        if (event.kind === 'CompileSuccess') {
+          compiledFns.add(event);
+        }
+      },
+    },
+  };
   const result: InitializeResult = {
     capabilities: {
       textDocumentSync: TextDocumentSyncKind.Full,
@@ -48,44 +64,58 @@ connection.onInitialized(() => {
   connection.console.log('initialized');
 });
 
-documents.onDidOpen(async event => {
-  if (SUPPORTED_LANGUAGE_IDS.has(event.document.languageId)) {
-    const text = event.document.getText();
-    const result = await compile({
-      text,
-      file: event.document.uri,
-      options: compilerOptions,
-    });
-    if (result.code != null) {
-      lastResult = result;
-    }
-  }
-});
-
 documents.onDidChangeContent(async event => {
+  connection.console.info(`Changed: ${event.document.uri}`);
+  compiledFns.clear();
   if (SUPPORTED_LANGUAGE_IDS.has(event.document.languageId)) {
     const text = event.document.getText();
-    const result = await compile({
+    await compile({
       text,
       file: event.document.uri,
       options: compilerOptions,
     });
-    if (result.code != null) {
-      lastResult = result;
-    }
   }
 });
 
 connection.onDidChangeWatchedFiles(change => {
+  compiledFns.clear();
   connection.console.log(
     change.changes.map(c => `File changed: ${c.uri}`).join('\n'),
   );
 });
 
 connection.onCodeLens(params => {
-  connection.console.log('lastResult: ' + JSON.stringify(lastResult, null, 2));
-  connection.console.log('params: ' + JSON.stringify(params, null, 2));
-  return [];
+  connection.console.info(`Handling codelens for: ${params.textDocument.uri}`);
+  if (compiledFns.size === 0) {
+    return;
+  }
+  const lenses: Array<CodeLens> = [];
+  for (const compiled of compiledFns) {
+    if (compiled.fnLoc != null) {
+      const fnLoc = babelLocationToRange(compiled.fnLoc);
+      if (fnLoc === null) continue;
+      const lens = CodeLens.create(
+        getRangeFirstCharacter(fnLoc),
+        compiled.fnLoc,
+      );
+      if (lastResult?.code != null) {
+        lens.command = {
+          title: 'Optimized by React Compiler',
+          command: 'todo',
+        };
+      }
+      lenses.push(lens);
+    }
+  }
+  return lenses;
+});
+
+connection.onCodeLensResolve(lens => {
+  connection.console.info(`Resolving codelens for: ${JSON.stringify(lens)}`);
+  if (lastResult?.code != null) {
+    connection.console.log(lastResult.code);
+  }
+  return lens;
 });
 
 documents.listen(connection);
