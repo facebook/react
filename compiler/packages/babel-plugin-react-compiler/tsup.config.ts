82b8128b25b41dd325036322112e02b6fa131501
Lauren Tan <poteto@users.noreply.github.com>
2025-03-07 15:17:57 -0500

    [compiler] Migrate compiler packages to tsup
    Currently in the `compiler` workspace, we invoke esbuild directly to build most packages (with the exception of `snap`). This has been mostly fine, but does not allow us to do things like generate type declaration files.
    
    I would like #32416 to be able to consume the merged eslint-plugin-react-compiler from source rather than via npm, and one of the things that has come up from my exploration in that stack using the compiler from source is that babel-plugin-react-compiler is missing type declarations.
    
    [`tsup`](https://github.com/egoist/tsup) is esbuild based so build performance is comparable. It it is a little bit slower now because we also generate d.ts files, but it's still much faster than rollup which we used prior to esbuild.
    
    ```
    # BEFORE: build all compiler packages
    $ time yarn build
    
    ✨  Done in 15.61s.
    yarn build  13.82s user 1.54s system 96% cpu 15.842 total
    
    # AFTER: build all compiler packages
    $ time yarn build
    
    ✨  Done in 30.69s.
    yarn build  43.57s user 3.20s system 150% cpu 31.061 total
    ```
    
    I still need to test if this unblocks #32416 but this stack can be landed independently though as we could probably just release type declarations on npm. No one should be using the compiler directly, but if they really wanted to, lack of type declarations would not stop them (cf React secret internals).
    
    Note that I still kept esbuild as we still use it directly for forgive.

---
+++ compiler/packages/babel-plugin-react-compiler/tsup.config.ts
@@ -0,0 +1,29 @@
+import {defineConfig} from 'tsup';
+
+export default defineConfig({
+  entry: ['./src/index.ts'],
+  outDir: './dist',
+  external: ['@babel/types'],
+  splitting: false,
+  sourcemap: false,
+  dts: true,
+  bundle: true,
+  format: 'cjs',
+  platform: 'node',
+  banner: {
+    js: `/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @lightSyntaxTransform
+ * @noflow
+ * @nolint
+ * @preventMunge
+ * @preserve-invariant-messages
+ */
+
+"use no memo";`,
+  },
+});
