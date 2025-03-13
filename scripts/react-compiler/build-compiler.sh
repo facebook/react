485c8d629654ad7743a0616927d534361ea53a5e
Lauren Tan <poteto@users.noreply.github.com>
2025-03-12 20:35:01 -0400

    fail fast

--- scripts/react-compiler/build-compiler.sh
+++ scripts/react-compiler/build-compiler.sh
@@ -4,6 +4,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+set -eo pipefail
+
 echo "Building babel-plugin-react-compiler..."
 yarn --cwd compiler install --frozen-lockfile
 yarn --cwd compiler workspace babel-plugin-react-compiler build --dts
