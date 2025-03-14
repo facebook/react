5093a471290b0c858e31f72752e3025298668d38
Lauren Tan <poteto@users.noreply.github.com>
2025-03-14 11:31:35 -0400

    [eprh] Move to compiler directory
    Moves the plugin into the compiler directory.
    
    - Update bundles.js to point to new location for now to keep builds the same
    - Remove jest.config.js
    - Replace babel.config.js with copy from eslint-plugin-react-compiler

--- packages/eslint-plugin-react-hooks/jest.config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-process.env.NODE_ENV = 'development';
-
-module.exports = {
-  setupFiles: [require.resolve('../../scripts/jest/setupEnvironment.js')],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-};
