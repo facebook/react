341280688d4b0a5ff898f1bc027d9a487a2ddf7b
Lauren Tan <poteto@users.noreply.github.com>
2025-03-18 14:41:31 -0400

    [eprh] Remove __EXPERIMENTAL__
    `__EXPERIMENTAL__` flag doesn't have any use outside of the React repo. Let's remove these flags for now.

--- compiler/packages/eslint-plugin-react-hooks/src/rules/ExhaustiveDeps.ts
+++ compiler/packages/eslint-plugin-react-hooks/src/rules/ExhaustiveDeps.ts
@@ -2056,10 +2056,7 @@ function isAncestorNodeOf(a: Node, b: Node): boolean {
 }
 
 function isUseEffectEventIdentifier(node: Node): boolean {
-  if (__EXPERIMENTAL__) {
-    return node.type === 'Identifier' && node.name === 'useEffectEvent';
-  }
-  return false;
+  return node.type === 'Identifier' && node.name === 'useEffectEvent';
 }
 
 function getUnknownDependenciesMessage(reactiveHookName: string): string {
