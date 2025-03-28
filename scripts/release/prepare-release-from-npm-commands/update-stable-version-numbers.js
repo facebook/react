df10e773467b5e3b091e071cfbd260471dafd598
Lauren Tan <poteto@users.noreply.github.com>
2025-03-28 14:05:42 -0400

    [release] Don't lookup build-info.json when updating version numbers
    From what we can see, `build-info.json` is a vestigal file that we were previously including in builds but are no longer since 2022 (see https://github.com/facebook/react/pull/23257, which removes `build-info.json` which would have broken scripts/release/build-release-locally-commands/add-build-info-json.js).
    
    Since this file is no longer built, instead of looking it up we default to the `version` that was passed in as an argument to scripts/release/prepare-release-from-npm.js. Since `version` is what is pulled from npm, there should only be 1 consistent version for all the packages that are pulled. Therefore, only 1 version (eg canary) needs to be replaced to the new stable version.

--- scripts/release/prepare-release-from-npm-commands/update-stable-version-numbers.js
+++ scripts/release/prepare-release-from-npm-commands/update-stable-version-numbers.js
@@ -117,16 +117,7 @@ const run = async ({cwd, packages, version, ci}, versionsMap) => {
     // A separate "React version" is used for the embedded renderer version to support DevTools,
     // since it needs to distinguish between different version ranges of React.
     // We need to replace it as well as the "next" version number.
-    const buildInfoPath = join(nodeModulesPath, 'react', 'build-info.json');
-    const {reactVersion} = await readJson(buildInfoPath);
-
-    if (!reactVersion) {
-      console.error(
-        theme`{error Unsupported or invalid build metadata in} {path build/node_modules/react/build-info.json}` +
-          theme`{error . This could indicate that you have specified an outdated "next" version.}`
-      );
-      process.exit(1);
-    }
+    const reactVersion = version;
 
     // We print the diff to the console for review,
     // but it can be large so let's also write it to disk.
@@ -151,10 +142,6 @@ const run = async ({cwd, packages, version, ci}, versionsMap) => {
         // Replace all "next" version numbers (e.g. header @license).
         while (afterContents.indexOf(version) >= 0) {
           afterContents = afterContents.replace(version, newStableVersion);
-        }
-        // Replace inline renderer version numbers (e.g. shared/ReactVersion).
-        while (afterContents.indexOf(reactVersion) >= 0) {
-          afterContents = afterContents.replace(reactVersion, newStableVersion);
         }
         if (beforeContents !== afterContents) {
           numFilesModified++;
