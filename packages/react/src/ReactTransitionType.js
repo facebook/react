7accce602298386aab14c37a2000af85b76e5718
Sebastian Markbage <sebastian@calyptus.eu>
2025-03-30 11:46:40 -0400

    Add TransitionTypes to startGestureTransition

--- packages/react/src/ReactTransitionType.js
+++ packages/react/src/ReactTransitionType.js
@@ -8,17 +8,32 @@
  */
 
 import ReactSharedInternals from 'shared/ReactSharedInternals';
-import {enableViewTransition} from 'shared/ReactFeatureFlags';
+import {
+  enableViewTransition,
+  enableGestureTransition,
+} from 'shared/ReactFeatureFlags';
 
 export type TransitionTypes = Array<string>;
 
+// This one is only available synchronously so we don't need to use ReactSharedInternals
+// for this state. Instead, we track it in isomorphic and pass it to the renderer.
+export let pendingGestureTransitionTypes: null | TransitionTypes = null;
+
 export function addTransitionType(type: string): void {
   if (enableViewTransition) {
-    const pendingTransitionTypes: null | TransitionTypes =
-      ReactSharedInternals.V;
-    if (pendingTransitionTypes === null) {
-      ReactSharedInternals.V = [type];
-    } else if (pendingTransitionTypes.indexOf(type) === -1) {
+    let pendingTransitionTypes: null | TransitionTypes = null;
+    if (
+      enableGestureTransition &&
+      ReactSharedInternals.T !== null &&
+      ReactSharedInternals.T.gesture !== null
+    ) {
+    } else {
+      pendingTransitionTypes = ReactSharedInternals.V;
+      if (pendingTransitionTypes === null) {
+        pendingTransitionTypes = ReactSharedInternals.V = [];
+      }
+    }
+    if (pendingTransitionTypes.indexOf(type) === -1) {
       pendingTransitionTypes.push(type);
     }
   }
