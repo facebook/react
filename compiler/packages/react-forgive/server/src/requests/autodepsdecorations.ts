b5f64fc0a1f8ab4b0829320b6718aa3815d400b4
Lauren Tan <poteto@users.noreply.github.com>
2025-04-23 21:47:39 -0400

    [compiler] Add missing copyrights
    `yarn copyright`

--- compiler/packages/react-forgive/server/src/requests/autodepsdecorations.ts
+++ compiler/packages/react-forgive/server/src/requests/autodepsdecorations.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import {type AutoDepsDecorationsEvent} from 'babel-plugin-react-compiler/src/Entrypoint';
 import {type Position} from 'vscode-languageserver-textdocument';
 import {RequestType} from 'vscode-languageserver/node';
