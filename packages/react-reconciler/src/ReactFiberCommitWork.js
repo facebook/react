693803a9bb3073b2ff5c99f8ae804f855db9aae2
Sebastian Markbåge <sebastian@calyptus.eu>
2025-04-24 16:53:34 -0400

    Rename Suspense unstable_name to name (#33014)
    This was only used by Transition Tracing which isn't really used
    anywhere.
    
    However, we want to start using it for other DevTools.

--- packages/react-reconciler/src/ReactFiberCommitWork.js
+++ packages/react-reconciler/src/ReactFiberCommitWork.js
@@ -1062,9 +1062,9 @@ function commitTransitionProgress(offscreenFiber: Fiber) {
     if (
       parent !== null &&
       parent.tag === SuspenseComponent &&
-      parent.memoizedProps.unstable_name
+      parent.memoizedProps.name
     ) {
-      name = parent.memoizedProps.unstable_name;
+      name = parent.memoizedProps.name;
     }
 
     if (!wasHidden && isHidden) {
@@ -4952,7 +4952,7 @@ function commitPassiveUnmountInsideDeletedTreeOnFiber(
         if (transitions !== null) {
           const abortReason = {
             reason: 'suspense',
-            name: current.memoizedProps.unstable_name || null,
+            name: current.memoizedProps.name || null,
           };
           if (
             current.memoizedState === null ||
