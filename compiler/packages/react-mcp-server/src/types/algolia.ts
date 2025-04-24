44d7d0cfa72392c8030b1d06c1a70ded410ce355
Lauren Tan <poteto@users.noreply.github.com>
2025-04-23 21:55:45 -0400

    [compiler] Add missing copyrights
    `yarn copyright`

--- compiler/packages/react-mcp-server/src/types/algolia.ts
+++ compiler/packages/react-mcp-server/src/types/algolia.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // https://github.com/algolia/docsearch/blob/15ebcba606b281aa0dddc4ccb8feb19d396bf79e/packages/docsearch-react/src/types/DocSearchHit.ts
 type ContentType =
   | 'content'
