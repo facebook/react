0e4d1f473fcfc52ee87bf683934c4d7101cfeca8
Dawid Małecki <maleckidawid@meta.com>
2025-05-05 17:03:44 +0200

    HostInstance

--- scripts/flow/react-native-host-hooks.js
+++ scripts/flow/react-native-host-hooks.js
@@ -49,7 +49,6 @@ type RawEventEmitterEvent = $ReadOnly<{
   // and a stateNode of the native element/Fiber the event was emitted to.
   nativeEvent: {[string]: mixed, ...},
 }>;
-opaque type __PublicInstance;
 
 declare module 'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface' {
   declare export function deepDiffer(
@@ -159,7 +158,7 @@ declare module 'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface'
     emit: (channel: string, event: RawEventEmitterEvent) => string,
     ...
   };
-  declare export opaque type PublicInstance = __PublicInstance;
+  declare export opaque type PublicInstance;
   declare export opaque type PublicTextInstance;
   declare export opaque type PublicRootInstance;
   declare export type MeasureOnSuccessCallback = __MeasureOnSuccessCallback;
@@ -206,7 +205,7 @@ declare module 'react-native/Libraries/ReactPrivate/ReactNativePrivateInitialize
 }
 
 declare module 'react-native' {
-  declare export opaque type HostInstance = __PublicInstance;
+  declare export opaque type PublicInstance;
   declare export opaque type PublicTextInstance;
   declare export opaque type PublicRootInstance;
   declare export type MeasureOnSuccessCallback = __MeasureOnSuccessCallback;
