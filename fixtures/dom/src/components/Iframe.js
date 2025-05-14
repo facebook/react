db8a7449cc882600cbb92b35eb626146ed588945
Ronitkumar Sabhaya <RonitkumarSabhaya@my.unt.edu>
2025-05-13 19:47:28 -0500

    Add PropTypes validation to Iframe components

--- fixtures/dom/src/components/Iframe.js
+++ fixtures/dom/src/components/Iframe.js
@@ -1,5 +1,6 @@
 const React = window.React;
 const ReactDOM = window.ReactDOM;
+import PropTypes from 'prop-types';
 
 class IframePortal extends React.Component {
   iframeRef = null;
@@ -41,6 +42,35 @@ class IframePortal extends React.Component {
     );
   }
 }
+/**
+ * Iframe Components
+* 
+* This file contains two components for rendering content inside iframes:
+* 
+* 1. IframePortal - Used when ReactDOM.createPortal is available. This component:
+*    - Creates an iframe and renders children into it using portals
+*    - Allows setting custom HTML in the iframe's head
+*    - Configurable iframe height
+* 
+* 2. IframeSubtree - Fallback for older React versions where createPortal isn't available.
+*    - Simply renders children directly while logging a warning
+* 
+* The file exports the appropriate component based on React version capabilities.
+* 
+* Props:
+*   - children: React nodes to render inside the iframe
+*   - head (optional): HTML string to inject into iframe's head
+*   - height (optional): Height of the iframe (defaults to '100%')
+*/
+IframePortal.propTypes = {
+  children: PropTypes.node.isRequired,
+  head: PropTypes.string,
+  height: PropTypes.string
+};
+
+IframePortal.defaultProps = {
+  height: '100%'
+};
 
 class IframeSubtree extends React.Component {
   warned = false;
@@ -54,5 +84,9 @@ class IframeSubtree extends React.Component {
     return <div>{this.props.children}</div>;
   }
 }
+
+IframeSubtree.propTypes = {
+  children: PropTypes.node
+};
 
 export default ReactDOM.createPortal ? IframePortal : IframeSubtree;
