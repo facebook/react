0d072884f9201f645ae298936f2933970b73bec4
mofeiZ <34200447+mofeiZ@users.noreply.github.com>
2025-05-22 16:14:49 -0400

    [compiler] Inferred effect dependencies now include optional chains (#33326)
    Inferred effect dependencies now include optional chains.
    
    This is a temporary solution while
    https://github.com/facebook/react/pull/32099 and its followups are
    worked on. Ideally, we should model reactive scope dependencies in the
    IR similarly to `ComputeIR` -- dependencies should be hoisted and all
    references rewritten to use the hoisted dependencies.
    
    `
    ---
    [//]: # (BEGIN SAPLING FOOTER)
    Stack created with [Sapling](https://sapling-scm.com). Best reviewed
    with [ReviewStack](https://reviewstack.dev/facebook/react/pull/33326).
    * __->__ #33326
    * #33325
    * #32286

--- compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/infer-effect-dependencies/reactive-optional-chain-complex.js
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/infer-effect-dependencies/reactive-optional-chain-complex.js
@@ -1,11 +1,14 @@
 // @inferEffectDependencies
 import {useEffect} from 'react';
-import {print} from 'shared-runtime';
+import {print, shallowCopy} from 'shared-runtime';
 
 function ReactiveMemberExpr({cond, propVal}) {
   const obj = {a: cond ? {b: propVal} : null, c: null};
-  useEffect(() => print(obj.a?.b));
-  useEffect(() => print(obj.c?.d));
+  const other = shallowCopy({a: {b: {c: {d: {e: {f: propVal + 1}}}}}});
+  const primitive = shallowCopy(propVal);
+  useEffect(() =>
+    print(obj.a?.b, other?.a?.b?.c?.d?.e.f, primitive.a?.b.c?.d?.e.f)
+  );
 }
 
 export const FIXTURE_ENTRYPOINT = {
