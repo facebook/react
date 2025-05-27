44d19b119f35c7f4f2da9af299f6d9c8662fc91e
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-05-27 09:54:24 +0200

    fmt

--- packages/react-dom/src/server/ReactDOMFizzServerBun.js
+++ packages/react-dom/src/server/ReactDOMFizzServerBun.js
@@ -37,10 +37,12 @@ ensureCorrectIsomorphicReactVersion();
 type Options = {
   identifierPrefix?: string,
   namespaceURI?: string,
-  nonce?: string | {
-    script?: string,
-    style?: string,
-  },
+  nonce?:
+    | string
+    | {
+        script?: string,
+        style?: string,
+      },
   bootstrapScriptContent?: string,
   bootstrapScripts?: Array<string | BootstrapScriptDescriptor>,
   bootstrapModules?: Array<string | BootstrapScriptDescriptor>,
