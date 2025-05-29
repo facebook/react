aeb24057e54d00dd1817c7f324f2bafcf8a9d74a
Sebastian Markbage <sebastian@calyptus.eu>
2025-05-29 16:13:27 -0400

    Track both awaited and previous sequence separately
    This lets us track both the thing we're awaiting and the path that led us
    to awaiting in the first place.

--- packages/react-server/src/ReactFlightAsyncSequence.js
+++ packages/react-server/src/ReactFlightAsyncSequence.js
@@ -15,21 +15,24 @@ export type IONode = {
   tag: 0,
   stack: Error, // callsite that spawned the I/O
   timestamp: number, // start time when the first part of the I/O sequence started
-  trigger: null | AwaitNode, // the preceeding await that spawned this new work
+  awaited: null, // I/O is only blocked on external.
+  previous: null | AwaitNode, // the preceeding await that spawned this new work
 };
 
 export type PromiseNode = {
   tag: 1,
-  stack: null,
-  timestamp: number, // resolve time of the promise. not used.
-  trigger: null | AsyncSequence,
+  stack: Error, // callsite that created the Promise. Only used if the I/O callsite is not in user space.
+  timestamp: number, // start time of the promise. Only used if the I/O was not relevant.
+  awaited: null | AsyncSequence, // the thing that ended up resolving this promise
+  previous: null, // where we created the promise is not interesting since creating it doesn't mean waiting.
 };
 
 export type AwaitNode = {
   tag: 2,
   stack: Error, // callsite that awaited (using await, .then(), Promise.all(), ...)
   timestamp: number, // the end time of the preceeding I/O operation (or -1.1 before it ends)
-  trigger: null | AsyncSequence, // the thing we were waiting on
+  awaited: null | AsyncSequence, // the promise we were waiting on
+  previous: null | AsyncSequence, // the sequence that was blocking us from awaiting in the first place
 };
 
 export type AsyncSequence = IONode | PromiseNode | AwaitNode;
