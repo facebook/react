4735f9c84944f6d2ba3d622dbb974a2ef7b7909f
Sebastian Markbage <sebastian@calyptus.eu>
2025-06-04 22:38:28 -0400

    Add Node Web Streams bundle for SSR

--- packages/react-dom/npm/server.node.js
+++ packages/react-dom/npm/server.node.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var l, s;
+var l, s, w;
 if (process.env.NODE_ENV === 'production') {
   l = require('./cjs/react-dom-server-legacy.node.production.js');
   s = require('./cjs/react-dom-server.node.production.js');
+  w = require('./cjs/react-dom-server.node-webstreams.production.js');
 } else {
   l = require('./cjs/react-dom-server-legacy.node.development.js');
   s = require('./cjs/react-dom-server.node.development.js');
+  w = require('./cjs/react-dom-server.node-webstreams.development.js');
 }
 
 exports.version = l.version;
@@ -15,4 +17,8 @@ exports.renderToStaticMarkup = l.renderToStaticMarkup;
 exports.renderToPipeableStream = s.renderToPipeableStream;
 if (s.resumeToPipeableStream) {
   exports.resumeToPipeableStream = s.resumeToPipeableStream;
+}
+exports.renderToReadableStream = w.renderToReadableStream;
+if (w.resume) {
+  exports.resume = w.resume;
 }
