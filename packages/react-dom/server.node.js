5457bb3dddab5075d98ad147a5ad6c3e99c67f52
Sebastian Markbage <sebastian@calyptus.eu>
2025-06-06 18:01:24 -0400

    Add Web Streams to Fizz Node entry point

--- packages/react-dom/server.node.js
+++ packages/react-dom/server.node.js
@@ -37,3 +37,17 @@ export function resumeToPipeableStream() {
     arguments,
   );
 }
+
+export function renderToReadableStream() {
+  return require('./src/server/react-dom-server.node').renderToReadableStream.apply(
+    this,
+    arguments,
+  );
+}
+
+export function resume() {
+  return require('./src/server/react-dom-server.node').resume.apply(
+    this,
+    arguments,
+  );
+}
