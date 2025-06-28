eee832754ee7c8f42406cbaf89816d1f4b48566e
Aishwayao11k <aishwaryadshetty019@gmail.com>
2025-06-28 19:54:37 +0530

    refactor(getComments): improve readability and naming for clarity

--- scripts/babel/getComments.js
+++ scripts/babel/getComments.js
@@ -1,30 +1,33 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 'use strict';
 
+/**
+ * Get leading comments from an AST path.
+ * Supports both Babel and Hermes ASTs.
+ */
 function getComments(path) {
   const allComments = path.hub.file.ast.comments;
+
   if (path.node.leadingComments) {
-    // Babel AST includes comments.
+    // Babel AST includes comments directly.
     return path.node.leadingComments;
   }
-  // In Hermes AST we need to find the comments by range.
+
+  // Hermes AST: find comments by location.
   const comments = [];
-  let line = path.node.loc.start.line;
+  let currentLine = path.node.loc.start.line;
   let i = allComments.length - 1;
-  while (i >= 0 && allComments[i].loc.end.line >= line) {
+
+  while (i >= 0 && allComments[i].loc.end.line >= currentLine) {
     i--;
   }
-  while (i >= 0 && allComments[i].loc.end.line === line - 1) {
-    line = allComments[i].loc.start.line;
-    comments.unshift(allComments[i]);
+
+  while (i >= 0 && allComments[i].loc.end.line === currentLine - 1) {
+    const comment = allComments[i];
+    currentLine = comment.loc.start.line;
+    comments.unshift(comment);
     i--;
   }
+
   return comments;
 }
 
