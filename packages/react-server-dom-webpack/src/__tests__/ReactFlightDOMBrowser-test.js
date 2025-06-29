7b52178bd44cccd4c741409a67d5b0db9caa71d8
Hendrik Liebau <mail@hendrik-liebau.de>
2025-06-29 11:10:28 +0200

    Add test from #32316

--- packages/react-server-dom-webpack/src/__tests__/ReactFlightDOMBrowser-test.js
+++ packages/react-server-dom-webpack/src/__tests__/ReactFlightDOMBrowser-test.js
@@ -2624,4 +2624,41 @@ describe('ReactFlightDOMBrowser', () => {
     expect(responseFoo.bar).toBe(responseBar);
     expect(Array.from(responseBar)[0]).toBe(responseFoo);
   });
+
+  it('should resolve deduped references in maps used in client component props', async () => {
+    const ClientComponent = clientExports(function ClientComponent({
+      shared,
+      map,
+    }) {
+      expect(map.get(42)).toBe(shared);
+      return JSON.stringify({shared, map: Array.from(map)});
+    });
+
+    function Server() {
+      const shared = {id: 42};
+      const map = new Map([[42, shared]]);
+
+      return <ClientComponent shared={shared} map={map} />;
+    }
+
+    const stream = await serverAct(() =>
+      ReactServerDOMServer.renderToReadableStream(<Server />, webpackMap),
+    );
+
+    function ClientRoot({response}) {
+      return use(response);
+    }
+
+    const response = ReactServerDOMClient.createFromReadableStream(stream);
+    const container = document.createElement('div');
+    const root = ReactDOMClient.createRoot(container);
+
+    await act(() => {
+      root.render(<ClientRoot response={response} />);
+    });
+
+    expect(container.innerHTML).toBe(
+      '{"shared":{"id":42},"map":[[42,{"id":42}]]}',
+    );
+  });
 });
