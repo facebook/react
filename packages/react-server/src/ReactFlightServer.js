cf6a0d273d5fc1226ce14a2ade106ae8d3b06a13
Sebastian Markbage <sebastian@calyptus.eu>
2025-07-08 11:30:04 -0400

    Fix stack getting object limited

--- packages/react-server/src/ReactFlightServer.js
+++ packages/react-server/src/ReactFlightServer.js
@@ -4048,10 +4048,6 @@ function emitIOInfoChunk(
     start: relativeStartTimestamp,
     end: relativeEndTimestamp,
   };
-  if (value !== undefined) {
-    // $FlowFixMe[cannot-write]
-    debugIOInfo.value = value;
-  }
   if (env != null) {
     // $FlowFixMe[cannot-write]
     debugIOInfo.env = env;
@@ -4063,6 +4059,10 @@ function emitIOInfoChunk(
   if (owner != null) {
     // $FlowFixMe[cannot-write]
     debugIOInfo.owner = owner;
+  }
+  if (value !== undefined) {
+    // $FlowFixMe[cannot-write]
+    debugIOInfo.value = value;
   }
   const json: string = serializeDebugModel(request, objectLimit, debugIOInfo);
   const row = id.toString(16) + ':J' + json + '\n';
