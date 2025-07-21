e9638c33d7e79162516b89da31549af4090997a5
Rubén Norte <rubennorte@meta.com>
2025-07-21 10:27:46 +0100

    Clean up feature flag to use lazy public instances in Fabric (#33943)
    ## Summary
    
    We have thoroughly tested this flag in production and proved stability
    and performance, so we can clean it up and "ship it".

--- packages/shared/forks/ReactFeatureFlags.native-fb-dynamic.js
+++ packages/shared/forks/ReactFeatureFlags.native-fb-dynamic.js
@@ -23,7 +23,6 @@ export const enableHiddenSubtreeInsertionEffectCleanup = __VARIANT__;
 export const enablePersistedModeClonedFlag = __VARIANT__;
 export const enableEagerAlternateStateNodeCleanup = __VARIANT__;
 export const passChildrenWhenCloningPersistedNodes = __VARIANT__;
-export const enableLazyPublicInstanceInFabric = __VARIANT__;
 export const renameElementSymbol = __VARIANT__;
 export const enableFragmentRefs = __VARIANT__;
 export const enableComponentPerformanceTrack = __VARIANT__;
