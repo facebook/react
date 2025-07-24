81ec0be8dfac185873c2eb543ed5e69e1fe0b11e
Sebastian Markbage <sebastian@calyptus.eu>
2025-07-24 14:15:47 -0400

    Add option to always open in editor

--- packages/react-devtools-shared/src/devtools/views/useOpenResource.js
+++ packages/react-devtools-shared/src/devtools/views/useOpenResource.js
@@ -13,8 +13,13 @@ import {useCallback, useContext, useSyncExternalStore} from 'react';
 
 import ViewElementSourceContext from './Components/ViewElementSourceContext';
 
-import {getOpenInEditorURL} from '../../utils';
-import {LOCAL_STORAGE_OPEN_IN_EDITOR_URL} from '../../constants';
+import {getOpenInEditorURL, getAlwaysOpenInEditor} from '../../utils';
+import {
+  LOCAL_STORAGE_OPEN_IN_EDITOR_URL,
+  LOCAL_STORAGE_ALWAYS_OPEN_IN_EDITOR,
+} from '../../constants';
+
+import {checkConditions} from './Editor/utils';
 
 const useOpenResource = (
   source: null | ReactFunctionLocation,
@@ -27,32 +32,52 @@ const useOpenResource = (
     ViewElementSourceContext,
   );
 
-  // TODO: const editorURL =
-  useSyncExternalStore(
-    function subscribe(callback) {
+  const editorURL = useSyncExternalStore(
+    useCallback(function subscribe(callback) {
       window.addEventListener(LOCAL_STORAGE_OPEN_IN_EDITOR_URL, callback);
       return function unsubscribe() {
         window.removeEventListener(LOCAL_STORAGE_OPEN_IN_EDITOR_URL, callback);
       };
-    },
-    function getState() {
-      return getOpenInEditorURL();
-    },
+    }, []),
+    getOpenInEditorURL,
   );
 
+  const alwaysOpenInEditor = useSyncExternalStore(
+    useCallback(function subscribe(callback) {
+      window.addEventListener(LOCAL_STORAGE_ALWAYS_OPEN_IN_EDITOR, callback);
+      return function unsubscribe() {
+        window.removeEventListener(
+          LOCAL_STORAGE_ALWAYS_OPEN_IN_EDITOR,
+          callback,
+        );
+      };
+    }, []),
+    getAlwaysOpenInEditor,
+  );
+
+  // First check if this link is eligible for being open directly in the configured editor.
+  const openInEditor =
+    alwaysOpenInEditor && source !== null
+      ? checkConditions(editorURL, symbolicatedSource || source)
+      : null;
   // In some cases (e.g. FB internal usage) the standalone shell might not be able to view the source.
   // To detect this case, we defer to an injected helper function (if present).
   const linkIsEnabled =
-    viewElementSourceFunction != null &&
-    source != null &&
-    (canViewElementSourceFunction == null ||
-      canViewElementSourceFunction(source, symbolicatedSource));
+    (openInEditor !== null && !openInEditor.shouldDisableButton) ||
+    (viewElementSourceFunction != null &&
+      source != null &&
+      (canViewElementSourceFunction == null ||
+        canViewElementSourceFunction(source, symbolicatedSource)));
 
   const viewSource = useCallback(() => {
-    if (viewElementSourceFunction != null && source != null) {
+    if (openInEditor !== null && !openInEditor.shouldDisableButton) {
+      // If we have configured to always open in the code editor, we do so if we can.
+      // Otherwise, we fallback to open in the local editor if possible (e.g. non-file urls).
+      window.open(openInEditor.url);
+    } else if (viewElementSourceFunction != null && source != null) {
       viewElementSourceFunction(source, symbolicatedSource);
     }
-  }, [source, symbolicatedSource]);
+  }, [openInEditor, source, symbolicatedSource]);
 
   return [linkIsEnabled, viewSource];
 };
