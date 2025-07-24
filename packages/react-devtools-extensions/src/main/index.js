3082604bdc03cfd8cee35e54dbb10caef956937e
Sebastian Markbåge <sebastian@calyptus.eu>
2025-07-24 17:38:12 -0400

    [DevTools] Feature detect sources panel (#33987)
    I broke Firefox DevTools extension in #33968.
    
    It turns out the Firefox has a placeholder object for the sources panel
    which is empty. We need to detect the actual event handler.

--- packages/react-devtools-extensions/src/main/index.js
+++ packages/react-devtools-extensions/src/main/index.js
@@ -111,7 +111,7 @@ function createBridge() {
     chrome.devtools.panels.elements.onSelectionChanged.removeListener(
       onBrowserElementSelectionChanged,
     );
-    if (sourcesPanel) {
+    if (sourcesPanel && sourcesPanel.onSelectionChanged) {
       currentSelectedSource = null;
       sourcesPanel.onSelectionChanged.removeListener(
         onBrowserSourceSelectionChanged,
@@ -124,7 +124,7 @@ function createBridge() {
   chrome.devtools.panels.elements.onSelectionChanged.addListener(
     onBrowserElementSelectionChanged,
   );
-  if (sourcesPanel) {
+  if (sourcesPanel && sourcesPanel.onSelectionChanged) {
     sourcesPanel.onSelectionChanged.addListener(
       onBrowserSourceSelectionChanged,
     );
