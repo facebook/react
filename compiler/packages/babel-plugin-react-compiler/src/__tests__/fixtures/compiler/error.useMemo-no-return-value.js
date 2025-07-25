c18e566f6176ac64430d43e4d8f537fdd8fc6f91
Lauren Tan <poteto@users.noreply.github.com>
2025-07-25 13:20:24 -0400

    [compiler] Add new ValidateNoVoidUseMemo pass
    Adds a new validation pass to validate against `useMemo`s that don't return anything. This usually indicates some kind of "useEffect"-like code that has side effects that need to be memoized to prevent overfiring, and is an anti-pattern.
    
    A follow up validation could also look at the return value of `useMemo`s to see if they are being used.

---
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/error.useMemo-no-return-value.js
@@ -0,0 +1,15 @@
+// @enableValidateNoVoidUseMemo
+function Component() {
+  const value = useMemo(() => {
+    console.log('computing');
+  }, []);
+  const value2 = React.useMemo(() => {
+    console.log('computing');
+  }, []);
+  return (
+    <div>
+      {value}
+      {value2}
+    </div>
+  );
+}
