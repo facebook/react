9c79367ba7631191b2c465f2d338560a10bfaa3c
Sebastian Markbage <sebastian@calyptus.eu>
2025-08-02 18:05:16 -0400

    Allow Temporary References to be awaited

--- packages/react-server/src/ReactFlightServerTemporaryReferences.js
+++ packages/react-server/src/ReactFlightServerTemporaryReferences.js
@@ -70,8 +70,12 @@ const proxyHandlers = {
             `Instead, you can export a Client Component wrapper ` +
             `that itself renders a Client Context Provider.`,
         );
-      // Allow returning a temporary reference from an async function
       case 'then':
+        // Allow returning a temporary reference from an async function
+        // Unlike regular Client References, a Promise would never have been serialized as
+        // an opaque Temporary Reference, but instead would have been serialized as a
+        // Promise on the server and so doesn't hit this path. So we can assume this wasn't
+        // a Promise on the client.
         return undefined;
     }
     throw new Error(
