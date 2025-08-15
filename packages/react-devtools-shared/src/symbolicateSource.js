452b6b35082792e6777b7694358daf0d059a4763
Sebastian Markbage <sebastian@calyptus.eu>
2025-08-15 13:55:04 -0400

    Pass around SourceMappedLocation which also includes whether it was ignore listed

--- packages/react-devtools-shared/src/symbolicateSource.js
+++ packages/react-devtools-shared/src/symbolicateSource.js
@@ -14,15 +14,20 @@ import type {FetchFileWithCaching} from 'react-devtools-shared/src/devtools/view
 
 const symbolicationCache: Map<
   string,
-  Promise<ReactFunctionLocation | null>,
+  Promise<SourceMappedLocation | null>,
 > = new Map();
+
+export type SourceMappedLocation = {
+  location: ReactFunctionLocation,
+  ignored: boolean, // Whether the file for this location was ignore listed
+};
 
 export function symbolicateSourceWithCache(
   fetchFileWithCaching: FetchFileWithCaching,
   sourceURL: string,
   line: number, // 1-based
   column: number, // 1-based
-): Promise<ReactFunctionLocation | null> {
+): Promise<SourceMappedLocation | null> {
   const key = `${sourceURL}:${line}:${column}`;
   const cachedPromise = symbolicationCache.get(key);
   if (cachedPromise != null) {
@@ -46,7 +51,7 @@ export async function symbolicateSource(
   sourceURL: string,
   lineNumber: number, // 1-based
   columnNumber: number, // 1-based
-): Promise<ReactFunctionLocation | null> {
+): Promise<SourceMappedLocation | null> {
   const resource = await fetchFileWithCaching(sourceURL).catch(() => null);
   if (resource == null) {
     return null;
@@ -90,6 +95,8 @@ export async function symbolicateSource(
 
           const column = columnZeroBased + 1;
 
+          const ignored = false; // TODO
+
           if (possiblyURL === null) {
             return null;
           }
@@ -97,7 +104,10 @@ export async function symbolicateSource(
             // sourceMapURL = https://react.dev/script.js.map
             void new URL(possiblyURL); // test if it is a valid URL
 
-            return [functionName, possiblyURL, line, column];
+            return {
+              location: [functionName, possiblyURL, line, column],
+              ignored,
+            };
           } catch (e) {
             // This is not valid URL
             if (
@@ -107,7 +117,10 @@ export async function symbolicateSource(
               possiblyURL.slice(1).startsWith(':\\\\')
             ) {
               // This is an absolute path
-              return [functionName, possiblyURL, line, column];
+              return {
+                location: [functionName, possiblyURL, line, column],
+                ignored,
+              };
             }
 
             // This is a relative path
@@ -116,7 +129,10 @@ export async function symbolicateSource(
               possiblyURL,
               sourceMapURL,
             ).toString();
-            return [functionName, absoluteSourcePath, line, column];
+            return {
+              location: [functionName, absoluteSourcePath, line, column],
+              ignored,
+            };
           }
         } catch (e) {
           return null;
