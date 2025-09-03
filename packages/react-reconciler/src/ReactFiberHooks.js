a45a3543e4980b3e2a952e91a36abb781aa5860a
Andrew Clark <git@andrewclark.io>
2025-09-03 17:31:25 -0400

    Fix: uDV skipped initial value if earlier transition suspended
    Fixes a bug in useDeferredValue's optional `initialValue` argument. In
    the regression case, if a new useDeferredValue hook is mounted while an
    earlier transition is suspended, the `initialValue` argument of the
    new hook was ignored. After the fix, the `initialValue` argument is
    correctly rendered during the initial mount, regardless of whether
    other transitions were suspended.
    
    The culprit was related to the mechanism we use to track whether a
    render is the result of a `useDeferredValue` hook: we assign the
    deferred lane a TransitionLane, then entangle that lane with the
    DeferredLane bit. During the subsequent render, we check for the
    presence of the DeferredLane bit to determine whether to switch to
    the final, canonical value.
    
    But because transition lanes can themselves become entangled with
    other transitions, the effect is that every entangled transition was
    being treated as if it were the result of a `useDeferredValue` hook,
    causing us to skip the initial value and go straight to the final one.
    
    The fix I've chosen is to reserve some subset of TransitionLanes to be
    used only for deferred work, instead of using entanglement. This is
    similar to how retries are already implemented. Originally I tried not
    to implement it this way because it means there are now slightly fewer
    lanes allocated for regular transitions, but I underestimated how
    similar deferred work is to retries; they end  up having a lot of the
    same requirements. Eventually it may be possible to merge the
    two concepts.

--- packages/react-reconciler/src/ReactFiberHooks.js
+++ packages/react-reconciler/src/ReactFiberHooks.js
@@ -73,6 +73,7 @@ import {
   includesSomeLane,
   isGestureRender,
   GestureLane,
+  UpdateLanes,
 } from './ReactFiberLane';
 import {
   ContinuousEventPriority,
@@ -2983,6 +2984,20 @@ function rerenderDeferredValue<T>(value: T, initialValue?: T): T {
   }
 }
 
+function isRenderingDeferredWork(): boolean {
+  if (!includesSomeLane(renderLanes, DeferredLane)) {
+    // None of the render lanes are deferred lanes.
+    return false;
+  }
+  // At least one of the render lanes are deferred lanes. However, if the
+  // current render is also batched together with an update, then we can't
+  // say that the render is wholly the result of deferred work. We can check
+  // this by checking if the root render lanes contain any "update" lanes, i.e.
+  // lanes that are only assigned to updates, like setState.
+  const rootRenderLanes = getWorkInProgressRootRenderLanes();
+  return !includesSomeLane(rootRenderLanes, UpdateLanes);
+}
+
 function mountDeferredValueImpl<T>(hook: Hook, value: T, initialValue?: T): T {
   if (
     // When `initialValue` is provided, we defer the initial render even if the
@@ -2991,7 +3006,7 @@ function mountDeferredValueImpl<T>(hook: Hook, value: T, initialValue?: T): T {
     // However, to avoid waterfalls, we do not defer if this render
     // was itself spawned by an earlier useDeferredValue. Check if DeferredLane
     // is part of the render lanes.
-    !includesSomeLane(renderLanes, DeferredLane)
+    !isRenderingDeferredWork()
   ) {
     // Render with the initial value
     hook.memoizedState = initialValue;
@@ -3038,8 +3053,7 @@ function updateDeferredValueImpl<T>(
     }
 
     const shouldDeferValue =
-      !includesOnlyNonUrgentLanes(renderLanes) &&
-      !includesSomeLane(renderLanes, DeferredLane);
+      !includesOnlyNonUrgentLanes(renderLanes) && !isRenderingDeferredWork();
     if (shouldDeferValue) {
       // This is an urgent update. Since the value has changed, keep using the
       // previous value and spawn a deferred render to update it later.
