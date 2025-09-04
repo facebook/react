c94d76945916e8517283de233c8f4029234fbe87
Jorge Cabiedes Acosta <jorgecab@meta.com>
2025-09-04 15:35:04 -0700

    [compiler] Add catching useStates that shadow a reactive value

---
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/useEffect/error.shadowed-props-with-onchange.js
@@ -0,0 +1,15 @@
+// @validateNoDerivedComputationsInEffects
+
+function EndDate({startDate, endDate, onStartDateChange}) {
+   const [localStartDate, setLocalStartDate] = useState(startDate);
+
+    useEffect(() => {
+        setLocalStartDate(startDate);
+    }, [startDate]);
+
+    const onChange = (date) => {
+        setLocalStartDate(date);
+        onStartDateChange(date);
+    }
+    return <DateInput value={localStartDate} second={endDate} onChange={onChange} />
+}
