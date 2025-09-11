475551cb7445af27c88434fb09fb0a9d8bebb2b6
Eugene Choi <4eugenechoi@gmail.com>
2025-09-11 16:37:15 -0400

    Update test

--- compiler/apps/playground/playwright.config.js
+++ compiler/apps/playground/playwright.config.js
@@ -61,7 +61,10 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: {...devices['Desktop Chrome']},
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: {width: 1280, height: 1024},
+      },
     },
     // {
     //   name: 'Desktop Firefox',
