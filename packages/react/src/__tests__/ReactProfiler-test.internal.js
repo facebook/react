747deef71581b9f1b6dd608204372f57c19f017a
Sebastian Markbage <sebastian@calyptus.eu>
2025-09-16 21:39:42 -0400

    Update test

--- packages/react/src/__tests__/ReactProfiler-test.internal.js
+++ packages/react/src/__tests__/ReactProfiler-test.internal.js
@@ -179,6 +179,7 @@ describe(`onRender`, () => {
         'read current time',
         'read current time',
         'read current time',
+        'read current time',
       ]);
     } else {
       assertLog([
