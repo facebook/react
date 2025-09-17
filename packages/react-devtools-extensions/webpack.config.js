81d66927afd396f446fbacc3dbae2b2e0033f852
Sebastian "Sebbie" Silbermann <sebastian.silbermann@vercel.com>
2025-09-17 15:36:21 +0200

    [DevTools] Stop polyfilling `Buffer` (#34512)

--- packages/react-devtools-extensions/webpack.config.js
+++ packages/react-devtools-extensions/webpack.config.js
@@ -119,7 +119,6 @@ module.exports = {
   plugins: [
     new Webpack.ProvidePlugin({
       process: 'process/browser',
-      Buffer: ['buffer', 'Buffer'],
     }),
     new Webpack.DefinePlugin({
       __DEV__,
