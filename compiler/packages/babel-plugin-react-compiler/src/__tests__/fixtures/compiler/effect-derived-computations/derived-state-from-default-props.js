f5634cd894a08918c72d2623f014804cdb32e6f5
Jorge Cabiedes Acosta <jorgecab@meta.com>
2025-09-23 14:08:04 -0700

    [compiler] ValidateNoDerivedComputationsInEffects test cases
    Summary:
    This creates the test cases we expect this first iteration of calculate in render to catch
    
    Test Plan:
    Test cases

---
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/effect-derived-computations/derived-state-from-default-props.js
@@ -0,0 +1,17 @@
+// @validateNoDerivedComputationsInEffects
+
+export default function Component(input = 'empty') {
+  const [currInput, setCurrInput] = useState(input);
+  const localConst = 'local const';
+
+  useEffect(() => {
+    setCurrInput(input + localConst);
+  }, [input, localConst]);
+
+  return <div>{currInput}</div>;
+}
+
+export const FIXTURE_ENTRYPOINT = {
+  fn: Component,
+  params: [{input: 'test'}],
+};
