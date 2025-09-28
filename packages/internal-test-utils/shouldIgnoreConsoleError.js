5702f8e6b2efc2164f4905f5391518d44657071b
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-09-28 17:54:20 +0200

    [test] Fail tests on `console.error(error)`

--- packages/internal-test-utils/shouldIgnoreConsoleError.js
+++ packages/internal-test-utils/shouldIgnoreConsoleError.js
@@ -37,17 +37,6 @@ module.exports = function shouldIgnoreConsoleError(format, args) {
         return true;
       }
     }
-  } else {
-    if (
-      format != null &&
-      typeof format.message === 'string' &&
-      typeof format.stack === 'string' &&
-      args.length === 0
-    ) {
-      // In production, ReactFiberErrorLogger logs error objects directly.
-      // They are noisy too so we'll try to ignore them.
-      return true;
-    }
   }
   // Looks legit
   return false;
