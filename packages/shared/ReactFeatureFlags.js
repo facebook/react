cafada6abf439b3d64b3a4dae5b7b3131fe91111
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-10-01 13:56:29 +0200

    Enable React performance tracks everywhere
    Co-authored-by: Ruslan Lesiutin <28902667+hoxyq@users.noreply.github.com>

--- packages/shared/ReactFeatureFlags.js
+++ packages/shared/ReactFeatureFlags.js
@@ -229,7 +229,7 @@ export const enableProfilerTimer = __PROFILE__;
 // Component rendering tracks to show up in the Performance tab.
 // This flag will be used for both Server Component and Client Component tracks.
 // All calls should also be gated on enableProfilerTimer.
-export const enableComponentPerformanceTrack = __EXPERIMENTAL__;
+export const enableComponentPerformanceTrack: boolean = true;
 
 // Adds user timing marks for e.g. state updates, suspense, and work loop stuff,
 // for an experimental timeline tool.
