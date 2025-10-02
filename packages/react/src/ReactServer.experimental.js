6a8a8ef32692cc42b53d072d5ce6a6a84e190482
Sebastian "Sebbie" Silbermann <sebastian.silbermann@vercel.com>
2025-10-02 23:14:52 +0200

    [Flight] Add `<Activity>` (#34697)

--- packages/react/src/ReactServer.experimental.js
+++ packages/react/src/ReactServer.experimental.js
@@ -57,6 +57,7 @@ export {
 
 export {
   Children,
+  REACT_ACTIVITY_TYPE as Activity,
   REACT_FRAGMENT_TYPE as Fragment,
   REACT_PROFILER_TYPE as Profiler,
   REACT_STRICT_MODE_TYPE as StrictMode,
@@ -82,5 +83,4 @@ export {
   // Experimental
   REACT_SUSPENSE_LIST_TYPE as unstable_SuspenseList,
   REACT_VIEW_TRANSITION_TYPE as unstable_ViewTransition,
-  REACT_ACTIVITY_TYPE as Activity,
 };
