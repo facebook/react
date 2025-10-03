7fdb783520f89b0e59d91731e1f6f0522c20b6e2
Shiv Shankar Tiwari <pythonsst@gmail.com>
2025-10-03 20:58:57 +0530

    chore(fixtures): migrate attribute-behavior fixture to createRoot API

--- fixtures/attribute-behavior/src/index.js
+++ fixtures/attribute-behavior/src/index.js
@@ -1,6 +1,9 @@
-import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
