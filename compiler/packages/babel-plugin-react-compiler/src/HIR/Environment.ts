c1e0b0e4cb07e19a67e4f13bac07c00b4fff3004
Joe Savona <joesavona@fb.com>
2025-10-16 16:43:16 -0700

    [compiler] More fbt compatibility
    In my previous PR I fixed some cases but broke others. So, new approach. Two phase algorithm:
    
    * First pass is forward data flow to determine all usages of macros. This is necessary because many of Meta's macros have variants that can be accessed via properties, eg you can do `macro(...)` but also `macro.variant(...)`.
    * Second pass is backwards data flow to find macro invocations (JSX and calls) and then merge their operands into the same scope as the macro call.
    
    Still WIP, ideally `fbt.plural()` and `fbt.param()` would be treated as shallow, but if we do that then PromoteUsedTemporaries can sometimes promote temporaries (@mofeiz called it) just because there are other scopes in between the fbt.plural operand and fbt.plural call.

--- compiler/packages/babel-plugin-react-compiler/src/HIR/Environment.ts
+++ compiler/packages/babel-plugin-react-compiler/src/HIR/Environment.ts
@@ -83,21 +83,11 @@ export type ExternalFunction = z.infer<typeof ExternalFunctionSchema>;
 export const USE_FIRE_FUNCTION_NAME = 'useFire';
 export const EMIT_FREEZE_GLOBAL_GATING = '__DEV__';
 
-export const MacroMethodSchema = z.union([
-  z.object({type: z.literal('wildcard')}),
-  z.object({type: z.literal('name'), name: z.string()}),
-]);
-
-// Would like to change this to drop the string option, but breaks compatibility with existing configs
-export const MacroSchema = z.union([
-  z.string(),
-  z.tuple([z.string(), z.array(MacroMethodSchema)]),
-]);
+export const MacroSchema = z.string();
 
 export type CompilerMode = 'all_features' | 'no_inferred_memo';
 
 export type Macro = z.infer<typeof MacroSchema>;
-export type MacroMethod = z.infer<typeof MacroMethodSchema>;
 
 const HookSchema = z.object({
   /*
