49de326518c1d5afb13034921a71b68e10f4a3e5
Karl Horky <karl.horky@gmail.com>
2025-10-22 19:43:26 +0200

    Add hint for Node.js cjs-module-lexer for eslint-plugin-react-hook types

--- packages/eslint-plugin-react-hooks/index.js
+++ packages/eslint-plugin-react-hooks/index.js
@@ -1,1 +1,10 @@
 module.exports = require('./src/index.ts');
+
+// Hint to Node’s cjs-module-lexer to make named imports work
+// https://github.com/facebook/react/issues/34801#issuecomment-3433478810
+0 &&
+  (module.exports = {
+    meta,
+    rules,
+    configs
+  });
