904ce3494a3a70458b9f0156b794ff6a29e29c53
amit0-01 <amit22414@gmail.com>
2025-10-26 13:13:24 +0530

    minor changes

--- babel.config-ts.js
+++ babel.config-ts.js
@@ -10,7 +10,7 @@ module.exports = {
     ['@babel/plugin-transform-class-properties', {loose: true}],
     ['@babel/plugin-transform-private-methods', {loose: true}],
     '@babel/plugin-transform-classes',
-  ],
+  ], 
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-typescript',
