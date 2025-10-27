f8845faae0ee749f937a47d48fa529d2a444be3d
Jorge Cabiedes Acosta <jorgecab@meta.com>
2025-10-27 15:48:41 -0700

    [compiler] Switch to track states by aliasing and id instead of identifier names

--- compiler/packages/babel-plugin-react-compiler/src/Validation/ValidateNoDerivedComputationsInEffects_exp.ts
+++ compiler/packages/babel-plugin-react-compiler/src/Validation/ValidateNoDerivedComputationsInEffects_exp.ts
@@ -37,6 +37,11 @@ type DerivationMetadata = {
   sourcesIds: Set<IdentifierId>;
 };
 
+// type setStateKey = {
+//   id: IdentifierId;
+//   usageType: 'all' | 'effect';
+// };
+
 type ValidationContext = {
   readonly functions: Map<IdentifierId, FunctionExpression>;
   readonly errors: CompilerError;
@@ -44,6 +49,9 @@ type ValidationContext = {
   readonly effects: Set<HIRFunction>;
   readonly setStateCache: Map<string | undefined | null, Array<Place>>;
   readonly effectSetStateCache: Map<string | undefined | null, Array<Place>>;
+  // These should replace the setStateCache and effectSetStateCache once we have
+  readonly setStateLoads: Map<IdentifierId, IdentifierId | null>;
+  readonly setStateUsages: Map<IdentifierId, SourceLocation>;
 };
 
 class DerivationCache {
@@ -189,6 +197,9 @@ export function validateNoDerivedComputationsInEffects_exp(
     Array<Place>
   > = new Map();
 
+  const setStateLoads: Map<IdentifierId, IdentifierId> = new Map();
+  const setStateUsages: Map<IdentifierId, SourceLocation> = new Map();
+
   const context: ValidationContext = {
     functions,
     errors,
@@ -196,6 +207,8 @@ export function validateNoDerivedComputationsInEffects_exp(
     effects,
     setStateCache,
     effectSetStateCache,
+    setStateLoads,
+    setStateUsages,
   };
 
   if (fn.fnType === 'Hook') {
@@ -242,10 +255,8 @@ export function validateNoDerivedComputationsInEffects_exp(
     validateEffect(effect, context);
   }
 
-  console.log(derivationCache.cache);
-  if (errors.hasAnyErrors()) {
-    throw errors;
-  }
+  console.log('SetStateLoads', context.setStateLoads);
+  return errors.asResult();
 }
 
 function recordPhiDerivations(
@@ -288,11 +299,29 @@ function joinValue(
   return 'fromPropsAndState';
 }
 
+function maybeRecordSetState(instr: Instruction, context: ValidationContext) {
+  for (const operand of eachInstructionLValue(instr)) {
+    if (isSetStateType(operand.identifier)) {
+      if (instr.value.kind === 'LoadLocal') {
+        context.setStateLoads.set(
+          operand.identifier.id,
+          instr.value.place.identifier.id,
+        );
+      } else {
+        context.setStateLoads.set(operand.identifier.id, null);
+      }
+    }
+  }
+}
+
 function recordInstructionDerivations(
   instr: Instruction,
   context: ValidationContext,
   isFirstPass: boolean,
 ): void {
+  // WIP
+  maybeRecordSetState(instr, context);
+
   let typeOfValue: TypeOfValue = 'ignored';
   const sources: Set<IdentifierId> = new Set();
   const {lvalue, value} = instr;
