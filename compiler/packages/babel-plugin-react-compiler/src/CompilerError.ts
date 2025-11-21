40b4a5bf71ba7864556a5589b270b237f453c032
Joseph Savona <6425824+josephsavona@users.noreply.github.com>
2025-11-20 19:30:35 -0800

    [compiler] ValidateExhaustiveDeps disallows unnecessary non-reactive deps (#34472)
    Just to be consistent, we disallow unnecessary deps even if they're
    known to be non-reactive.

--- compiler/packages/babel-plugin-react-compiler/src/CompilerError.ts
+++ compiler/packages/babel-plugin-react-compiler/src/CompilerError.ts
@@ -600,7 +600,8 @@ function printErrorSummary(category: ErrorCategory, message: string): string {
     case ErrorCategory.Suppression:
     case ErrorCategory.Syntax:
     case ErrorCategory.UseMemo:
-    case ErrorCategory.VoidUseMemo: {
+    case ErrorCategory.VoidUseMemo:
+    case ErrorCategory.MemoDependencies: {
       heading = 'Error';
       break;
     }
@@ -658,6 +659,10 @@ export enum ErrorCategory {
    * Checks that manual memoization is preserved
    */
   PreserveManualMemo = 'PreserveManualMemo',
+  /**
+   * Checks for exhaustive useMemo/useCallback dependencies without extraneous values
+   */
+  MemoDependencies = 'MemoDependencies',
   /**
    * Checks for known incompatible libraries
    */
@@ -1052,6 +1057,16 @@ function getRuleForCategoryImpl(category: ErrorCategory): LintRule {
         name: 'void-use-memo',
         description:
           'Validates that useMemos always return a value and that the result of the useMemo is used by the component/hook. See [`useMemo()` docs](https://react.dev/reference/react/useMemo) for more information.',
+        preset: LintRulePreset.RecommendedLatest,
+      };
+    }
+    case ErrorCategory.MemoDependencies: {
+      return {
+        category,
+        severity: ErrorSeverity.Error,
+        name: 'memo-dependencies',
+        description:
+          'Validates that useMemo() and useCallback() specify comprehensive dependencies without extraneous values. See [`useMemo()` docs](https://react.dev/reference/react/useMemo) for more information.',
         preset: LintRulePreset.RecommendedLatest,
       };
     }
