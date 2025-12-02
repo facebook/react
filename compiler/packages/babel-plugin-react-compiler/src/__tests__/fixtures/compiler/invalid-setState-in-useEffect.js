93c64c963636b8e144eaf75a4fb5c54c46ae6941
jynxio <jinxiaomatrix@gmail.com>
2025-12-03 02:50:01 +0800

    [compiler] Add fixture for React.useEffect in set-state-in-effect validation

--- compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/invalid-setState-in-useEffect.js
+++ compiler/packages/babel-plugin-react-compiler/src/__tests__/fixtures/compiler/invalid-setState-in-useEffect.js
@@ -1,10 +1,14 @@
 // @loggerTestOnly @validateNoSetStateInEffects
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 function Component() {
   const [state, setState] = useState(0);
+  const [state2, setState2] = React.useState(0);
   useEffect(() => {
     setState(s => s + 1);
   });
-  return state;
+  React.useEffect(() => {
+    setState2(s => s + 1);
+  });
+  return state + state2;
 }
