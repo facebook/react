34fc0cbba5f6fbf48d57f1a51a27ec222eaed7e5
pranamya123 <47019410+pranamya123@users.noreply.github.com>
2026-01-09 04:15:46 +0000

    removed warning and the corresponding test

--- packages/react-reconciler/src/ReactFiberReconciler.js
+++ packages/react-reconciler/src/ReactFiberReconciler.js
@@ -460,18 +460,6 @@ function updateContainerImpl(
   }
 }
 
-if (__DEV__) {
-  Object.defineProperty(exports, 'flushSync', {
-    get() {
-      console.warn(
-        'flushSync has been removed in react-reconciler v0.31+. ' +
-        'Please use updateContainerSync() and flushSyncWork() instead.'
-      );
-      return undefined;
-    }
-  });
-}
-
 export {
   batchedUpdates,
   deferredUpdates,
