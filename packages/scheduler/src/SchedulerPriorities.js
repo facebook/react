68d795fe8b28db49c9a9f709a6a2af5b7222afb8
Alice Kile <buoyantair@protonmail.com>
2026-01-21 19:57:27 +0100

    remove unnecessary type casting

--- packages/scheduler/src/SchedulerPriorities.js
+++ packages/scheduler/src/SchedulerPriorities.js
@@ -14,24 +14,22 @@ export opaque type NormalPriorityType: symbol = symbol;
 export opaque type LowPriorityType: symbol = symbol;
 export opaque type IdlePriorityType: symbol = symbol;
 
-export const NoPriority: NoPriorityType = (Symbol.for(
-  'scheduler.no_priority',
-): any);
-export const ImmediatePriority: ImmediatePriorityType = (Symbol.for(
+export const NoPriority: NoPriorityType = Symbol.for('scheduler.no_priority');
+export const ImmediatePriority: ImmediatePriorityType = Symbol.for(
   'scheduler.immediate_priority',
-): any);
-export const UserBlockingPriority: UserBlockingPriorityType = (Symbol.for(
+);
+export const UserBlockingPriority: UserBlockingPriorityType = Symbol.for(
   'scheduler.user_blocking_priority',
-): any);
-export const NormalPriority: NormalPriorityType = (Symbol.for(
+);
+export const NormalPriority: NormalPriorityType = Symbol.for(
   'scheduler.normal_priority',
-): any);
-export const LowPriority: LowPriorityType = (Symbol.for(
+);
+export const LowPriority: LowPriorityType = Symbol.for(
   'scheduler.low_priority',
-): any);
-export const IdlePriority: IdlePriorityType = (Symbol.for(
+);
+export const IdlePriority: IdlePriorityType = Symbol.for(
   'scheduler.idle_priority',
-): any);
+);
 
 export type PriorityLevel =
   | NoPriorityType
