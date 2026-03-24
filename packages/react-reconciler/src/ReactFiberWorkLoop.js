5ba0de3b4e2bd70489db95c10491ab871814cb06
Dan Abramov <dan.abramov@gmail.com>
2026-03-24 00:30:15 +0000

    Fix #35821 by recording pinged lanes

--- packages/react-reconciler/src/ReactFiberWorkLoop.js
+++ packages/react-reconciler/src/ReactFiberWorkLoop.js
@@ -5040,6 +5040,13 @@ function pingSuspendedRoot(
         // the special internal exception that we use to interrupt the stack for
         // selective hydration. That was temporarily reverted but we once we add
         // it back we can use it here.
+        //
+        // In the meantime, record the pinged lanes so markRootSuspended won't
+        // mark them as suspended, allowing a retry.
+        workInProgressRootPingedLanes = mergeLanes(
+          workInProgressRootPingedLanes,
+          pingedLanes,
+        );
       }
     } else {
       // Even though we can't restart right now, we might get an
