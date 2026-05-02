1ae9064b51aa54816d0799e07394bb9a3b201e8d
José Eduardo Ormeño Meneses <eduardoorm79@gmail.com>
2026-05-02 15:39:01 -0500

    fix(anonymize): remove duplicate copyright notice

--- compiler/scripts/anonymize.js
+++ compiler/scripts/anonymize.js
@@ -5,6 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 'use strict';
 
 const fs = require('fs');
