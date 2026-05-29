6a44d945c30887f9d64f27137fb361a086d0800d
Hendrik Liebau <mail@hendrik-liebau.de>
2026-05-29 13:52:10 +0200

    Use `setTimeout`, not `fs`

--- fixtures/flight/src/FileReader.js
+++ fixtures/flight/src/FileReader.js
@@ -1,16 +1,16 @@
-import {readFile, writeFile} from 'fs/promises';
-
 export default async function FileReader() {
   // This debug string is below the threshold for debug string length, so its
-  // value is sent to the client as the awaited value of the read.
-  await writeFile(`/tmp/react-flight-fixture-below.txt`, 'o'.repeat(1000000));
-  await readFile(`/tmp/react-flight-fixture-below.txt`, 'utf-8');
+  // value is sent to the client as the awaited value.
+  await new Promise(resolve => {
+    setTimeout(() => resolve('o'.repeat(1000000)), 1);
+  });
 
   // This debug string is above the threshold for debug string length, so the
   // client receives a placeholder as the awaited value instead of the actual
   // string.
-  await writeFile(`/tmp/react-flight-fixture-above.txt`, 'x'.repeat(1000001));
-  await readFile(`/tmp/react-flight-fixture-above.txt`, 'utf-8');
+  await new Promise(resolve => {
+    setTimeout(() => resolve('x'.repeat(1000001)), 1);
+  });
 
   return <p>FileReader</p>;
 }
